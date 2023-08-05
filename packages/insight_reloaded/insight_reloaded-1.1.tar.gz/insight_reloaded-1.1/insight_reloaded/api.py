6a8665ac1604ef784a6628091746c4ee9761114e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:45 +0000

    Add package insight_reloaded/1.1/insight_reloaded-1.1.tar.gz

---
+++ packages/insight_reloaded/insight_reloaded-1.1.tar.gz/insight_reloaded-1.1/insight_reloaded/api.py
@@ -0,0 +1,130 @@
+import json
+import os
+
+import tornadoredis
+import tornado.web
+import tornado.ioloop
+import tornado.gen
+import tornado.httpserver
+
+from insight_reloaded.insight_settings import (
+    REDIS_HOST, REDIS_PORT, REDIS_DB, SENTRY_DSN, REDIS_QUEUE_KEYS,
+    DEFAULT_REDIS_QUEUE_KEY, CROP_SIZE)
+
+
+try:
+    from raven import Client
+except ImportError:
+    if SENTRY_DSN:
+        SENTRY_DSN = None
+        print "SENTRY_DSN is defined but raven isn't installed."
+
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+try:
+    with open(os.path.join(HERE, '../VERSION')) as f:
+        VERSION = f.readlines()[0].strip()
+except IOError:
+    VERSION = 'N/A'
+
+
+c = tornadoredis.Client(host=REDIS_HOST, port=REDIS_PORT, selected_db=REDIS_DB)
+c.connect()
+
+
+class MainHandler(tornado.web.RequestHandler):
+    """Convert the querystring in a REDIS job JSON."""
+
+    @tornado.web.asynchronous
+    def get(self, queue):
+        # If there is no argument, display the version
+        if self.request.arguments == {}:
+            self.write(dict(insight_reloaded="Bonjour",
+                            name="insight-reloaded",
+                            version=VERSION))
+            self.finish()
+            return
+
+        if not queue:
+            queue = DEFAULT_REDIS_QUEUE_KEY
+
+        if queue not in REDIS_QUEUE_KEYS:
+            raise tornado.web.HTTPError(404, 'Queue "%s" not found' % queue)
+
+        self.queue = queue
+
+        # Else process the request
+        params = {'url': self.get_argument("url", None),
+                  'callback': self.get_argument("callback", None),
+                  }
+        # Get URL
+        if params['url']:
+            if params['url'].startswith('/'):
+                params['url'] = '%s://%s%s' % (self.request.protocol,
+                                               self.request.host,
+                                               params['url'])
+        else:
+            raise tornado.web.HTTPError(404, 'Input file not found')
+
+        # Max number of pages to compile
+        try:
+            params['max_previews'] = int(self.get_argument('pages', 20))
+        except:
+            params['max_previews'] = 20
+
+        try:
+            params['crop'] = int(self.get_argument('crop', 0))
+        except ValueError:
+            params['crop'] = CROP_SIZE
+
+        message = json.dumps(params)
+        c.rpush(self.queue, message, callback=self.on_response)
+
+    def on_response(self, response):
+        self.write(dict(insight_reloaded="Job added to queue '%s'." %
+                        self.queue,
+                        number_in_queue=response))
+        self.finish()
+
+
+class StatusHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    def get(self, queue):
+        if not queue:
+            queue = DEFAULT_REDIS_QUEUE_KEY
+
+        if queue not in REDIS_QUEUE_KEYS:
+            raise tornado.web.HTTPError(404, "Queue '%s' not found" % queue)
+        self.queue = queue
+        c.llen(self.queue, callback=self.on_response)
+
+    def on_response(self, response):
+        self.write(dict(insight_reloaded="There is %d job in the '%s' queue." %
+                        (response, self.queue),
+                        number_in_queue=response))
+        self.finish()
+
+
+application = tornado.web.Application([
+    (r"/([A-Za-z0-9_-]*)/?status", StatusHandler),
+    (r"/([A-Za-z0-9_-]*)", MainHandler),
+])
+
+
+def main():
+    if SENTRY_DSN:
+        client = Client(dsn=SENTRY_DSN)
+    else:
+        client = None
+    print "Launch tornado ioloop"
+    application.listen(8888)
+    try:
+        tornado.ioloop.IOLoop.instance().start()
+    except Exception:
+        if client:
+            client.get_ident(client.captureException())
+        else:
+            raise
+
+if __name__ == "__main__":
+    main()
---
+++ packages/insight_reloaded/insight_reloaded-1.1.tar.gz/insight_reloaded-1.1/setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""Python packaging."""
+from os.path import abspath, dirname, join
+from setuptools import setup, find_packages
+
+
+def read_relative_file(filename):
+    """Returns contents of the given file, which path is supposed relative
+    to this module."""
+    with open(join(dirname(abspath(__file__)), filename)) as f:
+        return f.read()
+
+name = 'insight_reloaded'
+version = read_relative_file('VERSION').strip()
+readme = read_relative_file('README')
+requirements = ['setuptools', 'tornado', 'tornado-redis',
+                'requests', 'redis', 'raven', 'boto']
+entry_points = {'console_scripts':
+                ['insight_api = insight_reloaded.api:main',
+                 'insight = insight_reloaded.worker:main',
+                 'fake_callback = insight_reloaded.fake_callback:main',
+                 ]
+                }
+
+
+if __name__ == '__main__':  # ``import setup`` doesn't trigger setup().
+    setup(name=name,
+          version=version,
+          description="""
+          A full async docsplit previewer server based on Tornado and Redis""",
+          long_description=readme,
+          classifiers=["Programming Language :: Python",
+                       'License :: Other/Proprietary License',
+                       ],
+          keywords='',
+          author='Novapost',
+          author_email='rd@novapost.fr',
+          url='https://github.com/novagile/%s' % name,
+          license='closed source',
+          packages=find_packages(),
+          include_package_data=True,
+          zip_safe=False,
+          install_requires=requirements,
+          entry_points=entry_points,
+          )
