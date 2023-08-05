d1d0e41088d0198301d230fcbab869b4a44c35b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:35 +0000

    Add package tkrequest/0.1.1/tkrequest-0.1.1-py3.3.egg

---
+++ packages/tkrequest/tkrequest-0.1.1-py3.3.egg/tkrequest/lib/settings.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python
+# coding=utf-8
+# Stan 2013-05-02
+
+from __future__ import ( division, absolute_import,
+                         print_function, unicode_literals )
+
+import sys, os, re, time, pickle, logging
+
+try:
+    try:    from .info import __VERSION__
+    except: from info import __VERSION__
+except:
+    __VERSION__ = '<undefined>'
+
+
+def save_entry(filename, entry):
+    with open(filename, 'wb') as f:
+        pickle.dump(entry, f, 2)
+
+
+def load_entry(filename):
+    entry = {}
+    if os.path.exists(filename):
+        if os.path.isfile(filename):
+            with open(filename, 'rb') as f:
+                try:
+                    entry = pickle.load(f)
+                except Exception as e:
+                    logging.error("Unable read/parse file: {0}".format(filename))
+        else:
+            logging.error("{0} must be a file!".format(filename))
+    return entry
+
+
+class Settings(object):
+    def __init__(self, name=None, app=None, location=None, for_instance=False):
+        self.home = os.path.expanduser("~")
+
+        abspath = os.path.abspath(__file__)
+        self.instance = os.path.dirname(os.path.dirname(abspath))
+
+        _basename = os.path.basename(self.instance)
+        _instancename = re.sub(r'\W', '_', self.instance)
+
+        self.location = os.path.join(self.home, ".config") if location is None \
+            else self.expand_path(location)
+        self.app = _basename if app is None else app
+        self.name = _basename if name is None else name
+
+        if for_instance:
+            self.path = os.path.join(self.location, self.app, _instancename)
+        else:
+            self.path = os.path.join(self.location, self.app)
+        self.check_path(self.path)
+        self.filename = os.path.join(self.path, "{0}.pickle".format(self.name))
+        self.settings = load_entry(self.filename)
+
+
+#     def __del__(self):
+#         save_entry(self.filename, self.settings)
+
+
+    def __iter__(self):
+        for key in sorted(self.settings.keys()):
+            yield key, self.settings[key]
+
+
+    def flush(self):
+        save_entry(self.filename, self.settings)
+
+
+    def get_dict(self):
+        return self.settings
+
+
+    def contains(self, key):
+        return key in self.settings
+
+
+    def get(self, key, default=None):
+        return self.settings.setdefault(key, default)
+
+
+    def set(self, key, value):
+        self.settings[key] = value
+        self.flush()
+
+
+    def remove(self, key):
+        if key in self.settings:
+            del self.settings[key]
+        self.flush()
+
+
+    def append(self, key, value, mode=0):
+        values_list = self.get(key, [])
+        if mode == 0:
+            values_list.append(value)
+        elif mode == 1 and value not in values_list:
+            values_list.append(value)
+        elif mode == 2:
+            if value in values_list:
+                values_list.remove(value)
+            values_list.append(value)
+        self.set(key, values_list)
+
+        return values_list
+
+
+    def insert(self, key, seq, value, mode=0):
+        values_list = self.get(key, [])
+        if mode == 0:
+            values_list.insert(seq, value)
+        elif mode == 1 and value not in values_list:
+            values_list.insert(seq, value)
+        elif mode == 2:
+            if value in values_list:
+                values_list.remove(value)
+            values_list.insert(seq, value)
+        self.set(key, values_list)
+
+        return values_list
+
+
+    def saveEnv(self):
+        if not self.contains("firsttime/time"):
+            self.saveEnv_d("firsttime")
+
+        self.saveEnv_d("lasttime")
+
+        runs = self.get("runs")
+        runs = runs + 1 if isinstance(runs, int) else 1
+        self.set("runs", runs)
+
+
+    def saveEnv_d(self, d=""):
+        tt, ct = time.time(), time.ctime()
+        self.set(d+"/time", tt)
+        self.set(d+"/time_str", ct)
+        self.set(d+"/Python", sys.version)
+        self.set(d+"/Core", __VERSION__)
+
+
+    def init_path(self, key, default, check=None):
+        value = self.get(key)
+
+        if not value or not isinstance(value, basestring):
+            value = self.expand_path(default)
+            self.set(key, value)
+
+        if check and not self.check_path(value):
+            self.remove(key)    # !!!
+
+
+    def set_path(self, key, path, check=None):
+        value = self.expand_path(path)
+        self.set(key, value)
+
+        if check and not self.check_path(value):
+            self.remove(key)    # !!!
+
+
+    def expand_prefix(self, path):
+        if path == '~':
+            return self.home
+        elif path == '~~':
+            return os.path.join(self.location)
+        elif path == '~~~':
+            return os.path.join(self.path)
+        elif path == '$':
+            return self.instance
+
+
+    def expand_path(self, path):
+        res = re.match('(~{1,3}|\$)(.*)', path)
+        if res:
+            prefix, path = res.groups()
+            return os.path.join(self.expand_prefix(prefix), path)
+        else:
+            return path
+
+
+    def check_path(self, path):
+        if not os.path.exists(path):
+            logging.info(u"Creating directory: {0}".format(path))
+            os.makedirs(path)
+
+        if os.path.isdir(path):
+            return True
+        else:
+            logging.error(u"Could not create directory: {0}".format(path))
+            return False
---
+++ packages/tkrequest/tkrequest-0.1.1-py3.3.egg/tkrequest/tkrequest.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python
+# coding=utf-8
+# Stan 2012-09-06, 2013-05-01
+
+from __future__ import ( division, absolute_import,
+                         print_function, unicode_literals )
+
+import sys, socket, webbrowser
+
+try:
+    from .lib.info import __VERSION__
+    from .lib.backwardcompat import *
+    from .lib.dump import plain
+    from .lib.settings import Settings
+except:
+    from lib.info import __VERSION__
+    from lib.backwardcompat import *
+    from lib.dump import plain
+    from lib.settings import Settings
+
+
+py_version = sys.version_info[:2]
+PY3 = py_version[0] == 3
+
+
+s = Settings()
+s.saveEnv()
+
+
+# recipe from http://effbot.org/zone/tk-menubar.htm
+class AppUI(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self.title("tkRequest")
+
+        self.Text = None
+        self.Entry = None
+
+        self.url = tk.StringVar()
+
+        self.status = tk.StringVar()
+        self.setStatus()
+
+        self.menubar = tk.Menu(self)
+
+        menu = tk.Menu(self.menubar, tearoff=0)
+        self.menubar.add_cascade(label="Help", menu=menu)
+        menu.add_command(command=self.onAbout, label="About")
+
+        try:
+            self.config(menu=self.menubar)
+        except AttributeError:
+            # master is a toplevel window (Python 1.4/tk 1.63)
+            self.tk.call(master, "config", "-menu", self.menubar)
+
+    def assignText(self, Text):
+        self.Text = Text
+
+    def assignEntry(self, Entry):
+        self.Entry = Entry
+
+    def setText(self, text=""):
+        self.Text.delete(1.0, tk.END)
+        self.Text.insert(tk.INSERT, "{0}\n".format(text))
+
+    def setEntry(self, values_list=[]):
+        self.Entry['values'] = values_list
+
+    def setStatus(self, text=""):
+        status = sys.executable
+        if text:
+            status += " :: " + text
+        self.status.set(status)
+
+    def onAbout(self):
+        print("Version {0}".format(__VERSION__))
+
+    def onRequest(self, event=None):
+        url = self.url.get()
+        urls = s.insert("urls", 0, url, 2)
+        self.setEntry(urls)
+        output = self.request(url, [])
+        text = "\n".join(output)
+        self.setText(text)
+
+    def onOpenLink(self, event=None):
+        if event.state == 4:
+            url = self.url.get()
+            webbrowser.open(url)
+        if event.state == 0:
+            wtype = event.widget.winfo_class()
+            if wtype not in ['Entry', 'Text', 'TCombobox']:
+                url = self.url.get()
+                webbrowser.open(url)
+
+    def request(self, url, output = [], level=0):
+        output.append("=== {0} ===".format(url))
+
+        try:
+            r = urllib2.Request(url)
+            page = urllib2.urlopen(r)
+            output.append(r.get_method())
+            output.append(r.get_type())
+            for h, v in r.header_items():
+                output.append("  {0}: {1}".format(h, v))
+            output.append("")
+
+        except Exception as e:
+            output.append(plain(e))
+
+        else:
+            head = page.info()._headers if PY3 else page.info().headers
+            output.append(plain(head))
+            output.append("")
+
+#             html = page.readlines()
+#             for i in html:
+#                 output.append(plain(i))
+#             output.append("")
+
+            returl = page.geturl()
+            if returl != url:
+                if level <= 15:
+                    self.request(returl, output, level+1)
+                else:
+                    pass
+
+        return output
+
+
+def main():
+    root = AppUI()
+
+    line1 = tk.Frame(root)
+    line2 = tk.Frame(root)
+    line3 = tk.Frame(root)
+
+    # Address
+    entry1 = ttk.Combobox(line1, textvariable=root.url)
+    root.assignEntry(entry1)
+    urls = s.get('urls', ["http://localhost:80/"])
+    root.setEntry(urls)
+    entry1.current(0)
+
+    button1 = tk.Button(line1, text="Request")
+
+    # Text Widget
+    dFont1 = Font(family="Courier", size=9)
+    text1 = tk.Text(line2, font=dFont1)
+    root.assignText(text1)
+    text1_yscrollbar = tk.Scrollbar(line2, orient=tk.VERTICAL, command=text1.yview)
+    text1['yscrollcommand'] = text1_yscrollbar.set
+
+    # Status
+    label1 = tk.Label(line3, textvariable=root.status, anchor=tk.W)
+
+    # Pack
+    line1.pack(fill=tk.X)
+    line2.pack(fill=tk.BOTH, expand=1)
+    line3.pack(fill=tk.X)
+
+    entry1.pack(side=tk.LEFT, fill=tk.X, expand=1)
+    button1.pack(side=tk.RIGHT)
+
+    text1.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
+    text1_yscrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+    label1.pack(fill=tk.X)
+
+    # Bind
+    button1.bind("<Button-1>", root.onRequest)
+    entry1.bind("<KeyPress-Return>", root.onRequest)
+    root.bind("w", root.onOpenLink)
+
+    # Main loop
+    root.update_idletasks()
+    root.minsize(root.winfo_reqwidth(), root.winfo_reqheight())
+    root.mainloop()
+
+
+
+if __name__ == '__main__':
+    main()
