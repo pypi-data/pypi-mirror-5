72a3dc546998c31f260026bb62353bf9b5241695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:25 +0000

    Add package json2xml/0.6/json2xml-0.6.tar.gz

---
+++ packages/json2xml/json2xml-0.6.tar.gz/json2xml-0.6/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+version = '0.6'
+
+setup(
+    name='json2xml',
+    version=version,
+    description='To covert json data to xml data',
+    author='Vinit Kumar',
+    author_email='vinit.kumar@changer.nl',
+    url='http://github.com:vinitcool76/json2xml.git',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
+    ],
+)
