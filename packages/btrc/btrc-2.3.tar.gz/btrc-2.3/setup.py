f7109d2eed5fb58a12383c69fcfdf4c014480166
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:38 +0000

    Add package btrc/2.3/btrc-2.3.tar.gz

---
+++ packages/btrc/btrc-2.3.tar.gz/btrc-2.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='btrc',
+    version='2.3',
+    description='Couchbase view stats collector',
+    author='Pavel Paulau',
+    author_email='pavel.paulau@gmail.com',
+    py_modules = ['btrc'],
+    entry_points={
+        'console_scripts': ['btrc = btrc:main']
+    },
+    install_requires=['requests==1.2.0'],
+    setup_requires=[],
+    tests_require=[],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
