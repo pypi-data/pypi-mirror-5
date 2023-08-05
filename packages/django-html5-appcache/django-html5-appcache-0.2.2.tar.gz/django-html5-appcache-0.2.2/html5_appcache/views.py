4e93f20d0bb78db9bc64807c11c5d4a70f38fa1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:45 +0000

    Add package django-html5-appcache/0.2.2/django-html5-appcache-0.2.2.tar.gz

---
+++ packages/django-html5-appcache/django-html5-appcache-0.2.2.tar.gz/django-html5-appcache-0.2.2/html5_appcache/views.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from django.utils.translation import ugettext_lazy as _
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+from html5_appcache import appcache_registry
+
+
+class ManifestAppCache(TemplateView):
+    """
+    Basic template view.
+    It just get the temlate from AppCacheManager and wrap it in a response
+    """
+    template_name = "html5_appcache/manifest"
+    appcache_update = 0
+
+    def get(self, request, *args, **kwargs):
+        appcache_registry.setup(self.request, self.template_name)
+        manifest = appcache_registry.get_manifest(update=kwargs.get("appcache_update", False))
+        if manifest:
+            return HttpResponse(content=manifest, content_type="text/cache-manifest")
+        return HttpResponse(content="CACHE MANIFEST", content_type="text/cache-manifest")
