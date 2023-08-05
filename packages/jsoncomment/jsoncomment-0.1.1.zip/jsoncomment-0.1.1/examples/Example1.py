e1fa9f3fe9306df4bb43377f0e42bf0ac9b64042
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:05 +0000

    Add package jsoncomment/0.1.1/jsoncomment-0.1.1.zip

---
+++ packages/jsoncomment/jsoncomment-0.1.1.zip/jsoncomment-0.1.1/examples/Example1.py
@@ -0,0 +1,42 @@
+﻿
+################################################################################
+
+import ujson
+
+from jsoncomment import JsonComment
+
+################################################################################
+
+if __name__ == '__main__':
+
+	string = """
+		/******************
+		Comment 1
+		Comment 2
+		******************/
+		[
+			# Objects
+			{
+				"key" : "value",
+				"another key" :
+				\"\"\"
+				A multiline string.\\n
+				No spacing or formatting is saved, 
+				the whole line is joined with all edge spaces
+				trimmed.
+				\"\"\"
+			},
+			; Other Values
+			81,
+			true
+		]
+	"""
+
+	parser = JsonComment(ujson)
+	parsed_object = parser.loads(string)
+
+	print(parsed_object[0]["another key"], "\n")
+
+	print(parser.dumps(parsed_object))
+
+################################################################################
---
+++ packages/jsoncomment/jsoncomment-0.1.1.zip/jsoncomment-0.1.1/examples/Example2.py
@@ -0,0 +1,23 @@
+﻿
+################################################################################
+
+from codecs import open
+
+import ujson
+
+from jsoncomment import JsonComment
+
+################################################################################
+
+if __name__ == '__main__':
+	parser = JsonComment(ujson)
+
+	with open("Example2.json", "r", "utf-8-sig") as file_json:
+		parsed_object = parser.load(file_json)
+
+		print(parsed_object["item 1"], "\n")
+		print(parsed_object["Section/Subsection"], "\n")
+
+		print(parser.dumps(parsed_object))
+
+################################################################################
