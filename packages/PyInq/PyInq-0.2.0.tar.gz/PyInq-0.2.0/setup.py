11a3f6790b55b66973f8116778131bd966d949eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:40 +0000

    Add package PyInq/0.2.0/PyInq-0.2.0.tar.gz

---
+++ packages/PyInq/PyInq-0.2.0.tar.gz/PyInq-0.2.0/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name='PyInq',
+    version='0.2.0',
+    author='Austin Noto-Moniz',
+    author_email='metalnut4@netscape.net',
+    url='http://pypi.python.org/pypi/PyInq',
+    packages=['pyinq','pyinq.asserts','pyinq.parsers','pyinq.tags','pyinq.printers','pyinq.printers.html','pyinq.printers.cli','pyinq.printers.cli.console','pyinq.printers.cli.bash'],
+    description='Python unit test framework, an alternative to unittest.',
+    long_description=open('README.txt').read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Testing'
+   ]
+)
