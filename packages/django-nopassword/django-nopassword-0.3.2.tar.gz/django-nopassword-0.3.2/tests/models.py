854612249547d8a96c97a2383f78fe1492d3db24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:04 +0000

    Add package django-nopassword/0.3.2/django-nopassword-0.3.2.tar.gz

---
+++ packages/django-nopassword/django-nopassword-0.3.2.tar.gz/django-nopassword-0.3.2/tests/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+try:
+    from django.contrib.auth.models import AbstractUser
+except ImportError:
+    from django.db.models import Model as AbstractUser
+
+
+class CustomUser(AbstractUser):
+    extra_field = models.CharField(max_length=2)
