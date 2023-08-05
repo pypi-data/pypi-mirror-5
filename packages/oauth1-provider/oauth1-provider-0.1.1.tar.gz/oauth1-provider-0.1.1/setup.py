4277e9493463285315074902a9e6c0827d6ba7f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:01 +0000

    Add package oauth1-provider/0.1.1/oauth1-provider-0.1.1.tar.gz

---
+++ packages/oauth1-provider/oauth1-provider-0.1.1.tar.gz/oauth1-provider-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+def readme():
+    with open('README') as f:
+        return f.read()
+
+setup(name='oauth1-provider',
+      version='0.1.1',
+      description='OAuth 1.0 Provider with Redis in Python',
+      long_description=readme(),
+      keywords='oauth redis xauth',
+      url='https://github.com/tistaharahap/oauth1-provider-redis-py/',
+      author='Batista Harahap',
+      author_email='batista@bango29.com',
+      license='MIT',
+      packages=['oauth1-provider'],
+      install_requires=[
+          'flask',
+          'redis'],
+      zip_safe=False)
