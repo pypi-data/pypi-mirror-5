a261e6277e03a4645cf980fc848be4a2f4d5289e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:36 +0000

    Add package deploy/1.6.0/deploy-1.6.0.tar.gz

---
+++ packages/deploy/deploy-1.6.0.tar.gz/deploy-1.6.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "deploy",
+    version = "1.6.0",
+    url = 'http://ondrejsika.com/docs/deploy',
+    download_url = 'https://github.com/sikaondrej/deploy',
+    license = 'GNU LGPL v.3',
+    description = "Easy deploy Python WSGI apps",
+    author = 'Ondrej Sika',
+    author_email = 'dev@ondrejsika.com',
+    packages = find_packages(),
+    scripts = [
+        "deploy/bin/deploy",
+        "deploy/bin/deploy-startserver",
+        "deploy/bin/deploy-serverconf",
+        "deploy/bin/deploy-init",
+        "deploy/bin/deploy-restart",
+        "deploy/bin/deploy-remove",
+    ],
+    install_requires = ["pip", ],
+    include_package_data = True,
+)
