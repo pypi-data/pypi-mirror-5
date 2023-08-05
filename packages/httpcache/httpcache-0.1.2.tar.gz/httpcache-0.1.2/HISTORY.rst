9ce0e8fb5b0482a277d50a6a4221451ce331dbdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:18 +0000

    Add package httpcache/0.1.2/httpcache-0.1.2.tar.gz

---
+++ packages/httpcache/httpcache-0.1.2.tar.gz/httpcache-0.1.2/HISTORY.rst
@@ -0,0 +1,34 @@
+History
+-------
+
+0.1.2 (2013-05-08)
+------------------
+
+* Fix broken setup.py.
+
+0.1.1 (2013-05-18)
+++++++++++++++++++
+
+* Cache a broader set of response codes.
+* Follow RFC 2616's recommendations about caching resources with query strings.
+* Don't cache non-idempotent methods.
+* Non-idempotent methods invalidate their cache resources.
+* Documentation!
+* Actually implement the capacity parameter.
+
+0.1.0 (2013-05-11)
+++++++++++++++++++
+
+* Provide a Requests Transport Adapter so that caching 'Just Works'.
+
+0.0.2 (2013-05-07)
+++++++++++++++++++
+
+* Correctly cache using the 'Expires' header.
+* Correctly handle some of the functionality exposed by the 'Cache-Control' header.
+
+0.0.1 (2013-05-05)
+++++++++++++++++++
+
+* Conception
+* If-Modified-Since and 304 handling.
---
+++ packages/httpcache/httpcache-0.1.2.tar.gz/httpcache-0.1.2/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+# Get the version
+import re
+version_regex = r'__version__ = ["\']([^"\']*)["\']'
+with open('httpcache/__init__.py', 'r') as f:
+    text = f.read()
+    match = re.search(version_regex, text)
+
+    if match:
+        version = match.group(1)
+    else:
+        raise RuntimeError("No version number found!")
+
+# Stealing this from Kenneth Reitz
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+packages = ['httpcache']
+
+requires = ['requests>=1.2.0']
+
+setup(
+    name='httpcache',
+    version=version,
+    description='Simple HTTP cache for Python Requests',
+    long_description=open('README.rst').read() + '\n\n' +
+                     open('HISTORY.rst').read(),
+    author='Cory Benfield',
+    author_email='cory@lukasa.co.uk',
+    url='http://httpcache.readthedocs.org/en/latest/',
+    packages=packages,
+    package_data={'': ['LICENSE']},
+    package_dir={'httpcache': 'httpcache'},
+    include_package_data=True,
+    install_requires=requires,
+    license=open('LICENSE').read(),
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7'),
+)
