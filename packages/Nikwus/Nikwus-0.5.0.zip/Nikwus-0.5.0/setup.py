b599cb6376af9dac73d693095677f045b2527464
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:50 +0000

    Add package Nikwus/0.5.0/Nikwus-0.5.0.zip

---
+++ packages/Nikwus/Nikwus-0.5.0.zip/Nikwus-0.5.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+long_description = None
+try:
+    with open('README.rst') as f:
+        long_description = f.read()
+except Exception:
+    print 'Could not open README file for long_description'
+
+setup(
+    name='Nikwus',
+    version='0.5.0',
+    description="Automatically sprite images in CSS files",
+    long_description=long_description,
+    author=", ".join([
+        "Patrice Neff <mail@patrice.ch>",
+    ]),
+    packages=find_packages(),
+    install_requires=[
+        'cssutils',
+        'Pillow',
+    ],
+    tests_require=[
+        'nose',
+    ],
+    test_suite='nose.collector',
+    license='BSD',
+    classifiers=[
+    ]
+)
