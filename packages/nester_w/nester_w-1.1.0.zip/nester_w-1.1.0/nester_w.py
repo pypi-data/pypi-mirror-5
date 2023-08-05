d7c83d87cc9b1be48344c84ea5662e4dc44df9ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:24 +0000

    Add package nester_w/1.1.0/nester_w-1.1.0.zip

---
+++ packages/nester_w/nester_w-1.1.0.zip/nester_w-1.1.0/nester_w.py
@@ -0,0 +1,11 @@
+"""This is my first module"""
+
+def show(a_list, space):
+    """递归输出所有列表项,'space'参数用于指定打印数据前的制表符数量"""
+    if isinstance(a_list, list):
+        for item in a_list:
+            show(item, space+1)
+    else:
+        for s in range(space):
+            print("\t", end='')
+        print(a_list)
