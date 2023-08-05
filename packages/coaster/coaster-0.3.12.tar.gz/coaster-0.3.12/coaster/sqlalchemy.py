e0b6dc31ed5dfdd0286889ef01c629cae3884c7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:59 +0000

    Add package coaster/0.3.12/coaster-0.3.12.tar.gz

---
+++ packages/coaster/coaster-0.3.12.tar.gz/coaster-0.3.12/coaster/sqlalchemy.py
@@ -0,0 +1,305 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from coaster import make_name
+from sqlalchemy import Column, Integer, DateTime, Unicode
+from sqlalchemy.sql import select, func
+from sqlalchemy.types import TypeDecorator, TEXT
+from sqlalchemy.ext.declarative import declared_attr
+from sqlalchemy.ext.mutable import Mutable
+from datetime import datetime
+import simplejson
+
+
+__all_mixins = ['IdMixin', 'TimestampMixin', 'PermissionMixin', 'UrlForMixin',
+    'BaseMixin', 'BaseNameMixin', 'BaseScopedNameMixin', 'BaseIdNameMixin',
+    'BaseScopedIdMixin', 'BaseScopedIdNameMixin']
+
+
+class IdMixin(object):
+    """
+    Provides the :attr:`id` primary key column
+    """
+    id = Column(Integer, primary_key=True)
+
+
+class TimestampMixin(object):
+    """
+    Provides the :attr:`created_at` and :attr:`updated_at` audit timestamps
+    """
+    created_at = Column(DateTime, default=func.now(), nullable=False)
+    updated_at = Column(DateTime, default=func.now(), onupdate=datetime.utcnow, nullable=False)
+
+
+class PermissionMixin(object):
+    """
+    Provides the :meth:`permissions` method used by BaseMixin and derived classes
+    """
+    def permissions(self, user, inherited=None):
+        """
+        Return permissions available to the given user on this object
+        """
+        if inherited is not None:
+            return inherited
+        else:
+            return set()
+
+
+class UrlForMixin(object):
+    """
+    Provides a placeholder :meth:`url_for` method used by BaseMixin-derived classes
+    """
+    def url_for(self, action='view', **kwargs):
+        return None
+
+
+class BaseMixin(IdMixin, TimestampMixin, PermissionMixin, UrlForMixin):
+    """
+    Base mixin class for all tables that adds id and timestamp columns and includes
+    stub :meth:`permissions` and :meth:`url_for` methods
+    """
+    pass
+
+
+class BaseNameMixin(BaseMixin):
+    """
+    Base mixin class for named objects
+    """
+    @declared_attr
+    def name(cls):
+        return Column(Unicode(250), nullable=False, unique=True)
+
+    @declared_attr
+    def title(cls):
+        return Column(Unicode(250), nullable=False)
+
+    def __init__(self, *args, **kw):
+        super(BaseNameMixin, self).__init__(*args, **kw)
+        if not self.name:
+            self.make_name()
+
+    def make_name(self):
+        if self.title:
+            if self.id:
+                checkused = lambda c: self.__class__.query.filter(self.__class__.id != self.id).filter_by(name=c).first()
+            else:
+                checkused = lambda c: self.__class__.query.filter_by(name=c).first()
+            self.name = make_name(self.title, maxlength=250,
+                checkused=checkused)
+
+
+class BaseScopedNameMixin(BaseMixin):
+    """
+    Base mixin class for named objects within containers. When using this,
+    you must provide an model-level attribute "parent" that is a synonym for
+    the parent object. You must also create a unique constraint on 'name' in
+    combination with the parent foreign key. Sample use case in Flask::
+
+        class Event(BaseScopedNameMixin, db.Model):
+            __tablename__ = 'event'
+            organizer_id = db.Column(db.Integer, db.ForeignKey('organizer.id'))
+            organizer = db.relationship(Organizer)
+            parent = db.synonym('organizer')
+            __table_args__ = (db.UniqueConstraint('name', 'organizer_id'),)
+    """
+    @declared_attr
+    def name(cls):
+        return Column(Unicode(250), nullable=False)
+
+    @declared_attr
+    def title(cls):
+        return Column(Unicode(250), nullable=False)
+
+    def __init__(self, *args, **kw):
+        super(BaseScopedNameMixin, self).__init__(*args, **kw)
+        if self.parent and not self.name:
+            self.make_name()
+
+    def make_name(self):
+        if self.title:
+            if self.id:
+                checkused = lambda c: self.__class__.query.filter(self.__class__.id != self.id).filter_by(
+                    name=c, parent=self.parent).first()
+            else:
+                checkused = lambda c: self.__class__.query.filter_by(name=c, parent=self.parent).first()
+            self.name = make_name(self.title, maxlength=250,
+                checkused=checkused)
+
+    def permissions(self, user, inherited=None):
+        """
+        Permissions for this model, plus permissions inherited from the parent.
+        """
+        if inherited is not None:
+            return inherited | super(BaseScopedNameMixin, self).permissions(user)
+        elif self.parent is not None and isinstance(self.parent, PermissionMixin):
+            return self.parent.permissions(user) | super(BaseScopedNameMixin, self).permissions(user)
+        else:
+            return super(BaseScopedNameMixin, self).permissions(user)
+
+
+class BaseIdNameMixin(BaseMixin):
+    """
+    Base mixin class for named objects with an id tag.
+    """
+    @declared_attr
+    def name(cls):
+        return Column(Unicode(250), nullable=False)
+
+    @declared_attr
+    def title(cls):
+        return Column(Unicode(250), nullable=False)
+
+    url_id_attr = 'id'
+
+    def __init__(self, *args, **kw):
+        super(BaseIdNameMixin, self).__init__(*args, **kw)
+        if not self.name:
+            self.make_name()
+
+    def make_name(self):
+        if self.title:
+            self.name = make_name(self.title, maxlength=250)
+
+    @property
+    def url_id(self):
+        return self.id
+
+    @property
+    def url_name(self):
+        return '%d-%s' % (self.url_id, self.name)
+
+
+class BaseScopedIdMixin(BaseMixin):
+    """
+    Base mixin class for objects with an id that is unique within a parent.
+    Implementations must provide a 'parent' attribute that is either a relationship
+    or a synonym to a relationship referring to the parent object, and must
+    declare a unique constraint between url_id and the parent. Sample use case in Flask::
+
+        class Issue(BaseScopedIdMixin, db.Model):
+            __tablename__ = 'issue'
+            event_id = db.Column(Integer, db.ForeignKey('event.id'))
+            event = db.relationship(Event)
+            parent = db.synonym('event')
+            __table_args__ = (db.UniqueConstraint('url_id', 'event_id'),)
+    """
+    @declared_attr
+    def url_id(cls):
+        return Column(Integer, nullable=False)
+
+    url_id_attr = 'url_id'
+
+    def __init__(self, *args, **kw):
+        super(BaseScopedIdMixin, self).__init__(*args, **kw)
+        if self.parent:
+            self.make_id()
+
+    def make_id(self):
+        if self.url_id is None:  # Set id only if empty
+            self.url_id = select([func.coalesce(func.max(self.__class__.url_id + 1), 1)],
+                self.__class__.parent == self.parent)
+
+    def permissions(self, user, inherited=None):
+        """
+        Permissions for this model, plus permissions inherited from the parent.
+        """
+        if inherited is not None:
+            return inherited | super(BaseScopedIdMixin, self).permissions(user)
+        else:
+            return self.parent.permissions(user) | super(BaseScopedIdMixin, self).permissions(user)
+
+
+class BaseScopedIdNameMixin(BaseScopedIdMixin):
+    """
+    Base mixin class for named objects with an id tag that is unique within a
+    parent. Implementations must provide a 'parent' attribute that is a
+    synonym to the parent relationship, and must declare a unique constraint
+    between url_id and the parent. Sample use case in Flask::
+
+        class Event(BaseScopedIdNameMixin, db.Model):
+            __tablename__ = 'event'
+            organizer_id = db.Column(db.Integer, db.ForeignKey('organizer.id'))
+            organizer = db.relationship(Organizer)
+            parent = db.synonym('organizer')
+            __table_args__ = (db.UniqueConstraint('url_id', 'organizer_id'),)
+    """
+    @declared_attr
+    def name(cls):
+        return Column(Unicode(250), nullable=False)
+
+    @declared_attr
+    def title(cls):
+        return Column(Unicode(250), nullable=False)
+
+    def __init__(self, *args, **kw):
+        super(BaseScopedIdNameMixin, self).__init__(*args, **kw)
+        if self.parent:
+            self.make_id()
+        if not self.name:
+            self.make_name()
+
+    def make_name(self):
+        if self.title:
+            self.name = make_name(self.title, maxlength=250)
+
+    @property
+    def url_name(self):
+        return '%d-%s' % (self.url_id, self.name)
+
+
+# --- Column types ------------------------------------------------------------
+
+__all_columns = ['JsonDict']
+
+
+# Adapted from http://docs.sqlalchemy.org/en/rel_0_8/orm/extensions/mutable.html#establishing-mutability-on-scalar-column-values
+
+class JsonDict(TypeDecorator):
+    """
+    Represents a JSON data structure. Usage::
+
+        column = Column(JsonDict)
+    """
+    impl = TEXT
+
+    def process_bind_param(self, value, dialect):
+        if value is not None:
+            value = simplejson.dumps(value)
+        return value
+
+    def process_result_value(self, value, dialect):
+        if value is not None:
+            value = simplejson.loads(value, use_decimal=True)
+        return value
+
+
+class MutableDict(Mutable, dict):
+    @classmethod
+    def coerce(cls, key, value):
+        "Convert plain dictionaries to MutableDict."
+
+        if not isinstance(value, MutableDict):
+            if isinstance(value, dict):
+                return MutableDict(value)
+
+            # this call will raise ValueError
+            return Mutable.coerce(key, value)
+        else:
+            return value
+
+    def __setitem__(self, key, value):
+        "Detect dictionary set events and emit change events."
+
+        dict.__setitem__(self, key, value)
+        self.changed()
+
+    def __delitem__(self, key):
+        "Detect dictionary del events and emit change events."
+
+        dict.__delitem__(self, key)
+        self.changed()
+
+MutableDict.associate_with(JsonDict)
+
+
+__all__ = __all_mixins + __all_columns
---
+++ packages/coaster/coaster-0.3.12.tar.gz/coaster-0.3.12/tests/test_models.py
@@ -0,0 +1,346 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from time import sleep
+from datetime import datetime, timedelta
+from coaster.sqlalchemy import (BaseMixin, BaseNameMixin, BaseScopedNameMixin,
+    BaseIdNameMixin, BaseScopedIdMixin, BaseScopedIdNameMixin, MutableDict, JsonDict)
+from sqlalchemy import create_engine, Column, Integer, Unicode, UniqueConstraint, ForeignKey
+from sqlalchemy.orm import relationship, synonym, sessionmaker, scoped_session
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.exc import IntegrityError
+
+engine = create_engine('sqlite://')
+Session = scoped_session(sessionmaker(autocommit=False,
+              autoflush=False,
+              bind=engine))
+Base = declarative_base(bind=engine)
+
+
+# --- Models ------------------------------------------------------------------
+class BaseContainer(Base):
+    __tablename__ = 'base_container'
+    id = Column(Integer, primary_key=True)
+    name = Column(Unicode(80), nullable=True)
+    query = Session.query_property()
+
+
+class Container(BaseMixin, Base):
+    __tablename__ = 'container'
+    name = Column(Unicode(80), nullable=True)
+    query = Session.query_property()
+
+    content = Column(Unicode(250))
+
+
+class UnnamedDocument(BaseMixin, Base):
+    __tablename__ = 'unnamed_document'
+    query = Session.query_property()
+    container_id = Column(Integer, ForeignKey('container.id'))
+    container = relationship(Container)
+
+    content = Column(Unicode(250))
+
+
+class NamedDocument(BaseNameMixin, Base):
+    __tablename__ = 'named_document'
+    query = Session.query_property()
+    container_id = Column(Integer, ForeignKey('container.id'))
+    container = relationship(Container)
+
+    content = Column(Unicode(250))
+
+
+class ScopedNamedDocument(BaseScopedNameMixin, Base):
+    __tablename__ = 'scoped_named_document'
+    query = Session.query_property()
+    container_id = Column(Integer, ForeignKey('container.id'))
+    container = relationship(Container)
+    parent = synonym('container')
+
+    content = Column(Unicode(250))
+    __table_args__ = (UniqueConstraint('name', 'container_id'),)
+
+
+class IdNamedDocument(BaseIdNameMixin, Base):
+    __tablename__ = 'id_named_document'
+    query = Session.query_property()
+    container_id = Column(Integer, ForeignKey('container.id'))
+    container = relationship(Container)
+
+    content = Column(Unicode(250))
+
+
+class ScopedIdDocument(BaseScopedIdMixin, Base):
+    __tablename__ = 'scoped_id_document'
+    query = Session.query_property()
+    container_id = Column(Integer, ForeignKey('container.id'))
+    container = relationship(Container)
+    parent = synonym('container')
+
+    content = Column(Unicode(250))
+    __table_args__ = (UniqueConstraint('url_id', 'container_id'),)
+
+
+class ScopedIdNamedDocument(BaseScopedIdNameMixin, Base):
+    __tablename__ = 'scoped_id_named_document'
+    query = Session.query_property()
+    container_id = Column(Integer, ForeignKey('container.id'))
+    container = relationship(Container)
+    parent = synonym('container')
+
+    content = Column(Unicode(250))
+    __table_args__ = (UniqueConstraint('url_id', 'container_id'),)
+
+
+class User(BaseMixin, Base):
+    __tablename__ = 'user'
+    username = Column(Unicode(80), nullable=False)
+    query = Session.query_property()
+
+
+class MyData(Base):
+    __tablename__ = 'my_data'
+    id = Column(Integer, primary_key=True)
+    data = Column(JsonDict)
+
+# -- Tests --------------------------------------------------------------------
+
+
+class TestCoasterModels(unittest.TestCase):
+    def setUp(self):
+        Base.metadata.create_all()
+        self.session = Session()
+
+    def tearDown(self):
+        self.session.rollback()
+        Base.metadata.drop_all()
+
+    def make_container(self):
+        c = Container()
+        self.session.add(c)
+        return c
+
+    def test_container(self):
+        c = self.make_container()
+        self.assertEqual(c.id, None)
+        self.session.commit()
+        self.assertEqual(c.id, 1)
+
+    def test_timestamp(self):
+        now1 = datetime.utcnow()
+        # The db may not store microsecond precision, so sleep at least 1 second
+        # to ensure adequate gap between operations
+        sleep(1)
+        c = self.make_container()
+        self.session.commit()
+        u = c.updated_at
+        sleep(1)
+        now2 = datetime.utcnow()
+        self.assertTrue(now1 < c.created_at)
+        self.assertTrue(now2 > c.created_at)
+        c.content = u"updated"
+        sleep(1)
+        self.session.commit()
+        self.assertTrue(c.updated_at > now2)
+        self.assertTrue(c.updated_at > c.created_at)
+        self.assertTrue(c.updated_at > u)
+
+    def test_unnamed(self):
+        c = self.make_container()
+        d = UnnamedDocument(content=u"hello", container=c)
+        self.session.add(d)
+        self.session.commit()
+        self.assertEqual(c.id, 1)
+        self.assertEqual(d.id, 1)
+
+    def test_named(self):
+        """Named documents have globally unique names."""
+        c1 = self.make_container()
+        d1 = NamedDocument(title=u"Hello", content=u"World", container=c1)
+        self.session.add(d1)
+        self.session.commit()
+        self.assertEqual(d1.name, u'hello')
+
+        c2 = self.make_container()
+        d2 = NamedDocument(title=u"Hello", content=u"Again", container=c2)
+        self.session.add(d2)
+        self.session.commit()
+        self.assertEqual(d2.name, u'hello1')
+        # check make_name() modified `name` parameter or not, since `name` value already exists.
+        name = d2.name
+        d2.make_name()
+        self.assertEqual(d2.name, name)
+
+    def test_scoped_named(self):
+        """Scoped named documents have names unique to their containers."""
+        c1 = self.make_container()
+        d1 = ScopedNamedDocument(title=u"Hello", content=u"World", container=c1)
+        u = User(username="foo")
+        self.session.add(d1)
+        self.session.commit()
+        self.assertEqual(d1.name, u'hello')
+        self.assertEqual(d1.make_name(), None)
+        self.assertEqual(d1.permissions(user=u), set([]))
+        self.assertEqual(d1.permissions(user=u, inherited=set(['view'])), set(['view']))
+
+        d2 = ScopedNamedDocument(title=u"Hello", content=u"Again", container=c1)
+        self.session.add(d2)
+        self.session.commit()
+        self.assertEqual(d2.name, u'hello1')
+
+        c2 = self.make_container()
+        d3 = ScopedNamedDocument(title=u"Hello", content=u"Once More", container=c2)
+        self.session.add(d3)
+        self.session.commit()
+        self.assertEqual(d3.name, u'hello')
+
+        c3 = BaseContainer()
+        self.session.add(c3)
+        d4 = ScopedNamedDocument(title=u"Hello", container=c3)
+        self.session.commit()
+        self.assertEqual(d4.permissions(user=u), set([]))
+
+    def test_id_named(self):
+        """Documents with a global id in the URL"""
+        c1 = self.make_container()
+        d1 = IdNamedDocument(title=u"Hello", content=u"World", container=c1)
+        self.session.add(d1)
+        self.session.commit()
+        self.assertEqual(d1.url_name, u'1-hello')
+
+        d2 = IdNamedDocument(title=u"Hello", content=u"Again", container=c1)
+        self.session.add(d2)
+        self.session.commit()
+        self.assertEqual(d2.url_name, u'2-hello')
+
+        c2 = self.make_container()
+        d3 = IdNamedDocument(title=u"Hello", content=u"Once More", container=c2)
+        self.session.add(d3)
+        self.session.commit()
+        self.assertEqual(d3.url_name, u'3-hello')
+
+    def test_scoped_id(self):
+        """Documents with a container-specific id in the URL"""
+        c1 = self.make_container()
+        d1 = ScopedIdDocument(content=u"Hello", container=c1)
+        u = User(username="foo")
+        self.session.add(d1)
+        self.assertEqual(d1.permissions(user=u, inherited=set(['view'])), set(['view']))
+        self.assertEqual(d1.permissions(user=u), set([]))
+
+        d2 = ScopedIdDocument(content=u"New document", container=c1)
+        self.session.add(d2)
+        self.session.commit()
+        self.assertEqual(d1.url_id, 1)
+        self.assertEqual(d2.url_id, 2)
+
+        c2 = self.make_container()
+        d3 = ScopedIdDocument(content=u"Once More", container=c2)
+        self.session.add(d3)
+        self.session.commit()
+        self.assertEqual(d3.url_id, 1)
+
+        d4 = ScopedIdDocument(content=u"Third", container=c1)
+        self.session.add(d4)
+        self.session.commit()
+        self.assertEqual(d4.url_id, 3)
+
+    def test_scoped_id_named(self):
+        """Documents with a container-specific id and name in the URL"""
+        c1 = self.make_container()
+        d1 = ScopedIdNamedDocument(title=u"Hello", content=u"World", container=c1)
+        self.session.add(d1)
+        self.session.commit()
+        self.assertEqual(d1.url_name, u'1-hello')
+
+        d2 = ScopedIdNamedDocument(title=u"Hello again", content=u"New name", container=c1)
+        self.session.add(d2)
+        self.session.commit()
+        self.assertEqual(d2.url_name, u'2-hello-again')
+
+        c2 = self.make_container()
+        d3 = ScopedIdNamedDocument(title=u"Hello", content=u"Once More", container=c2)
+        self.session.add(d3)
+        self.session.commit()
+        self.assertEqual(d3.url_name, u'1-hello')
+
+        d4 = ScopedIdNamedDocument(title=u"Hello", content=u"Third", container=c1)
+        self.session.add(d4)
+        self.session.commit()
+        self.assertEqual(d4.url_name, u'3-hello')
+
+    def test_scoped_id_without_parent(self):
+        d1 = ScopedIdDocument(content=u"Hello")
+        self.session.add(d1)
+        self.assertRaises(IntegrityError, self.session.commit)
+        self.session.rollback()
+        d2 = ScopedIdDocument(content=u"Hello again")
+        self.session.add(d2)
+        self.assertRaises(IntegrityError, self.session.commit)
+
+    def test_scoped_named_without_parent(self):
+        d1 = ScopedNamedDocument(title=u"Hello", content=u"World")
+        self.session.add(d1)
+        self.assertRaises(IntegrityError, self.session.commit)
+        self.session.rollback()
+        d2 = ScopedIdNamedDocument(title=u"Hello", content=u"World")
+        self.session.add(d2)
+        self.assertRaises(IntegrityError, self.session.commit)
+
+    def test_delayed_name(self):
+        c = self.make_container()
+        d1 = NamedDocument(container=c)
+        d1.title = u'Document 1'
+        d1.make_name()
+        self.session.add(d1)
+        d2 = ScopedNamedDocument(container=c)
+        d2.title = u'Document 2'
+        d2.make_name()
+        self.session.add(d2)
+        d3 = IdNamedDocument(container=c)
+        d3.title = u'Document 3'
+        d3.make_name()
+        self.session.add(d3)
+        d4 = ScopedIdNamedDocument(container=c)
+        d4.title = u'Document 4'
+        d4.make_name()
+        self.session.add(d4)
+        self.session.commit()
+
+    def test_has_timestamps(self):
+        # Confirm that a model with multiple base classes between it and
+        # TimestampMixin still has created_at and updated_at
+        c = self.make_container()
+        d = ScopedIdNamedDocument(title=u"Hello", content=u"World", container=c)
+        self.session.add(d)
+        self.session.commit()
+        self.assertTrue(d.created_at is not None)
+        self.assertTrue(d.updated_at is not None)
+        updated_at = d.updated_at
+        self.assertTrue(d.updated_at - d.created_at < timedelta(seconds=1))
+        self.assertTrue(isinstance(d.created_at, datetime))
+        self.assertTrue(isinstance(d.updated_at, datetime))
+        d.title = u"Updated hello"
+        self.session.commit()
+        self.assertTrue(d.updated_at > updated_at)
+
+    def test_url_for(self):
+        d = UnnamedDocument(content=u"hello")
+        self.assertEqual(d.url_for(), None)
+
+    def test_jsondict(self):
+        m1 = MyData(data={'value': 'foo'})
+        self.session.add(m1)
+        self.session.commit()
+        #Test for __setitem__
+        m1.data['value'] = 'bar'
+        self.assertEqual(m1.data['value'], 'bar')
+        del m1.data['value']
+        self.assertEqual(m1.data, {})
+        self.assertRaises(ValueError, MyData, data='NonDict')
+
+
+if __name__ == '__main__':
+    unittest.main()
