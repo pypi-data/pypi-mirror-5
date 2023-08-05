3dca057d5b41f3e3031b3eb82ca6c265fa4e27b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:26 +0000

    Add package django-otp-agents/0.1.3/django-otp-agents-0.1.3.tar.gz

---
+++ packages/django-otp-agents/django-otp-agents-0.1.3.tar.gz/django-otp-agents-0.1.3/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='django-otp-agents',
+    version='0.1.3',
+    description="Integration of django-otp and django-agent-trust.",
+    long_description=open('README').read(),
+    author='Peter Sagerson',
+    author_email='psagersDjwublJf@ignorare.net',
+    zip_safe=False,
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://bitbucket.org/psagers/django-otp',
+    license='BSD',
+    install_requires=[
+        'django-otp >= 0.1.7',
+        'django-agent-trust',
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 2",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: Security",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
