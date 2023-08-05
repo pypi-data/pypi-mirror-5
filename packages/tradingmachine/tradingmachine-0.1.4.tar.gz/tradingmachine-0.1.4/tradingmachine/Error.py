5c5c6e18d5bd78668acfced1474d5bdef202c39a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:52 +0000

    Add package tradingmachine/0.1.4/tradingmachine-0.1.4.tar.gz

---
+++ packages/tradingmachine/tradingmachine-0.1.4.tar.gz/tradingmachine-0.1.4/tradingmachine/Error.py
@@ -0,0 +1,14 @@
+class FileNotExistsError(Exception):
+    pass
+
+
+class TypeMismatchError(Exception):
+    pass
+
+
+class UnknownKeywordArgumentError(Exception):
+    pass
+
+
+class UnknownIntervalPeriod(Exception):
+    pass
