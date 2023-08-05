3e664db397644f3e669c3586f8daad6afbc25c17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:02 +0000

    Add package kabuki/0.5.1/kabuki-0.5.1.tar.gz

---
+++ packages/kabuki/kabuki-0.5.1.tar.gz/kabuki-0.5.1/kabuki/__init__.py
@@ -0,0 +1,22 @@
+from hierarchical import *
+
+import utils
+import analyze
+import step_methods as steps
+import generate
+import distributions
+
+__version__ = '0.5.1'
+
+try:
+    from IPython.core.debugger import Tracer; debug_here = Tracer()
+except:
+    try:
+        from IPython.Debugger import Tracer; debug_here = Tracer()
+    except:
+        def debug_here(): pass
+
+try:
+    from collections import OrderedDict
+except:
+    from OrderedDict import OrderedDict
