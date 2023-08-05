27139fb8f4233253c0b2e8186940ccdfe524a639
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:32 +0000

    Add package seantis.dir.contacts/1.4/seantis.dir.contacts-1.4.tar.gz

---
+++ packages/seantis.dir.contacts/seantis.dir.contacts-1.4.tar.gz/seantis.dir.contacts-1.4/seantis/dir/contacts/directory.py
@@ -0,0 +1,29 @@
+from five import grok
+from plone.namedfile.field import NamedImage
+
+from seantis.dir.base.directory import Directory, DirectoryViewletManager
+from seantis.dir.base.interfaces import IDirectory
+from seantis.dir.contacts import _
+
+
+class IContactsDirectory(IDirectory):
+    """Extends the seantis.dir.base.directory.IDirectory"""
+
+    image = NamedImage(
+        title=_(u'Image'),
+        required=False,
+        default=None
+    )
+
+
+class ContactsDirectory(Directory):
+    pass
+
+
+class ContactsDirectoryViewlet(grok.Viewlet):
+    grok.context(IContactsDirectory)
+    grok.name('seantis.dir.contacts.directory.detail')
+    grok.require('zope2.View')
+    grok.viewletmanager(DirectoryViewletManager)
+
+    template = grok.PageTemplateFile('templates/directorydetail.pt')
