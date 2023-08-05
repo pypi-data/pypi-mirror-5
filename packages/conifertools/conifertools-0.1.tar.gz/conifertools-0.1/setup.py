923d92a8cba47651a20c0414e2c86c864527f703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:01 +0000

    Add package conifertools/0.1/conifertools-0.1.tar.gz

---
+++ packages/conifertools/conifertools-0.1.tar.gz/conifertools-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='conifertools',
+      version='0.1',
+      description='A collection of helpful tools for use with CoNIFER',
+      url='http://github.com/nkrumm/conifer-tools',
+      download_url = 'https://github.com/nkrumm/conifer-tools/tarball/0.1',
+      author='Niklas Krumm',
+      author_email='nkrumm@gmail.com',
+      license='',
+      packages=['conifertools'],
+      install_requires=[],
+      zip_safe=False)
