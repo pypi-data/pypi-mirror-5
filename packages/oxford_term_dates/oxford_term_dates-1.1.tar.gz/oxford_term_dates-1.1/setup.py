182911441cafb14d128c9fc43fefbbe42c53f5ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:43 +0000

    Add package oxford_term_dates/1.1/oxford_term_dates-1.1.tar.gz

---
+++ packages/oxford_term_dates/oxford_term_dates-1.1.tar.gz/oxford_term_dates-1.1/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='oxford_term_dates',
+      version='1.1',
+      description='A Python library for translating between real dates and Oxford term dates',
+      author='Oxford University Computing Services',
+      author_email='mobileoxford@oucs.ox.ac.uk',
+      url='https://github.com/oucs/oxford-term-dates',
+      packages=['oxford_term_dates','oxford_term_dates.templatetags'],
+      classifiers=[
+        'Framework :: Django',
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: Academic Free License (AFL)',
+        'Intended Audience :: Education',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Education',
+        'Topic :: Internet',
+      ],
+     )
