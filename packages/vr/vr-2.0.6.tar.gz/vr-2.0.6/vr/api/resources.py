8b03f7c3f48d880178a4217b7f79de4f5da1f993
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:43 +0000

    Add package vr/2.0.6/vr-2.0.6.tar.gz

---
+++ packages/vr/vr-2.0.6.tar.gz/vr-2.0.6/vr/api/resources.py
@@ -0,0 +1,204 @@
+from tastypie.resources import ModelResource
+from tastypie import fields
+from tastypie import authentication as auth
+from tastypie.authorization import Authorization
+from tastypie.api import Api
+from tastypie.constants import ALL_WITH_RELATIONS, ALL
+
+from vr.deployment import models
+
+
+# XXX Note that procs don't have a resource in this file.  It turns out that
+# Tastypie is not as good at handling non-ORM resources as I was led to
+# believe.  I made an effort, and ended up getting it mostly working, but it
+# was ugly as hell and using tons of subclasses and overrides.  In the end I
+# decided to just write a Django view and hard-code a route to
+# /api/v1/hosts/<name>/procs/. --Brent
+
+v1 = Api(api_name='v1')
+
+
+class SquadResource(ModelResource):
+    hosts = fields.ToManyField('api.resources.HostResource', 'hosts',
+                               full=True)
+
+    class Meta:
+        queryset = models.Squad.objects.all()
+        resource_name = 'squads'
+        filtering = {
+            'hosts': ALL_WITH_RELATIONS,
+        }
+        authentication = auth.MultiAuthentication(
+            auth.BasicAuthentication(),
+            auth.SessionAuthentication(),
+        )
+        authorization = Authorization()
+        detail_uri_name = 'name'
+v1.register(SquadResource())
+
+
+class IngredientResource(ModelResource):
+    recipes = fields.ToManyField('api.resources.RecipeResource', 'recipes')
+
+    class Meta:
+        queryset = models.ConfigIngredient.objects.all()
+        resource_name = 'ingredients'
+        authentication = auth.MultiAuthentication(
+            auth.BasicAuthentication(),
+            auth.SessionAuthentication(),
+        )
+        authorization = Authorization()
+v1.register(IngredientResource())
+
+
+class AppResource(ModelResource):
+
+    class Meta:
+        queryset = models.App.objects.all()
+        resource_name = 'apps'
+        filtering = {
+            'name': ALL,
+        }
+        authentication = auth.MultiAuthentication(
+            auth.BasicAuthentication(),
+            auth.SessionAuthentication(),
+        )
+        authorization = Authorization()
+        detail_uri_name = 'name'
+v1.register(AppResource())
+
+
+class BuildPackResource(ModelResource):
+    class Meta:
+        queryset = models.BuildPack.objects.all()
+        resource_name = 'buildpacks'
+        authentication = auth.MultiAuthentication(
+            auth.BasicAuthentication(),
+            auth.SessionAuthentication(),
+        )
+        authorization = Authorization()
+v1.register(BuildPackResource())
+
+
+class BuildResource(ModelResource):
+    class Meta:
+        queryset = models.Build.objects.all()
+        resource_name = 'builds'
+        authentication = auth.MultiAuthentication(
+            auth.BasicAuthentication(),
+            auth.SessionAuthentication(),
+        )
+        authorization = Authorization()
+v1.register(BuildResource())
+
+
+class SwarmResource(ModelResource):
+    app = fields.ToOneField('api.resources.AppResource', 'app')
+    squad = fields.ToOneField('api.resources.SquadResource', 'squad')
+    release = fields.ToOneField('api.resources.ReleaseResource', 'release')
+
+    shortname = fields.CharField('shortname')
+
+    compiled_config = fields.DictField('get_config')
+    compiled_env = fields.DictField('get_env')
+
+    class Meta:
+        queryset = models.Swarm.objects.all()
+        resource_name = 'swarms'
+        filtering = {
+            'recipe': ALL_WITH_RELATIONS,
+            'squad': ALL_WITH_RELATIONS,
+            'proc_name': ALL,
+
+        }
+        authentication = auth.MultiAuthentication(
+            auth.BasicAuthentication(),
+            auth.SessionAuthentication(),
+        )
+        authorization = Authorization()
+
+    def dehydrate(self, bundle):
+        # add in proc data
+        # TODO: Make these proper attributes so they can be saved by a PUT/POST
+        # to the swarm resource.
+        bundle.data['procs_uri'] = bundle.data['resource_uri'] + 'procs/'
+        bundle.data['procs'] = [p.as_dict() for p in
+                                bundle.obj.get_procs(check_cache=True)]
+        bundle.data['squad_name'] = bundle.obj.squad.name
+        bundle.data['version'] = bundle.obj.release.build.tag
+
+        # Also add in convenience data
+        bundle.data.update(app_name=bundle.obj.app.name,
+                           recipe_name=bundle.obj.config_name)
+        return bundle
+v1.register(SwarmResource())
+
+
+class ReleaseResource(ModelResource):
+    recipe = fields.ToOneField('api.resources.RecipeResource', 'recipe')
+    build = fields.ToOneField('api.resources.BuildResource', 'build')
+
+    class Meta:
+        queryset = models.Release.objects.all()
+        resource_name = 'releases'
+        authentication = auth.MultiAuthentication(
+            auth.BasicAuthentication(),
+            auth.SessionAuthentication(),
+        )
+        authorization = Authorization()
+v1.register(ReleaseResource())
+
+
+class TestResultResource(ModelResource):
+    testrun = fields.ToOneField('api.resources.TestRunResource', 'run',
+                                 related_name='tests')
+
+    class Meta:
+        queryset = models.TestResult.objects.all()
+        resource_name = 'testresults'
+        authentication = auth.MultiAuthentication(
+            auth.BasicAuthentication(),
+            auth.SessionAuthentication(),
+        )
+        authorization = Authorization()
+v1.register(TestResultResource())
+
+
+class TestRunResource(ModelResource):
+    testresults = fields.ToManyField('api.resources.TestResultResource',
+                                     'tests')
+
+    class Meta:
+
+        queryset = models.TestRun.objects.all()
+        resource_name = 'testruns'
+        authentication = auth.MultiAuthentication(
+            auth.BasicAuthentication(),
+            auth.SessionAuthentication(),
+        )
+        authorization = Authorization()
+v1.register(TestRunResource())
+
+
+class HostResource(ModelResource):
+    squad = fields.ToOneField('api.resources.SquadResource', 'squad')
+
+    class Meta:
+        queryset = models.Host.objects.all()
+        resource_name = 'hosts'
+        filtering = {
+            'name': ALL,
+        }
+        authentication = auth.MultiAuthentication(
+            auth.BasicAuthentication(),
+            auth.SessionAuthentication(),
+        )
+        authorization = Authorization()
+        detail_uri_name = 'name'
+
+    def dehydrate(self, bundle):
+        bundle.data['procs_uri'] = bundle.data['resource_uri'] + 'procs/'
+        bundle.data['procs'] = [p.as_dict() for p in
+                                bundle.obj.get_procs(check_cache=True)]
+        return bundle
+v1.register(HostResource())
---
+++ packages/vr/vr-2.0.6.tar.gz/vr-2.0.6/vr/deployment/forms.py
@@ -0,0 +1,131 @@
+from django import forms
+from django.conf import settings
+from django.contrib.auth import authenticate
+
+import yaml
+
+from vr.deployment import models
+
+
+class ConfigIngredientForm(forms.ModelForm):
+    class Meta:
+        model = models.ConfigIngredient
+
+    def clean_value(self):
+        value = self.cleaned_data.get('value', None)
+        if value:
+            try:
+                yaml.safe_load(value)
+            except:
+                raise forms.ValidationError("Invalid YAML")
+        return value
+
+
+class BuildForm(forms.Form):
+
+    app_id = forms.ChoiceField(choices=[], label='App')
+    tag = forms.CharField()
+
+    def __init__(self, *args, **kwargs):
+        super(BuildForm, self).__init__(*args, **kwargs)
+        self.fields['app_id'].choices = [(a.id, a) for a in
+                                         models.App.objects.all()]
+
+
+class BuildUploadForm(forms.ModelForm):
+    class Meta:
+        model = models.Build
+
+
+class SquadForm(forms.ModelForm):
+    class Meta:
+        model = models.Squad
+
+
+class HostForm(forms.ModelForm):
+    class Meta:
+        model = models.Host
+
+
+class ReleaseForm(forms.ModelForm):
+    class Meta:
+        model = models.Release
+
+
+class DeploymentForm(forms.Form):
+
+    release_id = forms.ChoiceField(choices=[], label='Release')
+    # TODO: proc should be a drop down of the procs available for a given
+    # release.  But I guess we can't narrow that down until a release is
+    # picked.
+    proc = forms.CharField(max_length=50)
+
+    config_name = forms.CharField(help_text=models.config_name_help)
+
+    hostname = forms.ChoiceField(choices=[])
+    port = forms.IntegerField()
+    contain = forms.BooleanField(help_text="Run inside LXC container?")
+
+    def __init__(self, *args, **kwargs):
+        super(DeploymentForm, self).__init__(*args, **kwargs)
+        self.fields['release_id'].choices = [(r.id, r) for r in
+            models.Release.objects.all()]
+        self.fields['hostname'].choices = [(h.name, h.name) for h in
+                                       models.Host.objects.filter(active=True)]
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self):
+        self.user = authenticate(**self.cleaned_data)
+        if not self.user:
+            raise forms.ValidationError('Bad username or password')
+        return self.cleaned_data
+
+
+class SwarmForm(forms.Form):
+    """
+    Form for creating or updating a swarm.
+    """
+    app_id = forms.ChoiceField(choices=[], label='App')
+    tag = forms.CharField(max_length=50)
+    config_name = forms.CharField(max_length=50,
+                                  help_text=models.config_name_help)
+    config_yaml = forms.CharField(required=False,
+                                  widget=forms.widgets.Textarea(attrs={'class':
+                                                                       'codearea'}))
+    env_yaml = forms.CharField(required=False,
+                               widget=forms.widgets.Textarea(attrs={'class':
+                                                                    'codearea'}))
+    proc_name = forms.CharField(max_length=50)
+    squad_id = forms.ChoiceField(choices=[], label='Squad')
+    size = forms.IntegerField()
+    pool = forms.CharField(max_length=50, required=False)
+
+    balancer_help = "Required if a pool is specified."
+    balancer = forms.ChoiceField(choices=[], label='Balancer', required=False,
+                                 help_text=balancer_help)
+
+    def __init__(self, data, *args, **kwargs):
+        super(SwarmForm, self).__init__(data, *args, **kwargs)
+        self.fields['squad_id'].choices = [(s.id, s) for s in
+                                            models.Squad.objects.all()]
+        self.fields['app_id'].choices = [(a.id, a) for a in
+                                            models.App.objects.all()]
+        self.fields['balancer'].choices = [('', '-------')] + [(b, b) for b in
+                                                               settings.BALANCERS]
+
+    def clean(self):
+        data = super(SwarmForm, self).clean()
+        if data['pool'] and not data['balancer']:
+            raise forms.ValidationError('Swarms that specify a pool must '
+                                        'specify a balancer')
+        return data
+
+    class Media:
+        js = (
+            'js/jquery.textarea.min.js',
+        )
+
