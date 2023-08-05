ed3cace09affd3d361cd9a7c4ff5410aaf347ca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:09 +0000

    Add package pp.server/0.1.3/pp.server-0.1.3.zip

---
+++ packages/pp.server/pp.server-0.1.3.zip/pp.server-0.1.3/setup.py
@@ -0,0 +1,49 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
+
+requires = [
+    'pyramid',
+    'pyramid_debugtoolbar',
+    'pyramid_xmlrpc',
+    'waitress',
+    'sqlalchemy',
+    'celery',
+    'flower',
+    'pastescript',
+    'waitress',
+    'plac',
+    ]
+
+setup(name='pp.server',
+      version='0.1.3',
+      description='pp.server - Produce & Publish Server',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='Andreas Jung',
+      author_email='info@zopyx.com',
+      url='http://pypi.python.org/pypi/pp.server',
+      keywords='web pyramid pylons',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=requires,
+      test_suite="pp.server",
+      entry_points="""\
+      [paste.app_factory]
+      main = pp.server:main
+      [console_scripts]
+      service-client = pp.server.scripts.service_client:main
+      worker = pp.server.scripts.worker:main
+      """,
+      )
