92851b70b3cfd5975643bbd662727d3b52c991ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:34 +0000

    Add package masks/0.1.1/masks-0.1.1.tar.gz

---
+++ packages/masks/masks-0.1.1.tar.gz/masks-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='masks',
+    version='0.1.1',
+    author='Nigel Cleland',
+    author_email='nigel.cleland@gmail.com',
+    packages=['masks'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/masks/',
+    license='LICENSE.txt',
+    description='Syntactic sugar for data filtering with pandas.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "pandas >= 0.11.0",
+        "numpy >= 1.7.1",
+    ],
+)
