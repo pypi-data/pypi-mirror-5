095f4e69730372ad076cf2bd0d1dae92018f3309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:51 +0000

    Add package netappConfigMaker/1.5.0/netappConfigMaker-1.5.0.tar.gz

---
+++ packages/netappConfigMaker/netappConfigMaker-1.5.0.tar.gz/netappConfigMaker-1.5.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='netappConfigMaker',
+    version='1.5.0',
+    author='Shah Mohsin WAHED',
+    author_email='shahmohsin.wahed@gmail.com',
+    #packages=['netappConfigMaker', 'netappConfigMaker.test'],
+    scripts=['netapp.config.maker.py', ],
+    url='http://pypi.python.org/pypi/netappConfigMaker/',
+    license='LICENSE.txt',
+    description='Netapp 7Mode Configuration generator based on a XML configuration file.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "argparse >= 1.2.1",
+        "lxml >= 2.3.5",
+        "cheetah >= 2.4.4",
+    ],
+)
