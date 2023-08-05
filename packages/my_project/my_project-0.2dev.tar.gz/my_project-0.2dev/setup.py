d92d2d33e1bdc2dc9d8ea0a2072f9456b4c1232c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:27 +0000

    Add package my_project/0.2dev/my_project-0.2dev.tar.gz

---
+++ packages/my_project/my_project-0.2dev.tar.gz/my_project-0.2dev/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.2'
+
+setup(name='my_project',
+      version=version,
+      description="Esta es la descripcion breve",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='package test muestra',
+      author='Franco Pellegrini',
+      author_email='frapell@gmail.com',
+      url='http://google.com  ',
+      license='GPL2',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+          'caipyrinha',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
