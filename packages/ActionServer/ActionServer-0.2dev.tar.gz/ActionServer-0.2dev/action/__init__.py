6a171e08de92ef9f4405a3ddd3402cf113414b17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:30 +0000

    Add package ActionServer/0.2dev/ActionServer-0.2dev.tar.gz

---
+++ packages/ActionServer/ActionServer-0.2dev.tar.gz/ActionServer-0.2dev/action/__init__.py
@@ -0,0 +1,19 @@
+
+import os
+import sys
+
+HOME   = os.path.basename(sys.argv[0])
+PUBLIC = HOME + '/public
+
+PATH_ACTION = os.path.basepath(__file__)
+
+print 'HOME', HOME
+print 'PUBL', PUBLIC
+print 'ACTN', PATH_ACTION
+
+import compact
+import template
+import database
+import http
+
+
