8f3463b22126c5d88ce5e4462d9bd5bd8d42a4b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:29 +0000

    Add package checkmyreqs/0.1.6/checkmyreqs-0.1.6.tar.gz

---
+++ packages/checkmyreqs/checkmyreqs-0.1.6.tar.gz/checkmyreqs-0.1.6/setup.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+try:
+    import multiprocessing
+except:
+    pass
+
+APP_NAME = 'checkmyreqs'
+VERSION = '0.1.6'
+
+# Grab requirments.
+with open('requirements.txt') as f:
+    required = f.readlines()
+
+settings = dict()
+
+# Publish Helper.
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+# Build Helper.
+if sys.argv[-1] == 'build':
+    try:
+        import py2exe
+    except ImportError:
+        print('py2exe is required to continue.')
+        sys.exit(1)
+
+    sys.argv.append('py2exe')
+
+    settings.update(
+        zipfile = None,
+        options = {
+            'py2exe': {
+                'compressed': 1,
+                'optimize': 0,
+                'bundle_files': 1}})
+
+settings.update(
+    name=APP_NAME,
+    version=VERSION,
+    author='Dustin Collins',
+    author_email='dustinrcollins@gmail.com',
+    packages=['tests'],
+    scripts=['checkmyreqs'],
+    url='https://github.com/dustinmm80/checkmyreqs',
+    license='MIT',
+    description='Check your project requirements for Python version compatibility',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3'
+    ],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    install_requires=required
+)
+
+setup(**settings)
