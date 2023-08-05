546b444fbd9d17ee6af81cb44b54c96db025a3d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:04 +0000

    Add package forsta/1.0.1/forsta-1.0.1.zip

---
+++ packages/forsta/forsta-1.0.1.zip/forsta-1.0.1/forsta.py
@@ -0,0 +1,7 @@
+def print_lol(the_list):
+     for film in the_list:
+          print film
+
+
+
+          
---
+++ packages/forsta/forsta-1.0.1.zip/forsta-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name            ='forsta',
+        version         ='1.0.1',
+        py_modules      =['forsta'],
+	author          ='forsta',
+        author_email    ='forsta@163.com',
+        url             ='http://www.forsta18.com',
+        description     ='forsta'
+     )
