1f68407dde2531a02625e325ee6fb86be5e0f16f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:36 +0000

    Add package jmbo-calendar/0.2.4/jmbo-calendar-0.2.4.tar.gz

---
+++ packages/jmbo-calendar/jmbo-calendar-0.2.4.tar.gz/jmbo-calendar-0.2.4/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jmbo-calendar',
+    version='0.2.4',
+    description='Jmbo calendar app.',
+    long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekelt.com',
+    license='BSD',
+    url='http://github.com/praekelt/jmbo-calendar',
+    packages = find_packages(),
+    install_requires = [
+        'jmbo-foundry>=1.0',
+    ],
+    tests_require=[
+        'django-setuptest>=0.1.2',
+        'pysqlite>=2.5'
+    ],
+    test_suite="setuptest.setuptest.SetupTestSuite",
+    include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
+)
