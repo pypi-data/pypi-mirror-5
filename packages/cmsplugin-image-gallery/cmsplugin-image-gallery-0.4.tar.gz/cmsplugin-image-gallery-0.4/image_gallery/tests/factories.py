7e2c95eed0fdd63255e663572ae76bf77b3868c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:24 +0000

    Add package cmsplugin-image-gallery/0.4/cmsplugin-image-gallery-0.4.tar.gz

---
+++ packages/cmsplugin-image-gallery/cmsplugin-image-gallery-0.4.tar.gz/cmsplugin-image-gallery-0.4/image_gallery/tests/factories.py
@@ -0,0 +1,37 @@
+"""Factories for the models of the ``image_gallery`` app."""
+from factory import Factory, LazyAttribute, SubFactory, Sequence
+from filer.models import Folder, Image
+
+from image_gallery.models import Gallery, GalleryCategory, GalleryPlugin
+
+
+class FolderFactory(Factory):
+    FACTORY_FOR = Folder
+
+    name = 'Test Folder'
+
+
+class ImageFactory(Factory):
+    FACTORY_FOR = Image
+
+
+class GalleryCategoryFactory(Factory):
+    FACTORY_FOR = GalleryCategory
+
+    name = Sequence(lambda i: 'name {}'.format(i))
+    slug = LazyAttribute(lambda a: a.name.replace(' ', '-'))
+
+
+class GalleryFactory(Factory):
+    FACTORY_FOR = Gallery
+
+    title = 'Test Gallery'
+    folder = SubFactory(FolderFactory)
+    category = SubFactory(GalleryCategoryFactory)
+    is_published = True
+
+
+class GalleryPluginFactory(Factory):
+    FACTORY_FOR = GalleryPlugin
+
+    gallery = SubFactory(GalleryFactory)
