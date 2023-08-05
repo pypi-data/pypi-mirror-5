a5a26e710f38732e20440e771a207b902e132312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:25 +0000

    Add package Flask-SeaSurf/0.1.17/Flask-SeaSurf-0.1.17.tar.gz

---
+++ packages/Flask-SeaSurf/Flask-SeaSurf-0.1.17.tar.gz/Flask-SeaSurf-0.1.17/setup.py
@@ -0,0 +1,39 @@
+'''
+    Flask-SeaSurf
+    -------------
+
+    An updated cross-site forgery protection extension for Flask.
+
+    Links
+    `````
+
+    * `documentation <http://packages.python.org/Flask-SeaSurf>`_
+'''
+
+from setuptools import setup
+
+setup(
+    name='Flask-SeaSurf',
+    version='0.1.17',
+    url='https://github.com/maxcountryman/flask-seasurf/',
+    license='BSD',
+    author='Max Countryman',
+    author_email='maxc@me.com',
+    description='An updated CSRF extension for Flask.',
+    long_description=__doc__,
+    py_modules=['flask_seasurf'],
+    test_suite='test_seasurf',
+    zip_safe=False,
+    platforms='any',
+    install_requires=['Flask'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
