00fc17f5fc2402d9627dd9cc72fc550f556522b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:05 +0000

    Add package maxclient/3.3/maxclient-3.3.tar.gz

---
+++ packages/maxclient/maxclient-3.3.tar.gz/maxclient-3.3/CHANGES.rst
@@ -0,0 +1,14 @@
+CHANGELOG
+==========
+
+3.3 (2013-06-04)
+----------------
+* Updated to 3.3 MAX
+
+3.0 (2013-04-15)
+----------------
+* Updated to latest implementations
+
+1.0 (Unreleased)
+----------------
+*  Initial version
---
+++ packages/maxclient/maxclient-3.3.tar.gz/maxclient-3.3/maxclient/resources.py
@@ -0,0 +1,64 @@
+PUBLIC_RESOURCES = {
+    'users': {'route': '/people'},
+    'user': {'route': '/people/{username}'},
+    'avatar': {'route': '/people/{username}/avatar'},
+    'user_activities': {'route': '/people/{username}/activities'},
+
+    'activities': {'route': '/activities'},
+    'comments': {'route': '/activities/comments'},
+    'activity': {'route': '/activities/{activity}'},
+    'timeline': {'route': '/people/{username}/timeline'},
+    'timeline_authors': {'route': '/people/{username}/timeline/authors'},
+
+    'activity_comments': {'route': '/activities/{activity}/comments'},
+
+    'subscriptions': {'route': '/people/{username}/subscriptions'},
+    'subscription': {'route': '/people/{username}/subscriptions/{hash}'},
+
+    'user_conversations': {'route': '/people/{username}/conversations'},
+    'user_conversation': {'route': '/people/{username}/conversations/{id}'},
+
+    'contexts': {'route': '/contexts'},
+    'context': {'route': '/contexts/{hash}'},
+    'context_activities': {'route': '/contexts/{hash}/activities'},
+    'context_avatar': {'route': '/contexts/{hash}/avatar'},
+    'public_contexts': {'route': '/contexts/public'},
+    'context_user_permission': {'route': '/contexts/{hash}/permissions/{username}/{permission}'},
+    'context_activities': {'route': '/contexts/{hash}/activities'},
+    'context_activities_authors': {'route': '/contexts/{hash}/activities/authors'},
+
+    # MAX 3.0
+    'conversations': {'route': '/conversations'},
+    'conversation': {'route': '/conversations/{id}'},
+    'messages': {'route': '/conversations/{id}/messages'},
+    'message': {'route': '/conversations/{id}/messages/{activity}'},
+    'participants': {'route': '/conversations/{id}/participants'},
+    'participant': {'route': '/conversations/{id}/participant'},
+
+    # MAX 4.0
+    'user_shares': {'route': '/people/{username}/shares'},
+    'user_likes': {'route': '/people/{username}/likes'},
+    'follows': {'route': '/people/{username}/follows'},
+    'follow': {'route': '/people/{username}/follows/{followedDN}'},
+    'likes': {'route': '/activities/{activity}/likes'},
+    'like': {'route': '/activities/{activity}/likes/{likeId}'},
+    'shares': {'route': '/activities/{activity}/shares'},
+    'share': {'route': '/activities/{activity}/shares/{shareId}'},
+
+    # Not implemented / Not in roadmap
+    'user_comments': {'route': '/people/{username}/comments'},
+    'user_conversations': {'route': '/people/{username}/conversations'},
+    'comment': {'route': '/activities/{activity}/comments/{commentId}'},
+    'context_permissions': {'route': '/contexts/{hash}/permissions'},
+    'context_user_permissions': {'route': '/contexts/{hash}/permissions/{username}'},
+
+}
+
+RESTRICTED_RESOURCES = {
+
+    'admin_security': {'route': '/admin/security'},
+}
+
+RESOURCES = {}
+RESOURCES.update(PUBLIC_RESOURCES)
+RESOURCES.update(RESTRICTED_RESOURCES)
