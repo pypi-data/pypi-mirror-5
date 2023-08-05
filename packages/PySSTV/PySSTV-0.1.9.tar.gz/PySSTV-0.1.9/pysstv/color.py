e3967dde0f6adb2a7519a6cfec84aad0a8b607fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:35 +0000

    Add package PySSTV/0.1.9/PySSTV-0.1.9.tar.gz

---
+++ packages/PySSTV/PySSTV-0.1.9.tar.gz/PySSTV-0.1.9/pysstv/color.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+from __future__ import division
+from sstv import byte_to_freq, FREQ_BLACK
+from grayscale import GrayscaleSSTV
+
+
+RED, GREEN, BLUE = range(3)
+
+class ColorSSTV(GrayscaleSSTV):
+    def encode_line(self, line):
+        msec_pixel = self.SCAN / self.WIDTH
+        image = self.image.load()
+        for index in self.COLOR_SEQ:
+            for item in self.before_channel(index):
+                yield item
+            for col in xrange(self.WIDTH):
+                pixel = image[col, line]
+                freq_pixel = byte_to_freq(pixel[index])
+                yield freq_pixel, msec_pixel
+            for item in self.after_channel(index):
+                yield item
+
+    def before_channel(self, index):
+        return []
+
+    after_channel = before_channel
+
+
+class MartinM1(ColorSSTV):
+    COLOR_SEQ = (GREEN, BLUE, RED)
+    VIS_CODE = 0x2c
+    WIDTH = 320
+    HEIGHT = 256
+    SYNC = 4.862
+    SCAN = 146.432
+    INTER_CH_GAP = 0.572
+
+    def before_channel(self, index):
+        if index == GREEN:
+            yield FREQ_BLACK, self.INTER_CH_GAP
+
+    def after_channel(self, index):
+        yield FREQ_BLACK, self.INTER_CH_GAP
+
+
+class MartinM2(MartinM1):
+    VIS_CODE = 0x28
+    WIDTH = 160
+    SCAN = 73.216
+
+
+class ScottieS1(MartinM1):
+    VIS_CODE = 0x3c
+    SYNC = 9
+    INTER_CH_GAP = 1.5
+    SCAN = 138.24 - INTER_CH_GAP
+
+    def horizontal_sync(self):
+        return []
+
+    def before_channel(self, index):
+        if index == ColorSSTV.RED:
+            for item in MartinM1.horizontal_sync(self):
+                yield item
+        yield FREQ_BLACK, self.INTER_CH_GAP
+
+
+class ScottieS2(ScottieS1):
+    VIS_CODE = 0x38
+    SCAN = 88.064 - ScottieS1.INTER_CH_GAP
+    WIDTH = 160
+
+MODES = (MartinM1, MartinM2, ScottieS1, ScottieS2)
