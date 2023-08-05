db621300f66969e4eaaf35bfb63f06ea0e81ade1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:50 +0000

    Add package oauthnesia/1.0.3/oauthnesia-1.0.3.tar.gz

---
+++ packages/oauthnesia/oauthnesia-1.0.3.tar.gz/oauthnesia-1.0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='oauthnesia',
+      version='1.0.3',
+      description='Client for Urbanesia\'s OAUTH v1.0a API',
+      long_description=readme(),
+      keywords='urbanesia oauth poi web service api',
+      url='http://github.com/Urbanesia/oauthnesia-py',
+      author='Batista Harahap',
+      author_email='tista@urbanesia.com',
+      license='MIT',
+      packages=['oauthnesia'],
+      install_requires=[
+          'requests',
+          'requests_oauthlib',
+          'json'],
+      zip_safe=False)
