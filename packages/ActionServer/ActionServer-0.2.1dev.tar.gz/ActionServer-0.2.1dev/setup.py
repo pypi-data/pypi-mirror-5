f39c4c676a1fb68d1df6b1636fa24ed342aae67f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:31 +0000

    Add package ActionServer/0.2.1dev/ActionServer-0.2.1dev.tar.gz

---
+++ packages/ActionServer/ActionServer-0.2.1dev.tar.gz/ActionServer-0.2.1dev/setup.py
@@ -0,0 +1,18 @@
+
+from distutils.core import setup
+
+setup(
+    name='ActionServer',
+    author='Josueh Machado',
+    author_email='josueh.cg@gmail.com',
+    description=('Framework Web (Tornado + Django)'),
+    version='0.2.1dev',
+    packages=['action',],
+    license='CC by Josueh',
+)
+
+''' INSTRUCTION FOR UPDATE PACKAGE:
+    $ python setup.py sdist
+    $ python setup.py register
+    $ python setup.py sdist upload
+'''
