cd69b79a2acffbd55640008a83cd65fbc46bace3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:45 +0000

    Add package caldwellpy/1.5.3/caldwellpy-1.5.3.tar.gz

---
+++ packages/caldwellpy/caldwellpy-1.5.3.tar.gz/caldwellpy-1.5.3/pyutil/django/templatetags/pyutil.py
@@ -0,0 +1,16 @@
+from django import template
+
+register = template.Library()
+
+@register.filter(name='getkey')
+def getkey(value, arg):
+    ''' Gets a value from a dict by key.  This allows keys to contain spaces, dashes, etc. '''
+    return value.get(arg, '')
+
+@register.filter(name='split')
+def split(value, arg):
+    return value.split(arg)
+
+@register.filter(name='cut')
+def cut(value, arg):
+    return value.replace(arg, '')
