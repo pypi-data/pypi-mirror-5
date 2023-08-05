726c0faf5959df7fdfa5e4e4527ad8ad6526148a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:43 +0000

    Add package pypepa/0.4.1dev/pypepa-0.4.1dev.tar.gz

---
+++ packages/pypepa/pypepa-0.4.1dev.tar.gz/pypepa-0.4.1dev/pypepa/__init__.py
@@ -0,0 +1,6 @@
+___author__ = "Dariusz Dwornikowski"
+__copyright__ = "Dariusz Dwornikowski, Poznan University of Technology"
+__email__ = "dariusz.dwornikowski@cs.put.poznan.pl"
+__version__ = "0.4.1dev"
+
+from pypepa.pepa_model import PEPAModel
