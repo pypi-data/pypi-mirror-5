347ae315f6b0122803a206ef39046980da8259d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:04 +0000

    Add package trytond_nereid_project/2.6.1.0/trytond_nereid_project-2.6.1.0.tar.gz

---
+++ packages/trytond_nereid_project/trytond_nereid_project-2.6.1.0.tar.gz/trytond_nereid_project-2.6.1.0/__init__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+'''
+    nereid_project
+
+    :copyright: (c) 2010-2013 by Openlabs Technologies & Consulting (P) Ltd.
+    :license: GPLv3, see LICENSE for more details
+
+'''
+from trytond.pool import Pool
+
+from project import WebSite, ProjectUsers, ProjectInvitation, \
+    ProjectWorkInvitation, Project, Tag, TaskTags, \
+    ProjectHistory, ProjectWorkCommit
+from company import Company, CompanyProjectAdmins, NereidUser
+
+
+def register():
+    """This function will register trytond module project_billing
+    """
+    Pool.register(
+        WebSite,
+        ProjectUsers,
+        ProjectInvitation,
+        ProjectWorkInvitation,
+        Project,
+        Tag,
+        TaskTags,
+        ProjectHistory,
+        ProjectWorkCommit,
+        Company,
+        CompanyProjectAdmins,
+        NereidUser,
+        module='nereid_project', type_='model',
+    )
