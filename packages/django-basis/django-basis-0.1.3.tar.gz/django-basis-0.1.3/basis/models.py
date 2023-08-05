944a80272b41d03cc9fe4f6a88ce76541b6b0292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:22 +0000

    Add package django-basis/0.1.3/django-basis-0.1.3.tar.gz

---
+++ packages/django-basis/django-basis-0.1.3.tar.gz/django-basis-0.1.3/basis/models.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+
+from django.db import models
+from django.contrib.auth import get_user_model
+
+from .managers import BasisModelManager
+
+
+class BasisModel(models.Model):
+    deleted = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(default=datetime.now, auto_now_add=True)
+    created_by = models.ForeignKey(get_user_model(), null=True, default=None, related_name="%(class)s_created")
+
+    updated_at = models.DateTimeField(default=datetime.now, auto_now=True)
+    updated_by = models.ForeignKey(get_user_model(), null=True, default=None, related_name="%(class)s_updated")
+
+    objects = BasisModelManager()
+    all_objects = models.Manager()
+
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+
+        if 'current_user' in kwargs:
+            current_user = kwargs.pop("current_user")
+
+            if not self.id:
+                self.created_by = current_user
+
+            self.updated_by = current_user
+
+        super(BasisModel, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        self.deleted = True
+        self.save()
+
+    def restore(self, *args, **kwargs):
+        self.deleted = False
+        self.save()
