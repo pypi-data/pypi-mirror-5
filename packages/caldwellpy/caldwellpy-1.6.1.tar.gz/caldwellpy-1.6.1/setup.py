9af8f009b147ff5430e2d67ae06ccf40c9eeaf01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:08 +0000

    Add package caldwellpy/1.6.1/caldwellpy-1.6.1.tar.gz

---
+++ packages/caldwellpy/caldwellpy-1.6.1.tar.gz/caldwellpy-1.6.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='caldwellpy',
+      version='1.6.1',
+      description='Python Utilities Collection',
+      author='Matt Caldwell',
+      author_email='matt.caldwell@gmail.com',
+      url='https://github.com/mattcaldwell/pyutil',
+      packages=['pyutil', 'pyutil.django', 'pyutil.django.templatetags'],
+      install_requires=[
+          'Django==1.4.5',
+          'coverage==3.6',
+          'mock==1.0.1',
+          'nose==1.3.0',
+          'requests==1.2.3',
+          'wsgiref==0.1.2',
+      ]
+)
