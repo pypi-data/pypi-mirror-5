b09b216d0627424ab455e7f4f9efb686c897e020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:22 +0000

    Add package pytracker/1.0.1/pytracker-1.0.1.tar.gz

---
+++ packages/pytracker/pytracker-1.0.1.tar.gz/pytracker-1.0.1/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, Extension
+
+trackable = Extension('pytracker.trackable',
+                      sources = ['pytracker/trackable.c'])
+
+readme = """pytracker is a simple python extension that allows you to track instance usage in 
+production-level environments.
+
+================   =====================================
+Documentation      http://garywiz.github.io/pytracker
+pytracker Source   http://github.com/garywiz/pytracker
+pypi link          http://pypi.python.org/pypi/pytracker
+================   =====================================
+"""
+
+setup(
+    name = "pytracker",
+    version = '1.0.1',
+    description = 'Object tracking classes for efficient inventory of allocated/freed objects',
+    long_description = readme,
+    ext_modules = [trackable],
+    py_modules = ['pytracker/tracker', 'pytracker/__init__'],
+    test_suite = "pyt_tests.tests.test_all",
+    license = "BSD 3",
+    author = "Gary Wisniewski",
+    author_email = "pypi-garywiz@gw.spidereye.com",
+    url = "http://github.com/garywiz/pytracker",
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: C",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Quality Assurance",
+        "Topic :: System :: Monitoring",
+        ]
+    )
