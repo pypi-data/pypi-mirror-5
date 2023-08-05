4fd9d221b40f73fde5b1be39fdacd58fc12b1249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:05 +0000

    Add package jsoncomment/0.1.0/jsoncomment-0.1.0.zip

---
+++ packages/jsoncomment/jsoncomment-0.1.0.zip/jsoncomment-0.1.0/jsoncomment/comments.py
@@ -0,0 +1,66 @@
+﻿
+################################################################################
+
+from .wrapper import GenericWrapper
+
+################################################################################
+
+# Comments
+COMMENT_PREFIX = ("#",";")
+MULTILINE_START = "/*"
+MULTILINE_END = "*/"
+
+# Data strings
+LONG_STRING = '"""'
+
+################################################################################
+
+class JsonComment(GenericWrapper):
+
+	def loads(self, custom_json_string, *args, **kwargs):
+		lines = custom_json_string.splitlines()
+		standard_json = json_preprocess(lines)
+		return self.object_to_wrap.loads(standard_json, *args, **kwargs)
+
+	def load(self, custom_json_file, *args, **kwargs):
+		custom_json_string = custom_json_file.read()
+		return self.loads(custom_json_string, *args, **kwargs)
+
+################################################################################
+
+def json_preprocess(lines):
+
+	standard_json = ""
+	is_multiline = False
+
+	for line in lines:
+
+		line = line.strip()
+
+		if len(line) == 0:
+			continue
+
+		if line.startswith(COMMENT_PREFIX):
+			continue
+
+		elif line.startswith(MULTILINE_START):
+			is_multiline = True
+			# In case both start and end are on the same line
+			# Example: /***** Comment *****/
+			if line.endswith(MULTILINE_END):
+				is_multiline = False
+			continue
+
+		elif is_multiline:
+			if line.endswith(MULTILINE_END):
+				is_multiline = False
+			continue
+
+		elif LONG_STRING in line:
+			line = line.replace(LONG_STRING, '"')
+
+		standard_json += line
+
+	return standard_json
+
+################################################################################
---
+++ packages/jsoncomment/jsoncomment-0.1.0.zip/jsoncomment-0.1.0/setup.py
@@ -0,0 +1,40 @@
+﻿
+################################################################################
+
+from distutils.core import setup
+
+from codecs import open
+
+################################################################################
+
+def read(fname):
+	return open(fname, "r", "utf-8-sig").read()
+
+################################################################################
+
+setup (
+	name = "jsoncomment",
+	version = "0.1.0",
+	description = "A wrapper to JSON parsers allowing comments and multi line strings",
+	author = "Gaspare Iengo",
+	author_email = "gaspareiengo@gmail.com",
+	keywords = "json comments multiline",
+	url = "https://bitbucket.org/Dando_Real_ITA/json-comment",
+	packages = [
+		"jsoncomment"
+	],
+	long_description = read("README.md"),
+	classifiers = [
+		"Development Status :: 3 - Alpha",
+		"Intended Audience :: End Users/Desktop",
+		"License :: OSI Approved :: MIT License",
+		"Natural Language :: English",
+		"Operating System :: OS Independent",
+		"Programming Language :: Python",
+		"Programming Language :: Python :: 3.3",
+		"Topic :: Software Development :: Pre-processors",
+		"Topic :: Text Editors :: Text Processing",
+	],
+)
+
+################################################################################
