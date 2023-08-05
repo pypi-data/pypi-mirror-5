fd9f9bcb53a33eb2675803b6fd4d87e54435447a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:47 +0000

    Add package mf/0.1.21/mf-0.1.21.tar.gz

---
+++ packages/mf/mf-0.1.21.tar.gz/mf-0.1.21/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='mf',
+      version='0.1.21',
+      description='MongoKit forms generation and Pyramid Administration Dashboard',
+      author='Olivier Sallou',
+      author_email='olivier.sallou@irisa.fr',
+      license='LGPL',
+      url='https://github.com/mobyle2/mf-pyramid',
+      packages=['mf'],
+      install_requires=["mongokit >= 0.8.2"]
+     )
