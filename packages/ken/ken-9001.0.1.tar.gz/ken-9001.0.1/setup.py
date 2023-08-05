3cd69f34515b28c6ddd0cd480ac1c8824ec929a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:00 +0000

    Add package ken/9001.0.1/ken-9001.0.1.tar.gz

---
+++ packages/ken/ken-9001.0.1.tar.gz/ken-9001.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+setup(name="ken",
+      version="9001.0.1",
+      description="Ken is over 9000",
+      long_description=long_description,
+      py_modules=["ken"],
+      author="Infoshift Inc",
+      author_email="me@jpanganiban.com")
