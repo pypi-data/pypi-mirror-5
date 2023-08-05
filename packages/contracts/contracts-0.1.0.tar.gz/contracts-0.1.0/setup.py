f7c83ac7d041210ebd18d76369c516c2c52807b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:31 +0000

    Add package contracts/0.1.0/contracts-0.1.0.tar.gz

---
+++ packages/contracts/contracts-0.1.0.tar.gz/contracts-0.1.0/setup.py
@@ -0,0 +1,19 @@
+"""
+setup.py
+"""
+from setuptools import setup
+
+setup (
+    name='contracts',
+    version='0.1.0',
+    author='Rodney Gomes',
+    author_email='rodneygomes@gmail.com',
+    url='',
+    install_requires = ["setuptools"],
+    test_suite="test",
+    keywords = ['contracts'],
+    py_modules = ['contracts'],
+    license='Apache 2.0 License',
+    description='',
+    long_description=''
+)
