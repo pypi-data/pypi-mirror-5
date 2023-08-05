2f7e8bd487af688a4110d177b8bce9262d388a57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:02 +0000

    Add package pyCardDAV/0.5.0-2/pyCardDAV-0.5.0-2.tar.gz

---
+++ packages/pyCardDAV/pyCardDAV-0.5.0-2.tar.gz/pyCardDAV-0.5.0-2/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python2
+
+import sys
+#from distutils.core import setup
+from setuptools import setup
+
+requirements = [
+    'lxml',
+    'vobject',
+    'requests',
+    'urwid',
+    'pyxdg'
+]
+if sys.version_info[:2] in ((2, 6),):
+    # there is no argparse in python2.6
+    requirements.append('argparse')
+
+setup(
+    name='pyCardDAV',
+    version='0.5.0-2',
+    description='A CardDAV based address book tool',
+    long_description=open('README.rst').read(),
+    author='Christian Geier',
+    author_email='pycarddav@lostpackets.de',
+    url='http://lostpackets.de/pycarddav/',
+    license='Expat/MIT',
+    packages=['pycarddav'],
+    scripts=['bin/pycardsyncer', 'bin/pc_query', 'bin/pycard-import'],
+    requires=requirements,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Environment :: Console :: Curses",
+        "Intended Audience :: End Users/Desktop",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 2 :: Only",
+        "Topic :: Utilities",
+        "Topic :: Communications :: Email :: Address Book"
+    ],
+)
