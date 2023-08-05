386b8b1015fa210a0f29ca4e32bcc6574523627e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:13 +0000

    Add package plog/0.12/plog-0.12.tar.gz

---
+++ packages/plog/plog-0.12.tar.gz/plog-0.12/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name="plog",
+      packages=["plog"],
+      version="0.12",
+      description="Logged process",
+      author="Daniel Narvaez",
+      author_email="dwnarvaez@gmail.com",
+      url="http://github.com/dnarvaez/plog",
+      classifiers=["License :: OSI Approved :: BSD License",
+                   "Programming Language :: Python :: 3",
+                   "Topic :: Software Development :: Libraries"])
