ff73bde241de69408a6bce052d831eddd5f5311f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:27 +0000

    Add package rpcpdb/0.3.4/rpcpdb-0.3.4.tar.gz

---
+++ packages/rpcpdb/rpcpdb-0.3.4.tar.gz/rpcpdb-0.3.4/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(
+    name="rpcpdb",
+    version="0.3.4",
+    description="Debug support for RPC servers",
+    long_description=open('README.txt').read(),
+    author="Ben Bass",
+    author_email="benbass@codedstructure.net",
+    url="http://bitbucket.org/codedstructure/rpcpdb",
+    packages=["rpcpdb"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Topic :: Software Development :: Debuggers",
+    ]
+)
