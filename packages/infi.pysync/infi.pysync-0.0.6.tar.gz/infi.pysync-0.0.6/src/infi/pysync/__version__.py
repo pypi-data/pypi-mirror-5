e9c6634e8bbf97b1e730c2fa19bf55d4803df568
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:02 +0000

    Add package infi.pysync/0.0.6/infi.pysync-0.0.6.tar.gz

---
+++ packages/infi.pysync/infi.pysync-0.0.6.tar.gz/infi.pysync-0.0.6/src/infi/pysync/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.6"
+__git_commiter_name__ = "Tal Yalon"
+__git_commiter_email__ = "tal.yalon@gmail.com"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '64657878be642f0ba299aee7c00430373b891625'
+__git_head_subject__ = 'HOSTDEV-969 fixed'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
