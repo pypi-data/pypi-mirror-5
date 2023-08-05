5e9390d199090e043b0704e83c9a01a8bb0a3ed9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:01 +0000

    Add package shiver/0.11/shiver-0.11.tar.gz

---
+++ packages/shiver/shiver-0.11.tar.gz/shiver-0.11/shiver/shiver.py
@@ -0,0 +1,253 @@
+import math
+import multiprocessing
+import numpy as np 
+import Queue 
+import sys
+import threading
+
+from llvm.core import Function 
+from llvm.ee import GenericValue 
+from llvm_helpers import return_type, shared_exec_engine, optimize
+from llvm_helpers import module_from_c, from_c  
+from type_helpers import is_llvm_float_type, is_llvm_int_type, is_llvm_ptr_type
+from type_helpers import lltype_to_dtype, ty_int8, ty_int64, ty_float64, ty_void  
+from type_helpers import dtype_to_ctype_name, lltype_to_dtype, python_to_lltype
+from wrappers import parfor_wrapper 
+
+def safediv(x,y):
+  return int(math.ceil(x / float(y)))
+
+def cpu_count():
+  return multiprocessing.cpu_count()
+
+def parse_iter_range(x):
+  start = None 
+  stop = None 
+  step = None
+  if isinstance(x, int):
+    stop = x
+  elif isinstance(x, (list, tuple)):
+    if len(x) == 1:
+      stop = x[0]
+    elif len(x) == 2:
+      start = x[0]
+      stop = x[1]
+    else:
+      assert len(x) == 3
+      start = x[0]
+      stop = x[1]
+      step = x[2]
+  else:
+    assert isinstance(x, slice)
+    start = x.start 
+    stop = x.stop 
+    step = x.step 
+  
+  if start is None:
+    start = 0
+  if step is None:
+    step = 1
+  assert isinstance(start, int)
+  assert isinstance(stop, int)
+  assert isinstance(step, int)
+  assert stop > start 
+  return (start, stop, step) 
+
+
+def parse_iters(niters):
+  if isinstance(niters, int):
+    niters = (niters,)
+  assert isinstance(niters, (list, tuple))
+  iter_ranges = [parse_iter_range(x) for x in niters]
+  counts = [(r[1] - r[0] / r[2]) for r in iter_ranges]
+  steps = [r[2] for r in iter_ranges]
+  return iter_ranges, tuple(steps), tuple(counts) 
+
+def smallest_divisor(n):
+  for i in xrange(2,n):
+    if n % i == 0:
+      return i 
+  return n 
+
+
+def split_iters(iter_ranges, n_threads = None):
+  """
+  For now, we just pick the biggest dimension and split 
+  it into min(dimsize, 10*n_cores pieces)
+  """
+
+
+  if n_threads is None:
+    n_threads = cpu_count()
+  
+  counts = [safediv(r[1] - r[0], r[2]) for r in iter_ranges]
+  # largest_dim = np.max(counts)
+  total_count = float(np.sum(counts))
+  split_factors = [ (c / total_count) ** 2 for c in counts ]
+  if len(counts) > 2:
+    # kludgy heuristic
+    # if you're reading across multiple dimensions
+    # assume there might be reuse of data read in 
+    # and try to split up work so it fits into cache  
+    expected_bytes = 8 
+    for dim in counts:
+      expected_bytes *= dim
+    expected_kb = expected_bytes / 1024
+    l2_cache_size = 8192
+    n_pieces = max(n_threads, expected_kb / l2_cache_size)
+  else: 
+    n_pieces = 2*n_threads 
+  
+  # initialize work_items with an empty single range 
+  work_items = [[]]
+  for (dim_idx,dim_count) in enumerate(counts):
+
+    dim_start, _, dim_step = iter_ranges[dim_idx]
+    n_dim_pieces = int(math.ceil(split_factors[dim_idx] * n_pieces))
+    dim_factor = float(dim_count) / n_dim_pieces
+    
+    old_work_items = [p for p in work_items]
+    work_items = []
+    for i in xrange(n_dim_pieces):
+      # copy all the var ranges, after which we'll modifying 
+      # the biggest dimension 
+
+      start = dim_start + int(math.floor(dim_step * dim_factor * i))
+      stop = dim_start + int(math.floor(dim_step * dim_factor * (i+1)))
+      
+      dim_work_item = (start,stop,dim_step)
+      for old_work_item in old_work_items:
+        new_work_item = [r for r in old_work_item]
+        new_work_item.append(dim_work_item) 
+        work_items.append(new_work_item)
+
+  return work_items 
+  
+          
+
+def gv_from_python(x, llvm_type = None):
+  if isinstance(x, GenericValue):
+    return x
+  elif isinstance(x, (int,long)):
+    llvm_type =  ty_int64 if llvm_type is None else llvm_type
+    assert is_llvm_int_type(llvm_type), \
+      "Expected LLVM integer type, not %s" % (llvm_type,) 
+    return GenericValue.int(llvm_type, x)
+  elif isinstance(x, float):
+    llvm_type = ty_float64 if llvm_type is None else llvm_type
+    assert is_llvm_float_type(llvm_type), \
+        "Expected LLVM float type, not %s" % (llvm_type,)
+    return GenericValue.real(llvm_type, x)  
+  elif isinstance(x, bool):
+    llvm_type = ty_int8 if llvm_type is None else llvm_type
+    assert is_llvm_int_type(llvm_type), \
+      "Expected LLVM integer type, not %s" % (llvm_type,)
+    return GenericValue.int(llvm_type, x)
+  else:
+    assert isinstance(x, np.ndarray)
+    assert llvm_type is not None 
+    assert is_llvm_ptr_type(llvm_type), \
+      "Native argument receiving numpy array must be a pointer, not %s" % (llvm_type,)
+    elt_type = llvm_type.pointee
+    assert is_llvm_float_type(elt_type) or is_llvm_int_type(elt_type)
+    assert elt_type == python_to_lltype(x.dtype), \
+        "Can't pass array with %s* data pointer to function that expects %s*" % (x.dtype, elt_type)
+    return GenericValue.pointer(x.ctypes.data)
+  
+
+def run_with_generic_values(fn, gv_inputs, ee):  
+  n_given = len(gv_inputs)
+  n_expected = len(gv_inputs)
+  assert n_given == n_expected, "Expected %d inputs but got %d" % (n_expected, n_given)
+  return ee.run_function(fn, gv_inputs)
+
+def run(fn, *input_values, **kwds):
+  """
+  Given a compiled LLVM function and Python input values, 
+  convert the input to LLVM generic values and run the 
+  function 
+  """
+  
+  ee = kwds.get('ee', shared_exec_engine)
+  input_types = [arg.type for arg in fn.args]
+  gv_inputs = [gv_from_python(x, t) 
+               for (x,t) in 
+               zip(input_values, input_types)]
+  
+  return run_with_generic_values(fn, gv_inputs, ee)
+
+
+class Worker(threading.Thread):
+  def __init__(self, q, ee, llvm_fn, fixed_args):
+    self.ee = ee 
+    self.q = q 
+    self.llvm_fn = llvm_fn
+    self.fixed_args = list(fixed_args)    
+    threading.Thread.__init__(self)
+  
+  
+  def run(self):
+    while True:
+      try:
+        ranges = self.q.get(False)
+        # TODO: have to make these types actually match the expected input size
+        index_types = [arg.type for arg in self.llvm_fn.args[-len(ranges):]]
+        starts = [gv_from_python(r[0], t) 
+                  for (r,t) in 
+                  zip(ranges, index_types)]
+        stops = [gv_from_python(r[1], t) 
+                  for (r,t) in 
+                  zip(ranges, index_types)]        
+        args = self.fixed_args + starts + stops
+        run_with_generic_values(self.llvm_fn, args, self.ee)
+        self.q.task_done()
+      except Queue.Empty:
+        return
+
+
+def launch(work_fn, iter_ranges, fixed_args = (), ee = shared_exec_engine):
+  q = Queue.Queue()
+  # put all the index ranges into the queue
+  for work_item in split_iters(iter_ranges):
+    q.put(work_item)
+  
+  # start worker threads
+  for _ in xrange(cpu_count()):
+    Worker(q, ee, work_fn, fixed_args).start()
+  q.join()
+
+        
+def parfor(fn, niters, fixed_args = (), ee = shared_exec_engine):
+  """
+  Given a function from index integers to void, run it in parallel 
+  over the index range specified by the tuple 'niters'
+  """
+  assert isinstance(fn, Function), \
+    "Can only run LLVM functions, not %s" % type(fn)
+  
+  # in case fixed arguments aren't yet GenericValues, convert them
+  fixed_args = tuple(gv_from_python(v, arg.type) 
+                     for (v,arg) in 
+                     zip(fixed_args, fn.args))
+  iter_ranges, steps, shape = parse_iters(niters)
+  result_lltype = return_type(fn) 
+  if result_lltype == ty_void:
+    work_fn = parfor_wrapper(fn, steps)
+    launch(work_fn, iter_ranges, fixed_args, ee)
+    return 
+  else:
+    assert is_llvm_float_type(result_lltype) or is_llvm_int_type(result_lltype)
+    dtype = lltype_to_dtype(result_lltype)
+    result_array = np.empty(shape = shape, dtype = dtype)
+    fixed_args = (GenericValue.pointer(result_array.ctypes.data),) + fixed_args
+    work_fn = parfor_wrapper(fn, steps, shape)
+    n_given = len(fixed_args) + 2*len(steps)
+    n_expected = len(work_fn.args)
+    assert n_given == n_expected, \
+       "Work function expects %d arguments but got %d" % (n_expected, n_given)
+    launch(work_fn, iter_ranges, fixed_args, ee)
+    return result_array
+    assert False, "Collecting results not yet implemented"
+  
+  
+
---
+++ packages/shiver/shiver-0.11.tar.gz/shiver-0.11/shiver/wrappers.py
@@ -0,0 +1,196 @@
+ 
+from llvm.core import *  
+
+from llvm_helpers import const_float, const_int, empty_fn, shared_module, optimize
+from llvm_helpers import return_type, input_types, input_names 
+from type_helpers import ty_pyobj, ty_ptr_pyobj, ty_void , ty_int64
+from type_helpers import is_llvm_float_type, is_llvm_int_type
+
+def libfn(name, return_type, input_types):
+  def get_ref(module = shared_module):
+    t = Type.function(return_type, input_types)
+    return module.get_or_insert_function(t, name)
+  return get_ref 
+
+PyEval_InitThreads = libfn("PyEval_InitThreads", ty_void, [])
+PyEval_SaveThread = libfn("PyEval_SaveThread", ty_ptr_pyobj, [])
+PyEval_RestoreThread = libfn("PyEval_RestoreThread", ty_void, [ty_ptr_pyobj])
+PyThreadState_Swap = libfn("PyThreadState_Swap", ty_ptr_pyobj, [ty_ptr_pyobj])
+PyEval_ReleaseLock = libfn ("PyEval_ReleaseLock", ty_void, [])
+PyEval_AcquireLock = libfn ("PyEval_AcquireLock", ty_void, [])
+
+
+class LoopBuilder(object):
+  def __init__(self, wrapper, original_fn, closure_values, 
+               start_values, stop_values, step_constants):
+    self.wrapper = wrapper 
+    self.original_fn = original_fn 
+    self.closure_values = closure_values 
+    self.start_values = start_values 
+    self.stop_values = stop_values 
+    self.step_constants = step_constants 
+    self.n_loops = len(start_values)
+    assert len(stop_values) == self.n_loops 
+    assert len(step_constants) == self.n_loops 
+    n_args = len(original_fn.args)
+    assert len(closure_values) + self.n_loops == n_args
+    self.inner_call = None 
+    
+  _loop_var_names = ['i', 'j', 'k', 'l']
+  
+  def new_block(self, name):
+    bb = self.wrapper.append_basic_block(name)
+    builder = Builder.new(bb)
+    return bb, builder
+  
+  def get_var_name(self, pos):
+    maxpos = len(self._loop_var_names)
+    return self._loop_var_names[pos % maxpos] * ((pos/maxpos)+1)
+  
+  def inline_inner_call(self):
+    """
+    After the wrapper function is fully constructed, 
+    call this method to inline the user function into 
+    the innermost loop nest
+    """
+    llvm.core.inline_function(self.inner_call)
+    
+  def create(self, bb, builder, loop_idx_values = []):
+    n = len(loop_idx_values)
+    if n == self.n_loops:
+      # save the LLVM call node so we can inline it later 
+      self.inner_call = builder.call(self.original_fn, self.closure_values + loop_idx_values)
+     
+      
+      return builder
+    else:
+      start = self.start_values[n]
+      stop = self.stop_values[n]
+      var = builder.alloca(start.type, self.get_var_name(n))
+  
+      builder.store(start, var)
+      test_bb, test_builder =  self.new_block("test%d" % (n+1))
+      builder.branch(test_bb)
+      idx_value = test_builder.load(var)                         
+      cond = test_builder.icmp(ICMP_ULT, idx_value, stop, "stop_cond%d" % (n+1))
+      body_bb, body_builder = self.new_block("body%d" % (n+1))
+      after_bb, after_builder = self.new_block("after_loop%d" % (n+1))
+      test_builder.cbranch(cond, body_bb, after_bb)
+      body_builder = self.create(body_bb, body_builder, loop_idx_values + [idx_value])
+      step = const_int(self.step_constants[n], idx_value.type)
+      next_idx_value = body_builder.add(idx_value, step)
+      body_builder.store(next_idx_value, var)
+      body_builder.branch(test_bb)
+      return after_builder 
+
+  
+def mk_parfor_wrapper_no_return(fn, step_sizes):
+  n_indices = len(step_sizes)
+
+  # need to double the integer index inputs to allow for a stop argument 
+  # of each   the number of index inputs to
+  old_input_types = [arg.type for arg in fn.args]
+  new_input_types = [t for t in old_input_types]
+  for i in xrange(n_indices):
+    old_index_type = old_input_types[-n_indices + i]
+    assert old_index_type.kind == TYPE_INTEGER, \
+        "Last %d input(s) to %s must be integers" % (n_indices, fn.name)
+    new_input_types.append(old_index_type)
+  wrapper = empty_fn(fn.name + "_wrapper",  new_input_types, module = fn.module)
+   
+  for arg_idx in xrange(len(new_input_types)):
+    if arg_idx < len(old_input_types):
+      wrapper.args[arg_idx].name = fn.args[arg_idx].name 
+    else:
+      wrapper.args[arg_idx].name = fn.args[arg_idx - n_indices].name + "_stop"
+  index_vars = wrapper.args[-2*n_indices:]
+  closure_vars = wrapper.args[:-2*n_indices]
+  start_vars = index_vars[:n_indices]
+  stop_vars = index_vars[n_indices:]
+  
+  assert len(start_vars) == len(stop_vars)
+  loop_builder = LoopBuilder(wrapper, 
+                             fn, 
+                             closure_vars, 
+                             start_vars, 
+                             stop_vars, 
+                             step_sizes)
+  
+  
+  entry_bb, entry_builder = loop_builder.new_block("entry")
+  
+  
+  null = llvm.core.Constant.null(ty_ptr_pyobj)
+  
+  thread_state = entry_builder.call(PyThreadState_Swap(fn.module), [null], 'thread_state')
+  entry_builder.call(PyEval_ReleaseLock(fn.module), [])
+  
+  exit_builder = loop_builder.create(entry_bb, entry_builder)
+  
+  exit_builder.call(PyEval_AcquireLock(fn.module), [])
+  exit_builder.call(PyThreadState_Swap(fn.module), [thread_state])
+  exit_builder.ret_void()
+  
+  # have to inline the call to the user function after the wrapper
+  # is fully constructed or else LLVM crashes complaining 
+  # about missing block terminators 
+  loop_builder.inline_inner_call()
+  return wrapper 
+
+def mk_parfor_wrapper_collect_returned_values(fn, step_sizes, dim_sizes, result_t):
+  
+  name = "write_output_%s" % fn.name 
+  for step_size in step_sizes:
+    name = name + "_%d" % step_size
+  write_output_fn = empty_fn(name, 
+                     [Type.pointer(result_t)] + input_types(fn), 
+                     output_type=ty_void, 
+                     module = fn.module)
+  output_array = write_output_fn.args[0]
+  output_array.add_attribute(llvm.core.ATTR_NO_ALIAS)
+  output_array.add_attribute(llvm.core.ATTR_NO_CAPTURE)
+  
+  original_args = write_output_fn.args[1:]
+  bb = write_output_fn.append_basic_block("entry")
+  builder = Builder.new(bb)
+  value = builder.call(fn, original_args, "elt_result")
+  
+  n_indices = len(step_sizes)
+  idx_args = write_output_fn.args[-n_indices:]
+  strides = [1]
+  for dim_size in reversed(dim_sizes[1:]):
+    strides.append(strides[-1] * dim_size)
+  strides.reverse()
+  output_idx = const_int(0, ty_int64)
+  for (idx, stride) in zip(idx_args, strides):
+    dim_offset = builder.mul(idx, const_int(stride, idx.type))
+    output_idx = builder.add(output_idx, dim_offset)
+  
+
+
+  output_ptr = builder.gep(output_array, [output_idx])
+  builder.store(value, output_ptr)
+  builder.ret_void()
+  llvm.core.inline_function(value)
+  return mk_parfor_wrapper_no_return(write_output_fn, step_sizes)
+   
+
+def parfor_wrapper(fn, step_sizes, dim_sizes = None, _cache = {}):
+  cache_key = id(fn), tuple(step_sizes) 
+  if cache_key in _cache:
+    return _cache[cache_key]
+  else:
+    result_t = fn.type.pointee.return_type 
+    if result_t == ty_void:
+      wrapper = mk_parfor_wrapper_no_return(fn, step_sizes)
+    else:
+      assert is_llvm_float_type(result_t) or is_llvm_int_type(result_t), \
+         "Function can't return type %s" % result_t
+      assert dim_sizes is not None 
+      wrapper = mk_parfor_wrapper_collect_returned_values(fn, step_sizes, dim_sizes, result_t)
+    optimize(wrapper)
+    _cache[cache_key] = wrapper 
+    print wrapper 
+    return wrapper 
+  
+  
