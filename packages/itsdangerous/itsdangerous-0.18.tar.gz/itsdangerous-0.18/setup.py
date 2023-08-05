d150e4b5959f9c614cfc5347444858591ee343f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:38 +0000

    Add package itsdangerous/0.18/itsdangerous-0.18.tar.gz

---
+++ packages/itsdangerous/itsdangerous-0.18.tar.gz/itsdangerous-0.18/setup.py
@@ -0,0 +1,21 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='itsdangerous',
+    author='Armin Ronacher',
+    author_email='armin.ronacher@active-4.com',
+    version='0.18',
+    url='http://github.com/mitsuhiko/itsdangerous',
+    py_modules=['itsdangerous'],
+    description='Various helpers to pass trusted data to '
+                'untrusted environments and back.',
+    zip_safe=False,
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python'
+    ]
+)
