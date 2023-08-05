943bc48c1bc774364e8ac148178a4d2ce082d85e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:25 +0000

    Add package ua2.djfab/0.0.2/ua2.djfab-0.0.2.tar.gz

---
+++ packages/ua2.djfab/ua2.djfab-0.0.2.tar.gz/ua2.djfab-0.0.2/setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+
+Name='ua2.djfab'
+ProjecUrl="https://bitbucket.org/ua2web/ua2.djfab"
+Version='0.0.2'
+Author='avkoval'
+AuthorEmail='alex@ua2web.com'
+Maintainer='avkoval'
+Summary='Generic fabric commands for all django projects'
+License='BSD License'
+ShortDescription=Summary
+Description=Summary
+
+needed = [
+]
+
+EagerResources = [
+    'ua2',
+]
+
+ProjectScripts = [
+##    'scripts/runweb',
+]
+
+PackageData = {
+    '': ['*.*'],
+}
+
+# Make exe versions of the scripts:
+EntryPoints = {
+}
+
+setup(
+    url=ProjecUrl,
+    name=Name,
+    zip_safe=False,
+    version=Version,
+    author=Author,
+    author_email=AuthorEmail,
+    description=ShortDescription,
+    long_description=Description,
+    license=License,
+    scripts=ProjectScripts,
+    install_requires=needed,
+    include_package_data=True,
+    packages=find_packages('src'),
+    package_data=PackageData,
+    package_dir = {'': 'src'},
+    eager_resources = EagerResources,
+    entry_points = EntryPoints,
+    namespace_packages = ['ua2'],
+)
