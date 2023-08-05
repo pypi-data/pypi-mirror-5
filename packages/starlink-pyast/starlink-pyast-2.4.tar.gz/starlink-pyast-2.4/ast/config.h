50801966d888830906782d921bb30f93a72ac61d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:40 +0000

    Add package starlink-pyast/2.4/starlink-pyast-2.4.tar.gz

---
+++ packages/starlink-pyast/starlink-pyast-2.4.tar.gz/starlink-pyast-2.4/ast/config.h
@@ -0,0 +1,21 @@
+/* Define to 1 if you have the declaration of `isfinite', and to 0 if you
+   don't. */
+#define HAVE_DECL_ISFINITE 1
+
+/* Define to 1 if you have the declaration of `isnan', and to 0 if you don't.
+   */
+#define HAVE_DECL_ISNAN 1
+
+/* Define to 1 if the system has the type `int64_t'. */
+#define HAVE_INT64_T 1
+#define HAVE_UINT64_T 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `isfinite' function. */
+/* #undef HAVE_ISFINITE */
+
+/* Define to 1 if you have the `isnan' function. */
+#define HAVE_ISNAN 1
+
