22bd009e449ee9998e40c2e0a49c559a8a20d311
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:19 +0000

    Add package flexipy/0.4.2/flexipy-0.4.2.tar.gz

---
+++ packages/flexipy/flexipy-0.4.2.tar.gz/flexipy-0.4.2/flexipy/api.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+This module implements the flexipy API.
+
+:copyright: (c) 2012 by Jakub Ječmínek.
+:license: BSD, see LICENSE for more details.
+"""
+from adresar import Adresar
+from faktura import Faktura
+from bank import Banka
