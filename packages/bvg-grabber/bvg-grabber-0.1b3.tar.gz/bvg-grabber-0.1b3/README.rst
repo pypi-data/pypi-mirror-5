e8b3bda787efe2a215245abefd1c2cfe755c0c50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:55 +0000

    Add package bvg-grabber/0.1b3/bvg-grabber-0.1b3.tar.gz

---
+++ packages/bvg-grabber/bvg-grabber-0.1b3.tar.gz/bvg-grabber-0.1b3/README.rst
@@ -0,0 +1,6 @@
+bvg-grabber
+===========
+
+.. image:: https://secure.travis-ci.org/Markush2010/bvg-grabber.png?branch=master
+    :alt: Build Status
+    :target: http://travis-ci.org/Markush2010/bvg-grabber
---
+++ packages/bvg-grabber/bvg-grabber-0.1b3.tar.gz/bvg-grabber-0.1b3/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import codecs
+
+from os import path
+from setuptools import setup, find_packages
+
+from bvggrabber import get_version
+
+BASEDIR = path.dirname(__file__)
+
+open = lambda filepath: codecs.open(filepath, 'r', 'utf-8')
+
+description = open(path.join(BASEDIR, 'README.rst')).read()
+requirements = [line for line in open(path.join(BASEDIR, 'requirements.txt'))]
+
+setup(
+    name='bvg-grabber',
+    description='Querying the upcoming public transport departures in Berlin',
+    long_description=description,
+    version=get_version(),
+    url='https://github.com/Markush2010/bvg-grabber',
+    author='Christian Struck, Markus Holtermann',
+    author_email='',
+    license='BSD',
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    install_requires=requirements,
+    test_suite="tests",
+    scripts=['bvg-grabber.py'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+)
