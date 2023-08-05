16a69752f082f862e95584fd7b6cb20d2c039f4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:18 +0000

    Add package sorted_test01/1.0.0/sorted_test01-1.0.0.tar.gz

---
+++ packages/sorted_test01/sorted_test01-1.0.0.tar.gz/sorted_test01-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+		name='sorted_test01',
+		version='1.0.0',
+		py_modules=['sorted_test01'],
+		author='ssin6505',
+		author_email='ssin6505@gmail.com',
+		url='',
+		description='A sorted test.',
+	 )
+
