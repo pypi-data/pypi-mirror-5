150bd832d8175e4a289b316bcc61647125dfe520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:03 +0000

    Add package beantop/0.2.5/beantop-0.2.5.tar.gz

---
+++ packages/beantop/beantop-0.2.5.tar.gz/beantop-0.2.5/setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup, find_packages
+
+
+version = '0.2.5'
+README = os.path.join(os.path.dirname(__file__), 'README.md')
+long_description = open(README).read() + 'nn'
+
+setup(name='beantop', version=version, 
+    author='alexsiri7',
+    author_email='alexsiri7@gmail.com',
+    license='GPL',
+    long_description=long_description,
+    url='https://github.com/alexsiri7/beantop', 
+    packages = find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Programming Language :: Python", 
+        "Intended Audience :: System Administrators", 
+        "Topic :: Utilities"], 
+    entry_points = {
+     'console_scripts': [
+         'beantop = beantop.beantop:main',
+         ],
+      },
+    install_requires=["PyYAML"]
+    )
