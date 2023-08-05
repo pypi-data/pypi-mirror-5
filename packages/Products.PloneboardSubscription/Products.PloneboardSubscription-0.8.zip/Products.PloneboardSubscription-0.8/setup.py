16d8088af5f700bf0aee13f03af4fa0c0b2a3ebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:55 +0000

    Add package Products.PloneboardSubscription/0.8/Products.PloneboardSubscription-0.8.zip

---
+++ packages/Products.PloneboardSubscription/Products.PloneboardSubscription-0.8.zip/Products.PloneboardSubscription-0.8/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.8'
+
+setup(name='Products.PloneboardSubscription',
+      version=version,
+      description="Enables email subscription to Ploneboard posts",
+      long_description=open("README.rst").read() + "\n\n\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Suresh V',
+      author_email='suresh@grafware.com',
+      url='http://plone.org/products/products.ploneboardsubscription',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
