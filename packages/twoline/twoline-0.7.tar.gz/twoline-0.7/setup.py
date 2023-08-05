c12f5218c96bcad1d0653077973b6750c6b6d96d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:36 +0000

    Add package twoline/0.7/twoline-0.7.tar.gz

---
+++ packages/twoline/twoline-0.7.tar.gz/twoline-0.7/setup.py
@@ -0,0 +1,35 @@
+import os
+import multiprocessing
+
+from setuptools import setup, find_packages
+
+with open(
+    os.path.join(
+        os.path.dirname(__file__),
+        'requirements.txt'
+    )
+) as f:
+    required = f.read().splitlines()
+
+setup(
+    name='twoline',
+    version='0.7',
+    url='http://bitbucket.org/latestrevision/twoline/',
+    description='Manager for simple character LCDs',
+    author='Adam Coddington',
+    author_email='me@adamcoddington.net',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ],
+    install_requires=required,
+    packages=find_packages(),
+    entry_points={'console_scripts': [
+        'twoline = twoline.cmdline:run_from_cmdline']},
+    test_suite='nose.collector',
+    tests_require=[
+        'nose',
+    ]
+)
