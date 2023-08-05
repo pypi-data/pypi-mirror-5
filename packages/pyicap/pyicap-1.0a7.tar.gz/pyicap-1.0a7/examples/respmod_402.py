e6dd170eada6026b09baaa19457ab6e2cc93de78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:59 +0000

    Add package pyicap/1.0a7/pyicap-1.0a7.tar.gz

---
+++ packages/pyicap/pyicap-1.0a7.tar.gz/pyicap-1.0a7/examples/respmod_402.py
@@ -0,0 +1,30 @@
+#!/bin/env python
+# -*- coding: utf8 -*-
+
+import random
+import SocketServer
+
+from pyicap import *
+
+class ThreadingSimpleServer(SocketServer.ThreadingMixIn, ICAPServer):
+    pass
+
+class ICAPHandler(BaseICAPRequestHandler):
+
+    def example_OPTIONS(self):
+        self.set_icap_response(200)
+        self.set_icap_header('Methods', 'RESPMOD')
+        self.set_icap_header('Preview', '0')
+        self.send_headers(False)
+
+    def example_RESPMOD(self):
+        self.no_adaptation_required()
+
+port = 13440
+
+server = ThreadingSimpleServer(('', port), ICAPHandler)
+try:
+    while 1:
+        server.handle_request()
+except KeyboardInterrupt:
+    print "Finished"
