1078adede3f3c1280ab64ca65c56fa4e59c5359e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:13 +0000

    Add package range-regex/1.0.4/range-regex-1.0.4.tar.gz

---
+++ packages/range-regex/range-regex-1.0.4.tar.gz/range-regex-1.0.4/setup.py
@@ -0,0 +1,20 @@
+import os
+from setuptools import find_packages, setup
+
+root = os.path.dirname(os.path.realpath(__file__))
+long_description = open(os.path.join(root, 'README.rst')).read()
+
+setup(
+    name='range-regex',
+    version='1.0.4',
+    description='Python numeric range regular expression generator',
+    long_description=long_description,
+    url='http://github.com/dimka665/range-regex',
+    author='Dmitry Voronin',
+    author_email='dimka665@gmail.com',
+    license='BSD',
+    # packages=['range_regex'],
+    packages=find_packages(),
+    include_package_data=True,
+    keywords='numeric range regex regular expression generator',
+)
