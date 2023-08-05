f7a1c3e45eedaeedc3cc015e93c55319c0f528ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:15 +0000

    Add package pyuploadcare/1.0.2/pyuploadcare-1.0.2.tar.gz

---
+++ packages/pyuploadcare/pyuploadcare-1.0.2.tar.gz/pyuploadcare-1.0.2/pyuploadcare/conf.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+"""
+Configuration variables.
+
+"""
+
+from __future__ import unicode_literals
+
+pub_key = None
+secret = None
+
+api_version = '0.2'
+api_base = 'https://api.uploadcare.com/'
+upload_base = 'https://upload.uploadcare.com/'
+cdn_base = 'https://ucarecdn.com/'
+
+verify_api_ssl = True
+verify_upload_ssl = True
