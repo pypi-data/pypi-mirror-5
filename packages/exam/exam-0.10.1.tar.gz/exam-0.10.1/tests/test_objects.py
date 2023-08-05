92891e4ad0e0f847e99c456a2982642c20ba566f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:16 +0000

    Add package exam/0.10.1/exam-0.10.1.tar.gz

---
+++ packages/exam/exam-0.10.1.tar.gz/exam-0.10.1/tests/test_objects.py
@@ -0,0 +1,23 @@
+from mock import sentinel
+from tests import TestCase
+
+from exam.objects import always, noop
+
+
+class TestAlways(TestCase):
+
+    def test_always_returns_identity(self):
+        fn = always(sentinel.RESULT_VALUE)
+        assert fn() is sentinel.RESULT_VALUE
+        assert fn(1, key='value') is sentinel.RESULT_VALUE
+
+    def test_can_be_called_with_anything(self):
+        noop()
+        noop(1)
+        noop(key='val')
+        noop(1, key='val')
+        noop(1, 2, 3, key='val')
+        noop(1, 2, 3, key='val', another='thing')
+
+    def test_returns_none(self):
+        self.assertIsNone(noop())
