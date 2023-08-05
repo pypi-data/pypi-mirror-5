21c589c17cce4a2afa46e97e3c4346f23e673ed3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:50 +0000

    Add package awstools/0.3.3/awstools-0.3.3.zip

---
+++ packages/awstools/awstools-0.3.3.zip/awstools-0.3.3/CHANGES.rst
@@ -0,0 +1,36 @@
+Changelog for Awstools
+======================
+
+
+0.3.3 (2013-07-04)
+------------------
+
+- Add --force option to `cfn update` command
+- Add a `cfn batch-update` command
+
+
+0.3.2 (2013-06-11)
+------------------
+
+- Complete hgignore
+- ec2ssh: enhance fallback when denying connection to multiple instances
+- Fix wrong current_capacity displayed in autoscale update utility
+
+
+0.3.1 (2013-03-13)
+------------------
+
+- fix *cfn setcapacity* setting 0 instead of the desired value
+
+
+0.3 (2013-03-11)
+----------------
+
+- start using zest.releaser for versioning
+
+
+0.2.3 (2013-02-01)
+------------------
+
+- display template description in cfn subcommands
+- create subcommand "cfn activities"
---
+++ packages/awstools/awstools-0.3.3.zip/awstools-0.3.3/setup.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2012 Ludia Inc.
+# This software is licensed as described in the file LICENSE, which
+# you should have received as part of this distribution.
+# Author: Pior Bastida <pbastida@socialludia.com>
+
+from setuptools import setup, find_packages
+import codecs
+
+version = '0.3.3'
+
+
+def read(filename):
+    return unicode(codecs.open(filename, encoding='utf-8').read())
+
+long_description = '\n\n'.join([read('README.rst'),
+                                read('CREDITS.rst'),
+                                read('CHANGES.rst')])
+
+setup(name='awstools',
+      version=version,
+      description="High level tools to manage an AWS infrastructure",
+      long_description=long_description,
+      classifiers=[
+          "Intended Audience :: System Administrators",
+          "Environment :: Console",
+          "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+          "Operating System :: POSIX :: Linux",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.7",
+      ],
+      keywords='',
+      author='Pior Bastida',
+      author_email='pior@pabstida.net',
+      url='https://bitbucket.org/pior/awstools',
+      license='GPL',
+      packages=find_packages(exclude=["awstools.costreport"]),
+      # include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          "argh",
+          "PyYaml",
+          "boto",
+      ],
+      extras_require={
+          'test': ['nose', 'nosexcover', 'coverage', 'mock']
+      },
+      entry_points={},
+      scripts=[
+          "scripts/cfn",
+          "scripts/ec2ssh",
+      ]
+      )
