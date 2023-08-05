d78da9eb59264cd217563de9742003e8f6d7c31d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:44 +0000

    Add package simpleblog3/0.9.3/simpleblog3-0.9.3.tar.gz

---
+++ packages/simpleblog3/simpleblog3-0.9.3.tar.gz/simpleblog3-0.9.3/setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3 -u
+"""
+Setup script for SIMPLEBLOG package
+Copyright (C) 2012-2013 by Peter A. Donis
+
+Released under the GNU General Public License, Version 2
+See the LICENSE and README files for more information
+"""
+
+from simpleblog import __version__ as version
+
+name = "simpleblog3"
+description = "A simple Python 3 blogging system."
+
+startline = 5
+endspec = "Copyright and License"
+
+author = "Peter A. Donis"
+author_email = "peterdonis@alum.mit.edu"
+
+data_dirs = ["examples"]
+
+dev_status = "Beta"
+
+license = "GPLv2"
+
+classifiers="""
+Environment :: Console
+Intended Audience :: Developers
+Intended Audience :: End Users/Desktop
+Operating System :: POSIX
+Operating System :: POSIX :: Linux
+"""
+
+requires = """
+plib3.stdlib (>=0.9.2)
+"""
+
+rst_header_template = """**{basename}** for {name} {version}
+
+:Author:        {author}
+:Release Date:  {releasedate}
+"""
+
+
+if __name__ == '__main__':
+    from distutils.core import setup
+    from setuputils import convert_md_to_rst, current_date, setup_vars
+    
+    convert_md_to_rst(rst_header_template,
+        startline=2,
+        name=name.upper(),
+        version=version,
+        author=author,
+        releasedate=current_date("%d %b %Y")
+    )
+    
+    setup(**setup_vars(globals()))
