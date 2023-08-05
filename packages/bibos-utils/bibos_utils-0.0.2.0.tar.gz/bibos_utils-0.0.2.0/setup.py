1919605df27a19b89bde7d794b60fabc666d6e07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:00 +0000

    Add package bibos-utils/0.0.2.0/bibos_utils-0.0.2.0.tar.gz

---
+++ packages/bibos-utils/bibos_utils-0.0.2.0.tar.gz/bibos_utils-0.0.2.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+def readme():
+    with open('README') as f:
+        return f.read()
+
+
+setup(
+    name='bibos_utils',
+    version='0.0.2.0',
+    description='Utilities for the BibOS system',
+    long_description="""
+    Shared Python library for the BibOS admin and client systems""",
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+    ],
+    url='https://github.com/magenta-aps/',
+    author='Magenta ApS',
+    author_email='info@magenta-aps.dk',
+    license='GPLv3',
+    packages=['bibos_utils'],
+    install_requires=['PyYAML',],
+    scripts=['bin/get_bibos_config', 'bin/set_bibos_config'],
+    zip_safe=False
+)
