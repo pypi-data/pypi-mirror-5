bc1dc650c41a050c5f97ed3b110061083032ee31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:02 +0000

    Add package openaccess_epub/0.3.7/openaccess_epub-0.3.7.tar.gz

---
+++ packages/openaccess_epub/openaccess_epub-0.3.7.tar.gz/openaccess_epub-0.3.7/src/openaccess_epub/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+The openaccess_epub module provides a large suite of complementary tools and
+uilities that enable the conversion of xml files to epub for academic journals.
+"""
+
+from ._version import __version__
+from .main import OAEParser, main
---
+++ packages/openaccess_epub/openaccess_epub-0.3.7.tar.gz/openaccess_epub-0.3.7/src/openaccess_epub/ncx/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+from openaccess_epub.ncx.tocncx import TocNCX
