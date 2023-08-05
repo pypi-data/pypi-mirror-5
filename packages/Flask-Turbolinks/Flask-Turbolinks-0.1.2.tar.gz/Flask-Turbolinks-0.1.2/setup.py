791bbdcc1895e5fd5a3f05e581bb672645e22f6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:50 +0000

    Add package Flask-Turbolinks/0.1.2/Flask-Turbolinks-0.1.2.tar.gz

---
+++ packages/Flask-Turbolinks/Flask-Turbolinks-0.1.2.tar.gz/Flask-Turbolinks-0.1.2/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+try:
+    # python setup.py test
+    import multiprocessing
+except ImportError:
+    pass
+
+from setuptools import setup
+
+setup(
+    name='Flask-Turbolinks',
+    version='0.1.2',
+    url='https://github.com/lepture/flask-turbolinks',
+    author='Hsiaoming Yang',
+    author_email='me@lepture.com',
+    description='Turbolinks for Flask.',
+    long_description=open('README.rst').read(),
+    license=open('LICENSE').read(),
+    py_modules=['flask_turbolinks'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=['Flask'],
+    tests_require=['nose'],
+    test_suite='nose.collector',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
