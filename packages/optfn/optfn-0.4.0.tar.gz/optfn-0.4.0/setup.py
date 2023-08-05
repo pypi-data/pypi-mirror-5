0abef532a9e3a69425f8e147650c0323be391133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:18 +0000

    Add package optfn/0.4.0/optfn-0.4.0.tar.gz

---
+++ packages/optfn/optfn-0.4.0.tar.gz/optfn-0.4.0/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='optfn',
+      version='0.4.0',
+      description="Generate commandline flags from function arguments. Based off of simonw's optfunc",
+      author='Yusuf Simonson',
+      url='https://github.com/ysimonson/optfn',
+      license='BSD',
+      classifiers=['Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Programming Language :: Python :: 2 :: Only',
+                   'Environment :: Console',
+                   'Development Status :: 4 - Beta'],
+      py_modules=['optfn'])
