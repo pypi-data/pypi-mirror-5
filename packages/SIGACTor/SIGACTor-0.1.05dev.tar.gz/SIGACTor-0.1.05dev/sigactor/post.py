768378ccf5d40103ce0c3163dc3eb29f7cbcb834
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:18 +0000

    Add package SIGACTor/0.1.05dev/SIGACTor-0.1.05dev.tar.gz

---
+++ packages/SIGACTor/SIGACTor-0.1.05dev.tar.gz/SIGACTor-0.1.05dev/sigactor/post.py
@@ -0,0 +1,146 @@
+from .activity import Activity
+from bs4 import BeautifulSoup
+from datetime import datetime
+import dateutil.tz
+
+
+class Post():
+    def __init__(self, home, entry, date):
+        self.home = home
+        self.entry = entry
+
+        self.date = date
+        self.now = datetime.now(dateutil.tz.tzlocal())
+        self.timezone_info = self.now.tzinfo
+        self.current_date = self.date.astimezone(self.timezone_info)
+        self.formatted_date = self.current_date.strftime('%Y-%m-%d')
+        self.time = self.current_date.strftime('%Y-%m-%d-%H%M')
+
+        self.link = self.entry.link
+
+        self.content = self.entry.content[0]['value']
+        self.content = BeautifulSoup(self.content)
+        self.content = str(self.content)
+
+        print('\nPost content (' + self.time + '):\n' + self.content)
+        self.clean_content = self.content.translate(str.maketrans('', '', ':'))
+        self.words = self.clean_content.split()
+
+        self.province = ''
+        if len(self.words) > 1:
+            if 'province' in self.words:
+                term_location = self.words.index('province')
+                self.province = self.words[term_location - 1]
+                if self.words[1] == 'province':
+                    term_location = self.clean_content.find('province')
+                    self.clean_content = (
+                        self.clean_content[term_location + 9:]
+                    )
+
+        if self.words[0].lower() in ['rif', 'reef']:
+            self.province = self.words[1]
+            second_space = self.clean_content.replace(' ', '', 1).find(' ')
+            self.clean_content = self.clean_content[second_space + 2:]
+
+        self.town = ''
+        if self.words[0].lower() == 'damascus':
+            self.province = 'Dimashq'
+            if self.clean_content[:8].lower() == 'damascus':
+                self.clean_content = self.clean_content[9:]
+            self.town = 'Damascus'
+
+        self.sentences = self.clean_content.split('. ')
+        self.contents = []
+
+        self.strategic_terms = [
+                'clash',
+                'bomb ',
+                'ied',
+                'vbied',
+                'air',
+                'helicopter',
+                'seize',
+                'capture',
+                'taken over',
+                'take control',
+                'ambush',
+                'raid',
+            ]
+
+        for sentence in self.sentences:
+            words = sentence.split()
+
+            include = False
+            for term in self.strategic_terms:
+                if self.contains(sentence.lower(), term):
+                    include = True
+
+            if include:
+                if words[0].lower() in ['he', 'she', 'it', 'we', 'they']:
+                    self.contents[-1] += '. ' + sentence
+                else:
+                    self.contents.append(sentence)
+
+        print(
+            '\nAre there ' + str(len(self.contents)) +
+            ' activities in this post '
+            'with the following contents:'
+        )
+
+        for number in range(0, len(self.contents)):
+            print('\n' + str(number + 1) + ': ' + self.contents[number])
+
+        answer = input(
+            '\n? (If correct, press Enter. '
+            'Otherwise, enter the number of activities in this post, or enter "q" to finish entering posts) '
+        )
+
+        while True:
+            if answer == '':
+                for number in range(0, len(self.contents)):
+                    home.activities.append(Activity(
+                        self.home, self, self.contents[number], number
+                    ))
+                break
+            if answer == 'q':
+                self.home.stop = True
+                break
+            try:
+                answer = int(answer)
+            except ValueError:
+                answer = input(
+                    '\nInvalid answer. '
+                    '(Press Enter if the activities are divided correctly '
+                    'or enter the correct number of activities) '
+                )
+                continue
+            else:
+                answer = int(answer)
+                self.contents = []
+                for number in range(0, answer):
+                    content = input(
+                        '\nEnter the content of activity ' +
+                        str(number + 1) + ': '
+                    )
+                    self.contents.append(content)
+                answer = ''
+
+    def search(self, start_tag, end_tag, subject=None):
+        if subject is None:
+            subject = self.content
+        start = subject.find(start_tag) + len(start_tag)
+        end = subject[start:].find(end_tag)
+        return subject[start:start + end]
+
+    def contains(self, subject, *args):
+        for arg in args:
+            if (
+                isinstance(arg, str)
+            ):
+                if subject.find(arg) != -1:
+                    return True
+            else:
+                for element in arg:
+                    if subject.find(element) != -1:
+                        return True
+        return False
---
+++ packages/SIGACTor/SIGACTor-0.1.05dev.tar.gz/SIGACTor-0.1.05dev/sigactor/property.py
@@ -0,0 +1,253 @@
+class Property():
+    def __init__(self, home, post, activity, name):
+        self.home = home
+        self.post = post
+        self.activity = activity
+        self.name = name
+
+        self.value = ''
+        self.y = ''
+
+        if self.name not in self.home.unchecked_values:
+            print('\nContent:\n' + self.activity.content)
+
+        if self.name == 'Title':
+            self.value = ''
+
+        if self.name == 'Location':
+            self.province = 'Rif ' + self.post.province
+            self.y += self.province + ' comes before "province." '
+
+            self.town = ''
+            self.y = 'No town found. '
+
+            if self.post.province in ['Aleppo', 'Hama', 'Homs']:
+                self.town = self.post.province
+            if self.post.province == 'Raqqah':
+                self.town = 'al-Raqqah'
+            self.y = 'Town determined by province. '
+
+            self.town = self.parse_location('town', 'city', 'village')
+
+            if self.post.town != '':
+                self.town = self.post.town
+                self.y = '"' + self.town + '" begins post."'
+
+            for term in ['deir', 'tel']:
+                if term in self.activity.clean_words:
+                    term_location = self.activity.clean_words.index(term)
+                    if len(self.activity.clean_words) > term_location + 1:
+                        self.town = (
+                            self.activity.words[term_location] +
+                            ' ' + self.activity.words[term_location + 1]
+                        )
+                        self.y = '"' + self.town + '" follows "' + term + '." '
+            if self.town == 'the':
+                self.value = (
+                    self.post.activities[self.number - 1].properties[1].town
+                )
+
+            self.neighborhood = self.parse_location(
+                'neighborhod', 'neighbourhood'
+            )
+            if self.neighborhood == 'the':
+                self.value = (
+                    self.post.activities[self.number - 1].
+                    properties[1].neighborhood
+                )
+
+            self.area = self.parse_location('area')
+            if self.area == 'the':
+                self.value = (
+                    self.post.activities[self.number - 1].properties[1].area
+                )
+
+            if self.area == '':
+                self.y += '"Area" does not occur. '
+            if self.neighborhood == '':
+                self.y += '"Neighborhood" does not occur. '
+            if self.town == '':
+                self.y += '"Town" does not occur. '
+            if self.province == '':
+                self.y += '"Province" does not occur.'
+
+            for term in [
+                self.area, self.neighborhood, self.town, self.province
+            ]:
+                if term != '':
+                    self.value += term + ', '
+            if self.value[-2:] == ', ':
+                self.value = self.value[:-2]
+
+            self.value = self.check(self.value)
+            self.activity.location = self.value
+
+        if self.name == 'Lat-Long':
+            self.value = ''
+
+        if self.name == 'Grid Prec':
+            self.value = ''
+
+        if self.name == 'Description':
+            self.value = self.activity.content
+            self.value = self.check(self.value)
+
+        if self.name == 'Orientation':
+            self.value = 'Anti-regime'
+
+            if self.contains('regime forces'):
+                self.value = 'Pro-regime'
+                self.y = 'Contains "regime force." '
+            else:
+                self.y = 'Does not contain "regime forces." '
+
+            if self.contains('air') or self.contains('helicopter'):
+                self.value = 'Pro-regime'
+
+            self.value = self.check(self.value)
+            if self.value == 'Pro-Regime':
+                self.activity.orientation = 'Regime'
+            else:
+                self.activity.orientation = 'Rebel'
+
+        if self.name == 'Ground':
+            if self.contains('clashes'):
+                self.value = 'Complex'
+                self.y = 'Contains "clashes." '
+
+                if self.contains('died', 'killed', 'dead'):
+                    self.value = 'DF'
+                    self.y += 'Contains "died," "killed," or "dead." '
+
+            self.value = self.check(self.value)
+            if self.value != '':
+                self.activity.verb = 'Clashes'
+
+        if self.name == 'Air':
+            if self.contains('air'):
+                self.value = 'UNK Strike'
+            if self.contains('helicopter'):
+                self.value = 'Rotary Wing Strike'
+
+            self.value = self.check(self.value)
+            if self.value != '':
+                self.activity.verb = 'Airstrike'
+
+        if self.name == 'Blast':
+            if self.contains('bomb ', 'IED'):
+                self.value = 'IED'
+            if self.contains('car bomb ', 'VBIED'):
+                self.value = 'VBIED'
+
+            self.value = self.check(self.value)
+            if self.value != '':
+                self.activity.verb = self.value
+
+        if self.name == 'Targeted':
+            if self.post.contains('execut'):
+                self.value = 'Execution'
+
+            self.value = self.check(self.value)
+            if self.value != '':
+                self.activity.verb = self.value
+
+        if self.name == 'Tactical':
+            if self.post.contains('ambush'):
+                self.value = 'Ambush'
+            if self.post.contains('seize', 'taken'):
+                self.value = 'Seize'
+            if self.post.contains('raid'):
+                self.value = 'Raid'
+
+            self.value = self.check(self.value)
+
+            if self.value != '':
+                self.activity.verb = self.value
+
+        if self.name == 'Outcome':
+            if self.activity.properties[7] in ['IED', 'VBIED']:
+                self.value = 'Effective'
+            self.value = self.check(self.value)
+
+        if self.name == 'Full Text':
+            self.value = self.post.content
+
+        if self.name == 'URL':
+            self.value = self.post.link
+
+        if self.name == 'Source Text':
+            self.value = (
+                'Syrian Observatory for Human Rights Facebook Page ' +
+                self.post.date
+            )
+
+        if self.name == 'Country Team':
+            self.value = 'MESP-Syria'
+
+        if self.name == 'Date Begin':
+            self.value = self.post.formatted_date
+
+        if self.name == 'Date End':
+            self.value = self.post.formatted_date
+
+    def check(self, value):
+        while True:
+            answer = input(
+                "\nIs '" + self.value + "' the correct " + self.name +
+                " ? (If correct, press enter. " +
+                "Otherwise, enter the correct value or 'n' for none. " +
+                "To see why this value was determined, enter 'y') "
+            )
+            if answer == '':
+                return value
+            elif answer == 'n':
+                return ''
+            elif answer == 'y':
+                print('\n' + self.y)
+            else:
+                return answer
+
+    def parse_location(self, *args):
+        for arg in args:
+            if arg in self.activity.clean_words:
+                term_location = self.activity.clean_words.index(arg)
+
+                if len(self.activity.clean_words) > term_location + 2:
+                    if self.activity.clean_words[term_location + 1] == 'of':
+                        value = self.activity.words[term_location + 2]
+                        self.y += (
+                            '"' + value + '" occurs after "' + arg + 'of." '
+                        )
+
+                        if self.activity.words[term_location + 2][0].isupper():
+                            value = (
+                                self.activity.words[term_location + 1] + ' ' +
+                                self.activity.words[term_location + 2]
+                            )
+
+                        if value == 'the':
+                            value = ''
+                        return value
+
+                value = self.activity.words[term_location - 1]
+                if self.activity.words[term_location - 2][0].isupper():
+                    value = (
+                        self.activity.words[term_location - 2] + ' ' +
+                        self.activity.words[term_location - 1]
+                    )
+                self.y += (
+                    '"' + value + '" appears before "' + arg + '." '
+                )
+
+                if value == 'the':
+                            value = ''
+                return value
+        return ''
+
+    def contains(self, *args):
+        for arg in args:
+            isinstance(arg, str)
+            if self.activity.clean_content.find(arg) != -1:
+                self.y = 'Contains "' + arg + '." '
+                return True
+        return False
