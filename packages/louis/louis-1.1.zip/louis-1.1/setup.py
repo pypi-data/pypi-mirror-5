b96c8c0f0f2d570422141766f83e0ff521e65926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:55 +0000

    Add package louis/1.1/louis-1.1.zip

---
+++ packages/louis/louis-1.1.zip/louis-1.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'louis',
+	version	= '1.1',
+	py_modules = ['louisPrinter'],
+	author = 'louis',
+	author_email = 'louis@gmail.com',
+	url = 'http://louis.com',
+	description = 'A simple louis .py file'
+)
