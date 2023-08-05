202f7655edeb902bd7c4da962fc288bcc6327e76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:42 +0000

    Add package git-orm/0.1a0/git-orm-0.1a0.tar.gz

---
+++ packages/git-orm/git-orm-0.1a0.tar.gz/git-orm-0.1a0/git_orm/testcases.py
@@ -0,0 +1,41 @@
+import shutil
+import stat
+from tempfile import mkdtemp
+
+from nose.tools import eq_, ok_, assert_in
+import pygit2
+
+from git_orm import set_repository, get_branch
+
+
+class GitTestCase:
+    def setup(self):
+        self.repo = pygit2.init_repository(mkdtemp(), False)
+        set_repository(self.repo.path)
+        self.branchref = 'refs/heads/{}'.format(get_branch())
+        self.assert_commit_count(0)
+
+    def teardown(self):
+        shutil.rmtree(self.repo.workdir)
+
+    def assert_file_exists(self, filename):
+        oid = self.repo.lookup_reference(self.branchref).oid
+        tree = self.repo[oid].tree
+        *path, filename = filename.split('/')
+        for name in path:
+            assert_in(name, tree)
+            entry = tree[name]
+            ok_(entry.attributes & stat.S_IFDIR)
+            tree = entry.to_object()
+        assert_in(filename, tree)
+        entry = tree[filename]
+        ok_(entry.attributes & stat.S_IFREG)
+
+    def assert_commit_count(self, expected_count):
+        try:
+            oid = self.repo.lookup_reference(self.branchref).oid
+        except KeyError:
+            count = 0
+        else:
+            count = sum(1 for _ in self.repo.walk(oid, pygit2.GIT_SORT_NONE))
+        eq_(count, expected_count)
