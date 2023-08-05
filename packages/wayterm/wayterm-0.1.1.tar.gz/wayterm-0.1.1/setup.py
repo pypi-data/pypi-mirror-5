f2b0adfe5be1a6ca09a1e68d4ffe155b7b1a3c4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:22 +0000

    Add package wayterm/0.1.1/wayterm-0.1.1.tar.gz

---
+++ packages/wayterm/wayterm-0.1.1.tar.gz/wayterm-0.1.1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='wayterm',
+    version='0.1.1',
+    description='Weibo client for terminal',
+    author='peichao.yu',
+    author_email='nerocrux@gmail.com',
+    py_modules=['wayterm', ],
+    package_data={'': ['LICENSE'], },
+    url='http://wayterm.nerocrux.org/',
+    license=open('LICENSE').read(),
+    long_description=open('README.rdoc').read(),
+    install_requires=[
+        "weibo >= 0.2.0",
+        "pyyaml",
+    ],
+)
