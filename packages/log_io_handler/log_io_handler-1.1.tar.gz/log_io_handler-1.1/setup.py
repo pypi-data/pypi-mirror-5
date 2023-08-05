c10ad9845ba34b5bcc32007077244cb7a0283141
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:53 +0000

    Add package log_io_handler/1.1/log_io_handler-1.1.tar.gz

---
+++ packages/log_io_handler/log_io_handler-1.1.tar.gz/log_io_handler-1.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+from log_io_handler import VERSION,AUTHOR,EMAIL,URL
+
+setup (
+    name = "log_io_handler",
+    author = AUTHOR,
+    author_email = EMAIL,
+    url = URL,
+    version = VERSION,
+    packages = ['log_io_handler',],
+    license = 'GPL v3 or higher',
+    description ='A python logging frame handler for log.io',
+    long_description = open('README.txt').read(),
+)
