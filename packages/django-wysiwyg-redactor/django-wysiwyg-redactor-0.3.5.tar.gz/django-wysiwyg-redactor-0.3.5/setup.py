6184bcf1818def5e981e146bafd0a7d741343d83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:28 +0000

    Add package django-wysiwyg-redactor/0.3.5/django-wysiwyg-redactor-0.3.5.tar.gz

---
+++ packages/django-wysiwyg-redactor/django-wysiwyg-redactor-0.3.5.tar.gz/django-wysiwyg-redactor-0.3.5/setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup
+
+f = open(os.path.join(os.path.dirname(__file__), 'README.md'))
+readme = f.read()
+f.close()
+
+setup(
+    name='django-wysiwyg-redactor',
+    version='0.3.5',
+    description='django-wysiwyg-redactor is a lightweight responsive wysiwyg editor for Django',
+    long_description=readme,
+    author="Douglas Miranda",
+    author_email='douglasmirandasilva@gmail.com',
+    url='https://github.com/douglasmiranda/django-wysiwyg-redactor',
+    packages=['redactor'],
+    include_package_data=True,
+    install_requires=['setuptools'],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
