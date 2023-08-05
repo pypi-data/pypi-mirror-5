4a54beb31c243b6f94ee145863b8cf3a7a95f1c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:14 +0000

    Add package tinys3/0.1.8/tinys3-0.1.8.zip

---
+++ packages/tinys3/tinys3-0.1.8.zip/tinys3-0.1.8/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='tinys3',
+      version='0.1.8',
+      description=("A small library for uploading files to S3,"
+                   "With support of async uploads, worker pools, cache headers etc"),
+
+      author='Shlomi Atar',
+      author_email='shlomi@smore.com',
+      url='https://www.smore.com/labs/tinys3/',
+      packages=['tinys3'],
+
+      classifiers=[
+          # make sure to use :: Python *and* :: Python :: 3 so
+          # that pypi can list the package on the python 3 page
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3'
+      ],
+
+      platforms='Any',
+      keywords=('s3', 'upload'),
+
+      package_dir={'': '.'},
+      requires=['requests (>= 1.2.0)', 'futures (>= 2.1.3)', 'nose', 'flexmock']
+)
