da3c3fcd9a49d4ee42f812fbe601467396e59fdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:31 +0000

    Add package argparse_actions/0.4.2/argparse_actions-0.4.2.tar.gz

---
+++ packages/argparse_actions/argparse_actions-0.4.2.tar.gz/argparse_actions-0.4.2/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup_details = dict(
+    name='argparse_actions',
+    version='0.4.2',
+    author='Hai Vu',
+    author_email='haivu2004@gmail.com',
+    packages=['argparse_actions', 'argparse_actions.test'],
+    url='http://pypi.python.org/pypi/argparse_actions/',
+    license='LICENSE.txt',
+    description='Custom actions for argparse package',
+    long_description=open('README.md').read(),
+    classifiers = [
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: BSD License',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Environment :: Console',
+    ]
+)
+
+setup(**setup_details)
