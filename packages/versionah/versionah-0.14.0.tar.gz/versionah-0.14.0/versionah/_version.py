f177f591f3e26acb0bd3cdd9a86777b09152bc2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:38 +0000

    Add package versionah/0.14.0/versionah-0.14.0.tar.gz

---
+++ packages/versionah/versionah-0.14.0.tar.gz/versionah-0.14.0/versionah/_version.py
@@ -0,0 +1,9 @@
+# This is versionah version 0.14.0 (2013-05-22)
+# pylint: disable=C0103, C0111, C0121, W0622
+
+dotted = '0.14.0'
+libtool = '14:20'
+hex = 0x000e00
+date = '2013-05-22'
+tuple = (0, 14, 0)
+web = 'versionah/0.14.0'
