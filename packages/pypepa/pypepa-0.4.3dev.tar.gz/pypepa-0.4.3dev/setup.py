d3d30f130dbb2df1fbd28e34671e31e5ab1cbdf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:44 +0000

    Add package pypepa/0.4.3dev/pypepa-0.4.3dev.tar.gz

---
+++ packages/pypepa/pypepa-0.4.3dev.tar.gz/pypepa-0.4.3dev/setup.py
@@ -0,0 +1,49 @@
+import os
+import sys
+import pypepa
+
+try:
+        from setuptools import setup
+except ImportError:
+        from distutils.core import setup
+
+requires = ["matplotlib", 
+            "numpy", 
+            "scipy", 
+            "pyparsing",
+            "colorama"]
+
+setup (
+        name = "pypepa",
+        version = pypepa.__version__,
+        description = "pypepa is a PEPA library and a toolset for PEPA.",
+        long_description = open("README.rst").read() + '\n\n' +
+                           open("HISTORY.rst").read(),
+        author = "Dariusz Dwornikowski", 
+        author_email = "dariusz.dwornikowski@cs.put.poznan.pl",
+        url = "https://github.com/tdi/pyPEPA",
+        packages = ["pypepa",
+                    "pypepa.cli",
+                    "pypepa.parsing",
+                    "pypepa.derivation",
+                    "pypepa.experiments",
+                    "pypepa.solvers",
+            ],
+        package_data={'': ['LICENCE.txt']},
+        install_requires = requires,
+        entry_points = {
+            'console_scripts': ['pypepa=pypepa.cli.cli:main',]
+            },
+        classifiers = [
+            'Environment :: Console',
+            'Development Status :: 4 - Beta',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 2.7',
+            'License :: OSI Approved :: Apache Software License',
+            'Intended Audience :: Science/Research',
+            'Operating System :: POSIX',
+            ],
+)
+
+
