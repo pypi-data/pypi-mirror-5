44442d88a51fa074cceea28adadd95eacac5fa37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:40 +0000

    Add package collective.z3cform.mapwidget/2.0b1/collective.z3cform.mapwidget-2.0b1.zip

---
+++ packages/collective.z3cform.mapwidget/collective.z3cform.mapwidget-2.0b1.zip/collective.z3cform.mapwidget-2.0b1/README.rst
@@ -0,0 +1,23 @@
+Introduction
+============
+
+Map widget for z3c.form based on collective.geo.mapwidget.
+
+For more information about `collective.geo`, see the `Coactivative home page
+<http://www.coactivate.org/projects/collectivegeo/>`_.
+
+Installation
+============
+
+This addon can be installed has any other addons, please follow official
+documentation_.
+
+Usage
+=====
+
+In order to use the widget contained in this package, use
+``collective.z3cform.mapwidget.widget.MapFieldWidget`` and set this as
+the widget for a suitable field within your form schema. One such suitable
+field type is that of ``zope.schema.TextLine``.
+
+.. _documentation: http://plone.org/documentation/kb/installing-add-ons-quick-how-to
---
+++ packages/collective.z3cform.mapwidget/collective.z3cform.mapwidget-2.0b1.zip/collective.z3cform.mapwidget-2.0b1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import os
+
+version = '2.0b1'
+
+setup(name='collective.z3cform.mapwidget',
+      version=version,
+      description="z3c.form map widget",
+      long_description=(
+          open("README.rst").read() + "\n" +
+          open(os.path.join("docs", "HISTORY.txt")).read()
+      ),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+          "Framework :: Plone",
+          "Programming Language :: Python",
+      ],
+      keywords='z3c.form form widget collective.geo map OpenLayers',
+      author='collective.geo Project',
+      author_email='collectivegeo-discussion@lists.coactivate.org',
+      url='http://www.coactivate.org/projects/collectivegeo/project-home',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.z3cform'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'collective.geo.mapwidget',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
