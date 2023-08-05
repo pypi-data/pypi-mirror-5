03fde6ecd32ebc26e973a83fb7c2b2f4ed52d879
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:52 +0000

    Add package cmsplugin-twitter/1.0.3/cmsplugin-twitter-1.0.3.tar.gz

---
+++ packages/cmsplugin-twitter/cmsplugin-twitter-1.0.3.tar.gz/cmsplugin-twitter-1.0.3/cmsplugin_twitter/cms_plugins.py
@@ -0,0 +1,19 @@
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+from django.utils.translation import ugettext_lazy as _
+
+from models import Twitter
+
+
+class TwitterPlugin(CMSPluginBase):
+    model = Twitter
+    name = _("Django CMS Twitter")
+    render_template = "cms_plugins/twitter.html"
+
+    def render(self, context, instance, placeholder):
+        context.update({
+            'object': instance,
+            })
+        return context
+
+plugin_pool.register_plugin(TwitterPlugin)
---
+++ packages/cmsplugin-twitter/cmsplugin-twitter-1.0.3.tar.gz/cmsplugin-twitter-1.0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+version = '1.0.3'
+
+setup(
+    name='cmsplugin-twitter',
+    version=version,
+    description='Twitter plugin to work with Django-CMS',
+    author='Vinit Kumar',
+    author_email='vinit.kumar@changer.nl',
+    url='http://github.com:vinitcool76/cmsplugin-twitter.git',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
+    ],
+)
