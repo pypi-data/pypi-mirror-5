4e3de91ab95283ce19e5563bb1e46778595991a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:54 +0000

    Add package django-pagseguro/1.3/django-pagseguro-1.3.tar.gz

---
+++ packages/django-pagseguro/django-pagseguro-1.3.tar.gz/django-pagseguro-1.3/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name='django-pagseguro',
+    version="1.3",
+    author='Fábio Cerqueira',
+    author_email='cerqueirasfabio@gmail.com',
+    maintainer="Fábio Cerqueira",
+    maintainer_email="cerqueirasfabio@gmail.com",
+    url='http://github.com/fabiocerqueira/django-pagseguro',
+    install_requires=[
+        'Django>=1.0'
+    ],
+    description = 'A pluggable Django application for integrating PagSeguro payment system',
+    long_description=open('README.rst').read(),
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development"
+    ],
+)
