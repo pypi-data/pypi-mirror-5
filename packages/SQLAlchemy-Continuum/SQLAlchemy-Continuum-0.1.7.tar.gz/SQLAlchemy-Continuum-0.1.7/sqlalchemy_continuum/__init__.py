36c69a5d086d75a971cb5096fc7a943bd79ffca0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:04 +0000

    Add package SQLAlchemy-Continuum/0.1.7/SQLAlchemy-Continuum-0.1.7.tar.gz

---
+++ packages/SQLAlchemy-Continuum/SQLAlchemy-Continuum-0.1.7.tar.gz/SQLAlchemy-Continuum-0.1.7/sqlalchemy_continuum/__init__.py
@@ -0,0 +1,27 @@
+import sqlalchemy as sa
+from .versioned import Versioned
+from .listener import (
+    configure_versioned_classes,
+    instrument_versioned_classes,
+    make_versioned
+)
+
+__all__ = (
+    Versioned,
+    configure_versioned_classes,
+    instrument_versioned_classes,
+    make_versioned
+)
+
+
+def versioned_objects(iterator):
+    return [obj for obj in iterator if hasattr(obj, '__versioned__')]
+
+
+def versioned_session(session):
+    @sa.event.listens_for(session, 'before_commit')
+    def before_commit(session):
+        session.execute(
+            '''INSERT INTO transaction_log (id, issued_at)
+            VALUES (txid_current(), NOW())'''
+        )
---
+++ packages/SQLAlchemy-Continuum/SQLAlchemy-Continuum-0.1.7.tar.gz/SQLAlchemy-Continuum-0.1.7/tests/__init__.py
@@ -0,0 +1,75 @@
+import sqlalchemy as sa
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy_continuum import (
+    Versioned,
+    versioned_session,
+    make_versioned
+)
+
+
+make_versioned(sa.orm.mapper)
+
+
+class TestCase(object):
+    inspect_column_order = False
+
+    def setup_method(self, method):
+        self.engine = create_engine(
+            'postgres://postgres@localhost/sqlalchemy_versioned_test'
+        )
+        self.connection = self.engine.connect()
+        #self.engine.echo = True
+        self.Model = declarative_base()
+
+        self.create_models()
+
+        sa.orm.configure_mappers()
+        if hasattr(self, 'Article'):
+            self.ArticleHistory = self.Article.__versioned__['class']
+        self.create_tables()
+
+        Session = sessionmaker(bind=self.connection)
+        versioned_session(Session)
+        self.session = Session()
+
+    def create_tables(self):
+        self.Model.metadata.create_all(self.connection)
+
+    def drop_tables(self):
+        self.Model.metadata.drop_all(self.connection)
+
+    def teardown_method(self, method):
+        self.session.close_all()
+        self.drop_tables()
+        self.engine.dispose()
+        self.connection.close()
+
+    def create_models(self):
+        class Article(self.Model, Versioned):
+            __tablename__ = 'article'
+            __versioned__ = {
+                'base_classes': (self.Model, ),
+                'inspect_column_order': self.inspect_column_order
+            }
+
+            id = sa.Column(sa.Integer, autoincrement=True, primary_key=True)
+            name = sa.Column(sa.Unicode(255))
+            content = sa.Column(sa.UnicodeText)
+            description = sa.Column(sa.UnicodeText)
+
+        class Tag(self.Model, Versioned):
+            __tablename__ = 'tag'
+            __versioned__ = {
+                'base_classes': (self.Model, ),
+                'inspect_column_order': self.inspect_column_order
+            }
+
+            id = sa.Column(sa.Integer, autoincrement=True, primary_key=True)
+            name = sa.Column(sa.Unicode(255))
+            article_id = sa.Column(sa.Integer, sa.ForeignKey(Article.id))
+            article = sa.orm.relationship(Article, backref='tags')
+
+        self.Article = Article
+        self.Tag = Tag
