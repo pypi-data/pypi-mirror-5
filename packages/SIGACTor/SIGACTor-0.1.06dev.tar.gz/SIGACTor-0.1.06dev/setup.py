85d1f2b216ca4f6f7a291e8a936f50380394516d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:18 +0000

    Add package SIGACTor/0.1.06dev/SIGACTor-0.1.06dev.tar.gz

---
+++ packages/SIGACTor/SIGACTor-0.1.06dev.tar.gz/SIGACTor-0.1.06dev/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='SIGACTor',
+    version='0.1.06dev',
+    description=open('README.txt').read(),
+    url='http://bitbucket.org/davidystephenson/sigactor',
+    author='David Y. Stephenson',
+    author_email='david@davidystephenson.com',
+    packages=['sigactor'],
+    license='Proprietary',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        'beautifulsoup4',
+        'feedparser',
+        'python-dateutil',
+    ],
+)
