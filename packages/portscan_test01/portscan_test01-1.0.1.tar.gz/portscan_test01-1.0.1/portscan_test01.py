03f5f7bd4d542ca0fbb39e04d2ca7530ea10ab12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:02 +0000

    Add package portscan_test01/1.0.1/portscan_test01-1.0.1.tar.gz

---
+++ packages/portscan_test01/portscan_test01-1.0.1.tar.gz/portscan_test01-1.0.1/portscan_test01.py
@@ -0,0 +1,33 @@
+import sys
+import socket
+socket.setdefaulttimeout(2)
+def retBanner(ip,port):
+	try:
+		s=socket.socket()
+		s.connect((ip,port))
+		banner=s.recv(1024)
+		return(banner)
+	except:
+		return(None)
+def main():
+	s=socket.socket()
+	if not len(sys.argv)==3:
+		print("Usage : "+sys.argv[0]+" IPAddress PORT")
+	else:		
+		ip=sys.argv[1]
+		port=int(sys.argv[2])
+		print(ip)
+		ip1=ip.split("/")
+		
+		if int(ip1[1])==24:
+			splitter=ip.split('.',3)
+			for i in range(1,255):
+				splitter[3]=str(i)
+				t=".".join(splitter)
+				banner=retBanner(t,port)
+				print(banner)	
+		else:
+			banner=retBanner(ip,port)
+
+if __name__ == '__main__' :
+	main()
---
+++ packages/portscan_test01/portscan_test01-1.0.1.tar.gz/portscan_test01-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+	name='portscan_test01',
+	version='1.0.1',
+	py_modules=['portscan_test01'],
+	author='ssin6505',
+	author_email='ssin6505@gmail.com',
+	url='',
+	description="A portscan test. version=1.0.1 is area scan, however, can't stop",
+	)
+
