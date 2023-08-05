48de49ffc5b2effb74eeb5b6176370286f8e9fc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:33 +0000

    Add package flower/0.5.1/flower-0.5.1.tar.gz

---
+++ packages/flower/flower-0.5.1.tar.gz/flower-0.5.1/flower/app.py
@@ -0,0 +1,39 @@
+from __future__ import absolute_import
+
+import tornado.web
+from tornado import ioloop
+
+import celery
+
+from flower.events import Events
+from flower.state import State
+from flower.urls import handlers
+
+
+class Flower(tornado.web.Application):
+    def __init__(self, celery_app=None, events=None, state=None,
+                 io_loop=None, options=None, **kwargs):
+        kwargs.update(handlers=handlers)
+        super(Flower, self).__init__(**kwargs)
+        self.io_loop = io_loop or ioloop.IOLoop.instance()
+        self.options = options or object()
+        self.auth = getattr(self.options, 'auth', [])
+        self.basic_auth = getattr(self.options, 'basic_auth', None)
+        self.broker_api = getattr(self.options, 'broker_api', None)
+
+        self.celery_app = celery_app or celery.Celery()
+        self.events = events or Events(celery_app, db=options.db,
+                                       persistent=options.persistent,
+                                       io_loop=self.io_loop,
+                                       max_tasks_in_memory=options.max_tasks)
+        self.state = State(celery_app, self.broker_api)
+
+    def start(self):
+        self.events.start()
+        if self.options.inspect:
+            self.state.start()
+        self.listen(self.options.port, address=self.options.address)
+        self.io_loop.start()
+
+    def stop(self):
+        self.events.stop()
