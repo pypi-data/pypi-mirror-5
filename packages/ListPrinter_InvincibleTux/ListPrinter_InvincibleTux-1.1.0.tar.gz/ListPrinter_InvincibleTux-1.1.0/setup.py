380dcbda2089225805bdd0b5f6cd6ac7b22ba14f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:27 +0000

    Add package ListPrinter_InvincibleTux/1.1.0/ListPrinter_InvincibleTux-1.1.0.tar.gz

---
+++ packages/ListPrinter_InvincibleTux/ListPrinter_InvincibleTux-1.1.0.tar.gz/ListPrinter_InvincibleTux-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'ListPrinter_InvincibleTux',
+	version = '1.1.0',
+	py_modules = ['ListPrinter_InvincibleTux'],
+	author = 'invincibletux',
+	author_email = 'tuxinvincible@gmail.com',
+	url = 'http://invincibletux.blogspot.com/',
+	description = 'Simple List printer',
+)
