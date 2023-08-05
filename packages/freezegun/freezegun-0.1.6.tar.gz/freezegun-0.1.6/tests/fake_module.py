63ab17423a6de55a3c00df117f9c24f813a6becc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:04 +0000

    Add package freezegun/0.1.6/freezegun-0.1.6.tar.gz

---
+++ packages/freezegun/freezegun-0.1.6.tar.gz/freezegun-0.1.6/tests/fake_module.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+from datetime import date
+
+
+def fake_datetime_function():
+    return datetime.now()
+
+
+def fake_date_function():
+    return date.today()
---
+++ packages/freezegun/freezegun-0.1.6.tar.gz/freezegun-0.1.6/tests/test_class_import.py
@@ -0,0 +1,45 @@
+import sure
+from .fake_module import fake_datetime_function, fake_date_function
+from freezegun import freeze_time
+from freezegun.api import FakeDatetime
+import datetime
+
+
+@freeze_time("2012-01-14")
+def test_import_datetime_works():
+    fake_datetime_function().day.should.equal(14)
+
+
+@freeze_time("2012-01-14")
+def test_import_date_works():
+    fake_date_function().day.should.equal(14)
+
+
+def test_start_and_stop_works():
+    freezer = freeze_time("2012-01-14")
+
+    result = fake_datetime_function()
+    result.__class__.should.equal(datetime.datetime)
+    result.__class__.shouldnt.equal(FakeDatetime)
+
+    freezer.start()
+    fake_datetime_function().day.should.equal(14)
+    fake_datetime_function().should.be.a(datetime.datetime)
+    fake_datetime_function().should.be.a(FakeDatetime)
+
+    freezer.stop()
+    result = fake_datetime_function()
+    result.__class__.should.equal(datetime.datetime)
+    result.__class__.shouldnt.equal(FakeDatetime)
+
+
+def test_isinstance_works():
+    date = datetime.date.today()
+    now = datetime.datetime.now()
+
+    freezer = freeze_time('2011-01-01')
+    freezer.start()
+    isinstance(date, datetime.date).should.equal(True)
+    isinstance(now, datetime.datetime).should.equal(True)
+    isinstance(now, datetime.date).should.equal(True)
+    freezer.stop()
