e901565391075f79f778d0a5269f9e33db17a32c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:17 +0000

    Add package ditto/1.0/ditto-1.0.tar.gz

---
+++ packages/ditto/ditto-1.0.tar.gz/ditto-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='ditto',
+      version='1.0',
+      description='Python Class Mocking Framework',
+      author='Kyle Derr',
+      author_email='kyle.derr@gmail.com',
+      url='https://github.com/ironport/ditto',
+      packages=find_packages(),
+)
