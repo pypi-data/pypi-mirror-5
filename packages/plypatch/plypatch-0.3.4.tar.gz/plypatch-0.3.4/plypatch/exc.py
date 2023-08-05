f15d9144e5a09650f2c468727d4c7f570379a339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:59 +0000

    Add package plypatch/0.3.4/plypatch-0.3.4.tar.gz

---
+++ packages/plypatch/plypatch-0.3.4.tar.gz/plypatch-0.3.4/plypatch/exc.py
@@ -0,0 +1,26 @@
+class PlyException(Exception):
+    pass
+
+
+class AlreadyLinkedToPatchRepo(PlyException):
+    pass
+
+
+class NoLinkedPatchRepo(PlyException):
+    pass
+
+
+class NoPatchesApplied(PlyException):
+    pass
+
+
+class NothingToResolve(PlyException):
+    pass
+
+
+class PathNotFound(PlyException):
+    pass
+
+
+class UncommittedChanges(PlyException):
+    pass
