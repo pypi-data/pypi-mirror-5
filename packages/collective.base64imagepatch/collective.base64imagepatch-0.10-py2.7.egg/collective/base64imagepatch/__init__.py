d43f425ba47a6584aa67426038e495919222de16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:42 +0000

    Add package collective.base64imagepatch/0.10/collective.base64imagepatch-0.10-py2.7.egg

---
+++ packages/collective.base64imagepatch/collective.base64imagepatch-0.10-py2.7.egg/collective/base64imagepatch/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import pkg_resources
+
+try:
+    pkg_resources.get_distribution('Products.Archetypes')
+except pkg_resources.DistributionNotFound:
+    HAS_ARCHETYPES = False
+else:
+    HAS_ARCHETYPES = True
+
+try:
+    pkg_resources.get_distribution('plone.dexterity')
+except pkg_resources.DistributionNotFound:
+    HAS_DEXTERITY = False
+else:
+    HAS_DEXTERITY = True
+
+logger = logging.getLogger('patch_base64images')
+
+def initialize(context):
+    """
+    Initializer called when used as a Zope 2 product.
+    """
+    pass
+    
