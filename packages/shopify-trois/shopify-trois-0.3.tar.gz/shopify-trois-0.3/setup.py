1086defc7f4910a55d86978e2d5c18d98dbf97be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:11 +0000

    Add package shopify-trois/0.3/shopify-trois-0.3.tar.gz

---
+++ packages/shopify-trois/shopify-trois-0.3.tar.gz/shopify-trois-0.3/setup.py
@@ -0,0 +1,46 @@
+"""
+Shopify Trois
+---------------
+
+Shopify API for Python 3
+"""
+
+from setuptools import setup
+
+
+setup(
+    name='shopify-trois',
+    version='0.3',
+    url='http://www.github.com/masom/shopify-trois',
+    license='MIT',
+    author='Martin Samson',
+    author_email='pyrolian@gmail.com',
+    maintainer='Martin Samson',
+    maintainer_email='pyrolian@gmail.com',
+    description='Shopify API for Python 3',
+    long_description=__doc__,
+    packages=[
+        'shopify_trois', 'shopify_trois.models', 'shopify_trois.engines',
+        'shopify_trois.engines.http'
+    ],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'requests'
+    ],
+    test_suite='nose.collector',
+    tests_require=[
+        'pytest', 'nose', 'mock'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
