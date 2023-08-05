fcdd0e0fb592d16e274c517aa143bce658109168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:21 +0000

    Add package Flask-BasicAuth/0.1.1/Flask-BasicAuth-0.1.1.tar.gz

---
+++ packages/Flask-BasicAuth/Flask-BasicAuth-0.1.1.tar.gz/Flask-BasicAuth-0.1.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='Flask-BasicAuth',
+    version='0.1.1',
+    url='https://github.com/jpvanhal/flask-basicauth',
+    license='BSD',
+    author='Janne Vanhala',
+    author_email='janne.vanhala@gmail.com',
+    description='HTTP basic access authentication for Flask.',
+    long_description=open('README.rst').read() + '\n\n' +
+                     open('CHANGES.rst').read(),
+    py_modules=['flask_basicauth'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=['Flask'],
+    test_suite='test_basicauth.suite',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
