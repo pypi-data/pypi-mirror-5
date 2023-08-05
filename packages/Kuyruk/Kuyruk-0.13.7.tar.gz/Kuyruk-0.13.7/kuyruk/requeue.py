319e73b311cd1ee6d85189515837eb7df38727f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:04 +0000

    Add package Kuyruk/0.13.7/Kuyruk-0.13.7.tar.gz

---
+++ packages/Kuyruk/Kuyruk-0.13.7.tar.gz/Kuyruk-0.13.7/kuyruk/requeue.py
@@ -0,0 +1,32 @@
+from __future__ import absolute_import
+import logging
+
+from kuyruk.channel import LazyChannel
+from kuyruk.queue import Queue
+
+logger = logging.getLogger(__name__)
+
+
+def run(config, args):
+    channel = LazyChannel.from_config(config)
+    channel.tx_select()
+    failed_queue = Queue('kuyruk_failed', channel)
+
+    count = 0
+    while 1:
+        tag, task_description = failed_queue.receive()
+        if task_description is None:
+            break
+
+        print "Requeueing task: %r" % task_description
+
+        queue_name = task_description['queue']
+        del task_description['queue']
+        task_queue = Queue(queue_name, channel)
+        task_queue.send(task_description)
+
+        failed_queue.ack(tag)
+        channel.tx_commit()
+        count += 1
+
+    print "%i failed tasks are requeued." % count
