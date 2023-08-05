33a5b8376be8493dd4b78876214a6fbbabbd8f5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:00 +0000

    Add package pyzabbix/0.3/pyzabbix-0.3.tar.gz

---
+++ packages/pyzabbix/pyzabbix-0.3.tar.gz/pyzabbix-0.3/pyzabbix/__init__.py
@@ -0,0 +1,120 @@
+import logging
+import requests
+import json
+
+
+class _NullHandler(logging.Handler):
+    def emit(self, record):
+        pass
+
+logger = logging.getLogger(__name__)
+logger.addHandler(_NullHandler())
+
+
+class ZabbixAPIException(Exception):
+    """ generic zabbix api exception
+    code list:
+         -32602 - Invalid params (eg already exists)
+         -32500 - no permissions
+    """
+    pass
+
+
+class AlreadyExists(ZabbixAPIException):
+    pass
+
+
+class ZabbixAPI(object):
+    def __init__(self,
+                 server='http://localhost/zabbix',
+                 session=None):
+
+        if session:
+            self.session = session
+        else:
+            self.session = requests.Session()
+
+        # Default headers for all requests
+        self.session.headers.update({
+            'Content-Type': 'application/json-rpc',
+            'User-Agent': 'python/pyzabbix'
+        })
+
+        self.auth = ''
+        self.id = 0
+
+        self.url = server + '/api_jsonrpc.php'
+        logger.info("JSON-RPC Server Endpoint: %s", self.url)
+
+    def login(self, user='', password=''):
+        self.auth = self.user.login(user=user, password=password)
+
+    def api_version(self):
+        return self.apiinfo.version()
+
+    def do_request(self, method, params=None):
+        request_json = {
+            'jsonrpc': '2.0',
+            'method': method,
+            'params': params or {},
+            'auth': self.auth,
+            'id': self.id,
+        }
+
+        logger.debug("Sending: %s", str(request_json))
+        response = self.session.get(
+            self.url,
+            data=json.dumps(request_json),
+        )
+        logger.debug("Response Code: %s", str(response.status_code))
+
+        # NOTE: Getting a 412 response code means the headers are not in the
+        # list of allowed headers.
+        response.raise_for_status()
+
+        if not len(response.text):
+            raise ZabbixAPIException("Received empty response")
+
+        try:
+            response_json = json.loads(response.text)
+        except ValueError:
+            raise ZabbixAPIException(
+                "Unable to parse json: %s" % response.text
+            )
+        logger.debug("Response Body: %s", json.dumps(response_json,
+                                                     indent=4,
+                                                     separators=(',', ': ')))
+
+        self.id += 1
+
+        if 'error' in response_json:  # some exception
+            msg = "Error {code}: {message}, {data} while sending {json}".format(
+                code=response_json['error']['code'],
+                message=response_json['error']['message'],
+                data=response_json['error']['data'],
+                json=str(request_json)
+            )
+            raise ZabbixAPIException(msg, response_json['error']['code'])
+
+        return response_json
+
+    def __getattr__(self, attr):
+        """Dynamically create an object class (ie: host)"""
+        return ZabbixAPIObjectClass(attr, self)
+
+
+class ZabbixAPIObjectClass(object):
+    def __init__(self, name, parent):
+        self.name = name
+        self.parent = parent
+
+    def __getattr__(self, attr):
+        """Dynamically create a method (ie: get)"""
+
+        def fn(**kwargs):
+            return self.parent.do_request(
+                '{}.{}'.format(self.name, attr),
+                kwargs
+            )['result']
+
+        return fn
