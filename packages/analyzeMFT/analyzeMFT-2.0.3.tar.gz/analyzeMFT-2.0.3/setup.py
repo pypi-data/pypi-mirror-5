c76e80731b91f8f22ff3ffde000d7beec297953b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:50 +0000

    Add package analyzeMFT/2.0.3/analyzeMFT-2.0.3.tar.gz

---
+++ packages/analyzeMFT/analyzeMFT-2.0.3.tar.gz/analyzeMFT-2.0.3/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='analyzeMFT',
+    version='2.0.3',
+    author='David Kovar',
+    author_email='dkovar@gmail.com',
+    packages=['analyzemft','analyzemft.test'],
+    url='http://github.com/dkovar/analyzeMFT',
+    license='LICENSE.txt',
+    description='Analyze the $MFT from a NTFS filesystem.',
+    long_description=open('README.txt').read(),
+    install_requires=[],
+    scripts=['analyzeMFT.py']
+)
+
