23236cb014a61196db43e569f7c535a774bdcaf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:06 +0000

    Add package pyramid_mongokit/0.0.6/pyramid_mongokit-0.0.6.tar.gz

---
+++ packages/pyramid_mongokit/pyramid_mongokit-0.0.6.tar.gz/pyramid_mongokit-0.0.6/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import setuptools
+
+if not getattr(setuptools, "_distribute", False):
+    raise SystemExit("""Setuptools is not supported. Please use Distribute""")
+
+setup_requires = [
+    'd2to1',
+    ]
+
+tests_require = [
+    'coverage',
+    'mock',
+    'nose',
+    'nosexcover',
+    'webtest',
+    'waitress',
+    'yanc',
+    ]
+
+extras_require = {'mongo': ['mongokit', 'pymongo==2.3']}
+
+setuptools.setup(
+    setup_requires=setup_requires,
+    tests_require=tests_require,
+    extras_require=extras_require,
+    d2to1=True,
+    package_data={
+        },
+    entry_points="""
+        [paste.app_factory]
+            main = pyramid_rest:main
+        """,
+    paster_plugins=['pyramid'],
+    )
