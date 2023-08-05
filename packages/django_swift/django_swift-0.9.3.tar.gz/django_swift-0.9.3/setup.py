99dba602aa3d937e6aeb70751e5339c5f3f45c86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:01 +0000

    Add package django_swift/0.9.3/django_swift-0.9.3.tar.gz

---
+++ packages/django_swift/django_swift-0.9.3.tar.gz/django_swift-0.9.3/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+
+requires = [
+    'python-swiftclient',
+]
+
+# Use part of the sphinx docs index for the long description
+
+setup(
+    name="django_swift",
+    version='0.9.3',
+    packages=find_packages(),
+    install_requires=requires,
+    description = 'a django storage for swift',
+    long_description=README,
+    author="duanhongyi",
+    author_email="duanhongyi@doopai.com",
+    license="BSD",
+    url="https://github.com/duanhongyi/django_swift/",
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ]
+)
