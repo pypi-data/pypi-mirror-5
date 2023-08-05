498cfcc83c4083cd3acdcabe3a18d7118f3f36a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:12 +0000

    Add package SharedTest/1.0.0/SharedTest-1.0.0.zip

---
+++ packages/SharedTest/SharedTest-1.0.0.zip/SharedTest-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'SharedTest',
+	version = '1.0.0',
+	modules = ['nester'],
+	author = 'Cosmo',
+	author_email = 'cosmo_lee@gmail.com',
+	url = 'www.google.com',
+	description = 'This is a distributed test.'
+)
