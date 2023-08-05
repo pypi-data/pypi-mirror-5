3d5367b6cfab52a7c97cc7e826094f7e79654ede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:49 +0000

    Add package RuSocSci/0.2/RuSocSci-0.2.tar.gz

---
+++ packages/RuSocSci/RuSocSci-0.2.tar.gz/RuSocSci-0.2/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='RuSocSci',
+	version='0.2',
+	description='PsychoPy module for Radboud University Buttonbox',
+	long_description='RuSocSci supplies functions similar to the PsychoPy.event keyboard functions for use of the Radboud University Faculty of Social Sciences Buttonbox',
+	author='Wilbert van Ham',
+	author_email='W.vanHam@socsci.ru.nl',
+	url='http://www.socsci.ru.nl/~wilberth/psychopy/index.html',
+	packages=['rusocsci'],
+	platforms=['MacOS', 'Microsoft', 'POSIX'],
+	license='GPLv3'
+	)
