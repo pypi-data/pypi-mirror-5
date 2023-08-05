53ee2fc4c0a10a4176a9100a371411552b15ceda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:43 +0000

    Add package collective.geo.geographer/2.0b1/collective.geo.geographer-2.0b1.zip

---
+++ packages/collective.geo.geographer/collective.geo.geographer-2.0b1.zip/collective.geo.geographer-2.0b1/docs/index.rst
@@ -0,0 +1,18 @@
+.. include:: ../collective/geo/geographer/README.rst
+
+
+Package documentation
+=====================
+
+.. toctree::
+    :maxdepth: 2
+
+    api
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ packages/collective.geo.geographer/collective.geo.geographer-2.0b1.zip/collective.geo.geographer-2.0b1/setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup, find_packages
+import os
+
+version = '2.0b1'
+install_requires = [
+    'setuptools',
+    'Products.CMFCore'
+]
+
+# Test if we have built-in JSON - Python 2.6+, 3.0+.
+# Older Python versions require simplejson.
+try:
+    import json  # pylint: disable=W0611
+except ImportError:
+    install_requires.append('simplejson')
+
+setup(name='collective.geo.geographer',
+      version=version,
+      description="Geographic annotation for Plone",
+      long_description=open(
+          "collective/geo/geographer/README.rst").read() + "\n" + open(
+              os.path.join("docs", "CONTRIBUTORS.txt")).read() + "\n" + open(
+                  os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+          "Framework :: Plone",
+          "Topic :: Internet",
+          "Topic :: Scientific/Engineering :: GIS",
+          "Programming Language :: Python",
+      ],
+      keywords='Zope Plone GIS KML Google Maps Bing OpenLayers',
+      author='Giorgio Borelli',
+      author_email='giorgio@giorgioborelli.it',
+      url="http://plone.org/products/collective.geo",
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.geo'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      extras_require={
+          'test': [
+              'plone.app.testing',
+          ],
+      },
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
