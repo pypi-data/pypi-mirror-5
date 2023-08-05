ed37ee167d59b254de3a5af98d1a350b8c16c106
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:24 +0000

    Add package Divan/0.0.2/Divan-0.0.2.tar.gz

---
+++ packages/Divan/Divan-0.0.2.tar.gz/Divan-0.0.2/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='Divan',
+      version='0.0.2',
+      description='Effortless CouchDB ODM',
+      author='Max Thayer',
+      author_email='garbados@gmail.com',
+      url='https://github.com/garbados/divan',
+      packages=['divan'],
+      test_suite="test"
+      )
