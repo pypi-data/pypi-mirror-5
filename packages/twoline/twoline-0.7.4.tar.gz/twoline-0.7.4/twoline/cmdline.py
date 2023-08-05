de99a7b469b63be4e33b3439afd1bc417ffa1378
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:37 +0000

    Add package twoline/0.7.4/twoline-0.7.4.tar.gz

---
+++ packages/twoline/twoline-0.7.4.tar.gz/twoline-0.7.4/twoline/cmdline.py
@@ -0,0 +1,41 @@
+import logging
+from optparse import OptionParser
+
+from twoline.manager import Manager
+
+
+def run_from_cmdline():
+    parser = OptionParser()
+    parser.add_option(
+        '--port', '-p', dest='port', default='6224'
+    )
+    parser.add_option(
+        '--ip', '-i', dest='ip', default='0.0.0.0'
+    )
+    parser.add_option(
+        '--loglevel', '-l', dest='loglevel', default='INFO'
+    )
+    parser.add_option(
+        '--size-x', '-x', dest='size_x', default='16',
+    )
+    parser.add_option(
+        '--size-y', '-y', dest='size_y', default='2',
+    )
+    parser.add_option(
+        '--text-cycle-interval', dest='text_cycle_interval', default='2'
+    )
+    parser.add_option(
+        '--blink-interval', dest='blink_interval', default='0.25'
+    )
+
+    options, args = parser.parse_args()
+
+    if len(args) != 1:
+        parser.error('Device required (usually a path in /dev/)')
+
+    logging.basicConfig(
+        level=logging.getLevelName(options.loglevel)
+    )
+
+    manager = Manager(*args, **vars(options))
+    manager.run()
