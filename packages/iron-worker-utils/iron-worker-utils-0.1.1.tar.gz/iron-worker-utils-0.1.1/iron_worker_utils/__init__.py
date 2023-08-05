aa803ac11d5a7ce61f47896e405033873d532d54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:41 +0000

    Add package iron-worker-utils/0.1.1/iron-worker-utils-0.1.1.tar.gz

---
+++ packages/iron-worker-utils/iron-worker-utils-0.1.1.tar.gz/iron-worker-utils-0.1.1/iron_worker_utils/__init__.py
@@ -0,0 +1,33 @@
+import json
+import sys
+
+
+def get_payload():
+    """
+    Get the payload for the current task.
+
+    Returns a dict.
+    """
+
+    payload_file = None
+    payload = None
+
+    for i in range(len(sys.argv)):
+        if sys.argv[i] == "-payload" and (i + 1) < len(sys.argv):
+            payload_file = sys.argv[i + 1]
+            with open(payload_file, 'r') as f:
+                payload = json.loads(f.read())
+            break
+    return payload
+
+
+def get_task_id():
+    """
+    Get the IronWorker task id for the current task.
+
+    Returns a string.
+    """
+
+    for i in range(len(sys.argv)):
+        if sys.argv[i] == "-id" and (i + 1) < len(sys.argv):
+            return sys.argv[i + 1]
---
+++ packages/iron-worker-utils/iron-worker-utils-0.1.1.tar.gz/iron-worker-utils-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='iron-worker-utils',
+    version='0.1.1',
+    packages=['iron_worker_utils', ],
+    license=open('LICENSE').read(),
+    description='A collection of utilities for writing IronWorker tasks.',
+    long_description=open('README.md').read(),
+    author='Michael Warkentin',
+    author_email='mwarkentin@gmail.com',
+    url='http://mwarkentin.github.io/iron-worker-utils/',
+)
