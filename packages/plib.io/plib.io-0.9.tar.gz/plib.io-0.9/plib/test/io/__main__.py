e5d951504447fe2c6491c49ac89be6350bfcb688
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:02 +0000

    Add package plib.io/0.9/plib.io-0.9.tar.gz

---
+++ packages/plib.io/plib.io-0.9.tar.gz/plib.io-0.9/plib/test/io/__main__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+"""
+Module MAIN
+Sub-Package TEST.IO of Package PLIB
+Copyright (C) 2008-2013 by Peter A. Donis
+
+Released under the GNU General Public License, Version 2
+See the LICENSE and README files for more information
+
+This is the test-running script for the PLIB.IO test suite.
+"""
+
+
+if __name__ == '__main__':
+    from plib.test.support import run_tests
+    
+    modules_with_doctests = []
+    
+    # TODO: set module_relative and verbosity from command line options
+    module_relative = False
+    verbosity = 0
+    
+    run_tests(__name__, modules_with_doctests, module_relative, verbosity)
