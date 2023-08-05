ecd7165b257d9325e1d18049507e9b4efd3924f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:06 +0000

    Add package logger/0.1/logger-0.1.tar.gz

---
+++ packages/logger/logger-0.1.tar.gz/logger-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='logger',
+    version='0.1',
+    description='Python logging helper',
+    author='Pavel Paulau',
+    author_email='pavel.paulau@gmail.com',
+    packages=['logger'],
+)
