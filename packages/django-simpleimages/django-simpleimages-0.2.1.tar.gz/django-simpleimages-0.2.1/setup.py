bca608b04a2d6ac4193401497344a92440d21f0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:30 +0000

    Add package django-simpleimages/0.2.1/django-simpleimages-0.2.1.tar.gz

---
+++ packages/django-simpleimages/django-simpleimages-0.2.1.tar.gz/django-simpleimages-0.2.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='django-simpleimages',
+    version='0.2.1',
+    author='Saul Shanabrook',
+    author_email='s.shanabrook@gmail.com',
+    packages=['simpleimages', 'simpleimages.test'],
+    url='https://www.github.com/saulshanabrook/django-simpleimages',
+    license='LICENSE.txt',
+    description='Opinionated Django image transforms on models',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "Django >= 1.5",
+    ],
+)
