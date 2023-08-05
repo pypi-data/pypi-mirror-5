5c27bea2360f4914300b450ef98f131ab5ff9945
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:53 +0000

    Add package infi.pysync/0.0.5/infi.pysync-0.0.5-py2.7.egg

---
+++ packages/infi.pysync/infi.pysync-0.0.5-py2.7.egg/infi/pysync/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.5"
+__git_commiter_name__ = "Tal Yalon"
+__git_commiter_email__ = "tal.yalon@gmail.com"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '60f21c1c4f84e09cbb7a706b8fe708c269911a02'
+__git_head_subject__ = 'Finished release v0.0.5.'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
