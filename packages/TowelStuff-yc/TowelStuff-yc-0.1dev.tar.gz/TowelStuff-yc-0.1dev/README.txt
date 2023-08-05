08527ab6fc72e5662876dc96614676ca42669261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:54 +0000

    Add package TowelStuff-yc/0.1dev/TowelStuff-yc-0.1dev.tar.gz

---
+++ packages/TowelStuff-yc/TowelStuff-yc-0.1dev.tar.gz/TowelStuff-yc-0.1dev/README.txt
@@ -0,0 +1,2 @@
+a project to learn packaging a python project.
+http://guide.python-distribute.org/quickstart.html
---
+++ packages/TowelStuff-yc/TowelStuff-yc-0.1dev.tar.gz/TowelStuff-yc-0.1dev/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+	name='TowelStuff-yc',
+	version='0.1dev',
+	packages=['towelstuff',],
+	long_description=open('README.txt').read(),
+)
