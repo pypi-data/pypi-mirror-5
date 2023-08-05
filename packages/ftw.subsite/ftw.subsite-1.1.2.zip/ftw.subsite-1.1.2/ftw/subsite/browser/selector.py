6d5e667d29e5d7c4d5b840a4eb878f2ffd38f829
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:58 +0000

    Add package ftw.subsite/1.1.2/ftw.subsite-1.1.2.zip

---
+++ packages/ftw.subsite/ftw.subsite-1.1.2.zip/ftw.subsite-1.1.2/ftw/subsite/browser/selector.py
@@ -0,0 +1,35 @@
+from zope.interface import implements
+from zope.viewlet.interfaces import IViewlet
+from ftw.subsite.interfaces import ISubsite
+from Products.CMFCore.utils import getToolByName
+from ftw.subsite.utils import get_nav_root
+from plone.app.layout.viewlets import common
+
+
+class LanguageSelector(common.ViewletBase):
+    """Language selector.
+    """
+    implements(IViewlet)
+
+    def nav_root(self):
+        return get_nav_root(self.context)
+
+    def available(self):
+        if ISubsite.providedBy(self.nav_root()):
+            return bool(self.nav_root().getLanguage_references())
+
+    def languages(self):
+        """Returns all possible languages based on the Subsite configuration.
+        """
+        ltool = getToolByName(self.nav_root(), 'portal_languages')
+        languages = []
+        subsites = self.nav_root().getLanguage_references()
+
+        for subsite in subsites:
+            lang = subsite.getForcelanguage()
+            if lang:
+                languages.append(
+                    dict(code=lang,
+                         url=subsite.absolute_url(),
+                         native=ltool.getNameForLanguageCode(lang)))
+        return languages
