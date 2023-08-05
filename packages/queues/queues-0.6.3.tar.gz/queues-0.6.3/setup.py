fb620f4c02cf4f2702956ce3d7295d84ef12f09c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:50 +0000

    Add package queues/0.6.3/queues-0.6.3.tar.gz

---
+++ packages/queues/queues-0.6.3.tar.gz/queues-0.6.3/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='queues',
+      version='0.6.3',
+      description='A lowest-common-denominator API for interacting with lightweight queue services.',
+      author='Matt Croydon',
+      author_email='mcroydon@gmail.com',
+      maintainer='Ben Timby',
+      maintainer_email='btimby@gmail.com',
+      url='http://code.google.com/p/queues/',
+      packages=['queues', 'queues.backends'],
+      py_modules=['test'],
+      package_dir={'queues': 'queues'},
+     )
