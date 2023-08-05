828ff9384ea5fe1be78726f5ee42647b01da2be5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:47 +0000

    Add package infi.projector/0.6.3/infi.projector-0.6.3.tar.gz

---
+++ packages/infi.projector/infi.projector-0.6.3.tar.gz/infi.projector-0.6.3/src/infi/projector/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.6.3"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.6.3)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '075bff3aafafbc8ba42fa46b525d58c5c11eb081'
+__git_head_subject__ = 'HOSTDEV-1099 adding prefer-final = false to buildout.in'
+__git_head_message__ = '================================================================================\n\nHost Infrastructure / HOSTDEV-1099\nadd prefer-final to buildout.cfg\n\nType:              Task                              Status:        Open                       Assignee: Guy Rozendorn\nPriority:          Minor                             Resolution:    Unresolved                 Reporter: Guy Rozendorn\nAffects Version/s:                                   Fix Version/s: 0.30\nComponents: packaging                                                                          Created: 2013-07-10 15:46\nLabels:                                                                                        Updated: 2013-07-10 15:46\n\nIssue Links:\n\nSub-Tasks:\n\nDescription:\nNone\n\nComments:'
+__git_dirty_diff__ = ''
