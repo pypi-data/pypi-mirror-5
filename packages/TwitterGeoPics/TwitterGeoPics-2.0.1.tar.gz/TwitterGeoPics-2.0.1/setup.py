ee1195a342c545900a8a4a0de2ed767222d7bd42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:42 +0000

    Add package TwitterGeoPics/2.0.1/TwitterGeoPics-2.0.1.tar.gz

---
+++ packages/TwitterGeoPics/TwitterGeoPics-2.0.1.tar.gz/TwitterGeoPics-2.0.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='TwitterGeoPics',
+    version='2.0.1',
+    author='Jonas Geduldig',
+    author_email='boxnumber03@gmail.com',
+    packages=['TwitterGeoPics'],
+    package_data={'': []},
+    url='https://github.com/geduldig/TwitterGeoPics',
+    download_url = 'https://github.com/gedldig/TwitterGeoPics/tarball/master',
+    license='MIT',
+    keywords='twitter, geocode',
+    description='Command line scripts for geocoding tweets from twitter.com and for downloading embedded photos.',
+    install_requires = ['TwitterAPI', 'pygeocoder', 'fridge']
+)
