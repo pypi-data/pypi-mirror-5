807f7fd22b83712f95c25799cd2357ca382c4db6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:26 +0000

    Add package Wraptor/0.3.0/Wraptor-0.3.0.tar.gz

---
+++ packages/Wraptor/Wraptor-0.3.0.tar.gz/Wraptor-0.3.0/setup.py
@@ -0,0 +1,40 @@
+#!env python
+
+from distutils.core import setup
+from setuptools.command.test import test as TestCommand
+
+# get version
+with open('wraptor/__init__.py') as f:
+    exec(f.read())
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest, sys
+        errno = pytest.main(self.test_args)
+        raise sys.exit(errno)
+
+setup(
+    name='Wraptor',
+    version=__version__,
+    author='Carl Sverre',
+    author_email='carl@carlsverre.com',
+    packages=[
+        'wraptor',
+        'wraptor.test',
+        'wraptor.decorators',
+        'wraptor.decorators.test',
+        'wraptor.context',
+        'wraptor.context.test',
+    ],
+    url='http://github.com/carlsverre/wraptor',
+    license='LICENSE.txt',
+    description='Useful decorators and other utility functions.',
+    long_description=open('README.rst').read(),
+    tests_require=['pytest'],
+    cmdclass = { 'test': PyTest },
+)
