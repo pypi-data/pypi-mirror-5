f92f20b51af2015725ac71d1d94ecfc7ec69a088
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:06 +0000

    Add package list_print/1.0.1/list_print-1.0.1.tar.gz

---
+++ packages/list_print/list_print-1.0.1.tar.gz/list_print-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+name = "list_print",
+version = "1.0.1",
+py_modules = "list_print",
+author = "VenkatRayaprolu",
+author_email = "rvramanamurty@gmail.com",
+description = "Module to print the list"
+)
+
