02815dc24f78e0c3652a3d8146c743e8d1098ee9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:37 +0000

    Add package lydyTANG/1.0.0/lydyTANG-1.0.0.zip

---
+++ packages/lydyTANG/lydyTANG-1.0.0.zip/lydyTANG-1.0.0/dylb.py
@@ -0,0 +1,11 @@
+"""函数名为printlb，参数两个，BM为列表名，SJ为缩进的TAB数"""
+def printlb(bm,sj):
+    """I为循环控制参数"""
+    for i in bm:
+        if isinstance(i,list):
+            printlb(i,sj+1)
+        else:
+            for tab in range(sj):
+                print("\t",end='')
+            print(i)
+                      
