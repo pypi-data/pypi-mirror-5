c1afefd3b70a9ecb4b33015a23d125982f58c82d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:13 +0000

    Add package hypua2jamo/0.3/hypua2jamo-0.3.tar.gz

---
+++ packages/hypua2jamo/hypua2jamo-0.3.tar.gz/hypua2jamo-0.3/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+def read(filename):
+    import os.path
+    filename = os.path.join(os.path.dirname(__file__), filename)
+    f = open(filename, 'r')
+    try:
+        return f.read()
+    finally:
+        f.close()
+
+
+classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+    'Natural Language :: Korean',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 2.5',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: Implementation :: CPython',
+    'Programming Language :: Python :: Implementation :: Jython',
+    'Programming Language :: Python :: Implementation :: PyPy',
+    'Topic :: Text Processing :: Filters',
+]
+
+setup(name='hypua2jamo',
+      version='0.3',
+      license='GNU Lesser GPL v3+',
+      description='Convert Hanyang-PUA code to unicode Hangul Jamo, i.e., Syllable-Initial-Peak-Final Encoding (첫가끝 코드).',
+      long_description=read('README'),
+      author='mete0r',
+      author_email='mete0r@sarangbang.or.kr',
+      url='https://github.com/mete0r/hypua2jamo',
+      classifiers=classifiers,
+      packages=['hypua2jamo'],
+      package_dir={'': 'src'},
+      package_data={'hypua2jamo': ['*.pickle']})
