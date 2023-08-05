b3f00d61a8691b8a6800633da24e722dc5089483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:54 +0000

    Add package django-staticbuilder/0.5.0/django-staticbuilder-0.5.0.tar.gz

---
+++ packages/django-staticbuilder/django-staticbuilder-0.5.0.tar.gz/django-staticbuilder-0.5.0/setup.py
@@ -0,0 +1,50 @@
+#/usr/bin/env python
+import codecs
+import os
+from setuptools import setup, find_packages
+
+
+read = lambda filepath: codecs.open(filepath, 'r', 'utf-8').read()
+
+
+# Load package meta from the pkgmeta module without loading the package.
+pkgmeta = {}
+execfile(os.path.join(os.path.dirname(__file__), 'staticbuilder', 'pkgmeta.py'),
+         pkgmeta)
+
+
+setup(
+    name=pkgmeta['__title__'],
+    description='Optimize your static files.',
+    long_description=read(os.path.join(os.path.dirname(__file__), 'README.rst')),
+    version=pkgmeta['__version__'],
+    author=pkgmeta['__author__'],
+    author_email='m@tthewwithanm.com',
+    url='http://github.com/hzdg/django-staticbuilder',
+    download_url='http://github.com/hzdg/django-staticbuilder/tarball/master',
+    packages=find_packages(),
+    zip_safe=False,
+    keywords=['javascript', 'css', 'compressor', 'requirejs'],
+    include_package_data=True,
+    tests_require=[
+        'nose',
+        'unittest2',
+    ],
+    install_requires=[
+        'Django>=1.4',
+        'blessings==1.5',
+        'django-appconf>=0.5',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities'
+    ],
+    setup_requires=[],
+)
