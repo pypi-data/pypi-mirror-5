33d3d7c3ea779486c542e59a67730aea9bf1b6c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:12 +0000

    Add package booby/0.3.0/booby-0.3.0.tar.gz

---
+++ packages/booby/booby-0.3.0.tar.gz/booby-0.3.0/booby/fields.py
@@ -0,0 +1,148 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2013 Jaime Gil de Sagredo Luna
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""The `fields` module contains a list of `Field` classes
+for model's definition.
+
+The example below shows the most common fields and builtin validations::
+
+    class Token(Model):
+        key = StringField()
+        secret = StringField()
+
+    class User(Model):
+        login = StringField(required=True)
+        name = StringField()
+        role = StringField(choices=['admin', 'moderator', 'user'])
+        email = EmailField(required=True)
+        token = EmbeddedField(Token, required=True)
+        is_active = BooleanField(default=False)
+"""
+
+from booby import validators as builtin_validators
+
+
+class Field(object):
+    """This is the base class for all :mod:`booby.fields`. This class
+    can also be used as field in any :class:`models.Model` declaration.
+
+    :param default: This field default value. If passed a callable object then
+        uses its return value as the field's default. This is particularly
+        useful using `mutable objects <http://effbot.org/zone/default-values.htm>`_.
+
+    :param required: If `True` this field value should not be `None`.
+    :param choices: A `list` of values where this field value should be in.
+    :param \*validators: A list of field :mod:`validators` as positional arguments.
+
+    """
+
+    def __init__(self, *validators, **kwargs):
+        self.options = kwargs
+
+        self.default = kwargs.get('default')
+
+        # Setup field validators
+        self.validators = []
+
+        if kwargs.get('required'):
+            self.validators.append(builtin_validators.Required())
+
+        choices = kwargs.get('choices')
+
+        if choices:
+            self.validators.append(builtin_validators.In(choices))
+
+        self.validators.extend(validators)
+
+    def __get__(self, instance, owner):
+        if instance is not None:
+            try:
+                return instance._data[self]
+            except KeyError:
+                return instance._data.setdefault(self, self.default)
+
+        return self
+
+    def __set__(self, instance, value):
+        instance._data[self] = value
+
+    @property
+    def default(self):
+        if callable(self._default):
+            return self._default()
+        return self._default
+
+    @default.setter
+    def default(self, value):
+        self._default = value
+
+    def validate(self, value):
+        for validator in self.validators:
+            validator.validate(value)
+
+
+class StringField(Field):
+    """:class:`Field` subclass with builtin `string` validation."""
+
+    def __init__(self, *args, **kwargs):
+        super(StringField, self).__init__(builtin_validators.String(), *args, **kwargs)
+
+
+class IntegerField(Field):
+    """:class:`Field` subclass with builtin `integer` validation."""
+
+    def __init__(self, *args, **kwargs):
+        super(IntegerField, self).__init__(builtin_validators.Integer(), *args, **kwargs)
+
+
+class FloatField(Field):
+    """:class:`Field` subclass with builtin `float` validation."""
+
+    def __init__(self, *args, **kwargs):
+        super(FloatField, self).__init__(builtin_validators.Float(), *args, **kwargs)
+
+
+class BooleanField(Field):
+    """:class:`Field` subclass with builtin `bool` validation."""
+
+    def __init__(self, *args, **kwargs):
+        super(BooleanField, self).__init__(builtin_validators.Boolean(), *args, **kwargs)
+
+
+class EmbeddedField(Field):
+    """:class:`Field` subclass with builtin embedded :class:`models.Model`
+    validation.
+
+    """
+
+    def __init__(self, model, *args, **kwargs):
+        super(EmbeddedField, self).__init__(builtin_validators.Model(model),
+            *args, **kwargs)
+
+        self.model = model
+
+    def __set__(self, instance, value):
+        if isinstance(value, dict):
+            value = self.model(**value)
+
+        super(EmbeddedField, self).__set__(instance, value)
+
+
+class EmailField(Field):
+    """:class:`Field` subclass with builtin `email` validation."""
+
+    def __init__(self, *args, **kwargs):
+        super(EmailField, self).__init__(builtin_validators.Email(), *args, **kwargs)
---
+++ packages/booby/booby-0.3.0.tar.gz/booby-0.3.0/booby/models.py
@@ -0,0 +1,190 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2013 Jaime Gil de Sagredo Luna
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""The `models` module contains the `booby` highest level abstraction:
+the `Model`.
+
+To define a model you should subclass the :class:`Model` class and
+add a list of :mod:`fields` as attributes. And then you could instantiate
+your `Model` and work with these objects.
+
+Something like this::
+
+    class Repo(Model):
+         name = StringField()
+         owner = EmbeddedField(User)
+
+    booby = Repo(
+        name='Booby',
+        owner={
+            'login': 'jaimegildesagredo',
+            'name': 'Jaime Gil de Sagredo'
+        })
+
+    print booby.to_json()
+    '{"owner": {"login": "jaimegildesagredo", "name": "Jaime Gil de Sagredo"}, "name": "Booby"}'
+"""
+
+import json
+
+from booby import fields, errors
+
+
+class ModelMeta(type):
+    def __new__(cls, name, bases, attrs):
+        attrs['_fields'] = {}
+
+        for base in bases:
+            for k, v in base.__dict__.items():
+                if isinstance(v, fields.Field):
+                    attrs['_fields'][k] = v
+
+        for k, v in attrs.items():
+            if isinstance(v, fields.Field):
+                attrs['_fields'][k] = v
+
+        return super(ModelMeta, cls).__new__(cls, name, bases, attrs)
+
+
+class Model(object):
+    """The `Model` class. All Booby models should subclass this.
+
+    By default the `Model's` :func:`__init__` takes a list of keyword arguments
+    to initialize the `fields` values. If any of these keys is not a `field`
+    then raises :class:`errors.FieldError`. Of course you can overwrite the
+    `Model's` :func:`__init__` to get a custom behavior.
+
+    You can get or set Model `fields` values in two different ways: through
+    object attributes or dict-like items::
+
+        >>> booby.name is booby['name']
+        True
+        >>> booby['name'] = 'booby'
+        >>> booby['foo'] = 'bar'
+        Traceback (most recent call last):
+          File "<stdin>", line 1, in <module>
+        errors.FieldError: foo
+
+    :param \*\*kwargs: Keyword arguments with the fields values to initialize the model.
+
+    """
+
+    __metaclass__ = ModelMeta
+
+    def __new__(cls, *args, **kwargs):
+        model = super(Model, cls).__new__(cls)
+        model._data = {}
+
+        return model
+
+    def __init__(self, **kwargs):
+        self._update(kwargs)
+
+    def __raise_field_error(self, name):
+        raise errors.FieldError("'{}' model has no field '{}'".format(
+            type(self).__name__, name))
+
+    def __getitem__(self, k):
+        if k not in self._fields:
+            self.__raise_field_error(k)
+
+        return getattr(self, k)
+
+    def __setitem__(self, k, v):
+        if k not in self._fields:
+            self.__raise_field_error(k)
+
+        setattr(self, k, v)
+
+    def update(self, *args, **kwargs):
+        """This method updates the `model` fields values with the given `dict`.
+        The model can be updated passing a dict object or keyword arguments,
+        like the Python's builtin :py:func:`dict.update`.
+
+        """
+
+        self._update(dict(*args, **kwargs))
+
+    def _update(self, values):
+        for k, v in values.items():
+            self[k] = v
+
+    def validate(self):
+        """This method validates the entire `model`. That is, validates
+        all the :mod:`fields` within this model.
+
+        If some `field` validation fails, then this method raises the same
+        exception that the :func:`field.validate` method had raised.
+
+        """
+
+        for name, field in self._fields.items():
+            field.validate(getattr(self, name))
+
+    def validation_errors(self):
+        """This method returns a `dict` of field name and validation error
+        pairs if any `model` field is invalid. If there are no errors
+        then returns `None`.
+
+        """
+
+        result = {}
+
+        for name, field in self._fields.items():
+            try:
+                field.validate(getattr(self, name))
+            except errors.ValidationError as err:
+                result[name] = str(err)
+
+        if result:
+            return result
+
+    def to_dict(self):
+        """This method returns the `model` as a `dict`."""
+
+        result = {}
+        for field in self._fields:
+            value = getattr(self, field)
+
+            if isinstance(value, Model):
+                result[field] = value.to_dict()
+            elif isinstance(value, list):
+                result[field] = self._encode_list(value)
+            else:
+                result[field] = value
+        return result
+
+    def _encode_list(self, iterable):
+        result = []
+
+        for value in iterable:
+            if isinstance(value, Model):
+                value = value.to_dict()
+
+            result.append(value)
+
+        return result
+
+    def to_json(self, *args, **kwargs):
+        """This method returns the `model` as a `json string`. It receives
+        the same arguments as the builtin :py:func:`json.dump` function.
+
+        To build a json-serializable object for this `model` this method
+        uses the :func:`Model.to_dict` method.
+
+        """
+
+        return json.dumps(self.to_dict(), *args, **kwargs)
