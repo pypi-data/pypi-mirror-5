77e15bf0289e8df52970608d57a972a0d10f517e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:21 +0000

    Add package o2w_settings/0.0.1/o2w_settings-0.0.1.tar.gz

---
+++ packages/o2w_settings/o2w_settings-0.0.1.tar.gz/o2w_settings-0.0.1/o2w/settings/common.py
@@ -0,0 +1,172 @@
+#encoding:utf-8
+"""Common settings and globals."""
+from os.path import dirname, join, normpath, basename
+from helpers import gen_secret_key
+
+#from django.conf.settings import DJANGO_ROOT
+from o2w.settings import DJANGO_ROOT
+
+# Absolute filesystem path to the top-level project folder.
+SITE_ROOT = dirname(DJANGO_ROOT)
+SITE_NAME = basename(DJANGO_ROOT)
+SITE_HTDOCS = normpath(join(DJANGO_ROOT, '../htdocs'))
+
+# Absolute filesystem path to the secret file which holds this project's
+# SECRET_KEY. Will be auto-generated the first time this file is interpreted.
+SECRET_FILE = normpath(join(DJANGO_ROOT, 'settings', 'SECRET'))
+
+########## DEBUG CONFIGURATION
+# Disable debugging by default.
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+########## END DEBUG CONFIGURATION
+
+
+########## MANAGER CONFIGURATION
+# Admin and managers for this project. These people receive private site
+# alerts.
+ADMINS = (
+    ('Jose Sanchez Moreno', 'soporte@o2w.es'),
+)
+
+MANAGERS = ADMINS
+DEFAULT_FROM_EMAIL="soporte@o2w.es"
+########## END MANAGER CONFIGURATION
+
+
+########## GENERAL CONFIGURATION
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name although not all
+# choices may be available on all operating systems. On Unix systems, a value
+# of None will cause Django to use the same timezone as the operating system.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'Europe/Madrid'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html.
+LANGUAGE_CODE = 'es'
+
+# The ID, as an integer, of the current site in the django_site database table.
+# This is used so that application data can hook into specific site(s) and a
+# single database can manage content for multiple sites.
+SITE_ID = 1
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+# If you set this to False, Django will not format dates, numbers and
+# calendars according to the current locale.
+USE_L10N = True
+########## END GENERAL CONFIGURATION
+
+
+########## MEDIA CONFIGURATION
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+MEDIA_ROOT = normpath(join(SITE_HTDOCS, 'media'))
+
+# URL that handles the media served from MEDIA_ROOT.
+MEDIA_URL = '/media/'
+########## END MEDIA CONFIGURATION
+
+
+########## STATIC FILE CONFIGURATION
+# Absolute path to the directory static files should be collected to. Don't put
+# anything in this directory yourself; store your static files in apps' static/
+# subdirectories and in STATICFILES_DIRS.
+STATIC_ROOT = normpath(join(SITE_HTDOCS, 'static'))
+
+# URL prefix for static files.
+STATIC_URL = '/static/'
+
+# URL prefix for admin static files -- CSS, JavaScript and images.
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+
+########## MIDDLEWARE CONFIGURATION
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.middleware.doc.XViewMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+)
+########## END MIDDLEWARE CONFIGURATION
+
+
+########## CELERY CONFIGURATION
+#import djcelery
+#djcelery.setup_loader()
+########## END CELERY CONFIGURATION
+
+
+########## URL CONFIGURATION
+ROOT_URLCONF = '%s.urls' % SITE_NAME
+########## END URL CONFIGURATION
+
+
+########## KEY CONFIGURATION
+# Try to load the SECRET_KEY from our SECRET_FILE. If that fails, then generate
+# a random SECRET_KEY and save it into our SECRET_FILE for future loading. If
+# everything fails, then just raise an exception.
+try:
+    SECRET_KEY = open(SECRET_FILE).read().strip()
+except IOError:
+    try:
+        with open(SECRET_FILE, 'w') as f:
+            f.write(gen_secret_key(50))
+    except IOError:
+        raise Exception('Cannot open file `%s` for writing.' % SECRET_FILE)
+            ########## END KEY CONFIGURATION
+
+########## APP CONFIGURATION
+INSTALLED_APPS = (
+
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+#    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # South migration tool.
+    #	'south',
+
+    'annoying',
+    'pipeline',
+
+    'o2w.templates',
+    'o2w.core',
+    
+    'braces',
+    'django_extensions',    
+    'pure_pagination',
+
+    # Images and thumbnails
+    'easy_thumbnails',
+    'image_diet',
+
+    # Grappelli
+    'grappelli',
+    'grappelli_modeltranslation',
+    'django.contrib.admin',
+    'filebrowser',
+
+    # Translations
+    'modeltranslation',
+    'rosetta',
+
+    # Forms
+    'floppyforms',
+    
+    SITE_NAME
+)
+STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
+########## END APP CONFIGURATION
+
+gettext = lambda s: s
+MODELTRANSLATION_DEFAULT_LANGUAGE = 'es'
+MODELTRANSLATION_FALLBACK_LANGUAGES = ('es', 'en')
+ROSETTA_STORAGE_CLASS = 'rosetta.storage.SessionRosettaStorage'
---
+++ packages/o2w_settings/o2w_settings-0.0.1.tar.gz/o2w_settings-0.0.1/setup.py
@@ -0,0 +1,20 @@
+#encoding: utf-8
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='o2w_settings',
+    version='0.0.1',
+    author='José Sánchez Moreno',
+    author_email='jose@o2w.es',
+    package_data = {        
+    },
+
+    packages=find_packages(),
+    url='http://www.o2w.es/',
+    license='LICENSE.txt',
+    description=u'Helper funcions',
+    long_description=open('README.txt').read(),
+    install_requires=[
+    ],
+)
