94a9a7d3fdaca1d158ef917c200395a4ee0a756e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:05 +0000

    Add package twoline/0.6.1/twoline-0.6.1.tar.gz

---
+++ packages/twoline/twoline-0.6.1.tar.gz/twoline-0.6.1/setup.py
@@ -0,0 +1,29 @@
+import multiprocessing
+
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='twoline',
+    version='0.6.1',
+    url='http://bitbucket.org/latestrevision/twoline/',
+    description='Manager for simple character LCDs',
+    author='Adam Coddington',
+    author_email='me@adamcoddington.net',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ],
+    install_requires=required,
+    packages=find_packages(),
+    entry_points={'console_scripts': [
+        'twoline = twoline.cmdline:run_from_cmdline']},
+    test_suite='nose.collector',
+    tests_require=[
+        'nose',
+    ]
+)
---
+++ packages/twoline/twoline-0.6.1.tar.gz/twoline-0.6.1/twoline/web.py
@@ -0,0 +1,245 @@
+import logging
+
+from flask import Flask, request, jsonify
+
+
+logger = logging.getLogger(__name__)
+
+
+app = Flask(__name__)
+
+
+def pipe():
+    return app.config['PIPE']
+
+
+def send_data(msg, data=None):
+    if not data:
+        data = []
+    if not isinstance(data, (list, tuple)):
+        data = [data, ]
+    logger.debug(
+        'Sending data %s%s',
+        msg,
+        data
+    )
+    pipe().send((
+        msg, data
+    ))
+
+
+def send_and_receive(msg, data=None):
+    send_data(msg, data)
+    while not pipe().poll():
+        pass
+    t, args = pipe().recv()
+    if t == 'error':
+        logger.error('Received error response %s', args)
+        raise args[0]
+    logger.info('Received response %s', args)
+    return args
+
+
+@app.route('/', methods=['GET'])
+def index():
+    routes = {}
+    for rule in app.url_map.iter_rules():
+        if rule.endpoint in ('index', 'static'):
+            continue
+        routes[rule.endpoint] = {
+            'url': rule.rule,
+            'methods': list(rule.methods),
+        }
+    return jsonify(
+        **routes
+    )
+
+
+@app.route('/contrast/', methods=['GET', 'PUT'])
+def contrast():
+    try:
+        if request.method == 'GET':
+            response = send_and_receive(
+                'get_contrast'
+            )
+            return jsonify(
+                contrast=response[0]
+            )
+        elif request.method == 'PUT':
+            response = send_and_receive(
+                'set_contrast', request.data
+            )
+            return jsonify(
+                contrast=response[0]
+            )
+    except Exception as e:
+        response = jsonify(
+            error=str(e)
+        )
+        response.status_code = 500
+        return response
+
+
+@app.route('/brightness/', methods=['GET', 'PUT'])
+def brightness():
+    try:
+        if request.method == 'GET':
+            response = send_and_receive(
+                'get_brightness'
+            )
+            return jsonify(
+                brightness=response[0]
+            )
+        elif request.method == 'PUT':
+            response = send_and_receive(
+                'set_brightness', request.data
+            )
+            return jsonify(
+                brightness=response[0]
+            )
+    except Exception as e:
+        response = jsonify(
+            error=str(e)
+        )
+        response.status_code = 500
+        return response
+
+
+@app.route('/message/', methods=['GET', 'POST'])
+def message_list():
+    try:
+        if request.method == 'POST':
+            response = send_and_receive(
+                'post_message', request.data
+            )
+            return jsonify(
+                **response[0]
+            )
+        elif request.method == 'GET':
+            response = send_and_receive(
+                'get_messages', request.data
+            )
+            return jsonify(
+                objects=response
+            )
+    except HttpResponseBadRequest as e:
+        response = jsonify(
+            error=str(e)
+        )
+        response.status_code = 400
+        return response
+    except Exception as e:
+        response = jsonify(
+            error=str(e)
+        )
+        response.status_code = 500
+        return response
+
+
+@app.route('/flash/', methods=['GET', 'PUT', 'DELETE'])
+def flash():
+    try:
+        if request.method == 'PUT':
+            response = send_and_receive(
+                'put_flash', request.data
+            )
+            return jsonify(
+                **response[0]
+            )
+        elif request.method == 'DELETE':
+            response = send_and_receive(
+                'delete_flash'
+            )
+            return jsonify(
+                status=response[0]
+            )
+        elif request.method == 'GET':
+            response = send_and_receive(
+                'get_flash'
+            )
+            return jsonify(
+                **response[0]
+            )
+    except HttpResponseNotFound:
+        response = jsonify(
+            error='No flash message is currently set'
+        )
+        response.status_code = 404
+        return response
+    except HttpResponseBadRequest as e:
+        response = jsonify(
+            error=str(e)
+        )
+        response.status_code = 400
+        return response
+    except Exception as e:
+        import traceback
+        traceback.print_exc()
+        response = jsonify(
+            error=str(e)
+        )
+        response.status_code = 500
+        return response
+
+
+@app.route('/message/<message_id>/', methods=['GET', 'PUT', 'DELETE', 'PATCH'])
+def message(message_id):
+    try:
+        if request.method == 'GET':
+            response = send_and_receive(
+                'get_message_by_id', message_id
+            )
+            return jsonify(
+                **response[0]
+            )
+        elif request.method == 'DELETE':
+            response = send_and_receive(
+                'delete_message_by_id', message_id
+            )
+            return jsonify(
+                status=response[0]
+            )
+        elif request.method == 'PUT':
+            response = send_and_receive(
+                'put_message_by_id', [message_id, request.data, ]
+            )
+            return jsonify(
+                **response[0]
+            )
+        elif request.method == 'PATCH':
+            response = send_and_receive(
+                'patch_message_by_id', [message_id, request.data, ]
+            )
+            return jsonify(
+                **response[0]
+            )
+    except HttpResponseNotFound:
+        response = jsonify(
+            error='Message ID %s does not exist' % message_id
+        )
+        response.status_code = 404
+        return response
+    except HttpResponseBadRequest as e:
+        response = jsonify(
+            error=str(e)
+        )
+        response.status_code = 400
+        return response
+    except Exception as e:
+        response = jsonify(
+            error=str(e)
+        )
+        response.status_code = 500
+        return response
+
+
+class HttpResponse(Exception):
+    pass
+
+
+class HttpResponseNotFound(HttpResponse):
+    pass
+
+
+class HttpResponseBadRequest(HttpResponse):
+    pass
