34dd3c70bcd87d0d80929f96f7737e93e028995b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:37 +0000

    Add package decanter/0.1.0/decanter-0.1.0.tar.gz

---
+++ packages/decanter/decanter-0.1.0.tar.gz/decanter-0.1.0/decanter/app/bundles/home/controllers/index.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from lib.decorator import route
+from lib.decorator import get
+from app.common import errors
+
+
+@get('/', apply='json', skip='jinja2')
+def index():
+    return {'word': 'Hello Decanter!'}
+
+@get('/home/')
+@get('/home/<name>')
+def home(name='Decanter'):
+    return {'greeting': "Hello {0}!".format(name.lower().capitalize())}
