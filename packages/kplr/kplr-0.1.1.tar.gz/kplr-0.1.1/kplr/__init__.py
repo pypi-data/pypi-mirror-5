e5faa92cbd25a373c3ae1ceb3c8a1adc4d2a432d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:02 +0000

    Add package kplr/0.1.1/kplr-0.1.1.tar.gz

---
+++ packages/kplr/kplr-0.1.1.tar.gz/kplr-0.1.1/kplr/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from .kplr import TIME_BASELINE, EXPOSURE_TIMES
+from .data import Dataset
+from .api import API, KOI, Planet, Star, LightCurve, TargetPixelFile
+from . import ld
