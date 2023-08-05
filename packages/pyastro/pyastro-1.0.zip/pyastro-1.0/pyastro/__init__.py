4b556bcc2c4d73f9f7277a893ca567bcbefe20d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:11 +0000

    Add package pyastro/1.0/pyastro-1.0.zip

---
+++ packages/pyastro/pyastro-1.0.zip/pyastro-1.0/pyastro/__init__.py
@@ -0,0 +1,20 @@
+"""Astronomical Library.
+
+Library Release 1.0
+
+Copyright 2013 Paul Griffiths
+Email: mail@paulgriffiths.net
+
+Distributed under the terms of the GNU General Public License.
+http://www.gnu.org/licenses/
+
+"""
+
+from pyastro.functions import julian_date, UTC, kepler
+from pyastro.functions import zodiac_sign, rasc_to_zodiac
+from pyastro.functions import rasc_string, decl_string, rec_to_sph
+from pyastro.functions import J2000, SphCoords, RectCoords
+
+from pyastro.core import Mercury, Venus, Earth, Mars, Jupiter, Saturn
+from pyastro.core import Uranus, Neptune, Pluto, Sun, Moon
+from pyastro.core import positions
