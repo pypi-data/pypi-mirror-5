7b7909d8f956373dd20dbd3e1e742bfd83ef7c16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:30 +0000

    Add package pillowtop/0.1.0/pillowtop-0.1.0.tar.gz

---
+++ packages/pillowtop/pillowtop-0.1.0.tar.gz/pillowtop-0.1.0/pillowtop/management/commands/run_ptop.py
@@ -0,0 +1,20 @@
+from optparse import make_option
+from django.core.management.base import NoArgsCommand
+
+from restkit.conn import Connection
+from socketpool.pool import ConnectionPool
+from pillowtop.run_pillowtop import start_pillows
+
+class Command(NoArgsCommand):
+    help = "Run the pillowtop management command to scan all _changes feeds"
+    option_list = NoArgsCommand.option_list + (
+    )
+    def handle_noargs(self, **options):
+        import logging
+        requests_log = logging.getLogger("requests")
+        requests_log.setLevel(logging.WARNING)
+        start_pillows()
+
+
+
+
---
+++ packages/pillowtop/pillowtop-0.1.0.tar.gz/pillowtop-0.1.0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pillowtop',
+    version='0.1.0',
+    description='A couchdbkit changes listener for doing backend processing',
+    author='Dimagi',
+    author_email='dev@dimagi.com',
+    url='http://www.dimagi.com/',
+    packages = find_packages(exclude=['*.pyc']),
+    include_package_data = True,
+    install_requires = [
+        "restkit", 
+        "couchdbkit",
+        "simplejson",
+        "requests",
+        "gevent",
+        "greenlet",
+        "rawes"
+    ],
+)
+
