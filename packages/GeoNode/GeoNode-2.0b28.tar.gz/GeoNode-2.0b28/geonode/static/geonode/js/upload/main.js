2fdefe01ff0bc210f205f35e0a4627f93502704c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:04 +0000

    Add package GeoNode/2.0b28/GeoNode-2.0b28.tar.gz

---
+++ packages/GeoNode/GeoNode-2.0b28.tar.gz/GeoNode-2.0b28/geonode/static/geonode/js/upload/main.js
@@ -0,0 +1,26 @@
+/*globals define: true, requirejs: true */
+
+requirejs.config({
+    baseUrl: '/static/lib/js',
+    shim: {
+        'underscore': { exports: '_'}
+    },
+    paths: {
+        'upload': '../../geonode/js/upload',
+        'templates': '../../geonode/js/templates',
+    }
+});
+
+define(['upload/upload'], function (upload) {
+    'use strict';
+
+    $(function () {
+        upload.initialize({
+            form: '#file-uploader',
+            dropZone: '#drop-zone',
+            file_queue: '#file-queue',
+            clear_button: '#clear-button',
+            upload_button: '#upload-button'
+        });
+    });
+});
