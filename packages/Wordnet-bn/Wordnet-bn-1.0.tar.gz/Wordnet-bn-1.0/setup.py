9aceec2d02fa831e71cd1676ea384a303b7a246e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:29 +0000

    Add package Wordnet-bn/1.0/Wordnet-bn-1.0.tar.gz

---
+++ packages/Wordnet-bn/Wordnet-bn-1.0.tar.gz/Wordnet-bn-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name ='Wordnet-bn',
+version ='1.0',
+description ='Bengali Wordnet',
+author ='Soumen Ganguly',
+author_email ='soumendotganguly@gmail.com',
+packages =['wordnet-bn','wordnet-bn.test'],
+)
