44aaa91a940982f9e78c20ba2b6857deb61e7574
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:32 +0000

    Add package xuru.recipe.android/0.8.2/xuru.recipe.android-0.8.2.tar.gz

---
+++ packages/xuru.recipe.android/xuru.recipe.android-0.8.2.tar.gz/xuru.recipe.android-0.8.2/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+import os
+
+name = "xuru.recipe.android"
+version = '0.8.2'
+
+
+def read(*pathnames):
+    return open(os.path.join(os.path.dirname(__file__), *pathnames)).read()
+
+
+setup(
+    name=name,
+    version=version,
+    long_description=read('README.md'),
+
+    packages=find_packages(exclude=['ez_setup']),
+    include_package_data=True,
+    zip_safe=True,
+
+    install_requires=['zc.buildout', 'setuptools', 'pexpect'],
+    entry_points={'zc.buildout': ['default=%s:Recipe' % name]},
+
+    author="Eric Plaster",
+    author_email="plaster@gmail.com",
+    description="android zc.buildout recipe",
+    license="MIT License",
+    url='https://github.com/xuru/xuru.recipe.android',
+
+    namespace_packages=['xuru', 'xuru.recipe'],
+
+    keywords='android buildout',
+    classifiers=[
+        'Framework :: Buildout',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
