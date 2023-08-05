cfd37049c41db8463cd77192392aef989bb278ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:30 +0000

    Add package yandex.translate/0.3.2/yandex.translate-0.3.2.tar.gz

---
+++ packages/yandex.translate/yandex.translate-0.3.2.tar.gz/yandex.translate-0.3.2/setup.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+
+from distutils.core import setup
+import yandex_translate
+
+setup(name='yandex.translate',
+      version=yandex_translate.__version__,
+      author="Sofia Velmer",
+      author_email="me@require.pm",
+      description='Python library for Yandex.Translate API.',
+      license = "DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE",
+      keywords = "yandex yandex-translate translate",
+      url = "https://github.com/valmet/python-yandex-translate",
+      packages=['yandex_translate'],
+      package_dir={'yandex_translate': 'yandex_translate'},
+      provides=['yandex_translate'],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Development Status :: 3 - Alpha',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.3',
+      ],
+      platforms=['All'],
+      )
