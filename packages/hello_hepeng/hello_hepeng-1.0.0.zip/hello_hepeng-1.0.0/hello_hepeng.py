ae46849dab68d6db2e9e93edeb18e19b48bcf41f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:12 +0000

    Add package hello_hepeng/1.0.0/hello_hepeng-1.0.0.zip

---
+++ packages/hello_hepeng/hello_hepeng-1.0.0.zip/hello_hepeng-1.0.0/hello_hepeng.py
@@ -0,0 +1,7 @@
+#coding:utf-8
+
+def hello():
+	print "hello"
+
+if __name__ == '__main__':
+	hello()
---
+++ packages/hello_hepeng/hello_hepeng-1.0.0.zip/hello_hepeng-1.0.0/setup.py
@@ -0,0 +1,10 @@
+
+from distutils.core import setup
+setup (
+       name = "hello_hepeng",
+       version = "1.0.0",
+       description = "Distutils hello distribution #1",
+       py_modules = ['hello_hepeng'],
+       author = 'wolf_m',
+       author_email = 'wolmhp@gmail.com',
+      )
