3ecc17758929416041a6c3bf85339c5a5d3a3912
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:48 +0000

    Add package django-lime/0.2/django-lime-0.2.tar.gz

---
+++ packages/django-lime/django-lime-0.2.tar.gz/django-lime-0.2/src/lime/helpers.py
@@ -0,0 +1,23 @@
+__all__ = ('site')
+
+from collections import namedtuple
+
+from django.contrib.sites.models import Site
+
+SiteData = namedtuple('SiteData', 'name domain logo team')
+
+from lime.settings import SITE_NAME, SITE_DOMAIN, SITE_LOGO, SITE_TEAM
+
+site = None
+
+try:
+    current_site = Site.objects.get_current()
+    site_name = current_site.name
+    domain = current_site.domain
+    if SITE_NAME is not None:
+        site_name = SITE_NAME
+    if SITE_DOMAIN is not None:
+        domain = SITE_DOMAIN
+    site = SiteData(site_name, domain, SITE_LOGO, SITE_TEAM)
+except:
+    site = SiteData(SITE_NAME, SITE_DOMAIN, SITE_LOGO, SITE_TEAM)
---
+++ packages/django-lime/django-lime-0.2.tar.gz/django-lime-0.2/src/lime/settings.py
@@ -0,0 +1,6 @@
+from lime.conf import get_setting
+
+SITE_NAME = get_setting('SITE_NAME')
+SITE_DOMAIN = get_setting('SITE_DOMAIN')
+SITE_LOGO = get_setting('SITE_LOGO')
+SITE_TEAM = get_setting('SITE_TEAM')
