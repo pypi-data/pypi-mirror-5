61b30a21ef0d4a6f5b4fa658cd4211c6743cee8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:15 +0000

    Add package pytwain/2.0/pytwain-2.0.tar.gz

---
+++ packages/pytwain/pytwain-2.0.tar.gz/pytwain-2.0/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(name='pytwain',
+      version='2.0',
+      author='Mikhail Denisenko',
+      author_email='denisenkom@gmail.com',
+      url='https://github.com/denisenkom/pytwain',
+      package_dir={'': 'src'},
+      py_modules=['twain'],
+      description="TWAIN API for accessing scanners, cameras, etc on Windows",
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Win32 (MS Windows)',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Natural Language :: English',
+          'Operating System :: Microsoft :: Windows',
+          'Programming Language :: Python',
+          'Topic :: Multimedia :: Graphics :: Capture :: Digital Camera',
+          'Topic :: Multimedia :: Graphics :: Capture :: Scanners',
+          ],
+      )
