2172271602096a6f775a9bad00c2d69e4013375a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:27 +0000

    Add package githubmirror/0.1/githubmirror-0.1.tar.gz

---
+++ packages/githubmirror/githubmirror-0.1.tar.gz/githubmirror-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='githubmirror',
+    version='0.1',
+    packages=['githubmirror', ],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README').read(),
+    install_requires=open('requirements.txt').read(),
+    entry_points={
+        'console_scripts': ['github-mirror = githubmirror:cmd'],
+    }
+)
