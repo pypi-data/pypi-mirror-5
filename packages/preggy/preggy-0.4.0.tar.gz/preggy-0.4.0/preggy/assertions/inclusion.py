7247e80f5063b4e1f8185be7af77d224de86ab8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:41 +0000

    Add package preggy/0.4.0/preggy-0.4.0.tar.gz

---
+++ packages/preggy/preggy-0.4.0.tar.gz/preggy-0.4.0/preggy/assertions/inclusion.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+'''preggy inclusion assertion.  For use with `expect()` (see `preggy.core`).
+'''
+
+
+# preggy assertions
+# https://github.com/heynemann/preggy
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/mit-license
+# Copyright (c) 2013 Bernardo Heynemann heynemann@gmail.com
+
+from preggy import create_assertions
+
+
+@create_assertions
+def to_include(topic, expected):
+    '''Asserts that `expected` is in `topic`.'''
+    return expected in topic
