616e82d46e2da90d3d9e5b30d066cf4fc9078cd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:06 +0000

    Add package django-rss-widget/0.3/django-rss-widget-0.3.tar.gz

---
+++ packages/django-rss-widget/django-rss-widget-0.3.tar.gz/django-rss-widget-0.3/rss_widget/templatetags/rss_widget.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.template import loader, Context, Library
+import feedparser
+
+register = Library()
+
+@register.simple_tag
+def rss_widget(template_name='', feed_url=''):
+    template_name = template_name or settings['RSS_WIDGET']['template_name']
+    feed_url = feed_url or settings.RSS_WIDGET['feed_url']
+    feed = feedparser.parse(feed_url)
+    template = loader.get_template(template_name)
+    return template.render(Context({'feed': feed}))
