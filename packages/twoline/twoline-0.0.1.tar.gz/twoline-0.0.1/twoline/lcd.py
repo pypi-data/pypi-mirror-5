d3f360b074f5839898176876c9d9da4fce9f6180
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:01 +0000

    Add package twoline/0.0.1/twoline-0.0.1.tar.gz

---
+++ packages/twoline/twoline-0.0.1.tar.gz/twoline-0.0.1/twoline/lcd.py
@@ -0,0 +1,190 @@
+from functools import wraps
+import logging
+import time
+
+
+logger = logging.getLogger(__name__)
+
+
+COMMANDS = {}
+
+
+def command(fn):
+    @wraps(fn)
+    def wrapped(*args):
+        self = args[0]
+        logger.debug(
+            'Executing %s%s',
+            fn.func_name,
+            args
+        )
+        response = fn(*args)
+        logger.debug(
+            'Response %s',
+            response
+        )
+        if response:
+            self.send_manager_data('response', response)
+    COMMANDS[fn.func_name] = wrapped
+    return fn
+
+
+class LcdManager(object):
+    def __init__(self, device_path, pipe, size=None,
+            blink_interval=0.25, text_cycle_interval=2, size_x=16, size_y=2):
+        self.device = open(device_path, 'wb')
+        self.pipe = pipe
+        if not size:
+            size = [16, 2]
+        self.size = size
+
+        self.message = ''
+        self.message_lines = []
+        self.color = 0, 0, 0
+        self.backlight = True
+
+        self.sleep = 0.1
+
+        self.blink = []
+        self.blink_idx = 0
+        self.blink_counter = 0
+        self.blink_interval = int(
+            (1.0 / self.sleep) * blink_interval
+        )
+
+        self.text_idx = 0
+        self.text_cycle_counter = 0
+        self.text_cycle_interval = int(
+            (1.0 / self.sleep) * text_cycle_interval
+        )
+
+        self.clear()
+
+    def run(self):
+        while True:
+            if self.pipe.poll():
+                cmd, args = self.pipe.recv()
+                args.insert(0, self)
+                if cmd in COMMANDS:
+                    COMMANDS[cmd](*args)
+                else:
+                    logger.error(
+                        'Received unknown command \'%s\' from manager.',
+                        cmd
+                    )
+                    self.send_manager_data(
+                        'error', 'Command %s does not exist' % cmd
+                    )
+            if self.blink_counter >= self.blink_interval:
+                self.blink_counter = 0
+                self.handle_blink()
+            else:
+                self.blink_counter += 1
+            if self.text_cycle_counter >= self.text_cycle_interval:
+                self.text_cycle_counter = 0
+                self.handle_text_cycle()
+            else:
+                self.text_cycle_counter += 1
+            time.sleep(self.sleep)
+
+    def handle_text_cycle(self):
+        if len(self.message_lines) <= self.text_idx:
+            self.text_idx = 0
+        self.send('\xfe\x58')
+        cleaned_lines = [
+            line.ljust(self.size[0])
+            for line in self.message_lines[
+                self.text_idx:self.text_idx+self.size[1]
+            ]
+        ]
+        display_text = ''.join(cleaned_lines)
+        self.send(display_text.encode('ascii', 'replace'))
+        self.text_idx += 2
+
+    def handle_blink(self):
+        if not self.blink:
+            return
+        self.blink_idx += 1
+        if len(self.blink) <= self.blink_idx:
+            self.blink_idx = 0
+        self.set_backlight_color(self.blink[self.blink_idx])
+
+    def send_manager_data(self, msg, data=None):
+        if not data:
+            data = []
+        if not isinstance(data, (list, tuple)):
+            data = [data, ]
+        self.pipe.send((
+            msg, data
+        ))
+
+    def send(self, cmd):
+        self.device.write(cmd + '\n')
+
+    @command
+    def message(self, message):
+        if 'message' in message and self.message != message['message']:
+            self.set_message(message['message'])
+        if 'blink' in message and self.blink != message['blink']:
+            self.set_blink(message['blink'])
+        if not 'blink' in message:
+            self.set_blink([])
+        if not self.blink and message['color'] != self.color:
+            self.set_backlight_color(message['color'])
+        if message['backlight'] != self.backlight:
+            if message['backlight']:
+                self.on()
+            else:
+                self.off()
+
+    def get_message_lines(self, message):
+        lines = []
+        original_lines = message.split('\r')
+        for line in original_lines:
+            lines.extend(
+                line[i:i+self.size[0]]
+                for i in range(0, len(line), self.size[0])
+            )
+        logger.debug(lines)
+        return lines
+
+    @command
+    def set_blink(self, colors):
+        self.blink = colors
+        self.blink_idx = 0
+        if self.blink:
+            self.set_backlight_color(self.blink[self.blink_idx])
+
+    @command
+    def set_message(self, message):
+        logger.info('Setting message \'%s\'', message)
+        self.clear()
+        self.text_idx = 0
+        self.message = message.replace('\n', '')
+        self.message_lines = self.get_message_lines(self.message)
+        self.handle_text_cycle()
+
+    @command
+    def off(self, *args):
+        logger.info('Backlight Off')
+        self.backlight = False
+        self.send('\xfe\x46')
+
+    @command
+    def on(self, *args):
+        logger.info('Backlight On')
+        self.backlight = True
+        self.send('\xfe\x42')
+
+    @command
+    def clear(self, *args):
+        self.message = ''
+        self.text_idx = 0
+        self.message_lines = []
+        self.send('\xfe\x58')
+
+    @command
+    def set_backlight_color(self, color):
+        logger.info('Setting backlight to %s', color)
+        self.color = color
+        self.send('\xfe\xd0%s%s%s' % tuple([chr(c) for c in color]))
---
+++ packages/twoline/twoline-0.0.1.tar.gz/twoline-0.0.1/twoline/web.py
@@ -0,0 +1,129 @@
+import logging
+
+from flask import Flask, request, jsonify, abort
+
+
+logger = logging.getLogger(__name__)
+
+
+app = Flask(__name__)
+
+
+def pipe():
+    return app.config['PIPE']
+
+
+def send_data(msg, data=None):
+    if not data:
+        data = []
+    if not isinstance(data, (list, tuple)):
+        data = [data, ]
+    logger.debug(
+        'Sending data %s%s',
+        msg,
+        data
+    )
+    pipe().send((
+        msg, data
+    ))
+
+
+def send_and_receive(msg, data=None):
+    send_data(msg, data)
+    while not pipe().poll():
+        pass
+    t, args = pipe().recv()
+    if t == 'error':
+        logger.error('Received error response %s', args)
+        raise args[0]
+    logger.info('Received response %s', args)
+    return args
+
+
+@app.route('/message/', methods=['GET', 'POST'])
+def message_list():
+    if request.method == 'POST':
+        response = send_and_receive(
+            'post_message', request.data
+        )
+        return jsonify(
+            id=response[0]
+        )
+    elif request.method == 'GET':
+        response = send_and_receive(
+            'get_messages', request.data
+        )
+        return jsonify(
+            objects=response
+        )
+
+
+@app.route('/flash/', methods=['GET', 'PUT', 'DELETE'])
+def flash():
+    try:
+        if request.method == 'PUT':
+            response = send_and_receive(
+                'put_flash', request.data
+            )
+            return jsonify(
+                **response[0]
+            )
+        elif request.method == 'DELETE':
+            response = send_and_receive(
+                'delete_flash'
+            )
+            return jsonify(
+                status=response[0]
+            )
+        elif request.method == 'GET':
+            response = send_and_receive(
+                'get_flash'
+            )
+            return jsonify(
+                **response[0]
+            )
+    except HttpResponseNotFound:
+        abort(404)
+
+
+@app.route('/message/<message_id>/', methods=['GET', 'PUT', 'DELETE', 'PATCH'])
+def message(message_id):
+    try:
+        if request.method == 'GET':
+            response = send_and_receive(
+                'get_message_by_id', message_id
+            )
+            return jsonify(
+                **response[0]
+            )
+        elif request.method == 'DELETE':
+            response = send_and_receive(
+                'delete_message_by_id', message_id
+            )
+            return jsonify(
+                status=response[0]
+            )
+        elif request.method == 'PUT':
+            response = send_and_receive(
+                'put_message_by_id', [message_id, request.data, ]
+            )
+            return jsonify(
+                **response[0]
+            )
+        elif request.method == 'PATCH':
+            response = send_and_receive(
+                'patch_message_by_id', [message_id, request.data, ]
+            )
+            return jsonify(
+                **response[0]
+            )
+    except HttpResponseNotFound:
+        abort(404)
+
+
+class HttpResponse(Exception):
+    pass
+
+
+class HttpResponseNotFound(HttpResponse):
+    pass
