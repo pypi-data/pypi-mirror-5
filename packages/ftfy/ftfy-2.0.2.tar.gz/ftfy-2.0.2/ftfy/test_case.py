d2b23727393990607baa0d028d5302feb818ad89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:27 +0000

    Add package ftfy/2.0.2/ftfy-2.0.2.tar.gz

---
+++ packages/ftfy/ftfy-2.0.2.tar.gz/ftfy-2.0.2/ftfy/test_case.py
@@ -0,0 +1,4 @@
+from ftfy import fix_text
+thetext = open('vb.is.txt').read().decode('latin-1')                                                                                                                                                                           
+fix_text(thetext)
+
