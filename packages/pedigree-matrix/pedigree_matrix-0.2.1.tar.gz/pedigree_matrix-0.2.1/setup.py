2738a41094ec44bddf299108095773acdb297952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:06 +0000

    Add package pedigree-matrix/0.2.1/pedigree_matrix-0.2.1.tar.gz

---
+++ packages/pedigree-matrix/pedigree_matrix-0.2.1.tar.gz/pedigree_matrix-0.2.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+  name='pedigree_matrix',
+  version="0.2.1",
+  packages=["pedigree_matrix"],
+  author="Chris Mutel",
+  author_email="cmutel@gmail.com",
+  license=open('LICENSE.txt').read(),
+  long_description=open('README.txt').read(),
+)
