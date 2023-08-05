f731faca9ba17a8d5c2e9dd9133eb6f9897c3297
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:08 +0000

    Add package devflow/0.11/devflow-0.11.tar.gz

---
+++ packages/devflow/devflow-0.11.tar.gz/devflow-0.11/devflow/version.py
@@ -0,0 +1,7 @@
+__version__ = "0.11"
+__version_info__ = ['0', '11']
+__version_vcs_info__ = {'branch': 'master',
+ 'revid': '0384d20',
+ 'revno': 126,
+ 'toplevel': '/home/cstavr/okeanos/devflow'}
+__version_user_info__ = "cstavr@okeanosdev-91.admin.grnet.gr"
