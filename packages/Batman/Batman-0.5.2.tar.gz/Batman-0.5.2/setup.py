c34716198e7b62bed985f8725590bd9c1babb847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:35 +0000

    Add package Batman/0.5.2/Batman-0.5.2.tar.gz

---
+++ packages/Batman/Batman-0.5.2.tar.gz/Batman-0.5.2/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='Batman',
+    version='0.5.2',
+    description='A deployment toolbelt',
+    author='Kevin McCarthy',
+    author_email='me@kevinmccarthy.org',
+    url='https://github.com/realgeeks/batman',
+    packages=find_packages(),
+    license='MIT',
+    install_requires=['PyYAML==3.10', 'argparse==1.2.1', 'virtualenvwrapper==4.0'],
+    entry_points={
+        'console_scripts': [
+            'batman = batman.main:batmain',
+        ]
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Topic :: System :: Installation/Setup',
+        'Topic :: Software Development :: Version Control',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
