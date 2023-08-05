6dfcf3450847906c9c6d2b90b73858691234151b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:00 +0000

    Add package django-ecstatic/0.1/django-ecstatic-0.1.tar.gz

---
+++ packages/django-ecstatic/django-ecstatic-0.1.tar.gz/django-ecstatic-0.1/setup.py
@@ -0,0 +1,46 @@
+#/usr/bin/env python
+import codecs
+import os
+from setuptools import setup, find_packages
+
+
+read = lambda filepath: codecs.open(filepath, 'r', 'utf-8').read()
+
+
+# Load package meta from the pkgmeta module without loading the package.
+pkgmeta = {}
+execfile(os.path.join(os.path.dirname(__file__), 'ecstatic', 'pkgmeta.py'),
+         pkgmeta)
+
+
+setup(
+    name=pkgmeta['__title__'],
+    version=pkgmeta['__version__'],
+    description='An expansion pack for django.contrib.staticfiles!',
+    long_description=read(os.path.join(os.path.dirname(__file__), 'README.rst')),
+    author=pkgmeta['__author__'],
+    author_email='m@tthewwithanm.com',
+    url='http://github.com/hzdg/django-ecstatic',
+    download_url='http://github.com/hzdg/django-ecstatic/tarball/master',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    tests_require=[
+    ],
+    install_requires=[
+        'Django>=1.4',
+        'django-appconf>=0.5',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities'
+    ],
+)
