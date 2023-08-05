25751a5d18c99b5db6718030bde53baf923c694e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:32 +0000

    Add package python-logging-extra/0.20130515/python-logging-extra-0.20130515.tar.gz

---
+++ packages/python-logging-extra/python-logging-extra-0.20130515.tar.gz/python-logging-extra-0.20130515/setup.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name = 'python-logging-extra',
+    version = '0.20130515',
+    packages=['loggingx'],
+    # Metadata
+    author = 'ARCO Research Group',
+    author_email = 'sysadm(at)arco.esi.uclm.es',
+    description = 'Generic utilities for the Python loggin facility',
+    license = 'GPLv3',
+    url = ' http://arco.esi.uclm.es'
+)
