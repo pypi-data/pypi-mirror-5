aeb7d89cfcdaaf7bad5f7523f75ff54c186cbd0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:12 +0000

    Add package osvalidate/2013-05-23.01/osvalidate-2013-05-23.01.tar.gz

---
+++ packages/osvalidate/osvalidate-2013-05-23.01.tar.gz/osvalidate-2013-05-23.01/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='osvalidate',
+    version='2013-05-23.01',
+    description='OpenSpending Model/Data Validation',
+    author='Open Knowledge Foundation',
+    author_email='openspending-dev at lists okfn org',
+    url='http://github.com/okfn/osvalidate',
+
+    install_requires=[
+        "iso8601",
+        "colander==0.9.3",
+        "Unidecode==0.04.9"
+    ],
+    setup_requires=[
+        "nose==1.1.2"
+    ],
+    license='GPLv3',
+    packages=find_packages(
+        exclude=['ez_setup', 'openspending.validationtest']),
+    include_package_data=True,
+    package_data={
+        },
+    namespace_packages=['openspending', 'openspending.validation'],
+    test_suite='nose.collector',
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'osvalidate = openspending.validationcli:main'
+        ]
+    }
+)
