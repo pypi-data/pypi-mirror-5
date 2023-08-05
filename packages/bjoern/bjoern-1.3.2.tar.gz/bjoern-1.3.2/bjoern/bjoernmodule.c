2cb273a4ea676f92941fffa964ab16efcba6f9a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:29 +0000

    Add package bjoern/1.3.2/bjoern-1.3.2.tar.gz

---
+++ packages/bjoern/bjoern-1.3.2.tar.gz/bjoern-1.3.2/bjoern/bjoernmodule.c
@@ -0,0 +1,105 @@
+#include <Python.h>
+#include "server.h"
+#include "wsgi.h"
+#include "bjoernmodule.h"
+#include "filewrapper.h"
+
+
+PyDoc_STRVAR(listen_doc,
+"listen(application, host, port) -> None\n\n \
+\
+Makes bjoern listen to host:port and use application as WSGI callback. \
+(This does not run the server mainloop.)");
+static PyObject*
+listen(PyObject* self, PyObject* args)
+{
+  const char* host;
+  int port = 0;
+
+  if(wsgi_app) {
+    PyErr_SetString(
+      PyExc_RuntimeError,
+      "Only one bjoern server per Python interpreter is allowed"
+    );
+    return NULL;
+  }
+
+  if(!PyArg_ParseTuple(args, "Os|i:run/listen", &wsgi_app, &host, &port))
+    return NULL;
+
+  _initialize_request_module(host, port);
+
+  if(!port) {
+    /* Unix socket: "unix:/tmp/foo.sock" or "unix:@abstract-socket" (Linux) */
+    if(strncmp("unix:", host, 5)) {
+      PyErr_Format(PyExc_ValueError, "'port' missing but 'host' is not a Unix socket");
+      goto err;
+    }
+    host += 5;
+  }
+
+  if(!server_init(host, port)) {
+    if(port)
+      PyErr_Format(PyExc_RuntimeError, "Could not start server on %s:%d", host, port);
+    else
+      PyErr_Format(PyExc_RuntimeError, "Could not start server on %s", host);
+    goto err;
+  }
+
+  Py_RETURN_NONE;
+
+err:
+  wsgi_app = NULL;
+  return NULL;
+}
+
+PyDoc_STRVAR(run_doc,
+"run(application, host, port) -> None\n \
+Calls listen(application, host, port) and starts the server mainloop.\n \
+\n\
+run() -> None\n \
+Starts the server mainloop. listen(...) has to be called before calling \
+run() without arguments.");
+static PyObject*
+run(PyObject* self, PyObject* args)
+{
+  if(PyTuple_GET_SIZE(args) == 0) {
+    /* bjoern.run() */
+    if(!wsgi_app) {
+      PyErr_SetString(
+        PyExc_RuntimeError,
+        "Must call bjoern.listen(app, host, port) before "
+        "calling bjoern.run() without arguments."
+      );
+      return NULL;
+    }
+  } else {
+    /* bjoern.run(app, host, port) */
+    if(!listen(self, args))
+      return NULL;
+  }
+
+  server_run();
+  wsgi_app = NULL;
+  Py_RETURN_NONE;
+}
+
+static PyMethodDef Bjoern_FunctionTable[] = {
+  {"run", run, METH_VARARGS, run_doc},
+  {"listen", listen, METH_VARARGS, listen_doc},
+  {NULL, NULL, 0, NULL}
+};
+
+PyMODINIT_FUNC initbjoern(void)
+{
+  _init_common();
+  _init_filewrapper();
+
+  PyType_Ready(&FileWrapper_Type);
+  assert(FileWrapper_Type.tp_flags & Py_TPFLAGS_READY);
+  PyType_Ready(&StartResponse_Type);
+  assert(StartResponse_Type.tp_flags & Py_TPFLAGS_READY);
+
+  PyObject* bjoern_module = Py_InitModule("bjoern", Bjoern_FunctionTable);
+  PyModule_AddObject(bjoern_module, "version", Py_BuildValue("(iii)", 1, 3, 2));
+}
