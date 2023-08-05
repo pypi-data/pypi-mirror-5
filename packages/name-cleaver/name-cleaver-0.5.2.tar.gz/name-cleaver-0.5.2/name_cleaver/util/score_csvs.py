22230434600fd0bd798b66d90f05ab7ad4b9e431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:17 +0000

    Add package name-cleaver/0.5.2/name-cleaver-0.5.2.tar.gz

---
+++ packages/name-cleaver/name-cleaver-0.5.2.tar.gz/name-cleaver-0.5.2/name_cleaver/util/score_csvs.py
@@ -0,0 +1,11 @@
+from name_cleaver.cleaver import PoliticianNameCleaver
+
+# parse options
+# --file1 --file2
+# --col1 --col2
+# get two filenames/CSVs from options
+# get name of 
+
+
+# open CSVs
+# cle
