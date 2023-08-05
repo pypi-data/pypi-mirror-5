3d9f41e9878e378dc9b94b4cf066b53e4beb9529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:55 +0000

    Add package jmrua/1.1.0/jmrua-1.1.0.tar.gz

---
+++ packages/jmrua/jmrua-1.1.0.tar.gz/jmrua-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'jmrua',
+    version = '1.1.0',
+    py_modules = ['jmrua'],
+    author = 'Juan..Miguel.Ra.Gutirrez',
+    author_email = 'juanmiguel.rua.dev@gmail.com',
+    url = 'http://about.me/juanmiguelrua',
+    description = "ENHANCEMENT: Detect which is the current OS and set the default workspace",
+    )
