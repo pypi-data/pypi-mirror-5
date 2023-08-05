f9e7f6836c327a220d99fb0c3c925f70e3b0d3c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:22 +0000

    Add package o2w_web/0.0.5/o2w_web-0.0.5.tar.gz

---
+++ packages/o2w_web/o2w_web-0.0.5.tar.gz/o2w_web-0.0.5/setup.py
@@ -0,0 +1,36 @@
+#encoding: utf-8
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='o2w_web',
+    version='0.0.5',
+    author='José Sánchez Moreno',
+    author_email='jose@o2w.es',
+    packages=find_packages(),
+    url='http://www.o2w.es/',
+    license='LICENSE.txt',
+    description=u'Base package for web creation',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        'django',
+        'django-annoying',
+        'django-pipeline',
+        'cssmin',
+        'slimit',
+        'django-modeltranslation',
+        'easy-thumbnails',
+        'o2w-image-diet',
+        'django-extensions',
+        'django-grappelli',
+        'django-rosetta',
+        'django-filebrowser',
+        'grappelli-modeltranslation',
+        'django-pure-pagination',
+        'raven',
+        'o2w-cache-invalidator',
+        'o2w-templates',
+        'o2w-core',
+        'o2w-settings'
+    ],
+)
