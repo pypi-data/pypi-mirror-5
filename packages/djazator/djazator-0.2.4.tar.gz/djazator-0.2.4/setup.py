057aa971948bb0aee006b0996bb137676a399d32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:57 +0000

    Add package djazator/0.2.4/djazator-0.2.4.tar.gz

---
+++ packages/djazator/djazator-0.2.4.tar.gz/djazator-0.2.4/setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+description = ("A simple django plugin for sending push notifications"
+               " from django server to sockjs clients")
+
+setup(
+    name = "djazator",
+    version = "0.2.4",
+    author = "Mike Oskin",
+    author_email = "cheap.grayhat@gmail.com",
+    description = description,
+    license = "MIT",
+    keywords = "django zeromq tornado sockjs",
+    packages = find_packages('src'),
+    package_dir = {'':'src'},
+    include_package_data = True,
+    package_data = {
+        # If any package contains *.txt or *.rst files, include them:
+        '': ['*.txt', '*.rst', '*.md'],
+        },
+    entry_points={'console_scripts': [
+                     'djazator-server = djazator.server:main',
+                     'djazator-mq = djazator.mq:main',
+                 ],
+    },
+    install_requires=['pyzmq==2.2.0.1'],
+    long_description=read('README.rst'),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Framework :: Django"
+    ],
+)
---
+++ packages/djazator/djazator-0.2.4.tar.gz/djazator-0.2.4/src/djazator/utils.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from django.utils import importlib
+
+
+def _dumb(user):
+    return user.id or "''"
+
+try:
+    path = settings.DJAZATOR_TOKENIZER
+    module_name, sep, func_name = path.rpartition('.')
+    module = importlib.import_module(module_name)
+    func = getattr(module, func_name)
+    tokenize = func if callable(func) else _dumb
+except:
+    tokenize = _dumb
