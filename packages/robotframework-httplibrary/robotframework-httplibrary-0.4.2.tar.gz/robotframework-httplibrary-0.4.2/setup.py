853232ae75860cc9f8d3aba02f600dce670ffd71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:41 +0000

    Add package robotframework-httplibrary/0.4.2/robotframework-httplibrary-0.4.2.tar.gz

---
+++ packages/robotframework-httplibrary/robotframework-httplibrary-0.4.2.tar.gz/robotframework-httplibrary-0.4.2/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+CLASSIFIERS = """
+Programming Language :: Python
+Topic :: Software Development :: Testing
+"""[1:-1]
+
+from os.path import join, dirname
+long_description = open(join(dirname(__file__), 'README.rst',)).read()
+
+setup(
+    name='robotframework-httplibrary',
+    version="0.4.2",
+    description='Robot Framework keywords for HTTP requests',
+    long_description=long_description,
+    author='Filip Noetzel',
+    author_email='filip+rfhttplibrary@j03.de',
+    url='https://github.com/peritus/robotframework-httplibrary',
+    license='Beerware',
+    keywords='robotframework testing testautomation web http webtest',
+    platforms='any',
+    zip_safe=False,
+    classifiers=CLASSIFIERS.splitlines(),
+    package_dir={'': 'src'},
+    install_requires=['robotframework', 'webtest>=2.0', 'jsonpatch',
+                      'jsonpointer'],
+    packages=['HttpLibrary']
+)
