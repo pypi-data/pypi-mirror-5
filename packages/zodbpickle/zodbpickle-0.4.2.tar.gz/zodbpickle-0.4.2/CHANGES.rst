96a9e94ec73630b514f7defdbb5e11ec85df9b51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:03 +0000

    Add package zodbpickle/0.4.2/zodbpickle-0.4.2.tar.gz

---
+++ packages/zodbpickle/zodbpickle-0.4.2.tar.gz/zodbpickle-0.4.2/CHANGES.rst
@@ -0,0 +1,70 @@
+``zodbpickle`` Changelog
+========================
+
+0.4.2 (2013-06-07)
+------------------
+
+- Fix NameError in pure-Python version of ``Unpickler.noload_appends``.
+
+- Fix NameError in pure-Python version of ``Unpickler.noload_setitems``.
+
+0.4.1 (2013-04-29)
+------------------
+
+- Fix typo in Python2 version of ``zodbpickle.pickle`` module.
+
+0.4 (2013-04-28)
+----------------
+
+- Support the common pickle module interface for Python 2.6, 2.7, 3.2, and 3.3.
+
+- Split the Python implementations / tests into Python2- and Py3k-specific
+  variants.
+
+- Added a fork of the Python 2.7 ``_pickle.c``, for use under Python2.
+  The fork adds support for the Py3k ``protocol 3`` opcodes.
+
+- Added a custom ``binary`` type for use in Python2 apps.
+  Derived from ``bytes``, the ``binary`` type allows Python2 apps to pickle
+  binary data using opcodes which will cause it to be unpickled as ``bytes``
+  on Py3k.  Under Py3k, the ``binary`` type is just an alias for ``bytes``.
+
+0.3 (2013-03-18)
+----------------
+
+- Added ``noload`` code to Python 3.2 version of ``Unpickler``.  As with
+  the Python 3.3 version, this code remains untested.
+
+- Added ``bytes_as_strings`` option to the Python 3.2 version of
+  ``Pickler``, ``dump``, and ``dumps``.
+
+0.2 (2013-03-05)
+----------------
+
+- Added ``bytes_as_strings`` option to ``Pickler``, ``dump``, and ``dumps``.
+
+- Incomplete support for Python 3.2:
+
+  - Move ``_pickle.c`` -> ``_pickle_33.c``.
+
+  - Clone Python 3.2.3's ``_pickle.c`` -> ``_pickle_32.c`` and apply the
+    same patch.
+
+  - Choose between them at build time based on ``sys.version_info``.
+
+  - Disable some tests of 3.3-only features.
+
+  - Missing: implementation of ``noload()`` in ``_pickle_32.c``.
+
+  - Missing: implementation of ``bytes_as_strings=True`` in ``_pickle_32.c``.
+
+
+0.1.0 (2013-02-27)
+------------------
+
+- Initial release of Python 3.3's pickle with the patches of Python
+  `issue 6784`__ applied.
+
+.. __: http://bugs.python.org/issue6784#msg156166
+
+- Added support for ``errors="bytes"``.
---
+++ packages/zodbpickle/zodbpickle-0.4.2.tar.gz/zodbpickle-0.4.2/src/zodbpickle/tests/test_pickle_3.py
@@ -0,0 +1,183 @@
+import io
+import collections
+import unittest
+import doctest
+import sys
+
+from .pickletester_3 import _AbstractPickleTests
+from .pickletester_3 import _AbstractPickleModuleTests
+from .pickletester_3 import _AbstractPersistentPicklerTests
+from .pickletester_3 import _AbstractPicklerUnpicklerObjectTests
+from .pickletester_3 import _AbstractDispatchTableTests
+from .pickletester_3 import _BigmemPickleTests
+from .pickletester_3 import _AbstractBytestrTests
+from .pickletester_3 import _AbstractBytesFallbackTests
+from .pickletester_3 import _AbstractBytesAsStringTests
+
+from zodbpickle import pickle_3 as pickle
+
+try:
+    from zodbpickle import _pickle
+except ImportError:
+    has_c_implementation = False
+else:
+    has_c_implementation = True
+
+
+class PickleTests(_AbstractPickleModuleTests):
+    pass
+
+
+class PyPicklerBase(object):
+
+    pickler = pickle._Pickler
+    unpickler = pickle._Unpickler
+
+    def dumps(self, arg, proto=None, **kwds):
+        f = io.BytesIO()
+        p = self.pickler(f, proto, **kwds)
+        p.dump(arg)
+        f.seek(0)
+        return bytes(f.read())
+
+    def loads(self, buf, **kwds):
+        f = io.BytesIO(buf)
+        u = self.unpickler(f, **kwds)
+        return u.load()
+
+class PyPicklerTests(PyPicklerBase, _AbstractPickleTests):
+    pass
+
+class PyPicklerBytestrTests(PyPicklerBase, _AbstractBytestrTests):
+    pass
+
+class PyPicklerBytesFallbackTests(PyPicklerBase, _AbstractBytesFallbackTests):
+    pass
+
+class PyPicklerBytesAsStringTests(PyPicklerBase, _AbstractBytesAsStringTests):
+    pass
+
+class InMemoryPickleTests(_AbstractPickleTests, _BigmemPickleTests):
+
+    pickler = pickle._Pickler
+    unpickler = pickle._Unpickler
+
+    def dumps(self, arg, protocol=None):
+        return pickle.dumps(arg, protocol)
+
+    def loads(self, buf, **kwds):
+        return pickle.loads(buf, **kwds)
+
+
+class PyPersPicklerTests(_AbstractPersistentPicklerTests):
+
+    pickler = pickle._Pickler
+    unpickler = pickle._Unpickler
+
+    def dumps(self, arg, proto=None):
+        class PersPickler(self.pickler):
+            def persistent_id(subself, obj):
+                return self.persistent_id(obj)
+        f = io.BytesIO()
+        p = PersPickler(f, proto)
+        p.dump(arg)
+        f.seek(0)
+        return f.read()
+
+    def loads(self, buf, **kwds):
+        class PersUnpickler(self.unpickler):
+            def persistent_load(subself, obj):
+                return self.persistent_load(obj)
+        f = io.BytesIO(buf)
+        u = PersUnpickler(f, **kwds)
+        return u.load()
+
+
+class PyPicklerUnpicklerObjectTests(_AbstractPicklerUnpicklerObjectTests):
+
+    pickler_class = pickle._Pickler
+    unpickler_class = pickle._Unpickler
+
+
+if sys.version_info >= (3, 3):
+    class PyDispatchTableTests(_AbstractDispatchTableTests):
+        pickler_class = pickle._Pickler
+        def get_dispatch_table(self):
+            return pickle.dispatch_table.copy()
+
+
+    class PyChainDispatchTableTests(_AbstractDispatchTableTests):
+        pickler_class = pickle._Pickler
+        def get_dispatch_table(self):
+            return collections.ChainMap({}, pickle.dispatch_table)
+
+
+if has_c_implementation:
+    class CPicklerTests(PyPicklerTests):
+        pickler = _pickle.Pickler
+        unpickler = _pickle.Unpickler
+
+    class CPicklerBytestrTests(PyPicklerBytestrTests):
+        pickler = _pickle.Pickler
+        unpickler = _pickle.Unpickler
+
+    class CPicklerBytesFallbackTests(PyPicklerBytesFallbackTests):
+        pickler = _pickle.Pickler
+        unpickler = _pickle.Unpickler
+
+    class CPicklerBytesAsStringTests(PyPicklerBytesAsStringTests):
+        pickler = _pickle.Pickler
+        unpickler = _pickle.Unpickler
+
+    class CPersPicklerTests(PyPersPicklerTests):
+        pickler = _pickle.Pickler
+        unpickler = _pickle.Unpickler
+
+    class CDumpPickle_LoadPickle(PyPicklerTests):
+        pickler = _pickle.Pickler
+        unpickler = pickle._Unpickler
+
+    class DumpPickle_CLoadPickle(PyPicklerTests):
+        pickler = pickle._Pickler
+        unpickler = _pickle.Unpickler
+
+    class CPicklerUnpicklerObjectTests(_AbstractPicklerUnpicklerObjectTests):
+        pickler_class = _pickle.Pickler
+        unpickler_class = _pickle.Unpickler
+
+    if sys.version_info >= (3, 3):
+        class CDispatchTableTests(_AbstractDispatchTableTests):
+            pickler_class = pickle.Pickler
+            def get_dispatch_table(self):
+                return pickle.dispatch_table.copy()
+
+        class CChainDispatchTableTests(_AbstractDispatchTableTests):
+            pickler_class = pickle.Pickler
+            def get_dispatch_table(self):
+                return collections.ChainMap({}, pickle.dispatch_table)
+
+
+def choose_tests():
+    tests = [PickleTests, PyPicklerTests, PyPersPicklerTests,
+             PyPicklerBytestrTests, PyPicklerBytesFallbackTests,
+             PyPicklerBytesAsStringTests]
+    if sys.version_info >= (3, 3):
+        tests.extend([PyDispatchTableTests, PyChainDispatchTableTests])
+    if has_c_implementation:
+        tests.extend([CPicklerTests, CPersPicklerTests,
+                      CPicklerBytestrTests, CPicklerBytesFallbackTests,
+                      CPicklerBytesAsStringTests,
+                      CDumpPickle_LoadPickle, DumpPickle_CLoadPickle,
+                      PyPicklerUnpicklerObjectTests,
+                      CPicklerUnpicklerObjectTests,
+                      InMemoryPickleTests])
+        if sys.version_info >= (3, 3):
+            tests.extend([CDispatchTableTests, CChainDispatchTableTests])
+    return tests
+
+def test_suite():
+    return unittest.TestSuite([
+        unittest.makeSuite(t) for t in choose_tests()
+    ] + [
+        doctest.DocTestSuite(pickle),
+    ])
