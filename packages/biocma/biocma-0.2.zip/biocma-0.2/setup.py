e8243ea9c7e028f82e7dd49bf662480b0cedb977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:54 +0000

    Add package biocma/0.2/biocma-0.2.zip

---
+++ packages/biocma/biocma-0.2.zip/biocma-0.2/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+"""I/O and utilities for the Consensus Multiple Alignment (CMA) file format."""
+
+from glob import glob
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+setup(
+    name='biocma',
+    version='0.2',
+    description=__doc__,
+    author='Eric Talevich',
+    author_email='eric.talevich@gmail.com',
+    url='http://github.com/etal/biocma',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ],
+    packages=['biocma'],
+    #scripts=glob('scripts/*'),
+)
+
