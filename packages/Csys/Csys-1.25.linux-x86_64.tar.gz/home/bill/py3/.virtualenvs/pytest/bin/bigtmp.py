31a3206160c62adfa2988fa917c771beca71f8bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:01 +0000

    Add package Csys/1.25/Csys-1.25.linux-x86_64.tar.gz

---
+++ packages/Csys/Csys-1.25.linux-x86_64.tar.gz/home/bill/py3/.virtualenvs/pytest/bin/bigtmp.py
@@ -0,0 +1,9 @@
+#!/home/bill/py3/.virtualenvs/pytest/bin/python
+# EASY-INSTALL-ENTRY-SCRIPT: 'Csys==1.25','console_scripts','bigtmp.py'
+__requires__ = 'Csys==1.25'
+import sys
+from pkg_resources import load_entry_point
+
+sys.exit(
+   load_entry_point('Csys==1.25', 'console_scripts', 'bigtmp.py')()
+)
