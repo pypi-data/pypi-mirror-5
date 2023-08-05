327317ab3e3050f6290d0758153efe2a1c92a0fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:27 +0000

    Add package python-logging-proxy/1.0.1/python-logging-proxy-1.0.1.tar.gz

---
+++ packages/python-logging-proxy/python-logging-proxy-1.0.1.tar.gz/python-logging-proxy-1.0.1/src/python_logging_proxy/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+__author__ = 'Alistair Broomhead'
+__copyright__ = 'Copyright 2012, Mind Candy Ltd'
+__credits__ = ['Nadeem Douba', 'Alistair Broomhead']
+
+__license__ = 'GPL'
+__version__ = '0.1'
+__maintainer__ = 'Alistair Broomhead'
+__email__ = 'alistair.broomhead@gmail.com'
+__status__ = 'Development'
+
+__all__ = [
+    'proxy', 'logging_proxy', 'handlers'
+]
