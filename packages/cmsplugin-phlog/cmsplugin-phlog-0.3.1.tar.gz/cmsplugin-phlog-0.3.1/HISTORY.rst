ce0961484e209e58d9fd1eca9baf357b1a53b0ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:32 +0000

    Add package cmsplugin-phlog/0.3.1/cmsplugin-phlog-0.3.1.tar.gz

---
+++ packages/cmsplugin-phlog/cmsplugin-phlog-0.3.1.tar.gz/cmsplugin-phlog-0.3.1/HISTORY.rst
@@ -0,0 +1,60 @@
+0.3.1 (2013/06/26)
+------------------
+
+* Added proper ordering to gallery rendering
+
+0.3 (2013/06/07)
+----------------
+
+* Added copy relations to plugin for CMS publishing support
+* Fixed JS locations
+* Some upgrades and added some logging
+
+0.2.4.2 (2013/04/08)
+--------------------
+
+* Repackage to get static files and templates
+
+0.2.4.1 (2013/03/14)
+--------------------
+
+* Fixed an import error
+* Fixed a typo bug
+
+0.2.4 (2013/03/13)
+------------------
+
+* Updated to use static files app
+* Updated widget for new statics
+
+0.2.3 (2012/01/11)
+------------------
+
+* Added width and height attributes to Phlog Galleries
+
+0.2.2 (2011/12/01)
+------------------
+
+* Fixed a bug when a phlog photo plugin is added without a photo
+
+0.2.1 (2011/10/14)
+------------------
+
+* Fixed a bug with the rendered order of Phlog Galleries
+
+0.2 (2011/08/04)
+----------------
+
+* Deprecated 0.1 gallery
+* Added a gallery plugin that allows for other plugins to be added as items in 
+  the gallery
+* Added a Photo plugin for adding photologue photos to a page (and not the site 
+  wide photo manager)
+* Added South integration
+
+0.1 (2011/05/11)
+----------------
+
+* Ordered Photologue gallery added
+* CMS Plugin for ordered photologue gallery added
+* Admin configuration of new ordered gallery
---
+++ packages/cmsplugin-phlog/cmsplugin-phlog-0.3.1.tar.gz/cmsplugin-phlog-0.3.1/cmsplugin_phlog/cms_plugins.py
@@ -0,0 +1,116 @@
+from logging import getLogger
+from os.path import join
+
+from django.conf import settings
+from django.contrib.admin import StackedInline
+from django.utils.translation import ugettext as _
+
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+from cms.plugin_rendering import render_plugins
+
+from photologue.models import Photo
+
+from cmsplugin_phlog.forms import GalleryPluginForm
+from cmsplugin_phlog.models import PhlogGalleryPlugin, PhlogPhotoPlugin, PhlogPhoto, GalleryPlugin
+
+
+
+log = getLogger('cmsplugin_phlog.cms_plugins')
+
+class CMSPhlogPlugin(CMSPluginBase):
+    model = PhlogGalleryPlugin
+    name = _('Photologue Gallery (Deprecated)')
+    render_template = 'cms/plugins/phlog/gallery.html'
+
+    def render(self, context, instance, placeholder):
+        if instance.order == 'random':
+            images = instance.gallery.sample(limit=instance.limit,
+                                             public=instance.public)
+        elif instance.order == 'date':
+            images = instance.gallery.latest(limit=instance.limit,
+                                             public=instance.public)
+        else:
+            images = instance.gallery.in_order(public=instance.public,
+                                               limit=instance.limit)
+
+        context.update({
+            'gallery': instance.gallery,
+            'image_list': images,
+            'gallery_plugin': instance,
+            'placeholder': placeholder})
+
+        return context
+
+class PhotoAdminInline(StackedInline):
+    model = PhlogPhoto
+
+class SinglePhotoAdminInline(PhotoAdminInline):
+    max_num=1
+    extra=1
+    can_delete=False
+
+class PhlogPhotoPlugin(CMSPluginBase):
+    model = PhlogPhotoPlugin
+    name = _('Photologue Photo')
+    render_template = 'cms/plugins/phlog/photo.html'
+    inlines=[SinglePhotoAdminInline]
+    text_enabled = True
+
+    def render(self, context, instance, placeholder):
+        try:
+            photo = instance.photos.all()[0]
+        except:
+            log.exception('Unable to get a photo for a plugin')
+            photo = None
+
+        context.update({
+            'photo': photo,
+            'instance': instance})
+        return context
+
+    def icon_src(self, instance):
+        try:
+            instance.photos.all()[0].get_admin_thumbnail_url()
+        except:
+            return settings.CMS_MEDIA_URL + u"images/plugins/image.png"
+
+class PhlogGalleryPlugin(CMSPluginBase):
+    model = GalleryPlugin
+    form = GalleryPluginForm
+    name = _('Phlog Gallery')
+    render_template = 'cms/plugins/phlog/gallery.html'
+    change_form_template = 'cms/plugins/phlog/gallery_change_form.html'
+    # Need to create a form for the plugin to add a widget for child plugins
+    allow_children = True
+    child_classes = ["PhlogPhotoPlugin"]
+
+    def render(self, context, instance, placeholder):
+        context.update({
+            'placeholder': placeholder,
+            'gallery': instance})
+
+        context.update({
+            'plugins': render_plugins(
+                instance.get_children().order_by('position','id'),
+                context, placeholder)
+        })
+
+        return context
+
+    def render_change_form(self, request, context, add=False, change=False, form_url='', obj=None):
+        plugin = getattr(self, 'cms_plugin_instance', None)
+
+        if plugin:
+            context.update({
+                'base_plugin': plugin,
+                'installed_plugins': plugin_pool.get_all_plugins(plugin.placeholder.slot)
+            })
+            context['adminform'].form.initial.setdefault('plugins', plugin)
+
+        return super(PhlogGalleryPlugin, self).render_change_form(request, context, add, change, form_url, obj)
+
+
+plugin_pool.register_plugin(CMSPhlogPlugin)
+plugin_pool.register_plugin(PhlogPhotoPlugin)
+plugin_pool.register_plugin(PhlogGalleryPlugin)
