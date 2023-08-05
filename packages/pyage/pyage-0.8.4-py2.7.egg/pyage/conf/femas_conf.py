fa512699aabfced5bd9c5cb7f7b511096f6f2c8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:11 +0000

    Add package pyage/0.8.4/pyage-0.8.4-py2.7.egg

---
+++ packages/pyage/pyage-0.8.4-py2.7.egg/pyage/conf/femas_conf.py
@@ -0,0 +1,48 @@
+# coding=utf-8
+import logging
+import os
+import Pyro4
+
+from pyage.core import address
+from pyage.core.agent import    AggregateAgent, unnamed_agents
+from pyage.core.emas import EmasService
+from pyage.core.locator import  ParentLocator
+from pyage.core.migration import Pyro4Migration
+from pyage.core.statistics import SimpleStatistics
+from pyage.solutions.evolution.crossover import  AverageFloatCrossover
+from pyage.solutions.evolution.evaluation import  FloatRastriginEvaluation
+from pyage.solutions.evolution.initializer import  float_emas_initializer
+from pyage.solutions.evolution.mutation import  UniformFloatMutation
+
+
+logger = logging.getLogger(__name__)
+
+agents_count = int(os.environ['AGENTS'])
+logger.debug("AGGREGATE, %s agents", agents_count)
+agents = unnamed_agents(agents_count, AggregateAgent)
+
+step_limit = lambda: 500
+
+aggregated_agents = lambda: float_emas_initializer(10, energy=10, size=500, lowerbound=-100, upperbound=100)
+
+emas = EmasService
+
+minimal_energy = lambda: 2
+reproduction_minimum = lambda: 12
+migration_minimum = lambda: 12
+newborn_energy = lambda: 10
+
+evaluation = FloatRastriginEvaluation
+crossover = AverageFloatCrossover
+mutation = UniformFloatMutation
+
+address_provider = address.AddressProvider
+
+migration = Pyro4Migration
+locator = ParentLocator
+
+ns_hostname = lambda: os.environ['NS_HOSTNAME']
+pyro_daemon = Pyro4.Daemon()
+daemon = lambda: pyro_daemon
+
+stats = SimpleStatistics
