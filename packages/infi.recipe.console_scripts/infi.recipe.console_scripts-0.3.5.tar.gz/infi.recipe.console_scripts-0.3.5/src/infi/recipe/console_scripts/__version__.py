a76f7125674caf831b7af8122a783a735e1c0156
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:29 +0000

    Add package infi.recipe.console_scripts/0.3.5/infi.recipe.console_scripts-0.3.5.tar.gz

---
+++ packages/infi.recipe.console_scripts/infi.recipe.console_scripts-0.3.5.tar.gz/infi.recipe.console_scripts-0.3.5/src/infi/recipe/console_scripts/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.3.5"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.3.5)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '08c873431ec7edc1616abd948e09a0275adf3f3f'
+__git_head_subject__ = 'HOSTDEV-1082 fixing console-scripts after changes in distribute'
+__git_head_message__ = '================================================================================\n\nHost Infrastructure / HOSTDEV-1082\nconsole-scripts buildout recipe fails on non-windows hosts\n\nType:              Sub-task                          Status:        Open                       Assignee: Guy Rozendorn\nPriority:          Minor                             Resolution:    Unresolved                 Reporter: Guy Rozendorn\nAffects Version/s:                                   Fix Version/s: 0.30\nComponents: packaging                                                                          Created: 2013-07-06 20:04\nLabels:                                                                                        Updated: 2013-07-06 20:04\n\nIssue Links:\n\nSub-Tasks:\n\nDescription:\nNone\n\nComments:'
+__git_dirty_diff__ = ''
