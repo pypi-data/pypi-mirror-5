ad872a2ea540ee59aa436d7ed30eaaabf163e07f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:38 +0000

    Add package hubugs/0.17.4/hubugs-0.17.4.tar.gz

---
+++ packages/hubugs/hubugs-0.17.4.tar.gz/hubugs-0.17.4/hubugs/_version.py
@@ -0,0 +1,9 @@
+# This is hubugs version 0.17.4 (2013-05-22)
+# pylint: disable=C0103, C0111, C0121, W0622
+
+dotted = '0.17.4'
+libtool = '17:24'
+hex = 0x001104
+date = '2013-05-22'
+tuple = (0, 17, 4)
+web = 'hubugs/0.17.4'
