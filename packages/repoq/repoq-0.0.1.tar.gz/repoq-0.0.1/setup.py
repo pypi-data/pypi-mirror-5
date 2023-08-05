bbc31851063ce62f1f863b9c14133c650843228e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:06 +0000

    Add package repoq/0.0.1/repoq-0.0.1.tar.gz

---
+++ packages/repoq/repoq-0.0.1.tar.gz/repoq-0.0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+version = '0.0.1'
+
+
+setup(name='repoq',
+      version=version,
+      description="Collection of utilities to query a repository of python packages",
+      long_description="""\
+""",
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "Environment :: Console",
+          "Intended Audience :: Developers",
+          "Intended Audience :: System Administrators",
+          "Topic :: System :: Software Distribution",
+      ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='python repository pypi',
+      author='Bogdan Hodorog',
+      author_email='bogdan.hodorog@3pillarglobal.com',
+      url='',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          "boto",
+      ],
+      entry_points={
+          "console_scripts": [
+              "repoq = repoq.handlers:main",
+          ],
+      },
+      )
