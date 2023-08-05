634d9b16ca847256066128257c93a6ff3221f81b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:21 +0000

    Add package smoff/0.0.9/smoff-0.0.9.tar.gz

---
+++ packages/smoff/smoff-0.0.9.tar.gz/smoff-0.0.9/smoff/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python2
+SMOFF_NAMESPACE = "http://optim.al/smoff"
+
+from component import Component
+from label import Label
+from page import Page
---
+++ packages/smoff/smoff-0.0.9.tar.gz/smoff-0.0.9/smoff/markupfromfile.py
@@ -0,0 +1,36 @@
+from lxml import etree
+from StringIO import StringIO
+import pkgutil
+
+def basicMarkupFromClass(cls):
+    """
+    Load the file associated with the specific class, and return the raw element tree without processing any tags
+    """
+    markup = pkgutil.get_data(cls.__module__, "%s.html" % cls.__name__)
+    parser = etree.HTMLParser()
+    return etree.parse(StringIO(markup), parser)
+
+def markupFromClass(cls):
+    tree = basicMarkupFromClass(cls)
+    #If a component's markup has a <smoff:extend> tag that means it extends its parent's markup rather than replacing it
+    #TODO: Proper use of namespaces
+    extendElements = tree.findall('//extend', namespaces={'smoff': "http://optim.al/smoff"})
+    if(len(extendElements) > 1):
+        raise Exception("Found multiple <smoff:extend> tags in the same file")
+    elif(len(extendElements) == 1):
+        extendElement = extendElements[0]
+        parentMarkup = markupFromClass(cls.__bases__[0]) #TODO: Allow multiple inheritance
+        #Extend markup is inserted into the parent markup at the <smoff:child> tag
+        childElement = parentMarkup.find('//child')
+        childElement.getparent().replace(childElement, extendElement)
+        return parentMarkup
+    else:
+        return tree
+
+class MarkupFromFile(object):
+    """
+    Mixin for classes that expect to have an html file with them
+    (principally Page and Panel)
+    """
+    def markupFromFile(self):
+        return markupFromClass(self.__class__)
