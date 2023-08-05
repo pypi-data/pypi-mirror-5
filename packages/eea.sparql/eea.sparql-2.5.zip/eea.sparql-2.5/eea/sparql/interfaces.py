33561f36555d373d250f17d9aa2c47d002dc52aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:51 +0000

    Add package eea.sparql/2.5/eea.sparql-2.5.zip

---
+++ packages/eea.sparql/eea.sparql-2.5.zip/eea.sparql-2.5/eea/sparql/interfaces.py
@@ -0,0 +1,10 @@
+""" Sparql interfaces module
+"""
+
+from zope.interface import Interface
+
+class ISparql(Interface):
+    """ISparql"""
+
+class ISparqlBookmarksFolder(ISparql):
+    """ISparqlBookmarksFolder"""
