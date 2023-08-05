50ac21e9aacab7c056ad322c2e2fefb65b244684
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:30 +0000

    Add package pylint-mccabe/0.1.2/pylint-mccabe-0.1.2.tar.gz

---
+++ packages/pylint-mccabe/pylint-mccabe-0.1.2.tar.gz/pylint-mccabe-0.1.2/setup.py
@@ -0,0 +1,21 @@
+"""
+Package configuration file
+"""
+from setuptools import setup
+
+setup(
+    name='pylint-mccabe',
+    version='0.1.2',
+    author='Infoxchange Australia dev team',
+    author_email='devs@infoxchange.net.au',
+    py_modules=['pylint_mccabe'],
+    url='http://pypi.python.org/pypi/pylint-mccabe/',
+    license='MIT',
+    description='McCabe complexity checker as a PyLint plugin',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        'mccabe >= 0.2',
+        'pep8 >= 1.4.5',
+        'pylint >= 0.28.0',
+    ],
+)
