f0d6461f7621bb7b6e37168bb413c3c211cad8ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:44 +0000

    Add package plonesocial.buildout/0.5.0rc1/plonesocial.buildout-0.5.0rc1.zip

---
+++ packages/plonesocial.buildout/plonesocial.buildout-0.5.0rc1.zip/plonesocial.buildout-0.5.0rc1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+version = '0.5.0rc1'
+
+setup(name="plonesocial.buildout",
+      version=version,
+      author='Guido Stevens',
+      author_email='guido.stevens@cosent.net')
