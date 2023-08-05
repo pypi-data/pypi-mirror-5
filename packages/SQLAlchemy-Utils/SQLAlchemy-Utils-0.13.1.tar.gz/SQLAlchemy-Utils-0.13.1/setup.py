de1485842f04fcea4d11e17f353be55705d6bc2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:39 +0000

    Add package SQLAlchemy-Utils/0.13.1/SQLAlchemy-Utils-0.13.1.tar.gz

---
+++ packages/SQLAlchemy-Utils/SQLAlchemy-Utils-0.13.1.tar.gz/SQLAlchemy-Utils-0.13.1/setup.py
@@ -0,0 +1,55 @@
+"""
+SQLAlchemy-Utils
+----------------
+
+Various utility functions and custom data types for SQLAlchemy.
+"""
+
+from setuptools import setup, Command
+import subprocess
+
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        errno = subprocess.call(['py.test'])
+        raise SystemExit(errno)
+
+setup(
+    name='SQLAlchemy-Utils',
+    version='0.13.1',
+    url='https://github.com/kvesteri/sqlalchemy-utils',
+    license='BSD',
+    author='Konsta Vesterinen',
+    author_email='konsta@fastmonkeys.com',
+    description=(
+        'Various utility functions for SQLAlchemy.'
+    ),
+    long_description=__doc__,
+    packages=['sqlalchemy_utils'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'SQLAlchemy>=0.8.0',
+        'phonenumbers>=5.4b1',
+        'colour==0.0.2'
+    ],
+    cmdclass={'test': PyTest},
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
