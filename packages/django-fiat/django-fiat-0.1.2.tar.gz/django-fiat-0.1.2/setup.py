7cc3549b56a61e17adef48d5716715c2e358c75a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:09 +0000

    Add package django-fiat/0.1.2/django-fiat-0.1.2.tar.gz

---
+++ packages/django-fiat/django-fiat-0.1.2.tar.gz/django-fiat-0.1.2/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='django-fiat',
+    version='0.1.2',
+    author=u'Faisal Mahmud',
+    author_email='faisal@willandskill.se',
+    packages=find_packages(),
+    include_package_data=True,
+    url='http://bitbucket.org/willandskill/django-fiat',
+    license='BSD licence, see LICENCE.txt',
+    description='A simple django app that makes it easier to work with currencies and countries',
+    long_description=open('README.md').read(),
+    zip_safe=False,
+    test_suite = "testproject.runtests.runtests",
+)
