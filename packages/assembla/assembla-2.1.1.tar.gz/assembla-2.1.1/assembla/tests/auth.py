c3d3ddef60a1946bbbea449127462a3b83966056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:12 +0000

    Add package assembla/2.1.1/assembla-2.1.1.tar.gz

---
+++ packages/assembla/assembla-2.1.1.tar.gz/assembla-2.1.1/assembla/tests/auth.py
@@ -0,0 +1,4 @@
+auth = (
+    '2ba0d8611596aae7dcec', # API key, ex: `8a71541e5fb2e4741120`
+    'aa71bc60bc2ea019c488240f921d642d48b1f914' # API key secret, ex: `a260dc4448c81c907fc7c85ad09d31306c425417`
+)
