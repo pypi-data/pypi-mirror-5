93d9447452a4a27b0437448f76aedcf142a3daef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:01 +0000

    Add package plib.stdlib/0.9/plib.stdlib-0.9.tar.gz

---
+++ packages/plib.stdlib/plib.stdlib-0.9.tar.gz/plib.stdlib-0.9/plib/test/stdlib/__main__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+"""
+Module MAIN
+Sub-Package TEST.STDLIB of Package PLIB
+Copyright (C) 2008-2013 by Peter A. Donis
+
+Released under the GNU General Public License, Version 2
+See the LICENSE and README files for more information
+
+This is the test-running script for the PLIB.STDLIB test suite.
+"""
+
+
+if __name__ == '__main__':
+    from plib.test.support import run_tests
+    
+    modules_with_doctests = [
+        'plib.stdlib.classtools',
+        'plib.stdlib.coll._typed_namedtuple',
+        'plib.stdlib.decotools',
+        'plib.stdlib.iters',
+        'plib.stdlib.localize'
+    ]
+    
+    # TODO: set module_relative and verbosity from command line options
+    module_relative = False
+    verbosity = 0
+    
+    run_tests(__name__, modules_with_doctests, module_relative, verbosity)
---
+++ packages/plib.stdlib/plib.stdlib-0.9.tar.gz/plib.stdlib-0.9/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python -u
+"""
+Setup script for PLIB.STDLIB package
+Copyright (C) 2008-2013 by Peter A. Donis
+
+Released under the GNU General Public License, Version 2
+See the LICENSE and README files for more information
+"""
+
+from plib.stdlib import __version__ as version
+
+name = "plib.stdlib"
+description = "Useful packages and modules to extend the Python standard library."
+
+author = "Peter A. Donis"
+author_email = "peterdonis@alum.mit.edu"
+
+startline = 5
+endspec = "The Zen of PLIB"
+
+dev_status = "Beta"
+
+license = "GPLv2"
+
+ext_names = ['plib.stdlib.extensions._extensions']
+ext_srcdir = "src"
+
+classifiers = """
+Environment :: Console
+Environment :: MacOS X
+Environment :: Win32 (MS Windows)
+Intended Audience :: Developers
+Operating System :: MacOS :: MacOS X
+Operating System :: Microsoft :: Windows
+Operating System :: POSIX
+Operating System :: POSIX :: Linux
+Topic :: Software Development :: Libraries :: Python Modules
+"""
+
+
+if __name__ == '__main__':
+    from distutils.core import setup
+    from setuputils import setup_vars
+    setup(**setup_vars(globals()))
