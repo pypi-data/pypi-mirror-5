09b66956966b2957099d9feeca3202318ae253e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:08 +0000

    Add package Pyntifex/0.0.10/Pyntifex-0.0.10.tar.gz

---
+++ packages/Pyntifex/Pyntifex-0.0.10.tar.gz/Pyntifex-0.0.10/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python2.7
+from distutils.core import setup
+
+setup(
+ name = 'Pyntifex',
+ version = '0.0.10',
+ py_modules = ['password','formatter','solitaire','vtableau','pyrand'],
+ author = 'Daniel Dillon Duffield',
+ author_email = 'LucianSolaris@gmail.com',
+ url = 'https://www.facebook.com/LucianSolaris',
+ description = 'A little encryption keystream, password generation, and tableau lookup distribution centered around Bruce Schneier\'s Pontifex (or Solitaire) keystream algorithm.  This implementation is more flexible than others online, in that one can variate away from 52 members and use objects instead of letters.  Download this if you seek a killer password generating program or would like to use a form of Schneier\'s Pontifex/Solitaire in your program!',
+)
