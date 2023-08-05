eff29ccc9c6498040c99c56dd5b5352dccfe397e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:32 +0000

    Add package Library/0.0.0/Library-0.0.0.tar.gz

---
+++ packages/Library/Library-0.0.0.tar.gz/Library-0.0.0/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+setup(
+    name='Library',
+    version='0.0.0',
+    author='alamybs',
+    author_email='alam.ybs@gmail.com',
+    packages=['books'],
+    #scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
+    #url='http://pypi.python.org/pypi/TowelStuff/',
+    #license='LICENSE.txt',
+    description='Useful library package stuff.',
+    long_description=open('README.txt').read(),
+    # install_requires=[
+    #     "Django >= 1.1.1",
+    #     "caldav == 0.1.4",
+    # ],
+)
