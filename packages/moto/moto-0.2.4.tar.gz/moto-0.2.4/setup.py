1ca71cc7caf7d2d1cb9b36920c9fb010a09fec5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:52 +0000

    Add package moto/0.2.4/moto-0.2.4.tar.gz

---
+++ packages/moto/moto-0.2.4.tar.gz/moto-0.2.4/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='moto',
+    version='0.2.4',
+    description='A library that allows your python tests to easily'
+                ' mock out the boto library',
+    author='Steve Pulec',
+    author_email='spulec@gmail',
+    url='https://github.com/spulec/moto',
+    entry_points={
+        'console_scripts': [
+            'moto_server = moto.server:main',
+        ],
+    },
+    packages=find_packages(),
+    install_requires=[
+        "boto",
+        "flask",
+        "httpretty==0.6.0a",
+        "Jinja2",
+    ],
+    dependency_links=[
+        "https://github.com/gabrielfalcao/HTTPretty/tarball/2347df40a3a3cd00e73f0353f5ea2670ad3405c1#egg=httpretty-0.6.0a",
+    ],
+)
