ab408dcf616e3e19a085cac3b3f59e71be84118a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:29 +0000

    Add package python-propeller/0.2.2/python-propeller-0.2.2.tar.gz

---
+++ packages/python-propeller/python-propeller-0.2.2.tar.gz/python-propeller-0.2.2/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+    name='python-propeller',
+    version='0.2.2',
+    description='Pretty progress and load indicators',
+    author='Thomas Einsporn, Manuel Barkhau',
+    author_email='mb@nexttuesday.de, mb@nexttuesday.de',
+    long_description=open("README.md", 'r').read(),
+    license="BSD",
+    url="https://github.com/mbarkhau/python-propeller",
+    download_url="https://github.org/mbarkhau/python-propeller/",
+    packages=['propeller'],
+    keywords="progress loading indicator propeller",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries'
+    ],
+)
+
