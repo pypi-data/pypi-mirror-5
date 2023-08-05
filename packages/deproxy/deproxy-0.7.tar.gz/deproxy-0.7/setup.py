3aafb8a31e520e116a734f52c0dc56d4ce4247ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:04 +0000

    Add package deproxy/0.7/deproxy-0.7.tar.gz

---
+++ packages/deproxy/deproxy-0.7.tar.gz/deproxy-0.7/setup.py
@@ -0,0 +1,24 @@
+
+from distutils.core import setup
+import deproxy
+
+setup(
+    name='deproxy',
+    version=deproxy.__version__,
+    py_modules=['deproxy', ],
+    license=open('LICENSE').read(),
+    long_description=(open('README.rst').read() + '\n\n' +
+                      open('HISTORY.rst').read()),
+    author='izrik',
+    author_email='izrik@yahoo.com',
+    url='https://github.com/izrik/deproxy',
+    description='Python library for testing HTTP proxies.',
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ),
+)
