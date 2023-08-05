b958f7b9e540eaad0f04c5802f810835700e68a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:51 +0000

    Add package timestring/1.2.0/timestring-1.2.0.tar.gz

---
+++ packages/timestring/timestring-1.2.0.tar.gz/timestring-1.2.0/timestring/__init__.py
@@ -0,0 +1,19 @@
+import re
+
+version = '1.2.0'
+__version__ = '1.2.0'
+
+from Date import Date
+from Range import Range
+from timestring_re import TIMESTRING_RE
+
+
+def findall(text):
+    results = TIMESTRING_RE.findall(text)
+    dates = []
+    for date in results:
+        if re.compile('((next|last)\s(\d+|couple(\sof))\s(weeks|months|quarters|years))|(between|from)', re.I).match(date[0]):
+            dates.append((date[0], Range(date[0])))
+        else:
+            dates.append((date[0], Date(date[0])))
+    return dates
