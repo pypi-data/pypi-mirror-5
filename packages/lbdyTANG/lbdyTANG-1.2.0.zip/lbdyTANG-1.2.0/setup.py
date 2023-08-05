3e42cb8b97547324933872a422612e3ebca84b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:37 +0000

    Add package lbdyTANG/1.2.0/lbdyTANG-1.2.0.zip

---
+++ packages/lbdyTANG/lbdyTANG-1.2.0.zip/lbdyTANG-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='lbdyTANG',
+    version='1.2.0',
+    py_modules=['dylb'],
+    author='sqtangdk',
+    author_email='18951369810@189.cn',
+    url='http://www.headfirstlabs.com',
+    description='一个打印列表的测试模块',
+    )
