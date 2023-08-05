965a30bf5dae4cbbbc462ee390ab9518f78f494b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:14 +0000

    Add package django-yearlessdate/0.3-beta/django-yearlessdate-0.3-beta.tar.gz

---
+++ packages/django-yearlessdate/django-yearlessdate-0.3-beta.tar.gz/django-yearlessdate-0.3-beta/djangoyearlessdate/forms.py
@@ -0,0 +1,53 @@
+from django import forms
+from django.forms.widgets import MultiWidget, TextInput
+from django.core.validators import ValidationError
+from django.forms.widgets import Select
+
+import calendar
+from helpers import YearlessDate
+
+DAY_CHOICES =  tuple([('','---------' )] + [(i,i) for i in range(1,32)])
+MONTH_CHOICES = tuple([('','---------' )] + [(i, calendar.month_name[i]) for i in range(1,13)])
+
+    
+class YearlessDateSelect(MultiWidget):
+    def __init__(self, *args, **kwargs):
+        widgets = (Select(attrs={'class': 'select-dateinyear-day'}, choices=DAY_CHOICES), Select(attrs={'class': 'select-dateinyear-month'}, choices=MONTH_CHOICES))
+        super(YearlessDateSelect, self).__init__(widgets=widgets, *args, **kwargs)
+
+    def decompress(self, value):
+        if value is None:
+            return [None,None]
+        return [value.day, value.month]
+   
+class YearlessDateField(forms.Field):
+    widget = YearlessDateSelect
+    
+    def clean(self, value):
+        if value == ['','']:
+            #If the values are both None, trigger the default validation for null
+            super(YearlessDateField, self).clean(None)
+        else:
+            try:
+                return YearlessDate(*value)
+            except:
+                raise ValidationError('Invalid date.')
+        
+
+class YearField(forms.Field):
+    widget = TextInput
+    
+    def clean(self, value):
+        #First, run general validation (will catch, for example, a blank entry
+        #if the field is required
+        super(YearField, self).clean(value)
+        
+        try:
+            value = int(value)
+            #TODO: allow customization of these limits
+            if value < 1900 or value > 2200:
+                raise Exception()
+            return value
+        except:
+            raise ValidationError('Invalid year.')
+        
---
+++ packages/django-yearlessdate/django-yearlessdate-0.3-beta.tar.gz/django-yearlessdate-0.3-beta/djangoyearlessdate/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+from helpers import YearlessDate
+import forms
+
+class YearlessDateField(models.Field):
+    "A model field for storing dates without years"
+    description = "A date without a year, for use in things like birthdays"
+
+    __metaclass__ = models.SubfieldBase
+
+    def __init__(self, *args, **kwargs):
+        kwargs['max_length'] = 4
+        super(YearlessDateField, self).__init__(*args, **kwargs)
+
+    def to_python(self, value):
+        if isinstance(value, YearlessDate):
+            return value
+        if not value:
+            return None
+        # The string case.
+        return YearlessDate(value[2:], value[:2])
+    
+    def get_prep_value(self, value):
+        "The reverse of to_python, for inserting into the database"
+        if value is not None:
+            return ''.join(["%02d" % i for i in (value.month, value.day)])
+    
+    def get_internal_type(self):
+        return 'CharField'
+    
+    def value_to_string(self, obj):
+        "For serialization"
+        value = self._get_val_from_obj(obj)
+        return self.get_db_prep_value(value)
+    
+    def formfield(self, **kwargs):
+        # This is a fairly standard way to set up some defaults
+        # while letting the caller override them.
+        defaults = {'form_class': forms.YearlessDateField}
+        defaults.update(kwargs)
+        return super(YearlessDateField, self).formfield(**defaults)
+    
+class YearField(models.IntegerField):
+    "A model field for storing years, e.g. 2012"
+    def formfield(self, **kwargs):
+        defaults = {'form_class': forms.YearField}
+        defaults.update(kwargs)
+        return super(YearField, self).formfield(**defaults)
+    
+#South integration
+try:
+    from south.modelsinspector import add_introspection_rules
+except ImportError:
+    #South is not installed
+    pass
+else:
+    add_introspection_rules([], ["^djangoyearlessdate\.models\.YearlessDateField"])
+    add_introspection_rules([], ["^djangoyearlessdate\.models\.YearField"])
