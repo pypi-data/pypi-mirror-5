e9cb266befcef6e05e78cf4b4147a8e2557141c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:32 +0000

    Add package coinshot/1.2.2/coinshot-1.2.2.tar.gz

---
+++ packages/coinshot/coinshot-1.2.2.tar.gz/coinshot-1.2.2/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+NAME = 'coinshot'
+DESCRIPTION = 'simple python module for pushover.net'
+VERSION = open('VERSION').read().strip()
+LONG_DESC = open('README.rst').read()
+
+setup(
+    name = NAME,
+    version = VERSION,
+    author = 'Charles Thomas',
+    author_email = 'ch@rlesthom.as',
+    url = 'http://code.cha.rlesthom.as/%s' % NAME,
+    license = 'MIT',
+    description = DESCRIPTION,
+    long_description = LONG_DESC,
+    install_requires = ["simplejson >= 3.3.0"],
+)
