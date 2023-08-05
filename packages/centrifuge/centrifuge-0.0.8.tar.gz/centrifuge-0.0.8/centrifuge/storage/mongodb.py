593dbea2c161084f89e59065e10efb8df2890487
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:03 +0000

    Add package centrifuge/0.0.8/centrifuge-0.0.8.tar.gz

---
+++ packages/centrifuge/centrifuge-0.0.8.tar.gz/centrifuge-0.0.8/centrifuge/storage/mongodb.py
@@ -0,0 +1,377 @@
+# coding: utf-8
+#
+# Copyright (c) Alexandr Emelin. BSD license.
+# All rights reserved.
+#
+import uuid
+import time
+import logging
+import motor
+from bson import ObjectId
+from tornado.gen import Task, coroutine, Return
+from .. import auth
+
+
+logger = logging.getLogger('centrifuge')
+
+
+def on_error(error):
+    """
+    General error wrapper.
+    """
+    logger.error(str(error))
+    raise Return((None, error))
+
+
+def ensure_indexes(db, drop=False):
+    if drop:
+        logger.info('dropping indexes...')
+        db.project.drop_indexes()
+        db.category.drop_indexes()
+
+    db.project.ensure_index([('name', 1)], unique=True)
+    db.category.ensure_index([('name', 1), ('project', 1)], unique=True)
+
+    logger.info('Database ready')
+
+
+def init_db(app, settings):
+    """
+    Create MongoDB connection, ensure indexes
+    """
+    db = motor.MotorClient(
+        host=settings.get("host", "localhost"),
+        port=settings.get("port", 27017),
+        max_pool_size=settings.get("pool_size", 10)
+    ).open_sync()[settings.get("name", "centrifuge")]
+
+    app.db = db
+
+    ensure_indexes(db)
+
+
+def extract_obj_id(obj):
+    if isinstance(obj, dict):
+        obj_id = obj['_id']
+    else:
+        obj_id = obj
+    return obj_id
+
+
+@coroutine
+def insert(collection, data):
+    """
+    Insert data into collection.
+    """
+    if 'created_at' not in data:
+        data['created_at'] = int(time.time())
+    if 'is_active' not in data:
+        data['is_active'] = True
+    (result, error), _ = yield Task(collection.insert, data)
+    if error:
+        on_error(error)
+
+    raise Return((result, None))
+
+
+@coroutine
+def find(collection, haystack):
+    """
+    Find objects in MongoDB collection by haystack.
+    """
+    cursor = collection.find(haystack, limit=10000)
+    (objects, error), _ = yield Task(cursor.to_list)
+    if error:
+        on_error(error)
+
+    raise Return((objects, None))
+
+
+@coroutine
+def update(collection, haystack, update_data):
+    """
+    Update entries matching haystack with update_data.
+    """
+    if 'updated_at' not in update_data:
+        update_data['updated_at'] = int(time.time())
+    (result, error), _ = yield Task(
+        collection.update, haystack, {"$set": update_data}
+    )
+    if error:
+        on_error(error)
+    raise Return((result, None))
+
+
+@coroutine
+def find_one(collection, haystack):
+    """
+    Find object in MongoDB collection.
+    """
+    (obj, error), _ = yield Task(collection.find_one, haystack)
+    if error:
+        on_error(error)
+    if not obj:
+        raise Return((None, None))
+    raise Return((obj, None))
+
+
+@coroutine
+def remove(collection, haystack):
+    """
+    Find object in MongoDB collection.
+    """
+    (res, error), _ = yield Task(collection.remove, haystack)
+    if error:
+        on_error(error)
+
+    raise Return((res, None))
+
+
+@coroutine
+def find_one_or_create(collection, haystack):
+    """
+    First try to find object by haystack, create new if not found.
+    """
+    obj, error = yield find_one(collection, haystack)
+    if error:
+        on_error(error)
+
+    if not obj:
+        obj_id = str(ObjectId())
+        haystack['_id'] = obj_id
+        result, error = yield insert(collection, haystack)
+        if error:
+            on_error(error)
+        obj = haystack
+
+    raise Return((obj, None))
+
+
+@coroutine
+def check_auth(db, project, sign, encoded_data):
+    """
+    Authenticate incoming request. Make sure that it has all rights
+    to create new events in specified project.
+    """
+    assert isinstance(project, dict)
+
+    is_authenticated = auth.check_sign(
+        project['secret_key'],
+        project['_id'],
+        encoded_data,
+        sign
+    )
+    if not is_authenticated:
+        raise Return((None, None))
+
+    raise Return((True, None))
+
+
+@coroutine
+def project_list(db):
+    """
+    Get all projects
+    """
+    projects, error = yield find(db.project, {})
+    if error:
+        on_error(error)
+
+    raise Return((projects, None))
+
+
+@coroutine
+def get_project_by_name(db, project_name):
+    project, error = yield find_one(db.project, {'name': project_name})
+    if error:
+        on_error(error)
+    raise Return((project, None))
+
+
+@coroutine
+def get_project_by_id(db, project_id):
+    project, error = yield find_one(db.project, {'_id': project_id})
+    if error:
+        on_error(error)
+        return
+    raise Return((project, None))
+
+
+@coroutine
+def get_project_categories(db, project):
+    project_id = extract_obj_id(project)
+    categories, error = yield find(db.category, {'project': project_id})
+    if error:
+        on_error(error)
+        return
+    raise Return((categories, None))
+
+
+@coroutine
+def project_create(db, project_name, display_name,
+                   description, validate_url, auth_attempts,
+                   back_off_interval, back_off_max_timeout):
+    project_id = str(ObjectId())
+    to_insert = {
+        '_id': project_id,
+        'name': project_name,
+        'display_name': display_name,
+        'description': description,
+        'validate_url': validate_url,
+        'auth_attempts': auth_attempts,
+        'back_off_interval': back_off_interval,
+        'back_off_max_timeout': back_off_max_timeout,
+        'secret_key': uuid.uuid4().hex
+    }
+    result, error = yield insert(db.project, to_insert)
+    if error:
+        on_error(error)
+        return
+    raise Return((to_insert, None))
+
+
+@coroutine
+def regenerate_project_secret_key(db, project):
+    """
+    Create new secret key for specified project.
+    """
+    project_id = extract_obj_id(project)
+    haystack = {
+        '_id': project_id
+    }
+    update_data = {
+        'secret_key': uuid.uuid4().hex
+    }
+    result, error = yield update(db.project, haystack, update_data)
+    if error:
+        on_error(error)
+
+    raise Return((update_data, None))
+
+
+@coroutine
+def project_delete(db, project):
+    """
+    Delete project. Also delete all related categories and events.
+    """
+    haystack = {
+        '_id': project['_id']
+    }
+    _res, error = yield remove(db.project, haystack)
+    if error:
+        on_error(error)
+
+    haystack = {
+        'project': project['_id']
+    }
+    _res, error = yield remove(db.category, haystack)
+    if error:
+        on_error(error)
+
+    raise Return((True, None))
+
+
+@coroutine
+def project_edit(db, project, name, display_name,
+                 description, validate_url, auth_attempts,
+                 back_off_interval, back_off_max_timeout):
+    """
+    Edit project
+    """
+    to_update = {
+        'name': name,
+        'display_name': display_name,
+        'description': description,
+        'validate_url': validate_url,
+        'auth_attempts': auth_attempts,
+        'back_off_interval': back_off_interval,
+        'back_off_max_timeout': back_off_max_timeout
+    }
+    _res, error = yield update(
+        db.project,
+        {'_id': project['_id']},
+        to_update
+    )
+    if error:
+        on_error(error)
+
+    raise Return((True, None))
+
+
+@coroutine
+def category_create(db, project, category_name,
+                    bidirectional=False, publish_to_admins=False):
+
+    haystack = {
+        '_id': str(ObjectId()),
+        'project': project['_id'],
+        'name': category_name,
+        'bidirectional': bidirectional,
+        'publish_to_admins': publish_to_admins
+    }
+    category, error = yield insert(db.category, haystack)
+    if error:
+        on_error(error)
+
+    raise Return((category, None))
+
+
+@coroutine
+def category_delete(db, project, category_name):
+    """
+    Delete category from project. Also delete all related entries from
+    event collection.
+    """
+    category, error = yield get_project_category(db, project, category_name)
+
+    if not category:
+        raise Return((True, None))
+
+    haystack = {
+        'project': project['_id'],
+        '_id': category['_id']
+    }
+    _res, error = yield remove(db.category, haystack)
+    if error:
+        on_error(error)
+
+    raise Return((True, None))
+
+
+@coroutine
+def get_project_category(db, project, category_name):
+    """
+    Find category by name for project.
+    """
+    haystack = {
+        'project': project['_id'],
+        'name': category_name
+    }
+    category, error = yield find_one(db.category, haystack)
+    if error:
+        on_error(error)
+
+    raise Return((category, None))
+
+
+@coroutine
+def get_categories_for_projects(db, projects):
+    """
+    pass
+    """
+    project_ids = [extract_obj_id(x) for x in projects]
+    haystack = {
+        "project": {"$in": project_ids}
+    }
+
+    categories, error = yield find(db.category, haystack)
+    if error:
+        on_error(error)
+
+    to_return = {}
+
+    for category in categories:
+        if category['project'] not in to_return:
+            to_return[category['project']] = []
+        to_return[category['project']].append(category)
+
+    raise Return((to_return, None))
---
+++ packages/centrifuge/centrifuge-0.0.8.tar.gz/centrifuge-0.0.8/centrifuge/storage/postgresql.py
@@ -0,0 +1,399 @@
+# coding: utf-8
+#
+# Copyright (c) Alexandr Emelin. BSD license.
+# All rights reserved.
+#
+import logging
+from tornado.gen import coroutine, Return
+import momoko
+import psycopg2.extras
+import uuid
+from bson import ObjectId
+from .. import auth
+from functools import partial
+
+
+logger = logging.getLogger('centrifuge')
+
+
+def on_error(error):
+    """
+    General error wrapper.
+    """
+    logger.error(str(error))
+    raise Return((None, error))
+
+
+def init_db(app, settings):
+    dsn = 'dbname=%s user=%s password=%s host=%s port=%s' % (
+        settings.get('name', 'centrifuge'),
+        settings.get('user', 'postgres'),
+        settings.get('password', ''),
+        settings.get('host', 'localhost'),
+        settings.get('port', 5432)
+    )
+    callback = partial(on_connection_ready, app)
+    db = momoko.Pool(
+        dsn=dsn, size=settings.get('pool_size', 10), callback=callback
+    )
+    app.db = db
+
+
+@coroutine
+def on_connection_ready(app):
+
+    db = app.db
+
+    project = 'CREATE TABLE IF NOT EXISTS projects (id SERIAL, _id varchar(24) UNIQUE, ' \
+              'name varchar(100) NOT NULL UNIQUE, display_name ' \
+              'varchar(100) NOT NULL, description text, validate_url varchar(255), ' \
+              'auth_attempts integer, back_off_interval integer, ' \
+              'back_off_max_timeout integer, secret_key varchar(32))'
+
+    category = 'CREATE TABLE IF NOT EXISTS categories (id SERIAL, ' \
+               '_id varchar(24) UNIQUE, project_id varchar(24), ' \
+               'name varchar(100) NOT NULL UNIQUE, bidirectional bool, ' \
+               'publish_to_admins bool)'
+
+    yield momoko.Op(db.execute, project, ())
+    yield momoko.Op(db.execute, category, ())
+    logger.info("Database ready")
+
+
+def extract_obj_id(obj):
+    if isinstance(obj, dict):
+        obj_id = obj['_id']
+    else:
+        obj_id = obj
+    return obj_id
+
+
+@coroutine
+def project_list(db):
+    """
+    Get all projects user can see.
+    """
+    query = "SELECT * FROM projects"
+    try:
+        cursor = yield momoko.Op(
+            db.execute, query, {},
+            cursor_factory=psycopg2.extras.RealDictCursor
+        )
+    except Exception as e:
+        on_error(e)
+    else:
+        projects = cursor.fetchall()
+        raise Return((projects, None))
+
+
+@coroutine
+def get_categories_for_projects(db, projects):
+
+    categories = []
+
+    project_ids = [extract_obj_id(x) for x in projects]
+    query = "SELECT * FROM categories WHERE project_id IN %s"
+    if project_ids:
+        try:
+            cursor = yield momoko.Op(
+                db.execute, query, (tuple(project_ids),),
+                cursor_factory=psycopg2.extras.RealDictCursor
+            )
+        except Exception as e:
+            on_error(e)
+        else:
+            categories = cursor.fetchall()
+
+    to_return = {}
+    for category in categories:
+        if category['project_id'] not in to_return:
+            to_return[category['project_id']] = []
+        to_return[category['project_id']].append(category)
+
+    raise Return((to_return, None))
+
+
+@coroutine
+def get_project_by_name(db, project_name):
+    query = "SELECT * FROM projects WHERE name=%(name)s LIMIT 1"
+    try:
+        cursor = yield momoko.Op(
+            db.execute, query, {'name': project_name},
+            cursor_factory=psycopg2.extras.RealDictCursor
+        )
+    except Exception as e:
+        on_error(e)
+    else:
+        project = cursor.fetchone()
+        raise Return((project, None))
+
+
+@coroutine
+def get_project_by_id(db, project_id):
+    query = "SELECT * FROM projects WHERE _id=%(project_id)s LIMIT 1"
+    try:
+        cursor = yield momoko.Op(
+            db.execute, query, {'project_id': project_id},
+            cursor_factory=psycopg2.extras.RealDictCursor
+        )
+    except Exception as e:
+        on_error(e)
+    else:
+        project = cursor.fetchone()
+        raise Return((project, None))
+
+
+@coroutine
+def project_create(db, project_name, display_name,
+                   description, validate_url, auth_attempts,
+                   back_off_interval, back_off_max_timeout):
+    to_insert = {
+        '_id': str(ObjectId()),
+        'name': project_name,
+        'display_name': display_name,
+        'description': description,
+        'validate_url': validate_url,
+        'auth_attempts': auth_attempts or None,
+        'back_off_interval': back_off_interval or None,
+        'back_off_max_timeout': back_off_max_timeout or None,
+        'secret_key': uuid.uuid4().hex
+    }
+
+    query = "INSERT INTO projects (_id, name, display_name, description, " \
+            "validate_url, auth_attempts, back_off_interval, back_off_max_timeout, secret_key) " \
+            "VALUES (%(_id)s, %(name)s, %(display_name)s, %(description)s, " \
+            "%(validate_url)s, %(auth_attempts)s, %(back_off_interval)s, " \
+            "%(back_off_max_timeout)s, %(secret_key)s)"
+
+    try:
+        yield momoko.Op(
+            db.execute, query, to_insert
+        )
+    except Exception as e:
+        on_error(e)
+    else:
+        raise Return((to_insert, None))
+
+
+@coroutine
+def project_edit(db, project, name, display_name,
+                 description, validate_url, auth_attempts,
+                 back_off_interval, back_off_max_timeout):
+    """
+    Edit project
+    """
+    to_update = {
+        '_id': extract_obj_id(project),
+        'name': name,
+        'display_name': display_name,
+        'description': description,
+        'validate_url': validate_url,
+        'auth_attempts': auth_attempts or None,
+        'back_off_interval': back_off_interval or None,
+        'back_off_max_timeout': back_off_max_timeout or None
+    }
+
+    query = "UPDATE projects SET name=%(name)s, display_name=%(display_name)s, " \
+            "description=%(description)s, validate_url=%(validate_url)s, " \
+            "auth_attempts=%(auth_attempts)s, back_off_interval=%(back_off_interval)s, " \
+            "back_off_max_timeout=%(back_off_max_timeout)s WHERE " \
+            "_id=%(_id)s"
+
+    try:
+        yield momoko.Op(
+            db.execute, query, to_update
+        )
+    except Exception as e:
+        on_error(e)
+    else:
+        raise Return((to_update, None))
+
+
+@coroutine
+def project_delete(db, project):
+    """
+    Delete project. Also delete all related categories and events.
+    """
+    haystack = {
+        'project_id': project['_id']
+    }
+
+    query = "DELETE FROM projects WHERE _id=%(project_id)s"
+    try:
+        yield momoko.Op(
+            db.execute, query, haystack
+        )
+    except Exception as e:
+        on_error(e)
+
+    query = "DELETE FROM categories WHERE project_id=%(project_id)s"
+    try:
+        yield momoko.Op(
+            db.execute, query, haystack
+        )
+    except Exception as e:
+        on_error(e)
+
+    raise Return((True, None))
+
+
+@coroutine
+def get_project_categories(db, project):
+    project_id = extract_obj_id(project)
+
+    query = "SELECT * FROM categories WHERE project_id=%(project_id)s"
+
+    try:
+        cursor = yield momoko.Op(
+            db.execute, query, {'project_id': project_id},
+            cursor_factory=psycopg2.extras.RealDictCursor
+        )
+    except Exception as e:
+        on_error(e)
+    else:
+        categories = cursor.fetchall()
+        raise Return((categories, None))
+
+
+@coroutine
+def get_project_users(db, project):
+
+    query = "SELECT t1._id, t1.email, t2.is_active, t2.public_key, t2.secret_key, t2.readonly " \
+            "FROM users t1 JOIN project_keys t2 on t1._id=t2.user_id WHERE " \
+            "project_id=%(project_id)s AND is_active=%(is_active)s"
+
+    try:
+        cursor = yield momoko.Op(
+            db.execute, query, {'project_id': extract_obj_id(project), 'is_active': True},
+            cursor_factory=psycopg2.extras.RealDictCursor
+        )
+    except Exception as e:
+        on_error(e)
+    else:
+        users = cursor.fetchall()
+        raise Return((users, None))
+
+
+@coroutine
+def regenerate_project_secret_key(db, project):
+    """
+    Create new secret and public keys for user in specified project.
+    """
+    project_id = extract_obj_id(project)
+    secret_key = uuid.uuid4().hex
+    haystack = {
+        'project_id': project_id,
+        'secret_key': secret_key
+    }
+
+    query = "UPDATE projects * SET secret_key=%(secret_key)s " \
+            "WHERE _id=%(project_id)s"
+
+    try:
+        yield momoko.Op(
+            db.execute, query, haystack,
+            cursor_factory=psycopg2.extras.RealDictCursor
+        )
+    except Exception as e:
+        on_error(e)
+    else:
+        raise Return((secret_key, None))
+
+
+@coroutine
+def get_project_category(db, project, category_name):
+    """
+    Find category by name for project.
+    """
+    haystack = {
+        'project_id': project['_id'],
+        'name': category_name
+    }
+
+    query = "SELECT * FROM categories WHERE name=%(name)s AND " \
+            "project_id=%(project_id)s LIMIT 1"
+
+    try:
+        cursor = yield momoko.Op(
+            db.execute, query, haystack,
+            cursor_factory=psycopg2.extras.RealDictCursor
+        )
+    except Exception as e:
+        on_error(e)
+    else:
+        category = cursor.fetchone()
+        raise Return((category, None))
+
+
+@coroutine
+def category_create(db, project, category_name,
+                    bidirectional=False, publish_to_admins=False):
+
+    to_insert = {
+        '_id': str(ObjectId()),
+        'project_id': project['_id'],
+        'name': category_name,
+        'bidirectional': bidirectional,
+        'publish_to_admins': publish_to_admins
+    }
+
+    query = "INSERT INTO categories (_id, project_id, name, bidirectional, " \
+            "publish_to_admins) VALUES (%(_id)s, %(project_id)s, %(name)s, " \
+            "%(bidirectional)s, %(publish_to_admins)s)"
+
+    try:
+        yield momoko.Op(
+            db.execute, query, to_insert
+        )
+    except Exception as e:
+        on_error(e)
+    else:
+        raise Return((to_insert, None))
+
+
+@coroutine
+def category_delete(db, project, category_name):
+    """
+    Delete category from project. Also delete all related entries from
+    event collection.
+    """
+    category, error = yield get_project_category(db, project, category_name)
+
+    if not category:
+        raise Return((True, None))
+
+    haystack = {
+        'project_id': project['_id'],
+        '_id': category['_id']
+    }
+
+    query = "DELETE FROM categories WHERE _id=%(_id)s AND project_id=%(project_id)s"
+
+    try:
+        yield momoko.Op(
+            db.execute, query, haystack
+        )
+    except Exception as e:
+        on_error(e)
+    else:
+        raise Return((True, None))
+
+
+@coroutine
+def check_auth(db, project, sign, encoded_data):
+    """
+    Authenticate incoming request. Make sure that it has all rights
+    to create new events in specified project.
+    """
+    assert isinstance(project, dict)
+
+    is_authenticated = auth.check_sign(
+        project['secret_key'],
+        project['_id'],
+        encoded_data,
+        sign
+    )
+    if not is_authenticated:
+        raise Return((None, None))
+
+    raise Return((True, None))
