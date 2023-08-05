e803ca16f25a80cc7eae376ffb021341640af675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:51 +0000

    Add package nester_rever/1.3.0/nester_rever-1.3.0.zip

---
+++ packages/nester_rever/nester_rever-1.3.0.zip/nester_rever-1.3.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name           = 'nester_rever',
+        version         = '1.3.0',
+        py_modules     = ['nester_rever'],
+        author         = 'rever',
+        author_email   = 'rever@139.com',
+        url            = 'http://www.baybey.net',
+        description    = 'slist',
+
+    )
