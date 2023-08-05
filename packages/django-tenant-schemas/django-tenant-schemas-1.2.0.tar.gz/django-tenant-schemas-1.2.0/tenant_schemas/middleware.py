f11dab83ff6ce168704bc387728f285a05fe313b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:53 +0000

    Add package django-tenant-schemas/1.2.0/django-tenant-schemas-1.2.0.tar.gz

---
+++ packages/django-tenant-schemas/django-tenant-schemas-1.2.0.tar.gz/django-tenant-schemas-1.2.0/tenant_schemas/middleware.py
@@ -0,0 +1,31 @@
+from django.conf import settings
+from django.db import connection
+from django.shortcuts import get_object_or_404
+from tenant_schemas.utils import get_tenant_model, remove_www_and_dev, get_public_schema_name
+
+
+class TenantMiddleware(object):
+    """
+    This middleware should be placed at the very top of the middleware stack.
+    Selects the proper database schema using the request host. Can fail in
+    various ways which is better than corrupting or revealing data...
+    """
+    def process_request(self, request):
+        """
+        Resets to public schema
+
+        Some nasty weird bugs happened at the production environment without this call.
+        connection.pg_thread.schema_name would already be set and then terrible errors
+        would occur. Any idea why? My theory is django implements connection as some sort
+        of threading local variable.
+        """
+        connection.set_schema_to_public()
+        hostname_without_port = remove_www_and_dev(request.get_host().split(':')[0])
+
+        TenantModel = get_tenant_model()
+        request.tenant = get_object_or_404(TenantModel, domain_url=hostname_without_port)
+        connection.set_tenant(request.tenant)
+
+        # do we have a public-specific token?
+        if hasattr(settings, 'PUBLIC_SCHEMA_URL_TOKEN') and request.tenant.schema_name == get_public_schema_name():
+            request.path_info = settings.PUBLIC_SCHEMA_URL_TOKEN + request.path_info
---
+++ packages/django-tenant-schemas/django-tenant-schemas-1.2.0.tar.gz/django-tenant-schemas-1.2.0/tenant_schemas/test/cases.py
@@ -0,0 +1,24 @@
+from django.core.management import call_command
+from django.db import connection, DEFAULT_DB_ALIAS
+from django.test import TransactionTestCase
+from django.utils.unittest.case import TestCase
+from tenant_schemas.utils import get_tenant_model
+
+class TenantTestCase(TransactionTestCase):
+    @classmethod
+    def setUpClass(cls):
+        # create a tenant
+        tenant_domain = 'tenant.test.com'
+        cls.tenant = get_tenant_model()(domain_url=tenant_domain, schema_name='test')
+        cls.tenant.save(verbosity=0) # todo: is there any way to get the verbosity from the test command here?
+
+        connection.set_tenant(cls.tenant)
+
+    @classmethod
+    def tearDownClass(cls):
+        # delete tenant
+        connection.set_schema_to_public()
+        cls.tenant.delete()
+
+        cursor = connection.cursor()
+        cursor.execute('DROP SCHEMA test CASCADE')
