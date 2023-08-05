b57a9fe8c29730387a9a7494ac2ed7fa83698195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:31 +0000

    Add package infi.projector/0.6/infi.projector-0.6-py2.7.egg

---
+++ packages/infi.projector/infi.projector-0.6-py2.7.egg/infi/projector/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.6"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '2ec9b3906b07dc34467f6219e59be213688982b5'
+__git_head_subject__ = 'Finished release v0.6.'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
