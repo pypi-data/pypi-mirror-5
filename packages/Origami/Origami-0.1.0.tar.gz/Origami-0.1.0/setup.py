9a1694f7c558a12bc40dc8ce29b614a03726aa5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:55 +0000

    Add package Origami/0.1.0/Origami-0.1.0.tar.gz

---
+++ packages/Origami/Origami-0.1.0.tar.gz/Origami-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='Origami',
+    version='0.1.0',
+    author='Joe Cross',
+    author_email='joe.mcross@gmail.com',
+    packages=['origami'],
+    url='https://github.com/numberoverzero/origami',
+    license='LICENSE.txt',
+    description='Lightweight bit packing for classes',
+    long_description=open('README.rst').read(),
+    install_requires=["bitstring"],
+    requires=["bitstring"],
+)
