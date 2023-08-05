b49cf782167ca2b97defae7e22d3f59a63253ca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:14 +0000

    Add package testfixtures/3.0.1/testfixtures-3.0.1.tar.gz

---
+++ packages/testfixtures/testfixtures-3.0.1.tar.gz/testfixtures-3.0.1/testfixtures/tests/test_docs.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2009-2012 Simplistix Ltd
+#
+# See license.txt for more details.
+
+from doctest import REPORT_NDIFF,ELLIPSIS
+from glob import glob
+from manuel import doctest, capture
+from manuel.testing import TestSuite
+from nose.plugins.skip import SkipTest
+from os.path import dirname, join, pardir
+
+import os
+
+from . import compat
+
+workspace = os.environ.get('WORKSPACE', join(dirname(__file__), pardir, pardir))
+tests = glob(join(workspace,'docs', '*.txt'))
+
+if not tests:
+    raise SkipTest('No docs found to test') # pragma: no cover
+
+def test_suite():
+    m =  doctest.Manuel(optionflags=REPORT_NDIFF|ELLIPSIS,
+                        checker=compat.DocTestChecker())
+    m += compat.Manuel()
+    m += capture.Manuel()
+    return TestSuite(m, *tests)
