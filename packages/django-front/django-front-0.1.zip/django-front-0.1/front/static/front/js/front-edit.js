741741b8531e4b509077a9f0425ecb598295185f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:12 +0000

    Add package django-front/0.1/django-front-0.1.zip

---
+++ packages/django-front/django-front-0.1.zip/django-front-0.1/front/static/front/js/front-edit.js
@@ -0,0 +1,71 @@
+jQuery(document).ready(function($) {
+    var editables = $('.editable');
+
+    editables.on('dblclick', function(event) {
+        event.preventDefault();
+
+        var el = $(this),
+            html = el.html(),
+            el_id = el.attr('id'),
+            tag = 'textarea',
+            container,
+            editor;
+
+        if (document._front_edit.plugin == 'ace') {
+            tag = 'div';
+        }
+
+        container = $('<'+tag+' class="front-edit-container" id="edit-'+el_id+'"></'+tag+'><p><button class="cancel">cancel</button><button class="save">save</button></p>');
+
+        if(el.is('.editing')) {
+            return;
+        } else {
+            el.addClass('editing');
+        }
+
+        el.html(container);
+
+        switch(document._front_edit.plugin) {
+            case 'ace':
+                $.getScript('http://d1n0x3qji82z53.cloudfront.net/src-min-noconflict/ace.js', function(){
+                    el.addClass('front-edit-ace');
+                    editor = ace.edit("edit-" + el_id);
+                    editor.setTheme("ace/theme/monokai");
+                    editor.setValue(html);
+                    editor.getSession().setMode("ace/mode/html");
+                });
+                break;
+            default:
+                el.find('.front-edit-container').html(html);
+                break;
+        }
+
+        el.find('.cancel').on('click', function(event) {
+            el.html(html);
+            el.removeClass('editing');
+        });
+
+        el.find('.save').on('click', function(event) {
+            var new_html, key = el_id;
+
+            switch(document._front_edit.plugin) {
+                case 'ace':
+                    new_html = editor.getValue();
+                    break;
+                default:
+                    new_html = el.find('.front-edit-container').val();
+                    break;
+            }
+
+            $.post(document._front_edit.save_url, {
+                key: key,
+                val: new_html,
+                csrfmiddlewaretoken: document._front_edit.csrf_token
+            }, function(data, textStatus, xhr) {
+                // todo: return val
+            });
+            el.removeClass('editing');
+            el.html(new_html);
+        });
+    });
+});
