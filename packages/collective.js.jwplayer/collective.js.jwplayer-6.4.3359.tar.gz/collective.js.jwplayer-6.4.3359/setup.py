51149a7ca375e1c268547a29f4441da7fbb2f7e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:52 +0000

    Add package collective.js.jwplayer/6.4.3359/collective.js.jwplayer-6.4.3359.tar.gz

---
+++ packages/collective.js.jwplayer/collective.js.jwplayer-6.4.3359.tar.gz/collective.js.jwplayer-6.4.3359/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import os
+
+version = '6.4.3359'
+
+setup(name='collective.js.jwplayer',
+      version=version,
+      description="JWPlayer resource for Plone",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Environment :: Web Environment",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.0",
+        "Framework :: Plone :: 4.1",
+        "Framework :: Plone :: 4.2",
+        "Framework :: Plone :: 4.3",
+        "Programming Language :: Python",
+        ],
+      keywords='plone jwplayer',
+      author='JeanMichel FRANCOIS aka toutpt',
+      author_email='toutpt@gmail.com',
+      url='https://github.com/collective/collective.js.jwplayer',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.js'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
