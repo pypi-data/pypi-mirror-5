74a8569ea78af93dc8548886396acca46d338f6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:19 +0000

    Add package Samflow/0.2/Samflow-0.2.tar.gz

---
+++ packages/Samflow/Samflow-0.2.tar.gz/Samflow-0.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Samflow',
+    version='0.2',
+    packages=['samflow', 'samflow.testsuite'],
+    url='https://bitbucket.org/hanfeisun/pyflow',
+    license='MIT',
+    author='Hanfei Sun',
+    author_email='hfsun.tju@gmail.com',
+    description='A Python framework for writing flexible workflows'
+)
