f9ea97c0ae72692a1893f3feda100b06775811f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:16 +0000

    Add package django-jstemplate/1.2.1/django-jstemplate-1.2.1.tar.gz

---
+++ packages/django-jstemplate/django-jstemplate-1.2.1.tar.gz/django-jstemplate-1.2.1/jstemplate/preprocessors.py
@@ -0,0 +1,31 @@
+from __future__ import unicode_literals
+
+import re
+from django.utils.translation import ugettext
+from .conf import conf
+
+class I18nPreprocessor(object):
+    @property
+    def tagnames(self):
+        return conf.JSTEMPLATE_I18N_TAGS
+
+    @property
+    def trans_re(self):
+        # Should match strings like: {{# _ }}Hello, {{ name }}.{{/ _ }}
+        tagnames = '|'.join(['(?:{0})'.format(t) for t in self.tagnames])
+        start_tag = r'\{\{#\s*(?:' + tagnames + r')\s*\}\}'
+        end_tag = r'\{\{\/\s*(?:' + tagnames + r')\s*\}\}'
+
+        return start_tag + '(.*?)' + end_tag
+
+    def translate(self, match):
+        """Translate a result of matching the compiled trans_re pattern."""
+        string = match.group(1)
+        return ugettext(string) if len(string) > 0 else ''
+
+    def process(self, content):
+        # TODO: cache the compiled regex.
+        # We need to compile here because re.sub doesn't accept a flags
+        # argument in python < 2.7, but re.compile does.
+        pattern = re.compile(self.trans_re, flags=re.DOTALL)
+        return re.sub(pattern, self.translate, content)
