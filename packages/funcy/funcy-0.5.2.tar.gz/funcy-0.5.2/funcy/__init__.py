dbcf89f5e14efb38e78f6c606a818b74304ab544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:26 +0000

    Add package funcy/0.5.2/funcy-0.5.2.tar.gz

---
+++ packages/funcy/funcy-0.5.2.tar.gz/funcy-0.5.2/funcy/__init__.py
@@ -0,0 +1,25 @@
+from .calc import *
+from .colls import *
+from .decorators import *
+from .funcolls import *
+from .funcs import *
+from .seqs import *
+from .types import *
+from .strings import *
+from .flow import *
+from .objects import *
+from .namespaces import namespace
+from .debug import *
+
+
+def inc(x):
+    return x + 1
+
+def dec(x):
+    return x - 1
+
+def even(x):
+    return x % 2 == 0
+
+def odd(x):
+    return x % 2 == 1
