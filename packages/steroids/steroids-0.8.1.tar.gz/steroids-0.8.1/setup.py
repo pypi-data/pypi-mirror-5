3cd410d24fc6b597aa5c27724243801eb4e9d546
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:58 +0000

    Add package steroids/0.8.1/steroids-0.8.1.tar.gz

---
+++ packages/steroids/steroids-0.8.1.tar.gz/steroids-0.8.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup
+
+setup(name='steroids',
+      version='0.8.1',
+      description='Steroids: a set of tools to help python developers to build real-time ready, non-blocking, web application reducing efforts and time.',
+      author='Lorenzo Setale ( http://who.is.lorenzo.setale.me/? )',
+      author_email='koalalorenzo@gmail.com',
+      url='https://github.com/koalalorenzo/Steroids',
+      packages=['steroids', 'steroids.modules'],
+      scripts=['scripts/steroids']
+     )
