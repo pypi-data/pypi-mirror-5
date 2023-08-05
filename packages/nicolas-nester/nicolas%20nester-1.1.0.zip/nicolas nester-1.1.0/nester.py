76ad9e402b7c156d391cd52d52a3c7efcc69e2f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:49 +0000

    Add package nicolas-nester/1.1.0/nicolas%20nester-1.1.0.zip

---
+++ packages/nicolas-nester/nicolas%20nester-1.1.0.zip/nicolas nester-1.1.0/nester.py
@@ -0,0 +1,13 @@
+"""
+My first make code. 2013-05-16.
+This is a module of "study_nester.py"
+This provides on function called print_list() which prints lists that may or may not include nested list.
+"""
+def print_list(the_list, level):
+	for each_item in the_list:
+		if isinstance(each_item, list):
+			print_list(each_item, level+1)
+		else:
+			for tab_stop in range(level):
+				print("\t", end='')
+			print(each_item)
---
+++ packages/nicolas-nester/nicolas%20nester-1.1.0.zip/nicolas nester-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'nicolas nester',
+	version		= '1.1.0',
+	py_modules	= ['nester'],
+	author		= 'nicolas zang',
+	author_email	= 'nicolas.zang@gmail.com',
+	url			= 'http://www.nicolas.co.kr/',
+	description	= 'A simple printer of nester lists for The First head Python of book',
+	)
