c189deb159c89c2c61010d306ef19a6d35993ddf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:20 +0000

    Add package srccat/0.3/srccat-0.3.tar.gz

---
+++ packages/srccat/srccat-0.3.tar.gz/srccat-0.3/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+import srccat
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+packages = ['srccat', 'tests']
+
+requires = ['colout']
+
+setup(
+    name='srccat',
+    version='0.3',
+    description='Syntax highlights source code to terminal using colout',
+    long_description='',
+    author='cfeduke',
+    author_email='charles.feduke@gmail.com',
+    url='http://cfeduke.github.com/srccat/',
+    packages=packages,
+    package_data={'': ['LICENSE']},
+    package_dir={'srccat': 'srccat'},
+    scripts=['bin/srccat'],
+    include_package_data=True,
+    install_requires=requires,
+    license='GPL',
+    zip_safe=False,
+)
