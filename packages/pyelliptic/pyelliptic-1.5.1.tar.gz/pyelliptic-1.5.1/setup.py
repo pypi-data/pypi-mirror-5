134ceecdb38406992cc93000f460c900962e6bf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:39 +0000

    Add package pyelliptic/1.5.1/pyelliptic-1.5.1.tar.gz

---
+++ packages/pyelliptic/pyelliptic-1.5.1.tar.gz/pyelliptic-1.5.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyelliptic",
+    version='1.5.1',
+    url='https://github.com/yann2192/pyelliptic',
+    license='GPL',
+    description="Python OpenSSL wrapper. For modern cryptography with ECC, AES, HMAC, Blowfish, ...",
+    author='Yann GUIBET',
+    author_email='yannguibet@gmail.com',
+    packages=find_packages(),
+    classifiers=[
+        'Operating System :: Unix',
+        'Operating System :: Microsoft :: Windows',
+        'Environment :: MacOS X',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Security :: Cryptography',
+    ],
+)
