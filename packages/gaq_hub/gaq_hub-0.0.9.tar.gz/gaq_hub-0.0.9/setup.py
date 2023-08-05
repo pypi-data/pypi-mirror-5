bb5514fefb422853260c425fcc4aee9ee33fd222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:57 +0000

    Add package gaq_hub/0.0.9/gaq_hub-0.0.9.tar.gz

---
+++ packages/gaq_hub/gaq_hub-0.0.9.tar.gz/gaq_hub-0.0.9/setup.py
@@ -0,0 +1,37 @@
+"""gaq_helper installation script.
+"""
+import os
+
+from setuptools import setup
+from setuptools import find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, "README.md")).read()
+README = README.split("\n\n", 1)[0] + "\n"
+
+requires = []
+
+setup(name="gaq_hub",
+      version="0.0.9",
+      description="Lightweight Google Analytics support",
+      long_description=README,
+      classifiers=[
+        "Intended Audience :: Developers",
+        "Framework :: Pylons",
+        "Framework :: Pyramid",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        ],
+      keywords="web pylons",
+      author="Jonathan Vanasco",
+      author_email="jonathan@findmeon.com",
+      url="https://github.com/jvanasco/gaq_hub",
+      license="MIT",
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      tests_require = requires,
+      install_requires = requires,
+      test_suite="tests",
+      )
+
