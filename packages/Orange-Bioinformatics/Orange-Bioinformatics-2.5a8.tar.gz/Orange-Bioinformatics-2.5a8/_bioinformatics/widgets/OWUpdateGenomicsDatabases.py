33dc97627ef887fe5115152adfa3fee92ca9764c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:00 +0000

    Add package Orange-Bioinformatics/2.5a8/Orange-Bioinformatics-2.5a8.tar.gz

---
+++ packages/Orange-Bioinformatics/Orange-Bioinformatics-2.5a8.tar.gz/Orange-Bioinformatics-2.5a8/_bioinformatics/widgets/OWUpdateGenomicsDatabases.py
@@ -0,0 +1,24 @@
+"""
+<name>Databases</name>
+<description>Update of systems biology data and knowledge bases.</description>
+<contact>Ales Erjavec</contact>
+<priority>10</priority>
+<icon>icons/Databases.svg</icon>
+"""
+
+from Orange.OrangeWidgets.OWDatabasesUpdate import *
+
+class OWUpdateGenomicsDatabases(OWDatabasesUpdate): 
+    def __init__(self, parent=None, signalManager=None, name="Databases", **kwds):
+        OWDatabasesUpdate.__init__(self, parent, signalManager, name, domains = \
+                ["GO", "MeSH", "Taxonomy", "NCBI_geneinfo", "GEO", 
+                 "dictybase", "OMIM", "HomoloGene", "Affy", "miRNA", "gene_sets",
+                 "PPI"], **kwds)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    w = OWUpdateGenomicsDatabases()
+##    app.setMainWidget(w)
+    w.show()
+    app.exec_()
+    w.saveSettings()
