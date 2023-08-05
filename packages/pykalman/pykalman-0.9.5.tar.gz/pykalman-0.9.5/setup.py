f38be9ecacae815050268095613dbc54b2b1264c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:29 +0000

    Add package pykalman/0.9.5/pykalman-0.9.5.tar.gz

---
+++ packages/pykalman/pykalman-0.9.5.tar.gz/pykalman-0.9.5/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'pykalman',
+    version = '0.9.5',
+    author = 'Daniel Duckworth',
+    author_email = 'pykalman@gmail.com',
+    description = ('An implementation of the Kalman Filter, Kalman ' +
+      'Smoother, and EM algorithm in Python'),
+    license = 'BSD',
+    keywords = 'kalman filter smoothing em hmm tracking unscented ukf kf',
+    url = 'http://pykalman.github.com',
+    packages = find_packages(),
+    classifiers = [
+      'Development Status :: 4 - Beta',
+      'Intended Audience :: Science/Research',
+      'License :: OSI Approved :: BSD License',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 3',
+      'Topic :: Scientific/Engineering :: Artificial Intelligence'
+    ],
+    include_package_data = True,
+#    install_requires = [
+#      'numpy',
+#      'scipy',
+#      'Sphinx',
+#      'numpydoc',
+#    ],
+#    tests_require = [
+#      'nose',
+#    ]
+)
