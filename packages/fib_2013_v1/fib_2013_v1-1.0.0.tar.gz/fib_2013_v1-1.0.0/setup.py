c90c3aa7421745bcab20563310a131a1091ad88b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:46 +0000

    Add package fib_2013_v1/1.0.0/fib_2013_v1-1.0.0.tar.gz

---
+++ packages/fib_2013_v1/fib_2013_v1-1.0.0.tar.gz/fib_2013_v1-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup( 
+        name         = 'fib_2013_v1', 
+        version      = '1.0.0', 
+        ## TODO: be sure to change these next few lines to match your details!
+        py_modules   = ['fib_2013_v1'],
+        author       = 'dinesh',
+        author_email = 'dinesh.dce93@gmail.com',
+        url          = 'http://www.xrayroot.asa',
+        description  = 'fibonacci Series',
+     )
