bf19f82652b920e7088d5be81b383d17cf460e30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:48 +0000

    Add package utils/0.1/utils-0.1.tar.gz

---
+++ packages/utils/utils-0.1.tar.gz/utils-0.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='utils',
+    version='0.1',
+    description='Python Distribution Utilities',
+    author='Haak Saxberg',
+    author_email='haak.erling@gmail.com',
+    url='http://github.com/haaksmash/pyutils',
+    packages=find_packages(),
+)
