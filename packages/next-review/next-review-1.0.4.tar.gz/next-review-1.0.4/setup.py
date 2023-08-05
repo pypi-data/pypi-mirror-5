4ab5178e4c92893692d0434c06d5c332d393a00c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:19 +0000

    Add package next-review/1.0.4/next-review-1.0.4.tar.gz

---
+++ packages/next-review/next-review-1.0.4.tar.gz/next-review-1.0.4/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+
+setuptools.setup(
+    name='next-review',
+    description='Start your next gerrit code review without any hassle.',
+    version='1.0.4',
+    scripts=['next_review.py'],
+    install_requires=['paramiko'],
+    py_modules=['next_review'],
+    entry_points={'console_scripts': ['next-review = next_review:cli']})
