c484f446962086fe3f6d168dd53ec978aa2da7a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:12 +0000

    Add package django-csvimport/1.0/django-csvimport-1.0.tar.gz

---
+++ packages/django-csvimport/django-csvimport-1.0.tar.gz/django-csvimport-1.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='django-csvimport',
+      version=version,
+      description="Import CSV files to django models",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "TODO.txt")).read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Development Status :: 4 - Beta",
+        "Framework :: Django",
+        "License :: OSI Approved :: Apache Software License"
+        ],
+      keywords='CVS import django fixture',
+      author='Ed Crewe',
+      author_email='edmundcrewe@gmail.com',
+      url='https://github.com/edcrewe/django-csvimport',
+      license='Apache',
+      packages=find_packages(exclude=['ez_setup']),
+      include_package_data=True,
+      package_data = {
+        # If any package contains *.csv or *.rst files, include them:
+        '': ['*.csv', '*.rst'],
+      },
+      zip_safe=False,
+      install_requires=[
+          'chardet',
+          'django-appconf>=0.5',
+          'django>=1.4'
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
