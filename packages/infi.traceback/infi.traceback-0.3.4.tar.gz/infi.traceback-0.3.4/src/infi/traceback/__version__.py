49d938393778d42dc87fe0ee6e9861ba918f6e58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:09 +0000

    Add package infi.traceback/0.3.4/infi.traceback-0.3.4.tar.gz

---
+++ packages/infi.traceback/infi.traceback-0.3.4.tar.gz/infi.traceback-0.3.4/src/infi/traceback/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.3.4"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'd2ea1020e3577475fc3cf04bfe31d47f58a52de9'
+__git_head_subject__ = 'Finished release v0.3.4.'
+__git_head_message__ = '* release/v0.3.4:\n  TRIVIAL pretty_traceback_and_exit_decorator: re-raising SystemExit to maintain the return code\n  empty commit after version v0.3.3'
+__git_dirty_diff__ = ''
