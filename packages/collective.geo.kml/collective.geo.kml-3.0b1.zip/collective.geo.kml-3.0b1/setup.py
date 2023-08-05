129ee03b994f0770cd628c7327403ff9242bc9c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:40 +0000

    Add package collective.geo.kml/3.0b1/collective.geo.kml-3.0b1.zip

---
+++ packages/collective.geo.kml/collective.geo.kml-3.0b1.zip/collective.geo.kml-3.0b1/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+import os
+
+version = '3.0b1'
+
+setup(name='collective.geo.kml',
+      version=version,
+      description="Kml view for collective.geo",
+      long_description=(
+          open("README.rst").read() + "\n" +
+          open(os.path.join("docs", "HISTORY.txt")).read()
+      ),
+      classifiers=[
+          "Framework :: Plone",
+          "Topic :: Internet",
+          "Topic :: Scientific/Engineering :: GIS",
+          "Programming Language :: Python",
+      ],
+      keywords='Zope Plone GIS KML Google Maps Bing OpenLayers',
+      author='Giorgio Borelli',
+      author_email='giorgio@giorgioborelli.it',
+      url='https://github.com/collective/collective.geo.kml',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.geo'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Products.CMFCore',
+          'collective.geo.mapwidget > 1.6',
+          'collective.geo.geographer > 1.7',
+      ],
+      extras_require={
+          'test': [
+              'plone.app.testing',
+          ]
+      },
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
