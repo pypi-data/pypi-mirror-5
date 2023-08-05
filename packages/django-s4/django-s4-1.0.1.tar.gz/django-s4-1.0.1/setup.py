4abe89ae8d382aa30505e5b1c8b049cefbd861c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:42 +0000

    Add package django-s4/1.0.1/django-s4-1.0.1.tar.gz

---
+++ packages/django-s4/django-s4-1.0.1.tar.gz/django-s4-1.0.1/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "django-s4",
+    version = "1.0.1",
+    url = 'http://ondrejsika.com/docs/django-s4/',
+    download_url = 'https://github.com/sikaondrej/django-s4/',
+    license = 'GNU LGPL v.3',
+    description = "",
+    author = 'Ondrej Sika',
+    author_email = 'dev@ondrejsika.com',
+    packages = find_packages(),
+    requires = [],
+    include_package_data = True,
+    zip_safe = False,
+)
