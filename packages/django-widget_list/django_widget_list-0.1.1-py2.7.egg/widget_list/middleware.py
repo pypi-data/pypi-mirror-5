7a7c6eecd4f5dc20d4d7e3bf1272c5df4b1d0337
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:36 +0000

    Add package django-widget_list/0.1.1/django_widget_list-0.1.1-py2.7.egg

---
+++ packages/django-widget_list/django_widget_list-0.1.1-py2.7.egg/widget_list/middleware.py
@@ -0,0 +1,14 @@
+def request(self):
+    """
+    Get request
+    """
+    return self.REQUEST
+	
+
+class PaginationMiddleware(object):
+    """
+    Inserts a variable representing the current page onto the request object if
+    it exists in either **GET** or **POST** portions of the request.
+    """
+    def process_request(self, request):
+        request.__class__.page = property(get_page)
