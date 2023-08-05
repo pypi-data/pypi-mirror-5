03faead1def477aa3e69861651993217c0f02c22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:26 +0000

    Add package herokal-settings/0.0.1/herokal-settings-0.0.1.tar.gz

---
+++ packages/herokal-settings/herokal-settings-0.0.1.tar.gz/herokal-settings-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from herokal import __version__
+from setuptools import setup, find_packages
+
+long_description = open('README.rst').read()
+
+setup(
+    name="herokal-settings",
+    version=__version__,
+    author="Dan Drinkard",
+    author_email="dan.drinkard@gmail.com",
+    url="https://github.com/drinks/herokal-settings",
+    description="Make your django local_settings play nice with Heroku",
+    long_description=long_description,
+    license='BSD',
+    packages=find_packages(),
+    install_requires=['dj-database-url'],
+    platforms=['any']
+)
