0d67cf352179f728d6e3ba4e33c9c393399b06a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:27 +0000

    Add package easyirc/0.0.5/easyirc-0.0.5.tar.gz

---
+++ packages/easyirc/easyirc-0.0.5.tar.gz/easyirc-0.0.5/easyirc/client/bot.py
@@ -0,0 +1,11 @@
+
+from ..event import EventManager, bot
+from . import BasicClient
+
+class BotClient(BasicClient):
+    pass
+
+BotClient._events = _e = EventManager()
+_e.extends(BasicClient._events)
+_e.extends(bot.manager)
+_e.msgprefix = bot.msgprefix
