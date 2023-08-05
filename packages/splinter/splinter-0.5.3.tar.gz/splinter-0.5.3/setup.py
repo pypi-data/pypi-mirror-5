713898826e2c9dead353168c3bae307e096b59c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:59 +0000

    Add package splinter/0.5.3/splinter-0.5.3.tar.gz

---
+++ packages/splinter/splinter-0.5.3.tar.gz/splinter-0.5.3/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2012 splinter authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+from setuptools import setup, find_packages
+import codecs
+
+
+README = codecs.open('README.rst', encoding='utf-8').read()
+
+
+setup(
+    name='splinter',
+    version='0.5.3',
+    description='browser abstraction for web acceptance testing',
+    long_description=README,
+    author='CobraTeam',
+    author_email='andrewsmedina@gmail.com',
+    packages=find_packages(exclude=['docs', 'tests', 'samples']),
+    include_package_data=True,
+    install_requires=['selenium>=2.33.0'],
+    extras_require={'zope.testbrowser': ['zope.testbrowser>=4.0.2',
+                                         'lxml>=2.3.6', 'cssselect']},
+    tests_require=['coverage', 'flask'],
+)
