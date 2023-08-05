4352e5d25b01ec7dab40175a0e3e226116b074a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:49 +0000

    Add package redshift-sqlalchemy/0.2/redshift-sqlalchemy-0.2.tar.gz

---
+++ packages/redshift-sqlalchemy/redshift-sqlalchemy-0.2.tar.gz/redshift-sqlalchemy-0.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name='redshift-sqlalchemy',
+    version='0.2',
+    description='Amazon Redshift Dialect for sqlalchemy',
+    long_description=open("README.rst").read(),
+    author='Matt George',
+    author_email='mgeorge@gmail.com',
+    license="MIT",
+    url='https://github.com/binarydud/redshift_sqlalchemy',
+    packages=['redshift_sqlalchemy'],
+    install_requires=['psycopg2 >= 2.5'],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+    entry_points={
+        'sqlalchemy.dialects': [
+            'redshift = redshift_sqlalchemy.dialect:RedshiftDialect',
+            'redshift.psycopg2 = redshift_sqlalchemy.dialect:RedshiftDialect',
+        ]
+    }
+)
+
