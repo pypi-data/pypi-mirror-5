ac571e758bd4925a7ba0d354ef747c722c4706a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:35 +0000

    Add package sqlcc/0.3.1/sqlcc-0.3.1.tar.gz

---
+++ packages/sqlcc/sqlcc-0.3.1.tar.gz/sqlcc-0.3.1/__init__.py
@@ -0,0 +1,6 @@
+import funcs
+from sql import *
+from sql import _q
+from data_src import *
+
+rcsid = "$Id: __init__.py,v 1.4 2007/01/09 09:05:33 thinker Exp $"
---
+++ packages/sqlcc/sqlcc-0.3.1.tar.gz/sqlcc-0.3.1/funcs.py
@@ -0,0 +1,8 @@
+from sql import func_generic as generic
+
+class sum(generic):
+    pass
+
+class count(generic):
+    pass
+
