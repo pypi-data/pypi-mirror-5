3ac55a8d4475f5211b2fc309b8c95aff8901b8ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:50 +0000

    Add package companies/1.0.0/companies-1.0.0.zip

---
+++ packages/companies/companies-1.0.0.zip/companies-1.0.0/companies.py
@@ -0,0 +1,12 @@
+'''This Modules companies.py contains the list of top IT companies with their
+found year and founders'''
+def print_lol():
+    companies = ["Google",1996,"Larry Page and Sergey Brin"
+		,"Microsoft",1975,"Bill Gates and Paul Allen"
+		,"Apple",1976,"Steve Jobs,Steve Wozniak and Ronald Wayne"
+		,"Yahoo",1994,"Jerry Yang and David Filo"
+		,"Facebook",2004,"Mark Zuckerberg"
+		,"Oracle",1977,"Larry Ellison,Bob Miner and Ed Oates"
+		,"intel",1968,"Gordan E. Moore,Robert Noyce and Arthur Rock"]
+    for company in companies:
+            print(company)
---
+++ packages/companies/companies-1.0.0.zip/companies-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+        name        =   'companies',
+        version     =   '1.0.0',
+        py_modules  =   ['companies'],
+        author      =   'dharmjit',
+        author_email=   'dharmjit.bti@gmail.com',
+        description =   'simple printer of company list',
+      )
