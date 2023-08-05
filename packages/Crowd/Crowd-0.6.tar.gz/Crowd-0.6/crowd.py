13b0144cde41ed2a55dbd7d725e8ddc6f02eb83f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:29 +0000

    Add package Crowd/0.6/Crowd-0.6.tar.gz

---
+++ packages/Crowd/Crowd-0.6.tar.gz/Crowd-0.6/crowd.py
@@ -0,0 +1,314 @@
+# Copyright 2012 Alexander Else <aelse@else.id.au>.
+#
+# This file is part of the python-crowd library.
+#
+# python-crowd is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# python-crowd is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with python-crowd.  If not, see <http://www.gnu.org/licenses/>.
+
+import json
+import requests
+from urllib import urlencode
+
+
+class CrowdServer(object):
+    """Crowd server authentication object.
+
+    This is a Crowd authentication class to be configured for a
+    particular application (app_name) to authenticate users
+    against a Crowd server (crowd_url).
+
+    This module uses the Crowd JSON API for talking to Crowd.
+
+    An application account must be configured in the Crowd server
+    and permitted to authenticate users against one or more user
+    directories prior to using this module.
+
+    Please see the Crowd documentation for information about
+    configuring additional applications to talk to Crowd.
+    """
+
+    def __init__(self, crowd_url, app_name, app_pass):
+        self.crowd_url = crowd_url
+        self.app_name = app_name
+        self.app_pass = app_pass
+        self.rest_url = crowd_url.rstrip("/") + "/rest/usermanagement/1"
+
+        self.auth_info = requests.auth.HTTPBasicAuth(app_name, app_pass)
+        self.request_headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        }
+
+    def __str__(self):
+        return "Crowd Server at %s" % self.crowd_url
+
+    def __repr__(self):
+        return "<CrowdServer('%s', '%s', %s')>" % (self.crowd_url, self.app_name, self.app_pass)
+
+    def _get(self, url):
+        req = requests.get(url, auth=self.auth_info,
+            headers=self.request_headers)
+        return req
+
+    def _post(self, url, post_data):
+        req = requests.post(url, data=json.dumps(post_data), auth=self.auth_info,
+            headers=self.request_headers)
+        return req
+
+    def _delete(self, url):
+        req = requests.delete(url, auth=self.auth_info, headers=self.request_headers)
+        return req
+
+    def auth_ping(self):
+        """Test that application can authenticate to Crowd.
+
+        Attempts to authentication the application user against
+        the Crowd server. In order for user authentication to
+        work, an application must be able to authenticate.
+
+        Returns:
+            bool:
+                True if the application authentication succeeded.
+        """
+
+        url = self.rest_url + "/non-existent/location"
+        response = self._get(url)
+
+        if response.status_code == 401:
+            # and response.text.startswith("Application failed to authenticate"):
+            return False
+        elif response.status_code == 404:
+            return True
+        else:
+            # An error encountered - problem with the Crowd server?
+            return False
+
+    def auth_user(self, username, password):
+        """Authenticate a user account against the Crowd server.
+
+        Attempts to authenticate the user against the Crowd server.
+
+        Args:
+            username: The account username.
+
+            password: The account password.
+
+        Returns:
+            dict:
+                A dict mapping of user attributes if the application
+                authentication was successful. See the Crowd documentation
+                for the authoritative list of attributes.
+
+            None: If authentication failed.
+        """
+
+        url = self.rest_url + "/authentication?%s" % urlencode(
+            {"username": username})
+        response = self._post(url, {"value": password})
+
+        # If authentication failed for any reason return None
+        if not response.ok:
+            return None
+
+        # ...otherwise return a dictionary of user attributes
+        return json.loads(response.text)
+
+    def get_session(self, username, password, remote="127.0.0.1"):
+        """Create a session for a user.
+
+        Attempts to create a user session on the Crowd server.
+
+        Args:
+            username: The account username.
+
+            password: The account password.
+
+            remote:
+                The remote address of the user. This can be used
+                to create multiple concurrent sessions for a user.
+                The host you run this program may need to be configured
+                in Crowd as a trusted proxy for this to work.
+
+        Returns:
+            dict:
+                A dict mapping of user attributes if the application
+                authentication was successful. See the Crowd
+                documentation for the authoritative list of attributes.
+
+            None: If authentication failed.
+        """
+
+        params = {
+            "username": username,
+            "password": password,
+            "validation-factors": {
+                "validationFactors": [
+                    {"name": "remote_address", "value": remote, }
+                ]
+            }
+        }
+
+        url = self.rest_url + "/session?expand=user"
+        response = self._post(url, params)
+
+        # If authentication failed for any reason return None
+        if not response.ok:
+            return None
+
+        # Otherwise return the user object
+        ob = json.loads(response.text)
+        return ob
+
+    def validate_session(self, token, remote="127.0.0.1"):
+        """Validate a session token.
+
+        Validate a previously acquired session token against the
+        Crowd server. This may be a token provided by a user from
+        a http cookie or by some other means.
+
+        Args:
+            token: The session token.
+
+            remote: The remote address of the user.
+
+        Returns:
+            dict:
+                A dict mapping of user attributes if the application
+                authentication was successful. See the Crowd
+                documentation for the authoritative list of attributes.
+
+            None: If authentication failed.
+        """
+
+        params = {
+           "validationFactors": [
+              {"name": "remote_address", "value": remote, }
+           ]
+        }
+
+        url = self.rest_url + "/session/%s?expand=user" % token
+        response = self._post(url, params)
+
+        # For consistency between methods use None rather than False
+        # If token validation failed for any reason return None
+        if not response.ok:
+            return None
+
+        # Otherwise return the user object
+        ob = json.loads(response.text)
+        return ob
+
+    def terminate_session(self, token):
+        """Terminates the session token, effectively logging out the user
+        from all crowd-enabled services.
+
+        Args:
+            token: The session token.
+
+        Returns:
+            True: If session terminated
+
+            None: If session termination failed
+        """
+
+        url = self.rest_url + "/session/%s" % token
+        response = self._delete(url)
+
+        # For consistency between methods use None rather than False
+        # If token validation failed for any reason return None
+        if not response.ok:
+            return None
+
+        # Otherwise return True
+        return True
+
+    def get_groups(self, username):
+        """Retrieves a list of group names that have <username> as a direct member.
+
+        Returns:
+            list:
+                A list of strings of group names.
+        """
+
+        url = self.rest_url + "/user/group/direct?%s" % urlencode(
+            {"username": username})
+        response = self._get(url)
+
+        if not response.ok:
+            return None
+
+        return [g['name'] for g in json.loads(response.text)['groups']]
+
+    def get_nested_groups(self, username):
+        """Retrieve a list of all group names that have <username> as a direct or indirect member.
+
+        Args:
+            username: The account username.
+
+
+        Returns:
+            list:
+                A list of strings of group names.
+        """
+
+        url = self.rest_url + "/user/group/nested?%s" % urlencode(
+            {"username": username})
+        response = self._get(url)
+
+        if not response.ok:
+            return None
+
+        return [g['name'] for g in json.loads(response.text)['groups']]
+
+    def get_nested_group_users(self, groupname):
+        """Retrieves a list of all users that directly or indirectly belong to the given groupname.
+
+        Args:
+            groupname: The group name.
+
+
+        Returns:
+            list:
+                A list of strings of user names.
+        """
+
+        url = self.rest_url + "/group/user/nested?%s%s" % (urlencode(
+            {"groupname": groupname}), "&start-index=0&max-results=99999")
+        response = self._get(url)
+
+        if not response.ok:
+            return None
+
+        return [u['name'] for u in json.loads(response.text)['users']]
+
+
+    def user_exists(self, username):
+        """Determines if the user exists.
+
+        Args:
+            username: The user name.
+
+
+        Returns:
+            bool:
+                True if the user exists in the Crowd application.
+        """
+
+        url = self.rest_url + "/user?%s" % urlencode(
+            {"username": username})
+        response = self._get(url)
+
+        if not response.ok:
+            return None
+
+        return True
