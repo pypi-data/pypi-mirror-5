ec0ef5c42bce4affef853231fd8baabce64738b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:31 +0000

    Add package leap.common/0.2.4/leap.common-0.2.4.tar.gz

---
+++ packages/leap.common/leap.common-0.2.4.tar.gz/leap.common-0.2.4/setup.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+# setup.py
+# Copyright (C) 2013 LEAP
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+setup file for leap.common
+"""
+from setuptools import setup, find_packages
+
+# XXX parse pkg/requirements.pip
+requirements = [
+    "jsonschema",
+    "pyxdg",
+    'protobuf',
+    'protobuf.socketrpc',
+    "PyOpenSSL",
+    "python-dateutil",
+    "leap.soledad",
+    "python-gnupg",
+    "PyCrypto",
+]
+
+
+dependency_links = [
+    "https://protobuf-socket-rpc.googlecode.com/files/protobuf.socketrpc-1.3.2.tar.gz#egg=protobuf.socketrpc"
+]
+
+tests_requirements = [
+    'mock',
+]
+
+
+# XXX add classifiers, docs
+
+setup(
+    name='leap.common',
+    # If you change version, do it also in
+    # src/leap/common/__init__.py
+    version='0.2.4',
+    url='https://leap.se/',
+    license='GPLv3+',
+    author='The LEAP Encryption Access Project',
+    author_email='info@leap.se',
+    description='Common files used by the LEAP Client project.',
+    long_description=(
+        "Common files used by the LEAP Client project."
+    ),
+    namespace_packages=["leap"],
+    package_dir={'': 'src'},
+    # For now, we do not exclude tests because of the circular dependency
+    # between leap.common and leap.soledad.
+    #packages=find_packages('src', exclude=['leap.common.tests']),
+    packages=find_packages('src'),
+    test_suite='leap.common.tests',
+    install_requires=requirements,
+    dependency_links=dependency_links,
+    tests_require=tests_requirements,
+)
