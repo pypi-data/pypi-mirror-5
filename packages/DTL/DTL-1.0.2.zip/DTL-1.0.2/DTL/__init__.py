53b19391676f928aa5200a2df2245cf4815536d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:24 +0000

    Add package DTL/1.0.2/DTL-1.0.2.zip

---
+++ packages/DTL/DTL-1.0.2.zip/DTL-1.0.2/DTL/__init__.py
@@ -0,0 +1,33 @@
+"""
+Dev Tools Library (DTL) multiplatform, multiapplication tools suite.
+"""
+import os
+import sys
+import getpass
+
+__version__ = '1.0.2'
+__company__ = 'Cloud Imperium'
+__user__ = getpass.getuser()
+__authors__ = ['Kyle Rockman', 'John Crocker']
+__docformat__ = 'restructuredtext en'
+__pkgdir__ = os.path.dirname(sys.argv[0])
+__pkgname__ = os.path.basename(__pkgdir__)
+__pkgresources__ = os.path.join(__pkgdir__, 'resources')
+__installers__ = os.path.join(__pkgdir__,'installers')
+
+if sys.platform == 'win32' :
+    __appdata__ = os.path.join(os.environ['APPDATA'], __company__)
+else:
+    __appdata__ = os.path.join(os.environ['HOME'], 'Documents', __company__)
+    
+try:
+    os.makedirs(__appdata__,0777)
+except:
+    pass
+
+def cleanup():
+    from DTL.api.path import Path
+    local_path = Path(__file__).parent
+    for file_path in local_path.walk('*.pyc') :
+        file_path.remove()
+
---
+++ packages/DTL/DTL-1.0.2.zip/DTL-1.0.2/DTL/gui/__init__.py
@@ -0,0 +1,12 @@
+#Validate PyQt4
+from PyQt4 import QtCore, QtGui, uic
+
+from core import Core
+
+import guiUtils
+
+from window import Window
+from dialog import Dialog
+from widget import Widget
+from wizard import Wizard, WizardPage
+
