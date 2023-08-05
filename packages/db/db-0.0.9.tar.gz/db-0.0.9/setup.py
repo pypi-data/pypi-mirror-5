dc3417a7a885f123e7391a59743eaaa77d347e4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:04 +0000

    Add package db/0.0.9/db-0.0.9.tar.gz

---
+++ packages/db/db-0.0.9.tar.gz/db-0.0.9/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+from setuptools import find_packages
+
+
+if __name__ == "__main__":
+    setup(name="db",
+          version="0.0.9",
+          description="Databases for Humans",
+          author="John Evans",
+          author_email="lgastako@gmail.com",
+          license="MIT",
+          url="https://github.com/lgastako/db",
+          install_requires=["antiorm"],
+          packages=find_packages(),
+          provides=["db"])
