1d1f75e776591b4c7299f150ee6f85965715f8d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:27 +0000

    Add package RapidSMS/0.15.0/RapidSMS-0.15.0.tar.gz

---
+++ packages/RapidSMS/RapidSMS-0.15.0.tar.gz/RapidSMS-0.15.0/rapidsms/contrib/messagelog/tables.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4
+
+import django_tables2 as tables
+from rapidsms.contrib.messagelog.models import Message
+
+
+class MessageTable(tables.Table):
+
+    class Meta:
+        model = Message
+        exclude = ('id', )
+        order_by = ('-date', )
