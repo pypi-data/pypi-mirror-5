bbd9553274ecaafdcc2ef504c8ed9cbf504ebca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:54 +0000

    Add package Products.PloneboardSubscription/0.9/Products.PloneboardSubscription-0.9.zip

---
+++ packages/Products.PloneboardSubscription/Products.PloneboardSubscription-0.9.zip/Products.PloneboardSubscription-0.9/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.9'
+
+tests_require = [ 'collective.MockMailHost' ]
+
+setup(name='Products.PloneboardSubscription',
+      version=version,
+      description="Enables email subscription to Ploneboard posts",
+      long_description=open("README.rst").read() + "\n\n\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Suresh V',
+      author_email='suresh@grafware.com',
+      url='http://plone.org/products/products.ploneboardsubscription',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      tests_require = tests_require,
+      extras_require={'test': tests_require},
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
