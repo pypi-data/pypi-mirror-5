8387c4f4defc83ea5093a7ac9a40a96d9b845479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:21 +0000

    Add package buildout.minitagificator/2.4.29/buildout.minitagificator-2.4.29.zip

---
+++ packages/buildout.minitagificator/buildout.minitagificator-2.4.29.zip/buildout.minitagificator-2.4.29/README.txt
@@ -0,0 +1,54 @@
+
+.. contents::
+
+Introduction
+============
+
+
+    - This packages aims to replace some buildout parts by minitage's ones.
+    - It replaces:
+
+        - All zc.recipe.egg recipes but zc.recipe.custom:develop
+        - zc.buildout.easy_install.install.Installer
+        - zc.recipe.cmmi
+
+
+Installation
+=============
+
+    - just add *buildout.minitagificator* to your buildout 's extension variable like ::
+
+        [buildout]
+        extensions = buildout.minitagificator
+
+Makina Corpus sponsored software
+======================================
+|makinacom|_
+
+* `Planet Makina Corpus <http://www.makina-corpus.org>`_
+* `Contact us <mailto:python@makina-corpus.org>`_
+
+.. |makinacom| image:: http://depot.makina-corpus.org/public/logo.gif
+.. _makinacom:  http://www.makina-corpus.com
+
+
+TIPS
+========
+
+    -   set ``minitage-globalenv`` in buildout for the extension to set cflags
+        and other things borrowed from the project minibuild if any::
+
+        [minitagificator]
+        minitage-globalenv = true
+
+
+    - it will look for a section ``[minitagificator]`` to have regular options like
+      extra-flags, path or includes to add to the environment (see
+      minitage.recipe.common)::
+
+        [minitagificator]
+        cflags = -I/foo/bar
+        path = /foo/baz/bin
+
+
+
---
+++ packages/buildout.minitagificator/buildout.minitagificator-2.4.29.zip/buildout.minitagificator-2.4.29/setup.py
@@ -0,0 +1,53 @@
+from setuptools import setup, find_packages
+import os
+
+version = '2.4.29'
+
+entry_point = 'buildout.minitagificator.minitagificator:install'
+entry_points = {"zc.buildout.extension": ["default = %s" % entry_point]}
+
+setup(name='buildout.minitagificator',
+      version=version,
+      description="A buildout extension to replace buildout part by minitage interesant ones sponsored by Makina Corpus",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("src", "buildout", "minitagificator", "minitagificator.txt")).read()+"\n"+
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          "Framework :: Buildout",
+          "Framework :: Plone",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+      keywords='',
+      author='Mathieu Pasquet',
+      author_email='kiorky@cryptelium.net',
+      url='https://github.com/minitage/buildout.minitagificator',
+      license='GPL',
+      package_dir = {'': 'src'},
+      packages = find_packages('src'),
+      namespace_packages=['buildout', 'buildout.minitagificator'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'zc.buildout',
+          'minitage.recipe.egg >= 1.88',
+          'minitage.recipe.cmmi',
+          'minitage.recipe.scripts',
+          'minitage.recipe.common',
+          # -*- Extra requirements: -*-
+      ],
+
+      extras_require={'test': ['virtualenv',
+                               'ZopeSkel',
+                               'IPython',
+                               'zope.testing',
+                               'mocker',
+                               'zc.recipe.egg',
+                               'zc.recipe.cmmi'
+                              ]
+                     },
+      entry_points= entry_points,
+      )
+
