65390f17132d47541c7450e99ac8beb650b369ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:00 +0000

    Add package collective.recipe.solrinstance/5.0/collective.recipe.solrinstance-5.0.zip

---
+++ packages/collective.recipe.solrinstance/collective.recipe.solrinstance-5.0.zip/collective.recipe.solrinstance-5.0/collective/recipe/solrinstance/tests/test_doctests.py
@@ -0,0 +1,21 @@
+from doctest import ELLIPSIS, NORMALIZE_WHITESPACE, REPORT_UDIFF
+from doctest import DocFileSuite
+
+from zc.buildout.testing import buildoutSetUp, buildoutTearDown
+from zc.buildout.testing import install_develop
+
+
+def setUp(test):
+    buildoutSetUp(test)
+    install_develop('zope.exceptions', test)
+    install_develop('zope.interface', test)
+    install_develop('zope.testing', test)
+    install_develop('genshi', test)
+    install_develop('collective.recipe.solrinstance', test)
+
+
+def test_suite():
+    return DocFileSuite(
+           '../README.txt',
+           setUp=setUp, tearDown=buildoutTearDown,
+           optionflags=ELLIPSIS | NORMALIZE_WHITESPACE | REPORT_UDIFF)
