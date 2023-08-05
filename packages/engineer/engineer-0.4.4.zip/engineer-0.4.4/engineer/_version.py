18bbe15f842137817bd99ba721ba23ed5f24be9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:00 +0000

    Add package engineer/0.4.4/engineer-0.4.4.zip

---
+++ packages/engineer/engineer-0.4.4.zip/engineer-0.4.4/engineer/_version.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+import time
+from datetime import date
+from propane_distribution import version_class
+
+# This file is originally generated from Git information by running 'setup.py
+# version'. Distributions contain a pre-generated copy of this file.
+
+__version__ = '0.4.4'
+__date__ = date(2013, 6, 23)
+__time__ = time.gmtime(1372036741.83)
+
+version = version_class(__version__, __date__, __time__)
