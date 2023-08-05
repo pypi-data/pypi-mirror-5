38f632781a3aec0cdc198e8621d4def6bcfd5f06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:59 +0000

    Add package dstufft.testpkg3/5.0/dstufft.testpkg3-5.0.tar.gz

---
+++ packages/dstufft.testpkg3/dstufft.testpkg3-5.0.tar.gz/dstufft.testpkg3-5.0/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+
+setuptools.setup(
+    name="dstufft.testpkg3",
+    version="5.0",
+    description="A package to test packaging with",
+    download_url="https://s3.amazonaws.com/dstufft/packaging/dstufft.testpkg3/index.html",
+)
