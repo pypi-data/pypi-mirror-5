9eacec54d0cee684251779190c56555fcb76131d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:11 +0000

    Add package django-sape/0.2.2/django-sape-0.2.2.tar.gz

---
+++ packages/django-sape/django-sape-0.2.2.tar.gz/django-sape-0.2.2/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(name='django-sape',
+      description='Django + sape.ru.',
+      version='0.2.2',
+      packages=['sape'],
+      author='Dimmma',
+      author_email='https://github.com/Dimmma',
+      maintainer='Mikhail Fedosov',
+      maintainer_email='tbs.micle@gmail.com',
+      url='http://github.com/fedosov/sape',
+      keywords=['Django', 'sape', 'sape.ru', ],
+      #install_requires=['mechanize']
+      classifiers=
+      [
+	      'Development Status :: 4 - Beta',
+	      'Environment :: Console',
+	      'Environment :: Web Environment',
+	      'Intended Audience :: Developers',
+	      'License :: OSI Approved :: Python Software Foundation License',
+	      'Operating System :: MacOS :: MacOS X',
+	      'Operating System :: Microsoft :: Windows',
+	      'Operating System :: POSIX',
+	      'Programming Language :: Python',
+      ],
+)
