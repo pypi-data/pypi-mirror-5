8c0e4a9aecc7560faea81b23396ae2617ca35744
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:24 +0000

    Add package Kuyruk/0.14.0/Kuyruk-0.14.0.tar.gz

---
+++ packages/Kuyruk/Kuyruk-0.14.0.tar.gz/Kuyruk-0.14.0/kuyruk/__init__.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import
+import logging
+
+from kuyruk.kuyruk import Kuyruk
+from kuyruk.worker import Worker
+from kuyruk.task import Task
+from kuyruk.config import Config
+
+__version__ = '0.14.0'
+
+try:
+    # not available in python 2.6
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+# Add NullHandler to prevent logging warnings on startup
+null_handler = NullHandler()
+logging.getLogger('kuyruk').addHandler(null_handler)
+logging.getLogger('pika').addHandler(null_handler)
