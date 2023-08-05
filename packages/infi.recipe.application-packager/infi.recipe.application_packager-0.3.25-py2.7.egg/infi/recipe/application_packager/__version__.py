1475644e9bff6bc5a17ef35e91bbb3f8eecf3b1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:48 +0000

    Add package infi.recipe.application-packager/0.3.25/infi.recipe.application_packager-0.3.25-py2.7.egg

---
+++ packages/infi.recipe.application-packager/infi.recipe.application_packager-0.3.25-py2.7.egg/infi/recipe/application_packager/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.3.25"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.3.25)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '6adbb3ac8a8c53b7e4d4393fbd1dea11d362560b'
+__git_head_subject__ = 'HOSTDEV-1099 adding prefer-final to buildout.in'
+__git_head_message__ = '================================================================================\n\nHost Infrastructure / HOSTDEV-1099\nadd prefer-final to buildout.cfg\n\nType:              Task                              Status:        Open                       Assignee: Guy Rozendorn\nPriority:          Minor                             Resolution:    Unresolved                 Reporter: Guy Rozendorn\nAffects Version/s:                                   Fix Version/s: 0.30\nComponents: packaging                                                                          Created: 2013-07-10 15:46\nLabels:                                                                                        Updated: 2013-07-10 15:46\n\nIssue Links:\n\nSub-Tasks:\n\nDescription:\nNone\n\nComments:'
+__git_dirty_diff__ = ''
