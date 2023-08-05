32765ac7741cac82623f1d95cbe9d9ccd386b2d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:19 +0000

    Add package ugly/0.0.2/ugly-0.0.2.tar.gz

---
+++ packages/ugly/ugly-0.0.2.tar.gz/ugly-0.0.2/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import os
+import sys
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist upload")
+    sys.exit()
+
+setup(
+    name="ugly",
+    version="0.0.2",
+    description="Fucking ugly RSS",
+    long_description=open("README.rst").read(),
+    author="Dan F-M",
+    author_email="danfm@nyu.edu",
+    url="https://github.com/dfm/ugly",
+    packages=["ugly"],
+    scripts=["bin/ugly"],
+    install_requires=open("requirements.txt").read().splitlines(),
+    license="MIT",
+    classifiers=(
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.3",
+    ),
+)
