abdfdf0fb861dbcaa5dcaec92e819d8c95b2b6f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:57 +0000

    Add package catsnap/4.1.0/catsnap-4.1.0.tar.gz

---
+++ packages/catsnap/catsnap-4.1.0.tar.gz/catsnap-4.1.0/catsnap/config/base.py
@@ -0,0 +1,15 @@
+from __future__ import unicode_literals
+
+class Config(object):
+    CREDENTIAL_SETTINGS = ('aws_access_key_id', 'aws_secret_access_key')
+    ALL_SETTINGS = CREDENTIAL_SETTINGS + ('bucket',
+                                          'extension',
+                                          'owner_id',
+                                          'owner_email',
+                                          'api_host',
+                                          'cloudfront_url',
+                                          'api_key')
+    CLIENT_SETTINGS = ('extension', 'api_host', 'api_key')
+
+    def __getitem__(self, item): raise NotImplementedError
+    def __contains__(self, item): raise NotImplementedError
