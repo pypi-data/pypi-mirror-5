87d08929161d1104fb709b1c11eeee44ce773f48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:27 +0000

    Add package raptus.article.core/2.0b15/raptus.article.core-2.0b15.zip

---
+++ packages/raptus.article.core/raptus.article.core-2.0b15.zip/raptus.article.core-2.0b15/raptus/article/core/browser/view.py
@@ -0,0 +1,17 @@
+from zope.interface import implements
+
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+from raptus.article.core import interfaces
+
+
+class View(BrowserView):
+    """View of an article
+    """
+    implements(interfaces.IArticleView)
+    template = ViewPageTemplateFile('view.pt')
+
+    def __call__(self):
+        self.request.set('raptus_article_viewing', True)
+        return self.template()
