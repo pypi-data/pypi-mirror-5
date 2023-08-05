643bdd21670ce3e024f92fd6dd5bf568b08468d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:38 +0000

    Add package modules/1.0.0/modules-1.0.0.tar.gz

---
+++ packages/modules/modules-1.0.0.tar.gz/modules-1.0.0/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+
+
+name = 'modules',
+version = '1.0.0',
+py_modules = ['modules'],
+author = 'rakesh',
+author_email = 'rakeshnitcalicut@gmail.com',
+url = 'www.facebook.com/rakeshbubli',
+description = 'Simple test on modules',
+ 
+
+
+
+
+
+)
