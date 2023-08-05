c96643057dc460db5cdccd0c79d059dfe4533c42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:20 +0000

    Add package wger/1.2/wger-1.2.tar.gz

---
+++ packages/wger/wger-1.2.tar.gz/wger-1.2/README.rst
@@ -0,0 +1,92 @@
+Thank you for downloading wger Workout Manager. Workout Manager is a free, open
+source web application that manages your exercises, workouts, weight and diet
+plans.
+
+For more details and a live system, refer to the project's site: http://wger.de/
+
+
+Installation
+============
+
+These are the basic steps to get the application running. There are more
+detailed installation instructions on http://wger.de/en/software/code
+
+1) Install the necessary packages and their dependencies
+
+::
+
+ $ sudo apt-get install python-virtualenv
+ $ sudo apt-get install python-dev
+ $ virtualenv python-django
+ $ source python-django/bin/activate
+ $ pip install wger
+
+
+2) Start the application. This will create a SQlite database and populate it
+with data on the first run
+
+::
+
+ $ wger
+
+
+3) Log in as: **admin**, password **admin**
+
+
+Command line options
+--------------------
+
+The available options for the ``wger`` command are ::
+
+ Usage: main.py [options]
+
+ Run wger Workout Manager using django's builtin server
+
+ Options:
+  -h, --help            show this help message and exit
+  -a ADDRESS, --address=ADDRESS
+                        IP Address to listen on.
+  -p PORT, --port=PORT  Port to listen on.
+  --syncdb              Update/create database before starting the server.
+  --reset-admin         Make sure the user 'admin' exists and uses 'admin' as
+                        password.
+  -s SETTINGS, --settings=SETTINGS
+                        Path to the wger configuration file.
+  --no-reload           Do not reload the development server.
+  --version             Show version and exit.
+  --show-config         Show configuration paths and exit.
+
+Contact
+=======
+
+Feel free to write to the mailing list (https://groups.google.com/group/wger),
+to twitter (https://twitter.com/wger_de) or to me directly
+(``roland @ geider.net``) if you found this useful or if there was something
+that didn't behave as you expected. Alternatively, you can also open a ticket
+on the issue tracker:
+https://github.com/rolandgeider/wger/issues
+
+
+Sources
+=======
+
+All the code and the content is freely available:
+
+* **Main repository**: GIT, https://github.com/rolandgeider/wger
+* **Mirror**: HG, https://bitbucket.org/rolandgeider/wger
+
+
+Licence
+=======
+
+The application is licenced under the Affero GNU General Public License 3 or later
+(AGPL 3+).
+
+The initial exercise and ingredient data is licensed additionally under a
+Creative Commons Attribution Share-Alike 3.0 (CC-BY-SA 3.0)
+
+The YAML CSS framework is licensed under a Creative Commons Attribution 2.0
+License (CC-BY 2.0)
+
+Some images where taken from Wikipedia, see the SOURCES file in their respective
+folders for more details.
---
+++ packages/wger/wger-1.2.tar.gz/wger-1.2/wger/exercises/urls.py
@@ -0,0 +1,72 @@
+from django.conf.urls import patterns, url
+from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.decorators import login_required
+
+from wger.exercises.views import exercises
+from wger.exercises.views import comments
+from wger.exercises.views import categories
+from wger.exercises.views import muscles
+
+urlpatterns = patterns('wger.exercises.views',
+
+    # Exercises
+    url(r'^overview/$', 'exercises.overview'),
+
+    url(r'^search/$', 'exercises.search'),
+    url(r'^(?P<id>\d+)/view/(?P<slug>[-\w]+)/$', 'exercises.view'),
+    url(r'^(?P<id>\d+)/view/$', 'exercises.view'),
+    url(r'^add/$',
+        login_required(exercises.ExerciseAddView.as_view()),
+        name='exercise-add'),
+    url(r'^(?P<pk>\d+)/edit/$',
+        permission_required('exercises.add_exercise')(exercises.ExerciseUpdateView.as_view()),
+        name='exercise-edit'),
+    url(r'^(?P<pk>\d+)/delete/$',
+        permission_required('exercises.delete_exercise')(exercises.ExerciseDeleteView.as_view()),
+        name='exercise-delete'),
+    url(r'^pending/$',
+        permission_required('exercises.change_exercise')(exercises.PendingExerciseListView.as_view()),
+        name='exercise-pending'),
+    url(r'^(?P<pk>\d+)/accept/$',
+        'exercises.accept',
+        name='exercise-accept'),
+    url(r'^(?P<pk>\d+)/decline/$',
+        'exercises.decline',
+        name='exercise-decline'),
+
+    # Muscles
+    url(r'^muscle/overview/$',
+        muscles.MuscleListView.as_view(),
+        name='muscle-overview'),
+    url(r'^muscle/add/$',
+        permission_required('exercises.add_muscle')(muscles.MuscleAddView.as_view()),
+        name='muscle-add'),
+    url(r'^muscle/(?P<pk>\d+)/edit/$',
+        permission_required('exercises.change_muscle')(muscles.MuscleUpdateView.as_view()),
+        name='muscle-edit'),
+    url(r'^muscle/(?P<pk>\d+)/delete/$',
+        permission_required('exercises.delete_muscle')(muscles.MuscleDeleteView.as_view()),
+        name='muscle-delete'),
+
+    # Comments
+    url(r'^(?P<exercise_pk>\d+)/comment/add/$',
+        permission_required('exercises.add_exercisecomment')(comments.ExerciseCommentAddView.as_view()),
+        name='exercisecomment-add'),
+    url(r'^comment/(?P<pk>\d+)/edit/$',
+        permission_required('exercises.change_exercisecomment')(comments.ExerciseCommentEditView.as_view()),
+        name='exercisecomment-edit'),
+    url(r'^comment/(?P<id>\d+)/delete/$',
+        'comments.delete',
+        name='exercisecomment-delete'),
+
+    # Categories
+    url(r'^category/(?P<pk>\d+)/edit/$',
+        permission_required('exercises.change_exercisecategory')(categories.ExerciseCategoryUpdateView.as_view()),
+        name='exercisecategory-edit'),
+    url(r'^category/add/$',
+        permission_required('exercises.add_exercisecategory')(categories.ExerciseCategoryAddView.as_view()),
+        name='exercisecategory-add'),
+    url(r'^category/(?P<pk>\d+)/delete/$',
+        permission_required('exercises.delete_exercisecategory')(categories.ExerciseCategoryDeleteView.as_view()),
+        name='exercisecategory-delete'),
+)
