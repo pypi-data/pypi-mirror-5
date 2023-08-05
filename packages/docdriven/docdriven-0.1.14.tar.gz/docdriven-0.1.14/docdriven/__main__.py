5edceddca7c1aebbb652d2e1b687fcc461985e07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:45 +0000

    Add package docdriven/0.1.14/docdriven-0.1.14.tar.gz

---
+++ packages/docdriven/docdriven-0.1.14.tar.gz/docdriven-0.1.14/docdriven/__main__.py
@@ -0,0 +1,78 @@
+import interface
+import testcase
+import unittest
+import os
+import logging
+import sys
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+root = logging.getLogger()
+
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)25s - %(levelname)7s - %(message)s',"%m-%d %H:%M")
+ch.setFormatter(formatter)
+root.addHandler(ch)
+
+def drive_dd_file(dd_file_full_path):
+    docdriven_interface = interface.get_docdriven_interface(dd_file_full_path, debug_output='file_line_feed')
+    test_case = testcase.get_docdriven_testcase(docdriven_interface)
+    test_suite = unittest.TestLoader().loadTestsFromTestCase(test_case)
+    log.debug('running unittest')
+    text_test_runner = unittest.TextTestRunner(stream=sys.stdout, descriptions=True, verbosity=2)
+    test_suite_result = text_test_runner.run(test_suite)
+    print test_suite_result
+    log.debug('printing to files')
+    docdriven_interface.print_to_files(dd_all_response=True)
+
+
+
+
+def read_driven_docs_file(root_dir):
+    log.debug('root_dir: '+root_dir)
+    dd_filename = os.path.join(root_dir, 'DRIVEN_DOCS.txt')
+    # try:
+    log.debug('trying to open: '+dd_filename)
+    with open(dd_filename) as driven_docs_file:
+        log.debug('looping through lines in: '+dd_filename)
+        for line in driven_docs_file:
+            filepath = line.strip()
+            if filepath.startswith('!'): continue # skip commented-out lines
+            if len(filepath) < 1: continue # skip blank lines
+            log.debug('this line should be a filepath: '+filepath)
+            if os.path.isdir(filepath):
+                log.debug('found directory filepath - '+filepath)
+                read_driven_docs_file(filepath)
+            else:
+                head, tail = os.path.split(filepath)
+                if head is None or len(head)<1:
+                    log.debug('no root_dir in the filepath - using root_dir: '+root_dir)
+                    full_filepath = os.path.join(root_dir, tail)
+                else:
+                    full_filepath = filepath
+                log.debug('found normal full_filepath; initializing interface: '+full_filepath)
+                drive_dd_file(full_filepath)
+                # log.debug('got down here')
+    # except IOError:
+    #     raise IO
+    #     # this means we could not successfully open a DRIVEN_DOCS.txt file in that directory
+    #     files = os.listdir(root_dir)
+    #     log.debug('DRIVEN_DOCS.txt file not located in '+root_dir)
+    #     log.debug('looking for *.dd files in '+root_dir)
+    #     for f in files:
+    #         if os.path.isdir(f):
+    #             continue
+    #         else:
+    #             if f.endswith('.dd'):
+    #                 log.debug('found '+str(f))
+    #                 drive_dd_file(f)
+
+
+dd_home = os.environ.get('DOC_DRIVEN_HOME')
+if not dd_home:
+    dd_home = os.path.dirname(os.path.abspath(__file__))
+if not dd_home:
+    dd_home = os.environ.get('HOME')
+read_driven_docs_file(dd_home)
+
