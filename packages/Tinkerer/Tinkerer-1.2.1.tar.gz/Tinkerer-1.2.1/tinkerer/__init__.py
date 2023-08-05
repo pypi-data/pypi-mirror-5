e3d9bbfd870b34315da68945a91ada82970aedf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:39 +0000

    Add package Tinkerer/1.2.1/Tinkerer-1.2.1.tar.gz

---
+++ packages/Tinkerer/Tinkerer-1.2.1.tar.gz/Tinkerer-1.2.1/tinkerer/__init__.py
@@ -0,0 +1,14 @@
+'''
+    Tinkerer Blog
+    ~~~~~~~~~~~~~
+
+    Blogging with Sphinx. Like a boss.
+
+    :copyright: Copyright 2011-2013 by Vlad Riscutia and contributors (see
+    CONTRIBUTORS file)
+    :license: FreeBSD, see LICENSE file
+'''
+__version__ = "1.2.1"
+
+master_doc = "master"
+source_suffix = ".rst"
---
+++ packages/Tinkerer/Tinkerer-1.2.1.tar.gz/Tinkerer-1.2.1/tinkerer/utils.py
@@ -0,0 +1,54 @@
+'''
+    utils
+    ~~~~~
+
+    Tinkerer utility functions.
+
+    :copyright: Copyright 2011-2013 by Vlad Riscutia and contributors (see
+    CONTRIBUTORS file)
+    :license: FreeBSD, see LICENSE file
+'''
+import datetime
+import os
+import re
+
+
+
+def name_from_title(title):
+    '''
+    Returns a doc name from a title by replacing all characters which are not
+    alphanumeric or '_' with '_'.
+    '''
+    return re.sub(r"[\W_]", "_", title).lower()
+
+
+
+def name_from_path(path):
+    '''
+    Returns a doc name from a path by extracting the filename without
+    extension.
+    '''
+    return os.path.splitext(os.path.basename(path))[0]
+
+
+
+def get_path(*args):
+    '''
+    Creates a path if it doesn't already exist.
+    '''
+    path = os.path.join(*args)
+    if not os.path.exists(path):
+        os.makedirs(path)
+    return path
+
+
+
+def split_date(date=None):
+    '''
+    Splits a date into formatted year, month and day strings. If not date is
+    provided, current date is used.
+    '''
+    if not date:
+        date = datetime.datetime.today()
+
+    return date.strftime("%Y/%m/%d").split("/")
