6ba43b607076885f210d8f30f2f3db8c1f0ae105
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:52 +0000

    Add package sdict/0.11/sdict-0.11.zip

---
+++ packages/sdict/sdict-0.11.zip/sdict-0.11/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup,Extension
+import sys,os
+
+D = os.path.dirname(__file__)
+
+tst_module = Extension('_tst',
+    sources=[
+        os.path.join(D,'sdict/tst_wrap.cpp'),
+        os.path.join(D,'sdict/tst.cpp')
+    ]
+)
+
+setup(name='sdict',
+	version='0.11',
+	author='Sun, Junyi',
+	description='Sorted Dictionary Implementation',
+	ext_modules=[tst_module],
+	packages=['sdict'],
+	package_dir={'sdict':'sdict'}	
+)
+
