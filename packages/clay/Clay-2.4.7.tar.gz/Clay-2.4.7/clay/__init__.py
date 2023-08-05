4d388b440db0dd92008283390d441aeca5b0662a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:49 +0000

    Add package clay/2.4.7/Clay-2.4.7.tar.gz

---
+++ packages/clay/Clay-2.4.7.tar.gz/Clay-2.4.7/clay/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+    # Clay
+
+    An amazing prototyping tool
+
+    ------------
+    © 2011 by [Lúcuma] (http://lucumalabs.com).  
+    See `AUTHORS.md` for more details.  
+    License: Modified BSD License
+
+"""
+from main import Clay
+
+
+__version__ = '2.4.7'
---
+++ packages/clay/Clay-2.4.7.tar.gz/Clay-2.4.7/clay/manage.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+from os.path import dirname, join, realpath, sep, abspath
+
+from pyceo import Manager
+from voodoo import reanimate_skeleton
+
+from .main import Clay, DEFAULT_HOST, DEFAULT_PORT
+
+
+manager = Manager()
+
+SKELETON = join(dirname(realpath(__file__)), 'skeleton')
+
+SKELETON_HELP = """
+    Done!
+    Now go to %s, and do `clay run` to start the server.
+"""
+
+
+@manager.command
+def new(path='.'):
+    """[path='.']
+    Creates a new project
+    """
+    path = abspath(path.rstrip(sep))
+    reanimate_skeleton(SKELETON, path)
+    print(SKELETON_HELP % (path,))
+
+
+@manager.command
+def run(host=DEFAULT_HOST, port=DEFAULT_PORT, path='.'):
+    """[host] [port] [path='.']
+    Run the development server
+    """
+    path = abspath(path)
+    c = Clay(path)
+    c.run(host=host, port=port)
+
+
+@manager.command
+def build(pattern=None, path='.'):
+    """[path='.']
+    Generates a static copy of the sources
+    """
+    path = abspath(path)
+    c = Clay(path)
+    c.build(pattern)
+
+
+@manager.command
+def version():
+    """.
+    Returns the current Clay version
+    """
+    from . import __version__
+    print(__version__)
+
+
+def main():
+    manager.run()
