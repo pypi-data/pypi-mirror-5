ca63e2eecef76b868a0117a847a7d4ff705c0460
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:42 +0000

    Add package neomodel/0.3.1/neomodel-0.3.1.tar.gz

---
+++ packages/neomodel/neomodel-0.3.1.tar.gz/neomodel-0.3.1/neomodel/properties.py
@@ -0,0 +1,157 @@
+from neomodel.exception import InflateError, DeflateError
+from datetime import datetime, date
+import time
+import pytz
+import json
+import sys
+
+if sys.version_info >= (3, 0):
+    unicode = lambda x: str(x)
+
+
+def validator(fn):
+    fn_name = fn.func_name if hasattr(fn, 'func_name') else fn.__name__
+    if fn_name is 'inflate':
+        exc_class = InflateError
+    elif fn_name == 'deflate':
+        exc_class = DeflateError
+    else:
+        raise Exception("Unknown Property method " + fn.func_name)
+
+    def validator(self, value, node_id=None):
+        try:
+            return fn(self, value)
+        except Exception as e:
+            raise exc_class(self.name, self.owner, str(e), node_id)
+    return validator
+
+
+class Property(object):
+    def __init__(self, unique_index=False, index=False, required=False, default=None):
+        if default and required:
+            raise Exception("required and default are mutually exclusive")
+
+        if unique_index and index:
+            raise Exception("unique_index and index are mutually exclusive")
+
+        self.required = required
+        self.unique_index = unique_index
+        self.index = index
+        self.default = default
+        self.has_default = True if self.default is not None else False
+
+    def default_value(self):
+        if self.has_default:
+            if hasattr(self.default, '__call__'):
+                return self.default()
+            else:
+                return self.default
+        else:
+            raise Exception("No default value specified")
+
+    @property
+    def is_indexed(self):
+        return self.unique_index or self.index
+
+
+class StringProperty(Property):
+    @validator
+    def inflate(self, value):
+        return unicode(value)
+
+    @validator
+    def deflate(self, value):
+        return unicode(value)
+
+
+class IntegerProperty(Property):
+    @validator
+    def inflate(self, value):
+        return int(value)
+
+    @validator
+    def deflate(self, value):
+        return int(value)
+
+
+class FloatProperty(Property):
+    @validator
+    def inflate(self, value):
+        return float(value)
+
+    @validator
+    def deflate(self, value):
+        return float(value)
+
+
+class BooleanProperty(Property):
+    @validator
+    def inflate(self, value):
+        return bool(value)
+
+    @validator
+    def deflate(self, value):
+        return bool(value)
+
+
+class DateProperty(Property):
+    @validator
+    def inflate(self, value):
+        return datetime.strptime(unicode(value), "%Y-%m-%d").date()
+
+    @validator
+    def deflate(self, value):
+        if not isinstance(value, date):
+            msg = 'datetime.date object expected, got {0}'.format(repr(value))
+            raise ValueError(msg)
+        return value.isoformat()
+
+
+class DateTimeProperty(Property):
+    @validator
+    def inflate(self, value):
+        try:
+            epoch = int(value)
+        except ValueError:
+            raise ValueError('integer expected, got {0} cant inflate to datetime'.format(value))
+        return datetime.utcfromtimestamp(epoch).replace(tzinfo=pytz.utc)
+
+    @validator
+    def deflate(self, value):
+        if not isinstance(value, datetime):
+            raise ValueError('datetime object expected, got {0}'.format(value))
+        return time.mktime(value.utctimetuple())
+
+
+class JSONProperty(Property):
+    @validator
+    def inflate(self, value):
+        return json.loads(value)
+
+    @validator
+    def deflate(self, value):
+        return json.dumps(value)
+
+
+class AliasProperty(property, Property):
+    def __init__(self, to=None):
+        self.target = to
+        self.required = False
+        self.has_default = False
+
+    def aliased_to(self):
+        return self.target
+
+    def __get__(self, node, cls):
+        return getattr(node, self.aliased_to()) if node else self
+
+    def __set__(self, node, value):
+        setattr(node, self.aliased_to(), value)
+
+    @property
+    def index(self):
+        return getattr(self.owner, self.aliased_to()).index
+
+    @property
+    def unique_index(self):
+        return getattr(self.owner, self.aliased_to()).unique_index
---
+++ packages/neomodel/neomodel-0.3.1.tar.gz/neomodel-0.3.1/neomodel/util.py
@@ -0,0 +1,68 @@
+import re
+from py2neo import neo4j, rest
+from .exception import UniqueProperty, DataInconsistencyError
+
+camel_to_upper = lambda x: "_".join(word.upper() for word in re.split(r"([A-Z][0-9a-z]*)", x)[1::2])
+upper_to_camel = lambda x: "".join(word.title() for word in x.split("_"))
+
+
+class CustomBatch(neo4j.WriteBatch):
+
+    def __init__(self, graph, index_name, node='(unsaved)'):
+        super(CustomBatch, self).__init__(graph)
+        self.index_name = index_name
+        self.node = node
+
+    # Borrowed from nigel, to force status codes in batch response
+    def _submit(self):
+        """ Submits batch of requests, returning list of Response objects."""
+        rs = self._graph_db._send(rest.Request(self._graph_db, "POST", self._graph_db._batch_uri, [
+            request.description(id_)
+            for id_, request in enumerate(self.requests)
+        ], {'X-Stream': True}))
+        self.clear()
+        return [
+            rest.Response(
+                self._graph_db,
+                response.get("status", rs.status),
+                response["from"],
+                response.get("location", None),
+                response.get("body", None),
+                id=response.get("id", None),
+            )
+            for response in rs.body
+        ]
+
+    def submit(self):
+        results = []
+        requests = self.requests
+        try:
+            results = self._submit()
+            # pre create or fail support need to catch 200 response
+            if self._graph_db.neo4j_version < (1, 9):
+                self._check_for_conflicts(results, requests)
+        except rest.ResourceConflict as r:
+            key = requests[r.id].body['key']
+            value = requests[r.id].body['value']
+            raise UniqueProperty(key, value, self.index_name, self.node)
+        else:
+            return [
+                self._graph_db._resolve(response.body, response.status, id_=response.id)
+                for response in results
+            ]
+
+    def _check_for_conflicts(self, results, requests):
+        for i, r in enumerate(results):
+            if r.status == 200:
+                raise DataInconsistencyError(requests[i], self.index_name, self.node)
+
+
+def _legacy_conflict_check(cls, node, props):
+    for key, value in props.items():
+        if key in cls._class_properties() and cls.get_property(key).unique_index:
+                results = cls.index.__index__.get(key, value)
+                if len(results):
+                    if isinstance(node, (int,)):  # node ref
+                        raise UniqueProperty(key, value, cls.index.name)
+                    elif hasattr(node, 'id') and results[0].id != node.id:
+                        raise UniqueProperty(key, value, cls.index.name, node)
