734c225840fe4051f6e563e2cadf9bd2dc0bd6f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:06 +0000

    Add package schedule/0.1.3/schedule-0.1.3.tar.gz

---
+++ packages/schedule/schedule-0.1.3.tar.gz/schedule-0.1.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='schedule',
+    packages=['schedule'],
+    version='0.1.3',
+    description='Job scheduling for humans.',
+    author='Daniel Bader',
+    author_email='mail@dbader.org',
+    url='https://github.com/dbader/schedule',
+    download_url='https://github.com/dbader/schedule/tarball/0.1.3',
+    keywords=['schedule', 'periodic', 'jobs', 'scheduling', 'clockwork'],
+    classifiers=[],
+)
