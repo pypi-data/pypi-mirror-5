69e2cda88edc03c39d8971650ef8c05462cb6573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:07 +0000

    Add package django-genericadmin/0.4/django-genericadmin-0.4.tar.gz

---
+++ packages/django-genericadmin/django-genericadmin-0.4.tar.gz/django-genericadmin-0.4/setup.py
@@ -0,0 +1,25 @@
+
+from distutils.core import setup
+
+setup(
+    name='django-genericadmin',
+    version='0.4',
+    description="Adds support for generic relations within Django's admin interface.",
+    author='Weston Nielson, Jan Schrewe',
+    author_email='wnielson@gmail.com, jschrewe@googlemail.com',
+    url='https://github.com/jschrewe/django-genericadmin',
+    packages = ['genericadmin'],
+    package_data={'genericadmin': ['static/genericadmin/js/*.js']},
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['setuptools'],
+)
