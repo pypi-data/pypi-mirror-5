0057da6248b822838d5d900260c52a952581bfe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:28 +0000

    Add package pyrelic/0.4.0/pyrelic-0.4.0.tar.gz

---
+++ packages/pyrelic/pyrelic-0.4.0.tar.gz/pyrelic-0.4.0/pyrelic/__init__.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from .exceptions import (
+    NewRelicApiRateLimitException,
+    NewRelicApiException,
+    NewRelicCredentialException,
+    NewRelicInvalidApiKeyException,
+    NewRelicInvalidParameterException,
+    NewRelicUnknownApplicationException
+)
+from .base_client import BaseClient
+from .client import Client
+from .application import Application
+from .metric import Metric
+from .threshold import Threshold
+
+__all__ = (
+    'NewRelicCredentialException',
+    'NewRelicApiException',
+    'NewRelicInvalidApiKeyException',
+    'NewRelicUnknownApplicationException',
+    'NewRelicInvalidParameterException',
+    'Client',
+    'BaseClient'
+    'Application',
+    'Metric',
+    'Threshold'
+)
