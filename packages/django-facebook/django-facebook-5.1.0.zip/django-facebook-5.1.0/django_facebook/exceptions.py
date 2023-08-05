19debf7400874c6a859b82f55e5937d9ffe6720b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:53 +0000

    Add package django-facebook/5.1.0/django-facebook-5.1.0.zip

---
+++ packages/django-facebook/django-facebook-5.1.0.zip/django-facebook-5.1.0/django_facebook/exceptions.py
@@ -0,0 +1,16 @@
+class FacebookException(Exception):
+    pass
+
+
+class IncompleteProfileError(FacebookException):
+    pass
+
+
+class AlreadyConnectedError(FacebookException):
+
+    def __init__(self, users):
+        self.users = users
+
+
+class AlreadyRegistered(FacebookException):
+    pass
