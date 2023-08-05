f13dbb315650339fd1f79af02fec56ed7294d52a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:19 +0000

    Add package kaiso/0.1.2/kaiso-0.1.2.tar.gz

---
+++ packages/kaiso/kaiso-0.1.2.tar.gz/kaiso-0.1.2/kaiso/attributes/__init__.py
@@ -0,0 +1,79 @@
+import decimal
+import uuid
+import datetime
+import iso8601
+
+from kaiso.attributes.bases import RelationshipReference, wraps_type
+from kaiso.types import Attribute, DefaultableAttribute
+
+
+class Outgoing(RelationshipReference):
+    pass
+
+
+class Incoming(RelationshipReference):
+    pass
+
+
+@wraps_type(uuid.UUID)
+class Uuid(Attribute):
+    @property
+    def default(self):
+        return uuid.uuid4()
+
+    @staticmethod
+    def to_db(value):
+        return str(value)
+
+    @staticmethod
+    def to_python(value):
+        return uuid.UUID(hex=value)
+
+
+class Bool(DefaultableAttribute):
+    pass
+
+
+class Integer(DefaultableAttribute):
+    pass
+
+
+class Float(DefaultableAttribute):
+    pass
+
+
+class String(DefaultableAttribute):
+    pass
+
+
+@wraps_type(decimal.Decimal)
+class Decimal(DefaultableAttribute):
+    @staticmethod
+    def to_db(value):
+        if value is None:
+            return None
+        return str(value)
+
+    @staticmethod
+    def to_python(value):
+        return decimal.Decimal(value)
+
+
+@wraps_type(datetime.datetime)
+class DateTime(DefaultableAttribute):
+    @staticmethod
+    def to_db(value):
+        if value is None:
+            return None
+        return value.isoformat()
+
+    @staticmethod
+    def to_python(value):
+        return iso8601.parse_date(value)
+
+
+class Choice(String):
+    def __init__(self, *choices, **kwargs):
+        # again, super(Choice) vs Choice.__init__
+        super(Choice, self).__init__(**kwargs)
+        self.choices = choices
---
+++ packages/kaiso/kaiso-0.1.2.tar.gz/kaiso-0.1.2/kaiso/exceptions.py
@@ -0,0 +1,33 @@
+""" Provides all the exceptions that may be raised.
+"""
+
+
+class UniqueConstraintError(Exception):
+    """Raised when attempting to create more than one object with the same
+       value on an attribute declared to be unique"""
+
+
+class MultipleObjectsFound(Exception):
+    """ Raised when a caller of a RelationshipManager
+    expected a single object, but multiple were returned.
+    """
+
+
+class NoResultFound(Exception):
+    """ Raised when a caller of a RelationshipManager
+    expected at least one object, but none was found.
+    """
+
+
+class UnknownType(Exception):
+    """ Raised when trying to deserialise a class that hasn't been
+    registered
+    """
+
+
+class DeserialisationError(Exception):
+    """ Raised when trying to deserialise a dict with no __type__ key """
+
+
+class TypeAlreadyRegistered(Exception):
+    pass
