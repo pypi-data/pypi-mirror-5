75badb0cf786d21b563d4d0415d9474d31265ad9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:12 +0000

    Add package bibos-client/0.0.1.1/bibos_client-0.0.1.1.tar.gz

---
+++ packages/bibos-client/bibos_client-0.0.1.1.tar.gz/bibos_client-0.0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='bibos_client',
+    version='0.0.1.1',
+    description='Clients for the BibOS system',
+    url='https://github.com/magenta-aps/',
+    author='C. Agger and J.U.B. Kragh, Magenta ApS',
+    author_email='carstena@magenta-aps.dk',
+    license='GPLv3',
+    packages=['bibos_client'],
+    install_requires=['netifaces', 'bibos_utils'],
+    scripts=['bin/register_in_bibos_admin'],
+    zip_safe=False
+)
