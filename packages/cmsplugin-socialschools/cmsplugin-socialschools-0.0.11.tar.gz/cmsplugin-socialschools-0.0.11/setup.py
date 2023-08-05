4f60fb527634db3dd570f4f9f8adcbcc02e0d5b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:10 +0000

    Add package cmsplugin-socialschools/0.0.11/cmsplugin-socialschools-0.0.11.tar.gz

---
+++ packages/cmsplugin-socialschools/cmsplugin-socialschools-0.0.11.tar.gz/cmsplugin-socialschools-0.0.11/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+version = '0.0.11'
+
+setup(
+    name='cmsplugin-socialschools',
+    version=version,
+    description='socialschools plugin for django-cms',
+    author='Pratik Vyas',
+    author_email='pratik.vyas@changer.nl',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[],
+)
