d6604e328e58fb4b197ffa999da0a5cae4a5d890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:24 +0000

    Add package dimagi-utils/1.0.2/dimagi-utils-1.0.2.tar.gz

---
+++ packages/dimagi-utils/dimagi-utils-1.0.2.tar.gz/dimagi-utils-1.0.2/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='dimagi-utils',
+    version='1.0.2',
+    description='Dimagi Shared Utilities',
+    author='Dimagi',
+    author_email='dev@dimagi.com',
+    url='http://www.dimagi.com/',
+    packages=find_packages(exclude=['*.pyc']),
+    test_suite='dimagi.test_utils',
+    test_loader='unittest2:TestLoader',
+    install_requires=[
+        'django',
+        'openpyxl',
+        'python-dateutil',
+        'pytz',
+        'couchdbkit',
+    ],
+    tests_require=[
+        'unittest2',
+        'mock>=0.8.0',
+    ],
+)
