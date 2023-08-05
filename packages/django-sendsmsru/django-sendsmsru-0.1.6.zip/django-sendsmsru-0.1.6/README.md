b968066d2a91c141029ee15615555077d3c013c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:18 +0000

    Add package django-sendsmsru/0.1.6/django-sendsmsru-0.1.6.zip

---
+++ packages/django-sendsmsru/django-sendsmsru-0.1.6.zip/django-sendsmsru-0.1.6/README.md
@@ -0,0 +1,27 @@
+django-sendsmsru
+================
+
+Russian backends for django-sendsms
+
+backends for websms.ru
+-----------------------
+* sendsmsru.backends.websmsru.SMTPClient
+
+* sendsmsru.backends.websmsru.HTTPClient
+
+backends for infosmska.ru
+------------------------------
+
+* sendsmsru.backends.websmsru.HTTPClient
+
+CHANGES
+------------------
+
+* 0.1.6
+
+   * Added infosmska.ru http backend
+
+SEO keywords
+----------------------
+
+django отправка смс, django send russian sms, как отправить sms через django, django sms
---
+++ packages/django-sendsmsru/django-sendsmsru-0.1.6.zip/django-sendsmsru-0.1.6/sendsmsru/backends/infosmskaru.py
@@ -0,0 +1,69 @@
+#-*- coding: UTF-8 -*-
+import logging
+import urllib
+import urllib2
+
+from django.conf import settings
+from django.utils.encoding import smart_str
+
+from sendsms.backends.base import BaseSmsBackend
+
+logger = logging.getLogger(__name__)
+
+HTTP_URL = 'http://api.infosmska.ru/interfaces/SendMessages.ashx'
+
+USERNAME = settings.INFOSMSKARU_USERNAME
+PASSWORD = settings.INFOSMSKARU_PASSWORD
+
+
+class HTTPClient(BaseSmsBackend):
+
+    common = dict(login=USERNAME,
+                  pwd=PASSWORD)
+
+
+    def format_phone(self, phone):
+        return phone.lstrip('+')
+
+    def send_messages(self, messages):
+        result = 0
+        for message in messages:
+            result += self._send(message)
+        return result
+
+    def _send(self, message):
+        context = {
+            'message': smart_str(message.body, encoding='utf8'),
+            'phones': ','.join(self.format_phone(tel)
+                               for tel in message.to),
+            'sender': smart_str(message.from_phone,
+                                encoding='ascii', errors='ignore'),
+        }
+
+        context.update(self.common)
+
+        params = urllib.urlencode(context)
+        try:
+            resp = urllib2.urlopen('%s?%s' % (HTTP_URL, params,))
+        except IOError, exc:
+            if not self.fail_silently:
+                raise
+            else:
+                logger.error(u'Error sending: %s' % unicode(exc))
+                return False
+
+        resp = resp.read()
+        logger.debug('response was %s' % resp)
+        if not resp.startswith('Ok:'):
+            if not self.fail_silently:
+                raise RuntimeError(resp)
+            else:
+                logger.error(u'Error sending: %s' % resp)
+                return False
+
+        logger.debug(u'sms sended %s' % unicode(message.body))
+        return True
+
+
+class SOAPClient(BaseSmsBackend):
+    pass # TODO
