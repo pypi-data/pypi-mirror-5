745e62a8e8d60c77c551acaa12b4ec3bc678da1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:53 +0000

    Add package infi.storagemodel/0.1.53/infi.storagemodel-0.1.53.tar.gz

---
+++ packages/infi.storagemodel/infi.storagemodel-0.1.53.tar.gz/infi.storagemodel-0.1.53/src/infi/storagemodel/vendor/infinidat/infinibox/__init__.py
@@ -0,0 +1,11 @@
+
+from infi.pyutils.lazy import cached_method
+from ... import VendorMultipathBlockDevice, VendorSCSIBlockDevice, VendorSCSIStorageController, VendorSCSIEnclosureDevice
+
+from logging import getLogger
+log = getLogger(__name__)
+
+NFINIDAT_IEEE = 0x742B0F
+vid_pid = ("NFINIDAT" , "Infinidat A01")
+
+# pylint: disable=C0103
