1ac5aad54728df69a1dd8e5ae1eee14e51feb3b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:24 +0000

    Add package django-tz-detect/0.1.5/django-tz-detect-0.1.5.tar.gz

---
+++ packages/django-tz-detect/django-tz-detect-0.1.5.tar.gz/django-tz-detect-0.1.5/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from os.path import exists
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+from setuptools import find_packages
+from tz_detect import __version__
+
+setup(
+    name='django-tz-detect',
+    version=__version__,
+    author='Adam Charnock',
+    author_email='adam@adamcharnock.com',
+    packages=find_packages(),
+    package_dir={'tz_detect': 'tz_detect'},
+    package_data={
+        'tz_detect': [
+            'templates/tz_detect/*',
+            'static/tz_detect/js/*',
+        ]
+    },
+    include_package_data=True,
+    # Any executable scripts, typically in 'bin'. E.g 'bin/do-something.py'
+    scripts=[],
+    # REQUIRED: Your project's URL
+    url='https://github.com/adamcharnock/django-tz-detect',
+    license='MIT',
+    description='Automatic user timezone detection for django',
+    long_description=open('README.rst').read() if exists("README.rst") else "",
+    # Any requirements here, e.g. "Django >= 1.1.1"
+    install_requires=[
+        'django>=1.4',
+        'pytz',
+    ],
+)
