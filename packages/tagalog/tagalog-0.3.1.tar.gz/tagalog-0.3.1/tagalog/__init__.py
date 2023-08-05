ec2d40e7fcf8f774fa2b01a654fed469dd17d51d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:27 +0000

    Add package tagalog/0.3.1/tagalog-0.3.1.tar.gz

---
+++ packages/tagalog/tagalog-0.3.1.tar.gz/tagalog-0.3.1/tagalog/__init__.py
@@ -0,0 +1,7 @@
+import os
+
+__all__ = []
+__version__ = '0.3.1'
+
+# Use UTF8 for stdin, stdout, stderr
+os.environ['PYTHONIOENCODING'] = 'utf-8'
---
+++ packages/tagalog/tagalog-0.3.1.tar.gz/tagalog-0.3.1/tagalog/shipper/stdout.py
@@ -0,0 +1,19 @@
+import json
+
+from tagalog.shipper.formatter import format_as_json, format_as_elasticsearch_bulk_json
+from tagalog.shipper.ishipper import IShipper
+
+class StdoutShipper(IShipper):
+    def __init__(self, args, bulk=False, bulk_index='logs', bulk_type='message'):
+        self.args = args
+        self.bulk = bulk
+        self.bulk_index = bulk_index
+        self.bulk_type = bulk_type
+
+
+    def ship(self, msg):
+        if self.bulk:
+            payload = format_as_elasticsearch_bulk_json(self.bulk_index,self.bulk_type,msg)
+        else:
+            payload = format_as_json(msg)
+        print(payload)
