8bd74be7835f755e845a557db4a91089b1d68005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:27 +0000

    Add package django-sendsmsru/0.1.7/django-sendsmsru-0.1.7.zip

---
+++ packages/django-sendsmsru/django-sendsmsru-0.1.7.zip/django-sendsmsru-0.1.7/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -
+#
+# This file is part of sendsms released under the MIT license. 
+# See the NOTICE for more information.
+
+import os
+import sys
+from setuptools import setup, find_packages
+
+from sendsmsru import VERSION
+
+try:
+    import importlib
+except ImportError:
+    reqs.append('importlib')
+
+setup(
+    name='django-sendsmsru',
+    version=VERSION,
+    description='Russian backends for django-sendsms',
+    long_description=file(
+        os.path.join(
+            os.path.dirname(__file__),
+            'README.md'
+        )
+    ).read(),
+    author='Victor Safronovich',
+    author_email='vsafronovich@gmail.com',
+    license='MIT',
+    url='http://github.com/suvit/django-sendsmsru',
+    zip_safe=False,
+    packages=find_packages(exclude=['docs', 'examples', 'tests']),
+    install_requires=['django',
+                      'django-sendsms'],
+    include_package_data=True,
+)
