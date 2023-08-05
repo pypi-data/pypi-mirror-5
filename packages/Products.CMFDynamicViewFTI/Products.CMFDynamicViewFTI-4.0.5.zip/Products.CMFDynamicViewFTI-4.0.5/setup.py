6fda6f919f40e1c6bb70211fb8275cfab9c3c393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:00 +0000

    Add package Products.CMFDynamicViewFTI/4.0.5/Products.CMFDynamicViewFTI-4.0.5.zip

---
+++ packages/Products.CMFDynamicViewFTI/Products.CMFDynamicViewFTI-4.0.5.zip/Products.CMFDynamicViewFTI-4.0.5/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+version = '4.0.5'
+
+setup(name='Products.CMFDynamicViewFTI',
+      version=version,
+      description="CMFDynamicViewFTI is a product for dynamic views in CMF.",
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Programming Language :: Python",
+      ],
+      keywords='Zope CMF Plone dynamic view',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/Products.CMFDynamicViewFTI',
+      license='ZPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      extras_require=dict(
+        test=[
+            'plone.app.contentmenu',
+            'zope.publisher',
+            'Products.CMFTestCase',
+        ]
+      ),
+      install_requires=[
+        'setuptools',
+        'zope.browsermenu',
+        'zope.component',
+        'zope.interface',
+        'Products.CMFCore',
+        'Products.GenericSetup',
+        'Acquisition',
+        'ExtensionClass',
+        'Zope2',
+      ],
+)
