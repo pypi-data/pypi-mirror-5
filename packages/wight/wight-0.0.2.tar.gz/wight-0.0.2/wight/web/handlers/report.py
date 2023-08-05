3f11cc7f5eb91bf3599a50ab87611bbf99eef92d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:34 +0000

    Add package wight/0.0.2/wight-0.0.2.tar.gz

---
+++ packages/wight/wight-0.0.2.tar.gz/wight-0.0.2/wight/web/handlers/report.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# wight load testing
+# https://github.com/heynemann/wight
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/mit-license
+# Copyright (c) 2013 Bernardo Heynemann heynemann@gmail.com
+
+import tornado.web
+
+from wight.api.handlers.base import BaseHandler
+
+
+class ReportHandler(BaseHandler):
+
+    @tornado.web.asynchronous
+    def get(self, report_hash):
+        self.render('report.html')
+
+
+class DiffHandler(BaseHandler):
+
+    @tornado.web.asynchronous
+    def get(self, from_report_hash, to_report_hash):
+        self.render('diff.html')
+
+
+class TrendHandler(BaseHandler):
+
+    @tornado.web.asynchronous
+    def get(self, team_name, project_name, test_full_name):
+        self.render('trend.html')
