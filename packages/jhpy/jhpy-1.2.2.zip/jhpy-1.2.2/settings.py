51433b6ab56b12e0bb053de384c93244961a95a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:19 +0000

    Add package jhpy/1.2.2/jhpy-1.2.2.zip

---
+++ packages/jhpy/jhpy-1.2.2.zip/jhpy-1.2.2/settings.py
@@ -0,0 +1,7 @@
+# Settings
+name         = "jhpy"
+version      = "1.2.2"
+description  = "Command-line manager and template builder for JHPY."
+url          = "https://github.com/djwatt5/jhpy-clt"
+author       = "Daniel Watson"
+email        = "watsondaniel6@gmail.com"
