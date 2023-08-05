28d16b120cc3c2fe8405b10b7864958ca66c2386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:07 +0000

    Add package NMRPyStar/0.0.4/NMRPyStar-0.0.4.tar.gz

---
+++ packages/NMRPyStar/NMRPyStar-0.0.4.tar.gz/NMRPyStar-0.0.4/nmrpystar/parser.py
@@ -0,0 +1,42 @@
+from .tokenizer import scanner as tfull
+from .simple import scanner as tsimple
+from .parse import addLineCol, ConsList
+from .astparser import nmrstar
+
+
+
+def tokenize(scanner, string):
+    lcChars = ConsList.fromIterable(addLineCol(string))
+    return scanner.parse(lcChars, None)
+
+
+JUNKTYPES = set(['comment', 'whitespace', 'newlines'])
+
+def stripJunk(tokens):
+    return [t for t in tokens if t.tokentype not in JUNKTYPES]
+
+
+def astParse(tokenization):
+    stripped = stripJunk(tokenization['result'])
+    return nmrstar.parse(ConsList.fromIterable(stripped), None)
+
+
+def parse(scanner, string):
+    tokens = tokenize(scanner, string)
+    return tokens.bind(astParse)
+
+
+def fullParse(string):
+    '''
+    Parse string according to full NMR-Star
+    syntactic definition.
+    '''
+    return parse(tfull, string)
+
+
+def simpleParse(string):
+    '''
+    Parse string according to simplified NMR-Star
+    syntactic definition.
+    '''
+    return parse(tsimple, string)
