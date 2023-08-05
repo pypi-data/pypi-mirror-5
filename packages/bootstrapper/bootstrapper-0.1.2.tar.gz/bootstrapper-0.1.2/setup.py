23368fe9d0e6bae50264442363cb38d7e58651c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:36 +0000

    Add package bootstrapper/0.1.2/bootstrapper-0.1.2.tar.gz

---
+++ packages/bootstrapper/bootstrapper-0.1.2.tar.gz/bootstrapper-0.1.2/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import imp
+import os
+import sys
+
+from setuptools import setup
+
+
+DIRNAME = os.path.abspath(os.path.dirname(__file__))
+rel = lambda *parts: os.path.abspath(os.path.join(DIRNAME, *parts))
+
+kwargs = \
+    {'install_requires': ['argparse']} if sys.version_info < (2, 7) else {}
+
+handler = open(rel('README.rst'))
+README = handler.read()
+handler.close()
+
+module = imp.load_source('bootstrapper', rel('bootstrapper'))
+version = module.__version__
+os.unlink(rel('bootstrapperc'))
+
+setup(
+    name='bootstrapper',
+    version=version,
+    description='Bootstrap Python projects with virtualenv and pip.',
+    long_description=README,
+    author='Igor Davydenko',
+    author_email='playpauseandstop@gmail.com',
+    url='https://github.com/playpauseandstop/bootstrapper',
+    scripts=['bootstrapper'],
+    test_suite='tests',
+    platforms='any',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Topic :: System :: Systems Administration',
+    ],
+    keywords='bootstrap pip virtualenv',
+    license='BSD License',
+    **kwargs
+)
