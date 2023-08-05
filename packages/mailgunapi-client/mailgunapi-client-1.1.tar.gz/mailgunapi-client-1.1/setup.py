746721ca27b16ba94bd49a003857e81cfc80022b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:55 +0000

    Add package mailgunapi-client/1.1/mailgunapi-client-1.1.tar.gz

---
+++ packages/mailgunapi-client/mailgunapi-client-1.1.tar.gz/mailgunapi-client-1.1/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(name='mailgunapi-client',
+      version='1.1',
+      description='Python Wrapper around MailGun HTTP API',
+      author='Zach Goldberg',
+      author_email='zach@zachgoldberg.com',
+      url='https://github.com/ZachGoldberg/mailgun-python-api',
+      packages=[
+          'mailgun',
+          ],
+      package_dir={
+        'mailgun': 'mailgun/',
+        },
+      install_requires=[
+        'requests',
+          ],
+      )
