9195f42b6666c8d15ee15899370c014c7d0f30ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:55 +0000

    Add package lizard-ui/4.29.3/lizard-ui-4.29.3.zip

---
+++ packages/lizard-ui/lizard-ui-4.29.3.zip/lizard-ui-4.29.3/lizard_ui/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from lizard_security.admin import SecurityFilteredAdmin
+
+from lizard_ui.models import ApplicationIcon
+from lizard_ui.models import ApplicationScreen
+
+
+class ApplicationIconInline(admin.TabularInline):
+    model = ApplicationIcon
+    fk_name = 'application_screen'
+
+
+class ApplicationScreenAdmin(admin.ModelAdmin):
+    inlines = [ApplicationIconInline, ]
+
+class ApplicationIconAdmin(SecurityFilteredAdmin):
+    list_display = ['__str__', 'name', 'url']
+    list_editable = ['url', 'name']
+
+admin.site.register(ApplicationIcon, ApplicationIconAdmin)
+admin.site.register(ApplicationScreen, ApplicationScreenAdmin)
