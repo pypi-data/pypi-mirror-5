9f648a61e62e4d707fa8f7e6dffccc7ea70d6da3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:18 +0000

    Add package chatbot/1.3b/chatbot-1.3b.tar.gz

---
+++ packages/chatbot/chatbot-1.3b.tar.gz/chatbot-1.3b/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(name='chatbot',
+      version='1.3b',
+      description='Allows users on wikia wikis to create a chatbot for Special:Chat',
+      long_description="""This is a chat bot making module for wikia's Special:Chat.
+It allows you to connect to the chat and execute actions and retrieve events all specified by
+the author.  Very function for beginners to advanced python users.""",
+      author='Matthew Cunningham',
+      author_email='hairrazerrr@gmail.com',
+      url='http://packages.python.org/chatbot/',
+      download_url='https://github.com/hairr/chatbot/archive/master.zip',
+      requires="requests",
+      py_modules=['chatbot'],
+      keywords= "hairr wikia xhr chat bot",
+      license= "MIT License",
+      classifiers = [
+      	"Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Communications :: Chat"
+      ]
+     )
