de0b85f8c8ff0ca21fa838e65457a22b4253aaff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:14 +0000

    Add package kplr/0.0.3/kplr-0.0.3.tar.gz

---
+++ packages/kplr/kplr-0.0.3.tar.gz/kplr-0.0.3/kplr/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from .kplr import TIME_BASELINE, EXPOSURE_TIMES
+from .data import Dataset
+from .mast import API
+from . import ld
