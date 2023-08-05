13b2d569b1bc369827623e5329e907a0348230e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:39 +0000

    Add package fakecouch/0.0.4/fakecouch-0.0.4.tar.gz

---
+++ packages/fakecouch/fakecouch-0.0.4.tar.gz/fakecouch-0.0.4/setup.py
@@ -0,0 +1,23 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='fakecouch',
+    version='0.0.4',
+    description='Fake implementation of CouchDBKit api for testing purposes',
+    author='Dimagi',
+    author_email='dev@dimagi.com',
+    url='http://github.com/dimagi/fakecouch',
+    py_modules=['fakecouch'],
+    test_suite='tests',
+    test_loader='unittest2:TestLoader',
+    license='MIT',
+    install_requires=[
+        'couchdbkit',
+    ],
+    tests_require=[
+        'unittest2'
+    ]
+)
