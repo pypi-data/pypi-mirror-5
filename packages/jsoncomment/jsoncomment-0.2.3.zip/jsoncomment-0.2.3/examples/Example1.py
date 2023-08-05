0334d5341e2a8c772f42caf1cdbc2ed83a60cd02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:16 +0000

    Add package jsoncomment/0.2.3/jsoncomment-0.2.3.zip

---
+++ packages/jsoncomment/jsoncomment-0.2.3.zip/jsoncomment-0.2.3/examples/Example1.py
@@ -0,0 +1,46 @@
+﻿
+################################################################################
+
+try:
+	import ujson as json
+except ImportError:
+	import json
+
+from jsoncomment import JsonComment
+
+################################################################################
+
+if __name__ == '__main__':
+
+	string = """
+/******************
+Comment 1
+Comment 2
+******************/
+[
+	# Objects
+	{
+		"key" : "value",
+		"another key" :
+		\"\"\"
+		\\n
+		A multiline string.\\n
+		It will wrap to single line, 
+		but a trailing space per line is kept.
+		\"\"\",
+	},
+	; Other Values
+	81,
+	; Allow a non standard trailing comma
+	true,
+]
+"""
+
+	parser = JsonComment(json)
+	parsed_object = parser.loads(string)
+
+	print(parsed_object[0]["another key"], "\n")
+
+	print(parser.dumps(parsed_object))
+
+################################################################################
