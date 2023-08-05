ba78b2e2bd64d1c08eecfa5096aae82ca9ff027f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:47 +0000

    Add package job-runner/3.1.3/job-runner-3.1.3.tar.gz

---
+++ packages/job-runner/job-runner-3.1.3.tar.gz/job-runner-3.1.3/job_runner/settings/env/testing.py
@@ -0,0 +1,27 @@
+import os
+
+from job_runner.settings.base import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(PROJECT_ROOT, '..', 'test_database.sqlite'),
+    }
+}
+
+TIME_ZONE = 'Europe/Amsterdam'
+
+JOB_RUNNER_WS_SERVER = 'ws://localhost:5000/'
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+
+# Make sure that test with naive datetime objects will fail
+import warnings
+warnings.filterwarnings(
+    'error',
+    r"DateTimeField received a naive datetime",
+    RuntimeWarning,
+    r'django\.db\.models\.fields'
+)
