04e92e6cf0af52a3bc5fe7d8bfb4f3d071df1022
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:43 +0000

    Add package django-paypal-adaptive/0.1.5/django-paypal-adaptive-0.1.5.tar.gz

---
+++ packages/django-paypal-adaptive/django-paypal-adaptive-0.1.5.tar.gz/django-paypal-adaptive-0.1.5/paypaladaptive/tests/__init__.py
@@ -0,0 +1,4 @@
+from tests import *
+from ipn import TestPaymentIPN, TestPreapprovalIPN, TestIPNVerification
+from preapproval_return_url import TestPreapprovalReturnURL
+from payment_return_url import TestPaymentReturnURL
