736f52d3f57ac9f1e24ddf84075d90ff8502f144
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:10 +0000

    Add package jmbo-show/0.2.3/jmbo_show-0.2.3-py2.7.egg

---
+++ packages/jmbo-show/jmbo_show-0.2.3-py2.7.egg/show/views.py
@@ -0,0 +1,24 @@
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+
+from show.models import Show, RadioShow
+from show.utils import get_current_permitted_show
+
+
+def schedule(request):
+    di = {}
+    for show in  Show.permitted.all():
+        di.setdefault(show.repeat, [])
+        di[show.repeat].append(show.id)
+
+    # Convert values into querysets. The template requires it.
+    for k, v in di.items():
+        di[k] = Show.permitted.filter(id__in=v).order_by('start_time')
+
+    extra = dict(intervals=di)
+    return render_to_response('show/schedule.html', extra, context_instance=RequestContext(request))
+
+
+def current_radio(request):
+    extra = dict(object=get_current_permitted_show(RadioShow))
+    return render_to_response('show/current_radio.html', extra, context_instance=RequestContext(request))
