6df1eee04e6cbfc967a0c89a8467afa5b0fc8fd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:08 +0000

    Add package MAVProxy/1.0.3/MAVProxy-1.0.3.zip

---
+++ packages/MAVProxy/MAVProxy-1.0.3.zip/MAVProxy-1.0.3/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup, Extension
+
+setup (name = 'MAVProxy',
+       version = '1.0.3',
+       description = 'MAVProxy MAVLink ground station',
+       url = 'https://github.com/tridge/MAVProxy',
+       author = 'Andrew Tridgell',
+       author_email = 'andrew@tridgell.net',
+       package_dir = { 'MAVProxy' : '.' },
+       packages = ['MAVProxy',
+                   'MAVProxy.modules',
+                   'MAVProxy.modules.mavproxy_map',
+                   'MAVProxy.modules.lib',
+                   'MAVProxy.modules.lib.ANUGA',
+                   'MAVProxy.modules.mavproxy_CUAV' ],
+       scripts = [ 'mavproxy.py', 'tools/mavflightview.py' ],
+       package_data = { 'MAVProxy.modules.mavproxy_map' :
+                        [ 'data/*.jpg', 'data/*.png' ]}
+       )
