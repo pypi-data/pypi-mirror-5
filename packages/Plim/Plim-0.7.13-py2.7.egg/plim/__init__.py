7a840704fd2e2a97e528bf2181915d3314523c9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:27 +0000

    Add package Plim/0.7.13/Plim-0.7.13-py2.7.egg

---
+++ packages/Plim/Plim-0.7.13-py2.7.egg/plim/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from .lexer import compile_plim_source
+
+
+
+# ``preprocessor`` is a public object that always follows Mako's preprocessor API.
+# Do not use ``compile_plim_source`` in your projects, because its signature
+# may be changed in the future.
+preprocessor = compile_plim_source
