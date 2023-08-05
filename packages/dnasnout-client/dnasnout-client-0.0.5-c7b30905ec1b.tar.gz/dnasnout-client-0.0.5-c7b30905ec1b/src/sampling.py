7eb89bad7c226ae7f803f185f04e07db976f8401
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:54 +0000

    Add package dnasnout-client/0.0.5-c7b30905ec1b/dnasnout-client-0.0.5-c7b30905ec1b.tar.gz

---
+++ packages/dnasnout-client/dnasnout-client-0.0.5-c7b30905ec1b.tar.gz/dnasnout-client-0.0.5-c7b30905ec1b/src/sampling.py
@@ -0,0 +1,27 @@
+import random
+
+class ReservoirSample(object):
+    def __init__(self, size):
+        self._size = size
+        self._filled = False
+        self._entrynum = 0
+        self._fill_i = 0
+        self._l = list()
+    def add(self, item):
+        """ Add item to the sample. """
+        if self._filled:
+            i = random.randint(0, self._entrynum)
+            if i < self._fill_i:
+                self._l[i] = item
+                res = i
+            else:
+                res = None
+        else:
+            self._l.append(item)
+            res = len(self._l)
+            self._fill_i = res
+            if len(self._l) == self._size:
+                self._filled = True
+                self._fill_i = self._size
+        self._entrynum += 1
+        return res
