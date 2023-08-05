cad8c85d7cdf51c9fe62e7023840eb4f4393c920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:54 +0000

    Add package collective.portlet.twitter/1.0b2/collective.portlet.twitter-1.0b2.zip

---
+++ packages/collective.portlet.twitter/collective.portlet.twitter-1.0b2.zip/collective.portlet.twitter-1.0b2/README.rst
@@ -0,0 +1,61 @@
+**********************
+Twitter Widget Portlet
+**********************
+
+.. contents:: Table of Contents
+
+Life, the Universe, and Everything
+----------------------------------
+
+This portlet allows you to easily embed and customise JavaScript-based Twitter
+widgets into your Plone site.
+
+To read and understand more about such widgets, and to create your own, visit 
+https://twitter.com/settings/widgets.
+
+Don't Panic
+-----------
+
+Create twitter widget:
+
+.. image:: https://raw.github.com/collective/collective.portlet.twitter/master/docs/_img/Screen%20Shot%202012-11-09%20at%209.54.19%20AM.png
+
+Configure widget:
+
+.. image:: https://raw.github.com/collective/collective.portlet.twitter/master/docs/_img/Screen%20Shot%202012-11-09%20at%209.54.58%20AM.png
+
+Saved:
+
+.. image:: https://raw.github.com/collective/collective.portlet.twitter/master/docs/_img/Screen%20Shot%202012-11-09%20at%209.55.14%20AM.png
+
+Get widget id:
+
+.. image:: https://raw.github.com/collective/collective.portlet.twitter/master/docs/_img/Screen%20Shot%202012-11-09%20at%209.55.14%20AM-Get_id.png
+
+Add your widget ID into the **Data Widget ID** field as follows:
+
+.. image:: https://raw.github.com/collective/collective.portlet.twitter/master/docs/_img/Screen%20Shot%202012-11-09%20at%209.53.19%20AM.png
+
+For more information on the variety of client-side options 
+(such as theme, link color, width, height, and so forth) that can be 
+configured within this portlet's settings, see Twitter's development 
+documentation at https://dev.twitter.com/docs/embedded-timelines#customization.
+
+Make any customisations you'd like and click **Save**.
+
+That's it!
+
+.. image:: https://raw.github.com/collective/collective.portlet.twitter/master/docs/_img/Screen%20Shot%202012-11-09%20at%209.52.39%20AM.png
+
+
+
+
+Mostly Harmless
+---------------
+
+.. image:: https://secure.travis-ci.org/collective/collective.portlet.twitter.png
+    :target: http://travis-ci.org/collective/collective.portlet.twitter
+
+Got an idea? Found a bug? Let us know by `opening a support ticket`_.
+
+.. _`opening a support ticket`: https://github.com/collective/collective.portlet.twitter/issues
---
+++ packages/collective.portlet.twitter/collective.portlet.twitter-1.0b2.zip/collective.portlet.twitter-1.0b2/setup.py
@@ -0,0 +1,68 @@
+# -*- coding:utf-8 -*-
+
+from setuptools import find_packages
+from setuptools import setup
+
+import os
+
+version = '1.0b2'
+description = u"Twitter Widget Portlet - https://twitter.com/settings/widgets"
+long_description = (
+    open("README.rst").read() + "\n" +
+    open(os.path.join("docs", "INSTALL.rst")).read() + "\n" +
+    open(os.path.join("docs", "CREDITS.rst")).read() + "\n" +
+    open(os.path.join("docs", "HISTORY.rst")).read()
+)
+
+setup(name='collective.portlet.twitter',
+      version=version,
+      description=description,
+      long_description=long_description,
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.1",
+        "Framework :: Plone :: 4.2",
+        "Framework :: Plone :: 4.3",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='plone portlet twitter widget',
+      author='Thiago Avelino',
+      author_email='thiagoavelinoster@gmail.com',
+      url='https://github.com/collective/collective.portlet.twitter',
+      license='GPLv2',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['collective', 'collective.portlet'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'plone.app.portlets',
+          'plone.portlets',
+          'Products.CMFCore',
+          'Products.GenericSetup',
+          'setuptools',
+          'zope.i18nmessageid',
+          'zope.interface',
+          'zope.schema',
+      ],
+      extras_require={
+          'test': [
+              'plone.app.testing',
+              'unittest2',
+              'zope.component',
+          ],
+      },
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
