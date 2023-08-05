e8dcd89bdcc1858eab242248deaafbe3a5a50855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:02 +0000

    Add package djinn_core/1.0.0/djinn_core-1.0.0.tar.gz

---
+++ packages/djinn_core/djinn_core-1.0.0.tar.gz/djinn_core-1.0.0/djinn_core/utils.py
@@ -0,0 +1,27 @@
+def _class_implements(clazz, superclazz):
+
+    well_does_it = False
+
+    if superclazz in  clazz.__bases__:
+        well_does_it = True
+    else:
+        for base in clazz.__bases__:
+            well_does_it = _class_implements(base, superclazz)
+            if well_does_it:
+                break
+
+    return well_does_it
+
+
+def implements(instance, clazz):
+
+    """ Is it a bird? A plane? A clazz? """
+
+    return _class_implements(instance.__class__, clazz)
+
+
+def extends(clazz, otherclazz):
+
+    """ Does the class extend the other one? """
+
+    return _class_implements(clazz, otherclazz)
