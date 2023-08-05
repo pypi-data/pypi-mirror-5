f4b76e0d58fd774a12b54b8b9832873fe0e89109
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:34 +0000

    Add package brutha/1.0.1/brutha-1.0.1.tar.gz

---
+++ packages/brutha/brutha-1.0.1.tar.gz/brutha-1.0.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='brutha',
+    version='1.0.1',
+    description='Sync FLAC music files to Ogg Vorbis (or keep lossy as-is)',
+    long_description=open('README').read(),
+    author='Laurent Bachelier',
+    author_email='laurent@bachelier.name',
+    url='http://git.p.engu.in/laurentb/brutha/',
+    packages=['brutha'],
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Operating System :: POSIX',
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'Topic :: Multimedia :: Sound/Audio :: Conversion',
+    ],
+    entry_points={'console_scripts': ['brutha = brutha.__main__:main']},
+)
