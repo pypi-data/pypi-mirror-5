a01faebc4675666dbaa8c5027a4b3acac64dfb13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:21 +0000

    Add package HMFcalc/1.0.0/HMFcalc-1.0.0.tar.gz

---
+++ packages/HMFcalc/HMFcalc-1.0.0.tar.gz/HMFcalc-1.0.0/setup.py
@@ -0,0 +1,27 @@
+'''
+Created on May 20, 2013
+
+@author: Steven
+'''
+from setuptools import setup, find_packages
+import os
+
+version = '1.0.0'
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="HMFcalc",
+    version=version,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    requires=['numpy', 'pandas'],
+    author="Steven Murray",
+    author_email="steven.jeanette.m@gmail.com",
+    description="A halo mass function calculator web-app in django",
+    long_description=read('readme.txt'),
+    keywords="halo mass function",
+    url="https://github.com/steven-murray/HMFcalc",
+    # could also include long_description, download_url, classifiers, etc.
+)
