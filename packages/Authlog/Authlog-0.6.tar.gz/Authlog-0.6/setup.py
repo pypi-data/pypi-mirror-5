646c90a4bcf0a9cb54b97bec04d8dc408368811b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:27 +0000

    Add package Authlog/0.6/Authlog-0.6.tar.gz

---
+++ packages/Authlog/Authlog-0.6.tar.gz/Authlog-0.6/setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+        name = "Authlog",
+        version = "0.6",
+        author = "Joe Jasinski",
+        author_email = "jjasinski@imagescape.com",
+        url = "http://www.imagescape.com",
+
+        packages = find_packages('.'),
+        include_package_data=True,
+
+        description = "Log Django Authentications and Admin page visits.",
+        long_description ="""Log Django Authentications and Admin page visits.  See README for more info.""",
+        install_requires=[],
+        zip_safe=False,
+        classifiers = [
+            'Programming Language :: Python',
+        ]
+)
