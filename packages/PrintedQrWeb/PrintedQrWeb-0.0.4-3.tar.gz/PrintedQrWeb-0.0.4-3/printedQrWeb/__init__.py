6b395243a47686b403ce1cc75940b3cd02677be9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:20 +0000

    Add package PrintedQrWeb/0.0.4-3/PrintedQrWeb-0.0.4-3.tar.gz

---
+++ packages/PrintedQrWeb/PrintedQrWeb-0.0.4-3.tar.gz/PrintedQrWeb-0.0.4-3/printedQrWeb/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, make_response, request
+from printedQr_ import printedQr
+
+app = Flask(__name__)
+app.config['DEBUG'] = False
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/getQr', methods=['POST'])
+def getQr():
+    text = request.form['text']
+    scale = request.form['scale']
+    qr = printedQr.QRGen(scale=scale, data=text)
+    qr.make_qr()
+    response = make_response(qr.make_scad())
+    response.headers['Content-Type'] = 'application/octect-stream'
+    response.headers['Content-Disposition'] = 'attachment; filename=Qr' +  \
+        text[:3] + '.scad'
+    return response
+
+
+def server():
+    """ Main server, will allow us to make it wsgi'able """
+    app.run(host='0.0.0.0', port=8022)
+
+if __name__ == "__main__":
+    server()
