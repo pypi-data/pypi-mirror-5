8e7aa8e34568117e4f07eb25b508835b23a1ff18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:43 +0000

    Add package chube/0.1.14/chube-0.1.14.tar.gz

---
+++ packages/chube/chube-0.1.14.tar.gz/chube-0.1.14/TODO.md
@@ -0,0 +1,16 @@
+General
+-----
+
+* Add `_` prefix to getters and setters
+* Migrate to Github issue tracker
+
+Linode
+-----
+
+* Make clear single-user-mode setter
+* Add Linode class attributes for `status` attribute (up, down, ?)
+
+Nodebalacer
+-----
+
+Implement all Nodebalancer functionality
---
+++ packages/chube/chube-0.1.14.tar.gz/chube-0.1.14/chube/__init__.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import yaml
+
+from .api import api_handler as chube_api_handler
+from .plan import Plan
+from .datacenter import Datacenter
+from .kernel import Kernel
+from .distribution import Distribution
+from .stackscript import Stackscript, StackscriptInput
+from .linode_obj import Linode, Disk, Config, IPAddress
+from .dns import Domain
+__version__ = "0.1.14"
+
+def load_chube_config():
+    """Loads the Chube config from ~/.chube."""
+    config_path = os.path.join(os.environ["HOME"], ".chube")
+    if os.path.exists(config_path):
+        contents = yaml.load(open(config_path, "r"))
+        if not hasattr(contents, "__getitem__"):
+            raise ValueError("Config file at '%s' must be a valid YAML document; see README.md" % (config_path,))
+        chube_api_handler.api_key = contents["api_key"]
+        return
+    raise OSError("No config file found at '%s'" % (config_path))
