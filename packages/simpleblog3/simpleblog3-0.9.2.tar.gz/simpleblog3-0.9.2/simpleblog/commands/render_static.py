3a2a95beddd006c49ff1a4ebd8f768446c02cc77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:15 +0000

    Add package simpleblog3/0.9.2/simpleblog3-0.9.2.tar.gz

---
+++ packages/simpleblog3/simpleblog3-0.9.2.tar.gz/simpleblog3-0.9.2/simpleblog/commands/render_static.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""
+Module RENDER_STATIC -- Simple Blog Static Rendering
+Sub-Package SIMPLEBLOG.COMMANDS
+Copyright (C) 2012-2013 by Peter A. Donis
+
+Released under the GNU General Public License, Version 2
+See the LICENSE and README files for more information
+"""
+
+import os
+
+from plib.stdlib.ostools import data_changed
+
+from simpleblog.commands import BlogCommand
+
+
+class RenderStatic(BlogCommand):
+    """Static rendering of all blog pages.
+    """
+    
+    config_vars = dict(
+        static_dir="static"
+    )
+    
+    options = (
+        ("-f", "--force", {
+            'action': 'store_true',
+            'help': "force writing of unchanged files"
+        }),
+        ("-q", "--quiet", {
+            'action': 'store_true',
+            'help': "suppress console output"
+        })
+    )
+    
+    def run(self, blog):
+        for page in blog.pages:
+            data = page.encoded
+            path = os.path.abspath(os.path.join(self.static_dir, page.filepath))
+            if self.opts.force or data_changed(data, path):
+                if not self.opts.quiet:
+                    print("Rendering", path)
+                dir = os.path.split(path)[0]
+                if not os.path.isdir(dir):
+                    os.makedirs(dir)
+                with open(path, 'wb') as f:
+                    f.write(data)
+            else:
+                if not self.opts.quiet:
+                    print(path, "is unchanged")
