2b03e3172d8b871b7d8c76eec83cf56571c35ede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:37 +0000

    Add package jsoncomment/0.1.5/jsoncomment-0.1.5.zip

---
+++ packages/jsoncomment/jsoncomment-0.1.5.zip/jsoncomment-0.1.5/examples/Example2.py
@@ -0,0 +1,27 @@
+﻿
+################################################################################
+
+from codecs import open
+
+try:
+	import ujson as json
+except ImportError:
+	import json
+
+from jsoncomment import JsonComment
+
+################################################################################
+
+if __name__ == '__main__':
+
+	parser = JsonComment(json)
+
+	with open("Example2.json", "r", "utf-8-sig") as file_json:
+		parsed_object = parser.load(file_json)
+
+		print(parsed_object["item 1"], "\n")
+		print(parsed_object["Section/Subsection"], "\n")
+
+		print(parser.dumps(parsed_object))
+
+################################################################################
---
+++ packages/jsoncomment/jsoncomment-0.1.5.zip/jsoncomment-0.1.5/jsoncomment/__init__.py
@@ -0,0 +1,17 @@
+﻿
+################################################################################
+
+# User Interface
+
+from .comments import JsonComment
+	# Allows JSON libraries to ignore comments
+# (object_to_wrap)
+	# Module or Class Instance, which supports JSON load/loads interface
+
+################################################################################
+
+# Developer Interface
+
+
+
+################################################################################
