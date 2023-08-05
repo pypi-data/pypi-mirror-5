aa1406205ebd3e052c5e87322ac0db01aac92db8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:32 +0000

    Add package textblob/0.1/textblob-0.1.tar.gz

---
+++ packages/textblob/textblob-0.1.tar.gz/textblob-0.1/setup.py
@@ -0,0 +1,43 @@
+import sys
+import os
+
+import text
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'test':
+    try:
+        __import__('nose')
+    except ImportError:
+        print('nose required.')
+        sys.exit(1)
+
+    os.system('nosetests --verbosity 2')
+    sys.exit()
+
+setup(
+    name='textblob',
+    version=text.__version__,
+    description='Simple, Pythonic text processing',
+    long_description=(open('README.rst').read()),
+    author='Steven Loria',
+    author_email='sloria1@gmail.com',
+    url='https://github.com/sloria/TextBlob',
+    install_requires=['nltk'],
+    packages=[
+        'text'
+    ],
+    license='MIT',
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ),
+    tests_require=['nose'],
+)
