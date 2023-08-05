aa7cb7752f8d346e8798fc4edc82bcc450fd64b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:04 +0000

    Add package zapian/0.2.0dev/zapian-0.2.0dev.tar.gz

---
+++ packages/zapian/zapian-0.2.0dev.tar.gz/zapian-0.2.0dev/setup.py
@@ -0,0 +1,36 @@
+################################
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup (
+    name='zapian',
+    version='0.2.0dev',
+    author = "Pan Junyong",
+    author_email = "dev@zopen.cn",
+    description = "indexer addons: sort, cjksplitter, and so on",
+    long_description=(
+        read('README')
+        ),
+    license = "Private",
+    keywords = "zope3 z3c rpc  server client",
+    classifiers = [
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3'],
+    url = 'http://epk.zopen.cn/pypi/zapian',
+    packages = find_packages(),
+    include_package_data = True,
+    namespace_packages= [],
+    scripts=['scripts/list_xapian_database.py'],
+    install_requires = [
+        'setuptools',
+        'PyYAML'
+        ],
+    zip_safe = False,
+)
