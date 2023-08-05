cc19ade3e41534d24aed62661cb9b173f13e8bba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:23 +0000

    Add package collective.portlet.discussion/1.2.0/collective.portlet.discussion-1.2.0.zip

---
+++ packages/collective.portlet.discussion/collective.portlet.discussion-1.2.0.zip/collective.portlet.discussion-1.2.0/collective/portlet/discussion/utility/vocab.py
@@ -0,0 +1,25 @@
+try:
+    from zope.app.schema.vocabulary import IVocabularyFactory
+except ImportError:
+    from zope.schema.interfaces import IVocabularyFactory
+from zope.interface import implements
+from zope.schema.vocabulary import SimpleVocabulary
+from zope.i18n import translate
+from Acquisition import aq_get
+from collective.portlet.discussion import DiscussionPortletMessageFactory as _
+
+
+class DiscussionStatesVocabulary(object):
+    """
+    A simple vocab to translate the discussion states
+    """
+    implements(IVocabularyFactory)
+
+    def __call__(self, context):
+        context = getattr(context, 'context', context)
+        request = aq_get(context, 'REQUEST', None)
+
+        return SimpleVocabulary.fromItems(((translate(_('Pending'), context=request), 'pending'),
+                                           (translate(_('Published'), context=request), 'published'),))
+
+DiscussionStatesVocabularyFactory = DiscussionStatesVocabulary()
---
+++ packages/collective.portlet.discussion/collective.portlet.discussion-1.2.0.zip/collective.portlet.discussion-1.2.0/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.2.0'
+
+setup(name='collective.portlet.discussion',
+      version=version,
+      description="A simple portlet to show a list of comments",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Plone :: 3.3",
+        "Framework :: Plone :: 4.0",
+        "Framework :: Plone :: 4.1",
+        "Framework :: Plone :: 4.2",
+        "Framework :: Plone :: 4.3",
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='discussion portlet comments',
+      author='RedTurtle Technology',
+      author_email='sviluppoplone@redturtle.it',
+      url='http://plone.org/products/collective.portlet.discussion',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.portlet'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.discussion',
+      ],
+      entry_points="""
+      # -*- entry_points -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
