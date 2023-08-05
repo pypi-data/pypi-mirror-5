326ae5e432146afad7de700a6aab282d27f90165
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:01 +0000

    Add package unleash/0.4.1/unleash-0.4.1.tar.gz

---
+++ packages/unleash/unleash-0.4.1.tar.gz/unleash-0.4.1/unleash/util.py
@@ -0,0 +1,65 @@
+import contextlib
+import os
+import subprocess
+import sys
+
+import logbook
+from tempdir import TempDir
+import virtualenv
+
+from .git import export_to_dir
+
+log = logbook.Logger('util')
+
+
+@contextlib.contextmanager
+def dirch(dir):
+    prev = os.getcwd()
+    os.chdir(dir)
+    yield
+    os.chdir(prev)
+
+
+@contextlib.contextmanager
+def tmp_virtualenv():
+    with TempDir() as tmpdir:
+        log.info('Creating new virtualenv...')
+        log.debug(tmpdir.name)
+        virtualenv.create_environment(tmpdir.name, use_distribute=True)
+        yield os.path.abspath(tmpdir.name)
+
+
+@contextlib.contextmanager
+def tmp_checkout(repo, commit_id):
+    with TempDir() as tmpdir:
+        log.info('Checking out release commit...')
+        log.debug(tmpdir.name)
+        export_to_dir(repo, commit_id, tmpdir.name)
+        yield os.path.abspath(tmpdir.name)
+
+
+def checked_output(cmd, *args, **kwargs):
+    try:
+        log.debug('run %s' % ' '.join(cmd))
+        subprocess.check_output(cmd, *args, stderr=subprocess.STDOUT, **kwargs)
+    except subprocess.CalledProcessError as e:
+        log.error('Error calling external process.\n%s' % e.output)
+        raise
+
+
+def yesno(question):
+    while True:
+        inp = raw_input(question + ' [y/n]: ')
+
+        if inp in ('y', 'Y', 'yes', 'YES'):
+            return True
+        elif inp in ('n', 'N', 'no', 'NO'):
+            return False
+        else:
+            print 'Please answer \'yes\' or \'no\'.'
+
+
+def confirm(question):
+    if not yesno(question):
+        log.warning('Aborted by user request.')
+        sys.exit(1)
