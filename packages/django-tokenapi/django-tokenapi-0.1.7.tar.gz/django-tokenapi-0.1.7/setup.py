e9186665d95ccdeaa22feb2f3a320634e2c5bfb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:20 +0000

    Add package django-tokenapi/0.1.7/django-tokenapi-0.1.7.tar.gz

---
+++ packages/django-tokenapi/django-tokenapi-0.1.7.tar.gz/django-tokenapi-0.1.7/setup.py
@@ -0,0 +1,17 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='django-tokenapi',
+    version='0.1.7',
+    description='Add an API to your Django app using token-based authentication.',
+    long_description=read('README.md'),
+    author='Julian Pulgarin',
+    author_email='jp@julianpulgarin.com',
+    url='https://github.com/jpulgarin/django-tokenapi',
+    packages=['tokenapi'],
+    license='Apache License, Version 2.0',
+)
