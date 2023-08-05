a5debd6084c0f269ce02d4ba1642d227e18a82bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:34 +0000

    Add package parameterizedtestcase/0.0.3/parameterizedtestcase-0.0.3.tar.gz

---
+++ packages/parameterizedtestcase/parameterizedtestcase-0.0.3.tar.gz/parameterizedtestcase-0.0.3/parameterizedtestcase/__init__.py
@@ -0,0 +1,96 @@
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+# from functools import partial
+from functools import wraps
+
+
+def get_decorated_method_name(method_name, param_names, param_values):
+    name_components = []
+
+    for k, v in zip(param_names, param_values):
+        name_components.append(str(k))
+        name_components.append(str(v))
+
+    return '%s_%s' % (method_name, '_'.join(name_components))
+
+
+def augment_method_docstring(method, new_class_dict, classname, param_names, param_values, new_method):
+    param_assignments_str = '; '.join(['%s = %s' % (k, v) for (k, v) in zip(param_names, param_values)])
+    extra_doc = "%s (%s.%s) [with %s] " \
+        % (method.__name__, new_class_dict.get('__module__', '<module>'), classname, param_assignments_str)
+
+    try:
+        new_method.__doc__ = extra_doc + new_method.__doc__
+    except TypeError:  # Catches when new_method.__doc__ is None
+        new_method.__doc__ = extra_doc
+
+
+class ParameterizedTestCaseMetaClass(type):
+
+    def __new__(meta, classname, bases, class_dict):
+        new_class_dict = {}
+
+        for attr_name, attr_value in class_dict.items():
+            if callable(attr_value) and hasattr(attr_value, 'param_names'):
+                # print("Processing attr_name = %r; attr_value = %r" % (attr_name, attr_value))
+
+                method = attr_value
+                param_names = attr_value.param_names
+                data = attr_value.data
+
+                meta.process_method(classname, method, param_names, data, new_class_dict)
+            else:
+                new_class_dict[attr_name] = attr_value
+
+        return type.__new__(meta, classname, bases, new_class_dict)
+
+    @classmethod
+    def process_method(cls, classname, method, param_names, data, new_class_dict):
+        for param_values in data:
+            new_method = cls.new_method(method, param_values)
+            new_method.__name__ = get_decorated_method_name(method.__name__, param_names, param_values)
+            augment_method_docstring(method, new_class_dict, classname, param_names, param_values, new_method)
+            new_class_dict[new_method.__name__] = new_method
+
+
+    @classmethod
+    def new_method(cls, method, param_values):
+        @wraps(method)
+        def new_method(self):
+            return method(self, *param_values)
+
+        return new_method
+
+
+class ParameterizedTestMixin(object):
+    __metaclass__ = ParameterizedTestCaseMetaClass
+
+    @classmethod
+    def parameterize(cls, param_names, data):
+        """Decorator for parameterizing a test method - example:
+
+        @ParameterizedTestCase.parameterize(
+            ("isbn", "expected_title"), [
+                ("0262033844", "Introduction to Algorithms"),
+                ("0321558146", "Campbell Essential Biology")])
+
+        """
+
+        def decorator(func):
+            @wraps(func)
+            def newfunc(*arg, **kwargs):
+                return func(*arg, **kwargs)
+
+            newfunc.param_names = param_names
+            newfunc.data = data
+
+            return newfunc
+
+        return decorator
+
+
+class ParameterizedTestCase(unittest.TestCase, ParameterizedTestMixin):
+    __metaclass__ = ParameterizedTestCaseMetaClass
---
+++ packages/parameterizedtestcase/parameterizedtestcase-0.0.3.tar.gz/parameterizedtestcase-0.0.3/parameterizedtestcase/tests.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+from __future__ import with_statement
+
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+try:
+    from StringIO import StringIO  # Python 2
+except ImportError:
+    from io import StringIO as StringIO # Python 3
+
+from parameterizedtestcase import ParameterizedTestMixin, ParameterizedTestCase
+
+
+class MyParameterizedTestCase1(ParameterizedTestCase):
+    stringio = StringIO()
+
+    @ParameterizedTestCase.parameterize(("input", "expected_output"), [
+        ("2+4", 6),
+        ("3+5", 8),
+        ("6*9", 54),
+    ])
+    def test_eval(self, input, expected_output):
+        self.log_message("test_eval: input = %r; expected_output = %r\n" % (input, expected_output))
+        self.assertEqual(eval(input), expected_output)
+
+    def test_zzz_test_eval_called_multiple_times_with_correct_params(self):
+        self.assertEqual(
+            self.get_log_output(),
+            "\n".join([
+                "test_eval: input = '2+4'; expected_output = 6",
+                "test_eval: input = '3+5'; expected_output = 8",
+                "test_eval: input = '6*9'; expected_output = 54"]) + "\n")
+
+    def log_message(self, msg):
+        self.stringio.write(msg)
+
+    def get_log_output(self):
+        return self.stringio.getvalue()
+
+
+class MyParameterizedTestCase2(unittest.TestCase, ParameterizedTestMixin):
+
+    @ParameterizedTestMixin.parameterize(("numerator",), [(1,), (2,), (3,)])
+    def test_assertRaises(self, numerator):
+        """Test assertRaises with a context manager
+        
+        `assertRaises` is not present in `unittest.TestCase` in Python 2.5,
+        2.6, or 3.0, but it is present in unittest2.TestCase
+        
+        """
+        with self.assertRaises(ZeroDivisionError):
+            numerator / 0
+
+
+if __name__ == '__main__':
+    unittest.main()
