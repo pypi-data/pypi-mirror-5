60c117b4653544a22154e2edb1ed3c614b2056c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:01 +0000

    Add package livestatus-service/0.1.0/livestatus-service-0.1.0.tar.gz

---
+++ packages/livestatus-service/livestatus-service-0.1.0.tar.gz/livestatus-service-0.1.0/livestatus_service/__init__.py
@@ -0,0 +1,37 @@
+from __future__ import absolute_import
+__version__ = "0.1.0"
+'''
+    Livestatus-service wraps a MK-livestatus UNIX socket as a Flask application.
+    This is the initialization code for the application.
+'''
+
+import logging
+
+from .configuration import Configuration
+
+
+def initialize(config_file):
+    current_configuration = Configuration(config_file)
+    initialize_logging(current_configuration.log_file)
+
+
+def initialize_logging(log_file):
+    formatter = logging.Formatter("%(asctime)s [%(name)s] %(levelname)s: %(message)s")
+
+    log_file_handler = logging.FileHandler(log_file)
+    log_file_handler.setLevel(logging.DEBUG)
+    log_file_handler.setFormatter(formatter)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+    console_handler.setFormatter(formatter)
+
+    livestatus_logger = logging.getLogger("livestatus")
+    livestatus_logger.setLevel(logging.DEBUG)
+    livestatus_logger.addHandler(log_file_handler)
+    livestatus_logger.addHandler(console_handler)
+
+    werkzeug_logger = logging.getLogger("werkzeug")
+    werkzeug_logger.setLevel(logging.INFO)
+    werkzeug_logger.addHandler(log_file_handler)
+    werkzeug_logger.addHandler(console_handler)
---
+++ packages/livestatus-service/livestatus-service-0.1.0.tar.gz/livestatus-service-0.1.0/livestatus_service/configuration.py
@@ -0,0 +1,59 @@
+from __future__ import absolute_import
+try:
+    import ConfigParser
+    configparser = ConfigParser
+except ImportError:
+    import configparser
+
+
+def get_current_configuration():
+    configuration = Configuration('/etc/livestatus.cfg')
+    return configuration
+
+
+class Configuration(object):
+
+    DEFAULT_LOG_FILE = '/var/log/livestatus-service.log'
+    DEFAULT_LIVESTATUS_SOCKET = '/var/lib/nagios/rw/live'
+    DEFAULT_ICINGA_COMMAND_FILE = '/usr/local/icinga/var/rw/icinga.cmd'
+
+    OPTION_LOG_FILE = 'log_file'
+    OPTION_LIVESTATUS_SOCKET = 'livestatus_socket'
+    OPTION_ICINGA_COMMAND_FILE = 'icinga_command_file'
+
+    SECTION = 'livestatus-service'
+
+    def __init__(self, config_file_name):
+        self._config_parser = configparser.RawConfigParser()
+        self._load_config_file(config_file_name)
+        self._verify_config()
+
+    @property
+    def log_file(self):
+        return self._get_option(Configuration.OPTION_LOG_FILE, Configuration.DEFAULT_LOG_FILE)
+
+    @property
+    def livestatus_socket(self):
+        return self._get_option(Configuration.OPTION_LIVESTATUS_SOCKET, Configuration.DEFAULT_LIVESTATUS_SOCKET)
+
+    @property
+    def icinga_command_file(self):
+        return self._get_option(Configuration.OPTION_ICINGA_COMMAND_FILE, Configuration.DEFAULT_ICINGA_COMMAND_FILE)
+
+    def _get_option(self, option, default_value=None):
+        if not self._config_parser.has_option(Configuration.SECTION, option):
+            if default_value:
+                return default_value
+            raise ValueError("Missing configuration option '%s' in section '%s", option, Configuration.SECTION)
+        return self._config_parser.get(Configuration.SECTION, option)
+
+    def _load_config_file(self, config_file_name):
+        try:
+            if self._config_parser.read(config_file_name) != [config_file_name]:
+                raise ValueError("Failed to load config file '{0}'".format(config_file_name))
+        except configparser.Error as e:
+            raise ValueError('Error loading config file: {0}'.format(e))
+
+    def _verify_config(self):
+        if not self._config_parser.has_section(Configuration.SECTION):
+            raise ValueError("Invalid config file: No such section '{0}'".format(Configuration.SECTION))
