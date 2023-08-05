f1caf28b181a7760d48c6fd1744bdedb5c7519c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:32 +0000

    Add package mockredispy/2.7.2.5/mockredispy-2.7.2.5.tar.gz

---
+++ packages/mockredispy/mockredispy-2.7.2.5.tar.gz/mockredispy-2.7.2.5/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+# Match releases to redis-py versions
+__version__ = '2.7.2.5'
+
+# Jenkins will replace __build__ with a unique value.
+__build__ = ''
+
+setup(name='mockredispy',
+      version=__version__ + __build__,
+      description='Mock for redis-py',
+      url='http://www.github.com/locationlabs/mockredis',
+      license='Apache2',
+      packages=find_packages(exclude=['*.tests']),
+      setup_requires=[
+          'nose'
+      ],
+      install_requires=[
+      ],
+      tests_require=[
+          'redis>=2.7.2'
+      ],
+      test_suite='mockredis.tests',
+      )
