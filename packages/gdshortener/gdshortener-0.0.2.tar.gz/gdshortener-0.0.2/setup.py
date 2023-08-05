54229308a35869ba47434754072fb610bc729335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:01 +0000

    Add package gdshortener/0.0.2/gdshortener-0.0.2.tar.gz

---
+++ packages/gdshortener/gdshortener-0.0.2.tar.gz/gdshortener-0.0.2/setup.py
@@ -0,0 +1,29 @@
+import codecs
+from setuptools import setup
+
+long_description = codecs.open('README.rst', "r").read()
+
+setup(
+    name = "gdshortener",
+    version = "0.0.2",
+    author = "Gian Luca Dalla Torre",
+    author_email = "gianluca@gestionaleauto.com",
+    description = ("A module that provides access to .gd URL Shortener"),
+    license = "LGPL",
+    keywords = "url shortener gd",
+    url = "https://github.com/torre76/gd_shortener",
+    download_url = "https://github.com/torre76/gd_shortener/tarball/0.0.2",
+    packages=['gdshortener'],
+    long_description=long_description,
+    package_data = {
+        '': ['README.rst'],
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 2 :: Only"
+    ],
+)
