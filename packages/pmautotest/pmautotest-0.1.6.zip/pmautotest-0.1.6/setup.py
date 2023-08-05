a32a9aaaf201a0ea0ef7f1e0070c610ec366af0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:35 +0000

    Add package pmautotest/0.1.6/pmautotest-0.1.6.zip

---
+++ packages/pmautotest/pmautotest-0.1.6.zip/pmautotest-0.1.6/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+    name="pmautotest",
+    license="RA",
+    version="0.1.6",
+    description="Automation Test module",
+    author="Wenbin Wang",
+    author_email="wenbin.rc@gmail.com",
+    packages=["pmautotest"],
+    long_description="""AutoTest is a test platphome for RA,PowerMonitor development \s
+						team.""",
+    )
