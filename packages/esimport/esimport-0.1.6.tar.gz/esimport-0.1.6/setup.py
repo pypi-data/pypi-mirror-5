f1542df1718a8b616bc065c8ae71cdc576286055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:26 +0000

    Add package esimport/0.1.6/esimport-0.1.6.tar.gz

---
+++ packages/esimport/esimport-0.1.6.tar.gz/esimport-0.1.6/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(name="esimport",
+	version="0.1.6",
+	description="Facilitates the indexing of content from a CSV into ElasticSearch",
+	author="PlayNetwork, Inc.",
+	author_email="industrial@playnetwork.com",
+	url="https://github.com/playnetwork/esimport",
+	packages=["esimport"],
+	package_dir={"esimport" : "esimport"},
+	license='MIT license, see LICENSE.txt',
+	long_description=open('README').read(),
+	include_package_data=True,
+	install_requires=[
+		"rawes"
+	]
+)
