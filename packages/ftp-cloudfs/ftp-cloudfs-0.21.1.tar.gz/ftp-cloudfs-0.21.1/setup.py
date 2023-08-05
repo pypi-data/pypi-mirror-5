05ece3bdd70e64822edc09af36fd1f7b75849482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:50 +0000

    Add package ftp-cloudfs/0.21.1/ftp-cloudfs-0.21.1.tar.gz

---
+++ packages/ftp-cloudfs/ftp-cloudfs-0.21.1.tar.gz/ftp-cloudfs-0.21.1/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+from ftpcloudfs.constants import version
+
+def read(fname):
+    full_path = os.path.join(os.path.dirname(__file__), fname)
+    if os.path.exists(fname):
+        return open(full_path).read()
+    else:
+        return ""
+
+setup(name='ftp-cloudfs',
+      version=version,
+      download_url="http://pypi.python.org/packages/source/f/ftp-cloudfs/ftp-cloudfs-%s.tar.gz" % (version),
+      description='FTP interface to Rackspace Cloud Files and OpenStack Swift',
+      author='Chmouel Boudjnah',
+      author_email='chmouel.boudjnah@rackspace.co.uk',
+      url='https://github.com/chmouel/ftp-cloudfs',
+      long_description = read('README.rst'),
+      license='MIT',
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['pyftpdlib>=1.2.0', 'python-cloudfiles','python-daemon>=1.5.5', 'python-memcached'],
+      scripts=['bin/ftpcloudfs'],
+      packages = find_packages(exclude=['tests', 'debian']),
+      tests_require = ["nose"],
+      classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        'Environment :: No Input/Output (Daemon)',
+        'License :: OSI Approved :: MIT License',
+        ],
+      test_suite = "nose.collector",
+      )
