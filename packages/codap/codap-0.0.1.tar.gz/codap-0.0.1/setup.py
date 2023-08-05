e86a310372531aa92c95bdf90d450dfa91e85e1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:08 +0000

    Add package codap/0.0.1/codap-0.0.1.tar.gz

---
+++ packages/codap/codap-0.0.1.tar.gz/codap-0.0.1/setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+DESCRIPTION = """Coroutine Data Access Patterns (mouthful I know) are some data
+access patterns I found useful building web applications that use gevent or
+evenlet. They mimic common Python data types like lists and dictionaries."""
+setup(
+    name = 'codap',
+    version = '0.0.1',
+    author = 'Lateef Jackson',
+    author_email = 'lateef.jackson@gmail.com',
+    description = (DESCRIPTION),
+    license = 'BSD',
+    keywords = 'gevent eventlet coroutine ',
+    url = 'https://github.com/lateefj/codap',
+    packages=['codap', 'tests'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: BSD License',
+    ],
+)
