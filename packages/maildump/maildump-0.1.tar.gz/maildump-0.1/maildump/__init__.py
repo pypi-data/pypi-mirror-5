89fc4e8806434af3f10c0e185589196edaacb077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:25 +0000

    Add package maildump/0.1/maildump-0.1.tar.gz

---
+++ packages/maildump/maildump-0.1.tar.gz/maildump-0.1/maildump/__init__.py
@@ -0,0 +1,45 @@
+import asyncore
+import gevent
+from gevent.event import Event
+from logbook import Logger
+from socketio.server import SocketIOServer
+
+from maildump.db import connect, disconnect, create_tables
+from maildump.smtp import smtp_handler, SMTPServer
+from maildump.web import app
+from maildump.web_realtime import broadcast
+
+
+log = Logger(__name__)
+stopper = Event()
+socketio_server = None
+
+
+def start(http_host, http_port, smtp_host, smtp_port, db_path=None):
+    global socketio_server
+    # Webserver
+    log.debug('Starting web server')
+    socketio_server = SocketIOServer((http_host, http_port), app,
+                                     log='default' if app.debug else None)
+    socketio_server.start()
+    # SMTP server
+    log.debug('Starting smtp server')
+    SMTPServer((smtp_host, smtp_port), smtp_handler)
+    gevent.spawn(asyncore.loop)
+    # Database
+    connect(db_path)
+    create_tables()
+    # Wait....
+    try:
+        stopper.wait()
+    except KeyboardInterrupt:
+        print
+    else:
+        log.debug('Received stop signal')
+    # Clean up
+    disconnect()
+    log.info('Terminating')
+
+
+def stop():
+    stopper.set()
