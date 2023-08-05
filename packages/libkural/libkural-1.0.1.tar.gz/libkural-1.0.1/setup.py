e7f605b1a68fc78471efd4e8299f7c4936a75702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:38 +0000

    Add package libkural/1.0.1/libkural-1.0.1.tar.gz

---
+++ packages/libkural/libkural-1.0.1.tar.gz/libkural-1.0.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(name='libkural',
+      version='1.0.1',
+      description='Python library for accessing Tamil (UTF-8 encoded) literary work Thirukkural, with translations and commentary in English (G.U.Pope et-al version)',
+      author='Muthiah Annamalai',
+      author_email='',
+      url='https://github.com/arcturusannamalai/libkural',
+      py_modules=['libkural'],
+      license='BSD',
+      platforms='PC,Linux,Mac',
+      #classifiers='Tamil,UTF-8,literature,couplets',
+      download_url='https://pypi.python.org/pypi/libkural/1.0',#pip
+      )
+
