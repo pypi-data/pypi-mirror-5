cf6c875b845f461e2abb396d555362765332a0fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:15 +0000

    Add package Saycloud/0.2.2/Saycloud-0.2.2.tar.gz

---
+++ packages/Saycloud/Saycloud-0.2.2.tar.gz/Saycloud-0.2.2/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+PKG = 'Saycloud'
+VERSION = '0.2.2'
+
+requires = ['setuptools', 'soundcloud']
+scripts = ['bin/saycloud']
+
+setup(
+    name=PKG,
+    version=VERSION,
+    description="Saycloud - text to Soundcloud",
+    long_description=open('README.rst').read(),
+    author="Ross Duggan",
+    author_email="ross.duggan@acm.org",
+    url="http://github.com/duggan/saycloud",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requires,
+    scripts=scripts,
+    license="MIT License",
+    keywords="soundcloud, saycloud",
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+    ]
+)
