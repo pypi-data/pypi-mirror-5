a83c6f41bffbea174379557760ea7ba79d3bbe32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:26 +0000

    Add package django-ckeditor-fc/1.0/django-ckeditor-fc-1.0.zip

---
+++ packages/django-ckeditor-fc/django-ckeditor-fc-1.0.zip/django-ckeditor-fc-1.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-ckeditor-fc',
+    version='1.0',
+    description='Django admin CKEditor integration.',
+    long_description=open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst',
+                                                                                             'r').read(),
+    author='Future Colors (orginal author:Shaun Sephton)',
+    author_email='shaunsephton@gmail.com',
+    url='https://github.com/futurecolors/django-ckeditor',
+    packages=find_packages(),
+    dependency_links=[
+        'http://dist.plone.org/thirdparty/',
+    ],
+    install_requires=[
+        'PIL', 'pytils'
+    ],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
+)
