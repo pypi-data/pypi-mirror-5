20e64a77eb2ce66724d67c413c513768a6ddd8c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:05 +0000

    Add package Colonyzer2/1.0.54/Colonyzer2-1.0.54.zip

---
+++ packages/Colonyzer2/Colonyzer2-1.0.54.zip/Colonyzer2-1.0.54/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+setup(name='Colonyzer2',
+      version='1.0.54',
+      packages=find_packages(),
+      description='Image analysis for microbial cultures growing on solid agar surfaces',
+      long_description=open('README.txt').read(),
+      entry_points={"console_scripts":["independent = colonyzer2.independent:main","timecourse = colonyzer2.timecourse:main",
+                                       "parametryzer = colonyzer2.parameteryzer_script:main"]},
+      author='Conor Lawless',
+      author_email='conor.lawless@ncl.ac.uk',
+      url='http://research.ncl.ac.uk/colonyzer/',
+      py_modules=['colonyzer2'],
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Scientific/Engineering :: Image Recognition',
+        'Intended Audience :: Science/Research'
+        ]      
+      )
