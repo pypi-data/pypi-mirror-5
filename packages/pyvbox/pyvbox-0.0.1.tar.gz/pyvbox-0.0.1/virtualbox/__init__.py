403e6e6145efe8d82903fa24c1977676a8ab2932
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:56 +0000

    Add package pyvbox/0.0.1/pyvbox-0.0.1.tar.gz

---
+++ packages/pyvbox/pyvbox-0.0.1.tar.gz/pyvbox-0.0.1/virtualbox/__init__.py
@@ -0,0 +1,28 @@
+import vboxapi
+from virtualbox import library
+
+
+__doc__ = library.__doc__
+
+
+manager = vboxapi.VirtualBoxManager(None, None)
+
+
+class VirtualBox(library.IVirtualBox):
+    __doc__ = library.IVirtualBox.__doc__
+    def __init__(self, interface=None):
+        if interface is None:
+            self._i = manager.getVirtualBox()
+        else:
+            self._i = interface
+
+
+class Session(library.ISession):
+    __doc__ = library.ISession.__doc__
+    def __init__(self, interface=None):
+        if interface is None:
+            self._i = manager.platform.getSessionObject(None)
+        else:
+            self._i = interface
+
+
