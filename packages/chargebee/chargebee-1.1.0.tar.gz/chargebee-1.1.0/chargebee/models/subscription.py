eda58b85a583b24efda053ee86566f595f0b2f71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:21 +0000

    Add package chargebee/1.1.0/chargebee-1.1.0.tar.gz

---
+++ packages/chargebee/chargebee-1.1.0.tar.gz/chargebee-1.1.0/chargebee/models/subscription.py
@@ -0,0 +1,45 @@
+import json
+from chargebee.model import Model
+from chargebee import request
+from chargebee import APIError
+
+class Subscription(Model):
+    class Addon(Model):
+      fields = ["id", "quantity"]
+      pass
+    class Coupon(Model):
+      fields = ["coupon_id", "apply_till", "applied_count"]
+      pass
+
+    fields = ["id", "plan_id", "plan_quantity", "status", "trial_start", "trial_end", "current_term_start", \
+    "current_term_end", "remaining_billing_cycles", "created_at", "activated_at", "cancelled_at", \
+    "cancel_reason", "due_invoices_count", "due_since", "total_dues", "addons", "coupon", "coupons" ]
+
+
+    @staticmethod
+    def create(params, env=None):
+        return request.send('post', '/subscriptions', params, env)
+
+    @staticmethod
+    def list(params=None, env=None):
+        return request.send('get', '/subscriptions', params, env)
+
+    @staticmethod
+    def retrieve(id, env=None):
+        return request.send('get', '/subscriptions/%s' % id, None, env)
+
+    @staticmethod
+    def update(id, params=None, env=None):
+        return request.send('post', '/subscriptions/%s' % id, params, env)
+
+    @staticmethod
+    def change_term_end(id, params, env=None):
+        return request.send('post', '/subscriptions/%s/change_term_end' % id, params, env)
+
+    @staticmethod
+    def cancel(id, params=None, env=None):
+        return request.send('post', '/subscriptions/%s/cancel' % id, params, env)
+
+    @staticmethod
+    def reactivate(id, params=None, env=None):
+        return request.send('post', '/subscriptions/%s/reactivate' % id, params, env)
---
+++ packages/chargebee/chargebee-1.1.0.tar.gz/chargebee-1.1.0/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+requires = []
+try:
+    import json
+except ImportError:
+    try:
+        import simplejson
+    except ImportError:
+        requires.append('simplejson')  # For Python==2.5
+
+setup(
+    name='chargebee',
+    version='1.1.0',
+    author='ChargeBee',
+    author_email='support@chargebee.com',
+    url='https://apidocs.chargebee.com/docs/api?lang=python',
+    description='Subscription Billing API',
+    packages=find_packages(),
+    package_data={'chargebee': ['ssl/*.crt']},
+    install_requires=requires,
+    test_suite='tests',
+)
