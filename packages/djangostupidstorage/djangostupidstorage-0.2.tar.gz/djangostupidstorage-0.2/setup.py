9df6ab7049f9848a60ea514fc088ca0c2746eabe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:19 +0000

    Add package djangostupidstorage/0.2/djangostupidstorage-0.2.tar.gz

---
+++ packages/djangostupidstorage/djangostupidstorage-0.2.tar.gz/djangostupidstorage-0.2/setup.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+import re
+import os
+import sys
+
+
+install_requires = [
+    'django-rq>=0.4.6',
+    'easywebdav==1.0.7',
+]
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.match("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
+
+version = get_version('django_stupid_storage')
+
+
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist upload")
+    print("You probably want to also tag the version now:")
+    print("  git tag -a %s -m 'version %s'" % (version, version))
+    print("  git push --tags")
+    sys.exit()
+
+
+setup(
+    name='djangostupidstorage',
+    version=version,
+    url='https://github.com/irqed/django-stupid-storage',
+    download_url='http://pypi.python.org/pypi/djangostupidstorage/',
+    license='BSD',
+    description='Simple Django WebDAV storage with upload queue.',
+    author='Alexander Shchepetilnikov',
+    author_email='alex@irqed.com',
+    packages=get_packages('django_stupid_storage'),
+    package_data=get_package_data('django_stupid_storage'),
+    #test_suite='',
+    install_requires=install_requires,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
