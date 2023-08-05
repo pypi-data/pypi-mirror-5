406f3cbc2f7b9faaf2a7258ccf3135a3633a4abf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:24 +0000

    Add package BaseHash/1.0.6/BaseHash-1.0.6.zip

---
+++ packages/BaseHash/BaseHash-1.0.6.zip/BaseHash-1.0.6/setup.py
@@ -0,0 +1,50 @@
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import basehash
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+
+if sys.argv[-1] == 'prep':
+    os.system('python setup.py sdist')
+    sys.exit()
+
+
+setup(
+    name='BaseHash',
+    version=basehash.__version__,
+    description='Reversible obfuscated identifier hashes.',
+    long_description=open('README.md').read() + '\n\n' + open('HISTORY.md').read(),
+    author='Nathan Lucas',
+    author_email='nathan@bnlucas.com',
+    url='http://bnlucas.github.io/python-basehash',
+    download_url='https://github.com/bnlucas/python-basehash/archive/master.zip',
+    packages=['basehash'],
+    package_data={'': ['LICENSE'], 'tests': '*.py'},
+    package_dir={'basehash': 'basehash'},
+    include_package_data=True,
+    install_requires=[],
+    license=open('LICENSE').read(),
+    zip_safe=False,
+    keywords=['base', 'encoding', 'hash', 'hashing', 'security'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: Apache Software License',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Security',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities'
+    ],
+)
