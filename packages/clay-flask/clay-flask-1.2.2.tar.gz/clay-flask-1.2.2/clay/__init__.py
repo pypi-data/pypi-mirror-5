b1fafdd0ee609098e40e1e297f9c98f68c45a7b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:44 +0000

    Add package clay-flask/1.2.2/clay-flask-1.2.2.tar.gz

---
+++ packages/clay-flask/clay-flask-1.2.2.tar.gz/clay-flask-1.2.2/clay/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+__version__ = '1.2.2'
+
+from clay.server import app
+from clay import config, stats
+
+__all__ = ['app', 'config', 'stats']
---
+++ packages/clay-flask/clay-flask-1.2.2.tar.gz/clay-flask-1.2.2/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='clay-flask',
+    version='1.2.2',
+    author='Jeremy Grosser',
+    author_email='jeremy@uber.com',
+    packages=['clay'],
+    description='Clay is a framework for building RESTful backend services using best practices.',
+    install_requires=[
+        'flask',
+    ],
+    entry_points={
+        'console_scripts': [
+            'clay-devserver = clay.server:devserver',
+            'clay-celery = clay.celery:main',
+        ],
+    },
+)
