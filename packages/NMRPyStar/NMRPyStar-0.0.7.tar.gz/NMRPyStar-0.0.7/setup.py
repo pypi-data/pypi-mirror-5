782ea6c4f03c0a18fd5a2e6a8ca997033ef5df29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:07 +0000

    Add package NMRPyStar/0.0.7/NMRPyStar-0.0.7.tar.gz

---
+++ packages/NMRPyStar/NMRPyStar-0.0.7.tar.gz/NMRPyStar-0.0.7/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='NMRPyStar',
+    version='0.0.7',
+    packages=['nmrpystar', 'nmrpystar.parse', 'nmrpystar.simple'],
+    license='MIT',
+    author='Matt Fenwick',
+    author_email='mfenwick100@gmail.com',
+    url='https://github.com/mattfenwick/NMRPyStar',
+    description='a parser for the NMR-Star data format',
+    long_description = open('README.md').read(),
+)
