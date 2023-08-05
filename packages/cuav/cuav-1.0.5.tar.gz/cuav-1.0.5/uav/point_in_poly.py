8969ec53ea9257c8d2f8b38d26606c91df99b1a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:14 +0000

    Add package cuav/1.0.5/cuav-1.0.5.tar.gz

---
+++ packages/cuav/cuav-1.0.5.tar.gz/cuav-1.0.5/uav/point_in_poly.py
@@ -0,0 +1,16 @@
+
+class Polygon(object):
+    def __init__(self):
+        
+
+int pnpoly(int npol, float *xp, float *yp, float x, float y)
+{
+  int i, j, c = 0;
+  for (i = 0, j = npol-1; i < npol; j = i++) {
+    if ((((yp[i] <= y) && (y < yp[j])) ||
+         ((yp[j] <= y) && (y < yp[i]))) &&
+        (x < (xp[j] - xp[i]) * (y - yp[i]) / (yp[j] - yp[i]) + xp[i]))
+      c = !c;
+  }
+  return c;
+}
