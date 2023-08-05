b0e61d8037b24e7ce614d302cbb1b3ee4f929cb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:07 +0000

    Add package Flask-Injector/0.2.1/Flask-Injector-0.2.1.tar.gz

---
+++ packages/Flask-Injector/Flask-Injector-0.2.1.tar.gz/Flask-Injector-0.2.1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+long_description = open('README.rst').read()
+
+setup(
+    name='Flask-Injector',
+    version='0.2.1',
+    url='https://github.com/alecthomas/flask_injector',
+    license='BSD',
+    author='Alec Thomas',
+    author_email='alec@swapoff.org',
+    description='Adds Injector support to Flask.',
+    long_description=long_description,
+    py_modules=['flask_injector'],
+    zip_safe=True,
+    platforms='any',
+    install_requires=[
+        'Flask', 'injector',
+    ],
+)
