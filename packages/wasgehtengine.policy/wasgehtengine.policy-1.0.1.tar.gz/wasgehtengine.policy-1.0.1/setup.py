f27514e43f6040f18a287c846939f351f243d8fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:12 +0000

    Add package wasgehtengine.policy/1.0.1/wasgehtengine.policy-1.0.1.tar.gz

---
+++ packages/wasgehtengine.policy/wasgehtengine.policy-1.0.1.tar.gz/wasgehtengine.policy-1.0.1/setup.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import os
+
+version = '1.0.1'
+
+long_description = (
+    open('README.txt').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+setup(name='wasgehtengine.policy',
+      version=version,
+      description="Wasgehtengine Site Policy",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Fabian Köster',
+      author_email='koesterreich@fastmail.fm',
+      url='https://gitorious.org/wasgehtengine',
+      license='agpl',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['wasgehtengine', ],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Plone',
+          'wasgehtengine.contenttypes',
+          'wasgehtengine.import',
+          # -*- Extra requirements: -*-
+      ],
+      extras_require={'test': ['plone.app.testing']},
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      #setup_requires=["PasteScript"],
+      #paster_plugins=["templer.localcommands"],
+      )
