21fcc69f7db5f925d6745937ce481271614548b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:53 +0000

    Add package csvvalidator/1.2/csvvalidator-1.2.tar.gz

---
+++ packages/csvvalidator/csvvalidator-1.2.tar.gz/csvvalidator-1.2/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='csvvalidator',
+      version='1.2',
+      author='Alistair Miles',
+      author_email='alimanfoo@googlemail.com',
+      url='https://github.com/alimanfoo/csvvalidator',
+      license='MIT License',
+      py_modules=['csvvalidator'],
+      description='A simple library for validating data contained in CSV files or similar row-oriented data sources.',
+      long_description=open('README.txt').read(),
+      classifiers=['Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python',
+                   'Topic :: Software Development :: Libraries :: Python Modules'
+                   ]
+      )
