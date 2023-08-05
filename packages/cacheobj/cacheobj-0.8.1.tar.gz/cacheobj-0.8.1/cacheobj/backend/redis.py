ef3e3241ebdcd1e30cf83095b016a6fa1946bcaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:26 +0000

    Add package cacheobj/0.8.1/cacheobj-0.8.1.tar.gz

---
+++ packages/cacheobj/cacheobj-0.8.1.tar.gz/cacheobj-0.8.1/cacheobj/backend/redis.py
@@ -0,0 +1,28 @@
+
+from __future__ import absolute_import
+
+import redis
+from . import BaseBackend
+
+local_pool = redis.ConnectionPool()
+
+class RedisBackend(BaseBackend):
+    def __init__(self, pool=local_pool):
+        self.client = redis.Redis(connection_pool=pool)
+
+    def set(self, key, value, expiration=None, commit=True):
+        result = self.client.set(key, value)
+        if expiration:
+            self.client.expire(key, expiration)
+        if not result:
+            raise AttributeError("can't set attribute")
+        return result
+
+    def get(self, key, default=None):
+        value = self.client.get(key)
+        if value is None:
+            return default
+        return value
+
+    def delete(self, key, commit=True):
+        return self.client.delete(key)
