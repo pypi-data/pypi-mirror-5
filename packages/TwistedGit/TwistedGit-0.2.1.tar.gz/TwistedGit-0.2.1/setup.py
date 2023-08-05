9e7ce0fdbf39f5248eaf3f43db93fcc4e5aa2295
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:43 +0000

    Add package TwistedGit/0.2.1/TwistedGit-0.2.1.tar.gz

---
+++ packages/TwistedGit/TwistedGit-0.2.1.tar.gz/TwistedGit-0.2.1/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2011 Manuel Stocker <mensi@mensi.ch>
+#
+# This file is part of TwistedGit.
+#
+# TwistedGit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# TwistedGit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with TwistedGit.  If not, see http://www.gnu.org/licenses
+
+from setuptools import setup, find_packages
+
+setup(name='TwistedGit',
+      install_requires=['twisted', 'pycrypto', 'passlib', 'pyasn1'],
+      description=(
+                   'Twisted-based implementation of the '
+                   'network protocols supported by git'
+                   ' (git://, ssh:// and http://)'
+      ),
+      keywords='git ssh http',
+      version='0.2.1',
+      url='https://github.com/mensi/twistedgit',
+      license='GPL',
+      author='Manuel Stocker',
+      author_email='mensi@mensi.ch',
+      long_description=(
+                        'Twisted-based implementation of the '
+                        'network protocols supported by git'
+                        ' (git://, ssh:// and http://)'
+                        ),
+      packages=find_packages(),
+      zip_safe=True,
+      entry_points={'console_scripts': ['twistedgit = twistedgit:main']})
