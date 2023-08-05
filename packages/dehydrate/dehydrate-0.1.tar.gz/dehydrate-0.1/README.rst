1cf1fcec24c0576894c7f61dd5ab94f483c6b56b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:11 +0000

    Add package dehydrate/0.1/dehydrate-0.1.tar.gz

---
+++ packages/dehydrate/dehydrate-0.1.tar.gz/dehydrate-0.1/README.rst
@@ -0,0 +1,97 @@
+python-dehydrate
+----------------
+.. image:: https://travis-ci.org/l0kix2/python-dehydrate.png?branch=master
+    :target: https://travis-ci.org/l0kix2/python-dehydrate?branch=master
+
+.. image:: https://coveralls.io/repos/l0kix2/python-dehydrate/badge.png?branch=master
+    :target: https://coveralls.io/r/l0kix2/python-dehydrate?branch=master
+
+Small lib for representing python objects as a dicts.
+
+
+Motivation
+----------
+Why would you need library like this? One of obvious use cases is to prepare
+data for serializing (into json/yaml/xml/pickle/whatever). You can control
+dehydration process by describing how to fetch values from object and how to
+present it in dehydrated structure using simple syntax.
+
+
+Examples
+--------
+Simple case
+-----------
+In simplest of possible cases you just want get object, list wanted attributes
+and get mapping with keys based on attribute names and values from them.
+Use ``dehydrate`` shortcut for this case.::
+    >>> from dehydrate import dehydrate
+    >>> from examples import Person
+    >>> iron_man = Person(first_name='Tony', login='iron_man')
+    >>> dehydrated = dehydrate(obj=iron_man, fields=('first_name', 'login'))
+    >>> sorted(dehydrated.items())
+    [('first_name', 'Tony'), ('login', 'iron_man')]
+
+Some notes:
+  - I use list representation of dict in examples because it has predictable
+    order of items in it.
+  - In docs I will refer to ``examples`` package, which you can find in repo.
+
+But what if you want put ``first_name`` attribute in ``name`` key of resulted
+dict? Just specify both strings in fields.::
+    >>> from dehydrate import dehydrate
+    >>> from examples import Person
+    >>> iron_man = Person(first_name='Tony', login='iron_man')
+    >>> dehydrated = dehydrate(obj=iron_man, fields=(
+    ...     ('first_name', 'name'),
+    ...     'login',
+    ... ))
+    >>> sorted(dehydrated.items())
+    [('login', 'iron_man'), ('name', 'Tony')]
+
+
+Installation
+------------
+Simple ``pip install dehydrate`` must be fine.
+
+Requirements
+------------
+* six
+
+
+Philosophy
+----------
+* Easy things should be done easily.
+* Complex things must be possible.
+
+
+Testing
+-------
+Test written with use of `pytest`_ library and neat `pytest pep8 plugin`_.
+You should run ``python setup.py test`` for running full test suite or
+``coverage run --source=dehydrate setup.py test`` for tests with coverage.
+Tests automatically runs at `Travis CI`_. Examples in documentation are also
+picked by test command.
+
+.. _pytest: http://pytest.org/
+.. _pytest pep8 plugin: https://pypi.python.org/pypi/pytest-pep8
+.. _Travis CI: https://travis-ci.org/l0kix2/python-dehydrate?branch=master
+
+
+Contribution
+------------
+Any contribution is welcome. Use fork/pull request mechanism on github.
+
+If you add some code, you should add some tests, so coverage of master branch
+should always be 100%. Refer to Testing_ section for more instructions.
+
+Let me speak from my heart :). I will be very glad, if you correct my clumsy
+english phrases in docs and docstings or even advise more appropriate names
+for variables in code.
+
+
+TODO
+----
+* Think about giving opportunity to put results in Ordered dict instead of 
+  simple dict.
+* Add functionality for converting all values of some type using handlers on
+  dehydrator class.
---
+++ packages/dehydrate/dehydrate-0.1.tar.gz/dehydrate-0.1/dehydrate/fields.py
@@ -0,0 +1,171 @@
+# coding: utf-8
+"""
+This module contains fields classes. Each of them knows about structures
+which they can manipulate and how to fetch key and value for using in
+dehydrating process.
+This classes separated from Dehydrator class for ease of extending and adding
+new field types.
+"""
+from __future__ import unicode_literals
+
+import six
+from collections import Iterable, Mapping
+
+from .helpers import Registry
+from .exceptions import DehydrationException
+
+registry = Registry()
+
+# some handy shortcuts
+is_string = lambda val: isinstance(val, six.string_types)
+is_dict = lambda val: isinstance(val, Mapping)
+is_iterable = lambda val: isinstance(val, Iterable)
+is_pair = lambda val: is_iterable(val) and len(val) == 2
+
+
+class Field(object):
+
+    _target_info = None
+    _substitution = None
+
+    def __init__(self, dehydrator, spec):
+        self.dehydrator = dehydrator
+        self.spec = spec
+
+    @staticmethod
+    def is_relevant(spec):
+        """
+        Field classes knows about specification of structures, which are
+        relevant for them. So it can inform us whether this particular
+        specification is acceptable by redefining this method.
+        """
+        raise NotImplementedError
+
+    @property
+    def target_info(self):
+        if not self._target_info:
+            self._target_info, self._substitution = self.parse_spec()
+        return self._target_info
+
+    @property
+    def substitution(self):
+        if not self._substitution:
+            self._target_info, self._substitution = self.parse_spec()
+        return self._substitution
+
+    def parse_spec(self):
+        """
+        You should raise SpecParsingError if something goes wrong.
+        """
+        if is_pair(self.spec):
+            target_info, substitution = self.spec
+        else:
+            target_info = self.spec
+            substitution = None
+
+        try:
+            self.validate_target_info(target_info)
+        except Exception as e:
+            raise DehydrationException(description=str(e))
+
+        return target_info, substitution
+
+    def validate_target_info(self, target_info):
+        """
+        Hook for validate target info, i. e. check keys or attributes if it is
+        complex structure.
+        """
+        pass
+
+    def build_key(self):
+        return self.substitution or self.target
+
+    def resolve_target(self, obj, target_name):
+        # try to find getter method on dehydrator at first
+        dehydrator_getter_name = self.dehydrator.GETTER_PREFIX + target_name
+        if hasattr(self.dehydrator, dehydrator_getter_name):
+            return getattr(self.dehydrator, dehydrator_getter_name)
+
+        if hasattr(obj, target_name):
+            object_attribute = getattr(obj, target_name)
+            if callable(object_attribute):
+                return object_attribute
+            else:
+                return lambda: object_attribute
+
+        raise DehydrationException("Can't resolve target %s" % target_name)
+
+
+@registry.register
+class SimpleField(Field):
+
+    @staticmethod
+    def is_relevant(spec):
+        if is_string(spec):
+            return True
+        if is_pair(spec) and all(map(is_string, spec)):
+            return True
+        return False
+
+    @property
+    def target(self):
+        return self.target_info
+
+    def build_value(self, obj):
+        target_getter = self.resolve_target(obj=obj, target_name=self.target)
+        return target_getter()
+
+
+@registry.register
+class ComplexField(Field):
+
+    TARGET_FIELD_NAME = 'target'
+    ITERABLE_FLAG_FIELD_NAME = 'iterable'
+    DEHYDRATOR_FIELD_NAME = 'dehydrator'
+
+    @staticmethod
+    def is_relevant(spec):
+        if is_dict(spec):
+            return True
+        if is_pair(spec) and is_dict(spec[0]) and is_string(spec[1]):
+            return True
+        return False
+
+    @classmethod
+    def validate_target_info(cls, target_info):
+        if not cls.TARGET_FIELD_NAME in target_info:
+            message = (
+                "'{field_name}' field is required in specification "
+                "of {class_name}"
+            )
+            raise DehydrationException(description=message.format(
+                field_name=cls.TARGET_FIELD_NAME,
+                class_name=cls.__class__.__name__,
+            ))
+
+    def build_value(self, obj):
+        target_getter = self.resolve_target(obj=obj, target_name=self.target)
+        target = target_getter()
+
+        dehydrator = self.dehydrator_cls(fields=self.fields)
+        if self.is_iterable:
+            return map(dehydrator.dehydrate, target)
+        else:
+            return dehydrator.dehydrate(target)
+
+    @property
+    def target(self):
+        return self.target_info[self.TARGET_FIELD_NAME]
+
+    @property
+    def is_iterable(self):
+        return self.target_info.get(self.ITERABLE_FLAG_FIELD_NAME, False)
+
+    @property
+    def dehydrator_cls(self):
+        from dehydrate.base import Dehydrator
+        return self.target_info.get(self.DEHYDRATOR_FIELD_NAME, Dehydrator)
+
+    @property
+    def fields(self):
+        return self.target_info.get('fields')
