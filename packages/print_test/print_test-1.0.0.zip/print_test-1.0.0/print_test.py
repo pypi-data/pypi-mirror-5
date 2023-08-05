3ddcc895d4b481d151515828f825405af2b668e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:34 +0000

    Add package print_test/1.0.0/print_test-1.0.0.zip

---
+++ packages/print_test/print_test-1.0.0.zip/print_test-1.0.0/print_test.py
@@ -0,0 +1,2 @@
+def print_test():
+    print("这是一个pathon文件发布测试")
---
+++ packages/print_test/print_test-1.0.0.zip/print_test-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name='print_test',
+    version='1.0.0',
+    py_modules=['print_test'],
+    author='lidongwei',
+    author_email='lidongweio@126.com',
+    description='python_test',
+    )
