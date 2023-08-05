4932ae0c5cbc9169229e9f56a4657b997171e82f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:38 +0000

    Add package pyramid_mailer/0.12/pyramid_mailer-0.12.tar.gz

---
+++ packages/pyramid_mailer/pyramid_mailer-0.12.tar.gz/pyramid_mailer-0.12/README.txt
@@ -0,0 +1,8 @@
+pyramid_mailer is a package for sending email from your Pyramid application.
+It is compatible with Python 2.6. 2.7 and 3.2.
+
+See the documentation at
+http://docs.pylonsproject.org/projects/pyramid_mailer/en/latest/ for more info.
+
+pyramid_mailer uses code from the Lamson Project (http://lamsonproject.org/)
+with permission.  See the LICENSE.txt file for more information.
---
+++ packages/pyramid_mailer/pyramid_mailer-0.12.tar.gz/pyramid_mailer-0.12/setup.py
@@ -0,0 +1,85 @@
+"""
+pyramid_mailer is a package for taking the pain out of sending emails in your
+Pyramid project.  It is compatible with Python 2.5, 2.6, 2.7, and 3.2.
+
+This includes:
+
+1. Wrapping the low-level Python email library with an easy-to-use
+   API, which includes attachments and mulipart content.
+
+2. Send emails immediately or to add to a maildir queue.
+
+3. Managing email sends inside a transaction, to prevent emails being sent
+   if your code fails.
+
+4. Features to help with unit testing.
+
+pyramid_mailer uses the repoze_sendmail library for managing email sending
+and transacton management, and borrows code from Zed Shaw's Lamson for
+wrapping email messages.
+
+Links
+`````
+
+* `documentation
+  <http://docs.pylonsproject.org/projects/pyramid_mailer/en/latest/>`_
+* `development version
+  <https://github.com/Pylons/pyramid_mailer>`_
+
+"""
+
+from setuptools import setup
+
+docs_extras = [
+    'Sphinx',
+    'docutils',
+    'repoze.sphinx.autointerface',
+    ]
+
+tests_require = []
+
+testing_extras = tests_require + [
+    'nose',
+    'coverage',
+    ]
+
+setup(
+    name='pyramid_mailer',
+    version='0.12',
+    license='BSD',
+    author='Dan Jacob',
+    author_email='danjac354@gmail.com',
+    description='Sendmail package for Pyramid',
+    long_description=__doc__,
+    url="http://docs.pylonsproject.org/projects/pyramid_mailer/en/latest/",
+    packages=[
+        'pyramid_mailer',
+    ],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'pyramid',
+        'repoze.sendmail>=4.1',
+    ],
+    tests_require = tests_require,
+    extras_require = {
+        'testing':testing_extras,
+        'docs':docs_extras,
+        },
+    test_suite='pyramid_mailer',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        "Topic :: Communications :: Email",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Framework :: Pyramid",
+    ]
+)
