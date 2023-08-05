287c360a447874821bc6ea7f7419650c32cc8f64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:43 +0000

    Add package PyramidXmlRenderer/0.1.1/PyramidXmlRenderer-0.1.1.tar.gz

---
+++ packages/PyramidXmlRenderer/PyramidXmlRenderer-0.1.1.tar.gz/PyramidXmlRenderer-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='PyramidXmlRenderer',
+    version='0.1.1',
+    author='Alexander Vasilyev',
+    author_email='alexvassel@gmail.com',
+    packages=['pyramid_xml_renderer'],
+    url='http://pypi.python.org/pypi/pyramid_xml_renderer/',
+    description='Pyramid renderer to get xml output of the view and serializer to the xml format',
+    long_description=open('README.txt').read(),
+)
