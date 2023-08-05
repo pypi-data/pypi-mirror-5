9f69bc16272a71ee5d7bb47eb01d8d9cbf8d4aa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:32 +0000

    Add package smallinvoice/0.1.3/smallinvoice-0.1.3.tar.gz

---
+++ packages/smallinvoice/smallinvoice-0.1.3.tar.gz/smallinvoice-0.1.3/smallinvoice/assigns.py
@@ -0,0 +1,41 @@
+from smallinvoice import REQUEST_METHOD, BaseJsonEncodableObject
+
+assign_list = "assign/list"
+assign_details = "assign/get/id/%s"
+add_assign = "assign/add"
+delete_assign = "assign/delete/id/%s"
+update_assign = "assign/edit/id/%s"
+
+
+class Assign(BaseJsonEncodableObject):
+    def __init__(self, type, type_id, hours, date):
+        self.type = type
+        self.type_id = type_id
+        self.hours = hours
+        self.date = date
+
+
+class AssignClient(object):
+    def __init__(self, client):
+        self.client = client
+
+
+    def all(self):
+        """ returns all Assigns """
+        return self.client.request_with_method(assign_list)["items"]
+
+    def details(self, client_id):
+        """ returns the details to a specific assign """
+        return self.client.request_with_method(assign_details % (client_id,))[
+            "item"]
+
+    def add(self, client):
+        return self.client.request_with_method(add_assign, data=client)["id"]
+
+    def delete(self, assign_id):
+        return self.client.request_with_method(delete_assign % (assign_id,),
+                                               request_method=REQUEST_METHOD.POST)
+
+    def update(self, assign_id, assign):
+        return self.client.request_with_method(update_assign % (assign_id,),
+                                               data=assign)
---
+++ packages/smallinvoice/smallinvoice-0.1.3.tar.gz/smallinvoice-0.1.3/smallinvoice/tests/letters_test.py
@@ -0,0 +1,66 @@
+from smallinvoice import PREVIEW_SIZE
+from smallinvoice.client import *
+from smallinvoice.letters import *
+from smallinvoice.tests import TEST_API_TOKEN
+
+
+def test_letters():
+    client = Client(TEST_API_TOKEN)
+    result = client.letters.all()
+    assert len(result) > 0
+
+
+def test_letter_details():
+    client = Client(TEST_API_TOKEN)
+    details = client.letters.details(32497)
+    print details
+    assert details["title"] == "Python-Update"
+
+
+def test_letter_pdf():
+    client = Client(TEST_API_TOKEN)
+    pdf = client.letters.pdf(32497)
+    assert len(pdf) > 0
+
+
+def test_letter_preview():
+    client = Client(TEST_API_TOKEN)
+    preview = client.letters.preview(32497, 1, PREVIEW_SIZE.SMALL)
+    assert len(preview) > 0
+
+
+# def test_add_letter():
+#     l = Letter(client_id=24124, client_address_id=24183, date="2013-01-04",
+#                title="Python-Test")
+#     client = Client(TEST_API_TOKEN)
+#     letter_id = client.letters.add(l)
+#     details = client.letters.details(letter_id)
+#     assert details["title"] == "Python-Test"
+#     client.letters.delete(letter_id)
+
+
+def test_update_letter():
+    l = Letter(client_id=24124, client_address_id=24183, date="2013-01-04",
+               title="Python-Update")
+    l.id = 32497
+    client = Client(TEST_API_TOKEN)
+    client.letters.update(l.id, l)
+    details = client.letters.details(l.id)
+    assert details["title"] == "Python-Update"
+
+
+def test_email_letter():
+    r = Recipient(cc=False, email="wild.etienne@gmail.com", name="Test Name")
+    m = Mail(subject="Testsubject", body="Test email body", sendstatus=1,
+             afterstatus=1, recipients=[r])
+    m.id = 32497
+    client = Client(TEST_API_TOKEN)
+    client.letters.email(m.id, m)
+    assert True
+
+
+def test_status_letter():
+    s = State(status=State.DRAFT)
+    client = Client(TEST_API_TOKEN)
+    client.letters.status(32497, status=s)
+    assert client.letters.details(32497)["status"] == 7
