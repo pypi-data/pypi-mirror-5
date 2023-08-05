1e81cd98661c0dc8b1615cc1f82625eba597fd62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:51 +0000

    Add package hipsaint/0.4.1/hipsaint-0.4.1.tar.gz

---
+++ packages/hipsaint/hipsaint-0.4.1.tar.gz/hipsaint-0.4.1/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+version = __import__('hipsaint').__version__
+
+setup(
+    name="hipsaint",
+    version=version,
+    description="A command line tool for pushing Nagios host and service notifications to a HipChat room.",
+    author="Hannes Ljungberg",
+    author_email="hannes@5monkeys.se",
+    url="http://github.com/hannseman/hipsaint",
+    download_url="https://github.com/hannseman/hipsaint/tarball/%s" % (version,),
+    keywords=["nagios", "hipchat", "api", "plugin"],
+    license="WTFPL",
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 3 - Alpha",
+        "Natural Language :: English",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Utilities"
+    ],
+
+    zip_safe=False,
+    packages=find_packages(exclude=["examples"]),
+    include_package_data=True,
+    test_suite='hipsaint.tests',
+    tests_require=['mock'],
+    install_requires=[
+        "Jinja2==2.6",
+        "requests==1.1.0"
+    ],
+    entry_points="""
+    [console_scripts]
+    hipsaint=hipsaint.bin.commands:main
+    """
+)
