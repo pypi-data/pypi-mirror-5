3dfa0e9401e61ed99a423ddf69ce317d8d38e710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:04 +0000

    Add package pybossa-client/0.4.0/pybossa-client-0.4.0.tar.gz

---
+++ packages/pybossa-client/pybossa-client-0.4.0.tar.gz/pybossa-client-0.4.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'pybossa-client',
+    version = '0.4.0',
+    packages = find_packages(),
+    install_requires = ['requests>=0.13.0'],
+    # metadata for upload to PyPI
+    author = 'Open Knowledge Foundation Labs',
+    # TODO: change
+    author_email = 'okfnlabs@okfn.org',
+    description = 'pybossa-client is a tiny Python library makes it easy to work with PyBossa.',
+    long_description = '''
+''',
+    license = 'MIT',
+    url = 'https://github.com/pybossa/pybossa-client',
+    download_url = 'https://github.com/PyBossa/pybossa-client/zipball/master',
+    include_package_data = True,
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    entry_points = '''
+'''
+)
+
