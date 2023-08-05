a57a4f12f51913e86680236080a40dea8561c161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:27 +0000

    Add package devpi-server/0.9.0/devpi-server-0.9.0.tar.gz

---
+++ packages/devpi-server/devpi-server-0.9.0.tar.gz/devpi-server-0.9.0/test_devpi_server/test_db.py
@@ -0,0 +1,195 @@
+
+import py
+import os
+import pytest
+
+@pytest.fixture(params=[(), ("root/pypi",)])
+def bases(request):
+    return request.param
+
+def test_create_zipfile(tmpdir):
+    content = create_zipfile({"one": {"nested": "1"}, "two": {}})
+    tmpdir.join("hello.zip").write(content, "wb")
+
+
+class TestStage:
+    @pytest.fixture
+    def stage(self, request, db):
+        config = dict(user="hello", index="world", bases=(),
+                      type="stage", volatile=True)
+        if "bases" in request.fixturenames:
+            config["bases"] = request.getfuncargvalue("bases")
+        db.user_indexconfig_set(**config)
+        return db.getstage(user=config["user"], index=config["index"])
+
+    def test_create_and_delete(self, db):
+        db.user_indexconfig_set(user="hello", index="world", bases=(),
+                                type="stage", volatile=False)
+        db.user_indexconfig_set(user="hello", index="world2", bases=(),
+                                type="stage", volatile=False)
+        db.user_indexconfig_delete(user="hello", index="world2")
+        assert not db.user_indexconfig_get(user="hello", index="world2")
+        assert db.user_indexconfig_get(user="hello", index="world")
+
+    def test_not_configured_index(self, db):
+        stagename = "hello/world"
+        assert not db.getindexconfig(stagename)
+        assert not db.getstage(stagename)
+
+    def test_empty(self, stage, bases):
+        assert not stage.getreleaselinks("someproject")
+        assert not stage.getprojectnames()
+
+    def test_inheritance_simple(self, httpget, stage):
+        stage.configure(bases=("root/pypi",))
+        httpget.setextsimple("someproject",
+            "<a href='someproject-1.0.zip' /a>")
+        entries = stage.getreleaselinks("someproject")
+        assert len(entries) == 1
+        assert stage.getprojectnames() == ["someproject",]
+
+    def test_inheritance_error(self, httpget, stage):
+        stage.configure(bases=("root/pypi",))
+        httpget.setextsimple("someproject", status_code = -1)
+        entries = stage.getreleaselinks("someproject")
+        assert entries == -1
+        #entries = stage.getprojectnames()
+        #assert entries == -1
+
+    def test_store_and_get_releasefile(self, stage, bases):
+        content = "123"
+        content2 = "1234"
+        entry = stage.store_releasefile("some-1.0.zip", content)
+        entries = stage.getreleaselinks("some")
+        assert len(entries) == 1
+        assert entries[0].md5 == entry.md5
+        assert stage.getprojectnames() == ["some"]
+
+    def test_storedoczipfile(self, stage, bases):
+        content = create_zipfile({"index.html": "<html/>",
+            "_static": {}, "_templ": {"x.css": ""}})
+        filepath = stage.store_doczip("pkg1", content)
+        assert filepath.join("index.html").check()
+        assert filepath.join("_static").check(dir=1)
+        assert filepath.join("_templ", "x.css").check(file=1)
+
+    def test_storedoczipfile(self, stage, bases):
+        content = create_zipfile({"index.html": "<html/>",
+            "_static": {}, "_templ": {"x.css": ""}})
+        filepath = stage.store_doczip("pkg1", content)
+        content = create_zipfile({"nothing": "hello"})
+        filepath = stage.store_doczip("pkg1", content)
+        assert filepath.join("nothing").check()
+        assert not filepath.join("index.html").check()
+        assert not filepath.join("_static").check()
+        assert not filepath.join("_templ").check()
+
+    def test_store_and_get_volatile(self, stage):
+        stage.configure(volatile=False)
+        content = "123"
+        content2 = "1234"
+        entry = stage.store_releasefile("some-1.0.zip", content)
+        assert len(stage.getreleaselinks("some")) == 1
+
+        # rewrite  fails
+        entry = stage.store_releasefile("some-1.0.zip", content2)
+        assert entry == 409
+
+        # rewrite succeeds with volatile
+        stage.configure(volatile=True)
+        entry = stage.store_releasefile("some-1.0.zip", content2)
+        entries = stage.getreleaselinks("some")
+        assert len(entries) == 1
+        assert entries[0].FILE.get() == content2
+
+    def test_releasedata(self, stage):
+        assert stage.metadata_keys
+        assert not stage.get_metadata("hello", "1.0")
+        stage.register_metadata(dict(name="hello", version="1.0", author="xy"))
+        d = stage.get_metadata("hello", "1.0")
+        assert d["author"] == "xy"
+
+    def test_releasedata_description(self, stage):
+        source = py.std.textwrap.dedent("""\
+            test the *world*
+        """)
+        assert stage.metadata_keys
+        assert not stage.get_description("hello", "1.0")
+        stage.register_metadata(dict(name="hello", version="1.0",
+            description=source))
+        html = stage.get_description("hello", "1.0")
+        assert html
+        assert "test" in html and "world" in html
+
+    def test_releasedata_description_versions(self, stage):
+        stage.register_metadata(dict(name="hello", version="1.0",
+            description="hello"))
+        stage.register_metadata(dict(name="hello", version="1.1",
+            description="hello"))
+        ver = stage.get_description_versions("hello")
+        assert set(ver) == set(["1.0", "1.1"])
+
+
+class TestUsers:
+    def test_create_and_validate(self, db):
+        assert not db.user_exists("user")
+        db.user_create("user", "password", email="some@email.com")
+        assert db.user_exists("user")
+        userconfig = db.user_get("user")
+        assert userconfig["email"] == "some@email.com"
+        assert not set(userconfig).intersection(["pwsalt", "pwhash"])
+        userconfig = db.user_get("user")
+        assert db.user_validate("user", "password")
+        assert not db.user_validate("user", "password2")
+
+    def test_create_and_delete(self, db):
+        db.user_setpassword("user", "password")
+        db.user_delete("user")
+        assert not db.user_exists("user")
+        assert not db.user_validate("user", "password")
+
+    def test_create_and_list(self, db):
+        baselist = db.user_list()
+        db.user_setpassword("user1", "password")
+        db.user_setpassword("user2", "password")
+        db.user_setpassword("user3", "password")
+        newusers = db.user_list().difference(baselist)
+        assert newusers == set("user1 user2 user3".split())
+        db.user_delete("user3")
+        newusers = db.user_list().difference(baselist)
+        assert newusers == set("user1 user2".split())
+
+def test_setdefault_indexes(db):
+    from devpi_server.main import set_default_indexes
+    set_default_indexes(db)
+    ixconfig = db.getindexconfig("root/pypi")
+    assert ixconfig["type"] == "mirror"
+
+    ixconfig = db.getindexconfig("root/dev")
+    assert ixconfig["type"] == "stage"
+    assert ixconfig["bases"] == ("root/pypi",)
+    assert ixconfig["volatile"]
+
+
+def create_zipfile(contentdict):
+    f = py.io.BytesIO()
+    zip = py.std.zipfile.ZipFile(f, "w")
+    _writezip(zip, contentdict)
+    zip.close()
+    return f.getvalue()
+
+def _writezip(zip, contentdict, prefixes=()):
+    for name, val in contentdict.items():
+        if isinstance(val, dict):
+            newprefixes = prefixes + (name,)
+            if not val:
+                path = os.sep.join(newprefixes) + "/"
+                zipinfo = py.std.zipfile.ZipInfo(path)
+                zip.writestr(zipinfo, "")
+            else:
+                _writezip(zip, val, newprefixes)
+        else:
+            path = os.sep.join(prefixes + (name,))
+            zip.writestr(path, val)
+
+
---
+++ packages/devpi-server/devpi-server-0.9.0.tar.gz/devpi-server-0.9.0/test_devpi_server/test_extpypi.py
@@ -0,0 +1,351 @@
+import mock
+import py
+import pytest
+
+from devpi_server.extpypi import *
+from devpi_server.main import FatalResponse
+
+class TestIndexParsing:
+    simplepy = DistURL("http://pypi.python.org/simple/py/")
+
+    def test_parse_index_simple(self):
+        result = parse_index(self.simplepy,
+            """<a href="../../pkg/py-1.4.12.zip#md5=12ab">qwe</a>""")
+        link, = result.releaselinks
+        assert link.basename == "py-1.4.12.zip"
+        assert link.md5 == "12ab"
+
+    def test_parse_index_simple_nocase(self):
+        simplepy = DistURL("http://pypi.python.org/simple/Py/")
+        result = parse_index(simplepy,
+            """<a href="../../pkg/py-1.4.12.zip#md5=12ab">qwe</a>
+               <a href="../../pkg/PY-1.4.13.zip">qwe</a>
+               <a href="../../pkg/pyzip#egg=py-dev">qwe</a>
+        """)
+        assert len(result.releaselinks) == 3
+
+    def test_parse_index_simple_nomatch(self):
+        result = parse_index(self.simplepy,
+            """<a href="../../pkg/py-1.3.html">qwe</a>""")
+        assert not result.releaselinks
+
+    @pytest.mark.parametrize("rel", ["homepage", "download"])
+    def test_parse_index_with_rel(self, rel):
+        result = parse_index(self.simplepy, """
+               <a href="http://pylib.org" rel="%s">whatever</a>
+               <a href="http://pylib2.org" rel="%s">whatever2</a>
+               <a href="http://pylib3.org">py-1.0.zip</a>
+               <a href="http://pylib2.org/py-1.0.zip" rel="%s">whatever2</a>
+        """ % (rel,rel, rel))
+        assert len(result.releaselinks) == 1
+        link, = result.releaselinks
+        assert link == "http://pylib2.org/py-1.0.zip"
+        assert len(result.crawllinks) == 2
+        assert result.crawllinks == \
+                    set(["http://pylib.org", "http://pylib2.org"])
+
+    def test_parse_index_with_egg(self):
+        result = parse_index(self.simplepy,
+            """<a href="http://bb.org/download/py.zip#egg=py-dev" />
+               <a href="http://bb.org/download/py-1.0.zip" />
+               <a href="http://bb.org/download/py.zip#egg=something-dev" />
+        """)
+        assert len(result.releaselinks) == 2
+        link, link2 = result.releaselinks
+        assert link.basename == "py.zip"
+        assert link.eggfragment == "py-dev"
+        assert link2.basename == "py-1.0.zip"
+
+    def test_parse_index_with_two_eggs_ordering(self):
+        # it seems that pip/easy_install in some cases
+        # rely on the exact ordering of eggs in the html page
+        # for example with nose, there are two eggs and the second/last
+        # one is chosen due to the internal pip/easy_install algorithm
+        result = parse_index(self.simplepy,
+            """<a href="http://bb.org/download/py.zip#egg=py-dev" />
+               <a href="http://other/master#egg=py-dev" />
+        """)
+        assert len(result.releaselinks) == 2
+        link1, link2 = result.releaselinks
+        assert link1.basename == "master"
+        assert link1.eggfragment == "py-dev"
+        assert link2.basename == "py.zip"
+        assert link2.eggfragment == "py-dev"
+
+    def test_parse_index_with_matchingprojectname_no_version(self):
+        result = parse_index(self.simplepy,
+            """<a href="http://bb.org/download/py.zip" />
+            <a href="http://bb.org/download/py-1.0.zip" />""")
+        assert len(result.releaselinks) == 1
+
+    def test_releasefile_and_scrape(self):
+        result = parse_index(self.simplepy,
+            """<a href="../../pkg/py-1.4.12.zip#md5=12ab">qwe</a>
+               <a href="http://pylib.org" rel="homepage">whatever</a>
+               <a href="http://pylib2.org" rel="download">whatever2</a>
+        """)
+        assert len(result.releaselinks) == 1
+        assert len(result.crawllinks) == 2
+        result.parse_index(DistURL("http://pylib.org"), """
+               <a href="http://pylib.org/py-1.1.egg" />
+               <a href="http://pylib.org/other" rel="download" />
+        """, scrape=False)
+        assert len(result.crawllinks) == 2
+        assert len(result.releaselinks) == 2
+        links = list(result.releaselinks)
+        assert links[0].url == \
+                "http://pypi.python.org/pkg/py-1.4.12.zip#md5=12ab"
+        assert links[1].url == "http://pylib.org/py-1.1.egg"
+
+    def test_releasefile_md5_matching_and_ordering(self):
+        """ check that md5-links win over non-md5 links anywhere.
+        And otherwise the links from the index page win over scraped ones.
+        """
+        result = parse_index(self.simplepy,
+            """<a href="../../pkg/py-1.4.12.zip#md5=12ab">qwe</a>
+               <a href="../../pkg/py-1.4.11.zip">qwe</a>
+               <a href="../../pkg/py-1.4.10.zip#md5=2222">qwe</a>
+               <a href="http://pylib.org" rel="homepage">whatever</a>
+               <a href="http://pylib2.org" rel="download">whatever2</a>
+        """)
+        assert len(result.releaselinks) == 3
+        assert len(result.crawllinks) == 2
+        result.parse_index(DistURL("http://pylib.org"), """
+               <a href="http://pylib.org/py-1.4.12.zip" />
+               <a href="http://pylib.org/py-1.4.11.zip#md5=1111" />
+               <a href="http://pylib.org/py-1.4.10.zip#md5=12ab" />
+        """, scrape=False)
+        assert len(result.crawllinks) == 2
+        assert len(result.releaselinks) == 3
+        link1, link2, link3 = result.releaselinks
+        assert link1.url == \
+                "http://pypi.python.org/pkg/py-1.4.12.zip#md5=12ab"
+        assert link2.url == \
+                "http://pylib.org/py-1.4.11.zip#md5=1111"
+        assert link3.url == \
+                "http://pypi.python.org/pkg/py-1.4.10.zip#md5=2222"
+
+class TestExtPYPIDB:
+    def test_parse_project_nomd5(self, extdb):
+        extdb.setextsimple("pytest", text='''
+            <a href="../../pkg/pytest-1.0.zip#md5=123" />''')
+        links = extdb.getreleaselinks("pytest")
+        link, = links
+        assert link.url == "https://pypi.python.org/pkg/pytest-1.0.zip"
+        assert link.md5 == "123"
+        assert link.relpath.endswith("/pytest-1.0.zip")
+
+    def test_parse_and_scrape(self, extdb):
+        extdb.setextsimple("pytest", text='''
+                <a href="../../pkg/pytest-1.0.zip#md5=123" />
+                <a rel="download" href="https://download.com/index.html" />
+            ''', pypiserial=20)
+        extdb.url2response["https://download.com/index.html"] = dict(
+            status_code=200, text = '''
+                <a href="pytest-1.1.tar.gz" /> ''',
+            headers = {"content-type": "text/html"})
+        links = extdb.getreleaselinks("pytest")
+        assert len(links) == 2
+        assert links[0].url == "https://download.com/pytest-1.1.tar.gz"
+        assert links[0].relpath.endswith("/pytest-1.1.tar.gz")
+
+        # check refresh
+        extdb.setextsimple("pytest", text='''
+                <a href="../../pkg/pytest-1.0.1.zip#md5=456" />
+                <a href="../../pkg/pytest-1.0.zip#md5=123" />
+                <a rel="download" href="https://download.com/index.html" />
+            ''', pypiserial=25)
+        assert len(extdb.getreleaselinks("pytest")) == 2  # no refresh
+        links = extdb.getreleaselinks("pytest", refresh=25)
+        assert len(links) == 3
+        assert links[1].url == "https://pypi.python.org/pkg/pytest-1.0.1.zip"
+        assert links[1].relpath.endswith("/pytest-1.0.1.zip")
+
+    def test_parse_and_scrape_non_html_ignored(self, extdb):
+        extdb.setextsimple("pytest", text='''
+                <a href="../../pkg/pytest-1.0.zip#md5=123" />
+                <a rel="download" href="https://download.com/index.html" />
+            ''', pypiserial=20)
+        extdb.url2response["https://download.com/index.html"] = dict(
+            status_code=200, text = '''
+                <a href="pytest-1.1.tar.gz" /> ''',
+            headers = {"content-type": "text/plain"})
+        links = extdb.getreleaselinks("pytest")
+        assert len(links) == 1
+
+    def test_getreleaselinks_cache_refresh_semantics(self, extdb):
+        extdb.setextsimple("pytest", text='''
+                <a href="../../pkg/pytest-1.0.zip#md5=123" />
+                <a rel="download" href="https://download.com/index.html" />
+            ''', pypiserial=10)
+
+        # check getreleaselinks properly returns -2 on stale cache returns
+        ret = extdb.getreleaselinks("pytest", refresh=11)
+        assert ret == -2
+        ret = extdb.getreleaselinks("pytest", refresh=10)
+        assert len(ret) == 1
+
+        # disable httpget and see if we still get releaselinks for lower
+        # refresh serials
+        extdb.httpget = None
+        ret = extdb.getreleaselinks("pytest", refresh=9)
+        assert len(ret) == 1
+
+
+    @pytest.mark.parametrize("errorcode", [404, -1, -2])
+    def test_parse_and_scrape_error(self, extdb, errorcode):
+        extdb.setextsimple("pytest", text='''
+                <a href="../../pkg/pytest-1.0.zip#md5=123" />
+                <a rel="download" href="https://download.com/index.html" />
+            ''')
+        extdb.url2response["https://download.com/index.html"] = dict(
+            status_code=errorcode, text = 'not found')
+        links = extdb.getreleaselinks("pytest")
+        assert len(links) == 1
+        assert links[0].url == \
+                "https://pypi.python.org/pkg/pytest-1.0.zip"
+
+    def test_scrape_not_recursive(self, extdb):
+        extdb.setextsimple("pytest", text='''
+                <a rel="download" href="https://download.com/index.html" />
+            ''')
+        extdb.url2response["https://download.com/index.html"] = dict(
+            status_code=200, text = '''
+                <a href="../../pkg/pytest-1.0.zip#md5=123" />
+                <a rel="download" href="http://whatever.com" />''',
+            headers = {"content-type": "text/html"},
+        )
+        extdb.url2response["https://whatever.com"] = dict(
+            status_code=200, text = '<a href="pytest-1.1.zip#md5=123" />')
+        links = extdb.getreleaselinks("pytest")
+        assert len(links) == 1
+
+    def xxxtest_scrape_redirect_issue6(self, extdb):
+        extdb.url2response["http://github/path"] = dict(
+            status_code=302, headers=dict(location="/path"), content='''
+                <a rel="download" href="https://download.com/index.html" />
+            ''')
+        extdb.url2response["https://download.com/index.html"] = dict(
+            status_code=200, text = '''
+                <a href="../../pkg/pytest-1.0.zip#md5=123" />
+                <a rel="download" href="http://whatever.com" />'''
+        )
+        extdb.url2response["https://whatever.com"] = dict(
+            status_code=200, text = '<a href="pytest-1.1.zip#md5=123" />')
+        links = extdb.getreleaselinks("pytest")
+        assert len(links) == 1
+
+    def test_getprojectnames(self, extdb):
+        extdb.setextsimple("proj1", text='''
+                           <a href="../../pkg/proj1-1.0.zip#md5=123" /> ''')
+        extdb.setextsimple("proj2", text='''
+                           <a href="../../pkg/proj2-1.0.zip#md5=123" /> ''')
+        extdb.url2response["https://pypi.python.org/simple/proj3/"] = dict(
+            status_code=404)
+        assert len(extdb.getreleaselinks("proj1")) == 1
+        assert len(extdb.getreleaselinks("proj2")) == 1
+        assert extdb.getreleaselinks("proj3") == 404
+        names = extdb.getprojectnames()
+        assert names == ["proj1", "proj2"]
+
+
+def raise_ValueError():
+    raise ValueError(42)
+
+class TestRefreshManager:
+
+    def test_pypichanges_nochanges(self, extdb, keyfs):
+        proxy = mock.create_autospec(XMLProxy)
+        proxy.list_packages_with_serial.return_value = {"hello": 10}
+        proxy.changelog_since_serial.return_value = []
+        with pytest.raises(ValueError):
+            extdb.spawned_pypichanges(proxy, proxysleep=raise_ValueError)
+        proxy.list_packages_with_serial.assert_called_once_with()
+        assert keyfs.PYPISERIALS.get() == {"hello": 10}
+
+    def test_pypichanges_changes(self, extdb, httpget, keyfs, monkeypatch):
+        keyfs.PYPISERIALS.set({"pytest": 20})
+        httpget.setextsimple("pytest", '<a href="pytest-2.3.tgz"/a>',
+                             pypiserial=20)
+        assert len(extdb.getreleaselinks("pytest")) == 1
+        proxy = mock.create_autospec(XMLProxy)
+        proxy.changelog_since_serial.return_value = [
+            ["pylib", "1.4", 12123, 'new release', 11],
+            ["pytest", "2.4", 121231, 'new release', 27]]
+        httpget.setextsimple("pytest", '<a href="pytest-2.4.tgz"/a>',
+                             pypiserial=27)
+        with pytest.raises(ValueError):
+            extdb.spawned_pypichanges(proxy, proxysleep=raise_ValueError)
+        assert not proxy.list_packages_with_serial.called
+        assert keyfs.PYPISERIALS.get()["pytest"] == 27
+        assert extdb.getreleaselinks("pytest")[0].basename == "pytest-2.4.tgz"
+
+    @pytest.fixture(params=["protocol", "socket"])
+    def raise_error(self, request):
+        import socket
+        from xmlrpclib import ProtocolError
+        if request.param == "protocol":
+            exc = ProtocolError("http://pypi.python.org/pypi", 503, "", {})
+        else:
+            exc = socket.error(111)
+        def raise_error():
+            raise exc
+        return raise_error
+
+    def test_changelog_since_serial_nonetwork(self, extdb,
+                    keyfs, raise_error, monkeypatch, caplog):
+        from xmlrpclib import ServerProxy
+        got = []
+        keyfs.PYPISERIALS.set({"pytest": 10})
+        def raise_xmlrpcish(since_int):
+            got.append(since_int)
+            raise_error()
+        serverproxy = mock.Mock()
+        serverproxy.changelog_since_serial.side_effect = raise_xmlrpcish
+        xmlproxy = XMLProxy(serverproxy)
+        with pytest.raises(ValueError):
+            extdb.spawned_pypichanges(xmlproxy, proxysleep=raise_ValueError)
+        with pytest.raises(ValueError):
+            extdb.spawned_pypichanges(xmlproxy, proxysleep=raise_ValueError)
+        assert got == [10,10]
+        assert caplog.getrecords(".*since_serial.*error.*")
+
+    def test_changelog_list_packages_no_network(self, extdb,
+            keyfs, raise_error, monkeypatch, caplog):
+        from xmlrpclib import ProtocolError, ServerProxy
+
+        proxyanswers = [None]
+        loops = []
+        def proxysleep():
+            loops.append(1)
+            if not proxyanswers.pop():
+                raise ValueError
+
+        serverproxy = mock.Mock()
+        serverproxy.list_packages_with_serial.side_effect = raise_ValueError
+        xmlproxy = XMLProxy(serverproxy)
+        with pytest.raises(ValueError):
+            extdb.spawned_pypichanges(xmlproxy, proxysleep=raise_ValueError)
+        assert not keyfs.PYPISERIALS.exists()
+        assert caplog.getrecords(".*error.*")
+
+
+def test_requests_httpget_negative_status_code(xom_notmocked, monkeypatch):
+    import requests.exceptions
+    l = []
+    def r(*a, **k):
+        l.append(1)
+        raise requests.exceptions.RequestException()
+
+    monkeypatch.setattr(xom_notmocked._httpsession, "get", r)
+
+def test_requests_httpget_timeout(xom_notmocked, monkeypatch):
+    import requests.exceptions
+    def httpget(url, **kw):
+        assert kw["timeout"] == 1.2
+        raise requests.exceptions.Timeout()
+
+    monkeypatch.setattr(xom_notmocked._httpsession, "get", httpget)
+    r = xom_notmocked.httpget("http://notexists.qwe", allow_redirects=False,
+                              timeout=1.2)
+    assert r.status_code == -1
