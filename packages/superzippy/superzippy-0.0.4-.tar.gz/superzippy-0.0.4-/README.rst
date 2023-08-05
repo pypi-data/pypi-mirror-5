794e3ecdc0e33fdc91fb2aacf6089d3b53607cd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:09 +0000

    Add package superzippy/0.0.4-/superzippy-0.0.4-.tar.gz

---
+++ packages/superzippy/superzippy-0.0.4-.tar.gz/superzippy-0.0.4-/README.rst
@@ -0,0 +1,50 @@
+Super Zippy
+===========
+
+Super Zippy takes a Python package and its pure Python dependencies and transforms them all into a single executable file.
+
+This is similar to `cx_Freeze <http://cx-freeze.sourceforge.net/>`_ except that it does not attempt to deal with non-pure Python dependencies and thus is much lighter weight and easier to use.
+
+Examples
+--------
+
+.. code-block:: bash
+
+	$ superzippy superzippy superzippy.packaging:run
+	$ ./superzippy
+	Usage: superzippy [options] [PACKAGE] [ENTRY POINT]
+
+	superzippy: error: Exactly two arguments must be supplied.
+
+And now we have a single file containing all of Super Zippy's dependencies and code. We can send this single file (called ``superzippy`` above) to anyone and as long as they already have Python installed they can just run the file.
+
+How it Works
+------------
+
+It installs the Python package you specify into a ``virtualenv`` using ``pip`` (so anything you can tell ``pip`` to install can be given to ``superzippy``), then grabs the site-packages directory out of the virtual environment and sticks it in a zip file along with some super bootstrapping code. Then it just makes the zip file executable.
+
+Motivation
+----------
+
+I created this for a tool called the `Galah API Client <https://www.github.com/galah-group/galah-apiclient>`_ which is a simple tool that has only pure Python dependencies. It is basically a script but since it is a little complex I didn't want everything to be in a single file as that gets tedious fast... But I still wanted a good way to distribute it to my users who are not Python developers, may not have root access, and don't want to deal with ``virtualenv`` and ``pip``.
+
+Installing
+----------
+
+To install Super Zippy, just run
+
+```shell
+$ pip install superzippy
+```
+
+You can also install from the ``setup.py`` script yourself of course, or you can even create a super zipped up version of Super Zippy using Super Zippy. ``pip`` is probably the easiest way to go though if you're already familiar with Python packages.
+
+Authors
+-------
+
+`John Sullivan <http://brownhead.github.io>`_
+
+License
+-------
+
+Apache License v2.0 (see LICENSE)
---
+++ packages/superzippy/superzippy-0.0.4-.tar.gz/superzippy-0.0.4-/superzippy/packaging.py
@@ -0,0 +1,211 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2013 John Sullivan
+# Copyright (c) 2013 Other contributers as noted in the CONTRIBUTERS file
+#
+# This file is part of superzippy
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+#
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from optparse import OptionParser, make_option
+import subprocess
+import logging
+import sys
+import tempfile
+import os, os.path
+import zipdir
+import pkg_resources
+import shutil
+
+DEVNULL = open(os.devnull, "w")
+
+_dirty_files = []
+def destroy_dirty_files():
+    global _dirty_files
+
+    log = logging.getLogger("superzippy")
+
+    for i in _dirty_files:
+        log.debug("Deleting %s.", i)
+        shutil.rmtree(i)
+
+    _dirty_files = []
+
+def parse_arguments(args = sys.argv[1:]):
+    option_list = [
+        make_option(
+            "-v", "--verbose", action = "count",
+            help =
+                "May be specified thrice. If specified once, INFO messages and "
+                "above are output. If specified twice, DEBUG messages and "
+                "above are output. If specified thrice, DEBUG messages and "
+                "above are output, along with the output from any invoked "
+                "programs."
+        ),
+        make_option(
+            "-q", "--quiet", action = "store_true",
+            help = "Only CRITICAL log messages will be output."
+        ),
+        make_option(
+            "-o", "--output", action = "store", default = None,
+            help =
+                "The name of the output file. Defaults to the name of the "
+                "package."
+        )
+    ]
+
+    parser = OptionParser(
+        usage = "usage: %prog [options] [PACKAGE] [ENTRY POINT]",
+        description =
+            "Zips up a package and adds superzippy's super bootstrap logic to "
+            "it.",
+        option_list = option_list
+    )
+
+    options, args = parser.parse_args(args)
+
+    if len(args) != 2:
+        parser.error("Exactly two arguments must be supplied.")
+
+    return (options, args)
+
+def setup_logging(options, args):
+    if options.verbose >= 2:
+        log_level = logging.DEBUG
+    elif options.verbose == 1:
+        log_level = logging.INFO
+    elif options.quiet:
+        log_level = logging.CRITICAL
+    else:
+        log_level = logging.WARN
+
+    format = "[%(levelname)s] %(message)s"
+
+    logging.basicConfig(level = log_level, format = format)
+
+    logging.getLogger("superzippy").debug("Logging initialized.")
+
+def main(options, args):
+    log = logging.getLogger("superzippy")
+
+    # Create the virtualenv directory
+    virtualenv_dir = tempfile.mkdtemp()
+    _dirty_files.append(virtualenv_dir)
+
+    #### Create virtual environment
+
+    log.debug("Creating virtual environment at %s.", virtualenv_dir)
+    output_target = None if options.verbose >= 3 else DEVNULL
+
+    return_value = subprocess.call(
+        ["virtualenv", virtualenv_dir],
+        stdout = output_target,
+        stderr = subprocess.STDOUT
+    )
+
+    if return_value != 0:
+        log.critical(
+            "virtualenv returned non-zero exit status (%d).", return_value
+        )
+
+        return 1
+
+    ##### Install package and dependencies
+
+    package_to_install = args[0]
+    pip_path = os.path.join(virtualenv_dir, "bin", "pip")
+
+    log.debug("Installing the package %s.", package_to_install)
+
+    return_value = subprocess.call(
+        [pip_path, "install", package_to_install],
+        stdout = output_target,
+        stderr = subprocess.STDOUT
+    )
+
+    if return_value != 0:
+        log.critical("pip returned non-zero exit status (%d).", return_value)
+
+        return 1
+
+    #### Move site packages over to build directory
+
+    build_dir = tempfile.mkdtemp()
+    _dirty_files.append(build_dir)
+
+    site_package_dir = ""
+    for root, dirs, files in os.walk(virtualenv_dir):
+        if "site-packages" in dirs:
+            site_package_dir = os.path.join(root, "site-packages")
+
+    shutil.move(site_package_dir, build_dir)
+
+    ##### Install bootstrapper
+
+    log.debug("Adding bootstrapper to the archive.")
+
+    bootstrap_files = {
+        "bootstrapper.py": "__main__.py",
+        "zipsite.py": "zipsite.py",
+        "module_locator.py": "module_locator.py"
+    }
+
+    for k, v in bootstrap_files.items():
+        source = pkg_resources.resource_stream("superzippy.bootstrapper", k)
+        dest = open(os.path.join(build_dir, v), "w")
+
+        shutil.copyfileobj(source, dest)
+
+        source.close()
+        dest.close()
+
+    ##### Install configuration
+
+    log.debug("Adding configuration file to archive.")
+
+    with open(os.path.join(build_dir, "superconfig.py"), "w") as f:
+        f.write("entry_point = '%s'" % args[1])
+
+    ##### Zip everything up into final file
+
+    log.debug("Zipping up %s.", build_dir)
+
+    output_file = options.output if options.output else package_to_install
+
+    zipdir.zipdir(
+        build_dir,
+        output_file
+    )
+
+    #### Make that file executable
+
+    with file(output_file, "r") as f:
+        data = f.read()
+
+    with file(output_file, "w") as f:
+        f.write("#!/usr/bin/env python\n" + data)
+
+    os.chmod(output_file, 0755)
+
+    return 0
+
+def run():
+    options, args = parse_arguments()
+    setup_logging(options, args)
+    try:
+        main(options, args)
+    finally:
+        destroy_dirty_files()
+
+if __name__ == "__main__":
+    run()
