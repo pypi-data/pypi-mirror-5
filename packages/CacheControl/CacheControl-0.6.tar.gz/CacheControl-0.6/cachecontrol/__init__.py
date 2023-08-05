370ecb88ae7f8b7c1ca50d6ef8e13b2470533096
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:30 +0000

    Add package CacheControl/0.6/CacheControl-0.6.tar.gz

---
+++ packages/CacheControl/CacheControl-0.6.tar.gz/CacheControl-0.6/cachecontrol/__init__.py
@@ -0,0 +1,8 @@
+"""CacheControl import Interface.
+
+Make it easy to import from cachecontrol without long namespaces.
+"""
+
+from cachecontrol.wrapper import CacheControl
+from cachecontrol.adapter import CacheControlAdapter
+from cachecontrol.controller import CacheController
