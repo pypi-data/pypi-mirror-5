1fc8d7c9a2a4ff0005fa850386c0188e39b7832c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:15 +0000

    Add package pyRserve/0.7.1/pyRserve-0.7.1.tar.gz

---
+++ packages/pyRserve/pyRserve-0.7.1.tar.gz/pyRserve-0.7.1/pyRserve/__init__.py
@@ -0,0 +1,11 @@
+import warnings
+from .version import __version__
+
+# Show all deprecated warning only once:
+warnings.filterwarnings('once', category=DeprecationWarning)
+del warnings
+
+from .rconn import connect
+from .taggedContainers import TaggedList, TaggedArray, AttrArray
+
+
