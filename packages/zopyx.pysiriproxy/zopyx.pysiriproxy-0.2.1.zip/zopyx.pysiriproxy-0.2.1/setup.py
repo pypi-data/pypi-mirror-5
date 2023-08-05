937a7600fa24dad292e9ff33219a38ce66f912af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:11 +0000

    Add package zopyx.pysiriproxy/0.2.1/zopyx.pysiriproxy-0.2.1.zip

---
+++ packages/zopyx.pysiriproxy/zopyx.pysiriproxy-0.2.1.zip/zopyx.pysiriproxy-0.2.1/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# Copyright (C) 2012 Brett Ponsler
+# This file is part of pysiriproxy.
+#
+# pysiriproxy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pysiriproxy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pysiriproxy.  If not, see <http://www.gnu.org/licenses/>.
+from sys import stderr
+from os import environ
+from os import listdir
+from os.path import join, isfile
+from setuptools import setup
+
+setup(name='zopyx.pysiriproxy',
+      version='0.2.1',
+      description='Python implementation of SiriProxy.',
+      maintainer='Andreas Jung',
+      maintainer_email='andreas@andreas-jung.com',
+      author='Brett Ponsler',
+      author_email='ponsler@gmail.com',
+      url='https://code.google.com/p/pysiriproxy/',
+      packages=['pysiriproxy', 'pysiriproxy.connections',
+                'pysiriproxy.objects', 'pysiriproxy.options',
+                'pysiriproxy.plugins', 'pysiriproxy.testing'],
+      include_package_data=True,
+      license='GNU GPL v3',
+      install_requires=[
+        "biplist>=0.5",
+        "twisted==12.1.0",
+        "pyopenssl",
+        "pyamp>=1.2",
+        ],
+       entry_points="""
+       [console_scripts]
+       siriproxy = pysiriproxy.cmdline:main
+       """
+      )
