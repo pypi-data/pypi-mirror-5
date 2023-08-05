e10afa101bc9a8f15240bd1d3924fd5a53cdc941
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:31 +0000

    Add package autoblocks/1.0/autoblocks-1.0.tar.gz

---
+++ packages/autoblocks/autoblocks-1.0.tar.gz/autoblocks-1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+import os
+
+setup(
+    name='autoblocks',
+    author='Luke Hutscal',
+    author_email='support@strathcom.com',
+    platforms=['OS Independent'],
+    license='BSD License',
+    version='1.0',
+    description="Place arbitrary django-cms placeholders into templates",
+    long_description=open(
+        os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    url='https://github.com/Strathcom/django-cms-autoblocks',
+    install_requires=[
+        'django-cms>=2.4.1',
+    ],
+    packages=find_packages(),
+)
