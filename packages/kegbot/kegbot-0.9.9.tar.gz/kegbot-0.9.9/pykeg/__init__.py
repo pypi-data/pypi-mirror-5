f9e4c2a252c9229b055e3f068dc4daa392e74773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:55 +0000

    Add package kegbot/0.9.9/kegbot-0.9.9.tar.gz

---
+++ packages/kegbot/kegbot-0.9.9.tar.gz/kegbot-0.9.9/pykeg/__init__.py
@@ -0,0 +1,5 @@
+# Package version.
+__version__ = '0.9.9'
+
+# Internal version number, bumped every time "kb_upgrade" is needed.
+EPOCH = 100
---
+++ packages/kegbot/kegbot-0.9.9.tar.gz/kegbot-0.9.9/pykeg/core/optional_modules.py
@@ -0,0 +1,14 @@
+"""Defines HAVE_* flags for several optional packages."""
+
+try:
+  import debug_toolbar
+  HAVE_DEBUG_TOOLBAR = True
+except ImportError:
+  HAVE_DEBUG_TOOLBAR = False
+
+try:
+  import raven.contrib.django
+  HAVE_RAVEN = True
+except ImportError:
+  HAVE_RAVEN = False
+
