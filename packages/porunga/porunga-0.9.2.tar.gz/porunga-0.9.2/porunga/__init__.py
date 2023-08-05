f3667b7e81e32e73b0dec13e7261c6e7717e4b9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:55 +0000

    Add package porunga/0.9.2/porunga-0.9.2.tar.gz

---
+++ packages/porunga/porunga-0.9.2.tar.gz/porunga-0.9.2/porunga/__init__.py
@@ -0,0 +1,43 @@
+"""
+porunga is a test framework for simple algorithms
+"""
+import os
+import sys
+
+
+VERSION = (0, 9, 2)
+
+__version__ = '.'.join((str(each) for each in VERSION[:4]))
+
+def get_version():
+    """
+    Returns shorter version (digit parts only) as string.
+    """
+    return '.'.join((str(each) for each in VERSION[:4]))
+
+def update_sys_path():
+    dirpath = os.path.abspath(os.path.dirname(__file__))
+    sys.path.insert(0, dirpath)
+
+def get_manager():
+    update_sys_path()
+    from monolith.cli import ExecutionManager
+    from porunga.utils.imports import import_class
+
+    class PorungaManager(ExecutionManager):
+
+        def get_commands_to_register(self):
+            registry = {
+                'test': 'porunga.commands.test.PorungaTestCommand',
+            }
+            commands = dict((name, import_class(path)) for name, path in
+                registry.items())
+            return commands
+
+    return PorungaManager(['porunga'])
+
+
+def main():
+    manager = get_manager()
+    manager.execute()
+
