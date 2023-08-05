f8efc5e74ae7ab443205d7b95f144cc0227f41be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:30 +0000

    Add package myhello/1.0.0/myhello-1.0.0.tar.gz

---
+++ packages/myhello/myhello-1.0.0.tar.gz/myhello-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name = 'myhello',
+	version = '1.0.0',
+	py_modules=['myhello','gbk2utf8'],
+	author='alfred tt',
+	author_email='javadoc@163.com',
+	url = 'www.163.com',
+	description = 'my test hello',
+)
