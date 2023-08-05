3fa08299ac8007bb18abf3eacd2c1c606947976b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:11 +0000

    Add package jsonmatch/0.1/jsonmatch-0.1.tar.gz

---
+++ packages/jsonmatch/jsonmatch-0.1.tar.gz/jsonmatch-0.1/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='jsonmatch',
+    version='0.1',
+    author="jamesob",
+    author_email='jamesob@percolate.com',
+    packages=['jsonmatch'],
+    url='https://github.com/percolate/jsonmatch',
+    license='see LICENCE.txt',
+    description='A flexible framework for testing JSON dicts against schemas.',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Testing',
+    ],
+)
+
