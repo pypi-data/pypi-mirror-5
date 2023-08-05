56de673a354819b84914c494f7037572b4f75336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:20 +0000

    Add package collective.portlet.pythonscript/1.0.1/collective.portlet.pythonscript-1.0.1.tar.gz

---
+++ packages/collective.portlet.pythonscript/collective.portlet.pythonscript-1.0.1.tar.gz/collective.portlet.pythonscript-1.0.1/src/collective/portlet/pythonscript/browser/portlet.py
@@ -0,0 +1,137 @@
+import logging
+from time import time
+
+from plone.portlets.interfaces import IPortletDataProvider
+from Products.CMFPlone import PloneMessageFactory as _
+from zope import schema
+from zope.formlib import form
+from zope.interface import implements
+from plone.app.portlets.portlets import base
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.CMFCore.utils import getToolByName
+from collective.portlet.pythonscript.content.interface import IPythonScriptManager,\
+    IPythonScriptPortletItem
+
+logger = logging.getLogger(__name__)
+
+class IPythonScriptPortlet(IPortletDataProvider):
+    """Schema of Python Script portlet."""
+
+    portlet_title = schema.TextLine(
+        title=_(u"Portlet title"),
+        description=_(u"Title to display above portlet content"),
+        required=True
+    )
+
+    script_name = schema.Choice(
+        title=_(u"Python Script"),
+        description=_(u"Python Script used to generate list of results"),
+        required=True,
+        source='python-scripts'
+    )
+
+    limit_results = schema.Int(
+        title=_(u"Limit results"),
+        description=_(u"How many results should be displayed (none means all)"),
+        required=False,
+        min=1
+    )
+
+class PythonScriptPortletAssignment(base.Assignment):
+    """Assignment of Python Script portlet."""
+    implements(IPythonScriptPortlet)
+
+    def __init__(self, portlet_title=u"", script_name=None, limit_results=None):
+        self.portlet_title = portlet_title
+        self.script_name = script_name
+        self.limit_results = limit_results
+
+    @property
+    def title(self):
+        return _(u"Python Script ${portlet_title}", mapping={'portlet_title': self.portlet_title})
+
+class PythonScriptPortletAddForm(base.AddForm):
+    """Python Script portlet add form."""
+
+    form_fields = form.Fields(IPythonScriptPortlet)
+
+    label = _(u"Add Python Script Portlet")
+    description = _(u"This portlet displays list of catalog objects returned by assigned Python Script")
+
+    def create(self, data):
+        """Create portlet assignment."""
+        return PythonScriptPortletAssignment(
+            portlet_title=data['portlet_title'],
+            script_name=data['script_name'],
+            limit_results=data['limit_results']
+        )
+
+class PythonScriptPortletEditForm(base.EditForm):
+    """Python Script portlet edit form."""
+
+    form_fields = form.Fields(IPythonScriptPortlet)
+
+    label = _(u"Edit Python Script Portlet")
+    description = _(u"This portlet displays list of catalog objects returned by assigned Python Script")
+
+class PythonScriptPortletRenderer(base.Renderer):
+    """Python Script portlet renderer."""
+
+    template = ViewPageTemplateFile('portlet.pt')
+
+    def render(self):
+        """Render portlet."""
+        return self.template()
+
+    @property
+    def portlet_title(self):
+        """Returns portlet title."""
+        return self.data.portlet_title
+
+    @property
+    def available(self):
+        return len(self.items)
+
+    def run_script(self, script):
+        """Execute Python Script."""
+        # Change the context of the script to point to current portlet context.
+        bound_script = script.__of__(self.context)
+        # Execute the script.
+        results = bound_script()
+        return results
+
+    def wrap_results(self, results):
+        """Wrap results into form that is renderable for the portlet."""
+        return [IPythonScriptPortletItem(result) for result in results]
+
+    @property
+    def items(self):
+        """Returns list of results to be rendered inside portlet."""
+        portal_url = getToolByName(self.context, 'portal_url')
+        portal = portal_url.getPortalObject()
+        manager = IPythonScriptManager(portal)
+        script_name = self.data.script_name
+        try:
+            info = manager.getInfo(script_name)
+        except KeyError:
+            logger.exception(u'Could not find script %r' % script_name)
+            return []
+        if not info.enabled:
+            logger.warning(u'Script %r is not enabled' % script_name)
+            return []
+        script = manager.getScript(script_name)
+        before = time()
+        try:
+            results = self.run_script(script)
+        except Exception:
+            logger.exception(u'Error while running script %r' % script_name)
+            return []
+        else:
+            timing = time() - before
+            logger.info(u'Script %r executed successfully in %.3f sec' % (script_name, timing))
+            if info.timing:
+                info.addTiming(timing)
+            limit = self.data.limit_results
+            if limit:
+                results = results[:limit]
+            return self.wrap_results(results)
---
+++ packages/collective.portlet.pythonscript/collective.portlet.pythonscript-1.0.1.tar.gz/collective.portlet.pythonscript-1.0.1/src/collective/portlet/pythonscript/tests/test_scriptmanager.py
@@ -0,0 +1,215 @@
+from collective.portlet.pythonscript.tests.base import TestBase
+
+class TestScriptManager(TestBase):
+    """Test script manager functionality."""
+
+    def testEmpty(self):
+        """Test if by default no scripts are listed."""
+        manager = self.getScriptManager()
+        scripts = manager.getScripts()
+        self.assertRaises(StopIteration, scripts.next)
+        enabled = manager.getEnabledScripts()
+        self.assertRaises(StopIteration, enabled.next)
+
+    def testEmptyRescan(self):
+        """Test if after rescanning an empty site, no scripts are listed."""
+        manager = self.getScriptManager()
+        manager.rescanScripts()
+        scripts = manager.getScripts()
+        self.assertRaises(StopIteration, scripts.next)
+        enabled = manager.getEnabledScripts()
+        self.assertRaises(StopIteration, enabled.next)
+
+    def testSingleScript(self):
+        """Test if after rescanning a single script is find."""
+        ps = self.addPythonScript('first', u'First', 'return []')
+
+        # Script should not be visible until rescan.
+        self.testEmpty()
+
+        manager = self.getScriptManager()
+        # After rescan script should be visible...
+        manager.rescanScripts()
+        scripts = manager.getScripts()
+        name, info = scripts.next()
+        self.assertEqual(name, '/plone/first')
+        self.assertEqual(info.title, u'First')
+        self.assertEqual(info.enabled, False)
+        self.assertEqual(info.timing, False)
+        self.assertEqual(info.times, None)
+        self.assertRaises(StopIteration, scripts.next)
+        # ...but inactive.
+        enabled = manager.getEnabledScripts()
+        self.assertRaises(StopIteration, enabled.next)
+        return ps
+
+    def testGetInfoSingleScript(self):
+        """Test retrieving script metadata."""
+        self.testSingleScript()
+        manager = self.getScriptManager()
+        self.assertRaises(KeyError, manager.getInfo, '/plone/nonexisting')
+        info = manager.getInfo('/plone/first')
+        self.assertEqual(info.title, u'First')
+        self.assertEqual(info.enabled, False)
+        self.assertEqual(info.timing, False)
+        self.assertEqual(info.times, None)
+
+    def testGetScriptSingleScript(self):
+        """Test retrieving script."""
+        ps = self.testSingleScript()
+        manager = self.getScriptManager()
+        self.assertRaises(KeyError, manager.getScript, '/plone/nonexisting')
+        script = manager.getScript('/plone/first')
+        self.assertEqual(script, ps)
+
+    def testMoreScripts(self):
+        """Test adding more scripts."""
+        first = self.testSingleScript()
+        second = self.addPythonScript('second', None, 'return []')
+        third = self.addPythonScript('third', u'A Third Script', 'return []')
+
+        # Scripts are not visible until rescan.
+        manager = self.getScriptManager()
+        scripts = manager.getScripts()
+        name, _info = scripts.next()
+        self.assertEqual(name, '/plone/first')
+        self.assertRaises(StopIteration, scripts.next)
+
+        manager.rescanScripts()
+        scripts = manager.getScripts()
+        # Scripts should be ordered by title.
+        name, _info = scripts.next()
+        self.assertEqual(name, '/plone/third')
+        name, _info = scripts.next()
+        self.assertEqual(name, '/plone/first')
+        name, _info = scripts.next()
+        self.assertEqual(name, '/plone/second')
+        self.assertRaises(StopIteration, scripts.next)
+
+        enabled = manager.getEnabledScripts()
+        self.assertRaises(StopIteration, enabled.next)
+
+        return [first, second, third]
+
+    def testEnableScript(self):
+        """Test enabling script."""
+        self.testMoreScripts()
+
+        manager = self.getScriptManager()
+        manager.enableScript('/plone/third')
+
+        enabled = manager.getEnabledScripts()
+        name, _info = enabled.next()
+        self.assertEqual(name, '/plone/third')
+        self.assertRaises(StopIteration, enabled.next)
+
+    def testEnableMoreScripts(self):
+        """Test enabling more scripts."""
+        self.testEnableScript()
+
+        manager = self.getScriptManager()
+        manager.enableScript('/plone/first')
+
+        enabled = manager.getEnabledScripts()
+        name, _info = enabled.next()
+        self.assertEqual(name, '/plone/third')
+        name, _info = enabled.next()
+        self.assertEqual(name, '/plone/first')
+        self.assertRaises(StopIteration, enabled.next)
+
+    def testDisableScript(self):
+        """Test disabling scripts."""
+        self.testEnableMoreScripts()
+
+        manager = self.getScriptManager()
+        self.assertRaises(KeyError, manager.disableScript, '/plone/nonexisting')
+
+        manager.disableScript('/plone/third')
+
+        enabled = manager.getEnabledScripts()
+        name, _info = enabled.next()
+        self.assertEqual(name ,'/plone/first')
+        self.assertRaises(StopIteration, enabled.next)
+
+    def testEnableTiming(self):
+        """Test enable timing."""
+        self.testEnableMoreScripts()
+
+        manager = self.getScriptManager()
+        info = manager.getInfo('/plone/third')
+
+        # Cannot get or add timing when timing was not enabled.
+        self.assertRaises(AssertionError, info.getTiming)
+        self.assertRaises(AssertionError, info.addTiming, 0.3)
+
+        # Error is raised when unknown script is tried.
+        self.assertRaises(KeyError, manager.enableTiming, '/plone/nonexisting')
+        manager.enableTiming('/plone/third')
+
+        self.assertEqual(info.getTiming(), {
+            'min_time': 0.0,
+            'avg_time': 0.0,
+            'max_time': 0.0,
+            'samples': 0
+        })
+        info.addTiming(0.1)
+        info.addTiming(0.2)
+        info.addTiming(0.3)
+        info.addTiming(0.4)
+        self.assertEqual(info.getTiming(), {
+            'min_time': 0.1,
+            'avg_time': 0.25,
+            'max_time': 0.4,
+            'samples': 4
+        })
+
+    def testDisableTiming(self):
+        """Test disable timing."""
+        self.testEnableTiming()
+
+        manager = self.getScriptManager()
+        self.assertRaises(KeyError, manager.disableTiming, '/plone/nonexisting')
+
+        manager.disableTiming('/plone/third')
+        info = manager.getInfo('/plone/third')
+        # Cannot get or add timing when timing was not enabled.
+        self.assertRaises(AssertionError, info.getTiming)
+        self.assertRaises(AssertionError, info.addTiming, 0.3)
+
+        # After re-enabling timing timing data should be reset.
+        manager.enableTiming('/plone/third')
+        self.assertEqual(info.getTiming(), {
+            'min_time': 0.0,
+            'avg_time': 0.0,
+            'max_time': 0.0,
+            'samples': 0
+        })
+
+    def testDisableScriptTiming(self):
+        """Test if timing data is reset when script is disabled."""
+        self.testEnableTiming()
+
+        manager = self.getScriptManager()
+        manager.disableScript('/plone/third')
+
+        info = manager.getInfo('/plone/third')
+        self.assertEqual(info.timing, False)
+        # Cannot get or add timing when script is disabled.
+        self.assertRaises(AssertionError, info.getTiming)
+        self.assertRaises(AssertionError, info.addTiming, 0.3)
+
+        manager.enableScript('/plone/third')
+        self.assertEqual(info.timing, False)
+        # Cannot get or add timing when timing is disabled.
+        self.assertRaises(AssertionError, info.getTiming)
+        self.assertRaises(AssertionError, info.addTiming, 0.3)
+
+        # After re-enabling timing timing data should be reset.
+        manager.enableTiming('/plone/third')
+        self.assertEqual(info.timing, True)
+        self.assertEqual(info.getTiming(), {
+            'min_time': 0.0,
+            'avg_time': 0.0,
+            'max_time': 0.0,
+            'samples': 0
+        })
