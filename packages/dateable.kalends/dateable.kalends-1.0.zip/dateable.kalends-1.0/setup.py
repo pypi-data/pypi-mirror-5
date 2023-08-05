03db603ed8fad44a9a2db44efd0d95e63f46b470
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:59 +0000

    Add package dateable.kalends/1.0/dateable.kalends-1.0.zip

---
+++ packages/dateable.kalends/dateable.kalends-1.0.zip/dateable.kalends-1.0/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '1.0'
+
+f = open('README.txt')
+readme = f.read()
+f.close()
+
+f = open('CHANGES.txt')
+changes = f.read()
+f.close()
+
+setup(name='dateable.kalends',
+      version=version,
+      description="Dateable calendaring API",
+      long_description=readme + '\n\n' + changes,
+      classifiers=[
+          'Framework :: Plone',
+          'Programming Language :: Python',
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          ],
+      keywords='Dateable Calendaring calendar event icalendar',
+      author='Lennart Regebro',
+      author_email='regebro@gmail.com',
+      url='https://github.com/collective/dateable.kalends',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['dateable'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
