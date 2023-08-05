475f817a27d8e53272ed88ce72f58634ea2d7809
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:53 +0000

    Add package printlol/1.3.0/printlol-1.3.0.zip

---
+++ packages/printlol/printlol-1.3.0.zip/printlol-1.3.0/printlol.py
@@ -0,0 +1,17 @@
+# -*- coding:UTF-8 -*-
+movies = ["电影名称", "拍摄之时间", "导演", "时间",
+          ["主角1", ["配角1", "配角2", "配角3"]]]
+
+"""这是"printlol"模块，提供了一个叫做print_lol()的函数
+"""
+
+def print_lol(the_list, level):
+    for item in the_list:
+        if isinstance(item, list):
+            print_lol(item, level+1)
+        else:
+            for tab_stop in range(level):
+                print("\t", end="")
+            print(item)
+
+print_lol(movies, 0)
---
+++ packages/printlol/printlol-1.3.0.zip/printlol-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name        =   "printlol",
+    version     =   "1.3.0",
+    py_modules  =   ['printlol'],
+    author      =   'tester',
+    author_email    =   "test_se@163.com",
+    url         =   "test_se.com",
+    description     =   "test_se",
+    )
