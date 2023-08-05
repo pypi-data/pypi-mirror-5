33e4eac7a88445f54ef28685e495e1c3eed73ba2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:09 +0000

    Add package nose-stoq/0.1.3/nose-stoq-0.1.3.tar.gz

---
+++ packages/nose-stoq/nose-stoq-0.1.3.tar.gz/nose-stoq-0.1.3/stoqplugin.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+from nose.plugins import Plugin
+
+if 'STOQ_USE_GI' in os.environ:
+    from stoq.lib import gicompat
+    gicompat.enable()
+
+class StoqPlugin(Plugin):
+    name = 'stoq'
+    # This is a little hack to make sure that Stoq's database configuration
+    # is properly setup. If we import tests.base before Cover.setup() in the
+    # coverage plugin is called the statistics will skip the modules imported
+    # by tests.base
+    def begin(self):
+        for path in os.environ['PYTHONPATH'].split(':'):
+            sys.path.insert(0, path)
+
+        import tests.base
+        tests.base  # pyflakes
