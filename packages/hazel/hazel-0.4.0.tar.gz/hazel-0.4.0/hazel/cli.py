794bf6684c7a855bcb67ffb3fb4d9bf6e9113ead
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:06 +0000

    Add package hazel/0.4.0/hazel-0.4.0.tar.gz

---
+++ packages/hazel/hazel-0.4.0.tar.gz/hazel-0.4.0/hazel/cli.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+"""
+Usage: hazel init...
+       hazel generate
+       hazel write <title>
+
+Options: -h, --help
+
+"""
+from docopt import docopt
+
+from hazel.config import init
+from hazel.build import new_post, generate
+
+args = docopt(__doc__)
+
+def main():
+
+    if args['generate']:
+        generate()
+
+    if args['init']:
+        init()
+
+    if args['write']:
+        new_post(args['<title>'])
---
+++ packages/hazel/hazel-0.4.0.tar.gz/hazel-0.4.0/hazel/load.py
@@ -0,0 +1,27 @@
+# coding=utf-8
+from __future__ import with_statement
+import os
+import yaml
+
+from clint.textui import puts, indent, colored
+
+from hazel.utils import ObjectDict, get_path, force_mkdir, g, path
+
+
+def load_config():
+    with open('config.yml', 'r') as f:
+        g.config = yaml.load(f.read())
+
+
+def load_template_config():
+    with open(get_path(path.template,'config.yml'), 'r') as f:
+        g.template_config = yaml.load(f.read())
+
+
+def load_path():
+    path.posts = 'posts'
+    path.template = get_path('templates', g.config['template'])
+    path.template_assets = get_path(path.template, 'assets')
+    path.site = 'site'
+    path.site_post = get_path(path.site, 'post')
+    path.site_assets = get_path(path.site, 'assets')
