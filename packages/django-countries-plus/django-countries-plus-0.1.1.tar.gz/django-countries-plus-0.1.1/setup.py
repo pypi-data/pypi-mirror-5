a464a318e0b168af10a8fbd4fdfdcb4bd947608b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:53 +0000

    Add package django-countries-plus/0.1.1/django-countries-plus-0.1.1.tar.gz

---
+++ packages/django-countries-plus/django-countries-plus-0.1.1.tar.gz/django-countries-plus-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='django-countries-plus',
+    version='0.1.1',
+    author='Andrew Cordery',
+    author_email='cordery@gmail.com',
+    packages=['countriesplus',],
+    url='http://pypi.python.org/pypi/django-countries-plus/',
+    license='LICENSE.txt',
+    description='A django model & fixture containing all data from the countries table of Geonames.org',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "Django >= 1.2",
+    ],
+)
