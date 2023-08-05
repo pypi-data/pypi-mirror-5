76fd68f8d3b3a84604da11696b97add5978a3f83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:03 +0000

    Add package investigators/0.1.4.2/investigators-0.1.4.2.zip

---
+++ packages/investigators/investigators-0.1.4.2.zip/investigators-0.1.4.2/setup.py
@@ -0,0 +1,28 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distribute_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+
+setup(
+    name='investigators',
+    version='0.1.4.2',
+    py_modules=['distribute_setup'],
+    packages=find_packages(),
+    package_data={'': ['*.pyd', '*.png']},
+    install_requires=['numpy', 'PIL'],
+    tests_require=['mock', 'numpy', 'PIL'],
+    url='http://github.com/kobejohn/investigators',
+    license='MIT',
+    author='KobeJohn',
+    author_email='niericentral@gmail.com',
+    description='Achieve various investigations of data by combining'
+                ' easy-to-use investigator classes.',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Programming Language :: Python :: 2.7',
+        'Operating System :: Microsoft :: Windows']
+)
