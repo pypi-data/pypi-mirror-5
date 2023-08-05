c07a421431def303c76dd50d56c0ce648310d4d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:28 +0000

    Add package CssDeadwood/0.2.7/CssDeadwood-0.2.7.tar.gz

---
+++ packages/CssDeadwood/CssDeadwood-0.2.7.tar.gz/CssDeadwood-0.2.7/setup.py
@@ -0,0 +1,31 @@
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='CssDeadwood',
+    version='0.2.7',
+    author='Stefaan Lippens',
+    author_email='soxofaan@gmail.com',
+    url='https://github.com/soxofaan/CssDeadwood/',
+    packages=['cssdeadwood', 'cssdeadwood.test'],
+    entry_points={
+        'console_scripts': [
+            'cssdeadwood = cssdeadwood.app:main',
+        ],
+    },
+    include_package_data=True,
+    license='MIT',
+    keywords="css deadcode",
+    description='Tool to search CSS files for unused CSS selectors.',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "lxml >= 3.1.1",
+        "cssselect >= 0.8",
+    ],
+    test_suite='cssdeadwood.test'
+)
