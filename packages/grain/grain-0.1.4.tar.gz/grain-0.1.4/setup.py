e1188c44dde1dd034653fe06af5d6bd1143c68c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:33 +0000

    Add package grain/0.1.4/grain-0.1.4.tar.gz

---
+++ packages/grain/grain-0.1.4.tar.gz/grain-0.1.4/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='grain',
+    version='0.1.4',
+    description='Simple, extensible test assertions.',
+    long_description=open('README.md').read(),
+    author='Jimmy Do',
+    license='MIT',
+    py_modules=['grain'],
+    url='https://github.com/jimmydo/grain')
