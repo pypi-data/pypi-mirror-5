b0807e9563e3a412256070b53fc37b52fdef3801
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:05 +0000

    Add package PyStaticConfiguration/0.5.3/PyStaticConfiguration-0.5.3.tar.gz

---
+++ packages/PyStaticConfiguration/PyStaticConfiguration-0.5.3.tar.gz/PyStaticConfiguration-0.5.3/staticconf/__init__.py
@@ -0,0 +1,13 @@
+from staticconf import config
+from staticconf.loader import *
+from staticconf.getters import *
+from staticconf.readers import *
+
+
+version         = "0.5.3"
+
+view_help       = config.view_help
+reload          = config.reload
+validate        = config.validate
+ConfigurationWatcher = config.ConfigurationWatcher
+ConfigFacade    = config.ConfigFacade
