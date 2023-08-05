62ba700b03544ac0fa4c4ca924a7e89bc46bbdd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:51 +0000

    Add package markment/0.1.2/markment-0.1.2.tar.gz

---
+++ packages/markment/markment-0.1.2.tar.gz/markment-0.1.2/markment/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from .engine import Markment
+
+
+__all__ = ['Markment']
---
+++ packages/markment/markment-0.1.2.tar.gz/markment-0.1.2/markment/version.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+version = __version__ = '0.1.2'
