72158a22eb78eefe93c855357ea9ae0d4f179997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:15 +0000

    Add package sorl-thumbnail-async/0.2/sorl-thumbnail-async-0.2.tar.gz

---
+++ packages/sorl-thumbnail-async/sorl-thumbnail-async-0.2.tar.gz/sorl-thumbnail-async-0.2/setup.py
@@ -0,0 +1,35 @@
+import thumbnail
+from setuptools import setup, find_packages
+from setuptools.command.test import test
+
+
+class TestCommand(test):
+    def run(self):
+        from tests.runtests import runtests
+        runtests()
+
+
+setup(
+    name='sorl-thumbnail-async',
+    version=0.2,
+    description='Asynchronous thumbnailing app in django with remote storages like S3',
+    author='Nar Kumar Chhantyal',
+    author_email='neokya@gmail.com',
+    license='BSD',
+    url='https://github.com/neokya/sorl-thumbnail-async',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    platforms='any',
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Multimedia :: Graphics',
+        'Framework :: Django',
+    ],
+    cmdclass={"test": TestCommand},
+)
