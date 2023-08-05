5e9ed7dc11dadbe96d417066a9a97f68dea9c611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:59 +0000

    Add package PyFileSec/0.1.3/PyFileSec-0.1.3.tar.gz

---
+++ packages/PyFileSec/PyFileSec-0.1.3.tar.gz/PyFileSec-0.1.3/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='PyFileSec',
+    version='0.1.3',
+    author='Jeremy R. Gray',
+    author_email='jrgray@gmail.com',
+    maintainer='Jeremy R. Gray',
+    py_modules=['pyfilesec'],
+    classifiers=['Development Status :: 3 - Alpha',
+                 'Programming Language :: Python :: 2.6',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3',
+                 'Operating System :: POSIX :: Linux',
+                 'Operating System :: MacOS :: MacOS X',
+                 #'Operating System :: Microsoft :: Windows',  # soon!
+                 'Intended Audience :: Science/Research',
+                 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+                 'Topic :: Scientific/Engineering',
+                 'Topic :: Security'
+                 ],
+    keywords=['encryption', 'security', 'privacy', 'integrity', 'human subjects', 'research'],
+    url='https://github.com/jeremygray/pyFileSec',
+    description='File privacy & integrity tools, e.g., for human-subjects research',
+    long_description=open('README.txt').read(),
+    #install_requires=['ctypes'],
+)
