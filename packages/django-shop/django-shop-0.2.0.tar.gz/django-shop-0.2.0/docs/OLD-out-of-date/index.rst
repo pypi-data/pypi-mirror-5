b749ac7fd771cb042b402d6b9caca7637cdf6c7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:05 +0000

    Add package django-shop/0.2.0/django-shop-0.2.0.tar.gz

---
+++ packages/django-shop/django-shop-0.2.0.tar.gz/django-shop-0.2.0/docs/OLD-out-of-date/index.rst
@@ -0,0 +1,26 @@
+.. django SHOP documentation master file, created by
+   sphinx-quickstart on Mon Dec  6 14:42:25 2010.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to django SHOP's documentation!
+=======================================
+
+Contents:
+
+.. toctree::
+    :maxdepth: 3
+    
+    statement
+    architecture
+    requirements
+    products
+    taxes
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
---
+++ packages/django-shop/django-shop-0.2.0.tar.gz/django-shop-0.2.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+import shop
+
+CLASSIFIERS = [
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+]
+
+setup(
+    author="Christopher Glass",
+    author_email="tribaal@gmail.com",
+    name='django-shop',
+    version=shop.__version__,
+    description='An Advanced Django Shop',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    url='http://www.django-shop.org/',
+    license='BSD License',
+    platforms=['OS Independent'],
+    classifiers=CLASSIFIERS,
+    install_requires=[
+        'Django>=1.4',
+        'django-classy-tags>=0.3.3',
+        'django-polymorphic>=0.2',
+        'south>=0.7.2',
+        'jsonfield>=0.9.6'
+    ],
+    packages=find_packages(exclude=["example", "example.*"]),
+    include_package_data=True,
+    zip_safe=False,
+)
