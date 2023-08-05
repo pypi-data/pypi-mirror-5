088a55a412ed5297e28c57d1530d431306abb677
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:04 +0000

    Add package pycsg/0.2/pycsg-0.2.tar.gz

---
+++ packages/pycsg/pycsg-0.2.tar.gz/pycsg-0.2/README.md
@@ -0,0 +1,17 @@
+pycsg
+=====
+
+Constructive Solid Geometry (CSG) is a modeling technique that uses Boolean
+operations like union and intersection to combine 3D solids. This library
+implements CSG operations on meshes elegantly and concisely using BSP trees,
+and is meant to serve as an easily understandable implementation of the
+algorithm. All edge cases involving overlapping coplanar polygons in both
+solids are correctly handled.
+
+examples
+========
+$ python examples/pyopengl
+
+depends on:
+PyOpenGL
+PyOpenGL_accelerate
---
+++ packages/pycsg/pycsg-0.2.tar.gz/pycsg-0.2/setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup, find_packages
+
+# Utility function to read the README file.  
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='pycsg',
+    version='0.2',
+    description='Constructive Solid Geometry (CSG)',
+    long_description=read('README.md'),
+    keywords = "constructive solid geometry csg utilities",
+
+    author='Tim Knip',
+    author_email='tim@floorplanner.com',
+    url='https://github.com/timknip/pycsg',
+
+    install_requires = [],
+    packages=find_packages(),
+    license = "MIT",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
