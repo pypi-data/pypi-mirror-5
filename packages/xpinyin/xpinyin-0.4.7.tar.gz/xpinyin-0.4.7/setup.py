557928ea03742d62009a915fd1dec3e41394ba7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:17 +0000

    Add package xpinyin/0.4.7/xpinyin-0.4.7.tar.gz

---
+++ packages/xpinyin/xpinyin-0.4.7.tar.gz/xpinyin-0.4.7/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+setup(name="xpinyin",
+      version='0.4.7',
+      description="translate chinese hanzi to pinyin by python",
+      author="Eric Lo",
+      author_email="lxneng@gmail.com",
+      url="https://github.com/lxneng/xpinyin",
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      include_package_data=True,
+      license="MIT License")
