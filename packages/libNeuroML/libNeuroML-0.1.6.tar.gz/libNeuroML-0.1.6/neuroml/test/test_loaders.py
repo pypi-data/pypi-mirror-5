c612a47d3caa344699560ef60d6bde68dfe436d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:15 +0000

    Add package libNeuroML/0.1.6/libNeuroML-0.1.6.tar.gz

---
+++ packages/libNeuroML/libNeuroML-0.1.6.tar.gz/libNeuroML-0.1.6/neuroml/test/test_loaders.py
@@ -0,0 +1,26 @@
+"""
+Unit tests for loaders
+
+"""
+
+import neuroml
+from neuroml import loaders
+import os
+
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+class TestNeuroMLLoader(unittest.TestCase):
+    def test_load_neuroml(self):
+        root_dir = os.path.dirname(neuroml.__file__)
+        test_file_path = os.path.join(root_dir,'test')
+        test_file_path = os.path.join(test_file_path,'Purk2M9s.nml')
+        print test_file_path
+        f = open(test_file_path,'r')
+        print f
+        #print f.read()
+        doc = loaders.NeuroMLLoader.load(test_file_path)
+        print doc
+        self.assertEqual(doc.id,'Purk2M9s')
