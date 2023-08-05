cca6513f10dcd8f2d5a17e4c89851732b4b8c087
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:23 +0000

    Add package jmrua/1.0.0/jmrua-1.0.0.tar.gz

---
+++ packages/jmrua/jmrua-1.0.0.tar.gz/jmrua-1.0.0/jmrua.py
@@ -0,0 +1,9 @@
+import os
+
+"""
+This module define my personal develpoment wokplace in transltr-3
+"""
+def defineWorkingPath():
+    """ Set the development workplace in linux"""
+    workplace = '/home/juanmi/programas/python'
+    os.chdir(workplace)
