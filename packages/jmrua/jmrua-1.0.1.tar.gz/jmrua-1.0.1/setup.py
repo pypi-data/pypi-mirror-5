38243f66543f344975317729785a6faff7e7ca98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:32 +0000

    Add package jmrua/1.0.1/jmrua-1.0.1.tar.gz

---
+++ packages/jmrua/jmrua-1.0.1.tar.gz/jmrua-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'jmrua',
+    version = '1.0.1',
+    py_modules = ['jmrua'],
+    author = 'Juan..Miguel.Ra.Gutirrez',
+    author_email = 'juanmiguel.rua.dev',
+    url = 'http://about.me/juanmiguelrua',
+    description = 'Define the development workplace',
+    )
