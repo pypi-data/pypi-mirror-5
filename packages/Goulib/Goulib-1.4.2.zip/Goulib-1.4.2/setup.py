e972ca3115eb81d85d12bcb4f98d42ec88018cc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:07 +0000

    Add package Goulib/1.4.2/Goulib-1.4.2.zip

---
+++ packages/Goulib/Goulib-1.4.2.zip/Goulib-1.4.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='Goulib',
+    version='1.4.2',
+    author='Philippe Guglielmetti',
+    author_email='goulib@goulu.net',
+    packages=['Goulib'],
+    scripts=[],
+    url='https://sourceforge.net/p/goulib/',
+    license='LICENSE.txt',
+    description='My Python library.',
+    long_description=open('README.txt').read()
+)
