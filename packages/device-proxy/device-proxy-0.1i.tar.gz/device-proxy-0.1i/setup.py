5235958f3430f7c769779db0b6060c32b0ca070c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:38 +0000

    Add package device-proxy/0.1i/device-proxy-0.1i.tar.gz

---
+++ packages/device-proxy/device-proxy-0.1i.tar.gz/device-proxy-0.1i/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+
+def listify(filename):
+    return filter(None, open(filename, 'r').read().split('\n'))
+
+setup(
+    name="device-proxy",
+    version="0.1i",
+    url='http://github.com/praekelt/device-proxy',
+    license='BSD',
+    description="Device Proxy. A reverse HTTP Proxy that can inspect and "
+                "manipulate HTTP Headers before sending upstream.",
+    long_description=open('README.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekeltfoundation.org',
+    packages=[
+        "devproxy",
+        "twisted.plugins",
+    ],
+    package_data={
+        'twisted.plugins': ['twisted/plugins/devproxy_plugin.py'],
+        'devproxy': [
+            'config.yaml',
+            'get-wurfl-2.1-db.sh',
+            'haproxy.cfg',
+            'README.rst',
+            'requirements.pip'
+        ]
+    },
+    include_package_data=True,
+    install_requires=listify('requirements.pip'),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Networking',
+    ],
+)
