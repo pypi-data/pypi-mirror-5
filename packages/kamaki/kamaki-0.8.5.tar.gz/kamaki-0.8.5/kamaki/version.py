e7c512ce96a0e4d2fd29428a055592dcb3419d79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:44 +0000

    Add package kamaki/0.8.5/kamaki-0.8.5.tar.gz

---
+++ packages/kamaki/kamaki-0.8.5.tar.gz/kamaki-0.8.5/kamaki/version.py
@@ -0,0 +1,7 @@
+__version__ = "0.8.5"
+__version_info__ = ['0', '8', '5']
+__version_vcs_info__ = {'branch': 'master',
+ 'revid': 'de98f78',
+ 'revno': 1292,
+ 'toplevel': '/home/saxtouri/src/kamaki/kamaki'}
+__version_user_info__ = "saxtouri@terminus"
