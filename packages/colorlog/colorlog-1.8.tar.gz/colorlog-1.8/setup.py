168479c3c21f710e740ffa4a5339b1a44ce18a78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:25 +0000

    Add package colorlog/1.8/colorlog-1.8.tar.gz

---
+++ packages/colorlog/colorlog-1.8.tar.gz/colorlog-1.8/setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+
+setup(
+    name             = 'colorlog',
+    version          = '1.8',
+
+    description      = 'Log formatting with colors!',
+    long_description = open("README.rst").read(),
+
+    author           = 'Sam Clements',
+    author_email     = 'sam@borntyping.co.uk',
+    url              = 'https://github.com/borntyping/python-colorlog',
+    license          = open("LICENCE.txt").read(),
+
+    packages         = ['colorlog'],
+
+    classifiers      = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Terminals',
+        'Topic :: Utilities',
+    ],
+)
