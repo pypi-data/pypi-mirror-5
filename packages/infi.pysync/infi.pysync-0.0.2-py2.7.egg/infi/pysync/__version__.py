f22e06bb6db871d9ebd529f2e4e44ba62beaf9bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:49 +0000

    Add package infi.pysync/0.0.2/infi.pysync-0.0.2-py2.7.egg

---
+++ packages/infi.pysync/infi.pysync-0.0.2-py2.7.egg/infi/pysync/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.2"
+__git_commiter_name__ = "Tal Yalon"
+__git_commiter_email__ = "tal.yalon@gmail.com"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '389294e188835e14088f0966465022c0dc4f2174'
+__git_head_subject__ = 'fixed waiting on events'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
