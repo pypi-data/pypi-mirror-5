4d3f2bd20fdc91f5a1777090d3ab15c8873de18e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:46 +0000

    Add package complexity/0.1.2/complexity-0.1.2.tar.gz

---
+++ packages/complexity/complexity-0.1.2.tar.gz/complexity-0.1.2/complexity/complexity.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import errno
+import http.server
+import os
+import socketserver
+
+from jinja2 import FileSystemLoader
+from jinja2.environment import Environment
+
+
+def make_sure_path_exists(path):
+    try:
+        os.makedirs(path)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
+
+def serve_static_site():
+    # Serve the output directory
+    os.chdir('output/')
+    PORT = 9090
+    Handler = http.server.SimpleHTTPRequestHandler
+    httpd = socketserver.TCPServer(("", PORT), Handler)
+    print("serving at port", PORT)
+    httpd.serve_forever()
+
+def generate_html(pages):
+    env = Environment()
+    env.loader = FileSystemLoader('input/')
+
+    for page in pages:
+        tmpl = env.get_template('{0}.html'.format(page))
+        rendered_html = tmpl.render()
+
+        # Put index in the root. It's a special case.
+        if page == 'index':
+            with open('output/index.html','w') as fh:
+                fh.write(rendered_html)
+
+        # Put other pages in page/index.html, for better URL formatting.
+        else:
+            make_sure_path_exists('output/{0}/'.format(page))
+            with open('output/{0}/index.html'.format(page),'w') as fh:
+                fh.write(rendered_html)
+
+def command_line_runner():
+    """ Entry point for the package, as defined in setup.py. """
+
+    # List the stem of each file in input/
+    pages = [f.split('.')[0] for f in os.listdir('input/')]
+
+    # Generate and serve the HTML site
+    generate_html(pages)
+    serve_static_site()
+
+
+if __name__ == '__main__':
+    command_line_runner()
---
+++ packages/complexity/complexity-0.1.2.tar.gz/complexity-0.1.2/setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+import complexity
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+readme = open('README.rst', 'rt').read()
+history = open('HISTORY.rst', 'rt').read()
+license = open('LICENSE', 'rt').read()
+
+setup(
+    name='complexity',
+    version=complexity.__version__,
+    description='A refreshingly simple static site generator, for those who like to work in HTML.',
+    long_description=readme + '\n\n' + history,
+    author='Audrey Roy',
+    author_email='audreyr@gmail.com',
+    url='https://github.com/audreyr/complexity',
+    packages=[
+        'complexity',
+    ],
+    # package_data={'': ['LICENSE'], 'complexity': ['*.pem']},
+    # package_dir={'complexity': 'complexity'},
+    entry_points={
+        'console_scripts': [
+            'complexity = complexity.complexity:command_line_runner',
+        ]
+    },
+    include_package_data=True,
+    install_requires=[
+        'jinja2',
+    ],
+    license=license,
+    zip_safe=False,
+    classifiers=(
+        # 'Development Status :: 5 - Production/Stable',
+        "Environment :: Console",
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        "Programming Language :: Python :: 2",
+        # 'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+    ),
+    keywords='complexity static site generator HTML Jinja2 templates S3',
+)
