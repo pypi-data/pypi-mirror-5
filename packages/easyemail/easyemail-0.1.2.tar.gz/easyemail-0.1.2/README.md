43e4cf0d8ac3148533a5e2d0f0cdd4797e9ec82a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:04 +0000

    Add package easyemail/0.1.2/easyemail-0.1.2.tar.gz

---
+++ packages/easyemail/easyemail-0.1.2.tar.gz/easyemail-0.1.2/README.md
@@ -0,0 +1,45 @@
+easyemail
+=========
+
+Small lib abstracting creation and sending emails with smtplib. This is work in
+progress, please do not use it in production.
+
+Installation
+------------
+
+Install with pip::
+
+    $ pip install easyemail
+
+Issues and questions
+--------------------
+
+Have a bug? Please create an issue on GitHub!
+
+https://github.com/niktto/easyemail/issues
+
+
+Contributing
+------------
+
+EasyEmail is an open source software and your contribution is very much
+appreciated.
+
+1. Check for
+   `open issues <https://github.com/niktto/easyemail/issues>`_ or
+   `open a fresh issue <https://github.com/niktto/easyemail/issues/new>`_
+   to start a discussion around a feature idea or a bug.
+2. Fork the
+   `repository on Github <https://github.com/niktto/easyemail>`_
+   and make your changes.
+3. Follow these rules: PEP8, PEP257 and The
+   Zen of Python.
+4. Make sure to add yourself to AUTHORS and send a pull request.
+
+
+Licence
+-------
+
+TaskWorkshop CLI is available under the New BSD License. See
+`LICENSE <https://github.com/niktto/easyemail/blob/master/LICENSE>`_
+file.
---
+++ packages/easyemail/easyemail-0.1.2.tar.gz/easyemail-0.1.2/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+import os
+
+
+def read(filename):
+    """Open a file and return its contents."""
+    with open(os.path.join(os.path.dirname(__file__), filename)) as f:
+        return f.read()
+
+
+setup(
+    name='easyemail',
+    version='0.1.2',
+    description="Simple lib abstracting email sending with smtplib.",
+    long_description=read('README.md'),
+    url='http://github.com/niktto/easyemail/',
+    license=read('LICENSE'),
+    author=u'Marek Szwalkiewicz',
+    author_email='marek@szwalkiewicz.waw.pl',
+    packages=['easyemail'],
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'Mako==0.7.3',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
