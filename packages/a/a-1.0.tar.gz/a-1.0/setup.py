33a3a71636763b9b09392034014e1471e9bdd6b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:26 +0000

    Add package a/1.0/a-1.0.tar.gz

---
+++ packages/a/a-1.0.tar.gz/a-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='a',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Greg Ward',
+      author_email='gward@python.net',
+      url='http://www.python.org/sigs/distutils-sig/',
+      py_modules = ['a'],
+     )
