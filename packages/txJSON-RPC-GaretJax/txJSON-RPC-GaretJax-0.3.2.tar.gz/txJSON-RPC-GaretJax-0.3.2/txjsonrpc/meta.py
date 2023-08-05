c4e21de14f307bab168e53d483750a853f8943a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:01 +0000

    Add package txJSON-RPC-GaretJax/0.3.2/txJSON-RPC-GaretJax-0.3.2.tar.gz

---
+++ packages/txJSON-RPC-GaretJax/txJSON-RPC-GaretJax-0.3.2.tar.gz/txJSON-RPC-GaretJax-0.3.2/txjsonrpc/meta.py
@@ -0,0 +1,8 @@
+display_name = "txJSON-RPC-GaretJax"
+library_name = "txjsonrpc"
+version = "0.3.2"
+author = "Duncan McGreggor"
+author_email = "oubiwann@adytum.us"
+license = "BSD, GPL"
+url = "http://launchpad.net/%s" % library_name
+description = "Code for creatig Twisted JSON-RPC servers and clients."
