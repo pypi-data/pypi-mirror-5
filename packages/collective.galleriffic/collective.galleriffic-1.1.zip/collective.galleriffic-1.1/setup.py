132e3d4a254804fdd89ab761ce8c8a7456c556b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:29 +0000

    Add package collective.galleriffic/1.1/collective.galleriffic-1.1.zip

---
+++ packages/collective.galleriffic/collective.galleriffic-1.1.zip/collective.galleriffic-1.1/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.1'
+
+setup(name='collective.galleriffic',
+      version=version,
+      description="Galleriffic integration into Plone",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.0",
+        "Framework :: Plone :: 4.1",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='JeanMichel FRANCOIS aka toutpt',
+      author_email='toutpt@gmail.com',
+      url='https://github.com/collective/collective.galleriffic',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'collective.gallery',
+          'collective.configviews',
+          'collective.js.galleriffic',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
