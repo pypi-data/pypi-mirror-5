b46d086336df86854e18ce990358f443a0ed65dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:37 +0000

    Add package jsoncomment/0.1.6/jsoncomment-0.1.6.zip

---
+++ packages/jsoncomment/jsoncomment-0.1.6.zip/jsoncomment-0.1.6/jsoncomment/wrapper.py
@@ -0,0 +1,33 @@
+﻿
+################################################################################
+
+from types import ModuleType
+
+################################################################################
+
+# A Class to simulate dynamic inheritance
+# Allows to change behaviour of multiple modules or classes, with the same
+	# interface
+# Note: Class wrapping not tested
+class GenericWrapper:
+
+	# object_to_wrap can be:
+	# A Module
+	# A Class Instance
+	def __init__(self, object_to_wrap):
+		if isinstance(object_to_wrap, ModuleType):
+			self.is_module = True
+		elif isinstance(object_to_wrap, object):
+			self.is_module = False
+		else:
+			raise TypeError("Expected a Module or a Class Instance")
+		self.object_to_wrap = object_to_wrap
+
+	# Fallback lookup for undefined methods
+	def __getattr__(self, name):
+		if self.is_module:
+			return self.object_to_wrap.__dict__[name]
+		else:
+			return self.object_to_wrap.__getattr__(name)
+
+################################################################################
