26105fbd7ebb9f8b7d8715cc60ae06cfac02af3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:55 +0000

    Add package mixpanel-python/0.0.2/mixpanel-python-0.0.2.tar.gz

---
+++ packages/mixpanel-python/mixpanel-python-0.0.2.tar.gz/mixpanel-python-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(name='mixpanel-python',
+      version='0.0.2',
+      description='Minimal Mixpanel API Client',
+      author='Andrew Kortina',
+      author_email='kortina@gmail.com',
+      url='https://github.com/kortina/mixpanel-python',
+      packages=['mixpanel'],
+      keywords=['mixpanel'],
+      license='MIT'
+)
