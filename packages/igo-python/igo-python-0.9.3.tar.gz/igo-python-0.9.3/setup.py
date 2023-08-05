2a8c23419796f840736eeba304793b3ebdc48f14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:23 +0000

    Add package igo-python/0.9.3/igo-python-0.9.3.tar.gz

---
+++ packages/igo-python/igo-python-0.9.3.tar.gz/igo-python-0.9.3/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from setuptools import setup
+
+
+setup(
+    name='igo-python',
+    version='0.9.3',
+    description='Python port of Igo Japanese morphological analyzer',
+    long_description=open('README').read() + "\n\n" + open('CHANGES').read(),
+    author='Hideaki Takahashi',
+    author_email='mymelo@gmail.com',
+    url='https://github.com/hideaki-t/igo-python/',
+    classifiers=['Development Status :: 4 - Beta',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: MIT License',
+                 'Natural Language :: Japanese',
+                 'Operating System :: OS Independent',
+                 'Operating System :: Microsoft :: Windows',
+                 'Operating System :: POSIX :: Linux',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3.2',
+                 'Programming Language :: Python :: 3.3',
+                 'Topic :: Scientific/Engineering :: Information Analysis',
+                 'Topic :: Software Development :: Libraries :: Python Modules',
+                 'Topic :: Text Processing :: Linguistic',
+                 ],
+    keywords=['japanese', 'morphological analyzer', ],
+    license='MIT',
+    packages=['igo'],
+    )
