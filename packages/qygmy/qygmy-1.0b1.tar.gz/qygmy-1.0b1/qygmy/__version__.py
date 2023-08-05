c189d491d8b2bb154be06a55c626f6dd3c03226d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:38 +0000

    Add package qygmy/1.0b1/qygmy-1.0b1.tar.gz

---
+++ packages/qygmy/qygmy-1.0b1.tar.gz/qygmy-1.0b1/qygmy/__version__.py
@@ -0,0 +1,48 @@
+
+import os
+import datetime
+import subprocess
+
+version_info = (1, 0, 0, 'beta', 1)
+
+def _get_version():
+    v = '%s.%s' % version_info[:2]
+    if version_info[2] != 0:
+        v += '.%s' % version_info[2]
+    v += '%s%s' % ({
+        'alpha': 'a',
+        'beta': 'b',
+        'rc': 'c',
+        'final': '',
+    }[version_info[3]], version_info[4])
+
+    repo_dir = os.path.dirname(os.path.abspath(__file__))
+    timestamp_file = os.path.join(repo_dir, 'gittimestamp.txt')
+
+    if version_info[3] == 'alpha':
+        git_log = subprocess.Popen('git log --pretty=format:%ct --quiet -1 HEAD',
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                shell=True, cwd=repo_dir, universal_newlines=True)
+        timestamp = git_log.communicate()[0]
+        try:
+            timestamp = datetime.datetime.utcfromtimestamp(int(timestamp))
+            timestamp = timestamp.strftime('%Y%m%d%H%M%S')
+        except ValueError:
+            timestamp = '0'
+        if timestamp != '0':
+            with open(timestamp_file, 'w') as f:
+                f.write(timestamp + '\n')
+        elif os.path.isfile(timestamp_file):
+            with open(timestamp_file, 'r') as f:
+                timestamp = f.read().strip()
+        v += '.dev' + timestamp
+
+    else:
+        try:
+            os.remove(timestamp_file)
+        except:
+            pass
+
+    return v
+
+version = _get_version()
---
+++ packages/qygmy/qygmy-1.0b1.tar.gz/qygmy-1.0b1/setup.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+
+import os
+import glob
+import shutil
+from distutils.core import setup
+from distutils.command.build import build
+from distutils.command.clean import clean
+from distutils.command.sdist import sdist
+from distutils.spawn import spawn
+
+from qygmy.__version__ import version
+SRC_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+class build(build):
+    def run(self):
+        os.chdir(SRC_DIR)
+        spawn(['make'])
+        super().run()
+
+class clean(clean):
+    def run(self):
+        os.chdir(SRC_DIR)
+        spawn(['make', 'clean'])
+        super().run()
+
+class deb(sdist):
+    def run(self):
+        spawn(['make', 'dchrelease'])
+        self.dist_dir = os.path.join(self.dist_dir, 'deb')
+        super().run()
+        name = self.distribution.get_name()
+        version = self.distribution.get_version()
+        ext = 'tar.gz'
+        sourcedir = name + '-' + version
+        tar = sourcedir + '.' + ext
+        debversion = version.replace('.dev', '~dev')
+        origtar = name + '_' + debversion + '.orig.' + ext
+        os.chdir(self.dist_dir)
+        spawn(['tar', 'xvzf', tar])
+        os.rename(tar, origtar)
+        os.chdir(sourcedir)
+        spawn(['debuild'])
+        os.chdir('..')
+        shutil.rmtree(sourcedir, True)
+        for i in glob.glob(name + '_' + debversion + '-*.build'):
+            os.remove(i)
+
+
+setup(
+    name = 'qygmy',
+    version = version,
+    description = "Simple MPD client.",
+    long_description = """Simple MPD client written in Python and Qt/PySide.""",
+    author = 'tsufeki',
+    author_email = 'tsufeki@ymail.com',
+    url = 'https://github.com/tsufeki/qygmy',
+    install_requires=[
+        'PySide>=1.1.2',
+        'python-mpd2',
+    ],
+    packages = [
+        'qygmy',
+        'qygmy.templates',
+        'qygmy.ui',
+    ],
+    package_data={
+        'qygmy': ['translations/*.qm', 'gittimestamp.txt'],
+    },
+    scripts=['bin/qygmyrun'],
+    data_files=[
+        ('share/applications', ['qygmy.desktop']),
+        ('share/man/man1', ['qygmy.1']),
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: X11 Applications :: Qt',
+        'Intended Audience :: End Users/Desktop',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Multimedia :: Sound/Audio',
+    ],
+    cmdclass={
+        'clean': clean,
+        'build': build,
+        'deb': deb,
+    },
+)
