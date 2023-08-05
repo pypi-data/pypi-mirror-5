dbaf08da26d8dae3b98ab3ce547501f17bbdf1cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:04 +0000

    Add package LewisUtils/0.1.1/LewisUtils-0.1.1.tar.gz

---
+++ packages/LewisUtils/LewisUtils-0.1.1.tar.gz/LewisUtils-0.1.1/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='LewisUtils',
+    version='0.1.1',
+    author='Lewis Zhou',
+    author_email='lewisou@gmail.com',
+    packages=['lewisou.utils'],
+    # scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
+    url='http://pypi.python.org/pypi/LewisUtils/',
+    license='LICENSE.txt',
+    description='Useful utils stuff.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        # "Django >= 1.1.1",
+        # "caldav == 0.1.4",
+    ],
+)
