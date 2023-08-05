02b89ea374db8fd364ecc7b5ce11aba7a1144e7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:22 +0000

    Add package django-ember-rest/0.0.8/django-ember-rest-0.0.8.tar.gz

---
+++ packages/django-ember-rest/django-ember-rest-0.0.8.tar.gz/django-ember-rest-0.0.8/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-ember-rest',
+    version='0.0.8',
+    description='Django Models <> Ember-Data',
+    author='Mitchel Kelonye',
+    author_email='kelonyemitchel@gmail.com',
+    url='https://github.com/kelonye/django-ember-rest',
+    requires=['Django (>=1.3.0)'],
+    packages=find_packages(),
+    license='MIT License',
+    zip_safe=True)
