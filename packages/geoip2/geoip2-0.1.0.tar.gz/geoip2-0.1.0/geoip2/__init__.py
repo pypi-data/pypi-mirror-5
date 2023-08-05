d8bb26c08594993dc2e1ddfb99e103c010638c7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:32:36 +0000

    Add package geoip2/0.1.0/geoip2-0.1.0.tar.gz

---
+++ packages/geoip2/geoip2-0.1.0.tar.gz/geoip2-0.1.0/geoip2/__init__.py
@@ -0,0 +1,10 @@
+"""
+=================
+GeoIP2 Python API
+=================
+"""
+__title__ = 'geoip2'
+__version__ = '0.1.0'
+__author__ = 'Gregory Oschwald'
+__license__ = 'LGPLv2+'
+__copyright__ = 'Copyright 2013 Maxmind, Inc.'
