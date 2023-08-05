3374ed1160d5bd52faaffab542082bec6e55a0c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:15 +0000

    Add package collective.groupmail/1.0.1/collective.groupmail-1.0.1.zip

---
+++ packages/collective.groupmail/collective.groupmail-1.0.1.zip/collective.groupmail-1.0.1/setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0.1'
+
+long_description = (
+    open('README.rst').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+setup(name='collective.groupmail',
+      version=version,
+      description="Send feedback email to groups in Plone",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python :: 2.6",
+        "Framework :: Plone :: 4.1",
+        ],
+      keywords='plone group email',
+      author='Lennart Regebro',
+      author_email='regebro@gmail.com',
+      url='http://svn.plone.org/svn/collective/',
+      license='gpl',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['collective', ],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'z3c.jbot',
+          # -*- Extra requirements: -*-
+      ],
+      extras_require={'test': ['plone.app.testing', 'unittest2', 'collective.MockMailHost']},
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["templer.localcommands"],
+      )
