468b17cf3acb95f989b17c5870d0ade1f648e1ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:30 +0000

    Add package QualysConnect/0.0.7/QualysConnect-0.0.7.tar.gz

---
+++ packages/QualysConnect/QualysConnect-0.0.7.tar.gz/QualysConnect-0.0.7/src/qualysconnect/__init__.py
@@ -0,0 +1,8 @@
+# This is the version string assigned to the entire egg post
+#  setup.py install
+__version__ = "0.0.7"
+
+# Ownership and Copyright Information.
+__author__ = "Colin Bell <colin.bell@uwaterloo.ca>"
+__copyright__ = "Copyright 2011-2013, University of Waterloo"
+__license__ = "BSD-new"
