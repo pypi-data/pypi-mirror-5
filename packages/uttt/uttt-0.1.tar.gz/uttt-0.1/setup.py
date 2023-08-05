d64826a776682f7b195377c12b01ec7350289db4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:24 +0000

    Add package uttt/0.1/uttt-0.1.tar.gz

---
+++ packages/uttt/uttt-0.1.tar.gz/uttt-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='uttt',
+      version='0.1',
+      description='An Ultimate Tic Tac Toe Class',
+      url='http://github.com/projectdelphai/uttt_irc',
+      author='projectdelphai',
+      author_email='projectdelphai@gmail.com',
+      license='MIT',
+      packages=['uttt'],
+      zip_safe=False)
