767e7747c80f93195a448a5e45d2a9ca472e3442
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:01 +0000

    Add package ftw.file/1.6.3/ftw.file-1.6.3.zip

---
+++ packages/ftw.file/ftw.file-1.6.3.zip/ftw.file-1.6.3/ftw/file/testing.py
@@ -0,0 +1,30 @@
+from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import applyProfile
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import FunctionalTesting
+from zope.configuration import xmlconfig
+from plone.testing.z2 import installProduct
+
+
+class FtwFileLayer(PloneSandboxLayer):
+
+    defaultBases = (PLONE_FIXTURE, )
+
+    def setUpZope(self, app, configurationContext):
+        # Load ZCML
+        import ftw.file
+        xmlconfig.file(
+            'configure.zcml', ftw.file, context=configurationContext)
+        installProduct(app, 'ftw.file')
+
+    def setUpPloneSite(self, portal):
+        # Install into Plone site using portal_setup
+        applyProfile(portal, 'plone.app.registry:default')
+        applyProfile(portal, 'ftw.file:default')
+
+FTW_FILE_FIXTURE = FtwFileLayer()
+FTW_FILE_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(FTW_FILE_FIXTURE, ), name="ftw.file:Integration")
+FTW_FILE_FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(FTW_FILE_FIXTURE, ), name="ftw.file:Functional")
