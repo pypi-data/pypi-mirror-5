020f71745c66f8bc6b87b4f5e19dd0912697f693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:14 +0000

    Add package django-system-monitor/0.2/django-system-monitor-0.2.tar.gz

---
+++ packages/django-system-monitor/django-system-monitor-0.2.tar.gz/django-system-monitor-0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import os
+
+setup(
+    name='django-system-monitor',
+    version='0.2',
+    author='Hakan OZAY',
+    author_email='hakanzy@gmail.com',
+    url='https://github.com/hakanzy/django-sysmon',
+    description='View simple system statistics in django admin panel',
+    long_description=os.path.join(os.path.dirname(__file__), 'REAMDE.md'),
+    packages=find_packages(exclude=[]),
+    install_requires=[
+        'psutil==0.7.1',
+    ],
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+)
