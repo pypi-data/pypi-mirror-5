4c963c73ee935c8121967b10778c52dc891a6d45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:39 +0000

    Add package ro-manager/0.2.12/ro-manager-0.2.12.tar.gz

---
+++ packages/ro-manager/ro-manager-0.2.12.tar.gz/ro-manager-0.2.12/rocommand/__init__.py
@@ -0,0 +1,28 @@
+# __init__.py
+#__version__ = "0.2.1"       # Initial version with installation package
+#__version__ = "0.2.2"       # Updated README documentation for PyPI page
+#__version__ = "0.2.3"       # Experimenting with distribution options
+#__version__ = "0.2.4"       # Added MANIFEST.in so that data files are part of sdist
+#__version__ = "0.2.5"       # Drop references to lpod-show from test suite
+#__version__ = "0.2.6"       # Enhacements to handling of directories and external references
+#__version__ = "0.2.7"       # Decouple MINIM constraints from target RO
+                             # ROSRS (v6) support, support evaluation of RODL/ROSRS objects
+                             # new annotation and linking options, 
+                             # annotations with CURIE (QName) properties
+                             # add ro remove command, fix URI escaping problems
+#__version__ = "0.2.8"       # Optimize annotation access in ROSRS_Session
+                             # Add web services for rendering traffic light displays
+#__version__ = "0.2.9"       # Fix up experiment-workflow checklist
+#__version__ = "0.2.10"      # Fix problem evaluating RO with space in URI
+                             # RDFReport add escaping support; escape quotes in JSON strings
+                             # Fix problem with URI liveness test
+                             # New RO must have ore:Aggregation class
+                             # Allow reporting of missing <forall> entries
+                             # Add ro dump command
+                             # Ordering of checklist display
+                             # Various display formatting enhancements
+#__version__ = "0.2.11"      # Add RO evolution commands; ro snapshow, ro archive, ro freeze
+__version__ = "0.2.12"      # Fix queries to work with rdflib 4.0.1 and rdflib-sparql
+                            # Fix some bugs in RODL synchronization and evolution
+                            # Refactor Minim model and add some new capabilities
+
---
+++ packages/ro-manager/ro-manager-0.2.12.tar.gz/ro-manager-0.2.12/setup.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+#
+# Setup.py based on https://github.com/paltman/python-setup-template/blob/master/setup.py,
+# following http://www.ibm.com/developerworks/opensource/library/os-pythonpackaging/index.html
+
+import codecs
+import os
+import sys
+
+from distutils.util import convert_path
+from fnmatch import fnmatchcase
+from setuptools import setup, find_packages
+
+def read(fname):
+    return codecs.open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+# (Leftover from previous setup?)
+standard_exclude = ["*.py", "*.pyc", "*$py.class", "*~", ".*", "*.bak"]
+standard_exclude_directories = [
+    ".*", "CVS", "_darcs", "./build", "./dist", "EGG-INFO", "*.egg-info"
+]
+
+PACKAGE = "rocommand"
+NAME = "ro-manager"
+DESCRIPTION = "Research Object manager (command line tool)"
+AUTHOR = "Graham Klyne"
+AUTHOR_EMAIL = "gk-pypi@ninebynine.org"
+URL = "https://github.com/wf4ever/ro-manager"
+VERSION = __import__(PACKAGE).__version__
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=read("README.md"),
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license="MIT",
+    url=URL,
+    packages = ['rocommand','rocommand.test'
+               ,'iaeval','iaeval.test'
+               ,'MiscLib'
+               ,'samples'
+               # ,'uritemplate'], #@@TODO: uritemplate already in PyPI?
+               ],
+    package_data = {
+        'rocommand':    [ 'test/config/*','test/robase/README','test/nobase/README'
+                        , 'test/data/ro-test-1/subdir1/*','test/data/ro-test-1/subdir2/*'
+                        , 'test/data/ro-test-1/*.rdf','test/data/ro-test-1/README*','test/data/ro-test-1/file*'
+                        ],
+        'iaeval':       [ 'test/config/*','test/robase/README'
+                        , 'test/test-data-1/*.rdf'
+                        , 'test/test-data-1/data/*', 'test/test-data-1/docs/*'
+                        , 'test/test-simple-wf/TODO'
+                        , 'test/test-simple-wf/*.rdf', 'test/test-simple-wf/*.sh'
+                        , 'test/test-simple-wf/data/*', 'test/test-simple-wf/docs/*'
+                        ],
+        'samples':      [ '*.sh', '*.py', 'prefixes.*', 'README' ],
+        },
+    exclude_package_data = {
+        '': ['spike/*'] 
+        },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        ],
+    zip_safe=False,
+    install_requires=["rdflib == 4.0.1", "rdflib-sparql == 0.2", "httplib2", "uritemplate"],
+    entry_points = {
+        'console_scripts': [
+            'ro = rocommand.ro:runMain',
+            'ro-manager-test = rocommand.test.RunRoManagerTests:runTestSuite'
+            ],
+        },
+    )
