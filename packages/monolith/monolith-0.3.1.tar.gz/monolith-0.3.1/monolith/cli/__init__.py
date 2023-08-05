e8e564f1ee8efada0ece0e70f6e0783e28d7c481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:33 +0000

    Add package monolith/0.3.1/monolith-0.3.1.tar.gz

---
+++ packages/monolith/monolith-0.3.1.tar.gz/monolith-0.3.1/monolith/cli/__init__.py
@@ -0,0 +1,21 @@
+from .base import BaseCommand
+from .base import ExecutionManager
+from .base import SimpleExecutionManager
+from .base import LabelCommand
+from .base import Parser
+from .base import SingleLabelCommand
+from .base import arg
+from .completion import CompletionCommand
+
+
+__all__ = [
+    'ExecutionManager',
+    'SimpleExecutionManager',
+    'arg',
+    'Parser',
+    'BaseCommand',
+    'LabelCommand',
+    'SingleLabelCommand',
+    'CompletionCommand',
+]
+
