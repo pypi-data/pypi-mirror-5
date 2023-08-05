21411a307d4b424246686dafe77d81b18419cce8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:21 +0000

    Add package infi.recipe.application-packager/0.3.23/infi.recipe.application_packager-0.3.23.tar.gz

---
+++ packages/infi.recipe.application-packager/infi.recipe.application_packager-0.3.23.tar.gz/infi.recipe.application_packager-0.3.23/src/infi/recipe/application_packager/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.3.23"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.3.23)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'b8f852bac6cd965c5442bf4fee894bc83dce71e9'
+__git_head_subject__ = 'Finished release v0.3.23.'
+__git_head_message__ = '* release/v0.3.23:\n  HOSTDEV-974 removing md5hash from the url\n  HOSTDEV-974 implemented and tests added'
+__git_dirty_diff__ = ''
