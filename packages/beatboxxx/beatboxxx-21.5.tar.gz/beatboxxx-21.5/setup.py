a3e7b1bf63d8c71e8abd2d15d6b353fb2e78ad25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:58 +0000

    Add package beatboxxx/21.5/beatboxxx-21.5.tar.gz

---
+++ packages/beatboxxx/beatboxxx-21.5.tar.gz/beatboxxx-21.5/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: UTF-8 -*-
+from setuptools import setup
+
+setup(
+    name='beatboxxx',
+    packages=['beatbox'],
+    package_dir={'': 'src'},
+    version='21.5',  # be sure to update the version in _beatbox.py too
+    description="A Python client for the Saleforce.com SOAP API",
+    author="RegioHelden GmbH, Simon Fell et al",
+    author_email='opensource@regiohelden.de',
+    url="https://github.com/RegioHelden/beatboxxx",
+    keywords="python salesforce salesforce.com",
+    license="GNU GENERAL PUBLIC LICENSE Version 2",
+    classifiers=[
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "Development Status :: 5 - Production/Stable"
+    ],
+    include_package_data=True,
+    long_description=open('README.rst').read(),
+    zip_safe=False,
+)
