ab5ac2a214a2dc2d68b3bc8b31b56a10a9109fd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 14:31:30 +0000

    Add package xbob.db.mnist/1.0.0a/xbob.db.mnist-1.0.0a.zip

---
+++ packages/xbob.db.mnist/xbob.db.mnist-1.0.0a.zip/xbob.db.mnist-1.0.0a/setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+# Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
+
+from setuptools import setup, find_packages
+
+# The only thing we do in this file is to call the setup() function with all
+# parameters that define our package.
+setup(
+
+    name='xbob.db.mnist',
+    version='1.0.0a',
+    description='MNIST Database Access API for Bob',
+    url='http://github.com/bioidiap/bob.db.mnist',
+    license='GPLv3',
+    author='Laurent El Shafey',
+    author_email='laurent.el-shafey@idiap.ch',
+    long_description=open('README.rst').read(),
+
+    # This line is required for any distutils based packaging.
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+
+    install_requires=[
+      'setuptools',
+      'bob',  # base signal proc./machine learning library
+    ],
+
+    namespace_packages = [
+      'xbob',
+      'xbob.db',
+      ],
+
+    entry_points={
+
+      # declare database to bob
+      'bob.db': [
+        'mnist = xbob.db.mnist.driver:Interface',
+        ],
+
+      # declare tests to bob
+      'bob.test': [
+        'mnist = xbob.db.mnist.test:MNISTDatabaseTest',
+        ],
+
+      },
+
+    classifiers = [
+      'Development Status :: 4 - Beta',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+      'Natural Language :: English',
+      'Programming Language :: Python',
+      'Topic :: Scientific/Engineering :: Artificial Intelligence',
+      'Topic :: Database :: Front-Ends',
+      ],
+)
