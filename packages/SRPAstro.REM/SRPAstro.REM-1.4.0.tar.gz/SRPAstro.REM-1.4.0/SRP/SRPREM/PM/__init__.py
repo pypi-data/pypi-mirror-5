120f33e700f496d283cbd13c9ee0c789b6e86ce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:24 +0000

    Add package SRPAstro.REM/1.4.0/SRPAstro.REM-1.4.0.tar.gz

---
+++ packages/SRPAstro.REM/SRPAstro.REM-1.4.0.tar.gz/SRPAstro.REM-1.4.0/SRP/SRPREM/PM/__init__.py
@@ -0,0 +1,25 @@
+""" 
+
+Context : SRP
+Module  : REM
+Version : 1.0.1
+Author  : Stefano Covino
+Date    : 22/08/2013
+E-mail  : stefano.covino@brera.inaf.it
+URL     : http://www.me.oa-brera.inaf.it/utenti/covino
+
+
+Usage   : to be imported
+
+Remarks :
+
+History : (24/08/2012) First version.
+        : (22/08/2013) Funtions reordered.
+"""
+
+
+__all__ = ['Classic', 'FindOrigCoord', 'Full', 'minClassic', 'minFull',
+            'minSimple', 'Simple']
+
+
+PMFlavours = ['classic', 'full', 'simple']
