853594bd9f7a0c35f7d44c7f702de7d4d45c8cb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:09 +0000

    Add package lp-helpers/0.10/lp-helpers-0.10.tar.gz

---
+++ packages/lp-helpers/lp-helpers-0.10.tar.gz/lp-helpers-0.10/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name = "lp-helpers",
+    version = '0.10',
+    author = "Brendan Donegan",
+    author_email = "brendan-donegan@canonical.com",
+    license = "GPL",
+    description = "Helper scripts for working with Launchpad and Bazaar.",
+    long_description = """
+This project provides scripts for simplifying common workflows involving Launchpad and Bazaar. More information at https://github.com/brendan-donegan/lp-helpers
+""",
+    scripts = ["lp-propose-merge","lp-file-bug", "lp-recipe-build"],
+    install_requires = ["bzr", "launchpadlib"]
+)
+
