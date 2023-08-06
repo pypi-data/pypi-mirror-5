2d7dc9fbb7bacd68694b8cfe9b612548497127b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:10 +0000

    Add package support-cdm/1.0/support-cdm-1.0.tar.gz

---
+++ packages/support-cdm/support-cdm-1.0.tar.gz/support-cdm-1.0/__plugin__/cdm/__init__.py
@@ -0,0 +1,22 @@
+'''
+Created on Jul 15, 2011
+
+@package: support cdm
+@copyright: 2012 Sourcefabric o.p.s.
+@license: http://www.gnu.org/licenses/gpl-3.0.txt
+@author: Gabriel Nistor
+
+Contains setup and configuration files for cdm file system.
+'''
+# --------------------------------------------------------------------
+
+NAME = 'cdm'
+GROUP = 'cdm'
+VERSION = '1.0'
+DESCRIPTION = 'Provides the cdm file system service'
+KEYWORDS = ['Ally', 'REST', 'plugin', 'cdm']
+AUTHOR = 'Gabriel Nistor'
+AUTHOR_EMAIL = "gabriel.nistor@sourcefabric.org"
+INSTALL_REQUIRES = ['ally >= 1.0'] 
+
+# --------------------------------------------------------------------
---
+++ packages/support-cdm/support-cdm-1.0.tar.gz/support-cdm-1.0/setup.py
@@ -0,0 +1,31 @@
+
+'''
+Created on Oct 1, 2013
+ 
+@package: distribution_manager
+@copyright: 2013 Sourcefabric o.p.s.
+@license: http://www.gnu.org/licenses/gpl-3.0.txt
+@author: Cristian Domsa
+ 
+Setup configuration for components/plugins needed for pypi.
+'''
+
+# --------------------------------------------------------------------
+
+from setuptools import setup, find_packages
+
+# --------------------------------------------------------------------
+
+setup(packages=find_packages('.'),
+     platforms=['all'],
+     zip_safe=True,
+     license='GPL v3',
+     url='http://www.sourcefabric.org/en/superdesk/', # project home page     description='Provides the cdm file system service',
+     author='Gabriel Nistor',
+     install_requires=['ally >= 1.0'],
+     author_email='gabriel.nistor@sourcefabric.org',
+     version='1.0',
+     keywords=['Ally', 'REST', 'plugin', 'cdm'],
+     name='support-cdm',
+
+     )
