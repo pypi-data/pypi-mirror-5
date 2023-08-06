8bc7b8053badcfdd051fba7a9491eb14944ad518
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:03 +0000

    Add package conflate/0.5.2/conflate-0.5.2.tar.gz

---
+++ packages/conflate/conflate-0.5.2.tar.gz/conflate-0.5.2/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(name='conflate',
+      version='0.5.2',
+      py_modules=['conflate'],
+      url='https://github.com/rfinz/conflate',
+      description='Module for implementing stupid simple configuration files.' 
+      )
