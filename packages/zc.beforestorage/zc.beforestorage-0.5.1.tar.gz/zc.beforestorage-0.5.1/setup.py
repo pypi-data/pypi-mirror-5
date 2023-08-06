982a06bdfede83a4c54c7325863ac7507e1e4b79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:06 +0000

    Add package zc.beforestorage/0.5.1/zc.beforestorage-0.5.1.tar.gz

---
+++ packages/zc.beforestorage/zc.beforestorage-0.5.1.tar.gz/zc.beforestorage-0.5.1/setup.py
@@ -0,0 +1,62 @@
+##############################################################################
+#
+# Copyright (c) Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+name, version = 'zc.beforestorage', '0.5.1'
+
+import os
+from setuptools import setup, find_packages
+
+def read(rname):
+    return open(os.path.join(os.path.dirname(__file__), *rname.split('/')
+                             )).read()
+
+entry_points = """
+"""
+
+long_description = (
+        read('src/zc/beforestorage/README.txt')
+        + '\n' +
+        'Download\n'
+        '========\n'
+        )
+
+tests_require = ['zope.testing']
+
+setup(
+    name = name,
+    version = version,
+    author = 'Jim Fulton',
+    author_email = 'jim@zope.com',
+    description = 'View storage before a given time',
+    long_description=long_description,
+    license = 'ZPL 2.1',
+
+    include_package_data = True,
+    packages = find_packages('src'),
+    namespace_packages = ['zc'],
+    package_dir = {'': 'src'},
+    install_requires = ['setuptools'],
+    zip_safe = False,
+    entry_points=entry_points,
+    tests_require = tests_require,
+    extras_require = dict(test=tests_require),
+    classifiers = [
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Topic :: Database',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: Unix',
+        'Framework :: ZODB',
+        ],
+    )
---
+++ packages/zc.beforestorage/zc.beforestorage-0.5.1.tar.gz/zc.beforestorage-0.5.1/src/zc/beforestorage/tests.py
@@ -0,0 +1,46 @@
+##############################################################################
+#
+# Copyright (c) Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+import time, unittest
+import doctest
+import zope.testing.setupstack
+import zc.beforestorage
+
+
+def setUp(test):
+    zope.testing.setupstack.setUpDirectory(test)
+    now = [1200939762]
+    def timetime():
+        now[0] += 1
+        return now[0]
+
+    old_timetime = time.time
+    zope.testing.setupstack.register(
+        test,
+        lambda : setattr(time, 'time', old_timetime)
+        )
+    time.time = timetime
+    zc.beforestorage.startup_time_stamp = zc.beforestorage.time_stamp()
+
+
+def test_suite():
+    return unittest.TestSuite((
+        doctest.DocFileSuite(
+            'README.txt',
+            setUp=setUp, tearDown=zope.testing.setupstack.tearDown,
+            optionflags=doctest.IGNORE_EXCEPTION_DETAIL,
+            ),
+        ))
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
