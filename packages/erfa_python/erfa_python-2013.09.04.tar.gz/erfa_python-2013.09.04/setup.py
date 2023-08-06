c05ebabdfa6931140fc059c0a978ec0bbf8070a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:05 +0000

    Add package erfa_python/2013.09.04/erfa_python-2013.09.04.tar.gz

---
+++ packages/erfa_python/erfa_python-2013.09.04.tar.gz/erfa_python-2013.09.04/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup, Extension
+
+INC_DIR = ['$HOME/include',
+           '/usr/include',
+           '/usr/local/include']
+LIB_DIR = ['$HOME/lib',
+           '/usr/lib64',
+           '/usr/lib',
+           '/usr/local/lib',]
+
+moduleerfa = Extension('erfa',
+                    include_dirs = INC_DIR,
+                    libraries = ['erfa', 'm'],
+                    library_dirs = LIB_DIR,
+                    sources = ['src/erfamodule.c'])
+
+setup (name = 'erfa_python',
+       version = '2013.09.04',
+       description = 'Python wrapper for ERFA library',
+       url='https://github.com/nirinA/erfa_python',
+       author='nirinA raseliarison',
+       author_email='nirina.raseliarison@gmail.com',
+       ext_modules = [moduleerfa, ],
+       license="Public Domain")
