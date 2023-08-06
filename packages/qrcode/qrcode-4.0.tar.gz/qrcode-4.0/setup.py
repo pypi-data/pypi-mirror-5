2992f4d1d470a150f7e93d7baa5dccf59db9b1b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:45 +0000

    Add package qrcode/4.0/qrcode-4.0.tar.gz

---
+++ packages/qrcode/qrcode-4.0.tar.gz/qrcode-4.0/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='qrcode',
+    version='4.0',
+    url='https://github.com/lincolnloop/python-qrcode',
+    #download_url='',
+    description='QR Code image generator',
+    license='BSD',
+    long_description=open('README.rst').read(),
+    author='Lincoln Loop',
+    author_email='info@lincolnloop.com',
+    platforms=['any'],
+    packages=[
+        'qrcode',
+        'qrcode.image',
+    ],
+    scripts=[
+        'scripts/qr',
+    ],
+    requires=['six'],
+    data_files=[('share/man/man1', ['doc/qr.1'])],
+    package_data={'': ['LICENSE']},
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Multimedia :: Graphics',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
