bba92c8eac4afe4382f100c93fe2d4621994a1af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:24 +0000

    Add package recursion_xxx/1.1.0/recursion_xxx-1.1.0.zip

---
+++ packages/recursion_xxx/recursion_xxx-1.1.0.zip/recursion_xxx-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+name='recursion_xxx',
+version='1.1.0',
+py_modules=['recursion'],
+author='test',
+author_email='xuc@iphonele.com',
+url = 'http://www.guang.com',
+description='A simple recursion for list'
+)
