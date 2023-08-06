fe46cbd479efcdc715eb884f60cdd070db82e04c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:58 +0000

    Add package coviolations_app/0.2dev/coviolations_app-0.2dev.tar.gz

---
+++ packages/coviolations_app/coviolations_app-0.2dev.tar.gz/coviolations_app-0.2dev/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.2'
+
+setup(
+    name='coviolations_app',
+    version=version,
+    description="coviolations.io app",
+    long_description=open('README.rst').read(),
+    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='',
+    author='Vladimir Iakovlev',
+    author_email='nvbn.rm@gmail.com',
+    url='http://coviolations.io',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'pyyaml',
+        'requests',
+        'gitpython',
+    ],
+    entry_points={
+        'console_scripts': [
+            'covio=coviolations_app.covio:main',
+        ]
+    },
+)
