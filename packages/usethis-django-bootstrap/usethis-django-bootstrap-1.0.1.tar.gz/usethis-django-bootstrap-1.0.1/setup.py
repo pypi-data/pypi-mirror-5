84a98af35cd3c64775d78780bb2672f9d70a4102
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:56 +0000

    Add package usethis-django-bootstrap/1.0.1/usethis-django-bootstrap-1.0.1.tar.gz

---
+++ packages/usethis-django-bootstrap/usethis-django-bootstrap-1.0.1.tar.gz/usethis-django-bootstrap-1.0.1/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from distutils.core import setup
+
+setup(name='usethis-django-bootstrap',
+      version='1.0.1',
+      description="Bootstrap for Django with themes and theme chooser.",
+      author="Jeff Buttars",
+      author_email="jeffbuttars@gmail.com",
+      packages=['usethis_bootstrap'],
+      url='https://github.com/jeffbuttars/usethis-django-bootstrap',
+      download_url='https://github.com/jeffbuttars/usethis-django-bootstrap/releases/download/1.0.1/usethis-django-bootstrap-1.0.1-2.3.2.tar.gz',
+      license='MIT',
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Browsers',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+      ],
+      package_dir={'usethis_bootstrap': 'usethis_bootstrap'},
+      package_data={'usethis_bootstrap': 
+                    ['static/css/bsthemes/*/*.css',
+                     'static/bootstrap/css/*.css',
+                     'static/bootstrap/img/*.png',
+                     'static/bootstrap/js/*.js',
+                     'templates/*.html',
+                    ]}
+)
