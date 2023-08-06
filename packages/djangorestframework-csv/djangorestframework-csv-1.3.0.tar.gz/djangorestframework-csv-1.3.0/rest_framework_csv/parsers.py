f7f66cdbbbd7dc1573334e110d5ef3a618fd2066
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:17 +0000

    Add package djangorestframework-csv/1.3.0/djangorestframework-csv-1.3.0.tar.gz

---
+++ packages/djangorestframework-csv/djangorestframework-csv-1.3.0.tar.gz/djangorestframework-csv-1.3.0/rest_framework_csv/parsers.py
@@ -0,0 +1,29 @@
+import csv
+
+from rest_framework.parsers import BaseParser
+from rest_framework.exceptions import ParseError
+from rest_framework_csv.orderedrows import OrderedRows
+
+
+class CSVParser(BaseParser):
+    """
+    Parses CSV serialized data.
+
+    The parser assumes the first line contains the column names.
+    """
+
+    media_type = 'text/csv'
+
+    def parse(self, stream, media_type=None, parser_context=None):
+        parser_context = parser_context or {}
+        delimiter = parser_context.get('delimiter', ',')
+
+        try:
+            rows = csv.reader(stream, delimiter=delimiter)
+            data = OrderedRows(next(rows))
+            for row in rows:
+                row_data = dict(zip(data.header, row))
+                data.append(row_data)
+            return data
+        except Exception as exc:
+            raise ParseError('CSV parse error - %s' % str(exc))
