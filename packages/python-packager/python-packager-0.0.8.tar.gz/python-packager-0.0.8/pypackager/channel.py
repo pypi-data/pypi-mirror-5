ca0aace4701f931c6e16277b94b819d12131ca90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:49 +0000

    Add package python-packager/0.0.8/python-packager-0.0.8.tar.gz

---
+++ packages/python-packager/python-packager-0.0.8.tar.gz/python-packager-0.0.8/pypackager/channel.py
@@ -0,0 +1,92 @@
+from datetime import datetime
+import json
+import os
+import shutil
+try:
+    from urllib2 import urlopen
+except ImportError:
+    from urllib.request import urlopen
+
+from .base import BasePackager
+from .extractor import PackageExtractor
+
+
+class PackagerChannel(BasePackager):
+    _data = None
+    _timestamp = None
+
+    def __init__(self, settings):
+        super(PackagerChannel, self).__init__(settings)
+        self.templates_dir = settings['templates_dir']
+        self.channel_url = settings['index']
+        if not os.path.exists(self.templates_dir):
+            os.makedirs(self.templates_dir)
+
+    @property
+    def data(self):
+        if self._data is None:
+            self._data = self.fetch()
+            self._timestamp = datetime.utcnow()
+        return self._data
+
+    def fetch(self):
+        response = urlopen(self.channel_url)
+        return json.loads(response.read())
+
+    @property
+    def installed_list(self):
+        templates = [d for d in os.listdir(self.templates_dir) if os.path.isdir(os.path.join(self.templates_dir, d))]
+        templates.sort()
+        return templates
+
+    def installed(self):
+        for name in self.installed_list:
+            print(name)
+        print("%d templates installed." % len(self.installed_list))
+
+    @property
+    def template_list(self):
+        return self.data.keys()
+
+    def template_info(self, template_name, template_info):
+        return "%s v%s %s" % (template_name, template_info['version'], template_info['author'])
+
+    def list(self):
+        for name, info in self.data.items():
+            print(self.template_info(name, info))
+
+    def search(self, text):
+        for name, info in self.data.items():
+            if text in name:
+                print(self.template_info(name, info))
+
+    def template_path(self, template_name):
+        return os.path.join(self.templates_dir, template_name, self.settings['template_wrap_dir'])
+
+    def template_data(self, template_name):
+        if template_name not in self.data:
+            raise RuntimeError("Template '%s' not found." % template_name)
+        return self.data[template_name]
+
+    def template_url(self, template_name):
+        return self.template_data(template_name)['url']
+
+    def template_version(self, template_name):
+        return self.template_data(template_name)['version']
+
+    def template_author(self, template_name):
+        return self.template_data(template_name)['author']
+
+    def add(self, template_name, url=None):
+        if url is None:
+            url = self.template_url(template_name)
+        destination = os.path.join(self.templates_dir, template_name)
+        extractor = PackageExtractor(url=url, template_name=template_name)
+        extractor.extract(destination)
+
+    def remove(self, template_name):
+        template_dir = os.path.join(self.templates_dir, template_name)
+        if os.path.exists(template_dir):
+            shutil.rmtree(template_dir)
+        else:
+            print("Template '%s' is not installed." % template_name)
---
+++ packages/python-packager/python-packager-0.0.8.tar.gz/python-packager-0.0.8/pypackager/settings.py
@@ -0,0 +1,135 @@
+try:
+    from ConfigParser import SafeConfigParser
+except ImportError:
+    from configparser import SafeConfigParser
+import os
+
+from .constants import LICENSES
+from .loaders import URLLoader, FileSystemLoader, ChannelLoader
+from .utils import clean_dict, recursive_update
+
+DEFAULTS = {
+    'config_file': os.path.expanduser('~/.pypackager/pypackager.cfg'),
+    'template': {
+        'dir': os.path.join(os.path.dirname(os.path.abspath(__file__)), 'template'),
+        'syntax': 'pystache'
+    },
+    'index': 'https://raw.github.com/fcurella/pypackager-channel/master/index.json',
+    'template-loaders': (
+        URLLoader,
+        FileSystemLoader,
+        ChannelLoader
+    ),
+    'templates_dir': os.path.join(os.path.expanduser('~/.pypackager'), 'templates'),
+    'template_wrap_dir': 'template',
+    'support_dir': os.path.expanduser('~/.pypackager'),
+}
+
+
+
+class RecursiveDict(dict):
+    def recursive_update(self, kwargs):
+        self = recursive_update(self, clean_dict(kwargs))
+        return self
+
+
+class SettingsLoader(RecursiveDict):
+    def parse(self, config_file):
+        kwargs = {}
+        parser = SafeConfigParser()
+
+        if os.path.exists(config_file):
+            with open(config_file) as fh:
+                parser.readfp(fh)
+
+            for section in parser.sections():
+                kwargs[section] = recursive_update(kwargs.get(section, {}), dict(parser.items(section)))
+
+        if 'license' in kwargs and 'type' in kwargs['license']:
+            license_shortname = kwargs['license']['type']
+            if license_shortname in LICENSES:
+                kwargs['license']['classifier'] = LICENSES[license_shortname]
+                kwargs['license']['name'] = LICENSES[license_shortname].split('::')[-1].strip()
+            else:
+                kwargs['license']['classifier'] = LICENSES['other']
+                kwargs['license']['name'] = license_shortname
+
+        return kwargs
+
+
+class DefaultsLoader(SettingsLoader):
+    def __init__(self):
+        super(DefaultsLoader, self).__init__(**DEFAULTS)
+
+
+class UserSettingLoader(SettingsLoader):
+    config_file = DEFAULTS['config_file']
+
+    def __init__(self, config_file=None):
+        if config_file is not None:
+            self.config_file = config_file
+
+        kwargs = self.parse(self.config_file)
+
+        super(UserSettingLoader, self).__init__(**kwargs)
+
+
+class TemplateSettingLoader(SettingsLoader):
+    templates_dir = DEFAULTS['templates_dir']
+
+    def __init__(self, template, loaders, settings):
+        kwargs = {}
+
+        if 'templates_dir' not in settings:
+            settings['templates_dir'] = self.templates_dir
+
+        template_dir = None
+        for Loader in loaders:
+            loader = Loader(settings, template)
+            if loader.template_exists():
+                template_dir = loader.template_path()
+                break
+
+        package_cfg = os.path.join(template_dir, '.package.cfg')
+        kwargs = self.parse(package_cfg)
+        super(TemplateSettingLoader, self).__init__(**kwargs)
+
+
+class CommandLineSettingLoader(SettingsLoader):
+    pass
+
+
+class SettingsReader(dict):
+    support_dir = os.path.expanduser('~/.pypackager')
+
+    def __init__(self, config_file=None, *args, **kwargs):
+        _kwargs = RecursiveDict()
+
+        defaults = DefaultsLoader()
+        command_line = CommandLineSettingLoader(**kwargs)
+        user = UserSettingLoader(config_file)
+
+        if 'index' in command_line:
+            index = command_line['index']
+        else:
+            index = defaults['index']
+
+        template_loader_settings = {
+            'index': index,
+            'template_wrap_dir': DEFAULTS['template_wrap_dir'],
+            'support_dir': DEFAULTS['support_dir']
+        }
+
+        if 'template' in command_line and 'dir' in command_line['template']:
+            template_dir = command_line['template']['dir']
+        else:
+            template_dir = defaults['template']['dir']
+
+        template = TemplateSettingLoader(template_dir, defaults['template-loaders'], template_loader_settings)
+
+        _kwargs.recursive_update(defaults)
+        _kwargs.recursive_update(template)
+        _kwargs.recursive_update(user)
+        _kwargs.recursive_update(command_line)
+
+        super(SettingsReader, self).__init__(**_kwargs)
