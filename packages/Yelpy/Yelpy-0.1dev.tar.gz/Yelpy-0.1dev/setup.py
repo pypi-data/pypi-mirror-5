98e17ff91affba6efe2ac40406ebbeeac2719fda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:14 +0000

    Add package Yelpy/0.1dev/Yelpy-0.1dev.tar.gz

---
+++ packages/Yelpy/Yelpy-0.1dev.tar.gz/Yelpy-0.1dev/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Yelpy',
+    version='0.1dev',
+    description='Yelp API Client',
+    author='Bryan Marty',
+    author_email='hello@bryanmarty.com',
+    url='http://www.bryanmarty.com',
+    packages=['yelpy'],
+    license='GPLv2',
+)
