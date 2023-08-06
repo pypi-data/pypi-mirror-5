7398a4be66f4111f68ae82150fba47034a353c7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:46 +0000

    Add package vr.common/3.1.3/vr.common-3.1.3.tar.gz

---
+++ packages/vr.common/vr.common-3.1.3.tar.gz/vr.common-3.1.3/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+from setuptools import setup, find_packages
+
+setup(
+    name='vr.common',
+    namespace_packages=['vr'],
+    version='3.1.3',
+    author='Brent Tubbs',
+    author_email='brent.tubbs@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Fabric==1.4.0',
+        'isodate>=0.4.4',
+        'mercurial>=2.6.1',
+        'paramiko>=1.8.0,<2.0',
+        'suds==0.4',
+    ],
+    description=('Libraries and command line tools for deploying with '
+                 'Velociraptor'),
+)
