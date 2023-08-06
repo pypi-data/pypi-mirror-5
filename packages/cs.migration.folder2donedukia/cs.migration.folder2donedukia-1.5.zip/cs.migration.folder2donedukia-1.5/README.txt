828bb0e4fc95a5dbec28ca102c61430892ba7b55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:54 +0000

    Add package cs.migration.folder2donedukia/1.5/cs.migration.folder2donedukia-1.5.zip

---
+++ packages/cs.migration.folder2donedukia/cs.migration.folder2donedukia-1.5.zip/cs.migration.folder2donedukia-1.5/README.txt
@@ -0,0 +1,7 @@
+Introduction
+============
+
+Just a simple product that migrates a Folder structure to use DonEdukia, also migrating the content on default pages to DonEdukias
+
+
+
---
+++ packages/cs.migration.folder2donedukia/cs.migration.folder2donedukia-1.5.zip/cs.migration.folder2donedukia-1.5/cs/migration/folder2donedukia/browser/folder.py
@@ -0,0 +1,70 @@
+from Acquisition import aq_inner, aq_parent
+from Products.Five.browser import BrowserView
+
+class Folder2DonEdukia(BrowserView):
+    def __call__(self):
+        context = aq_inner(self.context)
+        parent = aq_parent(context)
+        default_page_id = context.getProperty('default_page', None)
+        if default_page_id is not None:
+            elem = getattr(context, default_page_id, None)
+            if elem is not None:
+                new_id = parent.invokeFactory(id=parent.generateUniqueId(),
+                                              type_name='DonEdukia',
+                                              title=elem.Title(),
+                                              description=elem.Description(),
+                                              text=elem.getText(),
+                                              )
+        else:
+            new_id = parent.invokeFactory(id=parent.generateUniqueId(),
+                                          type_name='DonEdukia',
+                                          title=context.Title(),
+                                          description=context.Description(),
+                                          ) 
+        new_obj = getattr(parent, new_id)
+        new_obj._renameAfterCreation()
+           
+        try:
+            # Try with translations
+            for lang, props in context.getTranslations().items():
+                obj = props[0]
+                if obj.UID() != context.UID():
+                    obj.removeTranslationReference(context)
+                    context.removeTranslationReference(obj)
+                    new_obj.addTranslationReference(obj)
+        except:
+            pass
+        
+        cp_data = context.manage_copyObjects([i.getId for i in context.getFolderContents()])
+        new_obj.manage_pasteObjects(cp_data)
+        parent.manage_delObjects(context.getId())
+        new_obj._renameAfterCreation()
+        return self.request.response.redirect(new_obj.absolute_url())
+
+
+class Doc2DonEdukia(BrowserView):
+    def __call__(self):
+        context = aq_inner(self.context)
+        parent = aq_parent(context)
+        new_id = parent.invokeFactory(id=parent.generateUniqueId(),
+                                      type_name='DonEdukia',
+                                      title=context.Title(),
+                                      description=context.Description(),
+                                      text=context.getText(),
+                                      ) 
+        new_obj = getattr(parent, new_id)
+        new_obj._renameAfterCreation()
+        try:
+            # Try with translations
+            for lang, props in context.getTranslations().items():
+                obj = props[0]
+                if obj.UID() != context.UID():
+                    obj.removeTranslationReference(context)
+                    context.removeTranslationReference(obj)
+                    new_obj.addTranslationReference(obj)
+        except:
+            pass
+        
+        new_obj._renameAfterCreation()
+        return self.request.response.redirect(new_obj.absolute_url())
+
