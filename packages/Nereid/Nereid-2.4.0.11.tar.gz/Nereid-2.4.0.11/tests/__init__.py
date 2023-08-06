b6e80aff5fd850ded9d21739788f36f6913c35e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:03 +0000

    Add package Nereid/2.4.0.11/Nereid-2.4.0.11.tar.gz

---
+++ packages/Nereid/Nereid-2.4.0.11.tar.gz/Nereid-2.4.0.11/tests/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    __init__
+
+    Generic tests for nereid.
+
+    :copyright: (c) 2011-2012 by Openlabs Technologies & Consulting (P) Limited
+    :license: GPLv3, see LICENSE for more details.
+"""
+from texttestrunner import suite
