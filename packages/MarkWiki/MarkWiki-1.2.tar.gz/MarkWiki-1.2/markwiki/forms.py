9d710cc48a67c2a99725ced6887f66ebf860be66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:57 +0000

    Add package MarkWiki/1.2/MarkWiki-1.2.tar.gz

---
+++ packages/MarkWiki/MarkWiki-1.2.tar.gz/MarkWiki-1.2/markwiki/forms.py
@@ -0,0 +1,30 @@
+# Copyright (c) 2013, Matt Layman
+'''Forms used by MarkWiki'''
+
+from flask.ext.wtf import Form
+from wtforms.fields import HiddenField
+from wtforms.fields import PasswordField
+from wtforms.fields import StringField
+from wtforms.validators import InputRequired
+from wtforms.validators import Length
+
+
+class AddUserForm(Form):
+    username = StringField('Username', [
+        InputRequired('You did not provide a username.'),
+        Length(max=80, message='Sorry, the max length of a username is 80.')
+    ])
+
+
+class LoginForm(Form):
+    username = StringField('Username', [
+        InputRequired('You did not provide a username.'),
+        Length(max=80, message='Sorry, the max length of a username is 80.')
+    ])
+
+    password = PasswordField('Password', [
+        InputRequired('You did not provide a password.')
+    ])
+
+    # 'next' is used by Flask-Login to handle redirects.
+    next = HiddenField()
---
+++ packages/MarkWiki/MarkWiki-1.2.tar.gz/MarkWiki-1.2/setup.py
@@ -0,0 +1,38 @@
+# Copyright (c) 2013, Matt Layman
+'''
+MarkWiki
+========
+
+MarkWiki is a simple wiki using Markdown.
+
+Follow development on `GitHub <https://github.com/mblayman/markwiki>`_.
+'''
+
+from setuptools import setup
+
+setup(
+    name='MarkWiki',
+    version='1.2',
+    url='https://github.com/mblayman/markwiki',
+    license='BSD',
+    author='Matt Layman',
+    author_email='matthewlayman@gmail.com',
+    description='A simple wiki using Markdown',
+    long_description=__doc__,
+    packages=['markwiki'],
+    entry_points={
+        'console_scripts': ['markwiki = markwiki.server:run']
+    },
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'argparse',
+        'Flask',
+        'Frozen-Flask',
+        'Flask-Login',
+        'Flask-WTF',
+        'Markdown',
+        'Pygments',
+    ],
+    test_suite='markwiki.tests'
+)
