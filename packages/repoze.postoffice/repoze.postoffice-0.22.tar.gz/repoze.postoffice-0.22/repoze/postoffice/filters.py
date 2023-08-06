c329bbd8e1109801cfae7761d9ffc819cbc58846
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:18 +0000

    Add package repoze.postoffice/0.22/repoze.postoffice-0.22.tar.gz

---
+++ packages/repoze.postoffice/repoze.postoffice-0.22.tar.gz/repoze.postoffice-0.22/repoze/postoffice/filters.py
@@ -0,0 +1,128 @@
+from __future__ import with_statement
+
+import codecs
+import re
+
+from repoze.postoffice.message import decode_header
+
+
+class ToHostnameFilter(object):
+    """
+    Matches the hostname of the email address in the 'To:' header of an email
+    message.
+    """
+    def __init__(self, expr):
+        self.expr = expr
+
+    def __call__(self, message):
+        addrs = []
+        for header in 'To', 'Cc':
+            value = message.get(header)
+            if not value:
+                continue
+            addrs.extend(value.split(','))
+
+        for addr in addrs:
+            lt = addr.find('<')
+            if lt != -1:
+                gt = addr.rfind('>')
+                if gt == -1:
+                    gt = None
+                addr = addr[lt+1:gt]
+            if '@' not in addr:
+                continue
+            hostname = addr.split('@')[1].lower()
+
+            for expr in self.expr.lower().split():
+                if expr.startswith('.') and hostname.endswith(expr[1:]):
+                    return 'to_hostname: %s matches %s' % (addr, expr)
+                if hostname == expr:
+                    return 'to_hostname: %s matches %s' % (addr, expr)
+
+        return None
+
+
+class HeaderRegexpFilter(object):
+    """
+    Matches a regular expression on the headers of an email message.
+    """
+    def __init__(self, *exprs):
+        self.regexps = [(expr, re.compile(expr))
+                        for expr in exprs]
+
+    def __call__(self, message):
+        for name in message.keys():
+            header = '%s: %s' % (name, decode_header(message.get(name)))
+            for regexp, compiled in self.regexps:
+                if compiled.match(header) is not None:
+                    return 'header_regexp: headers match %s' % repr(regexp)
+        return None
+
+
+class HeaderRegexpFileFilter(HeaderRegexpFilter):
+    """
+    Same as HeaderRegexpFilter but loads regexps from a file.
+    """
+    def __init__(self, path):
+        self.regexps = regexps = []
+        with codecs.open(path, 'r', 'UTF-8') as f:
+            for line in f:
+                expr = line.rstrip('\n').rstrip('\r')
+                regexps.append((expr, re.compile(expr)))
+
+
+class BodyRegexpFilter(object):
+    """
+    Matches a regular expression on the body of an email message (any part).
+    """
+    def __init__(self, *exprs):
+        self.regexps = [(expr, re.compile(expr, re.MULTILINE))
+                        for expr in exprs]
+
+    def __call__(self, message):
+        for part in message.walk():
+            if not part.get_content_type().startswith('text/'):
+                continue
+
+            # Get body for this message part as unicode
+            body = part.get_payload(decode=True)
+            charset = part.get_charset()
+            if charset is None:
+                content_type = part.get('Content-Type')
+                if content_type is not None and 'charset=' in content_type:
+                    double_slash = content_type.find('//')
+                    if double_slash != -1:
+                        content_type = content_type[:double_slash].strip()
+                    for piece in content_type.split(';'):
+                        piece = piece.strip()
+                        if piece.startswith('charset='):
+                            charset = piece[8:]
+                            break
+            else:
+                charset = str(charset)
+
+            try_charsets = filter(None, [charset, 'UTF-8', 'ISO-8859-1'])
+            for charset in try_charsets:
+                try:
+                    body = body.decode(charset)
+                    break
+                except (LookupError, UnicodeError):
+                    pass
+
+            # See if we match
+            for regexp, compiled in self.regexps:
+                if compiled.search(body) is not None:
+                    return 'body_regexp: body matches %s' % repr(regexp)
+
+        return None
+
+class BodyRegexpFileFilter(BodyRegexpFilter):
+    """
+    Same as BodyRegexpFilter but loads regexps from a file.
+    """
+    def __init__(self, path):
+        self.regexps = regexps = []
+        with codecs.open(path, 'r', 'UTF-8') as f:
+            for line in f:
+                expr = line.rstrip('\n').rstrip('\r')
+                regexps.append((expr, re.compile(expr, re.MULTILINE)))
---
+++ packages/repoze.postoffice/repoze.postoffice-0.22.tar.gz/repoze.postoffice-0.22/repoze/postoffice/tests/test_filters.py
@@ -0,0 +1,375 @@
+from __future__ import with_statement
+
+import unittest
+
+class TestToHostnameFilter(unittest.TestCase):
+    def _make_one(self, expr):
+        from repoze.postoffice.filters import ToHostnameFilter
+        return ToHostnameFilter(expr)
+
+    def test_absolute(self):
+        fut = self._make_one('example.com')
+        msg = {}
+        self.assertEqual(fut(msg), None)
+        msg['To'] = 'chris@foo.com'
+        self.assertEqual(fut(msg), None)
+        msg['To'] = 'chris@foo.example.com'
+        self.assertEqual(fut(msg), None)
+        msg['To'] = 'chris@example.com'
+        self.assertEqual(fut(msg),
+                         'to_hostname: chris@example.com matches example.com')
+        msg['To'] = 'Chris <chris@example.com>'
+        self.assertEqual(fut(msg),
+                         'to_hostname: chris@example.com matches example.com')
+        msg['To'] = 'Chris <chris@example.com'
+        self.assertEqual(fut(msg),
+                         'to_hostname: chris@example.com matches example.com')
+
+    def test_relative(self):
+        fut = self._make_one('.example.com')
+        msg = {}
+        self.assertEqual(fut(msg), None)
+        msg['To'] = 'chris@foo.com'
+        self.assertEqual(fut(msg), None)
+        msg['To'] = 'chris@foo.example.com'
+        self.assertEqual(fut(msg),
+                    'to_hostname: chris@foo.example.com matches .example.com')
+        msg['To'] = 'chris@example.com'
+        self.assertEqual(fut(msg),
+                    'to_hostname: chris@example.com matches .example.com')
+        msg['To'] = 'Chris <chris@example.com>'
+        self.assertEqual(fut(msg),
+                         'to_hostname: chris@example.com matches .example.com')
+
+
+    def test_case_insensitive(self):
+        fut = self._make_one('example.com')
+        msg = {}
+        self.assertEqual(fut(msg), None)
+        msg['To'] = 'chris@Example.com'
+        self.assertEqual(fut(msg),
+                         'to_hostname: chris@Example.com matches example.com')
+
+
+    def test_not_an_address(self):
+        fut = self._make_one('example.com')
+        msg = {'To': 'undisclosed recipients;;'}
+        self.assertEqual(fut(msg), None)
+
+    def test_malformed_address(self):
+        fut = self._make_one('example.com')
+        msg = {'To': 'karin@example.com <>'}
+        self.assertEqual(fut(msg), None)
+
+    def test_multiple_hosts(self):
+        fut = self._make_one('example1.com .example2.com example3.com')
+        msg = {'To': 'chris@foo.example2.com'}
+        self.assertEqual(fut(msg),
+            'to_hostname: chris@foo.example2.com matches .example2.com')
+        msg = {'To': 'chris@foo.example1.com'}
+        self.assertEqual(fut(msg), None)
+        msg = {'To': 'chris@example1.com'}
+        self.assertEqual(fut(msg),
+            'to_hostname: chris@example1.com matches example1.com')
+
+    def test_multiple_addrs(self):
+        fut = self._make_one('example.com')
+        msg = {'To': 'Fred <fred@exemplar.com>, Barney <barney@example.com>'}
+        self.assertEqual(fut(msg),
+                         'to_hostname: barney@example.com matches example.com')
+
+    def test_match_cc(self):
+        fut = self._make_one('example.com')
+        msg = {'Cc': 'Fred <fred@exemplar.com>, Barney <barney@example.com>'}
+        self.assertEqual(fut(msg),
+                         'to_hostname: barney@example.com matches example.com')
+
+    def test_match_to_or_cc(self):
+        fut = self._make_one('example.com')
+        msg = {'To': 'Fred <fred@examplar.com>',
+               'Cc': 'Barney <barney@example.com>'}
+        self.assertEqual(fut(msg),
+                         'to_hostname: barney@example.com matches example.com')
+        msg = {'To': 'Barney <barney@example.com>',
+               'Cc': 'Fred <fred@examplar.com>'}
+        self.assertEqual(fut(msg),
+                         'to_hostname: barney@example.com matches example.com')
+
+
+class TestHeaderRegexpFilter(unittest.TestCase):
+
+    def _make_one(self, *exprs):
+        from repoze.postoffice.filters import HeaderRegexpFilter as cut
+        return cut(*exprs)
+
+    def test_matches(self):
+        regexp = 'Subject:.+Party Time'
+        fut = self._make_one(regexp)
+        msg = {'Subject': "It's that time!  Party Time!"}
+        self.assertEqual(fut(msg),
+                         'header_regexp: headers match %s' % repr(regexp))
+
+    def test_does_not_match(self):
+        fut = self._make_one('Subject:.+Party Time')
+        msg = {'Subject': "It's time for a party!"}
+        self.assertEqual(fut(msg), None)
+
+    def test_matches_non_ascii(self):
+        regexp = u'Subject: .*R\xe9ponse automatique\xa0'
+        fut = self._make_one(regexp)
+        msg = {'Subject': '=?iso-8859-1?Q?R=E9ponse_automatique=A0:_'
+               '[Communications_&_PR]_Civil_soci?='}
+        self.assertEqual(fut(msg),
+                         'header_regexp: headers match %s' % repr(regexp))
+
+
+class TestHeaderRegexpFileFilter(unittest.TestCase):
+
+    def setUp(self):
+        import os
+        import tempfile
+        self.tmp = tempfile.mkdtemp('.repoze.postoffice.tests')
+        self.path = os.path.join(self.tmp, 'rules')
+        with open(self.path, 'w') as out:
+            print >> out, "Subject:.+Party Time"
+            print >> out, "Subject:.+pecial "
+            print >> out, "From:.+ROSSI"
+            print >> out, 'Subject: .*R\xc3\xa9ponse automatique\xc2\xa0'
+
+    def tearDown(self):
+        import shutil
+        shutil.rmtree(self.tmp)
+
+    def _make_one(self):
+        from repoze.postoffice.filters import HeaderRegexpFileFilter as cut
+        return cut(self.path)
+
+    def test_matches(self):
+        fut = self._make_one()
+        msg = {'Subject': "It's that time!  Party Time!"}
+        self.assertEqual(fut(msg),
+                         "header_regexp: headers match u'Subject:.+Party Time'")
+        msg = {'From': 'chris.ROSSI@jackalopelane.net'}
+        self.assertEqual(fut(msg),
+                         "header_regexp: headers match u'From:.+ROSSI'")
+
+    def test_respect_trailing_whitespace(self):
+        fut = self._make_one()
+        msg = {'Subject': "Call a specialist."}
+        self.assertEqual(fut(msg), None)
+        msg = {'Subject': 'Hmm, hmm, yummy pecial for you.'}
+        self.assertEqual(fut(msg),
+                         "header_regexp: headers match u'Subject:.+pecial '")
+
+    def test_does_not_match(self):
+        fut = self._make_one()
+        msg = {'Subject': "It's time for a party!"}
+        self.assertEqual(fut(msg), None)
+
+    def test_matches_non_ascii(self):
+        fut = self._make_one()
+        msg = {'Subject': '=?iso-8859-1?Q?R=E9ponse_automatique=A0:_'
+               '[Communications_&_PR]_Civil_soci?='}
+        self.assertEqual(fut(msg),
+                         "header_regexp: headers match "
+                         "u'Subject: .*R\\xe9ponse automatique\\xa0'")
+
+
+class TestBodyRegexpFilter(unittest.TestCase):
+
+    def _make_one(self, *exprs):
+        from repoze.postoffice.filters import BodyRegexpFilter as cut
+        return cut(*exprs)
+
+    def test_matches(self):
+        from email.message import Message
+        msg = Message()
+        msg.set_payload("I am full of happy babies.  All days for Me!")
+        fut = self._make_one('happy.+days')
+        self.assertEqual(fut(msg),
+                         "body_regexp: body matches 'happy.+days'")
+
+    def test_does_not_match(self):
+        from email.message import Message
+        msg = Message()
+        msg.set_payload("All Days for Me!  I am full of happy babies.")
+        fut = self._make_one('happy.+days')
+        self.assertEqual(fut(msg), None)
+
+    def test_multiline_matches(self):
+        from email.message import Message
+        msg = Message()
+        # we've seen messages with From/Subject looking headers in the body
+        msg.set_payload("From: foobar@example.com\nSubject: Auto-Response")
+        fut = self._make_one('^Subject: Auto-Response')
+        self.assertEqual(fut(msg),
+                         "body_regexp: body matches '^Subject: Auto-Response'")
+
+
+class TestBodyRegexpFileFilter(unittest.TestCase):
+
+    def setUp(self):
+        import os
+        import tempfile
+        self.tmp = tempfile.mkdtemp('.repoze.postoffice.tests')
+        self.path = os.path.join(self.tmp, 'rules')
+        with open(self.path, 'w') as out:
+            print >> out, "happy.+days"
+            print >> out, "amnesia"
+            print >> out, " (kitties|corndogs|puppies) "
+
+    def tearDown(self):
+        import shutil
+        shutil.rmtree(self.tmp)
+
+    def _make_one(self):
+        from repoze.postoffice.filters import BodyRegexpFileFilter as cut
+        return cut(self.path)
+
+    def test_matches(self):
+        from email.message import Message
+        msg = Message()
+        msg.set_payload("I am full of happy babies.  All days for Me!")
+        fut = self._make_one()
+        self.assertEqual(fut(msg),
+                         "body_regexp: body matches u'happy.+days'")
+
+    def test_matches_multipart(self):
+        from email.mime.multipart import MIMEMultipart
+        from email.mime.multipart import MIMEBase
+        from email.mime.text import MIMEText
+        msg = MIMEMultipart()
+        body = MIMEText('I am full of happy babies.  All days for Me!')
+        msg.attach(body)
+        other = MIMEBase('application', 'pdf')
+        other.set_payload('Not really a pdf.')
+        msg.attach(other)
+        fut = self._make_one()
+        self.assertEqual(fut(msg),
+                         "body_regexp: body matches u'happy.+days'")
+
+        msg = MIMEMultipart()
+        body = MIMEText("I can't remember if my amnesia is getting worse.")
+        msg.attach(body)
+        other = MIMEBase('application', 'pdf')
+        other.set_payload('Not really a pdf.')
+        msg.attach(other)
+        self.assertEqual(fut(msg),
+                         "body_regexp: body matches u'amnesia'")
+
+    def test_does_not_match(self):
+        from email.message import Message
+        msg = Message()
+        msg.set_payload("All Days for Me!  I am full of happy babies.")
+        fut = self._make_one()
+        self.assertEqual(fut(msg), None)
+
+    def test_does_not_multipart(self):
+        from email.mime.multipart import MIMEMultipart
+        from email.mime.multipart import MIMEBase
+        msg = MIMEMultipart()
+        body = MIMEBase('x-application', 'not-text')
+        body.set_payload('I am full of happy babies.  All Days for Me!')
+        msg.attach(body)
+        other = MIMEBase('application', 'pdf')
+        other.set_payload('Not really a pdf.')
+        msg.attach(other)
+        fut = self._make_one()
+        self.assertEqual(fut(msg), None)
+
+    def test_matches_multipart_w_charset_in_content_type(self):
+        """
+        Simulates mime messages created by stdlib email parser where  a part
+        can have a charset set in the Content-Type header but get_charset()
+        returns None.
+        """
+        from email.mime.multipart import MIMEMultipart
+        from email.mime.multipart import MIMEBase
+        from email.mime.text import MIMEText
+        msg = MIMEMultipart()
+        body = MIMEText('I am full of happy babies.  All days for Me!')
+        body.set_charset(None)
+        del body['Content-Type']
+        body['Content-Type'] = 'text/plain; charset=ISO-8859-1; flow=groovy'
+        msg.attach(body)
+        other = MIMEBase('application', 'pdf')
+        other.set_payload('Not really a pdf.')
+        msg.attach(other)
+        fut = self._make_one()
+        self.assertEqual(fut(msg),
+                         "body_regexp: body matches u'happy.+days'")
+
+    def test_matches_multipart_w_bogus_charset_in_content_type(self):
+        """
+        Simulates mime messages created by stdlib email parser where  a part
+        can have a charset set in the Content-Type header but get_charset()
+        returns None.
+        """
+        from email.mime.multipart import MIMEMultipart
+        from email.mime.multipart import MIMEBase
+        from email.mime.text import MIMEText
+        msg = MIMEMultipart()
+        body = MIMEText('I am full of happy babies.  All days for Me!')
+        body.set_charset(None)
+        del body['Content-Type']
+        body['Content-Type'] = 'text/plain; charset=bogus; flow=groovy'
+        msg.attach(body)
+        other = MIMEBase('application', 'pdf')
+        other.set_payload('Not really a pdf.')
+        msg.attach(other)
+        fut = self._make_one()
+        self.assertEqual(fut(msg),
+                         "body_regexp: body matches u'happy.+days'")
+
+    def test_matches_multipart_w_comment_in_charset(self):
+        """
+        At least one email client out there generates content type headers that
+        look like::
+
+            Content-Type: text/html; charset="utf-8" //iso-8859-2
+        """
+        from email.mime.multipart import MIMEMultipart
+        from email.mime.multipart import MIMEBase
+        from email.mime.text import MIMEText
+        msg = MIMEMultipart()
+        body = MIMEText('I am full of happy babies.  All days for Me!')
+        body.set_charset(None)
+        del body['Content-Type']
+        body['Content-Type'] = 'text/plain; charset="utf-8" //iso-8859-2'
+        msg.attach(body)
+        other = MIMEBase('application', 'pdf')
+        other.set_payload('Not really a pdf.')
+        msg.attach(other)
+        fut = self._make_one()
+        self.assertEqual(fut(msg),
+                         "body_regexp: body matches u'happy.+days'")
+
+    def test_does_not_match_multipart_w_no_charset_not_utf8(self):
+        """
+        Simulates mime messages created by stdlib email parser where  a part
+        can have a charset set in the Content-Type header but get_charset()
+        returns None.
+        """
+        from email.mime.multipart import MIMEMultipart
+        from email.mime.multipart import MIMEBase
+        from email.mime.text import MIMEText
+        body_text = u'Non \xe8 giusto costringermi ad usare il modulo email.'
+        msg = MIMEMultipart()
+        body = MIMEText(body_text.encode('ISO-8859-1'))
+        body.set_charset(None)
+        msg.attach(body)
+        other = MIMEBase('application', 'pdf')
+        other.set_payload('Not really a pdf.')
+        msg.attach(other)
+        fut = self._make_one()
+        self.assertEqual(fut(msg), None)
+
+    def test_respects_leading_and_trailing_whitespace(self):
+        from email.message import Message
+        msg = Message()
+        msg.set_payload("You are such scorndogs.")
+        fut = self._make_one()
+        self.assertEqual(fut(msg), None)
+        msg.set_payload("Have puppies for lunch!")
+        self.assertEqual(fut(msg),
+                "body_regexp: body matches u' (kitties|corndogs|puppies) '")
