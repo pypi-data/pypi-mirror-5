d5835297c03e6eaef3e4277cd6122d492c0ffe1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:14 +0000

    Add package fpGrowth/1.0.0/fpGrowth-1.0.0.tar.gz

---
+++ packages/fpGrowth/fpGrowth-1.0.0.tar.gz/fpGrowth-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name = 'fpGrowth',
+	version = '1.0.0',
+	py_modules = ['fpGrowth'],
+	author = 'chenliyu',
+	author_email = '2572065090@qq.com',
+	url = 'http://pypi.python.org/ ',
+	description = 'implimention of fpGrowth in python',
+)
+
