37cb7d75efff30c56175a6bc071ae192454461bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:35 +0000

    Add package ThreeScalePY/1.5.1-beta1/ThreeScalePY-1.5.1-beta1.tar.gz

---
+++ packages/ThreeScalePY/ThreeScalePY-1.5.1-beta1.tar.gz/ThreeScalePY-1.5.1-beta1/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+    name='ThreeScalePY',
+    version='1.5.1-beta1',
+    author='3scale',
+    author_email='support@3scale-net',
+    url="https://github.com/3scale/3scale_ws_api_for_python",
+    py_modules=['ThreeScalePY'],
+    dependency_links=[
+        "ftp://xmlsoft.org/libxml2/python/libxml2-python-2.6.21.tar.gz#egg=libxml2-python-2.6.21"
+    ]
+)
+
