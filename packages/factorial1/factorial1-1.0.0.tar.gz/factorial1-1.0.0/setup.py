9a73f0dd2f3d48a9dc3eb4564f905556c4db968f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:24 +0000

    Add package factorial1/1.0.0/factorial1-1.0.0.tar.gz

---
+++ packages/factorial1/factorial1-1.0.0.tar.gz/factorial1-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+		name			= 'factorial1',
+		version			= '1.0.0',
+		py_modules		= ['factorial'],
+		author			= 'fighterczy',
+		author_email	= 'czy1991@gmail.com',
+		url				= 'http://www.fighterczy.com',
+		description		= 'A simple factorial function',
+	)
