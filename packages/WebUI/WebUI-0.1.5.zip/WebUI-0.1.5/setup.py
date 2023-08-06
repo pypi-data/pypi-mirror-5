6f5377ea474bf80b5cb8f510de6acb8f2797f78d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:25 +0000

    Add package WebUI/0.1.5/WebUI-0.1.5.zip

---
+++ packages/WebUI/WebUI-0.1.5.zip/WebUI-0.1.5/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+	name='WebUI',
+	version='0.1.5',
+	author='Nick Johnstone',
+	author_email='ncwjohnstone@gmail.com',
+	packages=['webui'],
+	scripts=['examples/test_app.py'],
+	url='https://github.com/Widdershin/WebUI/',
+	license='MIT',
+	description='WebUI lets you create first class desktop applications in Python with HTML/CSS',
+	long_description=open('README.md').read(),
+	install_requires=required,
+	)
