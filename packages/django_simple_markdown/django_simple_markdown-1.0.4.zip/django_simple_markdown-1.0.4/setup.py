1df3867714bb96f52e7f6fa0e2177744ed98009b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:27 +0000

    Add package django_simple_markdown/1.0.4/django_simple_markdown-1.0.4.zip

---
+++ packages/django_simple_markdown/django_simple_markdown-1.0.4.zip/django_simple_markdown-1.0.4/setup.py
@@ -0,0 +1,26 @@
+# ~ le encoding: utf-8 ~
+
+# try:
+# 	from setuptools import setup
+# except ImportError:
+# 	from distutils.core import setup
+
+from distutils.core import setup
+
+setup(
+	name = 'django_simple_markdown',
+	version = '1.0.4',
+	description = 'Django package for markdown editing within forms. Uses simple-markdown.js',
+	package_dir = {
+		'django_simple_markdown' : 'djsmd'
+	},
+	packages = ['django_simple_markdown', 'django_simple_markdown.templatetags'],
+	package_data = {
+		'django_simple_markdown' : ['templates/*', 'static/djsmd/js/*']
+	},
+	requires = ['Markdown'],
+	install_requires = ['Markdown'],
+	author = 'Daniel Oliveira',
+	author_email = 'daniel@dvalbrand.com',
+	url = 'https://github.com/Valbrand/django-simple-markdown',
+)
