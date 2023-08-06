2d5b58aeeee0ad9e6a0a2157372079ca56f56708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:19 +0000

    Add package dropbox2/1.7/dropbox2-1.7.tar.gz

---
+++ packages/dropbox2/dropbox2-1.7.tar.gz/dropbox2-1.7/dropbox/six.py
@@ -0,0 +1,13 @@
+import sys
+
+
+def b(str_):
+    if sys.version_info >= (3,):
+        str_ = str_.encode('latin1')
+    return str_
+
+
+def u(str_):
+    if sys.version_info < (3,):
+        str_ = str_.decode('latin1')
+    return str_
