8db5b9b2a2d4cf40cd364cd6c3e15254059eeb13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:14 +0000

    Add package jhashcode/0.02/jhashcode-0.02.tar.gz

---
+++ packages/jhashcode/jhashcode-0.02.tar.gz/jhashcode-0.02/setup.py
@@ -0,0 +1,24 @@
+import os.path
+from setuptools import setup, find_packages
+
+
+def descr():
+    return 'Visit https://github.com/neuront/pyjhashcode for details please.'
+
+setup(
+    name='jhashcode',
+    version='0.02',
+    author='Neuron Teckid',
+    author_email='lene13@gmail.com',
+    license='MIT',
+    keywords='Java hashCode',
+    url='https://github.com/neuront/pyjhashcode',
+    description='Python implemented Java `hashCode` methods.',
+    packages=['jhashcode'],
+    long_description=descr(),
+    install_requires=[],
+    zip_safe=False,
+    entry_points=dict(
+        console_scripts=[],
+    ),
+)
