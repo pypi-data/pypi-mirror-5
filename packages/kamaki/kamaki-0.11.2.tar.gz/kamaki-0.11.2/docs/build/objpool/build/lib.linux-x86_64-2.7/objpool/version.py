53e2219e2ac981690bdd6c9bbee7b6cbef612478
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:56 +0000

    Add package kamaki/0.11.2/kamaki-0.11.2.tar.gz

---
+++ packages/kamaki/kamaki-0.11.2.tar.gz/kamaki-0.11.2/docs/build/objpool/build/lib.linux-x86_64-2.7/objpool/version.py
@@ -0,0 +1,7 @@
+__version__ = "0.2"
+__version_info__ = ['0', '2']
+__version_vcs_info__ = {'branch': 'master',
+ 'revid': 'dca8c0f',
+ 'revno': 35,
+ 'toplevel': '/home/cstavr/okeanos/objpool'}
+__version_user_info__ = "cstavr@okeanosdev-91.admin.grnet.gr"
---
+++ packages/kamaki/kamaki-0.11.2.tar.gz/kamaki-0.11.2/kamaki/version.py
@@ -0,0 +1,8 @@
+
+__version__ = "0.11.2"
+__version_vcs_info__ = {
+    'branch': 'hotfix-0.11.2',
+    'revid': 'b67dde5',
+    'revno': 1918}
+__version_user_email__ = "saxtouri@admin.grnet.gr"
+__version_user_name__ = "Stavros Sachtouris"
