4655e5f06ec777868517d741c34d8c7b34c1d53e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:03 +0000

    Add package threetaps/0.1.1/threetaps-0.1.1.tar.gz

---
+++ packages/threetaps/threetaps-0.1.1.tar.gz/threetaps-0.1.1/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+import threetaps
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+with open('README.rst') as f:
+    readme = f.read()
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='threetaps',
+    version=threetaps.__version__,
+    description='3taps Python API Client.',
+    long_description=readme,
+    author='Michael Kolodny',
+    author_email='michaelckolodny@gmail.com',
+    url='https://github.com/mkolodny/3taps',
+    packages=['threetaps'],
+    install_requires=['requests'],
+    license=license,
+)
