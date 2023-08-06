587830404af009ffc179dc20b65c4afcdc829020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:52 +0000

    Add package phasepack/1.3/phasepack-1.3.tar.gz

---
+++ packages/phasepack/phasepack-1.3.tar.gz/phasepack-1.3/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(	name = 'phasepack',
+	version = '1.3',
+	description = 'A toolkit for phase-based feature detection',
+	author = 'Alistair Muldal',
+	author_email = 'alistair.muldal@pharm.ox.ac.uk',
+	packages = ['phasepack'])
+
