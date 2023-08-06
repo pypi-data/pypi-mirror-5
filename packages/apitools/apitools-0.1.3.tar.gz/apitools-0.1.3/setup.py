fd2d08be25c5bf21e7a3bc3626e97994d70b795e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:32 +0000

    Add package apitools/0.1.3/apitools-0.1.3.tar.gz

---
+++ packages/apitools/apitools-0.1.3.tar.gz/apitools-0.1.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='apitools',
+    version='0.1.3',
+    author='Nicolas Esteves',
+    author_email='hamstahguru@gmail.com',
+    packages=['apitools'],
+    url='https://github.com/hamstah/apitools',
+    description='Tools to play with json-schema and rest apis',
+    long_description=open("README.md").read(),
+)
