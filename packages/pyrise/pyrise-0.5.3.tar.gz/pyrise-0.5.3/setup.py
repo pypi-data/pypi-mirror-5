85961b5ad27fb73619ff9f1a1e8251c47e9d34cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:52 +0000

    Add package pyrise/0.5.3/pyrise-0.5.3.tar.gz

---
+++ packages/pyrise/pyrise-0.5.3.tar.gz/pyrise-0.5.3/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(name="pyrise",
+      version='0.5.3',
+      description="Python wrapper for 37Signals Highrise",
+      long_description="A work in progress, but one that will be awesome when finished. Pyrise gives you class objects that work a lot like Django models, making the whole experience of integrating with Highrise just a little more awesome and Pythonic.",
+      license="MIT License",
+      author="Jason Ford",
+      author_email="jason@feedmagnet.com",
+      url="http://github.com/feedmagnet/pyrise",
+      packages = find_packages(),
+      install_requires = ['httplib2', 'requests'],
+      keywords= "python 37signals highrise api wrapper feedmagnet",
+      )
