2fe37e1b905d60b55fd4dc43824b27c9fe028300
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:49 +0000

    Add package pydouban/0.0.8/pydouban-0.0.8.tar.gz

---
+++ packages/pydouban/pydouban-0.0.8.tar.gz/pydouban-0.0.8/pydouban/t.py
@@ -0,0 +1,10 @@
+from client import DoubanClient
+
+c1 = DoubanClient('tianyu0915@126.com','ty283406767')
+print c1.login()
+
+
+#print 'x',client.reply_topic('http://www.douban.com/group/topic/39324575/','101001001011101010010')
+
+#for m in client.get_doumails(unread=True):
+#    print m
