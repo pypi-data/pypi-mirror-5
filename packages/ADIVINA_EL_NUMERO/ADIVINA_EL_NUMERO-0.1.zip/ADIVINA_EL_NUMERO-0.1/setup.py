a11175a90c2ad957e219e9d0947412bdbbcce432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:46 +0000

    Add package ADIVINA_EL_NUMERO/0.1/ADIVINA_EL_NUMERO-0.1.zip

---
+++ packages/ADIVINA_EL_NUMERO/ADIVINA_EL_NUMERO-0.1.zip/ADIVINA_EL_NUMERO-0.1/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+
+setup(name="ADIVINA_EL_NUMERO", version="0.1", description="Ejemplo del funcionamiento de distutils", author="JULIAN RODRIGUEZ", author_email="julianrodri11@gmail", url="http://mundogeek.net/tutorial-python/", license="GPL",scripts=["adivina.py"])
