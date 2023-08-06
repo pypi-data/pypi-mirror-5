70637eabcf77d99233ebe627eaf2b69dcf3bf02f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:32 +0000

    Add package flask-memcache-session/2.0/flask-memcache-session-2.0.tar.gz

---
+++ packages/flask-memcache-session/flask-memcache-session-2.0.tar.gz/flask-memcache-session-2.0/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+from setuptools import setup
+
+setup(name="flask-memcache-session",
+      version = '2.0',
+      description = "Use memcache for store session data",
+      author = "Constantine Slednev",
+      author_email = "c.slednev@gmail.com",
+      url = "https://github.com/unk2k/Flask-memcache-session",
+      license = "BSD License",
+      packages = ['flask_memcache_session']
+      )
