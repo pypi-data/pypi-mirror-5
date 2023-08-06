4782f5cb6078b7bf0d42ae053e91acb15b923b35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:37 +0000

    Add package lockdown/0.1.5/lockdown-0.1.5.tar.gz

---
+++ packages/lockdown/lockdown-0.1.5.tar.gz/lockdown-0.1.5/setup.py
@@ -0,0 +1,43 @@
+import os
+from setuptools import setup
+
+# lockdown
+# Module to encrypt local Python code using AES-256.
+# Decompresses code into the Lockdown object.
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "lockdown",
+    version = "0.1.5",
+    author = "Johan Nestaas",
+    author_email = "johannestaas@gmail.com",
+    description = ("An un-audited security tool to encrypt Python code."),
+    license = "BSD",
+    keywords = "crypto cryptography security source code",
+    url = "https://bitbucket.org/johannestaas/lockdown",
+    packages=['lockdown'],
+    long_description=read('README'),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Developers',
+        'Environment :: Console',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+        'Topic :: System :: Software Distribution',
+    ],
+    install_requires=[
+        'pycrypto',
+    ],
+    entry_points = {
+        'console_scripts': [
+            'lockdown = lockdown.bin:lockdown',
+        ],
+    },
+)
