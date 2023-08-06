4d1e78d906d1810c3d4247ffea5076d6306cbf1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:29 +0000

    Add package python-veracity/0.0.4/python-veracity-0.0.4.tar.gz

---
+++ packages/python-veracity/python-veracity-0.0.4.tar.gz/python-veracity-0.0.4/veracity/test/__init__.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+"""
+Unit tests for the veracity package.
+"""
+
+import os
+from random import randrange
+import logging
+from pprint import pformat
+
+from veracity import vv
+from veracity import objects
+from veracity import common
+from veracity import settings
+
+TEST = os.path.dirname(os.path.abspath(__file__))
+FILES = os.path.join(TEST, 'files')
+
+TEST_REPO_NAME = 'python-veracity-test'  # repository for integration tests
+TEMP_REPO_NAME = TEST_REPO_NAME + '-temp' + str(randrange(999))
+REASON = "no test repository available"
+if settings.CREATE_TEST_REPO and TEST_REPO_NAME not in vv.repos():
+    common.create_test_repo(TEST_REPO_NAME)
+if TEST_REPO_NAME in vv.repos():
+    TEST_REPO = objects.Repository(TEST_REPO_NAME)
+else:
+    TEST_REPO = None
+
+
+def log(*objs):
+    """Logs object representations during testing."""
+    for obj in objs:
+        logging.info("log: {0}".format(pformat(obj)))
