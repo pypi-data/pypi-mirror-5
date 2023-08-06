82a59e220ac6185a86c38bed9fb283cce7077311
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:39 +0000

    Add package doge/0.3/doge-0.3.tar.gz

---
+++ packages/doge/doge-0.3.tar.gz/doge-0.3/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name='doge',
+    version='0.3',
+    url='https://github.com/thiderman/doge',
+    author='Lowe Thiderman',
+    author_email='lowe.thiderman@gmail.com',
+    description=('wow very doge'),
+    license='MIT',
+    packages=['doge'],
+    package_data={'doge': ['*/*.txt']},
+    scripts=[
+        'bin/doge'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Operating System :: Unix',
+    ],
+)
