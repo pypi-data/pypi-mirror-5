6c469c424b4f702397ed2d75587bf62c892d6047
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:56 +0000

    Add package loggerclient/1.0.1/loggerclient-1.0.1.tar.gz

---
+++ packages/loggerclient/loggerclient-1.0.1.tar.gz/loggerclient-1.0.1/loggerclient/Logger.py
@@ -0,0 +1,66 @@
+import urllib
+import urllib2
+import json
+
+class Logger:
+  version = '1.0.1'
+  
+  api_key = None
+  
+  base_uri = 'https://getlogger.com/logger/%s/'
+  
+  @staticmethod
+  def init(api_key):
+    Logger.api_key = api_key
+  
+  @staticmethod
+  def getAPIURI():
+    if not Logger.api_key:
+      raise Exception("No API key set")
+    
+    return Logger.base_uri % (Logger.api_key)
+  
+  @staticmethod
+  def sendMessage(options):
+    try:
+      data = urllib.urlencode(options)
+      request = urllib2.Request(Logger.getAPIURI(), data)
+      response = urllib2.urlopen(request).read()
+      jResponse = json.loads(response)
+      
+      if jResponse['success']:
+        return True
+      else:
+        return False
+    except:
+      pass
+      # return False
+  
+  @staticmethod
+  def error(message):
+    return Logger.sendMessage({
+      'type'    : 'error',
+      'message' : message,
+      'version' : Logger.version
+    })
+  
+  @staticmethod
+  def warn(message):
+    return Logger.sendMessage({
+      'type'    : 'warn',
+      'message' : message,
+      'version' : Logger.version
+    })
+  
+  @staticmethod
+  def info(message):
+    return Logger.sendMessage({
+      'type'    : 'info',
+      'message' : message,
+      'version' : Logger.version
+    })
+  
+  @staticmethod
+  def log(message):
+    Logger.info(message)
+
---
+++ packages/loggerclient/loggerclient-1.0.1.tar.gz/loggerclient-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+  name='loggerclient',
+  version='1.0.1',
+  description='Client for Logger - an easy way to log errors',
+  author='Tim Davies',
+  author_email='support@getlogger.com',
+  url='http://getlogger.com/',
+  packages=['loggerclient',],
+  license='BSD-2-Clause',
+)
