38a4bf19533687f9418e3ca7982dd97844a2c0a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:52 +0000

    Add package auth-tkt/0.1.0/auth_tkt-0.1.0.tar.gz

---
+++ packages/auth-tkt/auth_tkt-0.1.0.tar.gz/auth_tkt-0.1.0/auth_tkt/helpers.py
@@ -0,0 +1,23 @@
+from auth_tkt.encrypted import EncryptedAuthTkt
+from auth_tkt.ticket import validate
+
+
+def get_ticket_data(ticket, authtkt_secret, crypted_cookie_secret=None,
+                    timeout=7200):
+    """We store user information in our session hashes. You can retreive that
+    data with this function."""
+    ticket = validate(ticket, authtkt_secret, timeout=timeout)
+
+    if not ticket:
+        return None
+
+    data = {
+        'id': ticket.uid,
+        'tokens': ticket.tokens,
+    }
+
+    if crypted_cookie_secret:
+        ticket = EncryptedAuthTkt(ticket, crypted_cookie_secret)
+        data.update(ticket.data)
+
+    return data
