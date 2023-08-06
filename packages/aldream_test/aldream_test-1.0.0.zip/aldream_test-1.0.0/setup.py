2e676c6745d60159eed666c7cdc22f6c83f06d9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:01 +0000

    Add package aldream_test/1.0.0/aldream_test-1.0.0.zip

---
+++ packages/aldream_test/aldream_test-1.0.0.zip/aldream_test-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'aldream_test',
+    version = '1.0.0',
+    py_modules = ['test'],
+    author = "vigen",
+    author_email = "aldeam.lau@gmail.com",
+    url = "www.google.com",
+    description = "i don't care",
+    )
+
