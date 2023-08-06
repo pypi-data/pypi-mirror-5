b12613d6bc26e9a11ff0eed7d97b89dcf4fb00b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:30 +0000

    Add package pyrsistent/0.1.0/pyrsistent-0.1.0.tar.gz

---
+++ packages/pyrsistent/pyrsistent-0.1.0.tar.gz/pyrsistent-0.1.0/tests/pyrsistent_performance_test.py
@@ -0,0 +1,178 @@
+from pyrsistent import pvector, pset
+
+#import pytest
+import time
+
+def test_big_iterator_initialization():
+    """
+    The results are comparable to those of doing it with a list since most of the
+    code is shared.
+    """
+    before = time.time()
+    iterator = (x for x in range(1000000))
+    print "Big iterator: " + str(time.time() - before)
+
+    before = time.time()
+    seq = pvector(iterator)
+    print "Big vector from iterator: " + str(time.time() - before)
+
+
+def test_big_list_initialization():
+    """
+    Some example results, these are some of the fastest I've seen, it tends to vary...
+
+    == CPython ==
+    Big list: 0.174705028534
+    Big vector: 0.353514909744
+
+    == PyPy ==
+    Slicing solution PyPy:
+    Big list: 0.0481958389282
+    Big vector from list: 0.166031122208
+
+    """
+    before = time.time()
+    list = [x for x in range(1000000)]
+    print "Big list: " + str(time.time() - before)
+
+    before = time.time()
+    seq = pvector(list)
+    print "Big vector from list: " + str(time.time() - before)
+
+
+def test_slicing_performance():
+    """
+    Again, the fastest, large variations exist...
+
+    == CPython ==
+    List slicing: 0.00921297073364
+    Pvec slicing: 0.225503921509
+
+    == PyPy ==
+    List slicing: 0.00399494171143
+    Pvec slicing: 0.102802991867
+
+    """
+
+    list = [x for x in range(1000000)]
+
+    before = time.time()
+    sublist = list[533:744444]
+    print "List slicing: " + str(time.time() - before)
+
+    vec = pvector(list)
+    before = time.time()
+    subvec = vec[533:744444]
+    print "Pvec slicing: " + str(time.time() - before)
+
+
+def test_create_many_small_vectors():
+    """
+    == PyPy ==
+    Single element: 0.149819135666
+    Double elements: 0.350324869156
+    Ten elements: 0.338271856308
+
+    === CPython ===
+    Single element: 3.62240195274
+    Double elements: 8.04715490341
+    Ten elements: 5.73595809937
+    """
+    iterations = range(1000000)
+
+    before = time.time()
+    single = [2]
+    for _ in iterations:
+        vec = pvector(single)
+    print "Single element: " + str(time.time() - before)
+
+
+    before = time.time()
+    double = [2, 3]
+    for _ in iterations:
+        vec = pvector(double)
+    print "Double elements: " + str(time.time() - before)
+
+    before = time.time()
+    ten = range(10)
+    for _ in iterations:
+        vec = pvector(ten)
+    print "Ten elements: " + str(time.time() - before)
+
+
+def test_set_performance():
+    """
+    == PyPy ==
+    Big set from list: 0.0152490139008
+    Big pset from list: 1.62447595596
+    Random access set: 0.0192308425903
+    Random access pset: 2.18643188477
+
+    === CPython ===
+    Big set from list: 0.0202131271362
+    Big pset from list: 2.87654399872
+    Random access set: 0.0950989723206
+    Random access pset: 11.2261350155
+    """
+    l = [x for x in range(100000)]
+
+    before = time.time()
+    s1 = set(l)
+    print "Big set from list: " + str(time.time() - before)
+
+    before = time.time()
+    s2 = pset(l, pre_size=2*len(l))
+    print "Big pset from list: " + str(time.time() - before)
+
+    before = time.time()
+    random_access(s1)
+    print "Random access set: " + str(time.time() - before)
+
+    before = time.time()
+    random_access(s2)
+    print "Random access pset: " + str(time.time() - before)
+
+def test_string_from_objects():
+    p = pvector(range(100000))
+
+    before = time.time()
+    s1 = str(p)
+    print "Str 1: " + str(time.time() - before)
+
+    before = time.time()
+    s2 = p.tostr()
+    print "Str 2: " + str(time.time() - before)
+
+def test_to_list():
+    p = pvector(range(100000))
+
+    before = time.time()
+    l1 = p.tolist()
+    print "Tolist: " + str(time.time() - before)
+
+    before = time.time()
+    l2 = list(p)
+    print "Iterator: " + str(time.time() - before)
+
+    before = time.time()
+    t1 = p.totuple()
+    print "Tuple: " + str(time.time() - before)
+
+
+def random_access(s):
+    testdata = [0, 4, 55, 10000, 98763, -2, 30000, 42004, 37289, 100, 2, 999999]
+    result = False
+    for x in range(100000):
+        for y in testdata:
+            result = y in s
+
+    return result
+
+if __name__ == "__main__":
+#    test_big_list_initialization()
+#    test_big_iterator_initialization()
+#    test_slicing_performance()
+#    test_create_many_small_vectors()
+    test_set_performance()
+    test_string_from_objects()
+    test_to_list()
---
+++ packages/pyrsistent/pyrsistent-0.1.0.tar.gz/pyrsistent-0.1.0/tests/pyrsistent_vector_test.py
@@ -0,0 +1,197 @@
+from pyrsistent import pvector, pvec
+
+import pytest
+
+
+def test_empty_initialization():
+    seq = pvector()
+    assert len(seq) == 0
+
+    with pytest.raises(IndexError):
+        x = seq[0]
+
+
+def test_initialization_with_one_element():
+    seq = pvector([3])
+    assert len(seq) == 1
+    assert seq[0] == 3
+
+
+def test_append_works_and_does_not_affect_original_within_tail():
+    seq1 = pvector([3])
+    seq2 = seq1.append(2)
+
+    assert len(seq1) == 1
+    assert seq1[0] == 3
+
+    assert len(seq2) == 2
+    assert seq2[0] == 3
+    assert seq2[1] == 2
+
+
+def test_append_works_and_does_not_affect_original_outside_tail():
+    original = pvector([])
+    seq = original
+
+    for x in range(33):
+        seq = seq.append(x)
+
+    assert len(seq) == 33
+    assert seq[0] == 0
+    assert seq[31] == 31
+    assert seq[32] == 32
+
+    assert len(original) == 0
+
+
+def test_append_when_root_overflows():
+    seq = pvector([])
+
+    for x in range(32 * 33):
+        seq = seq.append(x)
+
+    seq = seq.append(10001)
+
+    for i in range(32 * 33):
+        assert seq[i] == i
+
+    assert seq[32 * 33] == 10001
+
+
+def test_multi_level_sequence():
+    seq = pvector(range(8000))
+    seq2 = seq.append(11)
+
+    assert seq[5] == 5
+    assert seq2[7373] == 7373
+    assert seq2[8000] == 11
+
+
+def test_multi_level_sequence_from_iterator():
+    seq = pvector(iter(range(8000)))
+    seq2 = seq.append(11)
+
+    assert seq[5] == 5
+    assert seq2[7373] == 7373
+    assert seq2[8000] == 11
+
+
+def test_vector_to_list():
+    l = range(2000)
+    seq = pvector(l)
+
+    assert seq.tolist() == l
+
+
+def test_random_insert_within_tail():
+    seq = pvector([1, 2, 3])
+
+    seq2 = seq.assoc(1, 4)
+
+    assert seq2[1] == 4
+    assert seq[1] == 2
+
+
+def test_random_insert_outside_tail():
+    seq = pvector(range(20000))
+
+    seq2 = seq.assoc(19000, 4)
+
+    assert seq2[19000] == 4
+    assert seq[19000] == 19000
+
+
+def test_insert_beyond_end():
+    seq = pvector(range(2))
+    seq2 = seq.assoc(2, 50)    
+    assert seq2[2] == 50
+
+    with pytest.raises(IndexError):
+        seq2.assoc(19, 4)
+
+
+def test_string_representation():
+    pass
+
+
+def test_iteration():
+    y = 0
+    seq = pvector(range(2000))
+    for x in seq:
+        assert x == y
+        y += 1
+
+    assert y == 2000
+
+
+def test_slicing_range():
+    seq = pvector(range(10))
+    seq2 = seq[2:4]
+
+    assert seq2[0] == 2
+    assert seq2[1] == 3
+
+
+def test_slicing_step():
+    seq = pvector(range(10))
+    seq2 = seq[::2]
+
+    assert seq2[0] == 0
+    assert seq2[1] == 2
+    assert len(seq2) == 5
+
+
+def test_to_list():
+    l = list(range(32*32 + 64 + 7))
+    v = pvector(l)
+
+    assert l == v.tolist()
+
+
+def test_addition():
+    v = pvec(1, 2) + pvec(3, 4)
+
+    assert v.tolist() == [1, 2, 3, 4]
+    assert list(v) == v.tolist()
+
+
+def test_slicing_reverse():
+    seq = pvector(range(10))
+    seq2 = seq[::-1]
+
+    assert seq2[0] == 9
+    assert seq2[1] == 8
+    assert len(seq2) == 10
+
+    seq3 = seq[-3: -7: -1]
+    assert seq3[0] == 7
+    assert seq3[3] == 4
+    assert len(seq3) == 4
+
+
+def test_to_string():
+    seq = pvector(range(2000))
+
+    assert str(seq) == str(range(2000))
+
+
+def test_sorted():
+    seq = pvec(5, 2, 3, 1)
+    assert [1, 2, 3, 5] == sorted(seq)
+
+
+def test_extend():
+    seq = pvec(5, 2, 3, 1)
+    seq2 = pvec(5, 6)
+
+    seq3 = seq.extend(seq2)
+    assert seq3.tolist() == [5, 2, 3, 1, 5, 6]
+
+    empty = pvec()
+    seq4 = seq.extend(empty)
+    assert seq4 is seq
+
+
+def test_boolean_conversion():
+    assert not bool(pvec())
+    assert bool(pvec(1))
