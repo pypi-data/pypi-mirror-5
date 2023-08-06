38734d9ee7c9b28c3d98ccaf1fcd94ebe2e3f7dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:12 +0000

    Add package PyHawk/0.1.0/PyHawk-0.1.0.tar.gz

---
+++ packages/PyHawk/PyHawk-0.1.0.tar.gz/PyHawk-0.1.0/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import codecs
+import os
+from setuptools import setup
+
+
+def read(*parts):
+    return codecs.open(os.path.join(os.path.dirname(__file__), *parts)).read()
+
+README = read('README.rst')
+CHANGELOG = read('CHANGES.txt')
+
+setup(
+    name="PyHawk",
+    version="0.1.0",
+    url='https://github.com/mozilla/PyHawk',
+    author='Austin King',
+    author_email='ozten@mozilla.com',
+    description=README,
+    long_description=README + '\n' + CHANGELOG,
+    packages=['hawk', ],
+    include_package_data=True,
+    install_requires=['requests>=1.2.0'],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    test_suite='hawk.tests',
+)
