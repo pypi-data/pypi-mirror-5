0ac595700a4aed3efec3505e51bd5995a9e189b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:52 +0000

    Add package KNN/1.0.0/KNN-1.0.0.tar.gz

---
+++ packages/KNN/KNN-1.0.0.tar.gz/KNN-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name = 'KNN',
+	version = '1.0.0',
+	py_modules = ['KNN'],
+	author = 'chenliyu',
+	author_email = '2572065090@qq.com',
+	url = 'http://pypi.python.org/ ',
+	description = 'implimention of KNN in python',
+)
+
