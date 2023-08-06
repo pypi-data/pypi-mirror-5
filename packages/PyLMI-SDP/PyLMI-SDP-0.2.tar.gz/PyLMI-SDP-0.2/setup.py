403a33a4835bfbd23d98a73cacbc90c2ba1a955a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:59 +0000

    Add package PyLMI-SDP/0.2/PyLMI-SDP-0.2.tar.gz

---
+++ packages/PyLMI-SDP/PyLMI-SDP-0.2.tar.gz/PyLMI-SDP-0.2/setup.py
@@ -0,0 +1,40 @@
+from os.path import exists
+from setuptools import setup
+
+
+def read_file(name):
+    return open(name).read()
+
+
+description = ('Symbolic linear matrix inequalities (LMI) and semi-definite'
+               'programming (SDP) tools for Python')
+
+if exists('README.rst'):
+    long_description = read_file('README.rst')
+elif exists('README.md'):
+    long_description = read_file('README.md')
+else:
+    long_description = description
+
+setup(
+    name='PyLMI-SDP',
+    version='0.2',
+    author='Cristovao D. Sousa',
+    author_email='crisjss@gmail.com',
+    description=description,
+    license='BSD',
+    keywords='LMI SDP',
+    url='http://github.com/cdsousa/PyLMI-SDP',
+    packages=['lmi_sdp'],
+    long_description=long_description,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Manufacturing',
+        'Intended Audience :: Science/Research',
+    ],
+)
