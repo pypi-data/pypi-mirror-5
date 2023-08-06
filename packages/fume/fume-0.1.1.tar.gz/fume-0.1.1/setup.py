f04b8213f2f5dd69820b3d9ee7487c52036df562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:25 +0000

    Add package fume/0.1.1/fume-0.1.1.tar.gz

---
+++ packages/fume/fume-0.1.1.tar.gz/fume-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(name='fume',
+        version='0.1.1',
+        description="Fume: a smoke test scripting language",
+        author="Christian Fernandez",
+        author_email="c@hackbrightacademy.com",
+        packages=["fume"],
+        package_dir={"": "src"},
+        scripts=['scripts/fume'],
+        requires=["docopt (>= 0.6.1)", "requests (>=2.0.0)"]
+        )
