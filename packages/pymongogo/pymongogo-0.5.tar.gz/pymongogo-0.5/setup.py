49ca3c738a36f07797927736771f9d70aad99433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:49 +0000

    Add package pymongogo/0.3/pymongogo-0.5.tar.gz

---
+++ packages/pymongogo/pymongogo-0.5.tar.gz/pymongogo-0.5/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# Created by Matt Bodman on 2012-12-24
+#
+# THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from setuptools import setup, find_packages
+
+setup(name='pymongogo',
+      version='0.5',
+      description='A Pymongo wrapper',
+      author='Matt Bodman',
+      author_email='mattbodman@gmail.com',
+      url='https://bitbucket.org/mattbodman/pymongogo/src',
+      packages=find_packages(exclude=['tests']),
+      include_package_data=True,
+      package_data={},
+      zip_safe=False,
+      test_suite='nose',
+      install_requires=[
+          'pymongo>=2.4'
+      ],
+      scripts=[
+      ]
+)
+
