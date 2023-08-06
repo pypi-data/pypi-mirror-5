d1160c79bf77c490f2445dfceda493dec1f6eb03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:01 +0000

    Add package batou/0.2.12/batou-0.2.12.zip

---
+++ packages/batou/batou-0.2.12.zip/batou-0.2.12/examples/tutorial-component/components/supervisor/component.py
@@ -0,0 +1,27 @@
+from batou.component import Component
+from batou.lib.buildout import Buildout
+from batou.lib.service import Service
+
+
+class Supervisor(Component):
+
+    def configure(self):
+        self.programs = self.require('programs', self.host)
+        self += Buildout('supervisor', python='2.7',
+                         version='2.2', setuptools='0.8')
+        self += Service('bin/supervisord',
+                        pidfile='var/supervisord.pid')
+
+    def verify(self):
+        self.assert_file_is_current(
+            'var/supervisord.pid',
+            ['.batou.buildout.success'] + [d['path'] for d in self.programs])
+
+    def update(self):
+        out, err = self.cmd('bin/supervisorctl pid')
+        try:
+            int(out)
+        except ValueError:
+            self.cmd('bin/supervisord')
+        else:
+            self.cmd('bin/supervisorctl reload')
