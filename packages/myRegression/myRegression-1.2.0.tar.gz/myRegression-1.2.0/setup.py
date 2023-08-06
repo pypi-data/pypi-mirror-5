1dc4f50b939425fa4e5841b5288454c96245a0b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:23 +0000

    Add package myRegression/1.2.0/myRegression-1.2.0.tar.gz

---
+++ packages/myRegression/myRegression-1.2.0.tar.gz/myRegression-1.2.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name = 'myRegression',
+	version = '1.2.0',
+	py_modules = ['myRegression'],
+	author = 'chenliyu',
+	author_email = '2572065090@qq.com',
+	url = 'http://pypi.python.org/ ',
+	description = 'implimention of regression in python,including standard version,lwlr version,ridge version,an implemention of greedy algorithm of regression and least squares weight version,and then use a function to find the best weight of ridgeTest calculated from 30 iteration',
+)
+
