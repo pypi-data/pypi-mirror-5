6dea2229209452858e8342caaccfb647bcc13d8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:10 +0000

    Add package WTForms-Alchemy/0.7.15/WTForms-Alchemy-0.7.15.tar.gz

---
+++ packages/WTForms-Alchemy/WTForms-Alchemy-0.7.15.tar.gz/WTForms-Alchemy-0.7.15/setup.py
@@ -0,0 +1,76 @@
+"""
+WTForms-Alchemy
+---------------
+
+Generates WTForms forms from SQLAlchemy models.
+"""
+
+from setuptools import setup, Command
+import subprocess
+
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        errno = subprocess.call(['py.test'])
+        raise SystemExit(errno)
+
+
+extras_require = {
+    'test': [
+        'pytest>=2.3.5',
+        'flexmock>=0.9.7',
+        'psycopg2>=2.4.6',
+        'WTForms-Test>=0.1.1'
+    ],
+    'phone': ['phonenumbers3k==5.6b1'],
+    'password': ['passlib >= 1.6, < 2.0'],
+    'color': ['colour>=0.0.4'],
+    'i18n': ['SQLAlchemy-i18n >= 0.6.3']
+}
+
+
+# Add all optional dependencies to testing requirements.
+for name, requirements in extras_require.items():
+    if name != 'test':
+        extras_require['test'] += requirements
+
+
+setup(
+    name='WTForms-Alchemy',
+    version='0.7.15',
+    url='https://github.com/kvesteri/wtforms-alchemy',
+    license='BSD',
+    author='Konsta Vesterinen',
+    author_email='konsta@fastmonkeys.com',
+    description='Generates WTForms forms from SQLAlchemy models.',
+    long_description=__doc__,
+    packages=['wtforms_alchemy'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'SQLAlchemy>=0.8.0',
+        'WTForms>=1.0.4',
+        'WTForms-Components>=0.6.6',
+        'SQLAlchemy-Utils>=0.16.7'
+    ],
+    extras_require=extras_require,
+    cmdclass={'test': PyTest},
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
