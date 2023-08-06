c95c96ec81087ff671795b691c803ef765564ed1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:43 +0000

    Add package readability-lxml/0.3.0.2/readability-lxml-0.3.0.2.tar.gz

---
+++ packages/readability-lxml/readability-lxml-0.3.0.2.tar.gz/readability-lxml-0.3.0.2/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import sys
+
+if sys.platform == 'darwin':
+    lxml = "lxml<2.4"
+else:
+    lxml = "lxml"
+
+setup(
+    name="readability-lxml",
+    version="0.3.0.2",
+    author="Yuri Baburov",
+    author_email="burchik@gmail.com",
+    description="fast python port of arc90's readability tool",
+    test_suite = "tests.test_article_only",
+    long_description=open("README").read(),
+    license="Apache License 2.0",
+    url="http://github.com/buriy/python-readability",
+    packages=['readability'],
+    install_requires=[
+        "chardet",
+        lxml
+        ],
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        ],
+)
