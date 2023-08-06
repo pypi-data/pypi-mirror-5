58dc118e1d8281f968d0a4c52451826336690c1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:37 +0000

    Add package snsapi/0.6.5/snsapi-0.6.5.tar.gz

---
+++ packages/snsapi/snsapi-0.6.5.tar.gz/snsapi-0.6.5/snsapi/plugin_trial/__init__.py
@@ -0,0 +1,11 @@
+#-*- encoding: utf-8 -*-
+
+# example:
+# from xx import xx
+# Then this platform will appear in snsapi.platform
+
+from sqlite import SQLite
+from twitter import TwitterStatus
+from emails import Email
+from sina_wap import SinaWeiboWapStatus
+from facebook import FacebookFeed
