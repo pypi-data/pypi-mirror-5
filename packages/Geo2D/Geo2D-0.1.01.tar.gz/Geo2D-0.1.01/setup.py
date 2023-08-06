1bbfaae50f59faa2a4c26edd332d247e6d357f21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:35 +0000

    Add package Geo2D/0.1.01/Geo2D-0.1.01.tar.gz

---
+++ packages/Geo2D/Geo2D-0.1.01.tar.gz/Geo2D-0.1.01/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='Geo2D',
+    version='0.1.01',
+    author='Razvan C. Radulescu',
+    author_email='razvanc87@gmail.com',
+    packages=['geo2d', 'geo2d.test'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/Geo2D/',
+    license='LICENSE.txt',
+    description='Pure Python (3 compatible) geometry package.',
+    long_description=open('README.txt').read(),
+    install_requires=[],
+)
