7d43a808f03c742cc26f8b1501664764bab37da7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:00 +0000

    Add package holmium.core/0.1.8.2/holmium.core-0.1.8.2-py2.7.egg

---
+++ packages/holmium.core/holmium.core-0.1.8.2-py2.7.egg/holmium/core/__init__.py
@@ -0,0 +1,6 @@
+from holmium.core.logger import log
+from holmium.core.testcase import HolmiumTestCase, browser_mapping, capabilities
+from holmium.core.pageobject import Locators, PageObject, PageElement, PageElements
+from holmium.core.pageobject import PageElementMap
+from holmium.core.decorators import repeat
+from holmium.core.noseplugin import HolmiumNose
