2cdecc2826f78725d675864662f7098f9240ebc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:27 +0000

    Add package lambdaJSON/0.1.0/lambdaJSON-0.1.0.zip

---
+++ packages/lambdaJSON/lambdaJSON-0.1.0.zip/lambdaJSON-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='lambdaJSON',
+    version='0.1.0',
+    author='Pooya Eghbali',
+    author_email='persian.writer@gmail.com',
+    packages=['lambdaJSON'],
+    url='https://github.com/pooya-eghbali/lambdaJSON',
+    license='LICENSE.txt',
+    description='serialize python standard library objects with json',
+    long_description=open('README.txt').read()
+)
