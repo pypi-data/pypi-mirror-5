27b1667af0fd8c066025ccfc50a19374e8626231
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:12 +0000

    Add package django-hisstory/0.1.1/django-hisstory-0.1.1.tar.gz

---
+++ packages/django-hisstory/django-hisstory-0.1.1.tar.gz/django-hisstory-0.1.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'django-hisstory',
+    version = '0.1.1',
+    description = 'Lightweight read-only django model history app.',
+    keywords = 'django apps',
+    license = 'New BSD License',
+    author = 'Andrey Mozgunov',
+    author_email = 'andrey.mozgunov@softline.ru',
+    url = 'https://bitbucket.org/medsafe/django-hisstory',
+    install_requires = [
+        'jsonfield',
+        'django_globals==0.2.1',
+    ],
+    dependency_links = [
+        'git+https://github.com/ashvetsov/django-globals.git#egg=django_globals-0.2.1',
+    ],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Plugins',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    packages=[
+        'hisstory',
+    ],
+    include_package_data = True,
+)
+
+
