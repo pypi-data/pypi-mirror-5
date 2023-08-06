e8957088972530e032a86c2cfdd1ace3c1f06cc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:18 +0000

    Add package mongorm/0.6.1/mongorm-0.6.1.tar.gz

---
+++ packages/mongorm/mongorm-0.6.1.tar.gz/mongorm-0.6.1/mongorm/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from mongorm.database import Database
+from mongorm.document import Field, Index
+from mongorm.utils import DotDict
+
+VERSION = (0, 6, 1)
+
+
+class ValidationError(Exception):
+    pass
+
+__all__ = ['VERSION', 'Database', 'Field',
+           'Index', 'DotDict', 'ValidationError']
