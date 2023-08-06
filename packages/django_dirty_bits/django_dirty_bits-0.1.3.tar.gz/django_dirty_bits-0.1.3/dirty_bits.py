086022c22533785b5abf9e1b3dfd685c13d770a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:08 +0000

    Add package django_dirty_bits/0.1.3/django_dirty_bits-0.1.3.tar.gz

---
+++ packages/django_dirty_bits/django_dirty_bits-0.1.3.tar.gz/django_dirty_bits-0.1.3/dirty_bits.py
@@ -0,0 +1,54 @@
+from django.db.models import get_models
+from django.db.models.signals import post_init
+from threading import Lock
+
+REGISTRY_LOCK = Lock()
+
+REGISTRY = set()
+NEW_MODEL_HASH = None
+
+
+def register_all():
+    models = get_models()
+    for model in models:
+        register(model)
+
+
+def register(cls):
+    with REGISTRY_LOCK:
+        if cls in REGISTRY:
+            return
+        REGISTRY.add(cls)
+
+    def _init_hash(sender, instance):
+        if sender in REGISTRY:
+            instance.__dirty_hash = cls._get_hash(instance)
+        else:
+            instance.__dirty_hash = NEW_MODEL_HASH
+
+    def _get_hash(instance):
+        if not instance.pk:
+            return NEW_MODEL_HASH
+        model_key_values = tuple(
+            (
+                (field.name, field.value_to_string(instance)) for field in
+                (instance._meta.fields + instance._meta.many_to_many)
+            )
+        )
+
+        return hash(model_key_values)
+
+    def is_dirty(self):
+        if self.__dirty_hash == NEW_MODEL_HASH:
+            # initial state of a model is dirty
+            return True
+        return cls._get_hash(self) != self.__dirty_hash
+
+    cls._init_hash = _init_hash
+    cls._get_hash = _get_hash
+    cls.is_dirty = is_dirty
+
+    def _post_init(sender, instance, **kwargs):
+        _init_hash(sender, instance)
+
+    post_init.connect(_post_init, sender=cls, weak=False)
