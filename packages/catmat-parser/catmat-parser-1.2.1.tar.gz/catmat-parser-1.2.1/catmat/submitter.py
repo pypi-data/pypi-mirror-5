5e3fe6c94b3d4f0de8f7f793c795e5a0de142b4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:22 +0000

    Add package catmat-parser/1.2.1/catmat-parser-1.2.1.tar.gz

---
+++ packages/catmat-parser/catmat-parser-1.2.1.tar.gz/catmat-parser-1.2.1/catmat/submitter.py
@@ -0,0 +1,37 @@
+# coding: utf-8
+from xml.etree import ElementTree as ET
+from catmat.parser import CatmatParser
+from os.path import join, abspath, dirname
+import requests
+
+ROOT_PATH = join(abspath(dirname(__file__)))
+print ROOT_PATH
+
+class Submitter(object):
+
+    def __init__(self, cpf=1, password=1):
+        url = "http://www.comprasnet.gov.br/XML"
+        self.production_url = join(url, "producao/consultamatserv.asp")
+        self.development_url = join(url, "treinamento/consultamatserv.asp")
+        self.__input_xml_path = join(ROOT_PATH, 'input_template.xml')
+        tree = ET.parse(self.__input_xml_path)
+        self.__cpf = cpf
+        self.__password = password
+        self.__root = tree
+        self.__data = []
+
+
+    def _get_xml_input_string(self, radical1, radical2='', radical3=''):
+        return open(self.__input_xml_path, 'rw').read() %(self.__cpf, self.__password, 
+                                                          radical1, radical2, radical3)
+
+
+    def post_xml(self, radical1, radical2='', radical3=''):
+        input_xml = self._get_xml_input_string(radical1, radical2, radical3)
+        param_data = {'xml': input_xml}
+        
+        output_xml = requests.post(self.development_url, data=param_data)
+        output_xml_string = output_xml.text.encode('utf-8')
+        parser = CatmatParser(output_xml_string)
+
+        return parser.to_hash()
