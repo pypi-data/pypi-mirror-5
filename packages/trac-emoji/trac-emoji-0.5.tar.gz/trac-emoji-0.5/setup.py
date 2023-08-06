b8ab60927806ba99c3aa4672c4c7815d25145134
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:07 +0000

    Add package trac-emoji/0.5/trac-emoji-0.5.tar.gz

---
+++ packages/trac-emoji/trac-emoji-0.5.tar.gz/trac-emoji-0.5/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+version='0.5'
+
+setup(
+    name='trac-emoji',
+    url='https://github.com/pykler/TracEmoji',
+    long_description='Emoji plugin for trac implementing https://github.com/arvida/emoji-cheat-sheet.com',
+    author='Hatem Nassrat',
+    author_email='hatem@nassrat.ca',
+    version=version,
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    entry_points = """
+        [trac.plugins]
+        trac_emoji = trac_emoji
+    """,
+)
