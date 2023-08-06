a9c98fff29ffec51342ae7f4fc6ad737928a2125
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:06 +0000

    Add package dice/0.4.0/dice-0.4.0.tar.gz

---
+++ packages/dice/dice-0.4.0.tar.gz/dice-0.4.0/dice/tests/test_elements.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import
+
+from dice.elements import Integer, Roll, Dice
+
+
+def test_integer():
+    assert isinstance(Integer(1), int)
+
+
+def test_dice_from_iterable():
+    d = Dice.from_iterable((2, 6))
+    assert d.amount == 2 and d.sides == 6
+
+
+def test_dice_from_string():
+    d = Dice.from_string('2d6')
+    assert d.amount == 2 and d.sides == 6
+
+
+def test_roll():
+    amount, sides = 6, 6
+    assert len(Roll.roll(amount, sides)) == amount
+    assert (1 * sides) <= sum(Roll.roll(amount, sides)) <= (amount * sides)
