ff92f46e62804eaaf1800784c0244c8a9dd649b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:43 +0000

    Add package django-redisboard/0.2.7/django-redisboard-0.2.7.tar.gz

---
+++ packages/django-redisboard/django-redisboard-0.2.7.tar.gz/django-redisboard-0.2.7/setup.py
@@ -0,0 +1,33 @@
+# -*- encoding: utf-8 -*-
+from setuptools import setup, find_packages
+
+import os
+
+setup(
+    name = "django-redisboard",
+    version = "0.2.7",
+    url = 'https://github.com/ionelmc/django-redisboard',
+    download_url = '',
+    license = 'BSD',
+    description = "Brief redis monitoring in django admin",
+    long_description = file(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    author = 'Ionel Cristian Mărieș',
+    author_email = 'contact@ionelmc.ro',
+    packages = find_packages('src'),
+    package_dir = {'':'src'},
+    include_package_data = True,
+    install_requires = [
+        'redis',
+        'Django>=1.3',
+    ],
+    zip_safe = False,
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
