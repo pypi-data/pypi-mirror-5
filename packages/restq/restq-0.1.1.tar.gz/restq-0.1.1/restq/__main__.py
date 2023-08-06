f3b686eb1c391c00c69a12ecaf66cf4ca732624f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:19 +0000

    Add package restq/0.1.1/restq-0.1.1.tar.gz

---
+++ packages/restq/restq-0.1.1.tar.gz/restq-0.1.1/restq/__main__.py
@@ -0,0 +1,21 @@
+import bottle
+
+from restq import webapp
+from restq import realms 
+
+try:
+    # bjoern requires libev-dev to be installed for it to build
+    # pip install bjoern 
+    import bjoern
+    server="bjoern"
+except ImportError:
+    server=None
+
+if __name__ == "__main__":
+    bottle.run(app=webapp.app, 
+                server=server,
+                host='localhost', 
+                port=8080, 
+                debug=True)
+
+
