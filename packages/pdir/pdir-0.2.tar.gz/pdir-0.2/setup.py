ade746efbd6b426277f15cc2a80e6fc4c1b24fc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:03 +0000

    Add package pdir/0.2/pdir-0.2.tar.gz

---
+++ packages/pdir/pdir-0.2.tar.gz/pdir-0.2/setup.py
@@ -0,0 +1,30 @@
+#-*- coding:utf-8 -*-
+#
+# Copyright (C) 2013 - Pranjal Mittal <pranjal.mittal.ece10@iitbhu.ac.in>
+#
+# Distributed under the BSD license, see LICENSE.txt
+
+
+from setuptools import setup, find_packages
+import sys, os
+
+setup(
+    name='pdir',
+    version='0.2',
+    description='A Selective, Pretty dir printing utility for Python.',
+    long_description=open('README.txt').read(),
+    packages=['pdir',],
+    license='BSD',
+    keywords='dir namespaces',
+    author='Pranjal Mittal',
+    author_email='pranjal.mittal.ece10@iitbhu.ac.in',
+    classifiers=[
+      "Intended Audience :: Developers",
+      "Development Status :: 3 - Alpha",
+      "Programming Language :: Python :: 2",
+      ],
+    url='https://github.com/pramttl/pdir',
+    include_package_data=True,
+)
+
+
