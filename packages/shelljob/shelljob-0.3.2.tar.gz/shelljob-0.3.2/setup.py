3fd6ca572e936c70d5cce545084f7747f795c96e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:21 +0000

    Add package shelljob/0.3.2/shelljob-0.3.2.tar.gz

---
+++ packages/shelljob/shelljob-0.3.2.tar.gz/shelljob-0.3.2/setup.py
@@ -0,0 +1,36 @@
+# shelljob setup.py
+import os
+from distutils.core import setup
+
+import pypandoc
+desc = pypandoc.convert( 'README.md', 'rst' )
+
+def list_files(path):
+	m = []
+	for root, dirnames, filenames in os.walk(path):
+		for filename in filenames:
+			m.append(os.path.join(root, filename))
+	return m
+			
+setup(
+	name = 'shelljob',
+	packages = [ 'shelljob' ],
+	version = '0.3.2',
+	description = 'Run multiple subprocesses asynchronous/in parallel with streamed output/non-blocking reading',
+	author = 'edA-qa mort-ora-y',
+	author_email = 'eda-qa@disemia.com',
+	url = 'https://pypi.python.org/pypi/shelljob',
+	classifiers = [
+		'Development Status :: 4 - Beta',
+		'Programming Language :: Python',
+		'Intended Audience :: Developers',
+		'Environment :: Console',
+		'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+		'Operating System :: OS Independent',
+		'Topic :: Terminals',
+		'Topic :: System',
+		'Topic :: Software Development :: Build Tools',
+		],
+	long_description = desc,
+	package_data = { 'shelljob': [ 'doc/*' ] },
+)
