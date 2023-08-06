7ea2100bc21e43392db05df20a20fab7e3b2519a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:49 +0000

    Add package task-indicator/1.0/task-indicator-1.0.tar.gz

---
+++ packages/task-indicator/task-indicator-1.0.tar.gz/task-indicator-1.0/src/taskindicator/indicator.py
@@ -0,0 +1,281 @@
+#!/usr/bin/env python
+
+"""TaskWarrior active task count and duration indicator applet.
+"""
+
+from __future__ import print_function
+
+__author__ = "Justin Forest"
+__email__ = "hex@umonkey.net"
+
+
+import appindicator
+import datetime
+import dateutil.parser
+import json
+import os
+import sys
+import time
+
+import pygtk
+pygtk.require("2.0")
+import gtk
+
+from taskindicator import database
+from taskindicator import properties
+from taskindicator import search
+from taskindicator import util
+
+
+FREQUENCY = 1  # seconds
+
+
+def get_task_info(uuid):
+    out = util.run_command(["task", uuid, "export"])
+    return json.loads(out)
+
+
+def get_program_path(command):
+    for path in os.getenv("PATH").split(os.pathsep):
+        full = os.path.join(path, command)
+        if os.path.exists(full):
+            return full
+
+
+class Checker(object):
+    """The indicator applet.  Displays the TaskWarrior icon and current
+    activity time, if any.  The pop-up menu can be used to start or stop
+    running tasks.
+    """
+    appname = "task-indicator"
+    icon = "taskui"
+    icon_attn = "taskui-active"
+
+    def __init__(self):
+        self.toggle_lock = False
+
+        self.task_items = []
+        self.database = database.Database(callback=self.on_tasks_changed)
+
+        self.indicator = appindicator.Indicator(self.appname,
+            self.icon, appindicator.CATEGORY_APPLICATION_STATUS)
+
+        icondir = os.getenv("TASK_INDICATOR_ICONDIR")
+        if icondir:
+            self.indicator.set_icon_theme_path(icondir)
+            print("Appindicator theme path: %s, wanted: %s" % (self.indicator.get_icon_theme_path(), icondir))
+
+
+        self.indicator.set_status(appindicator.STATUS_ACTIVE)
+        self.indicator.set_attention_icon(self.icon_attn)
+
+        self.menu_setup()
+        self.indicator.set_menu(self.menu)
+
+        self.dialog = properties.Dialog(callback=self.on_task_info_closed)
+        self.dialog.on_task_start = self.on_start_task
+        self.dialog.on_task_stop = self.on_stop_task
+
+        self.search_dialog = search.Dialog()
+        self.search_dialog.on_activate_task = self.on_search_callback
+
+        self.database.start_polling()
+
+    def on_start_task(self, task):
+        util.run_command(["task", task["uuid"], "start"])
+        self.update_status()
+        self.open_task_webpage(task)
+
+    def on_stop_task(self, task):
+        util.run_command(["task", task["uuid"], "stop"])
+        self.update_status()
+
+    def open_task_webpage(self, task):
+        for word in task["description"].split(" "):
+            if "://" in word:
+                util.run_command(["xdg-open", word])
+
+    def menu_setup(self):
+        self.menu = gtk.Menu()
+
+        def add_item(text, handler):
+            item = gtk.MenuItem(text)
+            item.connect("activate", handler)
+            item.show()
+            self.menu.append(item)
+            return item
+
+        self.add_task_item = add_item("Add new task...", self.on_add_task)
+        self.show_all_item = add_item("Show more...", self.on_show_all_tasks)
+        self.stop_item = add_item("Stop all running tasks", self.stop)
+        if get_program_path("bugwarrior-pull"):
+            self.bw_item = add_item("Pull tasks from outside",
+                self.on_pull_tasks)
+        self.quit_item = add_item("Quit", self.quit)
+
+    def menu_add_tasks(self):
+        print("Updating menu contents.", file=sys.stderr)
+
+        for item in self.menu.get_children():
+            if item.get_data("is_dynamic"):
+                self.menu.remove(item)
+        self.task_items = []
+
+        data = self.database.get_tasks()
+
+        for task in sorted(data, key=self.task_sort)[:10]:
+            item = gtk.CheckMenuItem(self.format_menu_label(task), use_underline=False)
+            if task.get("start"):
+                item.set_active(True)
+            item.connect("activate", self.on_task_toggle)
+            item.set_data("task", task)
+            item.set_data("is_dynamic", True)
+            item.show()
+
+            self.menu.insert(item, len(self.task_items))
+            self.task_items.append(item)
+
+        if data:
+            item = gtk.SeparatorMenuItem()
+            item.set_data("is_dynamic", True)
+            item.show()
+            self.menu.insert(item, len(self.task_items))
+            self.task_items.append(item)
+
+    def format_menu_label(self, task):
+        proj = task["project"].split(".")[-1]
+        title = u"%s:\t%s" % (proj, util.strip_description(task["description"]))
+        return title
+
+    def task_sort(self, task):
+        """Returns the data to sort tasks by."""
+        # print("%s %s" % (task["urgency"], task["description"]), file=sys.stderr)
+        is_pinned = task.get("priority") == "H"  # "pin" in task.get("tags", [])
+        is_running = "start" in task
+        return -is_running, -is_pinned, -float(task["urgency"])
+
+    def on_add_task(self, widget):
+        self.dialog.show_task({"uuid": None, "status": "pending",
+            "description": "", "priority": "M"})
+
+    def on_pull_tasks(self, widget):
+        util.run_command(["bugwarrior-pull"])
+
+    def on_show_all_tasks(self, widget):
+        self.search_dialog.show_all()
+
+    def on_search_callback(self, uuid):
+        self.dialog.show_task(get_task_info(uuid))
+
+    def on_task_toggle(self, widget):
+        if self.toggle_lock:
+            return
+
+        self.toggle_lock = True
+        widget.set_active(not widget.get_active())
+
+        task = widget.get_data("task")
+        self.dialog.show_task(task)
+        self.toggle_lock = False
+        return True
+
+    def on_task_info_closed(self, updates):
+        """Updates the task when the task info window is closed.  Updates
+        is a dictionary with 'uuid' and modified fields."""
+        if "uuid" in updates:
+            uuid = updates["uuid"]
+            del updates["uuid"]
+        else:
+            uuid = None
+
+        if uuid:
+            command = ["task", uuid, "mod"]
+        else:
+            command = ["task", "add"]
+
+        if updates:
+            for k, v in updates.items():
+                if k == "tags":
+                    for tag in v:
+                        if tag.strip():
+                            command.append(tag)
+                elif k == "description":
+                    command.append(v)
+                else:
+                    command.append("%s:%s" % (k, v))
+            util.run_command(command)
+
+        self.update_status()
+
+    def main(self):
+        """Enters the main program loop"""
+        self.on_timer()
+
+    def stop(self, widget):
+        """Stops running tasks"""
+        for task in self.database.get_tasks():
+            if "start" in task:
+                util.run_command(["task", task["uuid"], "stop"])
+        self.stop_item.hide()
+        self.update_status()
+
+    def quit(self, widget):
+        """Ends the applet"""
+        sys.exit(0)
+
+    def on_tasks_changed(self, tasks):
+        print("on_tasks_changed", file=sys.stderr)
+        self.menu_add_tasks()
+        self.search_dialog.refresh(self.database.get_tasks())
+        self.dialog.refresh(self.database.get_tasks())
+
+    def on_timer(self):
+        """Timer handler which updates the list of tasks and the status."""
+        gtk.timeout_add(FREQUENCY * 1000, self.on_timer)
+        self.update_status()  # display current duration, etc
+
+    def update_status(self):
+        """Changes the indicator icon and text label according to running
+        tasks."""
+        tasks = [t for t in self.database.get_tasks() if "start" in t]
+
+        if not tasks:
+            self.indicator.set_label("Idle")
+            self.indicator.set_status(appindicator.STATUS_ACTIVE)
+            self.stop_item.hide()
+        else:
+            self.indicator.set_status(appindicator.STATUS_ATTENTION)
+            self.stop_item.show()
+
+            msg = "%u/%s" % (len(tasks),
+                self.format_duration(self.get_duration()))
+
+            self.indicator.set_label(msg)
+
+    def get_duration(self):
+        now = datetime.datetime.utcnow()
+
+        duration = datetime.timedelta()
+        for task in self.database.get_tasks():
+            if "start" in task:
+                ts = datetime.datetime.strptime(task["start"], "%Y%m%dT%H%M%SZ")
+                duration += (now - ts)
+
+        return int(duration.total_seconds())
+
+    def format_duration(self, seconds):
+        minutes = seconds / 60
+
+        hours = minutes / 60
+        minutes -= hours * 60
+
+        return "%u:%02u" % (hours, minutes)
+
+
+def main():
+    os.chdir("/")
+
+    app = Checker()
+    app.main()
+    app.search_dialog.show_all()
+    gtk.main()
---
+++ packages/task-indicator/task-indicator-1.0.tar.gz/task-indicator-1.0/src/taskindicator/properties.py
@@ -0,0 +1,324 @@
+# encoding=utf-8
+
+from __future__ import print_function
+
+import re
+import sys
+import webbrowser
+
+import pygtk
+pygtk.require("2.0")
+import gtk
+
+from taskindicator import util
+
+
+class Priority(gtk.ComboBox):
+    """A combo-box with predefined contents, for editing task priority.
+    Emulates get_text/set_text methods which work with H, M and L value,
+    while the human-readable longer priority descriptions are displayed.
+    """
+    def __init__(self):
+        super(Priority, self).__init__()
+
+        self.store = gtk.ListStore(str, str)
+        self.store.append(["H", "high"])
+        self.store.append(["M", "medium (normal)"])
+        self.store.append(["L", "low"])
+
+        self.set_model(self.store)
+
+        cell = gtk.CellRendererText()
+        self.pack_start(cell, True)
+        self.add_attribute(cell, "text", 1)
+
+    def set_text(self, value):
+        """Sets the current priority.  Value can be H, M or L."""
+        if value == "H":
+            self.set_active(0)
+        elif value == "L":
+            self.set_active(2)
+        else:
+            self.set_active(1)
+
+    def get_text(self):
+        """Returns H, M or L, depending on the selected priority."""
+        active = self.get_active()
+        if active == 0:
+            return "H"
+        elif active == 2:
+            return "L"
+        else:
+            return "M"
+
+
+class Project(gtk.ComboBox):
+    def __init__(self):
+        self.value = None
+
+        super(Project, self).__init__()
+        self.store = gtk.ListStore(str)
+        self.set_model(self.store)
+
+        cell = gtk.CellRendererText()
+        self.pack_start(cell, True)
+        self.add_attribute(cell, "text", 0)
+
+        self.refresh()
+
+    def refresh(self, projects=None):
+        self.store.clear()
+        if projects:
+            projects = list(projects)  # copy
+            projects.insert(0, "(none)")
+            for project in sorted(projects):
+                self.store.append([project])
+
+    def set_text(self, value):
+        self.value = value
+
+        for name in self.store:
+            if value == name[0]:
+                self.set_active(name.path[0])
+                return
+
+        self.set_active(0)
+
+    def get_text(self):
+        path = self.get_active()
+        if path == 0:
+            return None
+        return self.store[path][0]
+
+
+class Tags(gtk.Entry):
+    def get_tags(self):
+        return [t for t in re.split(",\s*", self.get_text()) if t.strip()]
+
+
+class Dialog(gtk.Window):
+    def __init__(self, callback=None, debug=False):
+        super(gtk.Window, self).__init__()
+        self.connect("delete_event", self.on_delete_event)
+        self.connect("key-press-event", self._on_keypress)
+
+        self.debug = debug
+        self.task = None
+        self.callback = callback
+
+        self.set_border_width(10)
+
+        self.grid = gtk.Table(6, 2)
+        self.add(self.grid)
+
+        def add_label(text):
+            l = gtk.Label(text)
+            l.set_alignment(0, 0.5)
+            self.grid.attach(l, 0, 1, row, row+1,
+                xoptions=gtk.FILL, yoptions=gtk.FILL, xpadding=2, ypadding=2)
+
+        def add_control(ctl, label):
+            self.grid.attach(ctl, 1, 2, row, row+1,
+                yoptions=gtk.FILL, xpadding=2, ypadding=2)
+            add_label(label)
+
+        row = 0
+        self.uuid = gtk.Entry()
+        self.uuid.set_property("editable", False)
+        add_control(self.uuid, "UUID:")
+
+        row += 1
+        self.description = gtk.Entry()
+        add_control(self.description, "Description:")
+
+        row += 1
+        self.project = Project()
+        add_control(self.project, "Project:")
+
+        row += 1
+        self.priority = Priority()
+        add_control(self.priority, "Priority:")
+
+        row += 1
+        self.tags = Tags()
+        add_control(self.tags, "Tags:")
+
+        row += 1
+        self.completed = gtk.CheckButton("completed")
+        self.grid.attach(self.completed, 1, 2, row, row+1,
+            yoptions=gtk.FILL, xpadding=2, ypadding=2)
+
+        row += 1
+        self.bbx = gtk.HButtonBox()
+        self.grid.attach(self.bbx, 0, 2, row, row+1,
+            yoptions=gtk.FILL, xpadding=2, ypadding=2)
+
+        self.start = gtk.Button("Start")
+        self.start.connect("clicked", self.on_start_stop)
+        self.bbx.add(self.start)
+
+        self.browse = gtk.Button("Open links")
+        self.browse.connect("clicked", self._on_browse)
+        self.bbx.add(self.browse)
+
+        self.close = gtk.Button("Close")
+        self.close.connect("clicked", self.on_close)
+        self.bbx.add(self.close)
+
+        self.set_title("Task properties")
+        self.set_position(gtk.WIN_POS_CENTER)
+        self.set_default_size(600, 100)
+
+        self.set_icon_name("taskui")
+
+    def show_task(self, task):
+        self.task = task
+
+        if task.get("uuid"):
+            self.show_existing_task(task)
+        else:
+            self.show_new_task(task)
+
+        self.show_all()
+        self.description.grab_focus()
+
+    def show_existing_task(self, task):
+        print("Showing task %s ..." % task["uuid"], file=sys.stderr)
+
+        self.uuid.set_text(task["uuid"])
+        self.description.set_text(task["description"])
+        self.project.set_text(task["project"])
+        self.priority.set_text(task["priority"])
+        self.tags.set_text(", ".join(task.get("tags", [])))
+
+        self.completed.set_active(task["status"] == "completed")
+
+        if "start" in task:
+            self.start.set_label("Stop")
+        else:
+            self.start.set_label("Start")
+
+    def show_new_task(self, task):
+        print("Showing new task dialog...", file=sys.stderr)
+
+        self.uuid.set_text("")
+        self.description.set_text("")
+        self.project.set_text("")
+        self.priority.set_text("M")
+        self.tags.set_text("")
+        self.completed.set_active(False)
+
+        self.description.grab_focus()
+
+        self.start.set_label("Add")
+
+    def refresh(self, tasks):
+        """Builds the list of all used project names and feeds it to the
+        project editor combo box."""
+        projects = {}
+        for task in tasks:
+            projects[task["project"]] = True
+        self.project.refresh(projects.keys())
+
+    def _on_browse(self, widget):
+        for word in self.task["description"].split(" "):
+            if "://" in word:
+                webbrowser.open(word)
+
+    def on_close(self, widget):
+        self.hide()
+
+        if self.callback:
+            updates = self.get_task_updates(self.task)
+            if updates and updates.get("uuid"):
+                self.callback(updates)
+
+        if self.debug:
+            gtk.main_quit()
+
+    def get_task_updates(self, task):
+        update = {}
+
+        if task.get("uuid"):
+            update["uuid"] = task["uuid"]
+
+        tmp = self.description.get_text()
+        if tmp is not None and tmp != self.task.get("description"):
+            update["description"] = tmp
+
+        tmp = self.project.get_text()
+        if tmp is not None and tmp != self.task.get("project"):
+            update["project"] = tmp
+
+        tmp = self.priority.get_text()
+        if tmp is not None and tmp != self.task.get("priority"):
+            update["priority"] = tmp
+
+        tmp = "completed" if self.completed.get_active() else "pending"
+        if tmp is not None and tmp != self.task.get("status"):
+            update["status"] = tmp
+
+        tmp = self.tags.get_tags()
+        old_tags = self.task.get("tags", [])
+        if tmp is not None and tmp != old_tags:
+            update["tags"] = []
+            for k in old_tags:
+                if k not in tmp:
+                    update["tags"].append("-" + k)
+            for k in tmp:
+                if k not in old_tags:
+                    update["tags"].append("+" + k)
+
+        return update
+
+    def on_delete_event(self, widget, event, data=None):
+        self.on_close(widget)
+        return True
+
+    def _on_keypress(self, widget, event):
+        if event.keyval == gtk.keysyms.Escape:
+            self.hide()
+        if event.keyval == gtk.keysyms.Return:
+            self.on_close(widget)
+
+    def on_start_stop(self, widget):
+        if not self.task.get("uuid"):
+            self.on_task_add(self.task)
+        elif "start" in self.task:
+            self.on_task_stop(self.task)
+        else:
+            self.on_task_start(self.task)
+        self.on_close(widget)
+
+    def on_task_add(self, task):
+        updates = self.get_task_updates(task)
+        if not updates:
+            print("new task not added: no changes.", file=sys.stderr)
+        else:
+            print("new task: %s" % updates, file=sys.stderr)
+            self.callback(updates)
+
+    def on_task_start(self, task):
+        print("task %s start" % self.task["uuid"], file=sys.stderr)
+
+    def on_task_stop(self, task):
+        print("task %s stop" % self.task["uuid"], file=sys.stderr)
+
+
+def main():
+    def take2(task):
+        if len(task) > 1:
+            print("Task update: %s" % task, file=sys.stderr)
+
+    w = Dialog(callback=take2, debug=True)
+    w.show_task({"uuid": "2ea544b9-a068-4e3e-a99d-5235ed53a17f",
+            "description": "Hello, world.",
+            "project": "oss.taskwarrior",
+            "tags": "hobby, linux",
+            "start": "20130203T163010Z",
+            "status": "completed"})
+    gtk.main()
+
+
+if __name__ == "__main__":
+    main()
