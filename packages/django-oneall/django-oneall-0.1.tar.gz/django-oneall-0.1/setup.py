8d3e7849ba1ac0e6dfc22b578b9073ea60e11d8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:40 +0000

    Add package django-oneall/0.1/django-oneall-0.1.tar.gz

---
+++ packages/django-oneall/django-oneall-0.1.tar.gz/django-oneall-0.1/setup.py
@@ -0,0 +1,14 @@
+import os
+from distutils.core import setup
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+setup(name='django-oneall',
+    version='0.1',
+    packages=['django_oneall'],
+    requires=['django (>=1.4)', 'pyoneall (==0.1)'],
+    license='MIT License, see LICENSE file',
+    description='Django Authentication support for OneAll. Provides unified authentication for 20+ social networks',
+    long_description=README,
+    url='http://www.leandigo.com/',
+    author='Michael Greenberg / Leandigo',
+    author_email='michael@leandigo.com'
+)
