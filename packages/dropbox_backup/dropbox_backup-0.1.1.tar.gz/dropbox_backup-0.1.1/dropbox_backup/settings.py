600a3c12f4167ef4ca430e3cd46fc293c2f4249c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:34 +0000

    Add package dropbox_backup/0.1.1/dropbox_backup-0.1.1.tar.gz

---
+++ packages/dropbox_backup/dropbox_backup-0.1.1.tar.gz/dropbox_backup-0.1.1/dropbox_backup/settings.py
@@ -0,0 +1,12 @@
+BACKUP_PATH = "/home/backups/"
+MYSQL_BACKUP = True
+
+BACKUP_PACKS = [
+    "git:/home/git/",
+    "nginx:/etc/nginx/",
+    "minecraft:/home/minecraft",
+]
+
+CHUNK_SIZE = 1024 * 10000
+DROPBOX_BASE_URL = "https://api.dropbox.com/1"
+DROPBOX_CONTENT_URL = "https://api-content.dropbox.com/1"
