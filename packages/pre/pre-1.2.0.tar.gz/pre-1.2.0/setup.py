0fd7053c8ccd656ae2444916de679196e7539bc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:22 +0000

    Add package pre/1.2.0/pre-1.2.0.tar.gz

---
+++ packages/pre/pre-1.2.0.tar.gz/pre-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name='pre',
+        version='1.2.0',
+        py_modules=['pre'],
+        author='naoki',
+        author_email='i.naoki@me.com',
+        url='http://www.naoki.tw/',
+        description='一個測試用的檔案！',
+     )
