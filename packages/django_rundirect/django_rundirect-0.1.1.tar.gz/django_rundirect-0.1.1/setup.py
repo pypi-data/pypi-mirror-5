83cc4fa03238f6b844c8691e2e8d5faf83bc8f94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:09 +0000

    Add package django_rundirect/0.1.1/django_rundirect-0.1.1.tar.gz

---
+++ packages/django_rundirect/django_rundirect-0.1.1.tar.gz/django_rundirect-0.1.1/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+sdict = {
+    'name': 'django_rundirect',
+    'version': "0.1.1",
+    'license': 'MIT',
+    'packages': ['django_rundirect',
+                 'django_rundirect.management',
+                 'django_rundirect.management.commands'],
+    'package_dir': {'django_rundirect': 'django_rundirect'}, 
+    'zip_safe': False,
+    'install_requires': ['django'],
+    'author': 'Lichun',
+    'url': 'https://gitcafe.com/yuexue/django_rundirect',
+    'classifiers': [
+        'Environment :: Console',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python']
+}
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(**sdict)
