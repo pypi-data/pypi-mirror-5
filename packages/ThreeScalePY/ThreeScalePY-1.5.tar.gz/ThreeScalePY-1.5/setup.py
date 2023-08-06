314e145cd83189909f08eccf679dcc10d7e8448d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:26 +0000

    Add package ThreeScalePY/1.5/ThreeScalePY-1.5.tar.gz

---
+++ packages/ThreeScalePY/ThreeScalePY-1.5.tar.gz/ThreeScalePY-1.5/setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='ThreeScalePY',
+    version='1.5',
+    author='3scale',
+    author_email='support@3scale-net',
+    url="http://3scale.net",
+    py_modules=['ThreeScalePY'],
+)
+
