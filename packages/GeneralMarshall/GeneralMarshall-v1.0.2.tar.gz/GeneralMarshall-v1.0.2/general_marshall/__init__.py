accc221b56ac3c88399301437fdc7c03df7704b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:05 +0000

    Add package GeneralMarshall/v1.0.2/GeneralMarshall-v1.0.2.tar.gz

---
+++ packages/GeneralMarshall/GeneralMarshall-v1.0.2.tar.gz/GeneralMarshall-v1.0.2/general_marshall/__init__.py
@@ -0,0 +1,9 @@
+"""
+A library for manipulating XML.
+
+Author: Daniel Lee
+"""
+
+VERSION = "v1.0.2"
+
+from .marshall import XML
---
+++ packages/GeneralMarshall/GeneralMarshall-v1.0.2.tar.gz/GeneralMarshall-v1.0.2/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2.7
+'''
+Created on 22.04.2013
+
+@author: dlee
+'''
+
+from distutils.core import setup
+
+from general_marshall import VERSION
+
+setup(name="GeneralMarshall",
+      version=VERSION,
+      description="An XML marshalling and demartialling library",
+      author="Daniel Lee",
+      author_email="Daniel.Lee@dwd.de",
+      license="GNU GPL",
+      url="https://github.com/erget/GeneralMarshall",
+      packages=["general_marshall"],
+      long_description=open("README.md").read()
+      )
