f766403f79c726779e23f9490b61484958d119ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:44 +0000

    Add package ftw.billboard/1.2.3/ftw.billboard-1.2.3.zip

---
+++ packages/ftw.billboard/ftw.billboard-1.2.3.zip/ftw.billboard-1.2.3/ftw/billboard/testing.py
@@ -0,0 +1,46 @@
+from plone.app.testing import IntegrationTesting, FunctionalTesting
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import applyProfile
+from plone.app.testing import setRoles, TEST_USER_ID, TEST_USER_NAME, login
+from plone.testing import z2
+from zope.configuration import xmlconfig
+
+
+class FtwBillboardLayer(PloneSandboxLayer):
+
+    defaultBases = (PLONE_FIXTURE,)
+
+    def setUpZope(self, app, configurationContext):
+        # Load ZCML
+        import plone.app.registry
+        xmlconfig.file(
+            'configure.zcml', plone.app.registry,
+                context=configurationContext)
+
+        import ftw.billboard
+
+        xmlconfig.file(
+            'configure.zcml', ftw.billboard,
+                context=configurationContext)
+
+        # installProduct() is *only* necessary for packages outside
+        # the Products.* namespace which are also declared as Zope 2 products,
+        # using <five:registerPackage /> in ZCML.
+        z2.installProduct(app, 'ftw.billboard')
+
+    def setUpPloneSite(self, portal):
+        # Install into Plone site using portal_setup
+        applyProfile(portal, 'plone.app.registry:default')
+        applyProfile(portal, 'ftw.billboard:default')
+
+        setRoles(portal, TEST_USER_ID, ['Manager'])
+        login(portal, TEST_USER_NAME)
+
+
+FTW_BILLBOARD_FIXTURE = FtwBillboardLayer()
+FTW_BILLBOARD_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(FTW_BILLBOARD_FIXTURE,), name="FtwBillboard:Integration")
+FTW_BILLBOARD_FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(FTW_BILLBOARD_FIXTURE,),
+    name="ftw.billboard:functional")
