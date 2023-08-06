a6d085562c9a4251cac10baeebf4f9511e727ca7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:52 +0000

    Add package jmbo-gallery/0.2.3/jmbo-gallery-0.2.3.tar.gz

---
+++ packages/jmbo-gallery/jmbo-gallery-0.2.3.tar.gz/jmbo-gallery-0.2.3/CHANGELOG.rst
@@ -0,0 +1,31 @@
+Changelog
+=========
+
+0.2.3
+-----
+#. Cache templates.
+
+0.2.2
+-----
+#. Do not assume the presence of jquery.
+#. Friendlier url to detail page.
+
+0.2.1
+-----
+#. Bulk image upload form
+
+0.2
+---
+#. Refactor embedded video so it can be used in a gallery.
+#. Finer grained photo sizes. Run `jmbo-foundry` load_photosizes to activate these new photo sizes.
+#. Videos launch the native player on mobile devices.
+
+0.1
+---
+#. Add dependency on `jmbo-foundry`.
+#. Port templates from `jmbo-foundry`.
+
+0.0.3
+-----
+#. Clean up detail view.
+
---
+++ packages/jmbo-gallery/jmbo-gallery-0.2.3.tar.gz/jmbo-gallery-0.2.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jmbo-gallery',
+    version='0.2.3',
+    description='Jmbo gallery app.',
+    long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekelt.com',
+    license='BSD',
+    url='http://github.com/praekelt/jmbo-gallery',
+    packages = find_packages(),
+    install_requires = [
+        'jmbo-foundry>=0.7',
+    ],
+    include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
+)
