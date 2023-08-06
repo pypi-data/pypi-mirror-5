a537ba312ae46618e5a0893fb37fc7b51fb29867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:18 +0000

    Add package tsuru-circus/0.5.0/tsuru-circus-0.5.0.tar.gz

---
+++ packages/tsuru-circus/tsuru-circus-0.5.0.tar.gz/tsuru-circus-0.5.0/tsuru/hooks/__init__.py
@@ -0,0 +1,46 @@
+# Copyright 2013 tsuru-circus authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+import os
+import yaml
+import subprocess
+
+
+def load_config(**kwargs):
+    watcher = kwargs.get("watcher")
+    files_name = ["app.yaml", "app.yml"]
+    for file_name in files_name:
+        try:
+            with open(os.path.join(watcher.working_dir, file_name)) as f:
+                return yaml.load(f.read())
+        except IOError:
+            pass
+    return {}
+
+
+def run_commands(name, **kwargs):
+    from tsuru.stream import Stream
+    config = load_config(**kwargs)
+    watcher = kwargs.get("watcher")
+    cmds = config.get('hooks', {}).get(name, [])
+    if cmds:
+        Stream(watcher_name=watcher.name)(
+            {"data": " ---> Running {}".format(name)})
+    for command in cmds:
+        try:
+            result = subprocess.check_output([command],
+                                             stderr=subprocess.STDOUT,
+                                             shell=True)
+        except subprocess.CalledProcessError as e:
+            result = str(e)
+        Stream(watcher_name=watcher.name)({"data": result})
+    return True
+
+
+def before_start(*args, **kwargs):
+    return run_commands('pre-restart', **kwargs)
+
+
+def after_start(*args, **kwargs):
+    return run_commands('post-restart', **kwargs)
