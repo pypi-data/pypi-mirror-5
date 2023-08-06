6333ff2df902ea7d09b5c8428a3ca25fd7af1bb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:46 +0000

    Add package Django-Pizza/13.10.6/Django-Pizza-13.10.6.tar.gz

---
+++ packages/Django-Pizza/Django-Pizza-13.10.6.tar.gz/Django-Pizza-13.10.6/pizza/kitchen_sink/static/ks/js/ckconfig.js
@@ -0,0 +1,21 @@
+CKEDITOR.editorConfig = function( config ) {
+  config.toolbar = 'Full';
+  config.scayt_autoStartup = true;
+  
+  config.toolbar_Full =
+  [
+    { name: 'document', items : [ 'Source' ] },
+    { name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
+  	{ name: 'editing', items : [ 'Find','Replace','-','SelectAll','-', 'Scayt' ] },
+  	//'/',
+  	{ name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
+  	{ name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote','CreateDiv',
+  	'-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','BidiLtr','BidiRtl' ] },
+  	{ name: 'links', items : [ 'Link','Unlink','Anchor' ] },
+  	{ name: 'insert', items : [ 'Image','Flash','Table','HorizontalRule','SpecialChar','Iframe' ] },
+  	'/',
+  	{ name: 'styles', items : [ 'Styles','Format', 'Font', 'FontSize' ] },
+  	{ name: 'colors', items : [ 'TextColor','BGColor' ] },
+  	{ name: 'tools', items : [ 'Maximize', 'ShowBlocks','-','About' ] }
+  ];
+}
