25936da0a5db0df12bd0b8ba6344258ff63270e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:05 +0000

    Add package python_util_thanq/0.0.1/python_util_thanq-0.0.1.zip

---
+++ packages/python_util_thanq/python_util_thanq-0.0.1.zip/python_util_thanq-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = 'python_util_thanq',
+    version = '0.0.1',
+    py_modules = ['nester'],
+    author = 'thanq',
+    author_email = 'thanq1@gmail.com',
+    description = "thanq's util"
+    )
