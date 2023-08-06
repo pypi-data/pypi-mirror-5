20eae1466d774b5521883d90b6693aac2f1e5dd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:47 +0000

    Add package weechat_notifier/0.1.11/weechat_notifier-0.1.11.tar.gz

---
+++ packages/weechat_notifier/weechat_notifier-0.1.11.tar.gz/weechat_notifier-0.1.11/test/test.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+import pdb
+import irc_message_processor as imp 
+import time
+
+imp.init( { "username": "fenton", "monitor_time": 5 } )
+start_time = time.time()
+
+incoming_message = { "from_who": "fenton",
+                     "channel" : "lisp",
+                     "message" : "john clean up your mess!",
+                     "to_me"   : False }
+response_actions = imp.generate_actions_from_irc_message(incoming_message)
+# pdb.set_trace()
+imp.printall(incoming_message, response_actions)
+imp.perform_actions(response_actions)
+
+imp.pring_sep(2)
+
+incoming_message = { "from_who": "john",
+                     "channel" : "lisp",
+                     "message" : "this is barrys mess!",
+                     "to_me"   : False }
+response_actions = imp.generate_actions_from_irc_message(incoming_message)
+imp.printall(incoming_message, response_actions)
+imp.perform_actions(response_actions)
+
+imp.pring_sep(4)
+
+incoming_message = { "from_who": "barry",
+                     "channel" : "lisp",
+                     "message" : "Let me look",
+                     "to_me"   : False }
+response_actions = imp.generate_actions_from_irc_message(incoming_message)
+imp.printall(incoming_message, response_actions)
+imp.perform_actions(response_actions)
+
+imp.pring_sep(2)
+
+incoming_message = { "from_who": "barry",
+                     "channel" : "lisp",
+                     "message" : "Fenton this is really your mess!",
+                     "to_me"   : True }
+response_actions = imp.generate_actions_from_irc_message(incoming_message)
+imp.printall(incoming_message, response_actions)
+imp.perform_actions(response_actions)
+
+imp.pring_sep(2)
+
+incoming_message = { "from_who": "fenton",
+                     "channel" : "lisp",
+                     "message" : "Ok I'll clean it up",
+                     "to_me"   : False }
+response_actions = imp.generate_actions_from_irc_message(incoming_message)
+imp.printall(incoming_message, response_actions)
+imp.perform_actions(response_actions)
+
