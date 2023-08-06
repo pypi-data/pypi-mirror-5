af64ae478562dcf0468b504cc554e9d547091a40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:15 +0000

    Add package myNaiveBayes/1.0.0/myNaiveBayes-1.0.0.tar.gz

---
+++ packages/myNaiveBayes/myNaiveBayes-1.0.0.tar.gz/myNaiveBayes-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name = 'myNaiveBayes',
+	version = '1.0.0',
+	py_modules = ['myNaiveBayes'],
+	author = 'chenliyu',
+	author_email = '2572065090@qq.com',
+	url = 'http://pypi.python.org/ ',
+	description = 'implimention of NaiveBayes in python,and two examples of easy application of it',
+)
+
