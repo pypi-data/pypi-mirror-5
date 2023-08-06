c80bff37127472d4faa812d6a3331f1780821609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:43 +0000

    Add package serrano/2.0.18/serrano-2.0.18.tar.gz

---
+++ packages/serrano/serrano-2.0.18.tar.gz/serrano-2.0.18/serrano/resources/field/stats.py
@@ -0,0 +1,33 @@
+from django.core.urlresolvers import reverse
+from avocado.events import usage
+from .base import FieldBase
+
+
+class FieldStats(FieldBase):
+    "Field Stats Resource"
+
+    def get(self, request, pk):
+        uri = request.build_absolute_uri
+        instance = request.instance
+
+        if instance.simple_type == 'number':
+            stats = instance.max().min().avg()
+        else:
+            stats = instance.count(distinct=True)
+
+        if stats is None:
+            resp = {}
+        else:
+            resp = next(iter(stats))
+
+        resp['_links'] = {
+            'self': {
+                'href': uri(reverse('serrano:field-stats', args=[instance.pk])),
+            },
+            'parent': {
+                'href': uri(reverse('serrano:field', args=[instance.pk])),
+            },
+        }
+
+        usage.log('stats', instance=instance, request=request)
+        return resp
