99d1f2377998d84e8993e1fa14c6d1cdfcadf3e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:06 +0000

    Add package django-relatives/0.3.0/django-relatives-0.3.0.tar.gz

---
+++ packages/django-relatives/django-relatives-0.3.0.tar.gz/django-relatives-0.3.0/docs/index.rst
@@ -0,0 +1,25 @@
+django-relatives
+================
+
+django-relatives provides various utilites that make it easy to link related
+objects in the Django admin site.
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   usage
+   api
+
+Visit the project `on Github`_ to view the source, submit issues and pull
+requests.
+
+.. _on github: https://github.com/treyhunner/django-relatives
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`search`
+
---
+++ packages/django-relatives/django-relatives-0.3.0.tar.gz/django-relatives-0.3.0/relatives/tests/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+
+from . import models
+
+
+class ShipAdmin(admin.ModelAdmin):
+    readonly_fields = ['name']
+
+
+admin.site.register(models.Ship, ShipAdmin)
+
+
+class SailorAdmin(admin.ModelAdmin):
+    readonly_fields = ['ship']
+
+
+admin.site.register(models.Sailor, SailorAdmin)
+
+
+class PetAdmin(admin.ModelAdmin):
+    readonly_fields = ['owner']
+
+
+admin.site.register(models.Pet, PetAdmin)
+
+
+admin.site.register(models.Movie)
+admin.site.register(models.Actor)
