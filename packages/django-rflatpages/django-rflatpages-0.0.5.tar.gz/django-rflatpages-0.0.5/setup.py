585a2a66276086ea79c778fa8e01e02058377638
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:51 +0000

    Add package django-rflatpages/0.0.5/django-rflatpages-0.0.5.tar.gz

---
+++ packages/django-rflatpages/django-rflatpages-0.0.5.tar.gz/django-rflatpages-0.0.5/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='django-rflatpages',
+    version='0.0.5',
+    author=u'Oscar M. Lage Guitian',
+    author_email='r0sk10@gmail.com',
+    #packages=['rflatpages'],
+    packages = find_packages(),
+    include_package_data = True,
+    package_data = {'': ['rflatpages/templates', 'rflatpages/static','rflatpages/fixtures',], 'rflatpages-example': ['rflatpages-example/*']},
+    url='http://bitbucket.org/r0sk/django-rflatpages',
+    license='BSD licence, see LICENSE file',
+    description='Yet another Django Flatpages App',
+    zip_safe=False,
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "Django >= 1.4.1",
+        "South == 0.7.5",
+        "django-tinymce >= 1.5.1b2",
+        #"django-allauth == 0.7.0",
+        #"django-email-confirmation == 0.2",
+        #"PIL == 1.1.7",
+        "django-filebrowser-no-grappelli == 3.1.1",
+        #"django-tagging == 0.3.1",
+        #"django-compressor == 1.1.2",
+        #"sorl-thumbnail == 11.12",
+    ],
+    keywords = "django application flatpages",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
