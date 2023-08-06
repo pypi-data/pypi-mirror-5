a2bac52848787b50bc0d8124653483a4f1df144e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:10 +0000

    Add package myRegression/1.0.0/myRegression-1.0.0.tar.gz

---
+++ packages/myRegression/myRegression-1.0.0.tar.gz/myRegression-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name = 'myRegression',
+	version = '1.0.0',
+	py_modules = ['myRegression'],
+	author = 'chenliyu',
+	author_email = '2572065090@qq.com',
+	url = 'http://pypi.python.org/ ',
+	description = 'implimention of regression in python,including standard version,lwlr version,ridge version,an implemention of greedy algorithm of regression and least squares weight version',
+)
+
