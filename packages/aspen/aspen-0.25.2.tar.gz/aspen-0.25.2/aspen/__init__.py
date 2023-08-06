1c3bc88bf1a05b79234e8b9406c40f379e0dab62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:22 +0000

    Add package aspen/0.25.2/aspen-0.25.2.tar.gz

---
+++ packages/aspen/aspen-0.25.2.tar.gz/aspen-0.25.2/aspen/__init__.py
@@ -0,0 +1,33 @@
+import sys
+import pkg_resources
+
+from backcompat import is_callable
+
+# imports of convenience
+from aspen.http.response import Response
+from aspen import json_ as json
+from aspen.logging import log, log_dammit
+
+# Shut up, PyFlakes. I know I'm addicted to you.
+Response, json, is_callable, log, log_dammit
+
+dist = pkg_resources.get_distribution('aspen')
+__version__ = dist.version
+WINDOWS = sys.platform[:3] == 'win'
+NETWORK_ENGINES = ['cheroot']
+
+for entrypoint in pkg_resources.iter_entry_points(group='aspen.network_engines'):
+    NETWORK_ENGINES.append(entrypoint.name)
+
+BUILTIN_RENDERERS = [ 'stdlib_format'
+            , 'stdlib_percent'
+            , 'stdlib_template'
+            ]
+
+RENDERERS = BUILTIN_RENDERERS[:]
+
+for entrypoint in pkg_resources.iter_entry_points(group='aspen.renderers'):
+    RENDERERS.append(entrypoint.name)
+
+RENDERERS.sort()
+
---
+++ packages/aspen/aspen-0.25.2.tar.gz/aspen-0.25.2/aspen/resources/json_resource.py
@@ -0,0 +1,30 @@
+from aspen import json
+from aspen.resources.dynamic_resource import DynamicResource
+
+
+class JSONResource(DynamicResource):
+
+    min_pages = 2
+    max_pages = 2
+    
+    def compile_page(self, page):
+        raise SyntaxError('JSON resources should only have logic pages')
+
+    def process_raised_response(self, response):
+        """Given a response, mutate it as needed.
+        """
+        self._process(response)
+
+    def get_response(self, context):
+        """Given a context dict, return a response object.
+        """
+        response = context['response']
+        self._process(response)
+        return response
+
+    def _process(self, response):
+        """Given a response object, mutate it for JSON.
+        """
+        if not isinstance(response.body, basestring):
+            response.body = json.dumps(response.body)
+        response.headers['Content-Type'] = self.website.media_type_json
