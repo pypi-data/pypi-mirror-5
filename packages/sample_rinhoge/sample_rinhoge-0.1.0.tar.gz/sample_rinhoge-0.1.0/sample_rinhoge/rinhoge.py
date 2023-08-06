f0e40d5c64b9564c49b76b4a39e3f17a2a54350f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:51 +0000

    Add package sample_rinhoge/0.1.0/sample_rinhoge-0.1.0.tar.gz

---
+++ packages/sample_rinhoge/sample_rinhoge-0.1.0.tar.gz/sample_rinhoge-0.1.0/sample_rinhoge/rinhoge.py
@@ -0,0 +1,13 @@
+
+from __future__ import absolute_import
+
+class RinHoge(object):
+	pass
+
+if __name__ == '__main__':
+	rin = RinHoge()
+	print("rinhoge")
+
+
+
+
---
+++ packages/sample_rinhoge/sample_rinhoge-0.1.0.tar.gz/sample_rinhoge-0.1.0/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+from distutils.core import setup
+
+setup(name='sample_rinhoge',
+		version='0.1.0',
+		description='Python Package',
+		long_description='Python Package',
+		author='yukirin',
+		author_email='standupdown@gmail.com',
+		url='http://updownlog.tumblr.com',
+		license='MIT',
+		packages=['sample_rinhoge'],
+		platforms=['Linux'],
+	 )
+
+
+
