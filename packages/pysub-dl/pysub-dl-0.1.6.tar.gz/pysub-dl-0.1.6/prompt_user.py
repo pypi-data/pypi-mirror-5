e9963a8256a48fc001962af4b0feec53c42bd580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:55 +0000

    Add package pysub-dl/0.1.6/pysub-dl-0.1.6.tar.gz

---
+++ packages/pysub-dl/pysub-dl-0.1.6.tar.gz/pysub-dl-0.1.6/prompt_user.py
@@ -0,0 +1,11 @@
+#/usr/bin/env python
+
+def prompt():
+    ''' Returns response from user.
+    '''
+    return raw_input('Was that appropriate? ')
+
+def prompt_again():
+    ''' Returns response from user.
+    '''
+    return raw_input('Enter the number respective to download subtitle: ')
