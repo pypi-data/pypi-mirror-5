f7524ebdf46b7f702304c8b252bd3ee6ef713e23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:26 +0000

    Add package setproctitle/1.1.8/setproctitle-1.1.8.zip

---
+++ packages/setproctitle/setproctitle-1.1.8.zip/setproctitle-1.1.8/src/c.h
@@ -0,0 +1,39 @@
+/*-------------------------------------------------------------------------
+ *
+ * c.h
+ *    A few fundamental C definitions.
+ *
+ * Copyright (c) 2009-2012 Daniele Varrazzo <daniele.varrazzo@gmail.com>
+ *-------------------------------------------------------------------------
+ */
+
+#ifndef C_H
+#define C_H
+
+#include "spt_config.h"
+
+#ifndef __cplusplus
+
+#ifndef bool
+typedef char bool;
+#endif
+
+#ifndef true
+#define true	((bool) 1)
+#endif
+
+#ifndef false
+#define false	((bool) 0)
+#endif
+#endif   /* not C++ */
+
+#include <stddef.h>
+
+/* Let's use our version of strlcpy to avoid portability problems */
+size_t spt_strlcpy(char *dst, const char *src, size_t siz);
+
+#ifdef WIN32
+#include <Windows.h>
+#endif
+
+#endif /* C_H */
