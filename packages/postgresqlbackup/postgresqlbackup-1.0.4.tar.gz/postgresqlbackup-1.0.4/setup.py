961f42c42918408a79020c4f909d5e9357049441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:13 +0000

    Add package postgresqlbackup/1.0.4/postgresqlbackup-1.0.4.tar.gz

---
+++ packages/postgresqlbackup/postgresqlbackup-1.0.4.tar.gz/postgresqlbackup-1.0.4/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+  name = 'postgresqlbackup',
+  url = 'https://github.com/Version2beta/postgresqlbackup',
+  version = '1.0.4',
+  author = 'Rob Martin',
+  author_email = 'rob@version2beta.com',
+  packages = ['postgresqlbackup'],
+  install_requires = [],
+  data_files = [('/etc/postgresql', ['backups.conf'])],
+  entry_points = {
+    'console_scripts': [
+      'postgresql_backup = postgresqlbackup.postgresqlbackup:main'
+    ]
+  }
+)
