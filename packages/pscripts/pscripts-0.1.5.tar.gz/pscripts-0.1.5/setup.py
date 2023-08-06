b17b333ad38e513e5bc43d9ce62e9f662e1c8d2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:44 +0000

    Add package pscripts/0.1.5/pscripts-0.1.5.tar.gz

---
+++ packages/pscripts/pscripts-0.1.5.tar.gz/pscripts-0.1.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+version='0.1.5'
+name='pscripts'
+scripts = ['scripts/python-deployment']
+classifiers = [
+        'Programming Language :: Python :: 3.3',
+    ]
+setup(
+    name = name,
+    version = version,
+    packages = [name],
+    description = 'Weechat Notification Plugin',
+    author='Fenton Travers',
+    author_email='fenton.travers@gmail.com',
+    url='www.google.com',
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description='Some description',
+    classifiers=classifiers,
+    scripts = scripts
+)
