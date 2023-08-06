186a21b1423b7c0be77f8cf16bb89ddaed691758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:17 +0000

    Add package catmat-parser/1.0.1/catmat-parser-1.0.1.tar.gz

---
+++ packages/catmat-parser/catmat-parser-1.0.1.tar.gz/catmat-parser-1.0.1/catmat/parser.py
@@ -0,0 +1,27 @@
+from xml.etree import ElementTree as ET
+
+class CatmatParser(object):
+
+    def __init__(self, xml_string):
+        self.__tree = ET.fromstring(xml_string)
+        self.__data = []
+
+    # @property
+    # def _valid_output_xml
+    #     if self.__tree.find('itens'):
+    #         return True
+    #     return False
+
+    def to_hash(self):
+        for item in self.__tree.find('itens').findall('item'):
+            hash_data = {}
+            unidades = []
+            descricao = item.find('descricao').text.strip()
+            for unidade in item.find('unidades').findall('unidade'):
+                unidades.append([unidade.find('sigla_unidade_fornecimento').text.strip(),
+                                 unidade.find('capacidade').text.strip(),
+                                 unidade.find('unidade_medida').text.strip()])
+            hash_data['descricao'] = descricao
+            hash_data['unidades']  = unidades
+            self.__data.append(hash_data)
+        return self.__data
