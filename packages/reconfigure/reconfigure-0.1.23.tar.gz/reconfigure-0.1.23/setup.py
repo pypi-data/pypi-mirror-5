1c24b4175702df239b2a8a29109a26d01fc082a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:49 +0000

    Add package reconfigure/0.1.23/reconfigure-0.1.23.tar.gz

---
+++ packages/reconfigure/reconfigure-0.1.23.tar.gz/reconfigure-0.1.23/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='reconfigure',
+    version='0.1.23',
+    install_requires=[
+    ],
+    description='An ORM for config files',
+    author='Eugeny Pankov',
+    author_email='e@ajenti.org',
+    url='http://ajenti.org/',
+    packages=find_packages(exclude=['*test*']),
+)
