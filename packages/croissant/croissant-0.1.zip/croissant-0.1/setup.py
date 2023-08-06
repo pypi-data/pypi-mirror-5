65a44890670f50aabca74cf319d43e939827621a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:16 +0000

    Add package croissant/0.1/croissant-0.1.zip

---
+++ packages/croissant/croissant-0.1.zip/croissant-0.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='croissant',
+    version='0.1',
+    packages=[
+            'language',
+            'organization',
+            'output',
+            'project',
+            'step',
+            'story',
+            'tests'
+    ],
+)
