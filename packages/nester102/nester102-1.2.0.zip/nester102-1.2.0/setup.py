6387e69622c7487943b6f352621216bdeddb77d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:13 +0000

    Add package nester102/1.2.0/nester102-1.2.0.zip

---
+++ packages/nester102/nester102-1.2.0.zip/nester102-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester102',
+    version = '1.2.0',
+    py_modules = ['nester102'],
+    author = 'TIN',
+    author_email = '',
+    url = '',
+    description = '',
+    )
