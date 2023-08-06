4a693908faeaaa4721ab3ea2691fea62bd60869c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:42 +0000

    Add package django-upyun-storage/0.0.1.beta/django-upyun-storage-0.0.1.beta.tar.gz

---
+++ packages/django-upyun-storage/django-upyun-storage-0.0.1.beta.tar.gz/django-upyun-storage-0.0.1.beta/setup.py
@@ -0,0 +1,28 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+version = __import__('django_upyun').__version__
+
+setup(
+    name="django-upyun-storage",
+    packages=['django_upyun'],
+    version=version,
+    author="Baye Wayly",
+    author_email="baye@wayly.net",
+    url="https://github.com/waylybaye/django-upyun",
+    install_requires=["requests", "python-dateutil"],
+    license="MIT",
+    tests_require=['Django', 'python-dateutil', 'requests'],
+    test_suite='tests.main',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ],
+)
