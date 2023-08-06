54f5bde52b7f3e33a7900393a16d075b6b6abe1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:22 +0000

    Add package robotframework-tools/0.1a76/robotframework-tools-0.1a76.tar.gz

---
+++ packages/robotframework-tools/robotframework-tools-0.1a76.tar.gz/robotframework-tools-0.1a76/robotshell/__init__.py
@@ -0,0 +1,43 @@
+# robotframework-tools
+#
+# Tools for Robot Framework and Test Libraries.
+#
+# Copyright (C) 2013 Stefan Zimmermann <zimmermann.code@gmail.com>
+#
+# robotframework-tools is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# robotframework-tools is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with robotframework-tools. If not, see <http://www.gnu.org/licenses/>.
+
+"""robotshell
+
+.. moduleauthor:: Stefan Zimmermann <zimmermann.code@gmail.com>
+"""
+__all__ = ['ExtensionRobot', 'load']
+
+from .plugin import RobotPlugin
+
+from .robot import ExtensionRobot
+
+robot_plugin = None
+
+def load_ipython_extension(shell):
+    if robot_plugin:
+        return
+    global robot_plugin = RobotPlugin(shell=shell)
+    try:
+        shell.plugin_manager.register_plugin('robot', robot_plugin)
+    except AttributeError:
+        pass
+
+def load_robotshell_extension(extension, shell):
+    load_ipython_extenstion(shell)
+    robot_plugin.register_extension(extension)
