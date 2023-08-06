673704bbfe642aa0d06b99d691325f7f5c716b25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:09 +0000

    Add package CoPing/0.1.3/CoPing-0.1.3.tar.gz

---
+++ packages/CoPing/CoPing-0.1.3.tar.gz/CoPing-0.1.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='CoPing',
+    version='0.1.3',
+    packages=['CoPing'],
+    url='https://github.com/joedborg/PyPing',
+    scripts=['CoPing/coping'],
+    license='GPLv2',
+    author='Joe Borg',
+    author_email='mail@jdborg.com',
+    description='A Cisco style ping tool'
+)
