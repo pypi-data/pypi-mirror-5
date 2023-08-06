888fdfcc52736095a149ba7b7d5e9325f10eba4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:24 +0000

    Add package ssl_sni/0.0.1/ssl_sni-0.0.1.tar.gz

---
+++ packages/ssl_sni/ssl_sni-0.0.1.tar.gz/ssl_sni-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='ssl_sni',
+    version='0.0.1',
+    author='Alex Orange',
+    author_email='crazycasta@gmail.com',
+    packages=['ssl_sni'],
+    url='https://pypi.python.org/pypi/ssl_sni/0.0.1',
+    license='LICENSE',
+    description='A wrapper to pyOpenSSL to provide an interface like the standard ssl module.',
+    long_description=open('README').read(),
+    install_requires=['pyOpenSSL >= 0.13',
+                      'pyasn1 >= 0.1.7']
+)
