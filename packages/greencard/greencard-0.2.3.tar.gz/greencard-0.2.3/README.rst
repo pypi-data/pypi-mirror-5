152a34a84a4049864b6a1b46dbe7d4e79777051c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:02 +0000

    Add package greencard/0.2.3/greencard-0.2.3.tar.gz

---
+++ packages/greencard/greencard-0.2.3.tar.gz/greencard-0.2.3/README.rst
@@ -0,0 +1,33 @@
+=============================
+greencard
+=============================
+
+.. image:: https://badge.fury.io/py/greencard.png
+    :target: http://badge.fury.io/py/greencard
+    
+.. image:: https://travis-ci.org/Nekroze/greencard.png?branch=master
+        :target: https://travis-ci.org/Nekroze/greencard
+
+.. image:: https://pypip.in/d/greencard/badge.png
+        :target: https://crate.io/packages/greencard?version=latest
+
+Documentation
+-------------
+
+The full documentation is at http://greencard.rtfd.org.
+
+Quickstart
+----------
+
+Install greencard::
+
+    pip install greencard
+
+Then you can use the ``greencard`` command line test runner to descover and run
+card tests defined as all ``.py`` files in the test directory from where the
+runner is executed. For more information see the usage documentation page.
+
+Features
+--------
+
+* A custom testing framework and test runner designed specifically for cards.
---
+++ packages/greencard/greencard-0.2.3.tar.gz/greencard-0.2.3/greencard/greencard.py
@@ -0,0 +1,80 @@
+"""Greencard implementation."""
+from functools import wraps
+
+
+TESTS = []
+
+
+def greencard(func):
+    """
+    A decorator for providing a unittesting function/method with every card in
+    a librarian card library database when it is called.
+    """
+    @wraps(func)
+    def wrapped(*args, **kwargs):
+        """Transparent wrapper."""
+        return func(*args, **kwargs)
+    TESTS.append(wrapped)
+    return wrapped
+
+
+def descovery(testdir):
+    """Descover and load greencard tests."""
+    from os.path import join, exists, isdir, splitext, basename, sep
+    if not testdir or not exists(testdir) or not isdir(testdir):
+        return None
+
+    from os import walk
+    import fnmatch
+    import imp
+
+    for root, _, filenames in walk(testdir):
+        for filename in fnmatch.filter(filenames, '*.py'):
+            path = join(root, filename)
+            modulepath = splitext(root)[0].replace(sep, '.')
+            imp.load_source(modulepath, path)
+
+
+RESULTS = """
+Results:
+{0} tests
+{1} cards
+{2} passes
+{3} failures"""
+
+
+def main(clargs=None):
+    """Command line entry point."""
+    from argparse import ArgumentParser
+    from librarian.library import Library
+    import sys
+
+    parser = ArgumentParser(
+        description="A test runner for each card in a librarian library.")
+    parser.add_argument("library", help="Library database")
+    parser.add_argument("-t", "--tests", default="test/",
+                        help="Test directory")
+    args = parser.parse_args(clargs)
+
+    descovery(args.tests)
+
+    library = Library(args.library)
+    passes = 0
+    failures = 0
+    cardcount = 0
+
+    for card in library.retrieve_all():
+        cardcount += 1
+        failed = False
+        for test in TESTS:
+            try:
+                test(card)
+            except AssertionError:
+                print("{0} failed {1}".format(card.__repr__(), test.__name__))
+                failed = True
+        if failed:
+            failures += 1
+        else:
+            passes += 1
+    print(RESULTS.format(len(TESTS), cardcount, passes, failures))
+    sys.exit(failures)
