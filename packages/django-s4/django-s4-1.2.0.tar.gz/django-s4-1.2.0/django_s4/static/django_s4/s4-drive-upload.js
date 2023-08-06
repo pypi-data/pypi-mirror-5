94700c901d88fed328af19b80329b3ad9ddf786b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:55 +0000

    Add package django-s4/1.2.0/django-s4-1.2.0.tar.gz

---
+++ packages/django-s4/django-s4-1.2.0.tar.gz/django-s4-1.2.0/django_s4/static/django_s4/s4-drive-upload.js
@@ -0,0 +1,34 @@
+// s4 javascript utils (drive utils)
+// author: Ondrej Sika, http://ondrejsika.com
+
+function s4Upload(uri, file, callback, error) {
+    xhr = new XMLHttpRequest();
+    fd = new FormData();
+    xhr.open("POST", uri, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            callback(xhr.responseText);
+        }
+        if (xhr.readyState == 4 && xhr.status != 200) {
+            error(xhr.status, xhr.responseText, xhr)
+        }
+    }
+    fd.append('file', file);
+    xhr.send(fd);
+}
+
+function s4MultiUpload(uri, files, callback, loopInterval) {
+    if (loopInterval == undefined) loopInterval = 300;
+    var responses = []; 
+    for (var i = 0; i < files.length; i++) {
+        s4Upload(uri, files[i], function(data){
+            responses.push(data);
+        }); 
+    };  
+    var loop = setInterval(function(){
+        if (responses.length == files.length) {
+            clearInterval(loop);
+            callback(responses);
+        }   
+    }, loopInterval);
+}
