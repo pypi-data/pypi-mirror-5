f6269710c3b40d76daeb4dbc1f26cbd320538f7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:28 +0000

    Add package KinoUtils/1.0.0/KinoUtils-1.0.0.zip

---
+++ packages/KinoUtils/KinoUtils-1.0.0.zip/KinoUtils-1.0.0/setup.py
@@ -0,0 +1,12 @@
+#!python
+
+from distutils.core import setup
+
+setup(name="KinoUtils",
+      version="1.0.0",
+      description="Python Util modules by kino",
+      author="kino811", 
+      author_email="kino811@gmail.com", 
+      url="http://kino811.blogspot.kr",
+      package=["kinoUtils"],
+    )
