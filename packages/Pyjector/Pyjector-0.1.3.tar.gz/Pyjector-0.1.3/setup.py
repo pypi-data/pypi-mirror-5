45ebdc911538e24b9ea34741192c5e0511f685d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:12 +0000

    Add package Pyjector/0.1.3/Pyjector-0.1.3.tar.gz

---
+++ packages/Pyjector/Pyjector-0.1.3.tar.gz/Pyjector-0.1.3/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Pyjector',
+    version='0.1.3',
+    description='Control your projector over a serial port',
+    author='John Brodie',
+    author_email='john@brodie.me',
+    url='http://www.github.com/JohnBrodie/pyjector',
+    packages=['pyjector'],
+    install_requires=[
+        'pyserial',
+    ],
+    data_files=[('pyjector/projector_configs', ['benq.json'])],
+)
