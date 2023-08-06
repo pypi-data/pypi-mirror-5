06a8553a13499b35b570ebdab640a7a052e4dd16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:23 +0000

    Add package dependency_injection/1.0.0rc1/dependency_injection-1.0.0rc1.tar.gz

---
+++ packages/dependency_injection/dependency_injection-1.0.0rc1.tar.gz/dependency_injection-1.0.0rc1/setup.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from setuptools import setup
+
+
+setup( name='dependency_injection'
+     , author='Gittip, LLC'
+     , author_email='support@gittip.com'
+     , description="dependency_injection helpers"
+     , url='https://dependency-injection-py.readthedocs.org'
+     , version='1.0.0rc1'
+     , py_modules=['dependency_injection']
+     , classifiers=[ 'Development Status :: 5 - Production/Stable'
+                   , 'Intended Audience :: Developers'
+                   , 'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication'
+                   , 'Operating System :: OS Independent'
+                   , 'Programming Language :: Python :: 2'
+                   , 'Programming Language :: Python :: 2.6'
+                   , 'Programming Language :: Python :: 2.7'
+                   , 'Programming Language :: Python :: 3'
+                   , 'Programming Language :: Python :: 3.2'
+                   , 'Programming Language :: Python :: 3.3'
+                   , 'Topic :: Database :: Front-Ends'
+                   , 'Topic :: Software Development :: Libraries :: Python Modules'
+                    ]
+      )
