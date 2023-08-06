2c3378d47a17ff6691d5e27fb98f7b7f156f6672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:27 +0000

    Add package ffprobe/0.2/ffprobe-0.2.zip

---
+++ packages/ffprobe/ffprobe-0.2.zip/ffprobe-0.2/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='ffprobe',
+    version='0.2',
+    description='Wrapper around ffprobe command to extract metadata from media files',
+    author='Simon Hargreaves',
+    author_email='simon@simon-hargreaves.com',
+    url='http://www.simon-hargreaves.com/ffprobe',
+    packages=['ffprobe'],
+	classifiers=[
+		'Development Status :: 3 - Alpha',
+		'Environment :: Console',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: MacOS :: MacOS X',
+		'Operating System :: Microsoft :: Windows',
+		'Operating System :: POSIX',
+		'Programming Language :: Python',
+		'Natural Language :: English',
+		'Topic :: Multimedia :: Video',
+		'Topic :: Software Development :: Libraries'
+		])
