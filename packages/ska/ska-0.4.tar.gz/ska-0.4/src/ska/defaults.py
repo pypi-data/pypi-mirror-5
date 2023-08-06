1e13841adcaa96a4d13151384039e1a3832fe9a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:21 +0000

    Add package ska/0.4/ska-0.4.tar.gz

---
+++ packages/ska/ska-0.4.tar.gz/ska-0.4/src/ska/defaults.py
@@ -0,0 +1,34 @@
+"""
+Application defaults.
+
+- `SIGNATURE_LIFETIME` (int): Signature lifetime in seconds. Default value is 600 (seconds).
+- `DEFAULT_SIGNATURE_PARAM` (str): Default name of the GET param holding the generated signature value.
+  Default value is `signature`.
+- `DEFAULT_AUTH_USER_PARAM` (str): Default name of the GET param holding the ``auth_user`` value.
+  Default value is `auth_user`.
+- `DEFAULT_VALID_UNTIL_PARAM` (str): Default name of the GET param holding the ``valid_until`` value.
+  Default value is `valid_until`.
+"""
+
+__title__ = 'ska.defaults'
+__version__ = '0.4'
+__build__ = 0x000004
+__author__ = 'Artur Barseghyan'
+__all__ = ('TIMESTAMP_FORMAT', 'SIGNATURE_LIFETIME', 'DEFAULT_SIGNATURE_PARAM', 'DEFAULT_AUTH_USER_PARAM', \
+           'DEFAULT_VALID_UNTIL_PARAM', 'DEBUG')
+
+TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S"
+
+# Signature lifetime in seconds
+SIGNATURE_LIFETIME = 600
+
+# Default name of the GET param holding the generated signature value.
+DEFAULT_SIGNATURE_PARAM = 'signature'
+
+# Default name of the GET param holding the ``auth_user`` value.
+DEFAULT_AUTH_USER_PARAM = 'auth_user'
+
+# Default name of the GET param holding the ``valid_until`` value.
+DEFAULT_VALID_UNTIL_PARAM = 'valid_until'
+
+DEBUG = False
