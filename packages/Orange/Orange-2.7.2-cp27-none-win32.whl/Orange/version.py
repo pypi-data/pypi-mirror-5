2f9238507a4dd539ef56c2540872d288232fb255
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:37 +0000

    Add package Orange/2.7.2/Orange-2.7.2-cp27-none-win32.whl

---
+++ packages/Orange/Orange-2.7.2-cp27-none-win32.whl/Orange/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM ORANGE SETUP.PY
+short_version = '2.7.2'
+version = '2.7.2'
+full_version = '2.7.2.dev-Unknown'
+hg_revision = 'Unknown'
+release = False
+
+if not release:
+    version = full_version
