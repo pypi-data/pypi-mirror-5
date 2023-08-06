efdd702aff1acbbe4fdd940b3667c202101287e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:13 +0000

    Add package mixpanel-py/2.0.0/mixpanel-py-2.0.0.tar.gz

---
+++ packages/mixpanel-py/mixpanel-py-2.0.0.tar.gz/mixpanel-py-2.0.0/setup.py
@@ -0,0 +1,18 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='mixpanel-py',
+    version='2.0.0',
+    author='Mixpanel, Inc.',
+    author_email='dev@mixpanel.com',
+    packages=['mixpanel'],
+    url='https://github.com/mixpanel/mixpanel-python',
+    license='LICENSE.txt',
+    description='Official Mixpanel library for Python',
+    long_description=open('README.txt').read(),
+    install_requires=[
+    ],
+)
