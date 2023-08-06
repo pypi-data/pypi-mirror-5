1ce3a45ec259a345a144fcda5ed9c7495e595503
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:27 +0000

    Add package argcommand/0.1/argcommand-0.1.tar.gz

---
+++ packages/argcommand/argcommand-0.1.tar.gz/argcommand-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = "argcommand",
+    version = "0.1",
+    description = "A Python module that provides simple object-oriented abstraction layer for creating command-line interfaces.",
+    author = "Roi Avinoam",
+    author_email = "avinoamr@gmail.com",
+    url = "https://github.com/avinoamr/argcommand",
+    license = "MIT",
+    py_modules = [ "argcommand" ]
+)
