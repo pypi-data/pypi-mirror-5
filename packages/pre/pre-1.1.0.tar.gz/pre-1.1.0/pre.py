5414c8f672518216b8709c0f0e8d052824ae7d54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:21 +0000

    Add package pre/1.1.0/pre-1.1.0.tar.gz

---
+++ packages/pre/pre-1.1.0.tar.gz/pre-1.1.0/pre.py
@@ -0,0 +1,12 @@
+"""這個模組是測試用的"""
+
+#這是第一個function
+def firstFun(theList,level):
+    for aa in theList:
+        if isinstance(aa,list):
+            firstFun(aa,level+1)
+        else:
+            for tab_stop in range(level):
+                print("\t",end='')
+            print(aa)
+
