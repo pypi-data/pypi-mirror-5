dde20a65273ff38d8aaa4ee088d342bfc22ec7a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:16 +0000

    Add package yturl/1.15.5/yturl-1.15.5.tar.gz

---
+++ packages/yturl/yturl-1.15.5.tar.gz/yturl-1.15.5/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name = "yturl",
+    version = "1.15.5",
+    description = "Prints direct URLs to YouTube videos.",
+    url = "https://github.com/cdown/yturl",
+
+    author = "Chris Down",
+    author_email = "chris@chrisdown.name",
+
+    scripts = [ "yturl" ],
+
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Topic :: Multimedia",
+        "Topic :: Internet",
+        "Topic :: Utilities",
+    ],
+)
