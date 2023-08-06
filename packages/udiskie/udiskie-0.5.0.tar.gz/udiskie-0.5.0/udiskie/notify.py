caa3d4c935760b73711cce2be69be6f7356cf89b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:48 +0000

    Add package udiskie/0.5.0/udiskie-0.5.0.tar.gz

---
+++ packages/udiskie/udiskie-0.5.0.tar.gz/udiskie-0.5.0/udiskie/notify.py
@@ -0,0 +1,58 @@
+"""
+Udiskie notification daemon.
+"""
+__all__ = ['Notify']
+
+import pynotify
+import gio
+
+class Notify:
+    """
+    Notification tool.
+
+    Can be connected to udisks daemon in order to automatically issue
+    notifications when system status has changed.
+
+    """
+    def __init__(self, name):
+        """Initialize notifier."""
+        pynotify.init(name)
+
+    # event handlers:
+    def device_mounted(self, device):
+        try:
+            device_file = device.device_file
+            mount_path = device.mount_paths[0]
+            pynotify.Notification('Device mounted',
+                                  '%s mounted on %s' % (device_file, mount_path),
+                                  'drive-removable-media').show()
+        except gio.Error:
+            pass
+
+    def device_umounted(self, device):
+        try:
+            device_file = device.device_file
+            pynotify.Notification('Device unmounted',
+                                  '%s unmounted' % (device_file,),
+                                  'drive-removable-media').show()
+        except gio.Error:
+            pass
+
+    def device_locked(self, device):
+        try:
+            device_file = device.device_file
+            pynotify.Notification('Device locked',
+                                  '%s locked' % (device_file,),
+                                  'drive-removable-media').show()
+        except gio.Error:
+            pass
+
+    def device_unlocked(self, device):
+        try:
+            device_file = device.device_file
+            pynotify.Notification('Device unlocked',
+                                  '%s unlocked' % (device_file,),
+                                  'drive-removable-media').show()
+        except gio.Error:
+            pass
+
---
+++ packages/udiskie/udiskie-0.5.0.tar.gz/udiskie-0.5.0/udiskie/umount.py
@@ -0,0 +1,9 @@
+"""
+Udiskie unmount utilities.
+"""
+__all__ = [
+    'unmount_device', 'lock_device', 'remove_device', 'lock_slave',
+    'unmount', 'unmount_all']
+
+from udiskie.mount import unmount_device, lock_device, remove_device, lock_slave, unmount, unmount_all
+
