cdcb160a94da0bdad00c0198e5b2a29e8e57f5b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:16 +0000

    Add package django-tango/0.1.3/django-tango-0.1.3.tar.gz

---
+++ packages/django-tango/django-tango-0.1.3.tar.gz/django-tango-0.1.3/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='django-tango',
+    version='0.1.3',
+    description='Faster, simpler Django content management.',
+    long_description=open('README.md').read(),
+    url='https://github.com/tBaxter/Tango',
+    license='BSD license, see LICENSE.txt',
+    author=u'Tim Baxter',
+    author_email='mail.baxter@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=required,
+    #zip_safe=False,
+    dependency_links = ['http://github.com/tBaxter/django-voting/tarball/master#egg=tango-voting-0.1']
+)
