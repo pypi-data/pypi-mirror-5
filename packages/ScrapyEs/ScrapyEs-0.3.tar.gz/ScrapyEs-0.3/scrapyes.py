c8f4117796cc808fd0322ce03a215e4bc618c911
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:34 +0000

    Add package ScrapyEs/0.3/ScrapyEs-0.3.tar.gz

---
+++ packages/ScrapyEs/ScrapyEs-0.3.tar.gz/ScrapyEs-0.3/scrapyes.py
@@ -0,0 +1,25 @@
+from scrapy import signals
+from scrapy.exceptions import NotConfigured
+from pyes import ES
+
+class Sender(object):
+
+    def __init__(self, crawler):
+        uri = "%s:%d" % (crawler.settings['ELASTICSEARCH_SERVER'], crawler.settings['ELASTICSEARCH_PORT'])
+        self.es = ES([uri], bulk_size=crawler.settings['ELASTICSEARCH_BULK_SIZE'])
+        self.index = crawler.settings['ELASTICSEARCH_INDEX']
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        # instantiate the extension object
+        ext = cls(crawler)
+        # connect the extension object to signals
+        crawler.signals.connect(ext.item_scraped, signal=signals.item_scraped)
+        # return the extension object	
+        return ext
+
+    def item_scraped(self, item, spider):
+        self.store(item, spider)
+
+    def store(self, item, spider):
+        self.es.index(dict(item), self.index, item.__class__.__name__, bulk=True)
