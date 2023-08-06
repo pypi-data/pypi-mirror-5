c6aceddef20add91a4b5325d66ae64549839e1d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:14 +0000

    Add package pyivi/0.0.5/pyivi-0.0.5.zip

---
+++ packages/pyivi/pyivi-0.0.5.zip/pyivi-0.0.5/setup.py
@@ -0,0 +1,36 @@
+import os
+from distutils.core import setup
+from distutils.command.install import install
+from distutils.command.bdist_wininst import bdist_wininst
+import subprocess
+
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "pyivi",
+    version = "0.0.5",
+    author = "Samuel Deleglise",
+    author_email = "samuel.deleglise@gmail.com",
+    description = ("""Wrapper around the IVI-COM and IVI-C drivers"""),
+    license = "BSD",
+    keywords = "instruments data-acquisition IVI interface",
+    url = "https://github.com/SamuelDeleglise/pyivi-package",
+    packages=['pyivi',
+              'pyivi/ivicom',
+              'pyivi/ivic',
+              'pyivi/ivifactory'],
+    long_description=read('README.rst'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Scientific/Engineering :: Human Machine Interfaces",
+        "License :: OSI Approved :: BSD License",
+    ],
+    install_requires=[
+    ]
+)
