c2226ad141b0154d6d531b8e99ad0e9c765dbf89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:15 +0000

    Add package alioss/0.22.1/alioss-0.22.1.zip

---
+++ packages/alioss/alioss-0.22.1.zip/alioss-0.22.1/oss/pkg_info.py
@@ -0,0 +1,18 @@
+package = "alioss"
+version = "0.22.1"
+url = "http://www.phodal.com"
+license = "GPL version 2"
+author="Phodal.Gmszone"
+author_emial="gmszone@gmail.com",
+short_description = "Command line tool for managing Aliyun Open Storage Service."
+
+long_description = """
+osscmd lets you create/delete/list bucket and upload/download/copy/delete file from/to
+        Aliyun OSS (Open Storage Service).
+
+The package would not be update .
+===============================
+
+
+"""
+
