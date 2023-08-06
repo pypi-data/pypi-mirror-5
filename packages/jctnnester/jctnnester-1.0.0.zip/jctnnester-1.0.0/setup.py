e65c556b3d91fdf43cdc8df94e89d87ec23806ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:18 +0000

    Add package jctnnester/1.0.0/jctnnester-1.0.0.zip

---
+++ packages/jctnnester/jctnnester-1.0.0.zip/jctnnester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name ='jctnnester',
+    version = '1.0.0',
+    py_modules=['jctnnester'],
+    author = 'jctn',
+    author_email = 'jinchengtn@163.com',
+    url='',
+    description = ' a simple demo printer of nester lists',
+    )
