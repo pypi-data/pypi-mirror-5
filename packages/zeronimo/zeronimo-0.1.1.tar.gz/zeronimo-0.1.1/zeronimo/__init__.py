c9082449f9bc67c1a75b383061d603a9527b7d58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:21 +0000

    Add package zeronimo/0.1.1/zeronimo-0.1.1.tar.gz

---
+++ packages/zeronimo/zeronimo-0.1.1.tar.gz/zeronimo-0.1.1/zeronimo/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+    zeronimo
+    ~~~~~~~~
+
+    :copyright: (c) 2013 by Heungsub Lee
+    :license: BSD, see LICENSE for more details.
+"""
+from __future__ import absolute_import
+
+from .components import Worker, Customer, Collector, Task
+from .exceptions import (
+    ZeronimoException, WorkerNotFound, SocketClosed, UnexpectedMessage, raises)
+from .messaging import send, recv
+
+
+__version__ = '0.1.1'
+__all__ = ['Worker', 'Customer', 'Collector', 'Task', 'ZeronimoException',
+           'WorkerNotFound', 'SocketClosed', 'UnexpectedMessage', 'raises',
+           'send', 'recv']
