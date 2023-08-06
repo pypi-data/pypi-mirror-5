cc7555c9b89e2e0bd3b31128726c10e05f3e4250
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:59 +0000

    Add package print_lvl/1.0.0/print_lvl-1.0.0.tar.gz

---
+++ packages/print_lvl/print_lvl-1.0.0.tar.gz/print_lvl-1.0.0/print_lvl.py
@@ -0,0 +1,6 @@
+def print_lvl(liste):
+    for element in liste:
+        if isinstance(element, list):
+            print_lvl(element)
+        else:
+                print(element)
---
+++ packages/print_lvl/print_lvl-1.0.0.tar.gz/print_lvl-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+	name	= 'print_lvl',
+	version	= '1.0.0',
+	py_modules	= ['print_lvl'],
+	author	= 'pyxanme',
+	author_email	= 'pyxanme@gmail.com',
+)
