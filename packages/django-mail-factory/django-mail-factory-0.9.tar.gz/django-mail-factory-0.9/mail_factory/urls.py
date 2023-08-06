678c3ae129bf6c3a30aff4245b7a092fe098cc55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:53 +0000

    Add package django-mail-factory/0.9/django-mail-factory-0.9.tar.gz

---
+++ packages/django-mail-factory/django-mail-factory-0.9.tar.gz/django-mail-factory-0.9/mail_factory/urls.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""URLconf for mail_factory admin interface."""
+from django.conf.urls import patterns, url
+
+from django.conf import settings
+
+from mail_factory.views import mail_list, form, preview_message, html_not_found
+
+
+LANGUAGE_CODES = '|'.join([code for code, name in settings.LANGUAGES])
+
+
+urlpatterns = patterns(
+    '',
+    url(r'^$',
+        mail_list,
+        name='mail_factory_list'),
+    url(r'^(?P<mail_name>[-\w]+)/$',
+        form,
+        name='mail_factory_form'),
+    url(r'^(?P<mail_name>[-\w]+)/preview/(?P<lang>(%s))/$' % LANGUAGE_CODES,
+        preview_message,
+        name='mail_factory_preview_message'),
+    url(r'^(?P<mail_name>[-\w]+)/html_not_found/$',
+        html_not_found,
+        name='mail_factory_html_not_found'),
+)
