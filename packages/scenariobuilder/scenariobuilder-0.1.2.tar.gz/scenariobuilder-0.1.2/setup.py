0d07546ea08c84297d682b52e2562c195c3b7c39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:06 +0000

    Add package scenariobuilder/0.1.2/scenariobuilder-0.1.2.tar.gz

---
+++ packages/scenariobuilder/scenariobuilder-0.1.2.tar.gz/scenariobuilder-0.1.2/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='scenariobuilder',
+    version='0.1.2',
+    author='Michael Chapman',
+    author_email='woppin@gmail.com',
+    packages=['scenariobuilder'],
+    scripts=['bin/sb'],
+    license='LICENSE.txt',
+    long_description=open('README').read(),
+    setup_requires=[
+        "python-novaclient",
+        "python-neutronclient",
+        "PyYaml>=3.10",
+        "jinja2>=2.7.1"
+    ],
+    
+)
