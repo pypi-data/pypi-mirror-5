755a7488e51161e869c69a10baf386b3445c6c0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:50 +0000

    Add package formpump/0.1.8/formpump-0.1.8-py2.7.egg

---
+++ packages/formpump/formpump-0.1.8-py2.7.egg/formpump/jinjapump.py
@@ -0,0 +1,271 @@
+"FormPump - It fills up forms"
+
+from jinja2 import Environment, environmentfunction, nodes
+from jinja2.utils import Markup
+from jinja2.ext import Extension
+
+from base import Form, StubForm
+
+def pumpwidget(func):
+    @environmentfunction
+    def wrap(*args, **kwargs):
+        args = list(args)
+        env_index = 0
+        if len(args) > 1 and not isinstance(args[env_index], Environment):
+            env_index = 1
+        env = args[env_index]
+        
+        jinjapump = None
+
+        for ext in env.extensions.values():
+            if  isinstance(ext, JinjaPump):
+                jinjapump = ext
+                break
+            if jinjapump is None:
+                raise Exception('Could not find JinjaPump in extensions')
+        args[env_index] = jinjapump
+        return func(*args, **kwargs)
+    
+    return wrap
+
+class JinjaPump(Extension):
+    # a set of names that trigger the extension.
+    tags = set(['checkbox', 'email', 'error', 'file', 'form', 'form_ctx', 'hidden', 'iferror', 'ifnoterror', 'label', 'password', 'quickselect', 'radio', 'submit', 'text', 'textarea'])
+
+    def __init__(self, environment):
+        Extension.__init__(self, environment)
+        environment.extend(
+            default_form_action = '',
+            error_renderers     = {},
+            value_dict_name     = 'form_vars',
+            error_dict_name     = 'form_errors',
+            form_name_key       = None,
+            form_ctx_key        = None,
+            )
+        self.form = StubForm()
+
+    def _form_vars_node(self):
+        return nodes.Or(nodes.Name(self.environment.value_dict_name, 'load'), nodes.Dict([]))
+    
+    def _form_errors_node(self):
+        return nodes.Or(nodes.Name(self.environment.error_dict_name, 'load'), nodes.Dict([]))
+
+    def parse(self, parser):
+        tag = parser.stream.next()
+
+        if tag.value == 'form':
+            return self._form(parser, tag)
+        elif tag.value == 'form_ctx':
+            return self._form_ctx(parser, tag)
+        elif tag.value in ( 'email', 'file', 'hidden', 'password', 'text'):
+            return self._input(parser, tag)
+        elif tag.value == 'checkbox':
+            return self._check(parser, tag)
+        elif tag.value == 'iferror':
+            return self._iferror(parser, tag)
+        elif tag.value == 'ifnoterror':
+            return self._ifnoterror(parser, tag)
+        elif tag.value == 'radio':
+            return self._radio(parser, tag)
+        elif tag.value == 'submit':
+            return self._submit(parser, tag)
+        elif tag.value == 'label':
+            return self._label(parser, tag)
+        elif tag.value == 'quickselect':
+            return self._quick_select(parser, tag)
+        elif tag.value == 'textarea':
+            return self._text_area(parser, tag)
+        elif tag.value == 'error':
+            return self._field_error(parser)
+
+    def _parse_attrs(self, parser, add_id=True):
+        name = None
+        if parser.stream.current.test('string'):
+            name = parser.parse_expression(with_condexpr=False)
+
+        attrs = {}
+        while parser.stream.current.type != 'block_end':
+            node = parser.parse_assign_target(with_tuple=False)
+
+            if parser.stream.skip_if('assign'):
+                attrs[node.name] = parser.parse_expression()
+            else:
+                attrs[node.name] = nodes.Const(node.name)
+
+        return (name, attrs)
+
+    def _form(self, parser, tag):
+        form_name, attrs = self._parse_attrs(parser)
+
+        form_name = form_name or nodes.Const(None)
+        
+        body = parser.parse_statements(['name:endform'], drop_needle=True)
+
+        attrs = nodes.Dict([nodes.Pair(nodes.Const(k), v) for k,v in attrs.items()])
+
+        return nodes.CallBlock(
+            self.call_method('_form_block', 
+                                     args=[form_name, attrs, 
+                                           self._form_vars_node(), 
+                                           self._form_errors_node()]),
+                    [], [], body).set_lineno(tag.lineno)
+
+    def _form_block(self, form_name, attrs, form_vars, form_errors, caller):
+        form = Form(form_name, 
+                    self.environment.form_name_key, 
+                    self.environment.form_ctx_key,
+                    attrs, 
+                    self.environment.default_form_action,
+                    form_vars,
+                    form_errors)
+        form, self.form = self.form, form
+        ret = caller()
+        form, self.form = self.form, form
+        return Markup(form.start_tag()) + ret + Markup(form.end_tag())
+
+    def _form_ctx(self, parser, tag):
+        name, attrs = self._parse_attrs(parser)
+
+        if name is not None:
+            attrs['name'] = name
+
+        attrs = nodes.Dict([nodes.Pair(nodes.Const(k), v) for k,v in attrs.items()])
+
+        return nodes.Output([self.call_method('_switch_form_ctx', args=[attrs])])
+
+    def _switch_form_ctx(self, attrs):
+        return Markup(self.form.context_tag(attrs))
+
+    def _input(self, parser, tag, method_name='input_tag'):
+        name, attrs = self._parse_attrs(parser)
+        if name is not None:
+            attrs['name'] = name
+
+        attrs['type'] = nodes.Const(tag.value)
+        
+        attrs = nodes.Dict([nodes.Pair(nodes.Const(k), v) for k,v in attrs.items()])
+
+        return nodes.Output([self.call_method(method_name, args=[attrs])])
+
+    def input_tag(self, attrs):
+        return Markup(self.form.input_tag(attrs))
+
+    def _check(self, parser, tag):
+        return self._input(parser, tag, method_name='checkbox_tag')
+
+    def checkbox_tag(self, attrs):
+        return Markup(self.form.checkbox_tag(attrs))
+
+    def _radio(self, parser, tag):
+        return self._input(parser, tag, method_name='radio_tag')
+
+    def radio_tag(self, attrs):
+        return Markup(self.form.radio_tag(attrs))
+
+    def _iferror(self, parser, tag):
+        name, attrs = self._parse_attrs(parser)
+        name = name or attrs.get('name', None)
+        if name is None:
+            raise ValueError('First argument of iferror tag must be a string')
+            
+        node = nodes.If()
+        node.test = self.call_method('_iferror_block', args=[name])
+        node.body = parser.parse_statements(('name:else', 'name:endiferror'))
+        token = next(parser.stream)
+        if token.test('name:else'):
+            node.else_ = parser.parse_statements(('name:endiferror',), drop_needle=True)
+        else:
+            node.else_ = []
+
+        return [node]
+
+    def _iferror_block(self, name):
+        return self.form.if_error(name)
+
+    def _ifnoterror(self, parser, tag):
+        name, attrs = self._parse_attrs(parser)
+        name = name or attrs.get('name', None)
+        if name is None:
+            raise ValueError('First argument of ifnoterror tag must be a string')
+            
+        node = nodes.If()
+        node.test = self.call_method('_ifnoterror_block', args=[name])
+        node.body = parser.parse_statements(('name:else', 'name:endifnoterror'))
+        token = next(parser.stream)
+        if token.test('name:else'):
+            node.else_ = parser.parse_statements(('name:endifnoterror',), drop_needle=True)
+        else:
+            node.else_ = []
+
+        return [node]
+
+    def _ifnoterror_block(self, name):
+        return self.form.if_not_error(name)
+
+    def _submit(self, parser, tag):
+        name, attrs = self._parse_attrs(parser)
+        if name is not None:
+            attrs['value'] = name
+
+        attrs['type'] = nodes.Const(tag.value)
+
+        attrs = nodes.Dict([nodes.Pair(nodes.Const(k), v) for k,v in attrs.items()])
+
+        return nodes.Output([self.call_method('input_tag', args=[attrs])])
+
+    def _label(self, parser, tag):
+        name, attrs = self._parse_attrs(parser)
+
+        if name is not None:
+            attrs['name'] = name
+            
+        body = parser.parse_statements(['name:endlabel'], drop_needle=True)
+        attrs = nodes.Dict([nodes.Pair(nodes.Const(k), v) for k,v in attrs.items()])
+
+        return nodes.CallBlock(self.call_method('_label_block', args=[attrs]),
+                               [], [], body).set_lineno(tag.lineno)
+
+
+    def _label_block(self, attrs, caller):
+        return Markup(self.form.label_tag(attrs)) + caller() + Markup(self.form.end_label_tag())
+
+    def _quick_select(self, parser, tag):
+        name, attrs = self._parse_attrs(parser)
+        if name is not None:
+            attrs['name'] = name
+
+        attrs = nodes.Dict([nodes.Pair(nodes.Const(k), v) for k,v in attrs.items()])
+
+        return nodes.Output([
+                self.call_method('quick_select_tag', args=[attrs])])
+
+    def quick_select_tag(self, attrs):
+        return Markup(self.form.quick_select_tag(attrs))
+
+    def _text_area(self, parser, tag):
+        name, attrs = self._parse_attrs(parser)
+        if name is not None:
+            attrs['name'] = name
+
+        attrs = nodes.Dict([nodes.Pair(nodes.Const(k), v) for k,v in attrs.items()])
+        return nodes.Output([
+                self.call_method('text_area_tag', args=[attrs])])
+
+    def text_area_tag(self, attrs):
+        return Markup(self.form.textarea_tag(attrs))
+
+    def _field_error(self, parser):
+        name, attrs = self._parse_attrs(parser)
+        name = name or attrs.get('name', None)
+        if name is None:
+            raise ValueError('First argument of error tag must be a string')
+
+        attrs.setdefault('render', nodes.Const('default'))
+
+        attrs = nodes.Dict([nodes.Pair(nodes.Const(k), v) for k,v in attrs.items()])
+
+        return nodes.Output([
+                self.call_method('field_error_tag', args=[name, attrs])])
+
+    def field_error_tag(self, name, attrs):
+        return Markup(self.form.error_tag(name, attrs, self.environment.error_renderers))
---
+++ packages/formpump/formpump-0.1.8-py2.7.egg/formpump/test/jinjatests.py
@@ -0,0 +1,134 @@
+import jinja2
+import logging
+import re
+import unittest
+
+import base
+import formpump
+
+class JinjaPumpTests(base.FormPumpTests):
+    def setUp(self):
+        self.env = jinja2.Environment(extensions=[formpump.JinjaPump])
+
+    def run_template(self, tpl, **kwargs):
+        return self.env.from_string(tpl).render(**kwargs)
+
+    def set_form_name_key(self, form_name_key):
+        form_name_key, self.env.form_name_key = self.env.form_name_key, form_name_key
+        return form_name_key
+
+    def set_form_ctx_key(self, form_ctx_key):
+        form_ctx_key, self.env.form_ctx_key = self.env.form_ctx_key, form_ctx_key
+        return form_ctx_key
+
+    def add_renderer(self, name, callback):
+        self.env.error_renderers[name] = callback
+
+    def remove_renderer(self, name):
+        self.env.error_renderers.pop(name, None)
+
+class JinjaPumpFormTests(JinjaPumpTests, base.FormTests):
+    def form(self):
+        return '{% form %}ok{% endform %}'
+
+    def form_name(self): 
+        return '{% form "test" %}ok{% endform %}'
+
+    def form_attrs(self):
+        return '{% form "test" action="x" class="y" %}ok{% endform %}'
+
+    def test_form_name_key(self):
+        return '{% form "test" %}ok{% endform %}'
+
+class JinjaPumpInputTests(JinjaPumpTests, base.InputTests):
+    def text(self):
+        return '{% text %}'
+
+    def name(self):
+        return '{% text "test" %}'
+
+    def dynamic_name(self):
+        return '{% text name="t"~var %}'
+
+    def attrs(self):
+        return '{% text "test" class="y" title="x" %}'
+    
+    def email(self):
+        return '{% email "test" %}'
+
+class JinjaPumpFillTests(JinjaPumpTests, base.FillTests):
+    def checkbox_fill(self):
+        return '{% form "test" %}{% checkbox "var_a" %}{% checkbox "var_b" %}{% checkbox "var_c" value="c" %}{% endform %}'
+
+    def email_fill(self):
+        return '{% form "test" %}{% email "var" %}{% endform %}'
+
+    def hidden_fill(self):
+        return '{% form "test" %}{% hidden "var" %}{% endform %}'
+
+    def password_fill(self):
+        return '{% form "test" %}{% password "var" %}{% endform %}'
+
+    def radio_fill(self):
+        return '{% form "test" %}{% radio "var" value="a" %}{% radio "var" value="b" %}{% radio "var" value="c" %}{% endform %}'
+
+    def submit_fill(self):
+        return '{% form "test" %}{% submit "var" %}{% endform %}'
+
+    def text_fill(self):
+        return '{% form "test" %}{% text "var" %}{% endform %}'
+
+    def textarea_fill(self):
+        return '{% form "test" %}{% textarea "var" %}{% endform %}'
+
+class JinjaPumpFormContextTests(JinjaPumpTests, base.FormContextTests):
+    def form_context(self):
+        return '{% form %}{% form_ctx "ctx" %}ok{% endform %}'
+
+    def form_context_key(self):
+        return '{% form "test" %}{% form_ctx "ctx" %}ok{% endform %}'
+
+    def form_context_attrs(self):
+        return '{% form "test" %}{% form_ctx "ctx" var="val" %}ok{% endform %}'
+
+    def form_context_fill(self):
+        return '{% form "test" %}{% form_ctx "ctx" %}{% text "var" %}{% endform %}'
+
+class JinjaPumpLabelTests(JinjaPumpTests, base.LabelTests):
+    def label(self):
+        return '{% form %}{% label name="var" %}ok{% endlabel %}{% endform %}'
+
+    def label_attrs(self):
+        return '{% form %}{% label name="var" var="val" %}ok{% endlabel %}{% endform %}'
+
+    def label_match_forward(self):
+        return '{% form %}{% label name="var" %}ok{% endlabel %}{% text name="var" %}{% endform %}'
+
+    def label_match_back(self):
+        return '{% form %}{% text name="var" %}{% label name="var" %}ok{% endlabel %}{% endform %}'
+
+    def label_match_forward_multi(self):
+        return '{% form %}{% label name="var" %}ok{% endlabel %}{% text name="var" %}{% label name="var" %}ok{% endlabel %}{% text name="var" %}{% endform %}'
+
+    def label_match_back_multi(self):
+        return '{% form %}{% text name="var" %}{% label name="var" %}ok{% endlabel %}{% text name="var" %}{% label name="var" %}ok{% endlabel %}{% endform %}'
+
+class JinjaPumpErrorTests(JinjaPumpTests, base.ErrorTests):
+    def error(self):
+        return '{% form "test" %}{% error "a" %}ok{% endform %}'
+
+    def multi_error(self):
+        return '{% form "test" %}{% error "a" %}ok{% error "b" %}{% endform %}'
+
+    def error_renderer(self):
+        return '{% form "test" %}{% error "a" render="test" %}ok{% endform %}'
+
+if __name__ == "__main__":
+    unittest.main()
+
+loader = unittest.TestLoader()
+suite = unittest.TestSuite()
+suite.addTest(loader.loadTestsFromTestCase(JinjaPumpFillTests))
+suite.addTest(loader.loadTestsFromTestCase(JinjaPumpFormContextTests))
+suite.addTest(loader.loadTestsFromTestCase(JinjaPumpFormTests))
+suite.addTest(loader.loadTestsFromTestCase(JinjaPumpInputTests))
