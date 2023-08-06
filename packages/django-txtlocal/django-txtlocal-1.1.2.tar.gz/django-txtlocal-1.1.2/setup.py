4a898454964138d8d28f249ff5b07e24a4763a4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:00 +0000

    Add package django-txtlocal/1.1.2/django-txtlocal-1.1.2.tar.gz

---
+++ packages/django-txtlocal/django-txtlocal-1.1.2.tar.gz/django-txtlocal-1.1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='django-txtlocal',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['requests>=1.2.3'],
+    version='1.1.2',
+    description='App for sending and receiving SMS messages via http://www.textlocal.com',
+    long_description=open('README.rst').read(),
+    author='Incuna Ltd',
+    author_email='admin@incuna.com',
+    url='https://github.com/incuna/django-txtlocal/',
+)
