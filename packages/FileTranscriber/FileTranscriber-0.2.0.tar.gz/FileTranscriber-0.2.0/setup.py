9c4fa4a14b20749c8b4adcae348dce888f7f11f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:34 +0000

    Add package FileTranscriber/0.2.0/FileTranscriber-0.2.0.tar.gz

---
+++ packages/FileTranscriber/FileTranscriber-0.2.0.tar.gz/FileTranscriber-0.2.0/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+import sys
+import os
+
+def long_description():
+    rfname = os.path.join(os.path.dirname(__file__), 'README.md')
+    with open(rfname, 'r') as readme:
+        readme_text = readme.read()
+    return(readme_text)
+
+setup(name='FileTranscriber',
+      version='0.2.0',
+      description='''A small utility that simulates user typing to aid \
+file transcription in limited environments''',
+      long_description=long_description(),
+      author='Paul Barton',
+      author_email='pablo.barton@gmail.com',
+      url='https://github.com/SavinaRoja/FileTranscriber',
+      install_requires = ['docopt', 'PyUserInput'],
+      license='http://opensource.org/licenses/MIT',
+      scripts=['transcribe'])
