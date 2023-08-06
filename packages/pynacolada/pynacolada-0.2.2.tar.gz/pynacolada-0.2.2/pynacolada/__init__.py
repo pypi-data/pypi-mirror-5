335e77f6a8fbf210d2df9823084b48d0873d685a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:49 +0000

    Add package pynacolada/0.2.2/pynacolada-0.2.2.tar.gz

---
+++ packages/pynacolada/pynacolada-0.2.2.tar.gz/pynacolada-0.2.2/pynacolada/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# Filename: __init__.py
+
+# process procedures
+from netcdf import *
+from pynacolada import *
+from ncdfconv import *
+from ncdfproc import *
+from ncdfextract import *
