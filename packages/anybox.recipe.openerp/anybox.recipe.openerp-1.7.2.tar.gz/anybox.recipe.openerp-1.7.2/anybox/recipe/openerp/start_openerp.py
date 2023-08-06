302396294c062d94e83e0ee9569f4e3eced802e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:16 +0000

    Add package anybox.recipe.openerp/1.7.2/anybox.recipe.openerp-1.7.2.tar.gz

---
+++ packages/anybox.recipe.openerp/anybox.recipe.openerp-1.7.2.tar.gz/anybox.recipe.openerp-1.7.2/anybox/recipe/openerp/start_openerp.py
@@ -0,0 +1,39 @@
+import sys
+import os
+from . import patch_openerp_v5
+
+
+def insert_args(arguments):
+    for i, a in enumerate(arguments):
+        sys.argv.insert(i+1, a)
+
+
+def main(starter, conf, version=None, just_test=False):
+    arguments = ['-c', conf]
+
+    if just_test:
+        arguments.extend(('--log-level',
+                          'test' if version >= (6, 0) else 'info',
+                          '--stop-after-init'))
+
+        if version >= (7, 0):
+            arguments.append('--test-enable')
+
+    insert_args(arguments)
+
+    if version == (5, 0):
+        patch_openerp_v5.do_patch()
+
+    os.chdir(os.path.split(starter)[0])
+    glob = globals()
+    glob['__name__'] = '__main__'
+    glob['__file__'] = starter
+    try:
+        execfile(starter, globals())
+    except SystemExit as exc:
+        if version == (5, 0):
+            # Without Agent.quit() the Timer threads may go on forever
+            # and the script will never stop
+            import netsvc
+            netsvc.Agent.quit()
+        return exc.code
