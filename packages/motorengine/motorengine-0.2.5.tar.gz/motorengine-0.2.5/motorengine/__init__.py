57f243a82334b8b46e5188015a59e2e88d7b20ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:39 +0000

    Add package motorengine/0.2.5/motorengine-0.2.5.tar.gz

---
+++ packages/motorengine/motorengine-0.2.5.tar.gz/motorengine-0.2.5/motorengine/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__version__ = '0.2.5'
+
+try:
+    from pymongo import ASCENDING, DESCENDING  # NOQA
+
+    from motorengine.connection import connect, disconnect  # NOQA
+    from motorengine.document import Document  # NOQA
+
+    from motorengine.fields import (  # NOQA
+        BaseField, StringField, BooleanField, DateTimeField,
+        UUIDField, ListField, EmbeddedDocumentField, ReferenceField, URLField,
+        EmailField, IntField, FloatField, DecimalField, BinaryField,
+        JsonField
+    )
+except ImportError:  # NOQA
+    pass  # likely setup.py trying to import version
