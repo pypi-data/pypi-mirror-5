5455de5ad8f6d4ebda70230b7208223e51bff098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:07 +0000

    Add package stormssh/0.4.7/stormssh-0.4.7.tar.gz

---
+++ packages/stormssh/stormssh-0.4.7.tar.gz/stormssh-0.4.7/setup.py
@@ -0,0 +1,21 @@
+import sys
+
+from distutils.core import setup
+
+setup(
+    name='stormssh',
+    version='0.4.7',
+    packages=['storm'],
+    url='http://github.com/emre/storm',
+    license='MIT',
+    author='Emre Yilmaz',
+    author_email='mail@emreyilmaz.me',
+    description='Management commands to ssh config files.',
+    scripts=[
+        'storm/bin/storm'
+    ],
+    install_requires=list(filter(None, [
+        "paramiko",
+        "termcolor",
+        "argparse" if sys.version_info[:2] < (2, 7) else None,
+    ])),)
