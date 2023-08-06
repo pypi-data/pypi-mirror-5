3a766304661f3f26baff70bb9bdd34f5e9971a44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:10 +0000

    Add package airbrake-flask/0.0.3/airbrake-flask-0.0.3.tar.gz

---
+++ packages/airbrake-flask/airbrake-flask-0.0.3.tar.gz/airbrake-flask-0.0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='airbrake-flask',
+    version='0.0.3',
+    author='Kien Pham, Kane Kim',
+    author_email='kien@sendgrid.com',
+    py_modules=['airbrake'],
+    url='https://github.com/kienpham2000/airbrake-flask',
+    license='LICENSE.txt',
+    description='Airbrake client for Python Flask',
+    long_description='Airbrake client for Python Flask',
+    )
