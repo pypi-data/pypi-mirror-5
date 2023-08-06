08a9434cb2abd5932e226474747499ad5b8433cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:00 +0000

    Add package transliterate/0.3/transliterate-0.3.tar.gz

---
+++ packages/transliterate/transliterate-0.3.tar.gz/transliterate-0.3/src/transliterate/contrib/languages/ru/translit_language_pack.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from transliterate.base import TranslitLanguagePack, registry
+
+class RussianLanguagePack(TranslitLanguagePack):
+    """
+    Language pack for Russian language. See http://en.wikipedia.org/wiki/Russian_alphabet for details.
+    """
+    language_code = "ru"
+    language_name = "Russian"
+    character_ranges = ((0x0400, 0x04FF), (0x0500, 0x052F))
+    mapping = (
+        u"abvgdeezijklmnoprstufh'y'eABVGDEEZIJKLMNOPRSTUFH'Y'E",
+        u"абвгдеёзийклмнопрстуфхъыьэАБВГДЕЁЗИЙКЛМНОПРСТУФХЪЫЬЭ",
+    )
+    pre_processor_mapping = {
+        u"zh": u"ж",
+        u"ts": u"ц",
+        u"ch": u"ч",
+        u"sh": u"ш",
+        u"sch": u"щ",
+        u"ju": u"ю",
+        u"ja": u"я",
+        u"Zh": u"Ж",
+        u"Ts": u"Ц",
+        u"Ch": u"Ч",
+        u"Sch": u"Щ",
+        u"Ju": u"Ю",
+        u"Ja": u"Я"
+    }
+
+registry.register(RussianLanguagePack)
