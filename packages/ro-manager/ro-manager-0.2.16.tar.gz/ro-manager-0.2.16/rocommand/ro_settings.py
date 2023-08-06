5abdf5f0f507dad69b9cae17bf780ca1d4399c1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:24 +0000

    Add package ro-manager/0.2.16/ro-manager-0.2.16.tar.gz

---
+++ packages/ro-manager/ro-manager-0.2.16.tar.gz/ro-manager-0.2.16/rocommand/ro_settings.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+"""
+RO manager parameter settings
+"""
+
+import __init__
+VERSION_NUM   = __init__.__version__
+
+RO_VERSION    = "v"+VERSION_NUM
+VERSION       = RO_VERSION+" (RODL)"
+MANIFEST_DIR  = ".ro"
+MANIFEST_FILE = "manifest.rdf"
+REGISTRIES_FILE = ".registries.json"
+
+# End.
