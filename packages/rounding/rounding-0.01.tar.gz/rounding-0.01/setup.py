1c2b5db1a02d08dbd0aa3a3bf1b861905506d988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:54 +0000

    Add package rounding/0.01/rounding-0.01.tar.gz

---
+++ packages/rounding/rounding-0.01.tar.gz/rounding-0.01/setup.py
@@ -0,0 +1,24 @@
+'''
+Created on Sep 22, 2013
+
+@author: dmaust
+'''
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Stochastic Rounding',
+    'author': 'David Maust',
+    'url': 'https://github.com/dmaust/rounding',
+    'author_email': 'david@dmaust.net',
+    'version': '0.01',
+    'install_requires': ['nose'],
+    'packages': ['rounding'],
+    'scripts': [],
+    'name': 'rounding'
+}
+
+setup(**config)
