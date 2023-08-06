1bf054a7720dd84edd89b547d9b7f5bcc7f89ddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:29 +0000

    Add package collective.responsivetheme/1.5.1/collective.responsivetheme-1.5.1.tar.gz

---
+++ packages/collective.responsivetheme/collective.responsivetheme-1.5.1.tar.gz/collective.responsivetheme-1.5.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.5.1'
+
+setup(name='collective.responsivetheme',
+      version=version,
+      description="A responsive theme based on sunburst for Plone 4",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Rob Porter',
+      author_email='robzonenet@gmail.com',
+      url='http://github.com/RobZoneNet/collective.responsivetheme',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plonetheme.sunburst',
+          'z3c.jbot>=0.6.0',
+          'jarn.jsi18n',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
