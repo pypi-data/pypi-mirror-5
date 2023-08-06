98fbaa629da6c06d38ddc59750b665effdb728ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:09 +0000

    Add package SQLAlchemy-Searchable/0.3.3/SQLAlchemy-Searchable-0.3.3.tar.gz

---
+++ packages/SQLAlchemy-Searchable/SQLAlchemy-Searchable-0.3.3.tar.gz/SQLAlchemy-Searchable-0.3.3/setup.py
@@ -0,0 +1,57 @@
+"""
+SQLAlchemy-Searchable
+---------------------
+
+Provides fulltext search capabilities for declarative SQLAlchemy models.
+"""
+
+from setuptools import setup, Command
+import subprocess
+
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        errno = subprocess.call(['py.test'])
+        raise SystemExit(errno)
+
+setup(
+    name='SQLAlchemy-Searchable',
+    version='0.3.3',
+    url='https://github.com/kvesteri/sqlalchemy-searchable',
+    license='BSD',
+    author='Konsta Vesterinen',
+    author_email='konsta@fastmonkeys.com',
+    description=(
+        'Provides fulltext search capabilities for declarative SQLAlchemy'
+        ' models.'
+    ),
+    long_description=__doc__,
+    packages=['sqlalchemy_searchable'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'pyparsing>=2.0.1',
+        'SQLAlchemy>=0.7.8',
+        'psycopg2>=2.4.6',
+        'SQLAlchemy-Utils>=0.16.2'
+    ],
+    cmdclass={'test': PyTest},
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
