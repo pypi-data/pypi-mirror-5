7bae34f2ee46e0fff8361f810ce61d59e2fd4fed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:06 +0000

    Add package Flask-NYC/0.1.0/Flask-NYC-0.1.0.tar.gz

---
+++ packages/Flask-NYC/Flask-NYC-0.1.0.tar.gz/Flask-NYC-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='Flask-NYC',
+    version='0.1.0',
+    description='New York Flask Meetup',
+    author='Andy Dirnberger',
+    author_email='dirn@dirnonline.com',
+    url='http://flask-nyc.org',
+    py_modules=['flask_nyc'],
+)
