dff27f6b04b74d4f2e1d3c0c5451aaeafd76fb3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:17 +0000

    Add package s3upload/0.2.1/s3upload-0.2.1.tar.gz

---
+++ packages/s3upload/s3upload-0.2.1.tar.gz/s3upload-0.2.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = 's3upload',
+    version = '0.2.1',
+    py_modules = ['s3upload'],
+    author = 'Lee Treveil',
+    author_email = 'leetreveil@gmail.com',
+    description = 'A python module for uploading data to s3 using the s3 multipart API',
+)
