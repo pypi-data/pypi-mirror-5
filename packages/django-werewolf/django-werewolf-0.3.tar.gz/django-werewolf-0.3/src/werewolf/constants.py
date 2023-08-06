405f2268213e0a99ef538162fe1682f5b07a5c46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:49 +0000

    Add package django-werewolf/0.3/django-werewolf-0.3.tar.gz

---
+++ packages/django-werewolf/django-werewolf-0.3.tar.gz/django-werewolf-0.3/src/werewolf/constants.py
@@ -0,0 +1,9 @@
+__title__ = 'werewolf.constants'
+__version__ = '0.3'
+__build__ = 0x000003
+__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
+__all__ = ('CHANGE_STATUS_TO', 'CAN_VIEW_STATUS')
+
+# Don't touch this!
+CHANGE_STATUS_TO = 'change_status_to'
+CAN_VIEW_STATUS = 'can_view_status'
