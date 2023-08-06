097ddc3e7f13ac1fbf994a35d1b691146da6bbb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:46 +0000

    Add package pillowfluff/0.0.3/pillowfluff-0.0.3.tar.gz

---
+++ packages/pillowfluff/pillowfluff-0.0.3.tar.gz/pillowfluff-0.0.3/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+import sys
+
+setup(
+    name='pillowfluff',
+    version='0.0.3',
+    description='Map over CouchDB changes feed built to run on Pillowtop',
+    author='Dimagi',
+    author_email='information@dimagi.com',
+    url='http://www.dimagi.com/',
+    packages=['fluff','fluff.fluff_filter'],
+    include_package_data=True,
+    test_suite='tests',
+    test_loader='unittest2:TestLoader',
+    install_requires=[
+        'jsonobject-couchdbkit>=0.6.5.2',
+        'pillowtop>=0.1.2',
+        'dimagi-utils>=1.0.2',
+        'pytz',
+    ],
+    tests_require=[
+        'django',
+        'unittest2',
+        'fakecouch>=0.0.3'
+    ]
+)
