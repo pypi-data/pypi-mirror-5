0ec3d46df3f48a0cfd0006747692bc881df95f89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:51 +0000

    Add package xbcfg/0.5/xbcfg-0.5.tar.gz

---
+++ packages/xbcfg/xbcfg-0.5.tar.gz/xbcfg-0.5/setup.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+
+import os
+import codecs
+from distutils.core import setup
+
+README =  codecs.open(
+    os.path.join(os.path.dirname(__file__), "README.rst"),
+    encoding="utf-8" ).read()
+
+setup(
+    name='xbcfg',
+    version='0.5',
+    author=u'Lauri Võsandi',
+    author_email='lauri.vosandi@gmail.com',
+    packages=['xbcfg'],
+    scripts=['bin/xbcfg'],
+    url='http://bitbucket.org/lauri.vosandi/xbcfg',
+    license='LICENSE',
+    description='Yet another X-CTU replacement',
+    long_description=README,
+    install_requires=[
+        "crc16",
+    ],
+)
