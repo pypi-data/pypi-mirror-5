35ec34f78498e7b74a8a202f0b80d6f1196dfdae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:42 +0000

    Add package DerpTime/0.1/DerpTime-0.1.tar.gz

---
+++ packages/DerpTime/DerpTime-0.1.tar.gz/DerpTime-0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='DerpTime',
+    version='0.1',
+    author="Tamas Leposa",
+    author_email="l@pulilab.com",
+    packages=['derptime',],
+    license='BSD 3-Clause License, see the LICENSE file for details',
+    long_description=open('README.txt').read(),
+)
