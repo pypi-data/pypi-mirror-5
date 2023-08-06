4f0d1d55bc00dafe82ee49f91970dfac5051a4b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:28 +0000

    Add package Github-url-converter/0.1.1/Github-url-converter-0.1.1.tar.gz

---
+++ packages/Github-url-converter/Github-url-converter-0.1.1.tar.gz/Github-url-converter-0.1.1/setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+
+setup(
+    name = "Github-url-converter",
+    version = "0.1.1",
+    author = "Ahmed Azaan",
+    author_email = "azaan@outlook.com",
+    description = ("Quick and easy way to convert Github remote url's from HTTPS"
+        "to SSH or vice versa"),
+    license = "MIT",
+    keywords = "github convert url",
+    url = "https://github.com/aeonaxan/github-url-converter",
+    packages=["src",],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+    entry_points={
+        "console_scripts": [
+            "github-url-converter = src.cli:main",
+        ],
+    }
+)
