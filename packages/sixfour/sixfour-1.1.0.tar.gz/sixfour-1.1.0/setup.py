9f97222d2e5c34bd88aabc1fc325e59f1de158b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:22 +0000

    Add package sixfour/1.1.0/sixfour-1.1.0.tar.gz

---
+++ packages/sixfour/sixfour-1.1.0.tar.gz/sixfour-1.1.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(name='sixfour',
+      version='1.1.0',
+      description='base64 Image Encoder and Embedder',
+      author='Christopher Simpkins',
+      author_email='chris@zerolabs.net',
+      url='https://github.com/chrissimpkins/six-four',
+      platforms=['any'],
+      py_modules=['sixfour'],
+      scripts=['sixfour']
+      )
