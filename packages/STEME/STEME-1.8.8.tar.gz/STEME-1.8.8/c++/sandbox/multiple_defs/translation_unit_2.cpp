d22fbe43ed0f7a5f663c2f1083b07732cf2dfffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:49 +0000

    Add package STEME/1.8.8/STEME-1.8.8.tar.gz

---
+++ packages/STEME/STEME-1.8.8.tar.gz/STEME-1.8.8/c++/sandbox/multiple_defs/translation_unit_2.cpp
@@ -0,0 +1,1 @@
+#include <seqan.h>
---
+++ packages/STEME/STEME-1.8.8.tar.gz/STEME-1.8.8/python/stempy/test/test_run_steme.py
@@ -0,0 +1,26 @@
+#
+# Copyright John Reid 2011
+#
+
+"""
+Test STEME on small-ish FASTA file.
+"""
+
+from setup_environment import init_test_env, update_path_for_stempy, logging
+init_test_env(__file__, level=logging.INFO)
+update_path_for_stempy()
+
+import os, sys, stempy
+
+
+#
+# Run STEME
+#
+options = stempy.get_default_options()
+options.output_dir = os.path.join('output', 'test-run-steme')
+options.min_w = options.max_w = 8
+options.write_em_stats = True
+options.meme_like_output = 'meme.out'
+algorithm = stempy.Algorithm(options)
+fasta = os.path.join(os.path.dirname(__file__), 'fasta', 'T00759-tiny.fa')
+algorithm(fasta.encode(sys.stdin.encoding or 'ascii'))
