76e83a0179c7255fe138e32a8d72ed056917e4d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:49 +0000

    Add package pyramid_restlogging/0.2.37/pyramid_restlogging-0.2.37.tar.gz

---
+++ packages/pyramid_restlogging/pyramid_restlogging-0.2.37.tar.gz/pyramid_restlogging-0.2.37/pyramid_restlogging/__init__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from pyramid.config import Configurator
+
+log = logging.getLogger(__name__)
+
+
+def includeme(config):
+    log.debug('Including pyramid_restlogging')
+
+    config.add_route(
+        'clientlogs',
+        '/clientlogs/{logger_name:[\d\w\.:]+}/'
+        '{log_level:debug|info|warning|error|critical}',
+        request_method='POST',
+        )
+    config.add_route(
+        'clientlogs_batch',
+        '/clientlogs/batch',
+        request_method='POST',
+        )
+    config.scan('pyramid_restlogging.views')
+    config.commit()
+
+
+def main(global_config, **settings):
+    # global_config is not used here,  see Paste doc
+    config = Configurator(settings=settings)
+    config.include(includeme)
+    return config.make_wsgi_app()
