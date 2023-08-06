479748cd82d59c56f6d125bf32697805f17fa13e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:39 +0000

    Add package infi.storagemodel/0.1.61/infi.storagemodel-0.1.61-py2.7.egg

---
+++ packages/infi.storagemodel/infi.storagemodel-0.1.61-py2.7.egg/infi/storagemodel/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.61"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '7a1e77306877d34ce5b97163ad31c1e06f846bc2'
+__git_head_subject__ = 'Finished release v0.1.61.'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
