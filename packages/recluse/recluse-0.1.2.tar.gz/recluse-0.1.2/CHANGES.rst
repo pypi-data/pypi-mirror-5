50c29eeeb37d153923b9abf4cd09e0219da648ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:33 +0000

    Add package recluse/0.1.2/recluse-0.1.2.tar.gz

---
+++ packages/recluse/recluse-0.1.2.tar.gz/recluse-0.1.2/CHANGES.rst
@@ -0,0 +1,19 @@
+=========
+RECLUSE
+=========
+
+
+Release 0.1.2 (2013-09-14)
+..........................
+* Added the nltk_based_segmenter_tokeniser.
+
+
+Release 0.1.1 (2013-09-10)
+..........................
+* Added a test of the commandline functionality of article_randomizer.
+* Added utils.py with the open_with_unicode function.
+
+
+Release 0.1.0 (2013-09-05)
+..........................
+Initial release.
---
+++ packages/recluse/recluse-0.1.2.tar.gz/recluse-0.1.2/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='recluse',
+      version='0.1.2',
+      description='Reproducible Experimentation for Computational Linguistics Use',
+      long_description=open('README.rst').read(),
+      author='L. Amber Wilcox-O\'Hearn',
+      author_email='amber@cs.toronto.edu',
+      url='https://github.com/lamber/recluse',
+      packages=['code', 'code.test'],
+      license='COPYING',
+     )
