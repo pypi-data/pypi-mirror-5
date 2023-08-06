1669c5a087e4b1a6308627a3fed9716d1c531c75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:39 +0000

    Add package django-pgfields/1.1.0/django-pgfields-1.1.0.tar.gz

---
+++ packages/django-pgfields/django-pgfields-1.1.0.tar.gz/django-pgfields-1.1.0/django_pg/utils/gis.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import, unicode_literals
+from django.db import connections
+from django.db.utils import DEFAULT_DB_ALIAS
+
+
+gis_backend = '.gis.' in connections[DEFAULT_DB_ALIAS].__module__
