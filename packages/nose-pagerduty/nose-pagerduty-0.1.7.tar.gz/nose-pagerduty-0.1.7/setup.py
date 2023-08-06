ef83c2b7505ad82165e3e062f163da2dc8092bfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:02 +0000

    Add package nose-pagerduty/0.1.7/nose-pagerduty-0.1.7.tar.gz

---
+++ packages/nose-pagerduty/nose-pagerduty-0.1.7.tar.gz/nose-pagerduty-0.1.7/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='nose-pagerduty',
+    install_requires=['setuptools', 'nose>=1.3.0', 'requests>=2.0.1'],
+    entry_points={
+        'nose.plugins.0.10': [
+            'pagerduty = nose_pagerduty.NosePagerDuty:NosePagerDutyPlugin'
+        ]
+    },
+    version='0.1.7',
+    description='PagerDuty alert plugin for nose',
+    long_description='Simple plugin to trigger PagerDuty alerts when one of your nose tests fail.',
+    author='Luca Martinetti',
+    author_email='luca@luca.io',
+    url='https://github.com/lucamartinetti/nose-pagerduty',
+    license='BSD',
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Testing",
+    ],
+    keywords='test nosetests nose nosetest plugin pagerduty alert',
+    packages=['nose_pagerduty'],
+    package_dir={'': 'src'},
+)
---
+++ packages/nose-pagerduty/nose-pagerduty-0.1.7.tar.gz/nose-pagerduty-0.1.7/src/nose_pagerduty/NosePagerDuty.py
@@ -0,0 +1,69 @@
+import logging
+import requests
+import json
+from nose.plugins import Plugin
+from nose.plugins.attrib import get_method_attr
+
+log = logging.getLogger('nose.plugins.pagerduty')
+
+
+def trigger_pagerduty(description, service_key):
+    pagerduty_url = "https://events.pagerduty.com/generic/2010-04-15/create_event.json"
+
+    data = {"description": description,
+            "event_type": "trigger",
+            "service_key": service_key}
+
+    resp = requests.post(pagerduty_url,
+                         headers={
+                             "Content-Type": "application/json; charset=utf-8"},
+                         data=json.dumps(data))
+    resp.raise_for_status()
+
+    return resp.json()
+
+
+class NosePagerDutyPlugin(Plugin):
+    name = 'pagerduty'
+
+    def options(self, parser, env):
+        super(NosePagerDutyPlugin, self).options(parser, env)
+
+    def configure(self, options, conf):
+        super(NosePagerDutyPlugin, self).configure(options, conf)
+        if not self.enabled:
+            return
+
+    def handle_alert(self, test, err):
+        is_on, service_key = self.get_attribs(test.test.test)
+        if is_on:
+            _, test_ex, _ = err
+            try:
+                description = "{test} - {test_ex}".format(test=test,
+                                                          test_ex=test_ex)
+                log.warn(
+                    "Triggering PagerDuty alert with description: "
+                    "\"{description}\" service key: \"{service_key}\"".format(
+                        description=description,
+                        service_key=service_key))
+
+                ret = trigger_pagerduty(description, service_key)
+
+                log.warn(
+                    "Alert triggered with incident key: {incident_key}".format(
+                        incident_key=ret[u'incident_key']))
+            except Exception as ex:
+                log.error(ex)
+        return False
+
+    def handleError(self, test, err):
+        return self.handle_alert(test, err)
+
+    def handleFailure(self, test, err):
+        return self.handle_alert(test, err)
+
+    @staticmethod
+    def get_attribs(function, cls=None):
+        is_on = get_method_attr(function, cls, 'trigger_pagerduty')
+        service_key = get_method_attr(function, cls, 'service_key', None)
+        return is_on, service_key
