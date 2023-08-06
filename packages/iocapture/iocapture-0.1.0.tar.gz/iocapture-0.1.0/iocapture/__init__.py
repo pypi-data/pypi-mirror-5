faa7aad01e5edb9b6c3521c1debdb80f60abc2d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:43 +0000

    Add package iocapture/0.1.0/iocapture-0.1.0.tar.gz

---
+++ packages/iocapture/iocapture-0.1.0.tar.gz/iocapture-0.1.0/iocapture/__init__.py
@@ -0,0 +1,94 @@
+import sys
+
+if sys.version_info > (3, 0):
+    from io import StringIO
+else:
+    from cStringIO import StringIO
+
+__author__ = "Kazuhiro Oinuma"
+__author_email__ = "oinume@gmail.com"
+__copyright__ = "2013"
+__license__ = "MIT"
+__version__ = "0.1.0"
+__maintainer__ = "Kazuhiro Oinuma"
+__status__ = "Production/Stable"
+
+def capture(stdout=True, stderr=True):
+    """
+    Return IOCapture object.
+
+    stdout: A flag of capturing stdout
+    stderr: A flag of capturing stderr
+    """
+    return IOCapture(stdout, stderr)
+
+class IOCapture(object):
+    """"""
+
+    def __init__(self, stdout = True, stderr = True):
+        """
+        Constructor.
+
+        stdout: A flag of capturing stdout
+        stderr: A flag of capturing stderr
+        """
+        self._stdout = None
+        self._stderr = None
+        if stdout:
+            self._stdout = StringIO()
+        if stderr:
+            self._stderr = StringIO()
+
+    def start(self):
+        """
+        Start capturing.
+        """
+        if self._stdout:
+            sys.stdout = self._stdout
+        if self._stderr:
+            sys.stderr = self._stderr
+        return self
+
+    def stop(self):
+        """
+        Stop capturing.
+        """
+        if self._stdout:
+            sys.stdout = sys.__stdout__
+        if self._stderr:
+            sys.stderr = sys.__stderr__
+        return self
+
+    @property
+    def stdout(self):
+        """
+        Return value of captured stdout
+        """
+        self._stdout.flush()
+        return self._stdout.getvalue()
+
+    @property
+    def stderr(self):
+        """
+        Return value of captured stderr
+        """
+        self._stderr.flush()
+        return self._stderr.getvalue()
+
+    def close(self):
+        """
+        Close buffers.
+        """
+        if self._stdout:
+            self._stdout.close()
+        if self._stderr:
+            self._stderr.close()
+        return self
+
+    def __enter__(self):
+        return self.start()
+
+    def __exit__(self, type, value, traceback):
+        self.stop().close()
+
+__all__ = [ capture, IOCapture ]
---
+++ packages/iocapture/iocapture-0.1.0.tar.gz/iocapture-0.1.0/setup.py
@@ -0,0 +1,60 @@
+import os
+import sys
+from iocapture import (
+    __author__,
+    __author_email__,
+    __status__,
+    __version__
+)
+
+def get_long_description():
+    return open("README.rst").read()
+
+try:
+    from setuptools import setup
+    setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.version_info < (2, 4):
+    print("iocapture requires at least Python 2.4.")
+    sys.exit(1)
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist upload")
+    sys.exit()
+
+tests_require = []
+for package in open("requirements-dev.txt").read().split("\n"):
+    if package.strip():
+        tests_require.append(package.strip())
+
+setup(
+    name = "iocapture",
+    version = __version__,
+    url = "https://github.com/oinume/iocapture",
+    license = "MIT",
+    author = __author__,
+    author_email = __author_email__,
+    description = "Capture stdout, stderr easily.",
+    long_description = get_long_description(),
+    packages = [ "iocapture" ],
+    zip_safe = False,
+    platforms = "unix",
+    install_requires = [],
+    tests_require = tests_require,
+    classifiers = [
+        "Development Status :: 5 - " + __status__,
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.4",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
