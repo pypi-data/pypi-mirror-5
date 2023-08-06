365e25163cded82aa80f07320d70f587db6cd265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:40 +0000

    Add package riakasaurus/1.1.1/riakasaurus-1.1.1.tar.gz

---
+++ packages/riakasaurus/riakasaurus-1.1.1.tar.gz/riakasaurus-1.1.1/riakasaurus/riak.py
@@ -0,0 +1,9 @@
+"""Mostly a compatibility module"""
+
+from riakasaurus import riak_object, mapreduce, bucket, client
+
+RiakClient = client.RiakClient
+RiakBucket = bucket.RiakBucket
+RiakObject = riak_object.RiakObject
+RiakMapReduce = mapreduce.RiakMapReduce
+RiakLink = mapreduce.RiakLink
