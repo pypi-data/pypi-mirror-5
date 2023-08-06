50ff769cdc7fe345949bf239510235b6b005e41c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:07 +0000

    Add package blanc-basic-events/0.2.7/blanc-basic-events-0.2.7.tar.gz

---
+++ packages/blanc-basic-events/blanc-basic-events-0.2.7.tar.gz/blanc-basic-events-0.2.7/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='blanc-basic-events',
+    version='0.2.7',
+    description='Blanc Basic Events for Django',
+    long_description=open('README.rst').read(),
+    url='http://www.blanctools.com/',
+    maintainer='Alex Tomkins',
+    maintainer_email='alex@hawkz.com',
+    platforms=['any'],
+    install_requires=[
+        'vobject>=0.8.1c',
+    ],
+    packages=[
+        'blanc_basic_events',
+        'blanc_basic_events.events',
+        'blanc_basic_events.events.templatetags',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    license='BSD-2',
+)
