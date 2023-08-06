5f7972ee66514a403e39c42393b5382ad1596c0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:43 +0000

    Add package pyconomic/0.1.2/pyconomic-0.1.2.tar.gz

---
+++ packages/pyconomic/pyconomic-0.1.2.tar.gz/pyconomic-0.1.2/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import os
+
+from distutils.core import setup
+
+docs = ''
+if os.path.exists('README.md'):
+    with open('README.md') as f:
+        docs = f.read()
+
+setup(
+    name='pyconomic',
+    version='0.1.2',
+    description='Abstraction for e-conomic.com API',
+    author='Mikkel Jans',
+    author_email='mikkeljans@gmail.com',
+    url='https://github.com/mikkeljans/pyconomic',
+    packages=['pyconomic'],
+    long_description=docs
+)
