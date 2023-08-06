8bf5624716d28210e6402004abd17a6cf3cdf85d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:13 +0000

    Add package simplecalculator/0.0.4/simplecalculator-0.0.4.tar.gz

---
+++ packages/simplecalculator/simplecalculator-0.0.4.tar.gz/simplecalculator-0.0.4/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='simplecalculator',
+      description='A simple calculator library.',
+      author='Jacek Artymiak',
+      author_email='jacek@artymiak.com',
+      url='https://github.com/badmetacoder/calculator',
+      version='0.0.4',
+      py_modules=['calculator.simple'],
+      )
