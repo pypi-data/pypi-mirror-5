478ea95d4f7dd6e1e107afead52bcf6e7fb21f31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:21 +0000

    Add package d3ploy/2.0.5/d3ploy-2.0.5.tar.gz

---
+++ packages/d3ploy/d3ploy-2.0.5.tar.gz/d3ploy-2.0.5/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name                    =   'd3ploy',
+    version                 =   '2.0.5',
+    description             =   'Script for uploading files to S3 with multiple environment support.',
+    long_description        =   'See http://d3ploy.com/ for more information.',
+    author                  =   'Daniel Ryan',
+    author_email            =   'dryan@dryan.com',
+    url                     =   'http://d3ploy.com/',
+    license                 =   'MIT License',
+    packages                =   find_packages(exclude=["d","3","p","l","o","y"]),
+    zip_safe                =   True,
+    include_package_data    =   True,
+    install_requires        =   "boto",
+    entry_points            =   """[console_scripts]
+d3ploy = d3ploy.d3ploy:main
+"""
+)
