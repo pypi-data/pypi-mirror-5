6a5608f733232c18e12b0c9a1e8e61a3b99df354
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:27 +0000

    Add package replmon/0.0.4/replmon-0.0.4.tar.gz

---
+++ packages/replmon/replmon-0.0.4.tar.gz/replmon-0.0.4/setup.py
@@ -0,0 +1,22 @@
+#coding:utf-8
+from setuptools import setup
+
+setup(
+    name="replmon",
+    version="0.0.4",
+    packages=["replmon"],
+    url="https://github.com/krallin/replmon",
+    license="Apache 2.0",
+    author="Thomas Orozco",
+    author_email="thomas@scalr.com",
+    install_requires=["python-daemon", "pymysql", "lockfile", "argparse", "procname"],
+    entry_points={
+        'console_scripts': [
+            'replmon = replmon.cli:main',
+            ],
+        },
+    setup_requires=["nose"],
+    tests_require=["tox", "nose"],
+    description="A simple MySQL replication monitor",
+    long_description=open("README.rst").read(),
+)
