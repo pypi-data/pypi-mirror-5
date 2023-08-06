ad470d39930d8206364bdabd2dac9299ce1bb198
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:37 +0000

    Add package tempdga/1.0.1/tempdga-1.0.1.zip

---
+++ packages/tempdga/tempdga-1.0.1.zip/tempdga-1.0.1/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(name='tempdga',
+      version='1.0.1',      
+      packages=['tempdga'],      
+      author = 'Douglas G. Allen',
+      author_email = 'kb9agt@gmail.com',
+      url = 'http://douglasallen.github.io/gh-pages/aboutme2/',
+      description = 'Converts Fahrenheit degrees to Celcuis degrees and Kelvin',
+      classifiers = [
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Education',
+    ],
+      )
