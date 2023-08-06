8b5a8b3cedc02528df2cc3557f9643fbc6fd9631
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:49 +0000

    Add package requests-auth-mashery/0.0.4/requests-auth-mashery-0.0.4.tar.gz

---
+++ packages/requests-auth-mashery/requests-auth-mashery-0.0.4.tar.gz/requests-auth-mashery-0.0.4/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+
+setup(
+    version='0.0.4',
+    name='requests-auth-mashery',
+    description='Mashery authentication for requests',
+    long_description=readme,
+    url='https://github.com/dasevilla/requests-auth-mashery',
+    author='Devin Sevilla',
+    author_email='dasevilla@gmail.com',
+    install_requires=['requests>=1.2.3'],
+    py_modules=['requests_auth_mashery'],
+    classifiers=[
+    ]
+)
