da9d520b1cbcd57c88db6722ae2eb8eae25ec319
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:14 +0000

    Add package melbdjango-joke/0.3/melbdjango-joke-0.3.tar.gz

---
+++ packages/melbdjango-joke/melbdjango-joke-0.3.tar.gz/melbdjango-joke-0.3/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+import multiprocessing
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='melbdjango-joke',
+      version='0.3',
+      description='Lulz',
+      long_description=readme(),
+      classifiers = [
+          'Development Status :: 1 - Planning',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License'
+      ],
+      keywords = 'melbdjango joke',
+      url='http://github.com/GVRV/melbdjango-demo/',
+      author='Gaurav Dadhania',
+      author_email='gaurav@dadhania.in',
+      license='MIT',
+      packages=['thejoke'],
+      install_requires=[
+          'sh>=1.08'
+      ],
+      zip_safe=False,
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      scripts=['bin/stupid-script'],
+      entry_points={
+          'console_scripts': [
+              'say-the-joke = thejoke.teller:tell'
+          ]
+      })
