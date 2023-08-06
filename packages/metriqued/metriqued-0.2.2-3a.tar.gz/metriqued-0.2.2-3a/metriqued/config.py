c6a8e2072e1d3e4a611c15c1ea759660808df384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:52 +0000

    Add package metriqued/0.2.2-3a/metriqued-0.2.2-3a.tar.gz

---
+++ packages/metriqued/metriqued-0.2.2-3a.tar.gz/metriqued-0.2.2-3a/metriqued/config.py
@@ -0,0 +1,247 @@
+#!/usr/bin/env python
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+# Author: "Chris Ward <cward@redhat.com>
+
+import logging
+logger = logging.getLogger(__name__)
+import os
+
+from metriqueu.jsonconf import JSONConf
+from metriqued.basemongodb import BaseMongoDB
+
+CONFIG_DIR = '~/.metrique'
+
+PID_FILE = os.path.join(CONFIG_DIR, 'server.pid')
+SSL_CERT_FILE = os.path.join(CONFIG_DIR, 'cert.pem')
+SSL_KEY_FILE = os.path.join(CONFIG_DIR, 'pkey.pem')
+
+ADMIN_USER = 'admin'
+
+DATE_FORMAT = '%Y%m%dT%H:%M:%S'
+LOG_FORMAT = u'%(processName)s:%(message)s'
+LOG_FORMATTER = logging.Formatter(LOG_FORMAT,
+                                  DATE_FORMAT)
+
+CUBE_QUOTA = None
+
+
+class metrique(JSONConf):
+
+    def __init__(self, config_file=None):
+        self.default_config = os.path.join(CONFIG_DIR, 'metrique_config')
+        self.defaults = {
+            'async': True,
+            'autoreload': False,
+            'gzip': True,
+            'host': '127.0.0.1',
+            'krb_auth': False,
+            'log_formatter': LOG_FORMATTER,
+            'logfile': None,
+            'login_url': '/login',
+            'max_processes': 0,
+            'port': 5420,
+            'realm': 'metrique',
+            'ssl': False,
+            'xsrf_cookies': False,
+        }
+        super(metrique, self).__init__(config_file=config_file)
+
+    @property
+    def superusers(self):
+        return self._default('superusers', [ADMIN_USER])
+
+    @superusers.setter
+    def superusers(self, value):
+        if isinstance(value, basestring):
+            value = [value]
+        if not isinstance(value, list):
+            raise TypeError(
+                "superuser must be a single or list of strings")
+        self.config['superusers'] = value
+
+    @property
+    def cookie_secret(self):
+        if self.config.get('cookie_secret'):
+            return self.config['cookie_secret']
+        else:
+            # automatically generate a new cookie secret
+            # NOTE A NEW SECRET WILL INVALIDATE ALL PREVIOUS
+            # COOKIES; IN PRODUCTION, MAKE SURE TO HARDCODE
+            # THE COOKIE SECRETE IN metrique_config.json
+            return self._default('cookie_secret',
+                                 '____UPDATE_COOKIE_SECRET_CONFIG____')
+
+    @property
+    def debug(self):
+        ''' Reflect whether debug is enabled or not '''
+        return self._default('debug', False)
+
+    @debug.setter
+    def debug(self, value):
+        ''' Update logger settings '''
+        if isinstance(value, (tuple, list)):
+            logger, value = value
+            self._debug_set(value, logger)
+        else:
+            try:
+                logger = self.logger
+            except AttributeError:
+                self._debug_set(value)
+            else:
+                self._debug_set(value, logger)
+        self.config['debug'] = value
+
+    def _debug_set(self, level, logger=None):
+        '''
+        if we get a level of 2, we want to apply the
+        debug level to all loggers
+        '''
+        if not logger or level == 2:
+            logger = logging.getLogger()
+
+        if self.logfile:
+            logfile = os.path.expanduser(self.logfile)
+            for hdlr in logger.handlers:
+                logger.removeHandler(hdlr)
+            logger.removeHandler
+            hdlr = logging.FileHandler(logfile)
+            logger.addHandler(hdlr)
+
+        if level in [-1, False]:
+            logger.setLevel(logging.WARN)
+        elif level in [0, None]:
+            logger.setLevel(logging.INFO)
+        elif level in [True, 1, 2]:
+            logger.setLevel(logging.DEBUG)
+
+    @property
+    def pid_file(self):
+        pid_file = os.path.expanduser(
+            self._default('pid_file', PID_FILE))
+        if not pid_file.endswith(".pid"):
+            raise ValueError('pid file must end with .pid')
+        return pid_file
+
+    @property
+    def static_path(self):
+        abspath = os.path.dirname(os.path.abspath(__file__))
+        static_path = os.path.join(abspath, 'static/')
+        return self._default('static_path', static_path)
+
+    @property
+    def ssl_certificate(self):
+        return os.path.expanduser(
+            self._default('ssl_certificate', SSL_CERT_FILE))
+
+    @ssl_certificate.setter
+    def ssl_certificate(self, value):
+        self.config['ssl_certificate'] = value
+
+    @property
+    def ssl_certificate_key(self):
+        return os.path.expanduser(
+            self._default('ssl_certificate_key', SSL_KEY_FILE))
+
+    @ssl_certificate_key.setter
+    def ssl_certificate_key(self, value):
+        self.config['ssl_certificate_key'] = value
+
+
+class mongodb(JSONConf):
+
+    def __init__(self, config_file=None, *args, **kwargs):
+        self.default_config = os.path.join(CONFIG_DIR, 'mongodb_config')
+        self.defaults = {
+            'auth': False,
+            'admin_password': None,
+            'admin_user': ADMIN_USER,
+            'data_password': None,
+            'data_user': 'metrique',
+            'db_metrique': 'metrique',
+            'db_timeline': 'timeline',
+            'collection_cube_profile': 'cube_profile',
+            'collection_user_profile': 'user_profile',
+            'host': '127.0.0.1',
+            'port': 27017,
+            'ssl': False,
+            'write_concern': 1,
+        }
+        super(mongodb, self).__init__(config_file, *args, **kwargs)
+
+    @property
+    def db_metrique_data(self):
+        return BaseMongoDB(host=self.host, db=self.db_metrique,
+                           auth=self.auth,
+                           user=self.data_user,
+                           password=self.data_password,
+                           ssl=self.ssl,
+                           ssl_certfile=self.ssl_certificate,
+                           ssl_keyfile=self.ssl_certificate_key,
+                           write_concern=self.write_concern)
+
+    @property
+    def db_metrique_admin(self):
+        return BaseMongoDB(host=self.host, db=self.db_metrique,
+                           auth=self.auth,
+                           user=self.admin_user,
+                           password=self.admin_password,
+                           ssl=self.ssl,
+                           ssl_certfile=self.ssl_certificate,
+                           ssl_keyfile=self.ssl_certificate_key,
+                           write_concern=self.write_concern)
+
+    @property
+    def db_timeline_admin(self):
+        return BaseMongoDB(host=self.host, db=self.db_timeline,
+                           auth=self.auth,
+                           user=self.admin_user,
+                           password=self.admin_password,
+                           ssl=self.ssl,
+                           ssl_certfile=self.ssl_certificate,
+                           ssl_keyfile=self.ssl_certificate_key,
+                           write_concern=self.write_concern)
+
+    @property
+    def db_timeline_data(self):
+        return BaseMongoDB(host=self.host, db=self.db_timeline,
+                           auth=self.auth,
+                           user=self.data_user,
+                           password=self.data_password,
+                           ssl=self.ssl,
+                           ssl_certfile=self.ssl_certificate,
+                           ssl_keyfile=self.ssl_certificate_key,
+                           write_concern=self.write_concern)
+
+    @property
+    def c_user_profile_data(self):
+        return self.db_metrique_data[self.collection_user_profile]
+
+    @property
+    def c_user_profile_admin(self):
+        return self.db_metrique_admin[self.collection_user_profile]
+
+    @property
+    def c_cube_profile_data(self):
+        return self.db_metrique_data[self.collection_cube_profile]
+
+    @property
+    def c_cube_profile_admin(self):
+        return self.db_metrique_admin[self.collection_cube_profile]
+
+    @property
+    def ssl_certificate(self):
+        return os.path.expanduser(
+            self._default('ssl_certificate', SSL_CERT_FILE))
+
+    @ssl_certificate.setter
+    def ssl_certificate(self, value):
+        self.config['ssl_certificate'] = value
+
+    @property
+    def ssl_certificate_key(self):
+        return os.path.expanduser(
+            self._default('ssl_certificate_key', SSL_KEY_FILE))
+
+    @ssl_certificate_key.setter
+    def ssl_certificate_key(self, value):
+        self.config['ssl_certificate_key'] = value
---
+++ packages/metriqued/metriqued-0.2.2-3a.tar.gz/metriqued-0.2.2-3a/metriqued/tornadohttp.py
@@ -0,0 +1,274 @@
+#!/usr/bin/env python
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+# Author: "Chris Ward <cward@redhat.com>
+
+import logging
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+
+from functools import partial
+import os
+import signal
+import time
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+from tornado.web import Application
+
+from metriqued.config import metrique, mongodb
+from metriqued import core_api, cube_api, query_api, user_api
+
+from metriqueu.utils import set_default
+
+
+def user_cube(value):
+    user_cube = r'(\w+)/(\w+)'
+    path = os.path.join(user_cube, str(value))
+    return path
+
+
+def api_v2(value):
+    return os.path.join(r'/api/v2', str(value))
+
+
+def api_v1(value):
+    return os.path.join(r'/api/v1', str(value))
+
+
+def ucv2(value):
+    uc = user_cube(value)
+    a2 = api_v2(uc)
+    return a2
+
+
+class TornadoHTTPServer(object):
+    ''' HTTP (Tornado >=3.0) implemntation of MetriqueServer '''
+    def __init__(self, metrique_config_file=None,
+                 mongodb_config_file=None, host=None, port=None,
+                 ssl=None, async=None, debug=None,
+                 pid_file=None, **kwargs):
+        self.metrique_config = mconf = metrique(metrique_config_file)
+        self.mongodb_config = mbconf = mongodb(mongodb_config_file)
+
+        mconf.debug = debug = set_default(debug, mconf.debug)
+        mconf.async = async = set_default(async, mconf.async)
+        mconf.ssl = ssl = set_default(ssl, mconf.ssl)
+        mconf.host = host = set_default(host, mconf.host)
+        mconf.port = port = set_default(port, mconf.port)
+
+        self._pid_file = pid_file = set_default(pid_file, mconf.pid_file)
+        self.parent_pid = None
+        self.child_pid = None
+
+        self._prepare_handlers()
+
+        self.uri = 'https://%s' % host if ssl else 'http://%s' % host
+        self.uri += ':%s' % port
+
+        logger.debug('======= metrique =======')
+        logger.debug(' Host: %s' % self.uri)
+        logger.debug('  SSL: %s' % ssl)
+        logger.debug('Async: %s' % async)
+        logger.debug('======= mongodb ========')
+        logger.debug(' Host: %s' % mbconf.host)
+        logger.debug('  SSL: %s' % mbconf.ssl)
+        logger.debug(' Port: %s' % mbconf.port)
+
+    @property
+    def pid(self):
+        return os.getpid()
+
+    @property
+    def pid_file(self):
+        return self._pid_file
+
+    def set_pid(self):
+        if os.path.exists(self.pid_file):
+            raise RuntimeError(
+                "pid (%s) found in (%s)" % (self.pid,
+                                            self.metrique_config.pid_file))
+        else:
+            with open(self.pid_file, 'w') as _file:
+                _file.write(str(self.pid))
+        logger.debug("PID stored (%s)" % self.pid)
+
+    def _remove_pid(self):
+        error = None
+        try:
+            os.remove(self.pid_file)
+        except OSError as error:
+            logger.error(
+                'pid file not removed (%s); %s' % (self.pid_file, error))
+        else:
+            logger.debug("removed PID file: %s" % self.pid_file)
+
+    def _prepare_handlers(self):
+        base_handlers = [
+            (r"/register", user_api.RegisterHdlr),
+            (r"/login", user_api.LoginHdlr),
+            (r"/logout", user_api.LogoutHdlr),
+
+            (r"/(\w+)/aboutme", user_api.AboutMeHdlr),
+            (r"/(\w+)/passwd", user_api.UpdatePasswordHdlr),
+            (r"/(\w+)/update_profile", user_api.UpdateProfileHdlr),
+            (r"/(\w+)/update_group", user_api.UpdateGroupHdlr),
+            (r"/(\w+)/update_properties", user_api.UpdatePropertiesHdlr),
+
+            (api_v1(r""), core_api.ObsoleteAPIHdlr),
+            (api_v2(r"ping"), core_api.PingHdlr),
+
+            (api_v2(r"(\w+)?/?(\w+)?"), cube_api.ListHdlr),
+        ]
+
+        user_cube_handlers = [
+            (ucv2(r"find"), query_api.FindHdlr),
+            (ucv2(r"history"), query_api.HistoryHdlr),
+            (ucv2(r"deptree"), query_api.DeptreeHdlr),
+            (ucv2(r"count"), query_api.CountHdlr),
+            (ucv2(r"aggregate"), query_api.AggregateHdlr),
+            (ucv2(r"distinct"), query_api.DistinctHdlr),
+            (ucv2(r"sample"), query_api.SampleHdlr),
+
+            (ucv2(r"index"), cube_api.IndexHdlr),
+            (ucv2(r"save"), cube_api.SaveObjectsHdlr),
+            (ucv2(r"remove"), cube_api.RemoveObjectsHdlr),
+            (ucv2(r"update_role"), cube_api.UpdateRoleHdlr),
+            (ucv2(r"drop"), cube_api.DropHdlr),
+            (ucv2(r"stats"), cube_api.StatsHdlr),
+            (ucv2(r"register"), cube_api.RegisterHdlr),
+        ]
+
+        self._api_v2_handlers = base_handlers + user_cube_handlers
+
+    def _prepare_web_app(self):
+        ''' Config and Views'''
+        logger.debug("tornado web app setup")
+        debug = self.metrique_config.debug == 2
+        gzip = self.metrique_config.gzip
+        login_url = self.metrique_config.login_url
+        static_path = self.metrique_config.static_path
+
+        # pass in metrique and mongodb config to all handlers (init)
+        init = dict(metrique_config=self.metrique_config,
+                    mongodb_config=self.mongodb_config)
+
+        handlers = [(h[0], h[1], init) for h in self._api_v2_handlers]
+
+        self._web_app = Application(
+            gzip=gzip,
+            debug=debug,
+            static_path=static_path,
+            handlers=handlers,
+            cookie_secret=self.metrique_config.cookie_secret,
+            login_url=login_url,
+            xsrf_cookies=self.metrique_config.xsrf_cookies,
+        )
+
+        if debug and not self.metrique_config.autoreload:
+            # FIXME hack to disable autoreload when debug is True
+            from tornado import autoreload
+            autoreload._reload_attempted = True
+            autoreload._reload = lambda: None
+
+        ssl = self.metrique_config.ssl
+        if ssl:
+            ssl_options = dict(
+                certfile=self.metrique_config.ssl_certificate,
+                keyfile=self.metrique_config.ssl_certificate_key)
+            try:
+                self.server = HTTPServer(self._web_app,
+                                         ssl_options=ssl_options)
+            except ValueError:
+                raise ValueError(
+                    "SSL ERROR (%s); "
+                    "try running `metriqued-setup`" % ssl_options)
+        else:
+            self.server = HTTPServer(self._web_app)
+
+    def _init_basic_server(self):
+        host = self.metrique_config.host
+        port = self.metrique_config.port
+        self.server.listen(port=port, address=host)
+        IOLoop.instance().start()
+
+    def spawn_instance(self):
+        logger.debug("spawning new tornado webapp instance...")
+
+        self._mongodb_check()
+
+        self.set_pid()
+
+        signal.signal(signal.SIGTERM, self._inst_terminate_handler)
+        signal.signal(signal.SIGINT, self._inst_kill_handler)
+
+        self._init_basic_server()
+
+    def start(self, fork=False):
+        ''' Start a new tornado web app '''
+        self._prepare_web_app()
+        if fork:
+            pid = os.fork()
+            self.parent_pid = self.pid
+            if pid == 0:
+                self.spawn_instance()
+            else:
+                time.sleep(0.5)  # give child a moment to start
+                self.child_pid = pid
+        else:
+            pid = self.pid
+            self.spawn_instance()
+        logger.debug("tornado listening on %s" % self.uri)
+        return pid
+
+    def stop(self, delay=None):
+        ''' Stop a running tornado web app '''
+        if self.child_pid:
+            os.kill(self.child_pid, signal.SIGTERM)
+            self._remove_pid()
+        else:
+            self.server.stop()  # stop this tornado instance
+            delayed_kill = partial(self._inst_delayed_stop, delay)
+            IOLoop.instance().add_callback(delayed_kill)
+
+    def _inst_stop(self, sig, delay=None):
+        if self.child_pid:
+            os.kill(self.child_pid, sig)
+        else:
+            self.stop(delay=delay)
+
+    def _inst_terminate_handler(self, sig, frame):
+        logger.debug("[INST] (%s) recieved TERM signal" % self.pid)
+        self._inst_stop(sig)
+
+    def _inst_kill_handler(self, sig, frame):
+        logger.debug("[INST] (%s) recieved KILL signal" % self.pid)
+        self._inst_stop(sig, 0)
+
+    def _inst_delayed_stop(self, delay=None):
+        if delay is None:
+            if self.metrique_config.debug:
+                delay = 0
+            else:
+                delay = 5
+        logger.debug("stop ioloop called (%s)... " % self.pid)
+        TIMEOUT = float(delay) + time.time()
+        logger.debug("Shutting down in T-%i seconds ..." % delay)
+        IOLoop.instance().add_timeout(TIMEOUT, self._stop_ioloop)
+
+    def _stop_ioloop(self):
+        IOLoop.instance().stop()
+        logger.debug("IOLoop stopped")
+
+    def _mongodb_check(self):
+        # Fail to start if we can't communicate with mongo
+        host = self.mongodb_config.host
+        logger.debug('testing mongodb connection status (%s) ...' % host)
+        try:
+            assert self.mongodb_config.db_metrique_admin.db
+            assert self.mongodb_config.db_metrique_data.db
+            assert self.mongodb_config.db_timeline_admin.db
+            assert self.mongodb_config.db_timeline_data.db
+            logger.debug('... mongodb connection ok')
+        except Exception:
+            logger.error(
+                'failed to communicate with mongodb')
+            raise
