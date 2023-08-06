7699ef0c3210a2d93316184b356f25d313ed5a7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:16 +0000

    Add package robotframework-rfdoc/0.3/robotframework-rfdoc-0.3.tar.gz

---
+++ packages/robotframework-rfdoc/robotframework-rfdoc-0.3.tar.gz/robotframework-rfdoc-0.3/src/rfdoc/urls.py
@@ -0,0 +1,23 @@
+from django.conf.urls import include, patterns, url
+from django.contrib import admin
+
+from rfdoc import settings
+from rfdoc.rfdocapp import views
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    (r'^upload/?$', views.upload),
+    (r'^search/?$', views.search),
+    (r'^lib/(.*)', views.library),
+    (r'^$', views.index)
+)
+
+# Force Django's server to serve static assets, if this is not the production
+if settings.PRODUCTION is False:
+    urlpatterns += patterns('', (
+        r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.STATIC_ROOT
+        }
+    ))
