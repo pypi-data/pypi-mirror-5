1b6bf3003e95cdc94b8485d83ed076f0ec1e7afd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:53 +0000

    Add package idstools/0.1.0/idstools-0.1.0.tar.gz

---
+++ packages/idstools/idstools-0.1.0.tar.gz/idstools-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name="idstools",
+      version="0.1.0",
+      description="IDS Utility Library",
+      author="Jason Ish",
+      author_email="ish@unx.ca",
+      packages=["idstools"],
+      url="https://github.com/jasonish/idstools.py",
+      classifiers=[
+        'License :: OSI Approved :: BSD License',
+        ],
+      )
