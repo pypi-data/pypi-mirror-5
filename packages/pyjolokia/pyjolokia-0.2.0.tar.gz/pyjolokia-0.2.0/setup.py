2f1e9f150697c2d1fa664ce8f8ddeb2cf01751f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:28 +0000

    Add package pyjolokia/0.2.0/pyjolokia-0.2.0.tar.gz

---
+++ packages/pyjolokia/pyjolokia-0.2.0.tar.gz/pyjolokia-0.2.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='pyjolokia',
+      version = '0.2.0',
+      description='Pure Python based Jolokia client',
+      author='Colin Wood',
+      license="Apache License Version 2.0",
+      author_email='cwood06@gmail.com',
+      py_modules=['pyjolokia'],
+      url='https://github.com/cwood/pyjolokia',
+      download_url='http://github.com/cwood/sshed/tarball/master',
+      long_description=open('README.rst').read(),
+      include_package_data=True,
+      keywords=['jolokia', 'jmx'],
+      use_2to3=True,
+)
