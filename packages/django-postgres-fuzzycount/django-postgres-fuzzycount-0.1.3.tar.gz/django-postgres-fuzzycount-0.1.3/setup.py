075926392d51b9c1618510d02abdf80cd6af02b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:02 +0000

    Add package django-postgres-fuzzycount/0.1.3/django-postgres-fuzzycount-0.1.3.tar.gz

---
+++ packages/django-postgres-fuzzycount/django-postgres-fuzzycount-0.1.3.tar.gz/django-postgres-fuzzycount-0.1.3/setup.py
@@ -0,0 +1,31 @@
+
+from setuptools import setup
+
+
+setup(
+    name = "django-postgres-fuzzycount",
+    version = "0.1.3",
+    author = "Stephen McDonald",
+    author_email = "stephen.mc@gmail.com",
+    description = ("A Django model manager providing fast / fuzzy counts "
+                   "for PostgreSQL database tables."),
+    long_description = open("README.rst").read(),
+    url = "http://github.com/kouio/django-postgres-fuzzycount",
+    zip_safe = False,
+    py_modules=["fuzzycount",],
+    install_requires = [
+        "sphinx-me >= 0.1.2",
+        "django >= 1.4",
+        "django-model-utils==1.4.0",
+    ],
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Internet :: WWW/HTTP :: Site Management",
+    ]
+)
