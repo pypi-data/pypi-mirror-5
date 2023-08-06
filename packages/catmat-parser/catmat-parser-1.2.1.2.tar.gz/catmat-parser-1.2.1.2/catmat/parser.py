ef7cedc59001f3119f8479d1394f5af63057e526
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:30 +0000

    Add package catmat-parser/1.2.1.2/catmat-parser-1.2.1.2.tar.gz

---
+++ packages/catmat-parser/catmat-parser-1.2.1.2.tar.gz/catmat-parser-1.2.1.2/catmat/parser.py
@@ -0,0 +1,22 @@
+#coding: utf-8
+from xml.etree import ElementTree as ET
+
+class CatmatParser(object):
+
+    def __init__(self, xml_string):
+        self.__tree = ET.fromstring(xml_string)
+        self.__data = []
+
+    def to_hash(self):
+        for item in self.__tree.find('itens').findall('item'):
+            hash_data = {}
+            unidades = []
+            descricao = item.find('descricao').text.strip().encoding('ISO-8859-1')
+            for unidade in item.find('unidades').findall('unidade'):
+                unidades.append([unidade.find('sigla_unidade_fornecimento').text.strip(),
+                                 unidade.find('capacidade').text.strip(),
+                                 unidade.find('unidade_medida').text.strip()])
+            hash_data['descricao'] = descricao
+            hash_data['unidades']  = unidades
+            self.__data.append(hash_data)
+        return self.__data
---
+++ packages/catmat-parser/catmat-parser-1.2.1.2.tar.gz/catmat-parser-1.2.1.2/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'A simple Catmat (SIASG) XML Service',
+    'author': 'Oswaldo Ferreira',
+    'url': 'https://github.com/oswluiz/CatmatParser',
+    'download_url': 'https://github.com/oswluiz/CatmatParser',
+    'author_email': 'oswluizf@gmail.com',
+    'version': '1.2.1.2',
+    'install_requires': ['should_dsl', 'requests'],
+    'packages': ['catmat'],
+    'scripts': [],
+    'name': 'catmat-parser'
+}
+
+setup(**config)
