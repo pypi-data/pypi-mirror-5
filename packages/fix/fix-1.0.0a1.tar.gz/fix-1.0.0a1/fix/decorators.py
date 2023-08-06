a31f25c49a0989ea1b12b5f3251ef79009877e46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:04 +0000

    Add package fix/1.0.0a1/fix-1.0.0a1.tar.gz

---
+++ packages/fix/fix-1.0.0a1.tar.gz/fix-1.0.0a1/fix/decorators.py
@@ -0,0 +1,33 @@
+"""Decorators."""
+
+from functools import wraps
+from fix.util import Context
+
+
+def with_fixture(create_fixture, **kwargs):
+    """Decorate a callable with a fixture."""
+    context = Context()
+    fixture = create_fixture(context, **kwargs)
+    if isinstance(fixture, tuple):
+        setup, teardown = fixture
+    else:
+        setup = fixture
+        teardown = lambda: None
+
+    def wrap(func):
+        """Wrap the callable."""
+        @wraps(func)
+        def call(*args, **kwargs):
+            """Call `setup` and `teardown` before and after `func`."""
+            try:
+                setup()
+                result = func(context, *args, **kwargs)
+            finally:
+                try:
+                    teardown()
+                finally:
+                    context.clear()
+            return result
+        return call
+
+    return wrap
