bb2b9bcb7225eaa453697b2af9c2a3e280a6fb3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:15 +0000

    Add package pyjavaproperties-unicode/0.1/pyjavaproperties-unicode-0.1.tar.gz

---
+++ packages/pyjavaproperties-unicode/pyjavaproperties-unicode-0.1.tar.gz/pyjavaproperties-unicode-0.1/pyjavapropertiesu.py
@@ -0,0 +1,43 @@
+"""
+This library wraps pyjavaproperties to transparently store non-ascii values::
+
+    props = Properties()
+    props.load(open('messages_de.properties', 'r'))
+    props.getProperty('section.key')
+    ...
+    props.setProperty('section.otherkey', u'unicode_data')
+"""
+from __future__ import unicode_literals
+
+import codecs
+import pyjavaproperties
+
+
+def unicode_escape_handler(e):
+    return (u'\\u%04x' % ord(e.object[e.start:e.end]), e.end)
+
+
+codecs.register_error('unicode_escape', unicode_escape_handler)
+
+
+class Properties(pyjavaproperties.Properties):
+    def getProperty(self, key, raw=None):
+        value = super(Properties, self).getProperty(key)
+        if raw:
+            return value
+        return decode_unicode(value)
+
+    def setProperty(self, key, value, raw=None):
+        if raw:
+            return super(Properties, self).setProperty(key, value)
+        return super(Properties, self).setProperty(key, encode_unicode(value))
+
+
+def encode_unicode(value):
+    if isinstance(value, str):
+        return value
+    return value.encode('ascii', 'unicode_escape')
+
+
+def decode_unicode(value):
+    return value.decode('unicode_escape')
---
+++ packages/pyjavaproperties-unicode/pyjavaproperties-unicode-0.1.tar.gz/pyjavaproperties-unicode-0.1/setup.py
@@ -0,0 +1,20 @@
+import os
+
+from setuptools import setup
+
+setup(
+    name='pyjavaproperties-unicode',
+    version='0.1',
+    author='Horst Gutmann',
+    author_email='horst@zerokspot.com',
+    py_modules=['pyjavapropertiesu'],
+    install_requires=[
+        'pyjavaproperties >= 0.6'
+    ],
+    license='MIT',
+    url='https://github.com/zerok/pyjavaproperties-unicode',
+    description='A small wrapper around pyjavaproperties to add unicode '
+                'support.',
+    long_description=open(os.path.join(os.path.dirname(__file__),
+                                       'README.rst')).read(),
+)
