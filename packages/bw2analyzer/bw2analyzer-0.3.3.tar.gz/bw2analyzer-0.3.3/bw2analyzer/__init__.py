08a28427dccf090dc958181437e1db4f0ccb0ca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:56 +0000

    Add package bw2analyzer/0.3.3/bw2analyzer-0.3.3.tar.gz

---
+++ packages/bw2analyzer/bw2analyzer-0.3.3.tar.gz/bw2analyzer-0.3.3/bw2analyzer/__init__.py
@@ -0,0 +1,7 @@
+# encoding: utf-8
+from .contribution import ContributionAnalysis
+from .page_rank import PageRank
+from .explorer import DatabaseExplorer
+from .report import SerializedLCAReport
+
+__version__ = (0, 3, 3)
