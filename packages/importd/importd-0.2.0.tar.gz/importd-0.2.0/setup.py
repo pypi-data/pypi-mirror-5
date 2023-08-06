eff6989720bca65428f731b31cf4f7f09eeb021d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:12 +0000

    Add package importd/0.2.0/importd-0.2.0.tar.gz

---
+++ packages/importd/importd-0.2.0.tar.gz/importd-0.2.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+try:
+    long_description=open('README.rst', 'rt').read()
+except Exception:
+    long_description=""
+
+setup(
+    name = "importd",
+    description = "a django based miniframework, inspired by sinatra",
+    long_description=long_description,
+
+    version = "0.2.0",
+    author = 'Amit Upadhyay',
+    author_email = "upadhyay@gmail.com",
+
+    url = 'http://amitu.com/importd/',
+    license = 'BSD',
+
+    install_requires = ["fhurl>=0.1.7", "gunicorn", "smarturls", "six"],
+    py_modules = ["importd"],
+
+    zip_safe = True,
+)
