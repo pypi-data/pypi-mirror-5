94bf68f5a6420d495c87e3514e99a94e45dd58d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:39 +0000

    Add package django-debreach/0.5.1/django-debreach-0.5.1.tar.gz

---
+++ packages/django-debreach/django-debreach-0.5.1.tar.gz/django-debreach-0.5.1/debreach/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from distutils import version
+
+
+__version__ = '0.5.1'
+version_info = version.StrictVersion(__version__).version
