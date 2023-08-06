8dc8f935fca64650607ba5bb9602bc1dd3df7a78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:16 +0000

    Add package trac-LighterTheme/0.3.1/trac-LighterTheme-0.3.1.tar.gz

---
+++ packages/trac-LighterTheme/trac-LighterTheme-0.3.1.tar.gz/trac-LighterTheme-0.3.1/README.txt
@@ -0,0 +1,6 @@
+A light, Bootstrap-inspired theme
+=================================
+
+LighterTheme skins Trac, slightly, for a "lighter" look-and-feel.  It provides a narrower (940px) main content div; fewer dark lines in the top (mainnav and metanav) banner; bigger buttons (using CSS from Twitter Bootstrap); rounded corners on input fields; and the "Ubuntu" font used on Launchpad.
+
+Screenshots are available on the plugin's Trac Hacks page: http://trac-hacks.org/wiki/LighterTheme
---
+++ packages/trac-LighterTheme/trac-LighterTheme-0.3.1.tar.gz/trac-LighterTheme-0.3.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+version='0.3.1'
+
+try:
+    long_description = open("README.txt").read()
+except:
+    long_description = ''
+try:
+    long_description += open("CHANGES.txt").read()
+except:
+    pass
+
+setup(name='trac-LighterTheme',
+      version=version,
+      description="A light, Bootstrap-inspired theme for Trac",
+      long_description=long_description,
+      author='Ethan Jucovy, Jacqueline Arasi',
+      author_email='ejucovy@gmail.com',
+      url='http://trac-hacks.org/wiki/LighterTheme',
+      keywords='trac plugin',
+      license="BSD",
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests*']),
+      include_package_data=True,
+      zip_safe=False,
+      entry_points = """
+      [trac.plugins]
+      lightertheme = lightertheme
+      """,
+      )
+
