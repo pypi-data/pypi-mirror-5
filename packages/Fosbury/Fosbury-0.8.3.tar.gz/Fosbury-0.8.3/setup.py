95171504aaf7a71706cbfc18caed84e55fdba57f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:24 +0000

    Add package Fosbury/0.8.3/Fosbury-0.8.3.tar.gz

---
+++ packages/Fosbury/Fosbury-0.8.3.tar.gz/Fosbury-0.8.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='Fosbury',
+      version='0.8.3',
+      author='Willem Spruijt',
+      author_email='willem@fosbury.co',
+      description='Python client for Fosbury (http://fosbury.co)',
+      packages=find_packages())
