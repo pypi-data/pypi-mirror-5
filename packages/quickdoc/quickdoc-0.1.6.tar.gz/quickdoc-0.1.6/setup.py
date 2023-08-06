2244bb08e16ca9e922134c50f9689b84ca748cf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:32 +0000

    Add package quickdoc/0.1.6/quickdoc-0.1.6.tar.gz

---
+++ packages/quickdoc/quickdoc-0.1.6.tar.gz/quickdoc-0.1.6/setup.py
@@ -0,0 +1,17 @@
+
+from setuptools import setup
+
+setup(
+    name="quickdoc",
+    version="0.1.6",
+    description="TBD",
+    author="Alexander Boyd",
+    author_email="alex@opengroove.org",
+    py_modules=["quickdoc_generator"],
+    install_requires=["fileutils", "singledispatch", "sphinx"],
+    entry_points = {
+        "console_scripts": [
+            "quickdoc = quickdoc_generator:main"
+        ]
+    }
+)
