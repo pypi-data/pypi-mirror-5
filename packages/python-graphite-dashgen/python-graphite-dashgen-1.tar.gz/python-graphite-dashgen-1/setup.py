55a7d17cbe76d9f19938e23001798732dc7b5e03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:25 +0000

    Add package python-graphite-dashgen/1/python-graphite-dashgen-1.tar.gz

---
+++ packages/python-graphite-dashgen/python-graphite-dashgen-1.tar.gz/python-graphite-dashgen-1/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# Standard library
+from distutils.core import setup
+
+with open("README.rst") as file:
+    long_description = file.read()
+
+setup(author="Timid Robot Zehta",
+      author_email="tim@clockwork.net",
+      classifiers=["Environment :: Console",
+                   "License :: OSI Approved :: MIT License",],
+      description=("Graphite-dashgen automates the creation of Graphite "
+                   "dashboards"),
+      download_url="https://github.com/ClockworkNet/graphite-dashgen/releases",
+      license="MIT License",
+      long_description=long_description,
+      name="python-graphite-dashgen",
+      py_modules=["dashgen",],
+      requires=["PyYAML",],
+      url="https://github.com/ClockworkNet/graphite-dashgen",
+      version="1",
+      )
