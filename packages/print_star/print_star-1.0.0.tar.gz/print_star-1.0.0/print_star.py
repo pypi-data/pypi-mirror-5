5d41e447e7030753ecd4ba29ecee753b6ab3f177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:50 +0000

    Add package print_star/1.0.0/print_star-1.0.0.tar.gz

---
+++ packages/print_star/print_star-1.0.0.tar.gz/print_star-1.0.0/print_star.py
@@ -0,0 +1,2 @@
+def print_star(sth):
+    print("***"+sth+"***")
---
+++ packages/print_star/print_star-1.0.0.tar.gz/print_star-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name			= 'print_star',
+	version			= '1.0.0',
+	py_modules		= ['print_star'],
+	author			= 'Ethan',
+	author_email	= 'dysalbert@gmail.com',
+	url				='http://www.cs.ubc.ca/~yingsaid/',
+	description 	='A simple print commend printing anything with 3 stars each beforehand and afterhand',
+
+	)
