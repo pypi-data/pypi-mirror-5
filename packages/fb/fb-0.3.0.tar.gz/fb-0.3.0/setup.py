28800c4d54389f4388288df0a866116b99a18792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:55 +0000

    Add package fb/0.3.0/fb-0.3.0.tar.gz

---
+++ packages/fb/fb-0.3.0.tar.gz/fb-0.3.0/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+    name='fb',
+    version='0.3.0',
+    author='Samir Ahmed',
+    author_email='blaklites@gmail.com',
+    packages=['fb'],
+    #scripts=[],
+    url='https://github.com/blaklites/fb',
+    license='The MIT License (MIT)',
+    description='Made Compatible with python3.X. Added better docs',
+    long_description=open('README.txt').read(),
+    install_requires=[],
+    keywords=['facebook', 'facebook-sdk', 'python-facebook', 'facebook-graph', 'facebook-api', 'facebook-python']
+)
