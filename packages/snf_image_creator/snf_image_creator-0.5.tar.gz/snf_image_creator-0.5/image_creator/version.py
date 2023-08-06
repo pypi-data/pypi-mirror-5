4782cb62b797ee754dba44193a10da63fc66ab08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:58 +0000

    Add package snf_image_creator/0.5/snf_image_creator-0.5.tar.gz

---
+++ packages/snf_image_creator/snf_image_creator-0.5.tar.gz/snf_image_creator-0.5/image_creator/version.py
@@ -0,0 +1,7 @@
+__version__ = "0.5"
+__version_info__ = ['0', '5']
+__version_vcs_info__ = {
+    'branch': 'master',
+    'revid': '5079560',
+    'revno': 387}
+__version_user_info__ = "skalkoto@darkstar.admin.grnet.gr"
