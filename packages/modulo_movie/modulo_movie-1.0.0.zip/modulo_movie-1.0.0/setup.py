e952d8a2c82711512330dcf802ade0291cd62847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:42 +0000

    Add package modulo_movie/1.0.0/modulo_movie-1.0.0.zip

---
+++ packages/modulo_movie/modulo_movie-1.0.0.zip/modulo_movie-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name            = 'modulo_movie',
+    version         = '1.0.0',
+    py_modules      = ['ModuloMovie'],
+    author          = 'hfpython',
+    author_email    = 'hfpython',
+    url             = 'http://www.headfirstlabs.com',
+    description     = 'Um simples "printer" de movie list'
+    )
