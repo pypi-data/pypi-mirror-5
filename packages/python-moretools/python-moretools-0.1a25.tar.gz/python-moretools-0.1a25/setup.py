9579d5d5c7540502057b6552f1927f6f87a3b4d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:46 +0000

    Add package python-moretools/0.1a25/python-moretools-0.1a25.tar.gz

---
+++ packages/python-moretools/python-moretools-0.1a25.tar.gz/python-moretools-0.1a25/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+setup(
+  name = 'python-moretools',
+  version = '0.1a25',
+  description = (
+    '[Obsolete] Renamed to https://pypi.python.org/pypi/moretools : '
+    'Many more basic tools for python 2/3'
+    ' extending itertools, functools, operator and collections.'
+    ),
+  author = 'Stefan Zimmermann',
+  author_email = 'zimmermann.code@gmail.com',
+  url = 'http://bitbucket.org/userzimmermann/python-moretools',
+
+  license = 'LGPLv3',
+
+  packages = ['moretools'],
+
+  classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved ::'
+    + ' GNU Library or Lesser General Public License (LGPL)',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3',
+    'Topic :: Software Development',
+    'Topic :: Utilities',
+    ],
+  keywords = [
+    'tools', 'itertools', 'functools', 'operator', 'collections',
+    'iterator', 'iteration', 'functional',
+    'filter', 'map', 'repeat', 'query',
+    'tuple', 'list', 'set', 'dict', 'simpledict',
+    'camelize', 'decamelize',
+    'python3',
+    ],
+  )
