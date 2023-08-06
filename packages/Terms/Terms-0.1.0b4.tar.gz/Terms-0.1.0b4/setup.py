8b4f5d6474f4813e1c56ea445d272872ca0ce126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:52 +0000

    Add package Terms/0.1.0b4/Terms-0.1.0b4.tar.gz

---
+++ packages/Terms/Terms-0.1.0b4.tar.gz/Terms-0.1.0b4/setup.py
@@ -0,0 +1,63 @@
+# Copyright (c) 2007-2012 by Enrique Pérez Arnaud <enriquepablo@gmail.com>
+#
+# This file is part of the terms project.
+# https://github.com/enriquepablo/terms
+#
+# The terms project is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# The terms project is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with any part of the terms project.
+# If not, see <http://www.gnu.org/licenses/>.
+
+
+import distribute_setup
+distribute_setup.use_setuptools()
+from setuptools import setup, find_packages
+
+VERSION = '0.1.0b4'
+
+setup(
+    name = 'Terms',
+    version = VERSION,
+    author = 'Enrique Pérez Arnaud',
+    author_email = 'enriquepablo@gmail.com',
+    url = 'http://pypi.python.org/terms.core',
+    license = 'GNU GENERAL PUBLIC LICENSE Version 3',
+    description = 'A smart knowledge store',
+    long_description = (open('README.rst').read() +
+                        '\n' + open('INSTALL.rst').read()) +
+                        '\n' + open('SUPPORT.rst').read(),
+
+    packages = find_packages(),
+    namespace_packages = ['terms'],
+    test_suite = 'nose.collector',
+    include_package_data = True,
+
+    entry_points = {
+        'console_scripts': [
+            'terms = terms.core.scripts.repl:repl',
+            'initterms = terms.core.scripts.initterms:init_terms',
+            'kbdaemon = terms.core.scripts.kbdaemon:main',
+        ],
+    },
+    tests_require = [
+        'Nose',
+        'coverage',
+    ],
+    extras_require = {
+        'PG': ['psycopg2 == 2.4.5',],
+        },
+    install_requires = [
+        'distribute',
+        'sqlalchemy >= 0.8.0b2',
+        'ply == 3.4',
+    ],
+)
