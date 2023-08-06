5a9302e7eefc50d66314a4355a91a3785c79fcf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:03 +0000

    Add package cmsplugin-demo/1.0.8/cmsplugin-demo-1.0.8.tar.gz

---
+++ packages/cmsplugin-demo/cmsplugin-demo-1.0.8.tar.gz/cmsplugin-demo-1.0.8/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cmsplugin-demo',
+    version='1.0.8',
+    description='Extendable demo form plugin for Django CMS with spam protection and i18n',
+    long_description=open('README.rst').read(),
+    author='Vinit Kumar',
+    author_email='vinit.kumar@changer.nl',
+    url='http://github.com/vinitkumar/cmsplugin-demo',
+    packages=find_packages(),
+    keywords='demo form django cms django-cms spam protection email',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
+)
