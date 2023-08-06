0593bfd3ceac3e8d575112cb7624e3e1a2d61f5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:47 +0000

    Add package pystun/0.0.4/pystun-0.0.4.tar.gz

---
+++ packages/pystun/pystun-0.0.4.tar.gz/pystun-0.0.4/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+src = os.path.realpath(os.path.dirname(__file__))
+
+README = open(os.path.join(src, 'README.rst')).read()
+
+setup(
+    name='pystun',
+    version="0.0.4",
+    packages=find_packages(),
+    scripts=['bin/pystun'],
+    zip_safe=False,
+    license='MIT',
+    author='Justin Riley (original author: gaohawk)',
+    author_email='justin.t.riley@gmail.com',
+    url="http://github.com/jtriley/pystun",
+    description="A Python STUN client for getting NAT type and external IP (RFC 3489)",
+    long_description=README,
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Internet",
+        "Topic :: System :: Networking :: Firewalls",
+        "Programming Language :: Python",
+    ],
+)
