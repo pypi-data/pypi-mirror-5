e2080b65798749929e07760578d6443ded395344
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:18 +0000

    Add package shtub/0.3.5/shtub-0.3.5.tar.gz

---
+++ packages/shtub/shtub-0.3.5.tar.gz/shtub-0.3.5/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup(
+          name = 'shtub',
+          version = '0.3.5',
+          description = 'shtub - shell command stub',
+          long_description = '''''',
+          author = "Alexander Metzner, Michael Gruber, Maximilien Riehl, Marcel Wolf, Udo Juettner",
+          author_email = "alexander.metzner@gmail.com, aelgru@gmail.com, maximilien.riehl@gmail.com, marcel.wolf@immobilienscout24.de, udo.juettner@gmail.com",
+          license = 'GNU GPL v3',
+          url = 'https://github.com/yadt/shtub',
+          scripts = [],
+          packages = ['shtub', 'shtub.verification'],
+          classifiers = ['Development Status :: 3 - Alpha', 'Programming Language :: Python'],
+          
+          
+          
+          
+          zip_safe=True
+    )
