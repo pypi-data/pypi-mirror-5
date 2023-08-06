dca86d6ab051b6002b7c8a3fd9440771d4361378
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:06 +0000

    Add package license-info/0.8.2/license-info-0.8.2.tar.gz

---
+++ packages/license-info/license-info-0.8.2.tar.gz/license-info-0.8.2/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='license-info',
+    version='0.8.2',
+    description='Show list of installed python packages with version and license info',
+    author='Michal Odnous',
+    author_email='odi.root@gmail.com',
+    url='https://github.com/odiroot/license-info',
+    py_modules=["license_info"],
+    install_requires=["pkgtools", "pip"],
+    entry_points={
+        'console_scripts': [
+            'li = license_info:main',
+        ],
+    },
+    license="BSD",
+    tests_require=['mock'],
+    test_suite='tests',
+)
