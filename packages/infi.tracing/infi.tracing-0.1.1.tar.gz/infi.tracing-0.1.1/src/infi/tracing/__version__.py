5fb11d1139ca0cecd1067f14fa5e494c2af2ef48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:50 +0000

    Add package infi.tracing/0.1.1/infi.tracing-0.1.1.tar.gz

---
+++ packages/infi.tracing/infi.tracing-0.1.1.tar.gz/infi.tracing-0.1.1/src/infi/tracing/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.1"
+__git_commiter_name__ = "Tal Yalon"
+__git_commiter_email__ = "tal.yalon@gmail.com"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '1024341155343b19e187f428832cd45fb033bbad'
+__git_head_subject__ = 'Finished release v0.1.1.'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
