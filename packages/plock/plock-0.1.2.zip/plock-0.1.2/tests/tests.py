5462d0b5484aed166f1f4421583973fff85d4d18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:39 +0000

    Add package plock/0.1.2/plock-0.1.2.zip

---
+++ packages/plock/plock-0.1.2.zip/plock-0.1.2/tests/tests.py
@@ -0,0 +1,21 @@
+def test_argument_parser_items():
+    from plock.config import argument_parser
+    import sys
+    sys.argv = ['']  # XXX Remove "test" arg passed to setup.py
+    parse_args = argument_parser.parse_args()
+    for arg in ['add_on', 'list_addons', 'preserve', 'raw', 'write_config']:
+        assert arg in parse_args
+
+
+def test_locale_format():
+    from plock.install import Installer
+    import locale
+    plock = Installer()
+    locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+    result = plock.locale_format(3000)
+    assert result == "3,000"
+
+
+if __name__ == '__main__':
+    test_argument_parser_items()
+    test_locale_format()
