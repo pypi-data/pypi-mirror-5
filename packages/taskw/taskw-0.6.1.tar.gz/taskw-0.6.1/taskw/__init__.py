dbff77b5ef8cf32d61b09076498fa9ca6bb484d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:12 +0000

    Add package taskw/0.6.1/taskw-0.6.1.tar.gz

---
+++ packages/taskw/taskw-0.6.1.tar.gz/taskw-0.6.1/taskw/__init__.py
@@ -0,0 +1,12 @@
+from taskw.warrior import TaskWarrior, TaskWarriorExperimental
+from taskw.utils import clean_task, encode_task, decode_task
+from taskw.utils import encode_task_experimental
+
+__all__ = [
+    TaskWarrior,
+    TaskWarriorExperimental,
+    clean_task,
+    encode_task,
+    decode_task,
+    encode_task_experimental,
+]
