1c60506140fa326c7475cc1a0a11c698da005795
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:31 +0000

    Add package bumple-downloader/0.1dev/bumple-downloader-0.1dev.tar.gz

---
+++ packages/bumple-downloader/bumple-downloader-0.1dev.tar.gz/bumple-downloader-0.1dev/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='bumple-downloader',
+    version='0.1dev',
+    packages=['bumple-downloader',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+)
