5259b13beeea75a68ea13cca7dc7a9f0bb39424a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:54 +0000

    Add package mang/0.1.2-3-g03bdf84/mang-0.1.2-3-g03bdf84.tar.gz

---
+++ packages/mang/mang-0.1.2-3-g03bdf84.tar.gz/mang-0.1.2-3-g03bdf84/mang/node/relu_node.py
@@ -0,0 +1,19 @@
+from .node import Node
+
+
+class ReLUNode(Node):
+    """Node with rectified linear activation function."""
+    _name = "relu"
+
+    def __init__(self, dim, option):
+        Node.__init__(self, dim, option)
+
+    def up(self):
+        self._add_b()
+        if self.on_gpu:
+            self.y.lower_bound(0.)
+        else:
+            self.y = self.y * (self.y >= 0.)
+
+    def down(self):
+        self.dy.apply_rectified_linear_deriv(self.y)
