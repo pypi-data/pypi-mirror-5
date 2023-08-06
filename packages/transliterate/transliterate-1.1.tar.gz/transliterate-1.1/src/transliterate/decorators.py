3302dfd17370bbab9f775814170078b514148a22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:40 +0000

    Add package transliterate/1.1/transliterate-1.1.tar.gz

---
+++ packages/transliterate/transliterate-1.1.tar.gz/transliterate-1.1/src/transliterate/decorators.py
@@ -0,0 +1,40 @@
+__title__ = 'transliterate.decorators'
+__version__ = '1.1'
+__build__ = 0x000011
+__author__ = 'Artur Barseghyan'
+__all__ = ('transliterate_function', 'transliterate_method')
+
+from transliterate.utils import translit
+
+class TransliterateFunction(object):
+    """
+    Function decorator.
+    """
+    def __init__(self, language_code, reversed=False):
+        self.language_code = language_code
+        self.reversed = reversed
+
+    def __call__(self, func):
+        def inner(*args, **kwargs):
+            value = unicode(func(*args, **kwargs))
+            return translit(value, language_code=self.language_code, reversed=self.reversed)
+        return inner
+
+transliterate_function = TransliterateFunction
+
+
+class TransliterateMethod(object):
+    """
+    Method decorator.
+    """
+    def __init__(self, language_code, reversed=False):
+        self.language_code = language_code
+        self.reversed = reversed
+
+    def __call__(self, func):
+        def inner(this, *args, **kwargs):
+            value = unicode(func(this, *args, **kwargs))
+            return translit(value, language_code=self.language_code, reversed=self.reversed)
+        return inner
+
+transliterate_method = TransliterateMethod
