4cfec4b35c2fe12580e619820d3b3b4a77e26353
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:56 +0000

    Add package ApiDoc/1.1a1/ApiDoc-1.1a1.tar.gz

---
+++ packages/ApiDoc/ApiDoc-1.1a1.tar.gz/ApiDoc-1.1a1/setup.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+
+if sys.version_info < (3, 2):
+    print("ApiDoc requires Python 3.2 or later")
+    raise SystemExit(1)
+
+from setuptools import setup, find_packages
+from setup_cmd import ApiDocTest, Resource
+
+from apidoc import __version__
+
+
+if (3, 2) <= sys.version_info < (3, 3):
+    requirements = ['Jinja2 == 2.6', 'PyYAML', 'jsonschema']
+else:
+    requirements = ['Jinja2', 'PyYAML', 'jsonschema']
+
+
+setup(
+    name='ApiDoc',
+    version=__version__,
+    description='Api Documentation Generator',
+    long_description=open("README.rst").read() + "\n\n" + open("CHANGES.rst").read(),
+    author='Jérémy Derussé',
+    author_email='jeremy.derusse@sfr.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Natural Language :: English',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Topic :: Documentation',
+        'Topic :: Software Development :: Documentation',
+    ],
+    platforms=['Any'],
+    license='GPLv3+',
+    url='http://solutionscloud.github.io/apidoc/',
+    packages=find_packages(exclude=['tests', 'tests.*', 'example', 'example.*', 'docs', 'docs.*']),
+    entry_points={
+        'console_scripts': [
+            'apidoc-analyse = apidoc.command.analyse:main',
+            'apidoc-render = apidoc.command.render:main',
+            'apidoc-watch = apidoc.command.watch:main',
+        ],
+    },
+    include_package_data=True,
+    package_data={'apidoc': [
+        'template/*.html',
+        'template/helper/*.html',
+        'template/partial/*.html',
+        'template/layout/*.html',
+        'template/resource/css/*.css',
+        'template/resource/js/*.js',
+        'template/resource/font/*',
+        'datas/schemas/*.yml',
+        'command/logging.yml',
+    ]},
+    install_requires=requirements,
+    tests_require=['mock', 'pytest'],
+    cmdclass={
+        'test': ApiDocTest,
+        'resources': Resource,
+    }
+)
