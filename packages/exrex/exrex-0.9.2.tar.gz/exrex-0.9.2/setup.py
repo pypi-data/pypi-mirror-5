227a3fb07e846c35dc2aad1ce30cf23263235dee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:16 +0000

    Add package exrex/0.9.2/exrex-0.9.2.tar.gz

---
+++ packages/exrex/exrex-0.9.2.tar.gz/exrex-0.9.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'exrex',
+    version = '0.9.2',
+    author = 'Adam Tauber',
+    author_email = 'asciimoo@gmail.com',
+    description = ('Exrex is a tool and python module that generates all - or random - matching strings to a given regular expression.'),
+    license = 'AGPLv3+',
+    keywords = "regexp generators string generation",
+    url = 'https://github.com/asciimoo/exrex',
+    scripts = ['exrex.py'],
+    py_modules = ['exrex'],
+    packages = find_packages(),
+    install_requires = [],
+    download_url = 'https://github.com/asciimoo/exrex/tarball/master',
+    entry_points={
+        "console_scripts": ["exrex=exrex:__main__"]
+    },
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU Affero General Public License v3'
+    ],
+)
