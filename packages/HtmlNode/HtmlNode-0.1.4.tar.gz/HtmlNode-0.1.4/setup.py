39d739ee1a7f8eff0a42ff9878343a6832207b77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:19 +0000

    Add package HtmlNode/0.1.4/HtmlNode-0.1.4.tar.gz

---
+++ packages/HtmlNode/HtmlNode-0.1.4.tar.gz/HtmlNode-0.1.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="HtmlNode",
+    version="0.1.4",
+    packages=find_packages(),
+    description="A simple Python HTML generator",
+    author="Hing-Lung Lau",
+    author_email="lung220@gmail.com",
+    url="http://github.com/hllau/html_node",
+    license="Apache v2",
+    keywords="html builder factory template"
+)
+
