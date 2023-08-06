d431ac5ec88fc0d38a2cda31f71116a56240a3d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:50 +0000

    Add package galapagos-assembler/0.20/galapagos-assembler-0.20.tar.gz

---
+++ packages/galapagos-assembler/galapagos-assembler-0.20.tar.gz/galapagos-assembler-0.20/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='galapagos-assembler',
+    version='0.20',
+    author=u'Sigve Sebastian Farstad',
+    author_email='sigvefarstad@gmail.com',
+    packages=['galapagos'],
+    url='http://github.com/dmpro-ytelse/galapagos-as',
+    description='Assembles Galapagos assembly.',
+    entry_points={
+        'console_scripts': [
+            'galapagos-as = galapagos:main'
+        ]
+    }
+)
