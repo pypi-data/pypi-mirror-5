dcdd1fce405a66f7158db62fd1b51c19a6d22123
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:22 +0000

    Add package pygeoip/0.2.7/pygeoip-0.2.7.tar.gz

---
+++ packages/pygeoip/pygeoip-0.2.7.tar.gz/pygeoip-0.2.7/tests/test_region.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+import pygeoip
+from tests.config import REGION_DB_PATH
+
+
+class TestGeoIPRegionFunctions(unittest.TestCase):
+    def setUp(self):
+        self.us_code = 'US'
+        self.us_hostname = 'apple.com'
+        self.us_ip = '17.172.224.47'
+        self.us_region_data = {
+            'region_name': 'CA',
+            'country_code': 'US'
+        }
+
+        self.gir = pygeoip.GeoIP(REGION_DB_PATH)
+
+    def testRegionByName(self):
+        region_name = self.gir.region_by_name(self.us_hostname)
+        self.assertEqual(region_name, self.us_region_data)
+
+    def testRegionByAddr(self):
+        region_name = self.gir.region_by_addr(self.us_ip)
+        self.assertEqual(region_name, self.us_region_data)
+
+    def testCountryCodeByName(self):
+        us_code = self.gir.country_code_by_name(self.us_hostname)
+        self.assertEqual(us_code, self.us_code)
+
+    def testCountryCodeByAddr(self):
+        us_code = self.gir.country_code_by_addr(self.us_ip)
+        self.assertEqual(us_code, self.us_code)
