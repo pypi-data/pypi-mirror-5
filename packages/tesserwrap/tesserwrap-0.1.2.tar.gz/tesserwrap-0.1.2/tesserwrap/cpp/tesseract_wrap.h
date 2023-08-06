a9c873abfac60afaf7ec70447632888bbd40b356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:02 +0000

    Add package tesserwrap/0.1.2/tesserwrap-0.1.2.tar.gz

---
+++ packages/tesserwrap/tesserwrap-0.1.2.tar.gz/tesserwrap-0.1.2/tesserwrap/cpp/tesseract_wrap.h
@@ -0,0 +1,34 @@
+#include <stdint.h>
+#include "tesseract_ext.h"
+
+#ifdef __cplusplus
+#  define TR_C_START           extern "C" {
+#  define TR_C_END             }
+#else
+#  define TR_C_START
+#  define TR_C_END
+#endif
+
+#define TESSERWRAP_CAPI
+
+TR_C_START
+
+typedef TessBaseAPIExt* TessH;
+
+TESSERWRAP_CAPI TessH Tesserwrap_Init(const char *datadir, const char *lang);
+TESSERWRAP_CAPI void Tesserwrap_Destroy(TessH tesserwrap);
+TESSERWRAP_CAPI void Tesserwrap_GetRectangle(TessH tesserwrap,
+    uint64_t *left, uint64_t *top,
+    uint64_t *width, uint64_t *height);
+TESSERWRAP_CAPI void Tesserwrap_SetRectangle(TessH tesserwrap,
+    uint64_t left, uint64_t top,
+    uint64_t width, uint64_t height);
+TESSERWRAP_CAPI void Tesserwrap_SetImage(TessH tesserwrap,
+    const unsigned char *picture, uint64_t size, uint64_t width, uint64_t height);
+TESSERWRAP_CAPI void Tesserwrap_SetPageSegMode(TessH tesserwrap,
+    tesseract::PageSegMode pageseg);
+TESSERWRAP_CAPI tesseract::PageSegMode Tesserwrap_GetPageSegMode(TessH tesserwrap);
+TESSERWRAP_CAPI const char *Tesserwrap_GetUTF8Text(TessH tesserwrap);
+TESSERWRAP_CAPI void Tesserwrap_Clear(TessH tesserwrap);
+
+TR_C_END
