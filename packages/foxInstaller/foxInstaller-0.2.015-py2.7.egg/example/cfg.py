a4d08798e9b10a6def15f424fcb732dda6bc2354
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:25 +0000

    Add package foxInstaller/0.2.015/foxInstaller-0.2.015-py2.7.egg

---
+++ packages/foxInstaller/foxInstaller-0.2.015-py2.7.egg/example/cfg.py
@@ -0,0 +1,11 @@
+# cfg.py
+
+basic_config = {
+    "PROJECT_NAME": "Test",
+    "VENV_DIR": "venv",
+    "INSTALL_DIR": "/srv/%(project_name)s",
+    "USERNAME": "ubuntu",
+    "USERGROUP": "ubuntu",
+    "DOMAIN": "example.com",
+    "PORT": 8000,
+}
