c211ee47a6824915d9b129b58b73a53bb33c088b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:53 +0000

    Add package nose-pattern-exclude/0.1.2/nose-pattern-exclude-0.1.2.tar.gz

---
+++ packages/nose-pattern-exclude/nose-pattern-exclude-0.1.2.tar.gz/nose-pattern-exclude-0.1.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+try:
+    import multiprocessing
+except ImportError:
+    pass
+
+
+setup(
+    name="nose-pattern-exclude",
+    version='0.1.2',
+    author="Jakub Roztocil",
+    author_email="jakub@roztocil.name",
+    description="Exclude specific files and directories from nosetests runs.",
+    long_description=open('README.rst').read().strip(),
+    license='BSD',
+    url="http://github.com/jkbr/nose-pattern-exclude",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Testing",
+        "Programming Language :: Python",
+    ],
+    py_modules=['nose_pattern_exclude'],
+    zip_safe=False,
+    entry_points={
+        'nose.plugins': [
+            'nose_pattern_exclude = nose_pattern_exclude:NosePatternExclude'
+        ]
+    },
+    install_requires=['nose'],
+)
