2a225dca32c620fc6de11b59f8701f013972ee12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:37 +0000

    Add package django_scaff/0.2rc1/django_scaff-0.2rc1.tar.gz

---
+++ packages/django_scaff/django_scaff-0.2rc1.tar.gz/django_scaff-0.2rc1/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name="django_scaff",
+    version="0.2rc1",
+    description="Scaffold creator for Django 1.5+ projects",
+    long_description="Please see the Github page for details: http://github.com/juliosmelo/django-scaffold",
+    author="Julio Siveira Melo",
+    author_email="juliocsmelo@gmail.com",
+    url="https://github.com/juliosmelo/django_scaffold/",
+    packages=[
+        "management",
+        "management.commands",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ]
+)
