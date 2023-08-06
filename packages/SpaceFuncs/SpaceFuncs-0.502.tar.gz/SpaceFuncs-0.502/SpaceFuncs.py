fe306aec024d2ba4c1c36796edd1e5c5b5875a02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:39 +0000

    Add package SpaceFuncs/0.502/SpaceFuncs-0.502.tar.gz

---
+++ packages/SpaceFuncs/SpaceFuncs-0.502.tar.gz/SpaceFuncs-0.502/SpaceFuncs.py
@@ -0,0 +1,10 @@
+# created by Dmitrey
+import os,sys
+Sep = os.sep
+sfPath = ''.join(elem+Sep for elem in __file__.split(Sep)[:-1])
+sys.path.append(sfPath + 'kernel')
+from baseObjects import Point, Line, LineSegment, Plane,  Circle, Disk, skewLinesNearestPoints, Sphere, Ball
+for name in ['Triangle', 'Tetrahedron', 'Polygon', 'Polytope']:
+    exec('from %s import %s' % (name, name))
+from kernel.Plot import Plot
+from numpy import pi
---
+++ packages/SpaceFuncs/SpaceFuncs-0.502.tar.gz/SpaceFuncs-0.502/examples/polygon.py
@@ -0,0 +1,26 @@
+from FuncDesigner import *
+from SpaceFuncs import *
+spaceDimension = 2
+nVertices = 15
+parametrizedVerticesCoords = oovars(nVertices, size=spaceDimension)# Python list of 15 oovars, each one of size spaceDimension
+P = Polygon(parametrizedVerticesCoords)
+
+parameterValues = dict([(parametrizedVerticesCoords[i], (sin(2 * pi * i / nVertices), cos(2 * pi  * i  / nVertices))) for i in range(nVertices)])
+#print(P.sides(parameterValues))
+#print(P.P(parameterValues))# perimeter: 6.23735072453
+#print(P.S(parameterValues))# area: 3.05052482307
+#areaPlusPerimeter = P.S + P.P
+
+#print('for these params area + perimeter equals to ' + str(areaPlusPerimeter(parameterValues)))# 9.2878755476 for nVertices = 15
+#
+#Plot(P(parameterValues))
+
+
+
+v1,v2,v3 = Point(0,0,weight=10),Point(1,0,weight = 20),Point(0,30,weight = 40) 
+v1,v2,v3 = Point(100,0),Point(1,0),Point(0,90) 
+r = Polytope(v1,v2,v3).M
+r2 = Triangle(v1,v2,v3).H
+
+print('r2.shape: ', r2.shape)
+print('r2: ', r2)
