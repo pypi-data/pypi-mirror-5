e3a620d4ffcc7e86c5a149790c4cdf14e9e78e7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:52 +0000

    Add package prettyplotlib/0.1.0/prettyplotlib-0.1.0.tar.gz

---
+++ packages/prettyplotlib/prettyplotlib-0.1.0.tar.gz/prettyplotlib-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='prettyplotlib',
+    version='0.1.0',
+    author='Olga B. Botvinnik',
+    author_email='olga.botvinnik@gmail.com',
+    packages=['prettyplotlib'],
+    license='LICENSE.txt',
+    description='Painlessly create beautiful default `matplotlib` plots.',
+    long_description=open('README.md').read()
+)
