543eca60c8946940ef2b758dcf92e8070e3d93c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:32 +0000

    Add package junos-multi-command/0.1/junos-multi-command-0.1.tar.gz

---
+++ packages/junos-multi-command/junos-multi-command-0.1.tar.gz/junos-multi-command-0.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='junos-multi-command',
+    version='0.1',
+    packages=['junos-multi-command',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
