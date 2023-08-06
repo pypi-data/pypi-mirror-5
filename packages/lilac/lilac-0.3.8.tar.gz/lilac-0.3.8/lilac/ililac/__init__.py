da7976a644c5b0be03288dd5340c073d306ccf51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:42 +0000

    Add package lilac/0.3.8/lilac-0.3.8.tar.gz

---
+++ packages/lilac/lilac-0.3.8.tar.gz/lilac-0.3.8/lilac/ililac/__init__.py
@@ -0,0 +1,47 @@
+#coding=utf8
+
+"""Usage:
+  ililac start
+  ililac stop
+  ililac restart
+  ililac status
+"""
+
+from daemon import Daemon
+from lilac.server import server
+from lilac.cli import docopt
+from lilac.logger import logger
+import logging
+import sys
+
+
+class LilacDaemon(Daemon):
+
+    def run(self):
+        logger.setLevel(logging.ERROR)
+        server.run(True, 8888)
+        logger.setLevel(logging.INFO)
+
+
+lilac_daemon = LilacDaemon("/tmp/lilac-daemon.pid", stdout="/dev/stdout")
+
+
+def main():
+    dct = docopt(__doc__)
+    # set logger level to info
+    logger.setLevel(logging.INFO)
+
+    if dct["start"]:
+        lilac_daemon.start()
+    elif dct["stop"]:
+        lilac_daemon.stop()
+    elif dct["restart"]:
+        lilac_daemon.restart()
+    elif dct["status"]:
+        lilac_daemon.status()
+    else:
+        exit(__doc__)
+
+
+if __name__ == '__main__':
+    main()
