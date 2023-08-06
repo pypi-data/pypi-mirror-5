c520cba9033daa709e75b8b9b99b1acaaeef0b90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:03 +0000

    Add package django-smart-settings/0.1.0/django-smart-settings-0.1.0.tar.gz

---
+++ packages/django-smart-settings/django-smart-settings-0.1.0.tar.gz/django-smart-settings-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+	name='django-smart-settings',
+	version='0.1.0',
+	author='ERt',
+	author_email='ert.wroc@gmail.com',
+	packages=['django_smart_settings'],
+	description='Helper for django settings',
+	url='https://github.com/ertwroc/django-smart-settings',
+	license='LICENSE.txt',
+	long_description=open('README.md').read(),
+)
