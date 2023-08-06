d87a8ed4c850b105ab46cee9096b83e50293eb74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:59 +0000

    Add package scipy/0.12.1/scipy-0.12.1.tar.gz

---
+++ packages/scipy/scipy-0.12.1.tar.gz/scipy-0.12.1/scipy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM SCIPY SETUP.PY
+short_version = '0.12.1'
+version = '0.12.1'
+full_version = '0.12.1'
+git_revision = '4b569aec3033c28b5bf3c432a0e7e8a9dce2ff1a'
+release = True
+
+if not release:
+    version = full_version
