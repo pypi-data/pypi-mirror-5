16470baa0a1c34b963035bf6cf02ffb25ec28238
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:36 +0000

    Add package unicards/0.4/unicards-0.4.tar.gz

---
+++ packages/unicards/unicards-0.4.tar.gz/unicards-0.4/setup.py
@@ -0,0 +1,31 @@
+#!/bin/env python
+# -*- coding: utf8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='unicards',
+    version='0.4',
+    description="Convert strings into unicode playing cards",
+    author='Luke Macken',
+    author_email='lmacken@redhat.com',
+    url='http://github.com/lmacken/unicards',
+    license='GPLv3+',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Topic :: Software Development",
+        "Topic :: Utilities",
+        "Topic :: Games/Entertainment",
+    ],
+    keywords='python unicode cards poker deck',
+    packages=find_packages(exclude=['ez_setup', 'tests']),
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[],
+)
