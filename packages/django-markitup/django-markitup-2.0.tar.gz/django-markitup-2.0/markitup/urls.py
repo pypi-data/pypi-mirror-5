a066e075b907bcc4b70cb4e80247c093bb434e38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:57 +0000

    Add package django-markitup/2.0/django-markitup-2.0.tar.gz

---
+++ packages/django-markitup/django-markitup-2.0.tar.gz/django-markitup-2.0/markitup/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, url
+
+from markitup.views import apply_filter
+
+urlpatterns = patterns(
+    '',
+    url(r'preview/$', apply_filter, name='markitup_preview')
+    )
