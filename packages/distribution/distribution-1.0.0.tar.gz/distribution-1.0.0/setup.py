7a593de772a25ea6ab493885dc5b5e254e532cb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:34 +0000

    Add package distribution/1.0.0/distribution-1.0.0.tar.gz

---
+++ packages/distribution/distribution-1.0.0.tar.gz/distribution-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+		name = "distribution",
+		version = "1.0.0",
+		author = "shawn",
+		py_modules = ['distribution'],
+		url = "http://www.baidu.com",
+	 )
