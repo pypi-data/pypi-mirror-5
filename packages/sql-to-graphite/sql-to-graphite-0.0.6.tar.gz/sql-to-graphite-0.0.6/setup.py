c57869baf3bef2d585ff1e98cc29dccc713d35a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:15 +0000

    Add package sql-to-graphite/0.0.6/sql-to-graphite-0.0.6.tar.gz

---
+++ packages/sql-to-graphite/sql-to-graphite-0.0.6.tar.gz/sql-to-graphite-0.0.6/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+def run_setup():
+    setup(
+        name='sql-to-graphite',
+        version='0.0.6',
+        description='A tool to send SQL results to Graphite',
+        keywords = 'SQL Graphite',
+        url='http://github.com/philipcristiano/sql-to-graphite',
+        author='Philip Cristiano',
+        author_email='philipcristiano@gmail.com',
+        license='BSD',
+        packages=['sql_to_graphite'],
+        install_requires=[
+            'sqlalchemy',
+            'mysql-python',
+        ],
+        test_suite='tests',
+        long_description=read('README.md'),
+        zip_safe=True,
+        classifiers=[
+        ],
+        entry_points="""
+        [console_scripts]
+        sql-to-graphite=sql_to_graphite:main
+        """,
+    )
+
+if __name__ == '__main__':
+    run_setup()
