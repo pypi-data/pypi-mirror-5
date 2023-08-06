013ca110c7c29ed73dc74366833e73d65c0ba75e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:49 +0000

    Add package easyimap/0.3.9/easyimap-0.3.9.tar.gz

---
+++ packages/easyimap/easyimap-0.3.9.tar.gz/easyimap-0.3.9/easyimap/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""
+easyimap
+~~~~~~~~
+
+Easy Imap wrapper.
+
+:license: BSD, see LICENSE.txt for more details.
+"""
+
+__title__ = 'easyimap'
+__version__ = '0.3.8'
+
+from .easyimap import connect
+
+__all__ = ['connect']
