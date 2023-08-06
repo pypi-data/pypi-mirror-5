4af7515694c988709555bd29b121809357f6f271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:38 +0000

    Add package douban/0.0.1/douban-0.0.1.tar.gz

---
+++ packages/douban/douban-0.0.1.tar.gz/douban-0.0.1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='douban',
+      version='0.0.1',
+      keywords='douban',
+      description='douban egg',
+      license='MIT License',
+      url='http://liluo.org',
+      author='liluo',
+      author_email='i@liluo.org',
+
+      py_modules=['douban'],
+      include_package_data=True,
+      platforms='any',
+      install_requires=[])
