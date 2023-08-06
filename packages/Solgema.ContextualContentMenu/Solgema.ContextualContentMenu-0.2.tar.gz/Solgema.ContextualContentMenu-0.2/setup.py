4e05ad7c7c211cb27c70aba60e43fc142d8d0c7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:33 +0000

    Add package Solgema.ContextualContentMenu/0.2/Solgema.ContextualContentMenu-0.2.tar.gz

---
+++ packages/Solgema.ContextualContentMenu/Solgema.ContextualContentMenu-0.2.tar.gz/Solgema.ContextualContentMenu-0.2/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.2'
+
+setup(name='Solgema.ContextualContentMenu',
+      version=version,
+      description="Solgema Contextual Content Menu",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.rst")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='Solgema, menu, javascript menu',
+      author='Martronic SA',
+      author_email='martronic@martronic.ch',
+      url='http://www.martronic.ch/Solgema/plone_products/solgema_contextualcontentmenu',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Solgema'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
