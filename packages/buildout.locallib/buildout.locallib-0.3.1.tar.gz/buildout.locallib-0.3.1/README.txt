546ea9f7594d8a7ace4e29cb32d82467bccb2125
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:29 +0000

    Add package buildout.locallib/0.3.1/buildout.locallib-0.3.1.tar.gz

---
+++ packages/buildout.locallib/buildout.locallib-0.3.1.tar.gz/buildout.locallib-0.3.1/README.txt
@@ -0,0 +1,189 @@
+`buildout.locallib` use egg-packages installed on site-packages folder.
+
+`zc.buildout <http://pypi.python.org/pypi/zc.buildout>`_ check
+package installation by zc.buildout.easy_install.Installer that
+exclude /path/to/site-packages folder path for checking.
+
+When 'site-packages' folder include some easy-installed packages,
+We want to re-use these packages in some cases.
+
+Features
+--------
+
+* Extends zc.buildout functionality to search installed packages.
+* create dummy '*.egg-info' / '*.egg-link' for specified packages.
+
+
+Using sample: using buildout.locallib extension
+-------------------------------------------------
+
+site-packages include some eggs::
+
+   $ ls /path/to/site-packages
+   easy-install.pth
+   pip-0.6.3-py2.6.egg/
+   pastedeploy-1.3.3-py2.6.egg/
+   pastescript-1.7.3-py2.6.egg/
+   paste-1.7.4-py2.6.egg/
+   setuptools-0.6c11-py2.6.egg
+   setuptools.pth
+
+
+write buildout.cfg::
+
+   [buildout]
+   parts = foo
+
+   [foo]
+   recipe = zc.recipe.egg
+   eggs =
+       PasteScript
+
+
+run bootstrap and buildout::
+
+   $ python bootstrap.py
+   $ bin/buildout -U
+   ...
+
+list eggs dir::
+
+   $ ls eggs
+   zc.recipe.egg-1.2.3b2-py2.6.egg
+   pastescript-1.7.3-py2.6.egg
+   pastedeploy-1.3.3-py2.6.egg
+   paste-1.7.4-py2.6.egg
+
+(clean-up)::
+
+   $ rm -R eggs
+
+Now update buildout.cfg to use ``buildout.locallib`` extension::
+
+   [buildout]
+   parts = foo
+   extensions = buildout.locallib
+   ...
+
+And run buildout again::
+
+   $ bin/buildout -U
+   ...
+
+Check your eggs folder again::
+
+   $ ls eggs
+   buildout.locallib-0.0.1-py2.6.egg
+   zc.recipe.egg-1.2.3b2-py2.6.egg
+
+If package exists at site-packages folder, now using installed version
+packages.
+
+
+Using sample: specified dummy package information
+--------------------------------------------------
+
+site-packages include some libraries, but they are not installed by easy_insttall::
+
+   $ ls /path/to/site-packages
+   ...
+   feedparser.py
+   ...
+
+write buildout.cfg with ``locallibs`` key and section::
+
+   [buildout]
+   parts = foo
+   newest = false #if you won't want to check newest version (network-access)
+   extensions = buildout.locallib
+   locallibs = locallibs
+
+   [foo]
+   recipe = zc.recipe.egg
+   eggs =
+       feedparser
+
+   [locallibs]
+   feedparser = feedparser==4.1
+
+``locallibs`` section require ``(dummy name) = (pkg name)==(version)`` style
+key-value pairs. Then, buildout.locallib create dummy ``feedparser-4.1.egg-info``
+into the develop-eggs directory. In this way, setuptools recognizes that a
+'feedparser' is installed and doesn't perform downloading.
+
+If target package have .egg-info file/directory, you don't need to write
+a `version` like below::
+
+   [locallibs]
+   PIL = PIL
+
+But, if you omit a version for no-egg-info package, buildout.locallib can't
+recognize package version, then it'll cause DistributionNotFound exception.
+If you want to avoid this exception, you should set `locallibs_check = false`
+in buildout section.
+
+Options
+--------
+locallibs
+   A dictionary mapping package names to skip installing distribution
+   that was installed on python's site-packages directory already.
+   This can be used to specify a set of distribution versions independent
+   of other requirements.
+
+locallibs_check
+   default is 'true'. buildout.locallib check distribution existing on
+   site-packages and raise exception if distribute was not found.
+   If set 'false', skip check.
+
+
+Requirements
+------------
+
+* Python 2.4 or later
+
+
+Dependencies
+------------
+
+* `setuptools <http://pypi.python.org/pypi/setuptools>`_ or
+  `distribute <http://pypi.python.org/pypi/distribute>`_
+
+* `zc.buildout <http://pypi.python.org/pypi/zc.buildout>`_
+
+
+Setup
+-----
+
+Write buildout.cfg with `extensions = buildout.locallib`::
+
+   [buildout]
+   parts = foo
+   extensions = buildout.locallib
+   ...
+
+
+ToDo
+-----
+* Write tests.
+* cleanup develop-eggs folder.
+
+
+History
+-------
+
+0.3.1 (2013-8-18)
+~~~~~~~~~~~~~~~~~~
+* Fix: create develop-eggs directory if missing.
+
+0.3.0 (2011-3-31)
+~~~~~~~~~~~~~~~~~~
+* Add: locallibs_check option is now available.
+
+0.2.0 (2010-7-12)
+~~~~~~~~~~~~~~~~~~
+* Add: create dummy '*.egg-info' / '*.egg-link' for specified packages.
+
+0.1.0 (2010-6-27)
+~~~~~~~~~~~~~~~~~~
+* first release
+
---
+++ packages/buildout.locallib/buildout.locallib-0.3.1.tar.gz/buildout.locallib-0.3.1/src/buildout/locallib/extension.py
@@ -0,0 +1,130 @@
+from zc.buildout import easy_install
+from distutils.sysconfig import get_python_lib
+import pkg_resources
+import os
+import sys
+import logging
+logger = logging.getLogger('buildout.locallib')
+
+SP_PATH = get_python_lib()
+
+class Installer(easy_install.Installer):
+    """ hook easy_install.Installer() for egg search path modification """
+
+    base = None
+
+    def __init__(self, *args, **kw):
+        # if exist args[5] (is path parameter) or 'path' keyword argument,
+        # update path list for including 'site-packages' path.
+
+        logger.debug('easy_install.Installer hooked.')
+        if len(args) > 5:
+            path = args[5]
+        elif kw.get('path') is not None:
+            path = kw['path']
+        else:
+            path = kw['path'] = []
+
+        if SP_PATH not in path:
+            path.append(SP_PATH)
+
+        self.base.__init__(self, *args, **kw)
+
+    __init__.func_doc = easy_install.Installer.__init__.func_doc
+
+
+def patch_to_Installer(buildout):
+    if easy_install.Installer != Installer:
+        logger.debug('easy_install.Installer patched')
+        Installer.base = easy_install.Installer
+        easy_install.Installer = Installer
+
+def create_dummy_egglink(base, name, location):
+    link_name = '%s.egg-link' % name
+    link_path = os.path.join(base, link_name)
+    if not os.path.exists(link_path):
+        logger.info('create dummy egg-link: %s', link_path)
+        f = open(link_path, 'wt')
+        f.write(location)
+        f.write("\n../\n")
+        f.close()
+    else:
+        # file was exist. need update?
+        pass
+
+def create_dummy_egginfo(base, name, version=None):
+    if not os.path.exists(base):
+        os.makedirs(base)
+    if version is None:
+        egg_name = '%(name)s.egg-info' % locals()
+    else:
+        egg_name = '%(name)s-%(version)s.egg-info' % locals()
+    egg_path = os.path.join(base, egg_name)
+    if not os.path.exists(egg_path):
+        logger.info('create dummy egg-info: %s', egg_path)
+        open(egg_path, 'wt').close() # create empty file.
+
+
+def construct_dummy_infos(buildout):
+    # TODO: print warning if newest=true.
+    # this extension use site-package's package, but if newest mode is
+    # enabled then setuptools get newest version than specified version
+    # at 'locallibs' parameter.
+    # if you using locallibs parameter, run buildout with -N options.
+
+    bo = buildout['buildout']
+
+    if 'locallibs' in bo:
+        locallibs = buildout.get(bo['locallibs'], {})
+    else:
+        return # buildout secsion has no 'locallibs' key.
+
+    locallibs_check = bo.get('locallibs-check', 'true')
+    locallibs_check = locallibs_check.lower() != 'false'
+
+    for key,name in locallibs.items():
+        req = pkg_resources.Requirement.parse(name)
+        proceeded = False
+        try:
+            info = pkg_resources.get_provider(req)
+            # expected package are exist (include no-version-specified).
+            # Installer patch make to find this.
+            create_dummy_egglink(
+                    bo['develop-eggs-directory'],
+                    info.key, info.location)
+            create_dummy_egginfo(
+                    bo['develop-eggs-directory'],
+                    info.key, info.version)
+            proceeded = True
+        except pkg_resources.VersionConflict,e:
+            # find another version (except no-version-specified).
+            create_dummy_egginfo(
+                    bo['develop-eggs-directory'],
+                    req.key, req.specs[0][1])
+            proceeded = True
+        except pkg_resources.DistributionNotFound,e:
+            # maybe using 'simple installed package' (no egg-info extention)
+            if req.specs:
+                create_dummy_egginfo(
+                        bo['develop-eggs-directory'],
+                        req.key, req.specs[0][1])
+                proceeded = True
+
+        if not proceeded:
+            if locallibs_check:
+                raise pkg_resources.ExtractionError(
+                    "Can't load `locallibs` specified package %r by %r. " \
+                    "If you using NON-EGGIFY-PACKAGE, you need set " \
+                    "'key = name==version' line format."
+                    % (req.key, sys.executable))
+
+            create_dummy_egginfo(
+                    bo['develop-eggs-directory'],
+                    req.key)
+            proceeded = True
+
+
+def load(buildout):
+    patch_to_Installer(buildout)
+    construct_dummy_infos(buildout)
+
