11dd30338d94966f6e27c29436dcf5739057c761
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:35 +0000

    Add package pydatagovph/1.4/pydatagovph-1.4.tar.gz

---
+++ packages/pydatagovph/pydatagovph-1.4.tar.gz/pydatagovph-1.4/pydatagovph/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "1.4"
+__app_name__ = "pydatagovph"
+__description__ = "Python API Client for data.gov.ph"
+__author__ = "Ferdinand Silva"
+__author_email__ = "ferdinandsilva@ferdinandsilva.com"
+__app_url__ = "http://ferdinandsilva.com"
+__download_url__ = "https://github.com/six519/PyDataGovPH"
+
+from .core import *
