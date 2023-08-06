32db943305eaa5e76b23a50a074f89ec4cae1e5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:50 +0000

    Add package django-data-exports/0.5/django-data-exports-0.5.tar.gz

---
+++ packages/django-data-exports/django-data-exports-0.5.tar.gz/django-data-exports-0.5/data_exports/test_urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls.defaults import patterns, url, include
+from django.contrib import admin
+
+
+admin.autodiscover()
+
+
+urlpatterns = patterns(
+    '',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^exports/', include('data_exports.urls', namespace='data_exports')),
+)
