8331ed6219551e02705e45564603328e1586ccf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:04 +0000

    Add package tissue/0.9.2/tissue-0.9.2.tar.gz

---
+++ packages/tissue/tissue-0.9.2.tar.gz/tissue-0.9.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+version = '0.9.2'
+
+setup(
+    name='tissue',
+    version=version,
+    description='Tissue - automated pep8 checker for nose',
+    long_description=open('README.rst').read(),
+    classifiers=[],
+    keywords='pep8 nose',
+    author='Jason K\xc3\xb6lker & Rick van Hattem',
+    author_email='rick@wol.ph',
+    url='https://github.com/WoLpH/tissue',
+    license='GNU LGPL',
+    py_modules=['tissue'],
+    install_requires=[
+        'nose',
+        'pep8',
+    ],
+    entry_points={
+        'nose.plugins': [
+            'tissue = tissue:Tissue',
+        ],
+    },
+)
