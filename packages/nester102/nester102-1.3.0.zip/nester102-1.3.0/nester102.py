cbae7ba23d25d9a07f4110419985bc9fe94c9419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:13 +0000

    Add package nester102/1.3.0/nester102-1.3.0.zip

---
+++ packages/nester102/nester102-1.3.0.zip/nester102-1.3.0/nester102.py
@@ -0,0 +1,10 @@
+"""test"""
+def lol(the_list, k=0):
+    """the blank key is horrible."""
+    for le in the_list:
+        if isinstance(le,list):
+            lol(le,k+1)
+        else:
+            for sth in range(k):
+                print("\t", end='')
+            print(le)
