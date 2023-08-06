22052a68b56d1e1ec2d971395bf1c127cd28011e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:18 +0000

    Add package pypeaks/0.2.2/pypeaks-0.2.2.tar.gz

---
+++ packages/pypeaks/pypeaks-0.2.2.tar.gz/pypeaks-0.2.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='pypeaks',
+      version='0.2.2',
+      description='Python module with different methods to identify peaks from data like histograms and time-series data',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+      keywords='python peaks histogram time-series maxima minima',
+      url='https://github.com/gopalkoduri/pypeaks',
+      author='Gopala Krishna Koduri',
+      author_email='gopala.koduri@gmail.com',
+      license='GNU Affero GPL v3',
+      packages=['pypeaks'],
+      data_files=[('examples', ['howto.ipynb', 'howto.py', 'ji-intervals.pickle' 'sample-histogram.pickle'])],
+      install_requires=[
+          'numpy',
+          'matplotlib',
+      ],
+      zip_safe=False)
