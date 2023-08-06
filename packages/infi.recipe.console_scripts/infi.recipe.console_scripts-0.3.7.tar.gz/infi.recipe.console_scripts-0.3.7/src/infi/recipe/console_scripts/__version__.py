c26f90e5f5df64432baa3fea6b3313be64ecc46e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:59 +0000

    Add package infi.recipe.console_scripts/0.3.7/infi.recipe.console_scripts-0.3.7.tar.gz

---
+++ packages/infi.recipe.console_scripts/infi.recipe.console_scripts-0.3.7.tar.gz/infi.recipe.console_scripts-0.3.7/src/infi/recipe/console_scripts/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.3.7"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '13390529e8c5268acb23c2652591d41e1baac39d'
+__git_head_subject__ = 'Finished release v0.3.7.'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
