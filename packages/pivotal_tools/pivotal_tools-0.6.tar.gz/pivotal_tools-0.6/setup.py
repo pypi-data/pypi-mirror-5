12cb708dff62a84c1006ceb8d7742e1be6696cae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:01 +0000

    Add package pivotal_tools/0.6/pivotal_tools-0.6.tar.gz

---
+++ packages/pivotal_tools/pivotal_tools-0.6.tar.gz/pivotal_tools-0.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='pivotal_tools',
+      version='0.6',
+      description='Collection of pivotal command line tools',
+      url='http://github.com/jtushman/pivotal_readme',
+      author='Jonathan Tushman',
+      author_email='jonathan.tushman@zefr.com',
+      license='MIT',
+      packages=['pivotal_tools'],
+      install_requires=[
+          'requests',
+          'docopt',
+          'termcolor'
+      ],
+      entry_points={
+          'console_scripts': ['pivotal_tools = pivotal_tools:main']
+      },
+      zip_safe=False)
