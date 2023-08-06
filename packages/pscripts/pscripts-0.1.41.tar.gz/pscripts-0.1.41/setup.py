5e3a422c2b2a3a6f4e987398aba429029a02f8a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:26 +0000

    Add package pscripts/0.1.41/pscripts-0.1.41.tar.gz

---
+++ packages/pscripts/pscripts-0.1.41.tar.gz/pscripts-0.1.41/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+version='0.1.41'
+name='pscripts'
+scripts = [
+    'scripts/python-deployment',
+    'scripts/hdmi_brightness'    
+]
+classifiers = [
+        'Programming Language :: Python :: 3.3',
+    ]
+setup(
+    name = name,
+    version = version,
+    packages = [name],
+    description = 'Automation Scripts for Linux',
+    author='Fenton Travers',
+    author_email='fenton.travers@gmail.com',
+    url='www.google.com',
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description='Automates some python deployment steps',
+    classifiers=classifiers,
+    scripts = scripts
+)
