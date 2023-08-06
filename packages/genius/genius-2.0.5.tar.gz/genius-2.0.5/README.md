9ffa540fa1afa1294cb83bd55efab18becab54a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:41 +0000

    Add package genius/2.0.5/genius-2.0.5.tar.gz

---
+++ packages/genius/genius-2.0.5.tar.gz/genius-2.0.5/README.md
@@ -0,0 +1,66 @@
+genius
+========
+genius中文分词，是基于crf条件随机场的分组件
+
+
+Feature
+========
+
+* 支持pinyin分词
+* 支持用户自定义break
+* 支持用户自定义词典
+* 支持词性标注
+
+Install
+==========
+* 安装git: 1) ubuntu or debian `apt-get install git` 2) fedora or redhat `yum install git`
+* 升级setuptools: `pip install easy_install --upgrade`
+* 升级pip: `pip install pip --upgrade`
+* `pip install genius`
+* 通过`import genius`来引用
+
+
+Algorithm
+==========
+* 采用trie树进行词典查找
+* 基于wapiti实现条件随机场分词
+* 可以通过genius.loader.ResourceLoader来重载默认的字典
+
+功能 1)：分词`genius.seg_text`方法
+==============
+
+* `genius.seg_text`函数接受5个参数: 
+* `text`第一个参数为需要分词的字符 
+* `use_break`代表对分词结构进行打断处理 
+* `use_combine`代表是否使用字典进行词合并
+* `use_tagging`代表是否进行词性标注
+* `use_pinyin_segment`代表是否对拼音进行分词处理
+
+代码示例( 全功能分词 )
+
+    #encoding=utf-8
+    import genius
+
+    seg_list = genius.seg_text(
+        u'中国人民站起来了pinyin',
+        use_combine=True,
+        use_pinyin_segment=True,
+        use_tagging=True)
+    print '\n'.join(seg_list)
+
+功能 2)：面向索引分词
+==============
+* `genius.seg_keywords`方法专门为搜索引擎索引准备，保留歧义分割。
+* `text`第一个参数为需要分词的字符 
+* `use_break`代表对分词结构进行打断处理 
+* `use_tagging`代表是否进行词性标注
+* `use_pinyin_segment`代表是否对拼音进行分词处理
+* 由于合并操作与此方法有意义上的冲突，此方法并不提供合并功能；并且如果采用此方法做索引时候，检索时不推荐`genius.seg_text`使用`use_combine=True`参数。
+
+代码示例
+
+    #encoding=utf-8
+    import genius
+
+    seg_list = genius.seg_keywords(u'中国人民站起来了pinyin')
+    print '\n'.join(seg_list)
---
+++ packages/genius/genius-2.0.5.tar.gz/genius-2.0.5/genius/process.py
@@ -0,0 +1,232 @@
+#encoding:utf-8
+from .trie import TrieTree
+from .loader import ResourceLoader
+from .tools import StringHelper
+from .digital import is_chinese_number, chinese_to_number
+
+
+class SimpleSegmentProcess(object):
+
+    def __init__(self):
+        self.loader = ResourceLoader()
+        self.seg_model = self.loader.load_crf_seg_model()
+
+    def process(self, text):
+        groups = StringHelper.group_ascii_cjk(text)
+        pre_words = []
+        for group in groups:
+            mark = StringHelper.mark(group)
+            if mark == 'ASCII':
+                pre_words.append(group)
+            elif mark == 'PUNC':
+                pre_words.append(group)
+            else:
+                label = self.label_sequence(group)
+                words = self.segment(label)
+                if words:
+                    pre_words.extend(words)
+                else:
+                    pre_words.extend(group)
+        return pre_words
+
+    def label_sequence(self, words, nbest=1):
+        self.seg_model.options.nbest = nbest
+        label = self.seg_model.label_sequence(
+            '\n'.join(words), include_input=True).decode('utf-8')
+        return label
+
+    @classmethod
+    def segment(cls, label):
+        result_words = []
+        prev_words = []
+        for word_label in filter(lambda x: x.strip('\n'), label.split('\n')):
+            word, label = word_label.split('\t')
+            if 'S' in label:
+                if prev_words:  # 这是一种错误的概率
+                    return []
+                result_words.append(word)
+            elif 'E' in label:
+                prev_words.append(word)
+                result_words.append(u''.join(prev_words))
+                prev_words = []
+            else:
+                prev_words.append(word)
+        result_words.extend(prev_words)
+        return result_words
+
+
+class KeywordsSegmentProcess(SimpleSegmentProcess):
+
+    def __init__(self):
+        SimpleSegmentProcess.__init__(self)
+        self.trie = self.loader.load_trie_tree()
+
+    def process(self, text):
+        length = len(text)
+        if length <= 3:
+            return self.crf_keywords(text, nbest=1)
+        else:
+            return self.crf_keywords(text, 2)
+
+    def crf_keywords(self, text, nbest=2):
+        groups = StringHelper.group_ascii_cjk(text)
+        pre_words = []
+        for group in groups:
+            mark = StringHelper.mark(group)
+            if mark == 'ASCII':
+                pre_words.append(group)
+            elif mark == 'PUNC':
+                pre_words.append(group)
+            else:
+                labels = self.label_sequence(group, nbest).split('\n\n')
+                words_list = filter(
+                    lambda x: x, [self.segment(label) for label in labels])
+                if words_list:
+                    pre_words.extend(self.merge_keywords(group, words_list))
+                else:
+                    pre_words.extend(group)
+        return pre_words
+
+    @classmethod
+    def merge_keywords(cls, text, words_list):
+        trie = TrieTree()
+        for words in words_list:
+            trie.add_all(words)
+        pos, length = 0, len(text)
+        pre_words = []
+        while pos < length:
+            dic = trie.search(text[pos:])
+            for i in range(pos + 1, length + 1):
+                word = text[pos:i]
+                if word in dic:
+                    pre_words.append(word)
+            pos += 1
+        return pre_words
+
+
+class PinyinSegmentProcess(object):
+
+    def __init__(self):
+        self.loader = ResourceLoader()
+        self.trie = self.loader.load_trie_tree()
+
+    def process(self, words):
+        pre_words = []
+        for word in words:
+            marker = StringHelper.mark(word)
+            if marker == 'ASCII':
+                pinyins = self.segment(word)
+                if pinyins:
+                    pre_words.extend(pinyins)
+                else:
+                    pre_words.append(word)
+            else:
+                pre_words.append(word)
+        return pre_words
+
+    def segment(self, text):
+        length = len(text)
+        pos = 0
+        pre_words = []
+        while pos < length:
+            dic = self.trie.search(text[pos:length])
+            max_matching_pos = 0
+            for i in range(pos + 1, length + 1):
+                if text[pos:i] in dic:
+                    max_matching_pos = i
+            if max_matching_pos == 0:
+                return text
+            else:
+                pinyin = text[pos:max_matching_pos]
+                pre_words.append(pinyin)
+            pos = max_matching_pos
+        return pre_words
+
+
+class BreakProcess(object):
+
+    def __init__(self):
+        self.loader = ResourceLoader()
+        self.tree = self.loader.load_break_table()
+
+    def process(self, word):
+        break_word_result = []
+        for term in word:
+            if term in self.tree:
+                break_word_result.extend(self.tree[term])
+            else:
+                break_word_result.append(term)
+        return break_word_result
+
+
+class CombineProcess(object):
+
+    def __init__(self):
+        self.loader = ResourceLoader()
+        self.trie = self.loader.load_trie_tree()
+
+    def process(self, words):
+        pos = 0
+        length = len(words)
+        pre_words = []
+        while pos < length:
+            word = words[pos]
+            max_matching_pos = 0
+            dic = self.trie.search(''.join(words[pos:length]))
+            for i in range(pos + 1, length + 1):
+                word = ''.join(words[pos:i])
+                if word in dic:
+                    max_matching_pos = i
+                elif is_chinese_number(word) and chinese_to_number(word):
+                    max_matching_pos = i
+            if max_matching_pos == 0:
+                max_matching_pos = pos + 1
+            pre_words.append(''.join(words[pos:max_matching_pos]))
+            pos = max_matching_pos
+        return pre_words
+
+
+class TaggingProcess(object):
+
+    def __init__(self):
+        self.loader = ResourceLoader()
+        self.tagging_model = self.loader.load_crf_pos_model()
+
+    def label_sequence(self, words, nbest=1):
+        self.seg_model.options.nbest = nbest
+        label_text = self.tagging_model.label_sequence(
+            ''.join([(
+                '%s\t%s\n' % (word, StringHelper.mark(word))) for word in words
+            ]),
+            include_input=True,
+        )
+        return label_text
+
+    def process(self, words):
+        label = self.label_sequence(words, nbest=1)
+        return self.tagging(label)
+
+    @classmethod
+    def tagging(cls, label):
+        result_words = []
+        for word_label in filter(lambda x: x, label.split('\n')):
+            text, marker, tagging = word_label.decode('utf-8').split('\t')
+            if StringHelper.is_number(text):
+                result_words.append('\t'.join((text, 'm')))
+            elif StringHelper.is_letter(text):
+                result_words.append('\t'.join((text, 'en')))
+            elif marker == 'PUNC':
+                result_words.append('\t'.join((text, 'w')))
+            else:
+                result_words.append('\t'.join((text, tagging)))
+        return result_words
+
+
+processes = {
+    'default': SimpleSegmentProcess,
+    'break': BreakProcess,
+    'combine': CombineProcess,
+    'tagging': TaggingProcess,
+    'pinyin_segment': PinyinSegmentProcess,
+    'segment_keywords': KeywordsSegmentProcess,
+}
