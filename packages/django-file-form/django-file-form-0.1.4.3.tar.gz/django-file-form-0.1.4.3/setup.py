f507f76ef4c6527946a3d172bde6930b2c9a89a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:33 +0000

    Add package django-file-form/0.1.4.3/django-file-form-0.1.4.3.tar.gz

---
+++ packages/django-file-form/django-file-form-0.1.4.3.tar.gz/django-file-form-0.1.4.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+version = '0.1.4.3'
+
+setup(
+    name='django-file-form',
+    version=version,
+    packages=find_packages(),
+    license='Apache License, Version 2.0',
+    include_package_data=True,
+    zip_safe=False,
+    author='Marco Braak',
+    author_email='mbraak@ridethepony.nl',
+    install_requires=['ajaxuploader==0.3.0.3', 'six', 'path.py'],
+    dependency_links=[
+        'https://github.com/mbraak/django-ajax-uploader/archive/0.3.0.3.tar.gz#egg=ajaxuploader-0.3.0.3',
+    ]
+)
