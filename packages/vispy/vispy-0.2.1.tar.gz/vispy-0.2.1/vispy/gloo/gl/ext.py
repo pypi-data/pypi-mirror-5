e9dac455054cde508a36ba108d2156c4f864db67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:01 +0000

    Add package vispy/0.2.1/vispy-0.2.1.tar.gz

---
+++ packages/vispy/vispy-0.2.1.tar.gz/vispy-0.2.1/vispy/gloo/gl/ext.py
@@ -0,0 +1,9 @@
+""" Namespace for functions and constants corresponding to 
+OpenGL ES 2.0 extensions.
+"""
+
+from __future__ import print_function, division, absolute_import
+
+from ._constants_ext import *
+
+# Filled with functions when vispy.gloo.gl is imported
