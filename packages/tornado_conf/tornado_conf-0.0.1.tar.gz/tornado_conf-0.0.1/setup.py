596160c16ef75e21adc68255792dce4100e817cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:26 +0000

    Add package tornado_conf/0.0.1/tornado_conf-0.0.1.tar.gz

---
+++ packages/tornado_conf/tornado_conf-0.0.1.tar.gz/tornado_conf-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='tornado_conf',
+    version='0.0.1',
+    packages=['tornado_conf'],
+    license='MIT',
+    author='Miphreal Adler',
+    author_email='miphreal@gmail.com',
+)
+
+
+
