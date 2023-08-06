b3961ed440a07acff516e238b7ed350a989e7280
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:22 +0000

    Add package shortuuid/0.4/shortuuid-0.4.tar.gz

---
+++ packages/shortuuid/shortuuid-0.4.tar.gz/shortuuid-0.4/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import sys
+assert sys.version >= '2.5', "Requires Python v2.5 or above."
+from setuptools import setup
+
+setup(
+    name = "shortuuid",
+    version = "0.4",
+    author = "Stochastic Technologies",
+    author_email = "info@stochastictechnologies.com",
+    url = "https://github.com/stochastic-technologies/shortuuid/",
+	description = "A generator library for concise, unambiguous and URL-safe UUIDs.",
+	long_description = "A library that generates short, pretty, unambiguous unique IDs "
+                       " by using an extensive, case-sensitive alphabet and omitting "
+                       "similar-looking letters and numbers.",
+	license = "BSD",
+    packages = ["shortuuid"],
+)
