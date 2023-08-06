a972339c1f206305b1d1a38f43fc4daa33f300ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:17 +0000

    Add package ftw.publisher.core/2.3.1/ftw.publisher.core-2.3.1.zip

---
+++ packages/ftw.publisher.core/ftw.publisher.core-2.3.1.zip/ftw.publisher.core-2.3.1/ftw/publisher/core/tests/interfaces.py
@@ -0,0 +1,19 @@
+from plone.directives import form
+from plone.namedfile.field import NamedFile
+from plone.namedfile.field import NamedImage
+
+
+class IFoo(form.Schema):
+    pass
+
+
+class IFileSchema(form.Schema):
+
+    form.primary('file')
+    file = NamedFile(title=u'File')
+
+
+class IImageSchema(form.Schema):
+
+    form.primary('image')
+    image = NamedImage(title=u'image')
