b85551d363162aeee433ea7ea3be89287432a8fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:10 +0000

    Add package django-poll/0.1.1/django-polls-0.1.1.tar.bz2

---
+++ packages/django-poll/django-polls-0.1.1.tar.bz2/polls/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from polls.models import Choice, Poll, UserChoice
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class PollAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+admin.site.register(Poll, PollAdmin)
+
+
+class UserChoiceAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(UserChoice, UserChoiceAdmin)
