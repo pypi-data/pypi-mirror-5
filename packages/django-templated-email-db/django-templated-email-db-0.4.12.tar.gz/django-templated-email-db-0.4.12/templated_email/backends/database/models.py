1637042b1a72f94f3b589605b8bf6c39cb32a27a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:49 +0000

    Add package django-templated-email-db/0.4.12/django-templated-email-db-0.4.12.tar.gz

---
+++ packages/django-templated-email-db/django-templated-email-db-0.4.12.tar.gz/django-templated-email-db-0.4.12/templated_email/backends/database/models.py
@@ -0,0 +1,16 @@
+from django.utils.translation import ugettext as _
+from django.db import models
+
+
+class EmailTemplate(models.Model):
+    name = models.CharField(_('Template name'), max_length=100, unique=True)
+    subject = models.CharField(_('Subject'), max_length=254)
+    text_message = models.TextField(_('Text message'), null=True, blank=True)
+    html_message = models.TextField(_('HTML message'), null=True, blank=True)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('Email template')
+        verbose_name_plural = _('Email templates')
