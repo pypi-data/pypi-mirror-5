827c26c668ccd0734b71fbc29952ba57ad715dac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:37 +0000

    Add package Products.CMFPlomino/1.18.1/Products.CMFPlomino-1.18.1.zip

---
+++ packages/Products.CMFPlomino/Products.CMFPlomino-1.18.1.zip/Products.CMFPlomino-1.18.1/Products/CMFPlomino/fields/events.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#
+# File: events.py
+#
+# Copyright (c) 2009 by ['Eric BREHAULT']
+#
+# Zope Public License (ZPL)
+#
+
+__author__ = """Eric BREHAULT <eric.brehault@makina-corpus.com>"""
+__docformat__ = 'plaintext'
+
+from Products.CMFPlomino.AppConfig import SCRIPTID_DELIMITER
+
+def afterFieldModified(obj, event):
+    """
+    """
+    obj.cleanFormulaScripts(SCRIPTID_DELIMITER.join(["field", obj.getPhysicalPath()[-2], obj.id]))
