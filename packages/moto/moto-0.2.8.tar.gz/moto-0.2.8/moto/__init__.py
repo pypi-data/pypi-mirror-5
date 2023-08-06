a2921a0661d34f5b4da480eaa51bdc3f7503bb71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:58 +0000

    Add package moto/0.2.8/moto-0.2.8.tar.gz

---
+++ packages/moto/moto-0.2.8.tar.gz/moto-0.2.8/moto/__init__.py
@@ -0,0 +1,11 @@
+import logging
+logging.getLogger('boto').setLevel(logging.CRITICAL)
+
+from .autoscaling import mock_autoscaling
+from .dynamodb import mock_dynamodb
+from .ec2 import mock_ec2
+from .elb import mock_elb
+from .s3 import mock_s3
+from .ses import mock_ses
+from .sqs import mock_sqs
+from .sts import mock_sts
