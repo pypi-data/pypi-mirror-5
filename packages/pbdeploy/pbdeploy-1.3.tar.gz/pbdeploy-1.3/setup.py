fb34eefa26be93a44f34607792fc413b659bfec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:06 +0000

    Add package pbdeploy/1.3/pbdeploy-1.3.tar.gz

---
+++ packages/pbdeploy/pbdeploy-1.3.tar.gz/pbdeploy-1.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='pbdeploy',
+    description='a port-based deployment framework for practicing continuous deployment',
+    version='1.3',
+    packages=['pbdeploy'],
+    scripts=['bin/pbdeploy'],
+    license='The MIT License',
+    author='Michael Rooney',
+    author_email='mrooney.pbdeploy@rowk.com',
+    url='https://github.com/mrooney/pbdeploy',
+    install_requires=['psutil'],
+)
