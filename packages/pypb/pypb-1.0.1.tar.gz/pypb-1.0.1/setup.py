22ff6f3509614537f731d201408ad02d50afdf05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:09 +0000

    Add package pypb/1.0.1/pypb-1.0.1.tar.gz

---
+++ packages/pypb/pypb-1.0.1.tar.gz/pypb-1.0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='pypb',
+    version='1.0.1',
+    author='Trey Stout',
+    author_email='treystout@gmail.com',
+    packages=['pypb', 'pypb.test'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/pypb/',
+    license='LICENSE.txt',
+    description='console-based progress bar',
+    long_description=open('README.txt').read()
+)
