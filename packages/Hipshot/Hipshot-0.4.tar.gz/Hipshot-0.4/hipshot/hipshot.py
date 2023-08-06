9533e389ea627758801085de6df965bb0baa022a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:30 +0000

    Add package Hipshot/0.4/Hipshot-0.4.tar.gz

---
+++ packages/Hipshot/Hipshot-0.4.tar.gz/Hipshot-0.4/hipshot/hipshot.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+
+'''Hipshot converts a video file into a single image
+simulating a long-exposure photograph.
+'''
+
+__author__ = 'Mansour Moufid'
+__copyright__ = 'Copyright 2013, Mansour Moufid'
+__license__ = 'ISC'
+__version__ = '0.4'
+__email__ = 'mansourmoufid@gmail.com'
+__status__ = 'Development'
+
+
+if __name__ == '__main__':
+    pass
