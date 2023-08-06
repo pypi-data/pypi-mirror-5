7d0e75dac164e1b89d26cac9d610d0d83363ed12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:04 +0000

    Add package clouds/0.1.a.dev/clouds-0.1.a.dev.tar.gz

---
+++ packages/clouds/clouds-0.1.a.dev.tar.gz/clouds-0.1.a.dev/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "clouds",
+    version = "0.1.a.dev",
+    packages = find_packages(),
+
+    author = "heightix",
+    author_email = "heightix@gmail.com",
+    url = "https://github.com/heightix/python-clouds",
+    license = "MIT",
+    classifiers = [
+        "Development Status :: 1 - Planning",
+        "Environment :: Web Environment",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.3",
+        "Topic :: Communications :: File Sharing",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
+
