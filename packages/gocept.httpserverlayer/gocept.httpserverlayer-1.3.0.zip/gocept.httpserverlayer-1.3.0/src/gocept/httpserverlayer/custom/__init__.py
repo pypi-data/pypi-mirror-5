de6ba581d49cb07ef5d356007d0b0d39dc175e92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:46 +0000

    Add package gocept.httpserverlayer/1.3.0/gocept.httpserverlayer-1.3.0.zip

---
+++ packages/gocept.httpserverlayer/gocept.httpserverlayer-1.3.0.zip/gocept.httpserverlayer-1.3.0/src/gocept/httpserverlayer/custom/__init__.py
@@ -0,0 +1,43 @@
+import BaseHTTPServer
+import threading
+import time
+import plone.testing
+
+
+class Layer(plone.testing.Layer):
+
+    host = 'localhost'
+    port = 0  # choose automatically
+
+    def __init__(self, request_handler=None, *args, **kw):
+        super(Layer, self).__init__(*args, **kw)
+        self.request_handler = request_handler
+
+    def setUp(self):
+        self['request_handler'] = self.request_handler
+        self['httpd'] = BaseHTTPServer.HTTPServer(
+            (self.host, self.port), self.request_handler)
+        self.thread = threading.Thread(
+            target=self['httpd'].serve_forever)
+        self.thread.daemon = True
+        self.thread.start()
+        # Wait as it sometimes takes a while to get the server started.
+        # XXX this is a little kludgy
+        time.sleep(0.001)
+        self.port = self['httpd'].server_port
+        self['http_host'] = self.host
+        self['http_port'] = self.port
+        self['http_address'] = '%s:%s' % (self.host, self.port)
+
+    def tearDown(self):
+        self['httpd'].shutdown()
+        self.thread.join()
+        del self['request_handler']
+        del self['httpd']
+
+
+class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    """Handler for testing which does not log to STDOUT."""
+
+    def log_message(self, format, *args):
+        pass
---
+++ packages/gocept.httpserverlayer/gocept.httpserverlayer-1.3.0.zip/gocept.httpserverlayer-1.3.0/src/gocept/httpserverlayer/wsgi/__init__.py
@@ -0,0 +1,96 @@
+from wsgiref.simple_server import WSGIServer, WSGIRequestHandler
+import os
+import plone.testing
+import socket
+import threading
+import urllib
+
+
+class LogWSGIRequestHandler(WSGIRequestHandler):
+
+    def log_request(self, *args):
+        if 'GOCEPT_HTTP_VERBOSE_LOGGING' in os.environ:
+            WSGIRequestHandler.log_request(self, *args)
+
+
+class Layer(plone.testing.Layer):
+
+    host = 'localhost'
+    port = 0  # choose automatically
+    request_handler_class = LogWSGIRequestHandler
+
+    def __init__(self, *args, **kw):
+        super(Layer, self).__init__(*args, **kw)
+        self.wsgi_app = None
+
+    def _get_wsgi_app(self):
+        return self.get('wsgi_app', self._wsgi_app)
+
+    def _set_wsgi_app(self, value):
+        self._wsgi_app = value
+
+    wsgi_app = property(_get_wsgi_app, _set_wsgi_app)
+
+    def setUp(self):
+        self.http = WSGIServer((self.host, self.port),
+                               self.request_handler_class)
+        self.port = self.http.server_port
+        self['http_host'] = self.host
+        self['http_port'] = self.port
+        self['http_address'] = '%s:%s' % (self.host, self.port)
+        self.http.set_app(self.wsgi_app)
+        self.thread = threading.Thread(target=self.serve)
+        self.thread.daemon = True
+        self.thread.start()
+
+    def tearDown(self):
+        self.shutdown()
+        self.thread.join(5)
+        if self.thread.isAlive():
+            raise RuntimeError('WSGI server could not be shut down')
+        # make the server really go away and give up the socket
+        self.http = None
+
+    def serve(self):
+        if hasattr(self.http, 'shutdown'):
+            self.http.serve_forever()
+        else:
+            # python < 2.6
+            self._running = True
+            while self._running:
+                self.http.handle_request()
+
+    def shutdown(self):
+        if hasattr(self.http, 'shutdown'):
+            self.http.shutdown()
+        else:
+            # python < 2.6
+            self._running = False
+            try:
+                urllib.urlopen('http://%s:%s/die' % (self.host, self.port))
+            except socket.error:
+                pass
+
+
+class FixupMiddleware(object):
+    """Fix problems between WSGI server and middlewares."""
+
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        # wsgiref.simple_server.ServerHandler.setup_environ adds
+        # 'CONTENT_LENGTH' key to environ which has the value '', but
+        # repoze.retry.Retry.__call__ 1.0. expects the value to be
+        # convertable to `int` See http://bugs.repoze.org/issue171.
+        if environ.get('CONTENT_LENGTH') == '':
+            del environ['CONTENT_LENGTH']
+
+        # gocept.httpserverlayer uses wsgiref but
+        # wsgiref.simple_server.ServerHandler.start_response bails when it
+        # sees the 'Connection' header, so we remove it here:
+        def clean_start_response(status, headers, exc_info):
+            headers = [(k, v) for (k, v) in headers if k != 'Connection']
+            return start_response(status, headers, exc_info)
+
+        return self.app(environ, clean_start_response)
