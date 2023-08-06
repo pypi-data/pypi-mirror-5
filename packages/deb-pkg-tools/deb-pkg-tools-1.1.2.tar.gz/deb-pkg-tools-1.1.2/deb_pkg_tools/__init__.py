818fa265e26bedbd14d389c0b6173ec51005a315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:29 +0000

    Add package deb-pkg-tools/1.1.2/deb-pkg-tools-1.1.2.tar.gz

---
+++ packages/deb-pkg-tools/deb-pkg-tools-1.1.2.tar.gz/deb-pkg-tools-1.1.2/deb_pkg_tools/__init__.py
@@ -0,0 +1,10 @@
+# Debian packaging tools.
+#
+# Author: Peter Odding <peter@peterodding.com>
+# Last Change: August 7, 2013
+# URL: https://github.com/xolox/python-deb-pkg-tools
+
+# Semi-standard module versioning.
+__version__ = '1.1.2'
+
+# vim: ts=4 sw=4 et
