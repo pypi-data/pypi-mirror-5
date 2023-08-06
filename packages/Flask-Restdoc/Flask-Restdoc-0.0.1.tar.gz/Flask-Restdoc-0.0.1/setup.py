1893fcaa0b4d0c953a1267de50d09c332c092d91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:59 +0000

    Add package Flask-Restdoc/0.0.1/Flask-Restdoc-0.0.1.tar.gz

---
+++ packages/Flask-Restdoc/Flask-Restdoc-0.0.1.tar.gz/Flask-Restdoc-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+from restdoc import VERSION
+
+setup(
+    name='Flask-Restdoc',
+    packages=['restdoc'],
+    version=VERSION,
+    description='Flask-Restdoc is a simple tool that generates RESTful API documentation automatically from python files.',
+    long_description=open('README.rst').read(),
+    license='MIT License',
+    author='Su Yeol Jeon',
+    author_email='devxoul@gmail.com',
+    url='https://github.com/devxoul/flask-restdoc',
+    download_url='https://pypi.python.org/packages/source/F/Flask-Restdoc/Flask-Restdoc-0.0.1.tar.gz',
+    keywords=['Flask', 'REST', 'API', 'Documentation'],
+    classifiers=[]
+)
