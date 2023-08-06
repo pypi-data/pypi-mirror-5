4b09211da7000421b20ebcf6b7d137a2314deb06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:17 +0000

    Add package moodle-ws-client/0.0.4/moodle-ws-client-0.0.4.tar.gz

---
+++ packages/moodle-ws-client/moodle-ws-client-0.0.4.tar.gz/moodle-ws-client-0.0.4/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013 by
+#   Francisco Moreno Llorca <packo@assamita.net>
+#
+from setuptools import setup
+
+import os
+import moodle_ws_client
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(
+    name = 'moodle-ws-client',
+    version=moodle_ws_client.__version__,
+    url='https://github.com/kotejante/python-moodle',
+    license='GNU Affero General Public License v3',
+    author='Francisco Moreno',
+    author_email='packo@assamita.net',
+    description='Moodle web services (2.5) connection library ',
+    long_description=(read('README')),
+    packages=['moodle_ws_client'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        ],
+)
+
