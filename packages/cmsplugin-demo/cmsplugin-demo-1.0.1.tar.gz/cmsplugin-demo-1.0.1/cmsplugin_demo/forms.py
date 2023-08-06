c5796105a6a4e4320083a8d73eb164b73dc39279
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:59 +0000

    Add package cmsplugin-demo/1.0.1/cmsplugin-demo-1.0.1.tar.gz

---
+++ packages/cmsplugin-demo/cmsplugin-demo-1.0.1.tar.gz/cmsplugin-demo-1.0.1/cmsplugin_demo/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+#import settings
+from cmsplugin_demo.nospam.forms import HoneyPotForm, RecaptchaForm, AkismetForm
+
+CHOICES=[('1', 'Morning'),
+         ('2', 'Evening'),
+         ('3', 'No preference')]
+
+  
+class DemoForm(forms.Form):
+    first_possibility = forms.DateField(label=_("First"), widget=forms.TextInput(attrs={'placeholder': 'first_possibility*'}))
+    first = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect(attrs={'class': 'unstyled'}))
+    second_possibility = forms.DateField(label=_("Second"), widget=forms.TextInput(attrs={'placeholder': 'Second possiblity*'}), required=False)
+    second = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect(attrs={'class': 'unstyled'}), required=False)
+    name = forms.CharField(label=_("Name"), widget=forms.TextInput(attrs={'placeholder': 'Your name*'}))
+    email = forms.EmailField(label=_("Email"), widget=forms.TextInput(attrs={'placeholder': 'Your email address*'}))
+    phone = forms.CharField(label=_("Phone"), widget=forms.TextInput(attrs={'placeholder': 'Phone number'})) 
+    remarks = forms.CharField(label=_("Remarks"), widget=forms.Textarea(attrs={'placeholder': 'Remarks*'}))
+
+    template = "cmsplugin_demo/contact.html"
+  
+class HoneyPotContactForm(HoneyPotForm):
+    pass
+
+class AkismetContactForm(AkismetForm):
+    akismet_fields = {
+        'comment_author_email': 'email',
+        'comment_content': 'content'
+    }
+    akismet_api_key = None
+    
+
+class RecaptchaContactForm(RecaptchaForm):
+    recaptcha_public_key = None
+    recaptcha_private_key = None
+    recaptcha_theme = None
---
+++ packages/cmsplugin-demo/cmsplugin-demo-1.0.1.tar.gz/cmsplugin-demo-1.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cmsplugin-demo',
+    version='1.0.1',
+    description='Extendable demo form plugin for Django CMS with spam protection and i18n',
+    long_description=open('README.rst').read(),
+    author='Maccesch',
+    author_email='maccesch@gmail.com',
+    url='http://github.com/maccesch/cmsplugin-demo',
+    packages=find_packages(),
+    keywords='demo form django cms django-cms spam protection email',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
+)
