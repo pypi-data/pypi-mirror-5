8f56b4b287e5dac48381280180b68e9ebeb003ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:50 +0000

    Add package ztfy.thesaurus/0.2.7.1/ztfy.thesaurus-0.2.7.1-py2.7.egg

---
+++ packages/ztfy.thesaurus/ztfy.thesaurus-0.2.7.1-py2.7.egg/ztfy/thesaurus/browser/resources/js/i18n/en.js
@@ -0,0 +1,9 @@
+(function($) {
+
+	$.ZTFY.thesaurus.I18n = {
+
+		CONFIRM_UNSELECT_WITH_CHILD: "Removing this term from this extract will also remove all it's specific terms. Are you sure ?"
+
+	}
+
+})(jQuery);
