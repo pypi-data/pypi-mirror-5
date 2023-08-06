ce718a60397815a83bd8025e64c728abd7d1b857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:07 +0000

    Add package uimap/1.0/uimap-1.0.tar.gz

---
+++ packages/uimap/uimap-1.0.tar.gz/uimap-1.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='uimap',
+      version='1.0',
+      description='Cross-platform UI Automation Virtualization',
+      author='Reno Ren',
+      author_email='friendkey@gmail.com',
+      url='http://www.python.org/sigs/uimap/',
+      packages=['uimap', 'uimap.drivers', 'uimap.drivers.ldtp', 'uimap.drivers.wpf'],
+      package_dir={'': 'src'},
+     )
