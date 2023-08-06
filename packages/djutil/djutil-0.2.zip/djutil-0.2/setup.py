22c646bd7548049b8d03be162b67dc83226d83f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:25 +0000

    Add package djutil/0.2/djutil-0.2.zip

---
+++ packages/djutil/djutil-0.2.zip/djutil-0.2/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+version = "0.2"
+
+setup(
+    name="djutil",
+    version=version,
+    description="Common Django utilities",
+    license="MIT",
+
+    author="Filip Wasilewski",
+    author_email="en@ig.ma",
+
+    url="https://github.com/nigma/djutil",
+    download_url="https://github.com/nigma/djutil/zipball/master",
+
+    long_description=open("README.rst").read(),
+
+    packages=["djutil"],
+    package_dir={"djutil": "src"},
+    include_package_data=True,
+    classifiers=(
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ),
+    tests_require=["django>=1.5"]
+)
