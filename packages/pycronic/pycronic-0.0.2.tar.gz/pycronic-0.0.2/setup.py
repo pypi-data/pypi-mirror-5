bb6f3d08defee80d5d6da3dfee9f9a90e70b8261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:00 +0000

    Add package pycronic/0.0.2/pycronic-0.0.2.tar.gz

---
+++ packages/pycronic/pycronic-0.0.2.tar.gz/pycronic-0.0.2/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='pycronic',
+      version='0.0.2',
+      description='A crontab script wrapper written by python',
+      long_description=readme(),
+      author='piglei',
+      url='https://github.com/piglei/pycronic/',
+      keywords='crontab cronic',
+      author_email='piglei2007@gmail.com',
+      license='GPL',
+      packages=['pycronic'],
+      install_requires=[
+          'configobj',
+      ],
+      scripts=['pycronic/cronic'],
+      zip_safe=False)
+
+
