ef9f46bbbcf72a2efcfb95b7fa0f49314569ef5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:35 +0000

    Add package textblob/0.6.2/textblob-0.6.2-py2.py3-none-any.whl

---
+++ packages/textblob/textblob-0.6.2-py2.py3-none-any.whl/text/parsers.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+'''Various parser implementations.
+
+.. versionadded:: 0.6.0
+'''
+from .en import parse as pattern_parse
+
+class BaseParser(object):
+
+    '''Abstract parser class from which all parsers inherit from. All
+    descendants must implement a `parse()` method.
+    '''
+
+    def parse(self, text):
+        '''Parses the text.'''
+        raise NotImplementedError("Must implement a parse() method.")
+
+class PatternParser(BaseParser):
+
+    '''Parser that uses the implementation in Tom de Smedt's pattern library.
+    http://www.clips.ua.ac.be/pages/pattern-en#parser
+    '''
+
+    def parse(self, text):
+        '''Parses the text.'''
+        return pattern_parse(text)
