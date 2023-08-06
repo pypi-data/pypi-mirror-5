a888e896543e899bfed494e90d2b14197c33918c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:15 +0000

    Add package pycse/1.17/pycse-1.17.zip

---
+++ packages/pycse/pycse-1.17.zip/pycse-1.17/setup.py
@@ -0,0 +1,43 @@
+from distutils.core import setup
+import os
+
+# for installing magic IPython stuff
+#import IPython
+#IPydir = os.path.join(IPython.utils.path.get_ipython_dir(),
+#                      'profile_default',
+#                      'startup')
+                      
+#print 'Installing ipython magic to : ',IPydir
+
+   
+setup(name = 'pycse',
+      version='1.17',
+      description='python computations in science and engineering',
+      url='http://github.com/jkitchin/pycse',
+      maintainer='John Kitchin',
+      maintainer_email='jkitchin@andrew.cmu.edu',
+      license='GPL',
+      platforms=['linux'],
+      packages=['pycse'],
+      scripts=['pycse/publish.py'],
+#      data_files=[(IPydir, ['pycse/00-pycse-magic.py'])],
+      long_description='''\
+python computations in science and engineering
+===============================================
+
+This package provides some utilities to perform:
+1. linear and nonlinear regression with confidence intervals
+2. Solve some boundary value problems.
+
+See http://jkitchin.github.io/pycse for documentation.
+
+      ''')
+
+#from setuptools.command import easy_install
+#easy_install.main( ["-U","quantities"] )
+#easy_install.main( ["-U","uncertainties"] )
+
+#package = 'https://github.com/jkitchin/pyreport/archive/master.zip'
+#easy_install.main( ["-U",package] )
+
+# to push to pypi - python setup.py sdist upload
