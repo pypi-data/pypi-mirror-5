8dec65043fbf9266b186e14beb6ca0eb3c2ad3a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:38 +0000

    Add package infi.projector/0.6.6/infi.projector-0.6.6.tar.gz

---
+++ packages/infi.projector/infi.projector-0.6.6.tar.gz/infi.projector-0.6.6/src/infi/projector/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.6.6"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '589ccca55aa8d632c76c7d1e8f41aca355482f40'
+__git_head_subject__ = 'HOSTDEV-1107 add prefer-final flag to devenv build'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
