8d7a2d19b97fc4b1063ba7e14ddc03b1077abdca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:46 +0000

    Add package flask-easymode/0.0.6/flask-easymode-0.0.6.tar.gz

---
+++ packages/flask-easymode/flask-easymode-0.0.6.tar.gz/flask-easymode-0.0.6/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='flask-easymode',
+	version='0.0.6',
+	url='http://github.com/petermelias/flask-easymode',
+	license='BSD',
+	author='Peter M. Elias',
+	author_email='petermelias@gmail.com',
+	description='Make Flask development even easier',
+	long_description=open('README.md').read(),
+	packages=find_packages(),
+	zip_safe=False,
+	include_package_data=True,
+	platforms='any',
+	install_requires=[
+		'Flask',
+		'Werkzeug',
+		'blinker',
+		'simplejson'
+	],
+	classifiers=[
+		'Environment :: Web Environment',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: BSD License',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+		'Topic :: Software Development :: Libraries :: Python Modules'
+	],
+)
