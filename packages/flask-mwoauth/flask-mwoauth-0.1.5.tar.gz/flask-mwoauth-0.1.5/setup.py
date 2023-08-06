1d01e2426f0535a95889d25248f7a66cb9aaa89b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:46 +0000

    Add package flask-mwoauth/0.1.5/flask-mwoauth-0.1.5.tar.gz

---
+++ packages/flask-mwoauth/flask-mwoauth-0.1.5.tar.gz/flask-mwoauth-0.1.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+from flask_mwoauth import __version__
+
+setup(name='flask-mwoauth',
+      version=__version__,
+      description='Flask blueprint to connect to a MediaWiki OAuth server',
+      url='http://github.com/valhallasw/flask-mwoauth',
+      author='Merlijn van Deen',
+      author_email='valhallasw@arctus.nl',
+      license='MIT',
+      packages=['flask_mwoauth'],
+      install_requires=['flask-oauth'],
+      zip_safe=True,
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "Environment :: Web Environment",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python :: 2",
+          "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ]
+)
