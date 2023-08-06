92e88175db84ead73eb3ff4513a9c535031bca7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:24 +0000

    Add package js.ember/1.0.0/js.ember-1.0.0.zip

---
+++ packages/js.ember/js.ember-1.0.0.zip/js.ember-1.0.0/README.txt
@@ -0,0 +1,21 @@
+js.ember
+*********
+
+Introduction
+============
+
+This library packages `ember.js`_ for `fanstatic`_.
+
+.. _`fanstatic`: http://fanstatic.org
+.. _`ember.js`: http://emberjs.com/
+
+This requires integration between your web framework and ``fanstatic``,
+and making sure that the original ember.js is published to some URL.
+
+.. WARNING::
+   Ember data is still a beta release!
+
+Contributors
+============
+
+* Matt Good (1.0.0)
---
+++ packages/js.ember/js.ember-1.0.0.zip/js.ember-1.0.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0.0'
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    read('js', 'ember', 'test_ember.txt')
+    + '\n' +
+    read('CHANGES.txt'))
+
+setup(
+    name='js.ember',
+    version=version,
+    description="fanstatic ember.js.",
+    long_description=long_description,
+    classifiers=[],
+    keywords='',
+    author='Fanstatic Developers',
+    author_email='fanstatic@googlegroups.com',
+    license='BSD',
+    packages=find_packages(),
+    namespace_packages=['js'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'fanstatic',
+        'js.jquery>=1.9.1',
+        'setuptools',
+        ],
+    entry_points={
+        'fanstatic.libraries': [
+            'ember = js.ember:library',
+            ],
+        },
+    )
