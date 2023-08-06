8138142b86fbf688a282551ad1b35f559335908d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:38 +0000

    Add package django-twemoir/0.1.6/django-twemoir-0.1.6.tar.gz

---
+++ packages/django-twemoir/django-twemoir-0.1.6.tar.gz/django-twemoir-0.1.6/twemoir/views.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+twemoir/views.py
+
+Created by FI$H 2000 on 2011-08-02.
+Copyright (c) 2011 Objects In Space And Time, LLC. All rights reserved.
+
+"""
