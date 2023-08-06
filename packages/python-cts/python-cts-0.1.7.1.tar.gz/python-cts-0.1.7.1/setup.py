4a9c578f2d4ee3599e130b777e51b2d9ce2ad035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:57 +0000

    Add package python-cts/0.1.7.1/python-cts-0.1.7.1.tar.gz

---
+++ packages/python-cts/python-cts-0.1.7.1.tar.gz/python-cts-0.1.7.1/setup.py
@@ -0,0 +1,18 @@
+# !/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='python-cts',
+      version="0.1.7.1",
+      description='Command line ctag search',
+      url="https://github.com/Refefer/python-cts",
+      license="LICENSE",
+      scripts=['bin/cts'],
+      author='Andrew Stanton',
+      author_email='Andrew Stanton',
+      install_requires=['python-ctags'],
+      classifiers=[
+       "License :: OSI Approved :: Apache Software License",
+       "Programming Language :: Python :: 2.6",
+       "Operating System :: OS Independent"
+      ])
