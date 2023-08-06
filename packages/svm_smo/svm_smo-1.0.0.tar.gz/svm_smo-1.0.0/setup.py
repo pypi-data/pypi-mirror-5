27a27f2c31e826a8f08ce1ff903c66ec0d743722
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:24 +0000

    Add package svm_smo/1.0.0/svm_smo-1.0.0.tar.gz

---
+++ packages/svm_smo/svm_smo-1.0.0.tar.gz/svm_smo-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name = 'svm_smo',
+	version = '1.0.0',
+	py_modules = ['svm_smo'],
+	author = 'chenliyu',
+	author_email = '2572065090@qq.com',
+	url = 'http://pypi.python.org/ ',
+	description = 'a simplfied version and a full version of implimention of svm using smo in python,and a test of linear and rbf',
+)
+
