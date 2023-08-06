caeb6fb136cb8c2fd5bd5a6a1e202a072986badd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:52 +0000

    Add package rumrunner/0.0.4/rumrunner-0.0.4.tar.gz

---
+++ packages/rumrunner/rumrunner-0.0.4.tar.gz/rumrunner-0.0.4/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='rumrunner',
+    version='0.0.4',
+    description='Client to send metrics to Speakeasy server',
+    author='Eric Wong',
+    py_modules=['rumrunner'],
+    )
