36f8012b000890a56a008d15f6266c75e2dc8709
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:33 +0000

    Add package djohno/0.1.6/djohno-0.1.6.tar.gz

---
+++ packages/djohno/djohno-0.1.6.tar.gz/djohno-0.1.6/djohno/tests/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+from .test_utils import DjohnoUtilTests
+from .test_mail_views import DjohnoMailViewTests
+from .test_views import DjohnoViewTests
+from .test_view_permissions import DjohnoViewPermissionTests
+from .test_version_views import DjohnoVersionViewTests
---
+++ packages/djohno/djohno-0.1.6.tar.gz/djohno-0.1.6/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+import djohno
+
+setup(
+    name='djohno',
+    version=djohno.__version__,
+    description="A tiny Django app for checking over your error pages.",
+    long_description=open('README.rst').read(),
+    author='Dominic Rodger',
+    author_email='internet@dominicrodger.com',
+    url='http://github.com/dominicrodger/djohno',
+    license='BSD',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "Django>=1.4",
+        "pkgtools==0.7.2",
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+    ],
+    tests_require=(
+        'django-setuptest==0.1.3',
+        'mock==1.0.1',
+        'httpretty==0.6.3',
+    ),
+    test_suite='setuptest.setuptest.SetupTestSuite',
+)
