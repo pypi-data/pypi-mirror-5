16e86e20217f8c76b1b86ef58d584553dc174049
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:20 +0000

    Add package ftw.workspace/1.7.1/ftw.workspace-1.7.1.zip

---
+++ packages/ftw.workspace/ftw.workspace-1.7.1.zip/ftw.workspace-1.7.1/ftw/workspace/interfaces.py
@@ -0,0 +1,37 @@
+from zope.interface import Interface
+
+
+class IWorkspace(Interface):
+    """A type for collaborative spaces."""
+
+
+class ITabbedViewFolder(Interface):
+    """Tabbed view folder marker interface.
+    """
+
+
+class IWorkspaceDetailsListingProvider(Interface):
+    """A adapter providing a listing (LaTeX) for the workspace details
+    PDF.
+    """
+
+    def __init__(context, request, layout, view):
+        """
+        """
+
+    def get_title():
+        """Returns the title of the listing (LaTeX).
+        """
+
+    def get_listing():
+        """Returns the complete listing (LaTeX).
+        """
+
+    def get_sort_key():
+        """A integer number which is used for sorting the listings.
+        Small numbers are at the top, big numbers at the bottom.
+        """
+
+
+class IWorkspaceLayer(Interface):
+    """Request marker interface for ftw.workspace"""
---
+++ packages/ftw.workspace/ftw.workspace-1.7.1.zip/ftw.workspace-1.7.1/setup.py
@@ -0,0 +1,63 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.7.1'
+
+tests_require = [
+    'plone.app.testing',
+    'ftw.testing',
+    'ftw.pdfgenerator',
+    'ftw.builder',
+    'pyquery',
+    ]
+
+setup(name='ftw.workspace',
+      version=version,
+      description='A project folder for plone.',
+      long_description=open('README.rst').read() + '\n' + \
+          open(os.path.join('docs', 'HISTORY.txt')).read(),
+
+      # Get more strings from
+      # http://www.python.org/pypi?%3Aaction=list_classifiers
+
+      classifiers=[
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.1',
+        'Framework :: Plone :: 4.2',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+
+      keywords='ftw workspace project folder plone',
+      author='4teamwork GmbH',
+      author_email='mailto:info@4teamwork.ch',
+      url='https://github.com/4teamwork/ftw.workspace',
+      license='GPL2',
+
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ftw'],
+      include_package_data=True,
+      zip_safe=False,
+
+      install_requires=[
+        'collective.js.jqsmartTruncation',
+        'ftw.calendar',
+        'ftw.tabbedview',
+        'ftw.upgrade',
+        'plone.principalsource',
+        'setuptools',
+        # -*- Extra requirements: -*-
+        ],
+
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require,
+                          pdf=['ftw.pdfgenerator',
+                               'ftw.file', ]),
+
+      test_suite='ftw.workspace.tests.test_docs.test_suite',
+      entry_points='''
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      ''',
+      )
