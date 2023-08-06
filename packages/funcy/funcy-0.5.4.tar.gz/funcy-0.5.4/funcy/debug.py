de6b91b9f74358cb4593b39039829aca7616e0b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:01 +0000

    Add package funcy/0.5.4/funcy-0.5.4.tar.gz

---
+++ packages/funcy/funcy-0.5.4.tar.gz/funcy-0.5.4/funcy/debug.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+from .decorators import decorator
+
+
+__all__ = ['tap', 'log_calls', 'print_calls', 'log_errors', 'print_errors']
+
+
+def tap(x):
+    print(x)
+    return x
+
+
+@decorator
+def log_calls(call, print_func):
+    arg_words = list(call._args) + ['%s=%s' % t for t in call._kwargs.items()]
+    print_func('Call %s(%s)' % (call._func.__name__, ', '.join(map(str, arg_words))))
+    result = call()
+    print_func('-> %s' % repr(result))
+    return result
+
+print_calls = log_calls(print)
+
+
+@decorator
+def log_errors(call, print_func):
+    try:
+        return call()
+    except Exception as e:
+        print_func('%s: %s in %s' % (e.__class__.__name__, e, call._func.__name__))
+        raise
+
+print_errors = log_errors(print)
