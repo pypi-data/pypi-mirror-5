8c817c9b4a0cfcde006a2dedafbd1fe4f237c0e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:21 +0000

    Add package gdapi/0.2.0/gdapi-0.2.0.tar.gz

---
+++ packages/gdapi/gdapi-0.2.0.tar.gz/gdapi-0.2.0/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+test_requirements = ['nose', 'mock', 'testfixtures', 'httpretty']
+config = {
+    'description': 'Google Drive API Wrapper',
+    'author': 'Cheng-Lung Sung',
+    'url': 'https://github.com/clsung/gdapi',
+    'download_url': 'http://pypi.python.org/pypi/gdapi',
+    'author_email': 'clsung@gmail.com',
+    'version': '0.2.0',
+    'test_requires': test_requirements,
+    'install_requires': ['requests'],
+    'packages': ['gdapi'],
+    'scripts': ['script/download_to_somewhere.py',
+                'script/upload_to_root.py'],
+    'name': 'gdapi'
+}
+
+setup(**config)
