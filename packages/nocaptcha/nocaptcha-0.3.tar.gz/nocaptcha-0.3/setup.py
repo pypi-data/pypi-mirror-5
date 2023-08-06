2c698de63a5394e37aca38708f867920911a6763
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:09 +0000

    Add package nocaptcha/0.3/nocaptcha-0.3.tar.gz

---
+++ packages/nocaptcha/nocaptcha-0.3.tar.gz/nocaptcha-0.3/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+    name = 'nocaptcha',
+    packages = ['nocaptcha'],
+    version = '0.3',
+    description = 'No bots with no captcha.',
+    author = 'Jakub Wasielak',
+    author_email = 'kuba.wasielak@gmail.com',
+    url = 'https://github.com/Gandi24/nocaptcha.git',
+    download_url = 'https://github.com/Gandi24/nocaptcha/tarball/0.1',
+    classifiers = [],
+)
