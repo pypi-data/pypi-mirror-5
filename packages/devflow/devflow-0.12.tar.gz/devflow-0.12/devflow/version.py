91fd25e3516c312110e6e604d4b8b7bf99c07fe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:46 +0000

    Add package devflow/0.12/devflow-0.12.tar.gz

---
+++ packages/devflow/devflow-0.12.tar.gz/devflow-0.12/devflow/version.py
@@ -0,0 +1,7 @@
+__version__ = "0.12"
+__version_vcs_info__ = {
+    'branch': 'master',
+    'revid': '8021416',
+    'revno': 157}
+__version_user_email__ = "cstavr@grnet.gr"
+__version_user_name__ = "Christos Stavrakakis"
