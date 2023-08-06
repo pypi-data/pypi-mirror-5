5f520f7770f9aea2829ba8ff4359729d17365478
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:07 +0000

    Add package mrpython/0.0.2/mrpython-0.0.2.tar.gz

---
+++ packages/mrpython/mrpython-0.0.2.tar.gz/mrpython-0.0.2/mrpython/mixin.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+
+class DictInterface(object):
+
+	_dict_attrs = ()
+
+	@property
+	def as_dict(self):
+		return self.to_dict()
+
+	def to_dict(self, only=[]):	
+
+		def _obj_to_dict(o, limit=[]):
+			names = self._dict_attrs or filter(lambda x: not x.startswith('_'), vars(o))
+			if limit:
+				names = list(set(limit).intersection(set(names)))
+			return {k: getattr(o, k) for k in names}
+
+		def _walk(o, parents=[], limit=[]):
+			c = {}
+			d = _obj_to_dict(o, limit)
+			for attr, value in d.iteritems():
+				if isinstance(value, DictInterface):
+					if value is o:
+						continue
+					if value in parents:
+						continue
+					parents.append(o)
+					c[attr] = _walk(value, parents)
+				else:
+					c[attr] = value
+			return c
+
+		
+		return _walk(self, limit=only)
