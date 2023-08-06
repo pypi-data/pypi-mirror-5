6785b076c899d8d6a75ab347122003da3613315f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:38 +0000

    Add package yumwat/0.4.3/yumwat-0.4.3.tar.gz

---
+++ packages/yumwat/yumwat-0.4.3.tar.gz/yumwat-0.4.3/install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This script installs yumwat on Fedora 18 and 19.
+# Not tested on other platforms.
+# Requires root privs.
+
+# Install config file
+cp yumwat/yumwat.conf /etc/yum/pluginconf.d/
+
+# TODO: make sure pluginpath is set in /etc/yum.conf
+# ... there has to be a better way to do that.
+
+# Install plugin
+cp yumwat/yumwat.py /usr/lib/yum-plugins
