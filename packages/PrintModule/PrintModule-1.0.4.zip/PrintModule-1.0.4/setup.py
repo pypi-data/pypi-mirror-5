a21bf921ab5f7f361c581c4872d3b4a3699b6cf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:24 +0000

    Add package PrintModule/1.0.4/PrintModule-1.0.4.zip

---
+++ packages/PrintModule/PrintModule-1.0.4.zip/PrintModule-1.0.4/setup.py
@@ -0,0 +1,15 @@
+""" New and improved version of the code to print the list of elements"""
+''' Author Vinaya DR'''
+
+from distutils.core import setup
+
+setup(
+
+        name            = 'PrintModule',
+        version         = '1.0.4',
+        py_modules      = ['print_lol'],
+        author          = 'Vinaya',
+        author_email    = 'vinayadr@gmail.com',
+        url             = 'http://www.google.com',
+        description     = 'New improvised module for printing list of elements',
+    )
