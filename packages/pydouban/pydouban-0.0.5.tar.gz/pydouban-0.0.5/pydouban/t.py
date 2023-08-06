6797993bc223640de99361b18932f26b07a73c92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:43 +0000

    Add package pydouban/0.0.5/pydouban-0.0.5.tar.gz

---
+++ packages/pydouban/pydouban-0.0.5.tar.gz/pydouban-0.0.5/pydouban/t.py
@@ -0,0 +1,8 @@
+from client import DoubanClient
+
+client = DoubanClient('373512566@qq.com','122126382')
+
+#print 'x',client.reply_topic('http://www.douban.com/group/topic/39324575/','101001001011101010010')
+
+#for m in client.get_doumails(unread=True):
+#    print m
