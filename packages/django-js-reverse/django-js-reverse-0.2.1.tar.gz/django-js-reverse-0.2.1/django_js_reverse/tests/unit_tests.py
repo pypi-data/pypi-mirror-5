7119361f5c2726fc2cd9750642a56b4413b0497a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:17 +0000

    Add package django-js-reverse/0.2.1/django-js-reverse-0.2.1.tar.gz

---
+++ packages/django-js-reverse/django-js-reverse-0.2.1.tar.gz/django-js-reverse-0.2.1/django_js_reverse/tests/unit_tests.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import os
+import sys
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
+from django.test.client import Client
+from django.utils import unittest
+from django.test import TestCase
+from django.test.utils import override_settings
+
+
+class JSReverseViewTestCase(TestCase):
+    client = None
+    urls = 'django_js_reverse.tests.test_urls'
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_view_no_url_args(self):
+        response = self.client.post('/jsreverse/')
+        self.assertContains(response, "'test_no_url_args', ['test_no_url_args/', []]")
+
+    def _test_view_one_url_arg(self):
+        response = self.client.post('/jsreverse/')
+        self.assertContains(response, "'test_one_url_args', ['test_one_url_args/%(arg_one)s/', ['arg_one']]")
+
+    def _test_view_two_url_args(self):
+        response = self.client.post('/jsreverse/')
+        self.assertContains(
+            response, "test_two_url_args', ['test_two_url_args/%(arg_one)s\\u002D%(arg_two)s/', ['arg_one','arg_two']]")
+
+    @override_settings(JS_REVERSE_JS_VAR_NAME='Foo')
+    def _test_js_var_name_changed_valid(self):
+        response = self.client.post('/jsreverse/')
+        self.assertContains(response, 'exports.Foo = (function () {')
+
+    @override_settings(JS_REVERSE_JS_VAR_NAME='1test')
+    def _test_js_var_name_changed_invalid(self):
+        from django.core.exceptions import ImproperlyConfigured
+        with self.assertRaises(ImproperlyConfigured):
+            self.client.post('/jsreverse/')
+
+
+if __name__ == '__main__':
+    sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..') + os.sep)
+    unittest.main()
