2e5cf45dac9855a75234b314545a3a7fb0052602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:26 +0000

    Add package flumelogger/0.2.1/flumelogger-0.2.1.tar.gz

---
+++ packages/flumelogger/flumelogger-0.2.1.tar.gz/flumelogger-0.2.1/flumelogger/handler.py
@@ -0,0 +1,26 @@
+import socket
+import logging
+from flumelogger.eventserver import FlumeEventServer
+
+class FlumeHandler(logging.Handler):
+    def __init__(self, host="localhost", port=9090, headers={}):
+        # run the regular Handler __init__
+        logging.Handler.__init__(self)
+
+        self.host = host
+        self.port = port
+        self.headers = headers
+        if not self.headers.has_key('host'):
+            self.headers['host'] = socket.gethostname()
+
+    def emit(self, record):
+        try:
+            evt = FlumeEventServer(host=self.host, port=self.port)
+            self.headers['pri'] = record.levelname.upper()
+            # record is the log message
+            evt.append(self.headers, self.format(record))
+            evt.close()
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            self.handleError(record)
