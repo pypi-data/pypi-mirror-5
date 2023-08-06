cdd8f882ce43fdd0a5fffa0e6588dce6954749cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:33 +0000

    Add package curlish/1.21/curlish-1.21.tar.gz

---
+++ packages/curlish/curlish-1.21.tar.gz/curlish-1.21/setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup
+
+readme = open(os.path.join(os.path.dirname(__file__), 'README'), 'r').read()
+
+setup(
+    name='curlish',
+    author='Fireteam Ltd.',
+    author_email='support@fireteam.net',
+    version='1.21',
+    url='http://github.com/fireteam/curlish',
+    py_modules=['curlish'],
+    description='A wrapper for curl that adds OAuth support',
+    long_description=readme,
+    entry_points={
+        'console_scripts': [
+            'curlish = curlish:main'
+        ]
+    },
+    zip_safe=False,
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python'
+    ]
+)
