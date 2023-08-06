4ae824c9656c410eb2bbccec00f40819aaf0bb2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:01 +0000

    Add package qingcloud-cli/(latest release)/qingcloud-cli-0.6.tar.gz

---
+++ packages/qingcloud-cli/qingcloud-cli-0.6.tar.gz/qingcloud-cli-0.6/qingcloud/cli/iaas_client/actions/image/describe_images.py
@@ -0,0 +1,55 @@
+# coding: utf-8
+
+from qingcloud.cli.misc.utils import explode_array
+from qingcloud.cli.iaas_client.actions.base import BaseAction
+
+class DescribeImagesAction(BaseAction):
+
+    action = 'DescribeImages'
+    command = 'describe-images'
+    usage = '%(prog)s [-i "image_id, ..."] [options] [-f <conf_file>]'
+
+    @classmethod
+    def add_ext_arguments(cls, parser):
+
+        parser.add_argument('-i', '--images', dest='images',
+                action='store', type=str, default='',
+                help='The comma separated IDs of images you want to describe. ')
+
+        parser.add_argument('-s', '--status', dest='status',
+                action='store', type=str, default='',
+                help='Status: pending, available, deleted')
+
+        parser.add_argument('-p', '--processor_type', dest='platform',
+                action='store', type=str, default='',
+                help='Filter by processor type, supported processor types are `64bit` and `32bit`')
+
+        parser.add_argument('-F', '--os_family', dest='os_family',
+                action='store', type=str, default='',
+                help='Filter by OS family, supported values are windows/debian/centos/ubuntu.')
+
+        parser.add_argument('-v', '--visibility', dest='visibility',
+                action='store', type=str, default='',
+                help='Filter by visibility, supported values are `public`, `private`')
+
+        parser.add_argument('-W', '--search_word', dest='search_word',
+                action='store', type=str, default='',
+                help='The combined search column')
+
+        parser.add_argument('-P', '--provider', dest='provider',
+                action='store', type=str, default='',
+                help='Filter by the image provider, supported values are `self`, `system`')
+
+    @classmethod
+    def build_directive(cls, options):
+        return {
+                'images': explode_array(options.images),
+                'processor_type': explode_array(options.processor_type),
+                'os_family':explode_array(options.os_family),
+                'visibility': explode_array(options.visibility),
+                'status': explode_array(options.status),
+                'provider': options.provider,
+                'search_word': options.search_word,
+                'offset':options.offset,
+                'limit': options.limit,
+                }
---
+++ packages/qingcloud-cli/qingcloud-cli-0.6.tar.gz/qingcloud-cli-0.6/setup.py
@@ -0,0 +1,43 @@
+# coding:utf-8
+
+import os
+from setuptools import setup, find_packages
+
+config_sample = '''
+qy_access_key_id: 'QINGCLOUDACCESSKEYID'
+qy_secret_access_key: 'QINGCLOUDSECRETACCESSKEYEXAMPLE'
+zone: 'pek1'
+'''
+
+def prepare_config_file():
+    config_file = os.path.expanduser('~/.qingcloud/config.yaml')
+    if os.path.exists(config_file):
+        return
+
+    d = os.path.dirname(config_file)
+    if not os.path.exists(d):
+        os.makedirs(d)
+
+    with open(config_file, 'w') as fd:
+        fd.write(config_sample)
+
+setup(
+    name = 'qingcloud-cli',
+    version = '0.6',
+    description = 'Command Line Interface for QingCloud.',
+    long_description = open('README.rst', 'rb').read().decode('utf-8'),
+    keywords = 'qingcloud iaas cli',
+    author = 'Yunify Team',
+    author_email = 'simon@yunify.com',
+    url = 'https://docs.qingcloud.com/cli/',
+    scripts=['bin/qingcloud', 'bin/qingcloud_completer', 'bin/qingcloud.cmd'],
+    packages = find_packages('.'),
+    package_dir = {'qingcloud-cli': 'qingcloud'},
+    include_package_data = True,
+    install_requires = [
+        'argparse>=1.1',
+        'PyYAML>=3.1',
+    ]
+)
+
+prepare_config_file()
