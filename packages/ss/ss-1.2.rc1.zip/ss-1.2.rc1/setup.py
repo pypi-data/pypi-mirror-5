128986491a6347c108a868ac4f43908ec3eda774
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:03 +0000

    Add package ss/1.2.rc1/ss-1.2.rc1.zip

---
+++ packages/ss/ss-1.2.rc1.zip/ss-1.2.rc1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+description = "Command line script that automatically searches for video subtitles using OpenSubtitles.org APIs."
+
+setup(
+    name = "ss",
+    version = "1.2.rc1",
+    packages = [],
+    scripts = ['ss.py'],
+    entry_points = {'console_scripts' : ['ss = ss:Main']},
+    
+    # metadata for upload to PyPI
+    author = "nicoddemus@gmail.com",
+    author_email = "nicoddemus@gmail.com",
+    description = description,
+    license = "GPL",
+    keywords = "subtitles script",
+    url = "http://nicoddemus.github.io/ss/",  
+    
+    use_2to3=True,
+)
