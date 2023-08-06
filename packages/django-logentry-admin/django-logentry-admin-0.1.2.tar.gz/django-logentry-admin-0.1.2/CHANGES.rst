0284988a37f0e668a7d32064cd22f24eade83ab7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:13 +0000

    Add package django-logentry-admin/0.1.2/django-logentry-admin-0.1.2.tar.gz

---
+++ packages/django-logentry-admin/django-logentry-admin-0.1.2.tar.gz/django-logentry-admin-0.1.2/CHANGES.rst
@@ -0,0 +1,17 @@
+v0.1.2 - 26/08/2013
+===================
+
+* Added an empty model.py file.
+* Minor documentation (readme) updates/fixes.
+
+
+v0.1.1 - 28/12/2011
+===================
+
+* Added object links and action description.
+
+
+v0.1.0 - 26/12/2011
+===================
+
+* Initial release.
---
+++ packages/django-logentry-admin/django-logentry-admin-0.1.2.tar.gz/django-logentry-admin-0.1.2/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+    name='django-logentry-admin',
+    author='Yuri Prezument',
+    author_email='y@yprez.com',
+    version='0.1.2',
+    packages=['logentry_admin'],
+    license='ISC',
+    url='http://github.com/yprez/django-logentry-admin',
+    description='Add Django LogEntries the the Django admin site',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        'Django >= 1.3',
+    ],
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ),
+)
