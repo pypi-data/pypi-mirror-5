78e6bb81f3f35f78f0eca670eb1e5c967d45798a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:30 +0000

    Add package icemac.addressbook/1.10.2/icemac.addressbook-1.10.2.tar.gz

---
+++ packages/icemac.addressbook/icemac.addressbook-1.10.2.tar.gz/icemac.addressbook-1.10.2/src/icemac/addressbook/browser/tests/test_doctests.py
@@ -0,0 +1,43 @@
+# -*- coding: latin-1 -*-
+# Copyright (c) 2008-2013 Michael Howitz
+# See also LICENSE.txt
+# $Id$
+
+import icemac.addressbook.browser.testing
+import icemac.addressbook.testing
+import zope.testing.renormalizing
+
+
+def test_suite():
+    suite = icemac.addressbook.testing.TestBrowserDocFileSuite(
+        "browser/authentication/login.txt",
+        "browser/entities/bugfix.txt",
+        "browser/entities/delete_choice_value.txt",
+        "browser/entities/entities.txt",
+        "browser/entities/file.txt",
+        "browser/entities/sortorder-fields.txt",
+        "browser/entities/sortorder.txt",
+        "browser/keyword/keyword.txt",
+        "browser/person/clone.txt",
+        "browser/person/file.txt",
+        "browser/person/person.txt",
+        "browser/person/sortorder.txt",
+        "browser/person/translation.txt",
+        "browser/principals/principals.txt",
+        "browser/rootfolder/rootfolder.txt",
+        "browser/search/result/handler/export/translation.txt",
+        "browser/search/result/handler/export/userfields.txt",
+        )
+    suite.addTest(
+        icemac.addressbook.testing.DocFileSuite(
+            "browser/search/result/handler/delete.txt",
+            "browser/search/result/handler/export/export.txt",
+            "browser/search/search.txt",
+            layer=icemac.addressbook.browser.testing.WSGI_SEARCH_LAYER
+            ))
+    suite.addTest(
+        icemac.addressbook.testing.TestBrowserDocFileSuite(
+            # Tests which must not run with the default <DATETIME> normalizer:
+            "browser/metadata.txt",
+            checker=zope.testing.renormalizing.RENormalizing([])))
+    return suite
---
+++ packages/icemac.addressbook/icemac.addressbook-1.10.2.tar.gz/icemac.addressbook-1.10.2/src/icemac/addressbook/sources.py
@@ -0,0 +1,113 @@
+# -*- coding: latin-1 -*-
+# Copyright (c) 2008-2013 Michael Howitz
+# See also LICENSE.txt
+# $Id$
+
+from icemac.addressbook.i18n import MessageFactory as _
+import stabledict
+import zc.sourcefactory.basic
+import zc.sourcefactory.contextual
+import zope.component
+import zope.component.hooks
+import zope.globalrequest
+
+
+class TitleMappingSource(zc.sourcefactory.basic.BasicSourceFactory):
+    "Abstract base class for sources using a mapping between value and title."
+
+    _mapping = None  # to be set in child class
+
+    def getValues(self):
+        return self._mapping.keys()
+
+    def getTitle(self, value):
+        return self._mapping[value]
+
+
+class YesNoSource(TitleMappingSource):
+    _mapping = stabledict.StableDict(
+        ((True, _(u'yes')),
+         (False, _(u'no'))))
+
+yes_no_source = YesNoSource()
+
+
+class AscDescSource(TitleMappingSource):
+    _mapping = stabledict.StableDict(
+        (('ascending', _(u'ascending (A-->Z)')),
+         ('descending', _(u'descending (Z-->A)'))))
+
+asc_desc_source = AscDescSource()
+
+
+class FieldTypeSource(TitleMappingSource):
+    _mapping = stabledict.StableDict(
+        ((u'Bool', _(u'bool')),
+         (u'Choice', _('choice')),
+         (u'Date', _(u'date')),
+         (u'Datetime', _(u'datetime')),
+         (u'Decimal', _('decimal number')),
+         (u'Int', _(u'integer number')),
+         (u'Text', _(u'text area')),
+         (u'TextLine', _(u'text line')),
+         (u'URI', _(u'URL')),
+         ))
+
+
+class KeywordSource(zc.sourcefactory.basic.BasicSourceFactory):
+    "Source of keywords in the address book."
+
+    def getValues(self):
+        import icemac.addressbook.interfaces  # avoid circular import
+        keywords = zope.component.getUtility(
+            icemac.addressbook.interfaces.IKeywords)
+        return sorted(keywords.get_keywords(), key=lambda x: x.title.lower())
+
+    def getTitle(self, value):
+        return value.title
+
+keyword_source = KeywordSource()
+
+
+class ContextByInterfaceSource(
+    zc.sourcefactory.contextual.BasicContextualSourceFactory):
+    "Source to select objects from context container by a given interface."
+
+    def __init__(self, interface):
+        super(ContextByInterfaceSource, self).__init__()
+        self.interface = interface
+
+    def getValues(self, context):
+        for value in context.values():
+            if self.interface.providedBy(value):
+                yield value
+
+    def getTitle(self, context, value):
+        import icemac.addressbook.interfaces  # avoid circular import
+        return icemac.addressbook.interfaces.ITitle(value)
+
+
+class SiteMenuSource(zc.sourcefactory.basic.BasicSourceFactory):
+    """Source containing items of a z3c.menu.ready2go.ISiteMenu."""
+
+    def __init__(self, view_class, menu_manager_class):
+        self.menu_manager_class = menu_manager_class
+        self.view_class = view_class
+
+    @property
+    def menu_manager(self):
+        request = zope.globalrequest.getRequest()
+        context = zope.component.hooks.getSite()
+        view = self.view_class(context, request)
+        menu_manager = self.menu_manager_class(context, request, view)
+        menu_manager.update()
+        return menu_manager
+
+    def getValues(self):
+        return self.menu_manager.viewlets
+
+    def getTitle(self, value):
+        return value.title
+
+    def getToken(self, value):
+        return value.url
