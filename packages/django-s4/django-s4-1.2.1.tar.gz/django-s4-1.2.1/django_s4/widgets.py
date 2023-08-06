fecbfa298be710edced1c0deb68d61cc28b99750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:21 +0000

    Add package django-s4/1.2.1/django-s4-1.2.1.tar.gz

---
+++ packages/django-s4/django-s4-1.2.1.tar.gz/django-s4-1.2.1/django_s4/widgets.py
@@ -0,0 +1,21 @@
+from django.forms.widgets import Widget
+from django.template import loader
+from django.utils.safestring import mark_safe
+from django.conf import settings
+
+
+class S4DriveFileWidget(Widget):
+    def __init__(self, url, drive_static_url, **kwargs):
+        self.url = url
+        self.drive_static_url = drive_static_url
+        return super(S4DriveFileWidget, self).__init__(**kwargs)
+
+    def render(self, name, value, attrs=None):
+        if not value: value = ""
+        return mark_safe(loader.render_to_string("django_s4/s4_drive_file.html", {
+            "STATIC_URL": settings.STATIC_URL,
+            "DRIVE_STATIC_URL": self.drive_static_url,
+            "name": name,
+            "url": self.url,
+            "value": value,
+        }))
