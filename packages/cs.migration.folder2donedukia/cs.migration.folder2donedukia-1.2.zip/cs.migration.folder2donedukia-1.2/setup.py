668f7df1c592be0c6c8f3b6a2f8a9f31158172a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:54 +0000

    Add package cs.migration.folder2donedukia/1.2/cs.migration.folder2donedukia-1.2.zip

---
+++ packages/cs.migration.folder2donedukia/cs.migration.folder2donedukia-1.2.zip/cs.migration.folder2donedukia-1.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.2'
+
+setup(name='cs.migration.folder2donedukia',
+      version=version,
+      description="",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Mikel Larreategi',
+      author_email='mlarreategi@codesyntax.com',
+      url='http://svn.plone.org/svn/plone/plone.app.example',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['cs', 'cs.migration'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'cs.donedukia',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
