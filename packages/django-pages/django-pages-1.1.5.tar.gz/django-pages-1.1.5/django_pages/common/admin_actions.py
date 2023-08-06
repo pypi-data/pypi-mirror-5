cdbd7fad7ad1a51dcaae77b9b48c8817676601f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:12 +0000

    Add package django-pages/1.1.5/django-pages-1.1.5.tar.gz

---
+++ packages/django-pages/django-pages-1.1.5.tar.gz/django-pages-1.1.5/django_pages/common/admin_actions.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+
+"""
+Common admin actions importable from any module
+"""
+
+def activate(modeladmin, request, queryset):
+
+    queryset.update(active=True)
+
+activate.short_description = 'Activate selected items.'
+
+
+def deactivate(modeladmin, request, queryset):
+
+    queryset.update(active=False)
+
+deactivate.short_description = 'Deactivate selected items.'
