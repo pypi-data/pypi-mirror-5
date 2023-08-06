2303e7fbc71b52509ff1a6ba54d26bac64906107
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:34 +0000

    Add package wheelcms_rss/0.8.2/wheelcms_rss-0.8.2.tar.gz

---
+++ packages/wheelcms_rss/wheelcms_rss-0.8.2.tar.gz/wheelcms_rss-0.8.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.8.2'
+
+setup(name='wheelcms_rss',
+      version=version,
+      description="WheelCMS RSS feed generator",
+      long_description=open("README.md").read(),
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Ivo van der Wijk',
+      author_email='wheelcms@in.m3r.nl',
+      url='http://github.com/wheelcms/wheelcms_rss',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=[],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'wheelcms_axle',
+          'setuptools',
+          'pytest',
+      ],
+      entry_points={
+      },
+
+      )
+
