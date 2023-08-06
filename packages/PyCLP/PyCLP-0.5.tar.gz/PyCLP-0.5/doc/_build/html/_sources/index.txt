b4512c55510001ac5c9421171989e87357450ba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:08 +0000

    Add package PyCLP/0.5/PyCLP-0.5.tar.gz

---
+++ packages/PyCLP/PyCLP-0.5.tar.gz/PyCLP-0.5/doc/_build/html/_sources/index.txt
@@ -0,0 +1,55 @@
+.. .. documentation master file, created by
+   sphinx-quickstart on Sat Jul 14 16:38:20 2012.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to `PyCLP <http://developer.berlios.de/projects/pyclp/>`_ 's documentation!
+###################################################################################
+
+Project development website
+***************************
+
+`PyCLP website <http://developer.berlios.de/projects/pyclp/>`_
+
+
+Contents
+********
+.. toctree::
+   :maxdepth: 2
+   
+   introduction
+   
+   installation
+   
+   pyclp
+   
+   examples
+   
+   external_predicates
+   
+   license
+
+
+Indices and tables
+##################
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
+.. image:: http://developer.berlios.de/bslogo.php?group_id=12904
+   :height: 32 px
+   :width: 124 px
+   :alt: BerliOS Logo
+   :align: left
+   :target: http://developer.berlios.de
+   
+   
+Bugs Reporting
+##############
+
+`Bugs reporting <http://developer.berlios.de/bugs/?group_id=12904>`_
+
+
+   
+   
---
+++ packages/PyCLP/PyCLP-0.5.tar.gz/PyCLP-0.5/setup.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+
+#Simplified BSD License
+#Copyright (c) 2012, Oreste Bernardi
+#All rights reserved.
+#
+#Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+#
+#    Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+#    Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer
+#    in the documentation and/or other materials provided with the distribution.
+#
+#THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, 
+#BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+#IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, 
+#OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+#OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+#OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+#THE POSSIBILITY OF SUCH DAMAGE.
+"""
+setup.py file for pyclp
+"""
+
+import distutils
+from distutils.core import setup
+from distutils.extension import Extension
+from distutils.msvccompiler import MSVCCompiler as oldMSVCCompiler
+import platform
+
+class mymsvcompiler(oldMSVCCompiler):
+    def link (self,*arg,**kargs):
+		#Generate .lib file if not yet done
+        self.announce("Microsoft Visual C requires eclipse.lib\n Building it") 
+        bits, linkage= platform.architecture()
+        if bits=="32bit":
+            machine_option="/MACHINE:X86"
+        elif bits=="64bit":
+            machine_option="/MACHINE:X64"
+        else:
+            raise "Architecture not supported"
+        lib_cmd=self.find_exe('lib.exe')
+        option="/def:" + os.path.join(eclipse_lib_path,"eclipse.def")
+        self.spawn([lib_cmd,machine_option,option])
+        oldMSVCCompiler.link(self,*arg,**kargs)
+
+#Substitute msvcompiler with one subclass.       
+distutils.msvccompiler.MSVCCompiler=mymsvcompiler
+
+
+from Cython.Distutils import build_ext
+from os import environ
+import os.path
+
+    
+
+if "ECLIPSEDIR" not in environ:
+    while(1):
+        eclipsedir=input('ECLIPSEDIR environmental variable is not defined \n\
+please provide path to ECLiPSe installation: ')
+        if os.path.exists(eclipsedir):
+            break
+        else:
+            print('Invalid Path')
+else:
+    eclipsedir=environ["ECLIPSEDIR"]
+
+if platform.system()=='Linux':
+    arch='i386_linux'
+elif platform.system()=='Windows':
+    arch='i386_nt'
+else:
+    print ("Platform not supported")
+    raise
+
+eclise_include_path=os.path.join(eclipsedir,'include',arch)
+eclipse_lib_path=os.path.join(eclipsedir,'lib',arch)
+pyclp_module = Extension('pyclp.pyclp',
+                           ['src/pyclp/pyclp.pyx'],
+                           library_dirs=[eclipse_lib_path],
+                           libraries=['eclipse'],
+                           include_dirs=[eclise_include_path,'src/pyclp/']
+                           )
+
+with open('README.txt') as file:
+    long_description_read = file.read()
+
+setup (name = 'PyCLP',
+       version = '0.5',
+       author      = "Oreste Bernardi",
+       maintainer = "Oreste Bernardi",
+       author_email= "<name>@<name>.eu  name=oreste",
+       url = "pyclp.berlios.de",
+       description = """Interface to ECLiPSe CLP""",
+       license= "Simplified BSD",
+       cmdclass = {'build_ext': build_ext},
+       ext_modules = [pyclp_module],
+       package_dir={'': 'src'},
+       packages=['pyclp'],
+       long_description=long_description_read,
+       classifiers=["Development Status :: 4 - Beta",
+                    "Programming Language :: Prolog" 
+                    ]
+       #py_modules = ["pyclp"],
+       )
