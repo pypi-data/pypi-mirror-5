de9ac364436bd7bdb2ebad5405db952c0f5686aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:50 +0000

    Add package opengraph-writer/0.0.5/opengraph_writer-0.0.5.tar.gz

---
+++ packages/opengraph-writer/opengraph_writer-0.0.5.tar.gz/opengraph_writer-0.0.5/setup.py
@@ -0,0 +1,39 @@
+"""opengraph_writer installation script.
+"""
+import os
+
+from setuptools import setup
+from setuptools import find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, "README.md")).read()
+README = README.split("\n\n", 1)[0] + "\n"
+
+requires = [
+    ]
+
+setup(name="opengraph_writer",
+      version="0.0.5",
+      description="Lightweight open graph support for writing and validating objects",
+      long_description=README,
+      classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Framework :: Pylons",
+        "Framework :: Pyramid",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        ],
+      keywords="web pylons pyramid facebook opengraph open graph",
+      py_modules=['opengraph_writer'],
+      author="Jonathan Vanasco",
+      author_email="jonathan@findmeon.com",
+      url="https://github.com/jvanasco/opengraph_writer",
+      license="MIT",
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      tests_require = requires,
+      install_requires = requires,
+      )
+
