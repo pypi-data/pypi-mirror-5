a6efef5e44dd9bb91703bdda7e8a106a6ec7d0d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:20 +0000

    Add package django-pony-forms/0.3.5/django_pony_forms-0.3.5.tar.gz

---
+++ packages/django-pony-forms/django_pony_forms-0.3.5.tar.gz/django_pony_forms-0.3.5/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+
+version = '0.3.5'
+
+setup(
+    name='django_pony_forms',
+    version=version,
+    license='Apache License, Version 2.0',
+    description='Django-pony-forms helps you to write better html for your Django forms',
+    packages=find_packages(exclude=['testproject', 'testproject.*']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'django',
+        'six'
+    ],
+    author='Marco Braak',
+    author_email='mbraak@ridethepony.nl',
+    url='https://github.com/mbraak/django_pony_forms',
+    download_url='https://github.com/mbraak/django_pony_forms/zipball/master',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
