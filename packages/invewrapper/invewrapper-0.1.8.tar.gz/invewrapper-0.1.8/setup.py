56af5859f58413a5edb903adc979116e0ef83fc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:10 +0000

    Add package invewrapper/0.1.8/invewrapper-0.1.8.tar.gz

---
+++ packages/invewrapper/invewrapper-0.1.8.tar.gz/invewrapper-0.1.8/setup.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+setup(
+	name='invewrapper',
+	version='0.1.8',
+	description='dummy/transitional package that depends on "pew"',
+    long_description='https://pypi.python.org/pypi/pew',
+	author='Dario Bertini',
+	author_email='berdario+pypi@gmail.com',
+	install_requires=['pew']
+)
