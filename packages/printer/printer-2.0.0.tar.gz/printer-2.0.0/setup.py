9214d8c90c007618301140ec6e95da4205f1edc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:26 +0000

    Add package printer/2.0.0/printer-2.0.0.tar.gz

---
+++ packages/printer/printer-2.0.0.tar.gz/printer-2.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+	name="printer",
+	version="2.0.0",
+	py_modules=["printer"],
+	author="YMJ",
+	url="www.github.com/tulpar008",
+	description="A perfect printer of list",
+	)
