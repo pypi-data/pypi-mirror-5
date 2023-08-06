1e8f95d2ee824fbced0e7b395bd0b572f043461e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:07 +0000

    Add package Relief/0.2.0/Relief-0.2.0.tar.gz

---
+++ packages/Relief/Relief-0.2.0.tar.gz/Relief-0.2.0/CHANGELOG.rst
@@ -0,0 +1,19 @@
+Changelog
+=========
+
+Version 0.2.0
+-------------
+
+- Values in :class:`~relief.Form` structures can now be validated by
+  subclassing :class:`~relief.Form` and adding `validate_{key}` methods.
+
+Version 0.1.1
+-------------
+
+- Removed mention of being able to mutate :class:`relief.Dict` and
+  :class:`relief.Form`.
+
+Version 0.1.0
+-------------
+
+Initial release.
---
+++ packages/Relief/Relief-0.2.0.tar.gz/Relief-0.2.0/relief/utils/__init__.py
@@ -0,0 +1,32 @@
+# coding: utf-8
+"""
+    relief.utils
+    ~~~~~~~~~~~~
+
+    :copyright: 2013 by Daniel Neuhäuser
+    :license: BSD, see LICENSE.rst for details
+"""
+import sys
+
+from relief.utils.idd import InheritingDictDescriptor
+
+
+class class_cloner(classmethod):
+    """
+    Like :class:`classmethod` but calls the method with a clone of the class.
+    """
+    def __get__(self, instance, cls):
+        attributes = {
+            "__doc__": getattr(cls, "__doc__", None),
+            # module name in the scope of the caller
+            "__module__": sys._getframe(1).f_globals.get("__name__", "__main__")
+        }
+        clone = cls.__class__(cls.__name__, (cls, ), attributes)
+        return super(class_cloner, self).__get__(instance, clone)
+
+
+def as_singleton(cls):
+    return cls()
+
+
+__all__ = ['InheritingDictDescriptor', 'class_cloner', 'as_singleton']
