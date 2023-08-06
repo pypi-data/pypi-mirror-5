14f9195f1676a7ac91ee63f0d955b2a8fa0270b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:44 +0000

    Add package ex/1.0.0/ex-1.0.0.tar.gz

---
+++ packages/ex/ex-1.0.0.tar.gz/ex-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name 		 = 'ex',
+		version 	 = '1.0.0',
+		py_mpdules 	 = ['ex'],
+		author		 = 'daodao',
+		author_email = 'daodao.gc@gmail.com',
+		url			 = 'http://daodao.org',
+		descript	 = 'test.......'
+	)
