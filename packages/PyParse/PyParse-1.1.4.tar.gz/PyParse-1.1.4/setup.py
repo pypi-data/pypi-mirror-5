d6026288c7364673cebc1c5adef0ccbc39812c25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:46 +0000

    Add package PyParse/1.1.4/PyParse-1.1.4.tar.gz

---
+++ packages/PyParse/PyParse-1.1.4.tar.gz/PyParse-1.1.4/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name='PyParse',
+      author='Matt Johnson, PharmD.',
+      author_email='johnson.matthew.h@gmail.com',
+      version='1.1.4',
+      py_modules=['PyParse'],
+      url = 'https://github.com/mhjohnson/PyParse',
+      description = "Parse text files in an easier and more maintainable manner",
+      long_description = "Parse text files in an easier and more maintainable manner"
+)
