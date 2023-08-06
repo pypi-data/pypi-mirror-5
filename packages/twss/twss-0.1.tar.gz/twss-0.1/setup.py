f5d54cc1d256b9602ed697f42485a9ff8c769640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:08 +0000

    Add package twss/0.1/twss-0.1.tar.gz

---
+++ packages/twss/twss-0.1.tar.gz/twss-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup 
+
+setup(name='twss',
+        version='0.1',
+        description='A Naive Bayes classifier that can identify double entendres.',
+        url='https://github.com/sengupta/twss',
+        author='Aditya Sengupta',
+        author_email='aditya@sengupta.me',
+        license='WTFPL',
+        packages=['twss',],
+        install_requires=[
+            "nltk==2.0.1rc1",
+            ],
+        zip_safe=False,
+        )
