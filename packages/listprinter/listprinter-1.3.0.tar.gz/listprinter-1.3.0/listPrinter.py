485ce6a09e13c46b41c2a9a23f8ac1665d37c791
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:25 +0000

    Add package listprinter/1.3.0/listprinter-1.3.0.tar.gz

---
+++ packages/listprinter/listprinter-1.3.0.tar.gz/listprinter-1.3.0/listPrinter.py
@@ -0,0 +1,15 @@
+"""this is listPrinter.py module which contain print_lol method which is
+capable of printing list in list"""
+
+
+def print_lol(the_list,indent=False,level=0):
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,indent,level+1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t",end='')
+            print(each_item)
+
+            
---
+++ packages/listprinter/listprinter-1.3.0.tar.gz/listprinter-1.3.0/system.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(
+    name='listprinter',
+    version='1.3.0',
+    py_modules=['listPrinter'],
+    author='ajagor')
