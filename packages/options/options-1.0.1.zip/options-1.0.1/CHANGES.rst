184e9fb5680542da59d9a185de510c2bfbd787d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:16 +0000

    Add package options/1.0.1/options-1.0.1.zip

---
+++ packages/options/options-1.0.1.zip/options-1.0.1/CHANGES.rst
@@ -0,0 +1,19 @@
+
+
+1.0.1
+=====
+
+  * Moved main documentation to Sphinx format in ./docs, and hosted
+    the long-form documentation on readthedocs.org. README.rst now
+    an abridged version/teaser for the module.
+
+1.0
+===
+
+  * Cleaned up source for better PEP8 conformance
+  * Bumped version number to 1.0 as part of move to `semantic 
+    versioning <http://semver.org>`_, or at least enough of it so
+    as to not screw up Python installation procedures (which don't
+    seem to understand 0.401 is a lesser version that 0.5, because
+    401 > 5).
+
---
+++ packages/options/options-1.0.1.zip/options-1.0.1/setup.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+
+from setuptools import setup, find_packages
+
+def linelist(text):
+    """
+    Returns each non-blank line in text enclosed in a list.
+    """
+    return [ l.strip() for l in text.strip().splitlines() if l.split() ]
+    
+    # The double-mention of l.strip() is yet another fine example of why
+    # Python needs en passant aliasing.
+
+
+setup(
+    name='options',
+    version='1.0.1',
+    author='Jonathan Eunice',
+    author_email='jonathan.eunice@gmail.com',
+    description='Container for flexible class, instance, and function call options',
+    long_description=open('README.rst').read(),
+    url='https://bitbucket.org/jeunice/options',
+    packages=['options'],
+    install_requires=['stuf>=0.9.12', 'six>=1.4.1'],
+    tests_require = ['tox', 'pytest'],
+    zip_safe = False,
+    keywords='options config configuration parameters arguments',
+    classifiers=linelist("""
+        Development Status :: 4 - Beta
+        Operating System :: OS Independent
+        License :: OSI Approved :: BSD License
+        Intended Audience :: Developers
+        Programming Language :: Python
+        Programming Language :: Python :: 2.6
+        Programming Language :: Python :: 2.7
+        Programming Language :: Python :: 3
+        Programming Language :: Python :: 3.2
+        Programming Language :: Python :: 3.3
+        Programming Language :: Python :: Implementation :: CPython
+        Programming Language :: Python :: Implementation :: PyPy
+        Topic :: Software Development :: Libraries :: Python Modules
+    """)
+)
