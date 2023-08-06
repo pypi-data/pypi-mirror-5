91db5b1f9c40314b63b0c145bb97ba801efc30be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:17 +0000

    Add package google.foresite-toolkit/1.3/google.foresite-toolkit-1.3.tar.gz

---
+++ packages/google.foresite-toolkit/google.foresite-toolkit-1.3.tar.gz/google.foresite-toolkit-1.3/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+version = '1.3'
+
+setup(
+  name='google.foresite-toolkit',
+  version=version,
+  description='Library for constructing, parsing, manipulating and serializing OAI-ORE Resource Maps',
+  long_description="""\
+  """,
+  classifiers=[],
+  author='Rob Sanderson',
+  author_email='azaroth@liv.ac.uk',
+  url='http://code.google.com/p/foresite-toolkit/',
+  license='BSD',
+  packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+  include_package_data=True,
+  zip_safe=False,
+
+  # Specify additional patterns to match files and directories that may or may
+  # not be matched by MANIFEST.in or found in source control.
+  package_data = {
+    '': ['*.txt'],
+  },
+
+  install_requires = [
+    'rdflib == 4.0.1',
+    'lxml'
+  ],
+
+  test_suite='foresite.tests.test_suite'
+)
