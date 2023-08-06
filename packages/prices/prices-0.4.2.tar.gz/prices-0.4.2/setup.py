aba5b32882f1fce98b0020014eadcc2f26027e53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:39 +0000

    Add package prices/0.4.2/prices-0.4.2.tar.gz

---
+++ packages/prices/prices-0.4.2.tar.gz/prices-0.4.2/setup.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+import prices
+
+from setuptools import setup
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.5',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+]
+
+setup(name='prices',
+      author='Mirumee Software',
+      author_email='hello@mirumee.com',
+      description='Python price handling for humans',
+      license='BSD',
+      version='0.4.2',
+      url='http://satchless.com/',
+      packages=['prices'],
+      test_suite='prices.tests',
+      include_package_data=True,
+      classifiers=CLASSIFIERS,
+      platforms=['any'])
