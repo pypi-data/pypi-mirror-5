5a5c6f23ec6027e0b8613a743e91beb53c9b8363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:10 +0000

    Add package pymcu/1.0.14/pymcu-1.0.14.zip

---
+++ packages/pymcu/pymcu-1.0.14.zip/pymcu-1.0.14/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pymcu',
+      version='1.0.14',
+      description='pyMCU - Python Micro Controller Wrapper',
+      author='Richard Wardlow',
+      author_email='richard@pymcu.com',
+      url='http://www.pymcu.com',
+      py_modules=['pymcu'],
+     )
