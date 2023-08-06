16d1d540f0931627bd7495d4d16d84abd0de92d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:26 +0000

    Add package SQLAlchemy-Utils/0.14.7/SQLAlchemy-Utils-0.14.7.tar.gz

---
+++ packages/SQLAlchemy-Utils/SQLAlchemy-Utils-0.14.7.tar.gz/SQLAlchemy-Utils-0.14.7/sqlalchemy_utils/__init__.py
@@ -0,0 +1,57 @@
+from .exceptions import ImproperlyConfigured
+from .functions import (
+    sort_query, defer_except, escape_like, primary_keys, table_name
+)
+from .listeners import coercion_listener
+from .merge import merge, Merger
+from .proxy_dict import ProxyDict, proxy_dict
+from .types import (
+    ColorType,
+    EmailType,
+    instrumented_list,
+    InstrumentedList,
+    IPAddressType,
+    PhoneNumber,
+    PhoneNumberType,
+    NumberRange,
+    NumberRangeException,
+    NumberRangeRawType,
+    NumberRangeType,
+    ScalarListType,
+    ScalarListException,
+    TSVectorType,
+    UUIDType,
+)
+
+
+__version__ = '0.14.7'
+
+
+__all__ = (
+    ImproperlyConfigured,
+    coercion_listener,
+    sort_query,
+    defer_except,
+    escape_like,
+    instrumented_list,
+    merge,
+    primary_keys,
+    proxy_dict,
+    table_name,
+    ColorType,
+    EmailType,
+    InstrumentedList,
+    IPAddressType,
+    Merger,
+    NumberRange,
+    NumberRangeException,
+    NumberRangeRawType,
+    NumberRangeType,
+    PhoneNumber,
+    PhoneNumberType,
+    ProxyDict,
+    ScalarListType,
+    ScalarListException,
+    TSVectorType,
+    UUIDType,
+)
