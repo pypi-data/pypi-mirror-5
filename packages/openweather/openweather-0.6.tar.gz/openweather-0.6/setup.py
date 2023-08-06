80f5fd50c1b243e6d6a6ef1a42a9adc86912376f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:41 +0000

    Add package openweather/0.6/openweather-0.6.tar.gz

---
+++ packages/openweather/openweather-0.6.tar.gz/openweather-0.6/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='openweather',
+      version='0.6',
+      description='OpenWeatherMap.org API wrapper',
+      author='Marian Steinbach',
+      author_email='marian@sendung.de',
+      url='http://github.com/marians/openweather',
+      py_modules=['openweather'],
+      install_requires=['daterangestr']
+      )
