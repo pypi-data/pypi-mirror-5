526471af05da066f7a83ce9fdaeed4ad07cdad3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:20 +0000

    Add package Glenn/0.1/Glenn-0.1.tar.gz

---
+++ packages/Glenn/Glenn-0.1.tar.gz/Glenn-0.1/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Glenn',
+      version="0.1",
+      description='Include tags in any kind of text document.',
+      author='Hannes Lohmander',
+      author_email='hannes@lohmander.me',
+      scripts=['glenn']
+      )
