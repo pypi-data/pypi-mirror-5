e251f62bfe1a480ce79927ba84ddbec2dc1bae95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:08 +0000

    Add package python-veracity/0.0.10/python_veracity-0.0.10-py33-none-any.whl

---
+++ packages/python-veracity/python_veracity-0.0.10-py33-none-any.whl/python_veracity-0.0.10.data/scripts/vv-poller-script.py
@@ -0,0 +1,10 @@
+#!python
+# EASY-INSTALL-ENTRY-SCRIPT: 'python-veracity==0.0.10','console_scripts','vv-poller'
+__requires__ = 'python-veracity==0.0.10'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('python-veracity==0.0.10', 'console_scripts', 'vv-poller')()
+    )
