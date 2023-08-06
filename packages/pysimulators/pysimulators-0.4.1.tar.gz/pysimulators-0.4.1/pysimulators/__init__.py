dec3ab350e0efb655a4afce8b4e67b22fd7fd793
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:59 +0000

    Add package pysimulators/0.4.1/pysimulators-0.4.1.tar.gz

---
+++ packages/pysimulators/pysimulators-0.4.1.tar.gz/pysimulators-0.4.1/pysimulators/__init__.py
@@ -0,0 +1,24 @@
+from pyoperators.utils.mpi import MPI
+
+# force gfortran's read statement to always use the dot sign as fraction
+# separator (PR47007)
+import locale
+locale.setlocale(locale.LC_NUMERIC, 'POSIX')
+del locale
+
+from . import _flib
+from . import geometry
+from .acquisitionmodels import *
+from .datatypes import *
+from .instruments import *
+#from .mpiutils import *
+from .configurations import *
+from .planes import *
+from .pointings import *
+#from .processing import *
+from .quantities import *
+from .datautils import *
+from .wcsutils import *
+
+import pkg_resources  # part of setuptools
+__version__ = pkg_resources.require("pysimulators")[0].version
