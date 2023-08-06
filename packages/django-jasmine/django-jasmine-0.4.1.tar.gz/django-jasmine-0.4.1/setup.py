2451abd0fc1c6c596cb37015da14a8f074ad43d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:43 +0000

    Add package django-jasmine/0.4.1/django-jasmine-0.4.1.tar.gz

---
+++ packages/django-jasmine/django-jasmine-0.4.1.tar.gz/django-jasmine-0.4.1/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+
+META_DATA = dict(
+    name='django-jasmine',
+    version='0.4.1',
+    description='Jasmine Javascript testing integration for Django.',
+    long_description=open('README.rst').read(),
+    author='Jonathan McCoy',
+    author_email='modelsolutions@gmail.com',
+    maintainer='Adrien Lemaire',
+    maintainer_email='adrien.lemaire@gmail.com',
+    url='https://github.com/Fandekasp/django-jasmine',
+    keywords='django javascript test',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: JavaScript',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Testing',
+    ],
+    install_requires= ['Django>=1.3', ],
+    packages=find_packages(exclude=["example", ]),
+    include_package_data=True,
+    zip_safe=False,
+)
+
+
+if __name__ == "__main__":
+    setup(**META_DATA)
