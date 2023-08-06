dfaf7038f61a84336896a1c590906ca802e04517
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:54 +0000

    Add package django-slim/0.6/django-slim-0.6.tar.gz

---
+++ packages/django-slim/django-slim-0.6.tar.gz/django-slim-0.6/src/slim/exceptions.py
@@ -0,0 +1,8 @@
+__title__ = 'slim.exceptions'
+__version__ = '0.6'
+__build__ = 0x000006
+__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
+__all__ = ('LocaleurlImportError',)
+
+class LocaleurlImportError(ImportError):
+    pass
