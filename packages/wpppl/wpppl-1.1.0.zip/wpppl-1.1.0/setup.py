30d302c11cc76a4189f87e9077fee4d57b91c7a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:51 +0000

    Add package wpppl/1.1.0/wpppl-1.1.0.zip

---
+++ packages/wpppl/wpppl-1.1.0.zip/wpppl-1.1.0/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+        name    ='wpppl',
+        version ='1.1.0',
+        py_modules=['wpppl'],
+        author ='wpppl',
+        author_email='wpppl@sina.com',
+        descrption='A simple printer of nested listes',
+        
+
+
+
+
+
+
+    )
---
+++ packages/wpppl/wpppl-1.1.0.zip/wpppl-1.1.0/wpppl.py
@@ -0,0 +1,15 @@
+'''这是“wpppl.py”模块，提供了一个名为wpppl（）的函数，这个函数的作用是打印列表，其中有可能
+包含或者不包含列表'''
+
+
+
+def wpppl(the_list,level):
+    '''这个函数取一个位置参数，名为the_list，这可以使任何的python列表。所指定的列表中的每一个数据项
+（递归地）输出到屏幕上，各数据项占一行。第二个参数是在遇到嵌套表格时插入制表符。'''
+    for each in the_list:
+        if isinstance(each,list):
+            wpppl(each,level+1)
+        else:
+            for tab_stop in range(level):
+                print("\t",end='')
+            print(each)
