21847e3aff42ed1f029160c16bcaadc80d9cf30e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:32 +0000

    Add package django-basic-stats/0.1/django-basic-stats-0.1.tar.gz

---
+++ packages/django-basic-stats/django-basic-stats-0.1.tar.gz/django-basic-stats-0.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from os import path
+from setuptools import setup, find_packages
+
+
+def read(name):
+    return open(path.join(path.dirname(__file__), name)).read()
+
+setup(
+    name='django-basic-stats',
+    description=("django-basic-stats is a simple traffic statistics application. "
+                 "It show latest referrer, google queried terms or overall hits count. "
+                 "It also provides optional logging and statistics for mobile devices."),
+    long_description=read("README.rst"),
+    version='0.1',
+    maintainer="Piotr Malinski",
+    maintainer_email="riklaunim@gmail.com",
+    include_package_data=True,
+    install_requires=(
+        'django',
+    ),
+    packages=find_packages(),
+)
