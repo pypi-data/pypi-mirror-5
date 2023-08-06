84071107520e7a425d327150d36d65e8cf65c3d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:15 +0000

    Add package deflacue/0.2.0/deflacue-0.2.0.tar.gz

---
+++ packages/deflacue/deflacue-0.2.0.tar.gz/deflacue-0.2.0/setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup
+from deflacue import VERSION
+
+f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
+readme = f.read()
+f.close()
+
+setup(
+    name='deflacue',
+    version=".".join(map(str, VERSION)),
+    description='deflacue is a SoX based audio splitter to split audio CD images incorporated with .cue files',
+    long_description=readme,
+    author="Igor 'idle sign' Starikov",
+    author_email='idlesign@yandex.ru',
+    url='http://github.com/idlesign/deflacue',
+    packages=['deflacue'],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.2',
+        'Topic :: Multimedia :: Sound/Audio :: Conversion',
+    ],
+)
