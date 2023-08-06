e3345f793615e3675c020650cc64125976e7bd3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:04 +0000

    Add package zathleteListClass/1.0.0/zathleteListClass-1.0.0.tar.gz

---
+++ packages/zathleteListClass/zathleteListClass-1.0.0.tar.gz/zathleteListClass-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name = 'zathleteListClass',
+		version = '1.0.0',
+		py_modules = ['zathleteListClass'],
+		author = 'surf',
+		author_email = 'surfielda@aol.com',
+		url = 'http://www.zurfgames.com',
+		description = 'A simple printer of athlete lists',
+	 )
