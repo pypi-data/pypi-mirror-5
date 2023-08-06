63a1671ef3bd91acc527cce824d97479f48c8f98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:09 +0000

    Add package ska/0.3/ska-0.3.tar.gz

---
+++ packages/ska/ska-0.3.tar.gz/ska-0.3/setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup, find_packages
+
+try:
+  readme = open(os.path.join(os.path.dirname(__file__), 'readme.rst')).read()
+except:
+  readme = ''
+
+version = '0.3'
+
+setup(
+    name = 'ska',
+    version = version,
+    description = ("Signing (HTTP) requests using symmetric-key algorithm."),
+    long_description = readme,
+    classifiers = [
+        "Programming Language :: Python",
+        "Environment :: Web Environment",
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+    keywords = 'signing (HTTP) requests, symmetric-key algorithm, signing URLs, python',
+    author = 'Artur Barseghyan',
+    author_email = 'artur.barseghyan@gmail.com',
+    package_dir = {'':'src'},
+    packages = find_packages(where='./src'),
+    install_requires = ['',]
+)
