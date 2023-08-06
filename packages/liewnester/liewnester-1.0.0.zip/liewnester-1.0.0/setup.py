119100d82cb4838356fb71740910e8580e938307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:14 +0000

    Add package liewnester/1.0.0/liewnester-1.0.0.zip

---
+++ packages/liewnester/liewnester-1.0.0.zip/liewnester-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+        name = 'liewnester',
+        version = '1.0.0',
+        py_modules =['liewnester'],
+        author = 'instituteliew',     
+    )
