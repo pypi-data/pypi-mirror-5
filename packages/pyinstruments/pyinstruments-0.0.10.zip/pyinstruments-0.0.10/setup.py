4fc39b84d0105da5572a3cf59792d75cf0753355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:53 +0000

    Add package pyinstruments/0.0.10/pyinstruments-0.0.10.zip

---
+++ packages/pyinstruments/pyinstruments-0.0.10.zip/pyinstruments-0.0.10/setup.py
@@ -0,0 +1,44 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "pyinstruments",
+    version = "0.0.10",
+    author = "Samuel Deleglise",
+    author_email = "samuel.deleglise@gmail.com",
+    description = ("""Control of data acquisition with remote instruments using 
+    (IVI-)dotnet, (IVI-)COM, Visa, and serial protocols.
+    python dotnet and/or comtypes should be installed"""),
+    license = "BSD",
+    keywords = "instruments data-acquisition IVI interface",
+    url = "https://github.com/SamuelDeleglise/pyinstruments",
+    packages=['pyinstruments',
+              'pyinstruments.config',
+              'pyinstruments.config.gui',
+              'pyinstruments.drivers',
+              'pyinstruments.drivers.ivi_interop',
+              'pyinstruments.drivers.ivi_interop.ivicom',
+              'pyinstruments.drivers.ivi_interop.ividotnet',
+              'pyinstruments.drivers.serial',
+              'pyinstruments.drivers.visa',
+              'pyinstruments.instruments',
+              'pyinstruments.wrappers',
+              'pyinstruments.factories',
+              'guiwrappersutils',
+              'conf_xml'],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Scientific/Engineering :: Human Machine Interfaces",
+        "License :: OSI Approved :: BSD License",
+    ],
+    install_requires=[
+]
+)
