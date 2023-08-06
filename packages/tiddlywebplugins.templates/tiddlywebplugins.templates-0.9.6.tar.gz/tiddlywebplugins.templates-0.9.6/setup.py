d0842e68fff24ff09f5e32c7bbeeccef24942c35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:06 +0000

    Add package tiddlywebplugins.templates/0.9.6/tiddlywebplugins.templates-0.9.6.tar.gz

---
+++ packages/tiddlywebplugins.templates/tiddlywebplugins.templates-0.9.6.tar.gz/tiddlywebplugins.templates-0.9.6/setup.py
@@ -0,0 +1,28 @@
+
+import os
+from setuptools import setup, find_packages
+
+VERSION = '0.9.6'
+
+setup(
+        namespace_packages = ['tiddlywebplugins'],
+        name = 'tiddlywebplugins.templates',
+        version = VERSION,
+        description = 'A TiddlyWeb plugin with tools and packaging for templates.',
+        long_description=file(os.path.join(os.path.dirname(__file__), 'README')).read(),
+        author = 'Chris Dent',
+        url = 'http://pypi.python.org/pypi/tiddlywebplugins.templates',
+        packages = find_packages(exclude=['test']),
+        author_email = 'cdent@peermore.com',
+        platforms = 'Posix; MacOS X; Windows',
+        install_requires = ['setuptools',
+            'tiddlyweb>=1.4.13',
+            'Jinja2'
+        ],
+        extras_require = {
+            'testing': ['pytest'],
+        },
+        include_package_data = True,
+        zip_safe = False,
+        license = 'BSD'
+)
