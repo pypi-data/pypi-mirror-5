335eb0e9a780dae881baf335c38b8c612dc85f12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:24 +0000

    Add package django_simple_markdown/1.0.2/django_simple_markdown-1.0.2.zip

---
+++ packages/django_simple_markdown/django_simple_markdown-1.0.2.zip/django_simple_markdown-1.0.2/setup.py
@@ -0,0 +1,14 @@
+# ~ le encoding: utf-8 ~
+
+from setuptools import find_packages, setup
+
+setup(
+	name = 'django_simple_markdown',
+	version = '1.0.2',
+	description = 'Django package for markdown editing within forms. Uses simple-markdown.js',
+	packages = find_packages(),
+	requires = ['Markdown'],
+	author = 'Daniel Oliveira',
+	author_email = 'daniel@dvalbrand.com',
+	url = 'https://github.com/Valbrand/django-simple-markdown',
+)
