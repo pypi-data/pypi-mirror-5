9323252ac274ae35a68ba8e1e0a4660cea734716
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:02 +0000

    Add package new_nester/1.0.1/new_nester-1.0.1.zip

---
+++ packages/new_nester/new_nester-1.0.1.zip/new_nester-1.0.1/setup.py
@@ -0,0 +1,3 @@
+from distutils.core import setup
+
+setup(name = 'new_nester',version = '1.0.1',py_modules = ['new_nester'],author = 'wu',author_email = 'theseaofstars@live.cn', url = 'http://www.headfirstlabs.com',description = 'A simple test')
