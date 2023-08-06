02f278a3220c9a5296c2bc218b771daa5d6f84e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:56 +0000

    Add package django-pages/1.0.7/django-pages-1.0.7.tar.gz

---
+++ packages/django-pages/django-pages-1.0.7.tar.gz/django-pages-1.0.7/django_pages/log/middleware.py
@@ -0,0 +1,10 @@
+import datetime
+import logging
+
+
+class RequestLog(object):
+
+    def process_request(self, request):
+        
+        log = logging.getLogger('requestlog')
+        log.info('REQUEST: %s' % unicode(request).replace('\n', ' ').encode('utf-8'))
---
+++ packages/django-pages/django-pages-1.0.7.tar.gz/django-pages-1.0.7/django_pages/settings.py
@@ -0,0 +1,30 @@
+# -*- encoding: utf-8 -*-
+
+from django.conf import settings
+
+
+try:
+    ADMIN_MEDIA_PREFIX = settings.ADMIN_MEDIA_PREFIX
+
+except AttributeError:
+    ADMIN_MEDIA_PREFIX = '/static/admin/'
+
+# where to upload flag images /media/FLAG_UPLOAD_DIR
+try:
+    FLAG_UPLOAD_DIR = settings.FLAG_UPLOAD_DIR
+
+except AttributeError:
+    FLAG_UPLOAD_DIR = settings.MEDIA_ROOT
+
+# where to look for template in templates/
+try:
+    TEMPLATE_PATH = settings.TEMPLATE_PATH
+
+except AttributeError:
+    TEMPLATE_PATH = 'default'
+
+try:
+    POSTS_ON_PAGE = settings.POSTS_ON_PAGE
+
+except AttributeError:
+    POSTS_ON_PAGE = 10
