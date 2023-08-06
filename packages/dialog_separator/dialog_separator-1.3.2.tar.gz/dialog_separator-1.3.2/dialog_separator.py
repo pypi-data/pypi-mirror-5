74baff8abedfd5bf363f3d35785cbd8fd24c8ab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:21 +0000

    Add package dialog_separator/1.3.2/dialog_separator-1.3.2.tar.gz

---
+++ packages/dialog_separator/dialog_separator-1.3.2.tar.gz/dialog_separator-1.3.2/dialog_separator.py
@@ -0,0 +1,33 @@
+a = []
+b = []
+from frula_nester import print_lol
+try:
+        data = open("sketch2.txt") #type your .txt file
+        for each_line in data:
+                try:
+                        (role,line_spoken) = each_line.split(":", 1)
+                        line_spoken = line_spoken.strip()
+                        if role == "A": #change role in your charachter, such as "Susane" 
+                                a.append(line_spoken)
+
+                        elif role == "B":
+                                b.append(line_spoken)
+                except ValueError:
+                        pass
+        data.close()
+except IOError:
+        print("The datafile is missing!")
+
+try:
+        with open("a_data.txt", "w") as a_file: #change output txt file in your format, such as Susane_data.txt
+                print_lol(a, fh=a_file)
+        with open("b_data.txt", "w") as b_file: #same here, but other participant
+                print_lol(b, fh=b_file)
+        
+except IOError as err:
+        print("File error: " + str(err))
+
+                
+
+
+        
