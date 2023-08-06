702e2a53f48a0df63e5b90dde91b04cce7ca5335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:34 +0000

    Add package pscripts/0.1.63/pscripts-0.1.63.tar.gz

---
+++ packages/pscripts/pscripts-0.1.63.tar.gz/pscripts-0.1.63/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+version='0.1.63'
+name='pscripts'
+scripts = [
+    'scripts/python-deployment',
+    'scripts/hdmi_brightness',
+    'scripts/update_external_ip',
+]
+classifiers = [
+        'Programming Language :: Python :: 3.3',
+    ]
+install_requires=[
+    'setuptools',
+    'SimpleDaemon >= 1.3.0',
+    'PyYAML >= 3.10',
+    'pidfile >= 0.1.0',
+    ]
+Data_Files = [('/etc/external_ip_updater/', ['config/urls.yaml','config/config.conf'])]
+setup(
+    name = name,
+    version = version,
+    packages = [name],
+    description = 'Automation Scripts for Linux',
+    author='Fenton Travers',
+    author_email='fenton.travers@gmail.com',
+    url='www.google.com',
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description='Automates some python deployment steps',
+    classifiers=classifiers,
+    scripts = scripts,
+    data_files=Data_Files,
+)
