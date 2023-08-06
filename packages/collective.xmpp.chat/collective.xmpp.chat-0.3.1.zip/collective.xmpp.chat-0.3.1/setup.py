cbb3629fd62355cc3c04508b9c6e3f7342f9a543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:59 +0000

    Add package collective.xmpp.chat/0.3.1/collective.xmpp.chat-0.3.1.zip

---
+++ packages/collective.xmpp.chat/collective.xmpp.chat-0.3.1.zip/collective.xmpp.chat-0.3.1/setup.py
@@ -0,0 +1,48 @@
+from distribute_setup import use_setuptools
+from setuptools import setup, find_packages
+import os
+
+use_setuptools()
+version = '0.3.1'
+
+setup(
+    name='collective.xmpp.chat',
+    version=version,
+    description="XMPP-based instant Messaging for Plone",
+    long_description=open("README.rst").read() + "\n" +
+                open(os.path.join("docs", "HISTORY.txt")).read(),
+    # Get more strings from
+    # http://pypi.python.org/pypi?:action=list_classifiers
+    classifiers=[
+    "Framework :: Plone",
+    "Programming Language :: Python",
+    ],
+    keywords='xmpp chat messaging',
+    author='JC Brand',
+    author_email='jc@opkode.com',
+    url='https://github.com/collective/collective.xmpp.chat',
+    license='GPL',
+    packages=find_packages('src', exclude=['ez_setup']),
+    package_dir={'': 'src'},
+    namespace_packages=['collective', 'collective.xmpp'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'plone.app.jquery>=1.7.2',
+        'setuptools',
+        'collective.xmpp.core',
+    ],
+    extras_require={
+        "sphinx": [
+            "Sphinx>=1.0",
+            "repoze.sphinx.autointerface",
+        ],
+        "test": ['plone.app.testing[robot]'],
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+    setup_requires=["PasteScript", "setuptools_git>=0.3"],
+    paster_plugins=["ZopeSkel"],
+)
