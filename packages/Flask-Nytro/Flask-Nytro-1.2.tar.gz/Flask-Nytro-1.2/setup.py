cfb0ffa917297a8f2b2fbaeb74bfc0b870190e14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:45 +0000

    Add package Flask-Nytro/1.2/Flask-Nytro-1.2.tar.gz

---
+++ packages/Flask-Nytro/Flask-Nytro-1.2.tar.gz/Flask-Nytro-1.2/setup.py
@@ -0,0 +1,38 @@
+"""
+Flask-Nytro
+-------------
+
+Nytro is an extension to help the developers providing a set of useful tools giving even more facility to development apps with Flask.
+"""
+from setuptools import setup
+
+
+setup(
+    name='Flask-Nytro',
+    version='1.2',
+    url='http://github.com/sacanix/Flask-Nytro',
+    license='BSD',
+    author='Tony Kamillo (Sacanix)',
+    author_email='tonysacanix@gmail.com',
+    description='''Nytro is an extension to help the developers providing a set of useful tools giving even more facility to development apps with Flask.''',
+    long_description=__doc__,
+    # if you would be using a package instead use packages instead
+    # of py_modules:
+    #py_modules=[''],
+    packages=['flask_nytro', 'flask_nytro.helpers'],
+    #zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
