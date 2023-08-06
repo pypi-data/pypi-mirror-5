737b540070d1b2546075091fff174091898b8948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:42 +0000

    Add package signalbox/0.1.9/signalbox-0.1.9.tar.gz

---
+++ packages/signalbox/signalbox-0.1.9.tar.gz/signalbox-0.1.9/setup.py
@@ -0,0 +1,31 @@
+import os
+from pip.req import parse_requirements
+from setuptools import setup, find_packages
+
+
+reqs = parse_requirements("requirements.txt")
+install_reqs = [str(ir.req) for ir in reqs]
+
+scripts = []
+if 'DYNO' in os.environ:  # assume we are on heroku
+  scripts = scripts + ['bin/pandoc']  # and use our bespoke pandoc build
+
+setup(
+    name='signalbox',
+    version='0.1.9',
+    author='Ben Whalley',
+    author_email='benwhalley@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    scripts=scripts,
+    url='http://pypi.python.org/pypi/signalbox/',
+    license='LICENSE.txt',
+    description='Run longitudinal studies and randomised trials over the web and telephone.',
+    long_description=open('README.txt').read(),
+    install_requires=install_reqs,
+    entry_points = {
+            'console_scripts': [
+                'bootstrap_signalbox = signalbox.utilities.bootstrap:bootstrap_signalbox',
+            ]
+    }
+)
