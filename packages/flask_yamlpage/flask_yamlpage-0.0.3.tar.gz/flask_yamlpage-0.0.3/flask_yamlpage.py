a1898992ec13ef58b25ea8290a89fc9f8159a37e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:34 +0000

    Add package flask_yamlpage/0.0.3/flask_yamlpage-0.0.3.tar.gz

---
+++ packages/flask_yamlpage/flask_yamlpage-0.0.3.tar.gz/flask_yamlpage-0.0.3/flask_yamlpage.py
@@ -0,0 +1,110 @@
+'''
+flask-yamlpage
+==============
+Flatpages in yaml syntax
+
+app.py
+------
+    from flask import Flask
+    from flask_yamlpage import FlaskYamlPage
+
+
+    app = Flask(__name__)
+    yamlpages = FlaskYamlPage(app)
+
+
+    @app.route('/')
+    def index():
+        return 'index'
+
+
+    if __name__ == '__main__':
+        app.run(debug=True, port=8000)
+
+
+yamlpages/test#.yml
+-------------------
+    title: test yamlpage
+    body: |
+        Yamlpage
+        ========
+        Hello, world!
+        {{ url_for('index') }}
+
+
+templates/yamlpage.html
+-----------------------
+    <title>{{ page.title }}</title>
+
+    {{ page.body | render | markdown | safe }}
+
+
+config defaults
+---------------
+    'YAMLPAGE_CONTENT_DIR'      : './yamlpages',
+    'YAMLPAGE_DEFAULT_TEMPLATE' : 'yamlpage.html',
+    'YAMLPAGE_AUTO_ROUTING'     : True,
+    'YAMLPAGE_MARKDOWN_FILTER'  : True,
+    'YAMLPAGE_RENDER_FILTER'    : True,
+
+'''
+
+try:
+    import flask
+    import yamlpage
+except ImportError:
+    # for installation
+    pass
+
+
+
+class FlaskYamlPage(yamlpage.YamlPage):
+    defaults = {
+        'YAMLPAGE_CONTENT_DIR'      : './yamlpages',
+        'YAMLPAGE_DEFAULT_TEMPLATE' : 'yamlpage.html',
+        'YAMLPAGE_AUTO_ROUTING'     : True,
+        'YAMLPAGE_MARKDOWN_FILTER'  : True,
+        'YAMLPAGE_RENDER_FILTER'    : True,
+    }
+
+    def __init__(self, app):
+        self.app = app
+        for k, v in self.defaults.iteritems():
+            app.config.setdefault(k, v)
+        super(FlaskYamlPage, self).__init__(app.config['YAMLPAGE_CONTENT_DIR'])
+
+        if app.config['YAMLPAGE_AUTO_ROUTING']:
+            @app.after_request
+            def routing(response):
+                if(response.status_code == 404):
+                    res = self.render(flask.request.path)
+                    if res:
+                        return app.make_response(res)
+                return response
+
+        if app.config['YAMLPAGE_MARKDOWN_FILTER']:
+            import misaka
+
+            @app.template_filter('markdown')
+            def markdown_filter(s):
+                return misaka.html(s)
+
+        if app.config['YAMLPAGE_MARKDOWN_FILTER']:
+            @app.template_filter('render')
+            def render_filter(s):
+                return flask.render_template_string(s)
+
+
+    def get_or_404(self, url):
+        page = self.get(url)
+        if page is None:
+            flask.abort(404)
+        return page
+
+    def render(self, url):
+        page = self.get_or_404(url)
+        if page is None:
+            return
+        template = page.get('template',
+                self.app.config['YAMLPAGE_DEFAULT_TEMPLATE'])
+        return flask.render_template(template, page=page)
---
+++ packages/flask_yamlpage/flask_yamlpage-0.0.3.tar.gz/flask_yamlpage-0.0.3/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+import os
+import sys
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+
+NAME = 'flask_yamlpage'
+README = open('README.md').read()
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit(1)
+
+if len(sys.argv) == 1:
+    print 'Use "./setup.py register" for registration or update package'
+    print 'Or  "./setup.py publish" for publication new release'
+    sys.exit()
+
+
+setup(
+    name         = NAME,
+    url          = 'https://github.com/imbolc/%s' % NAME.replace('_', '-'),
+    version      = '0.0.3',
+    description  = README.split('===\n')[1].strip().split('\n\n')[0],
+    long_description = README.split('\n\n', 1)[1],
+
+    py_modules   = [NAME],
+
+    author       = 'Imbolc',
+    author_email = 'imbolc@imbolc.name',
+    license      = 'MIT',
+
+    classifiers  = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Programming Language :: Python',
+    ],
+
+    install_requires=['flask', 'yamlpage'],
+)
