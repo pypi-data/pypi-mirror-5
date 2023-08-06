3a7998b8a1d462806cfd06163f89713d638e6cd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:23 +0000

    Add package pyprintflow2/0.3.22/pyprintflow2-0.3.22-py2.7.egg

---
+++ packages/pyprintflow2/pyprintflow2-0.3.22-py2.7.egg/printflow2/Workflow.py
@@ -0,0 +1,17 @@
+'''
+Created on Sep 16, 2013
+
+@author: "Colin Manning"
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
