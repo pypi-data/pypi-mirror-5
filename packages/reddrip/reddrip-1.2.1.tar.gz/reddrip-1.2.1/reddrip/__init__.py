6307f190dbd475268cc8b34a82520d4190001fa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:19 +0000

    Add package reddrip/1.2.1/reddrip-1.2.1.tar.gz

---
+++ packages/reddrip/reddrip-1.2.1.tar.gz/reddrip-1.2.1/reddrip/__init__.py
@@ -0,0 +1,7 @@
+import sys
+
+__VERSION__ = "1.2.1"
+
+if __name__ == "__main__":
+    print "Hi."
+    sys.exit(0)
---
+++ packages/reddrip/reddrip-1.2.1.tar.gz/reddrip-1.2.1/setup.py
@@ -0,0 +1,62 @@
+""" Setup file.
+
+    reddrip - simple reddid image ripper
+    Copyright (C) 2013 Nikola Kotur <kotnick+python@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import os
+from setuptools import setup, find_packages
+
+__VERSION__ = "1.2.1"
+
+root_dir = os.path.abspath(os.path.dirname(__file__))
+readme_file = os.path.join(root_dir, "README.md")
+README = 'Reddrip is Reddit subreddit picture ripper.'
+if os.path.exists(readme_file):
+    with open(readme_file) as f:
+        README = f.read()
+
+setup(name="reddrip",
+    version=__VERSION__,
+    description="Reddrip is Reddit subreddit picture ripper.",
+    long_description=README,
+    keywords="reddit crawler",
+    author="Nikola Kotur",
+    author_email="kotnick+python@gmail.com",
+    url="https://github.com/kotnik/reddrip",
+    download_url="https://github.com/kotnik/reddrip/archive/v1.1.tar.gz",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    scripts=[
+        "bin/reddrip",
+    ],
+    install_requires=[
+        "redis",
+        "requests",
+        "praw",
+        "colorama",
+        "texttable",
+    ],
+    platforms="any",
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Environment :: Console",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
+)
