8ccdf4476f4752bc5ed4672f122a90e32dd4160a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:26 +0000

    Add package simpleflake/0.1.4/simpleflake-0.1.4.tar.gz

---
+++ packages/simpleflake/simpleflake-0.1.4.tar.gz/simpleflake-0.1.4/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+setup(
+    name='simpleflake',
+    author='Mali Akmanalp (Custommade Ventures)',
+    author_email='mali+simpleflake@custommade.com',
+    description='Twitter snowflake compatible super-simple distributed ID generator.',
+    url='https://github.com/SawdustSoftware/simpleflake',
+    version='0.1.4',
+    packages=['simpleflake', ],
+    package_dir={'simpleflake':'simpleflake'},
+    license='MIT',
+    long_description=long_description,
+    classifiers=[
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 4 - Beta'
+    ],
+)
