240c41d710a9872db07ec660e02fdc54e82ad4c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:57 +0000

    Add package relaxxapi/0.1/relaxxapi-0.1.tar.gz

---
+++ packages/relaxxapi/relaxxapi-0.1.tar.gz/relaxxapi-0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+    name='relaxxapi',
+    packages=['relaxxapi'],
+    version='0.1',
+    description='A Wrapper around the RelaxxPlayer MPD Web Interface',
+    author='Felix Richter',
+    license='WTFPL',
+    author_email='pypi@syntax-fehler.de',
+    url='https://github.com/makefu/relaxxapi',
+    download_url='https://github.com/makefu/relaxxapi/tarball/0.1',
+    keywords=['relaxxplayer', 'api'],
+    long_description=open('README.md').read(),
+)
