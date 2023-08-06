8d764de81921e704c8719a0a402a8ddcc2fb4e98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:36 +0000

    Add package django-snikt/1.0/django-snikt-1.0.tar.gz

---
+++ packages/django-snikt/django-snikt-1.0.tar.gz/django-snikt-1.0/mach_info/admin.py
@@ -0,0 +1,11 @@
+from mach_info.models import *
+from django.contrib import admin
+
+admin.site.register(Machine)
+admin.site.register(NetInfo)
+admin.site.register(HwModel)
+admin.site.register(JobTracker)
+admin.site.register(Os)
+admin.site.register(Switch)
+admin.site.register(Profile)
+admin.site.register(Step)
