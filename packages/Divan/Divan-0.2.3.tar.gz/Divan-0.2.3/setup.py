4c594f471e92cdad4232c5ef0afa25d411c4bce8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:38 +0000

    Add package Divan/0.2.3/Divan-0.2.3.tar.gz

---
+++ packages/Divan/Divan-0.2.3.tar.gz/Divan-0.2.3/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='Divan',
+      version='0.2.3',
+      description='Asynchronous Cloudant / CouchDB Interface',
+      author='Max Thayer',
+      author_email='garbados@gmail.com',
+      url='https://github.com/garbados/divan',
+      packages=['divan'],
+      install_requires=[
+          'requests-futures==0.9.4',
+      ],
+      test_suite="test",
+      # install with `pip install -e divan[doc]`
+      extras_require={
+      'docs': [
+          'jinja2>=2.7',
+          'markdown>=2.3.1'
+      ]
+      }
+      )
