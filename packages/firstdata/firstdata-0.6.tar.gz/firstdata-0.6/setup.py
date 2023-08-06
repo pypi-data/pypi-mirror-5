f8232f52cd3d116367af7f19858f5f6a95c5163f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:07 +0000

    Add package firstdata/0.6/firstdata-0.6.tar.gz

---
+++ packages/firstdata/firstdata-0.6.tar.gz/firstdata-0.6/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+from firstdata import version
+
+setup(name='firstdata',
+      version=version,
+      description="Unofficial First Data G4 Handlers",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='firstdata gateway credit creditcard g4 processing',
+      author='@iopeak',
+      author_email='steve@stevepeak.net',
+      url='https://github.com/stevepeak/firstdata',
+      license='http://www.apache.org/licenses/LICENSE-2.0',
+      packages = ['firstdata'],
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
