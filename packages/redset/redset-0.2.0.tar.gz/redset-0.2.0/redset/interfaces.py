3f86f162d4bd035879675dd3013eed9f96effcf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:42 +0000

    Add package redset/0.2.0/redset-0.2.0.tar.gz

---
+++ packages/redset/redset-0.2.0.tar.gz/redset-0.2.0/redset/interfaces.py
@@ -0,0 +1,35 @@
+
+import abc
+
+
+class Serializer(object):
+    """
+    This is a guideline for implementing a serializer for redset. Serializers
+    need not subclass this directly, but should match the interface defined
+    here.
+
+    """
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def load(self, str_from_redis):
+        """
+        Deserialize a str item from redis into a Python object.
+
+        :param str_from_redis: the str corresponding with an item in redis
+        :type str_from_redis: str
+        :returns: object
+
+        """
+
+    @abc.abstractmethod
+    def dump(self, obj):
+        """
+        Serialize a Python object into a `str`
+
+        :param obj: the Python object to be stored in a sorted set
+        :returns: str
+
+        """
+
+
---
+++ packages/redset/redset-0.2.0.tar.gz/redset-0.2.0/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='redset',
+    version='0.2.0',
+    author='thekantian, jamesob',
+    author_email='zach@percolate.com, jamesob@percolate.com',
+    packages=['redset'],
+    url='https://github.com/percolate/redset',
+    license='see LICENSE',
+    description='Simple, distributed sorted sets with redis',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
