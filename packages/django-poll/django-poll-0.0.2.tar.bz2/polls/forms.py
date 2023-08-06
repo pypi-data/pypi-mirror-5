bf0223c68162b392718b42a7866fb7b352d0324b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:54 +0000

    Add package django-poll/0.0.2/django-poll-0.0.2.tar.bz2

---
+++ packages/django-poll/django-poll-0.0.2.tar.bz2/polls/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+from polls.models import Poll, Choice
+
+class PollForm(forms.ModelForm):
+    class Meta:
+        model = Poll
+        exclude = ('user',)
+        
+class PollVoteForm(forms.Form):
+    choice = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple)
