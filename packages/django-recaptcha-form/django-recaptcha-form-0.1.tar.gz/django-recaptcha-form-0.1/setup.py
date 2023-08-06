928175e4082757e228235244835c447aba2d6bc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:36 +0000

    Add package django-recaptcha-form/0.1/django-recaptcha-form-0.1.tar.gz

---
+++ packages/django-recaptcha-form/django-recaptcha-form-0.1.tar.gz/django-recaptcha-form-0.1/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import os.path
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='django-recaptcha-form',
+      version='0.1',
+      description='Recaptcha for Django Forms',
+      author='Nathaniel Tucker',
+      author_email='me@ntucker.me',
+      url='http://github.com/ntucker/django-recaptcha',
+      packages=['recaptcha_form', 'recaptcha_form.registration_backend'],
+      install_requires=['django>=1.4', 'recaptcha-client>=1.0.6', ],
+      long_description=read('README'),
+      license='BSD',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Web Environment',
+          'Framework :: Django',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+      ],
+     )
