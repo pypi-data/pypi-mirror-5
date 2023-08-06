239625daee9b30b3249452616d06a99d4aa92e5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:23 +0000

    Add package django-xadmin/0.4.0/django-xadmin-0.4.0.tar.gz

---
+++ packages/django-xadmin/django-xadmin-0.4.0.tar.gz/django-xadmin-0.4.0/xadmin/adminx.py
@@ -0,0 +1,10 @@
+import xadmin
+from xadmin import views
+from models import UserSettings
+from xadmin.layout import *
+
+
+class UserSettingsAdmin(object):
+    model_icon = 'cog'
+    hidden_menu = True
+xadmin.site.register(UserSettings, UserSettingsAdmin)
