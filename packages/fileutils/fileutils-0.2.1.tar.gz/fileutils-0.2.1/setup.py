5c028d97c801f0f18348bc76df92e2c243b1f19c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:25 +0000

    Add package fileutils/0.2.1/fileutils-0.2.1.tar.gz

---
+++ packages/fileutils/fileutils-0.2.1.tar.gz/fileutils-0.2.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="fileutils",
+    version="0.2.1",
+    description="An object-oriented file access library",
+    author="Alexander Boyd",
+    author_email="alex@opengroove.org",
+    py_modules=["fileutils"]
+)
