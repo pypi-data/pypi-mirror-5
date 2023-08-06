df5e5e30d92aedddb4f626a8eb8bc5fab6379be6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:28 +0000

    Add package django-queryset-csv/0.1.1/django-queryset-csv-0.1.1.tar.gz

---
+++ packages/django-queryset-csv/django-queryset-csv-0.1.1.tar.gz/django-queryset-csv-0.1.1/djqscsv/__init__.py
@@ -0,0 +1,2 @@
+from djqscsv import (render_to_csv_response, create_csv,  # NOQA
+                             generate_filename, CSVException)  # NOQA
---
+++ packages/django-queryset-csv/django-queryset-csv-0.1.1.tar.gz/django-queryset-csv-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='django-queryset-csv',
+    version='0.1.1',
+    description='A simple python module for writing querysets to csv',
+    author='Steve Lamb',
+    author_email='slamb@azavea.com',
+    url='http://github.com/azavea/django-queryset-csv',
+    packages=find_packages(exclude=('tests',)),
+    keywords="django queryset csv",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Environment :: Plugins",
+        "Framework :: Django",
+        "License :: OSI Approved :: GNU General Public License (GPL)"
+    ],
+    install_requires=['django>=1.5'],
+)
