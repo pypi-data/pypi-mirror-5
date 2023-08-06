ea41cef6cf6a089c1188092a91665eed504cbb36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:22 +0000

    Add package Flask-AppBuilder/0.1.9/Flask-AppBuilder-0.1.9.tar.gz

---
+++ packages/Flask-AppBuilder/Flask-AppBuilder-0.1.9.tar.gz/Flask-AppBuilder-0.1.9/setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name='Flask-AppBuilder',
+    version='0.1.9',
+    url='https://github.com/dpgaspar/flask-appbuilder/',
+    license='BSD',
+    author='Daniel Vaz Gaspar',
+    author_email='danielvazgaspar@gmail.com',
+    description='Simple and rapid Application builder, includes detailed security, auto form generation, google charts and much more.',
+    long_description=open("README.md").read(),
+    packages=find_packages(),
+    package_data={'': ['LICENSE']},
+    scripts=['bin/init_app.py'],
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'Flask>=0.10',
+        'Flask-Babel==0.8',
+        'Flask-Login>=0.1.2',
+        'Flask-OpenID>=1.1.0',
+        'Flask-SQLAlchemy==0.16',
+        'Flask-WTF==0.8.3',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    test_suite=''
+)
