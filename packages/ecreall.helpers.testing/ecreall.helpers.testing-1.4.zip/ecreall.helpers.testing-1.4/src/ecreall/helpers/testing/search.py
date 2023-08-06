93e241517b13f2afd47d8e1a4c9510de742be5c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:21 +0000

    Add package ecreall.helpers.testing/1.4/ecreall.helpers.testing-1.4.zip

---
+++ packages/ecreall.helpers.testing/ecreall.helpers.testing-1.4.zip/ecreall.helpers.testing-1.4/src/ecreall/helpers/testing/search.py
@@ -0,0 +1,37 @@
+from .base import BaseTest
+
+
+class BaseSearchTest(BaseTest):
+    """A class with many useful helpers to test search
+    """
+
+    def getBrain(self, uid):
+        ctool = self.portal.portal_catalog
+        return ctool.unrestrictedSearchResults(UID=uid)[0]
+
+    def assertCanFind(self, document):
+        """Check if user can find document through a catalog search
+        """
+        ctool = self.portal.portal_catalog
+        self.assertEqual(len(ctool.searchResults(UID=document.UID())), 1)
+
+    def assertCanNotFind(self, document):
+        """Check if user can find document through a catalog search
+        """
+        ctool = self.portal.portal_catalog
+        self.assertEqual(len(ctool.searchResults(UID=document.UID())), 0)
+
+    def assertSearchFinds(self, query, results_num):
+        """Check number of results of a query
+        """
+        ctool = self.portal.portal_catalog
+        self.assertEqual(len(ctool.unrestrictedSearchResults(**query)),
+                             results_num)
+
+    def assertSearchFindsInOrder(self, query, ids):
+        """Check order of the results
+        ids are the ids of documents
+        """
+        ctool = self.portal.portal_catalog
+        result_ids = [b.getId for b in ctool.unrestrictedSearchResults(**query)]
+        self.assertListEqual(result_ids, ids)
