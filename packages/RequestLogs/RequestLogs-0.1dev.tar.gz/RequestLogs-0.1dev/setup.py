08309843ea0bca2f33ae7b836a500c62c0f01e14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:35 +0000

    Add package RequestLogs/0.1dev/RequestLogs-0.1dev.tar.gz

---
+++ packages/RequestLogs/RequestLogs-0.1dev.tar.gz/RequestLogs-0.1dev/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='RequestLogs',
+    version='0.1dev',
+    author='dayski',
+    author_email='kapil@delhivery.com',
+    packages=['requestlogs',],
+    license='The MIT License (MIT)',
+    long_description=open('README').read(),
+)
