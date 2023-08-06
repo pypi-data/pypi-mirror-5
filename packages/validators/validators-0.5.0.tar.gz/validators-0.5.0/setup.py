314c3efb6d23c179141f28120557b8736c0d0235
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:31 +0000

    Add package validators/0.5.0/validators-0.5.0.tar.gz

---
+++ packages/validators/validators-0.5.0.tar.gz/validators-0.5.0/setup.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+validators
+----------
+
+Python Data Validation for Humans™.
+"""
+
+from setuptools import setup, Command, find_packages
+import subprocess
+import sys
+
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        errno = subprocess.call(['py.test'])
+        raise SystemExit(errno)
+
+
+PY3 = sys.version_info[0] == 3
+
+
+extras_require = {
+    'test': [
+        'pytest>=2.2.3',
+    ],
+}
+
+
+setup(
+    name='validators',
+    version='0.5.0',
+    url='https://github.com/kvesteri/validators',
+    license='BSD',
+    author='Konsta Vesterinen',
+    author_email='konsta@fastmonkeys.com',
+    description='Python Data Validation for Humans™.',
+    long_description=__doc__,
+    packages=find_packages('.'),
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'six>=1.4.0',
+        'decorator>=3.4.0',
+        'ordereddict>=1.1'
+        if sys.version_info[0] == 2 and sys.version_info[1] < 7 else '',
+        'total_ordering>=0.1'
+        if sys.version_info[0] == 2 and sys.version_info[1] < 7 else ''
+    ],
+    extras_require=extras_require,
+    cmdclass={'test': PyTest},
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
