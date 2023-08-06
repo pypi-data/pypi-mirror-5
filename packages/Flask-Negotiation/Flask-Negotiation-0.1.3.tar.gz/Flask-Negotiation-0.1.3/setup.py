cb7c181fd40a8ad32f0f6d8c85e969885426e51a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:20 +0000

    Add package Flask-Negotiation/0.1.3/Flask-Negotiation-0.1.3.tar.gz

---
+++ packages/Flask-Negotiation/Flask-Negotiation-0.1.3.tar.gz/Flask-Negotiation-0.1.3/setup.py
@@ -0,0 +1,30 @@
+"""
+Flask-Negotiation
+==================
+
+Provides better content negotiation for flask.
+"""
+import setuptools
+from setuptools import setup
+
+requires = [
+    'Flask',
+]
+
+setup(name='Flask-Negotiation',
+      version='0.1.3',
+      url='http://blog.hardtack.me/',
+      author='GunWoo Choi',
+      author_email='6566gun@gmail.com',
+      description='Better content negotiation for flask',
+      long_description=__doc__,
+      packages=setuptools.find_packages(exclude=('tests', )),
+      include_package_data=True,
+      zip_safe=False,
+      platforms='any',
+      install_requires=requires,
+      classifiers=[
+          'Development Status :: 2 - Pre-Alpha',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+      ])
