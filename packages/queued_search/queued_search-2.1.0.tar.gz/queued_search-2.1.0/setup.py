96af4de5a751a1af3666554140d32bf1f67ce33a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:01 +0000

    Add package queued_search/2.1.0/queued_search-2.1.0.tar.gz

---
+++ packages/queued_search/queued_search-2.1.0.tar.gz/queued_search-2.1.0/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='queued_search',
+    version='2.1.0',
+    description='A queuing setup for integration with Haystack.',
+    author='Daniel Lindsley',
+    author_email='daniel@toastdriven.com',
+    url='http://github.com/toastdriven/queued_search',
+    packages=[
+        'queued_search',
+        'queued_search.management',
+        'queued_search.management.commands',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2',
+        'Topic :: Utilities'
+    ],
+)
