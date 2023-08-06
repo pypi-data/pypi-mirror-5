bfb2bf06ef0a8944943eb7c32eecca7b19eb384a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:15 +0000

    Add package djangocms-bootstrap/0.1.1/djangocms-bootstrap-0.1.1.tar.gz

---
+++ packages/djangocms-bootstrap/djangocms-bootstrap-0.1.1.tar.gz/djangocms-bootstrap-0.1.1/README.md
@@ -0,0 +1,28 @@
+djangocms-bootstrap
+===================
+
+A collection of plugins for DjangoCMS to add various HTML elements from the CSS framework
+“Twitter Bootstrap” to CMS placeholders. This allows the editor of a web page to manipulate the DOM,
+without having to edit HTML templates.
+
+Build status
+------------
+[![Build Status](https://travis-ci.org/jrief/djangocms-bootstrap.png?branch=master)](https://travis-ci.org/jrief/djangocms-bootstrap)
+
+Features
+--------
+* Use the **Bootstrap** scaffolding technique.
+* Use **Bootstrap** buttons styles for your links.
+* Add thumbnails and images using highly customizable **Bootstraps** markup.
+* Its very easy to integrate your own plugin, often with less than 10 lines of code.
+* Can also be used for any other CSS framework.
+
+Find detailed documentation on [ReadTheDocs](http://djangocms-bootstrap.readthedocs.org/en/latest/).
+
+Please read http://getbootstrap.com/2.3.2/ for details about the *Bootstrap* CSS framework.
+
+License
+-------
+Released under the terms of MIT License.
+
+Copyright (C) 2013, Jacob Rief <jacob.rief@gmail.com>
---
+++ packages/djangocms-bootstrap/djangocms-bootstrap-0.1.1.tar.gz/djangocms-bootstrap-0.1.1/cmsplugin_bootstrap/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.utils.text import Truncator
+from jsonfield.fields import JSONField
+from cms.models import CMSPlugin
+
+
+class BootstrapElement(CMSPlugin):
+    """
+    The container to hold additional bootstrap elements.
+    """
+    cmsplugin_ptr = models.OneToOneField(CMSPlugin, related_name='+', parent_link=True)
+    tag_type = models.CharField(verbose_name=_('tag Type'), max_length=50)
+    class_name = models.CharField(_("class name"), max_length=50, blank=True, null=True)
+    extra_classes = JSONField(null=True, blank=True, help_text='Add extra CSS classes to this HTML element')
+    tagged_classes = JSONField(null=True, blank=True, help_text='Tag special CSS classes to this HTML element')
+    extra_styles = JSONField(null=True, blank=True, help_text='Add extra styles to this HTML element')
+
+    def __unicode__(self):
+        value = self.css_classes
+        if value:
+            return unicode(Truncator(value).words(3, truncate=' ...'))
+        return u''
+
+    @property
+    def css_classes(self):
+        css_classes = self.class_name and [self.class_name] or []
+        if isinstance(self.extra_classes, dict):
+            css_classes += [ec for ec in self.extra_classes.values() if ec]
+        if isinstance(self.tagged_classes, list):
+            css_classes += [tc for tc in self.tagged_classes if tc]
+        return u' '.join(css_classes)
+
+    @property
+    def inline_styles(self):
+        if isinstance(self.extra_styles, dict):
+            try:
+                return u' '.join(['{0}: {1};'.format(*s) for s in self.extra_styles.items() if s[1] is not None])
+            except IndexError:
+                pass
+        return ''
