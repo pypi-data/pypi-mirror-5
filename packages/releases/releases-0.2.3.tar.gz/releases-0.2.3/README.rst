8fc2408dcbca8f76282454fae13d1944372a6f26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:25 +0000

    Add package releases/0.2.3/releases-0.2.3.tar.gz

---
+++ packages/releases/releases-0.2.3.tar.gz/releases-0.2.3/README.rst
@@ -0,0 +1,106 @@
+=================================
+Releases, a Sphinx changelog tool
+=================================
+
+About
+=====
+
+Releases is a `Sphinx <http://sphinx-doc.org>`_ extension designed to help you
+keep a source control friendly, merge friendly changelog file & turn it into a
+useful, human readable HTML output.
+
+Specifically:
+
+* The source format is a stream-like timeline that plays well with source
+  control & only requires one entry per change (even for changes that exist in
+  multiple release lines).
+* The output is a traditional looking changelog with a section for every
+  release; multi-release issues are copied automatically into each release.
+* By default, feature and support issues are only displayed under feature
+  releases, and bugs are only displayed under bugfix releases. This can be
+  overridden on a per-issue basis.
+
+Usage
+=====
+
+Mimic the format seen `in Fabric's changelog
+<https://raw.github.com/fabric/fabric/master/docs/changelog.rst>`_:
+
+* Install ``releases`` and update your Sphinx ``conf.py`` to include it in the
+  ``extensions`` list setting: ``extensions = ['releases']``.
+
+    * Also set the ``releases_release_uri`` and ``releases_issue_uri`` top
+      level options. Both should have an unevaluated ``%s`` where the
+      release/issue number would go.
+    * See `Fabric's docs/conf.py
+      <https://github.com/fabric/fabric/blob/4afd33e971f1c6831cc33fd3228013f7484fbe35/docs/conf.py#L31>`_
+      for an example.
+
+* Create a docs file named ``changelog.rst`` with a top-level header followed
+  by a bulleted list.
+* Bullet list items must use the ``:support:``, ``:feature`` or ``:bug`` roles to
+  mark issues, or ``:release:`` to mark a release. These special roles must be
+  the first element in each list item.
+* Issue roles are of the form ``:type:`number[ keyword]```. Keywords are
+  optional and may be one of:
+
+    * ``backported``: Given on support or feature issues to denote
+      backporting to bugfix releases; will show up in both release types.
+    * ``major``: Given on bug issues to denote inclusion in feature, instead
+      of bugfix, releases.
+
+* Regular Sphinx content may be given after issue roles and will be preserved
+  as-is when rendering.
+* Release roles are of the form ``:release:`number <date>```. Do not place any
+  additional elements after release roles.
+
+Then build your docs; ``changelog.html`` should show issues grouped by release,
+as per the above rules. Example: `Fabric's rendered changelog
+<http://docs.fabfile.org/en/latest/changelog.html>`_.
+
+Changes
+=======
+
+In a fit of irony, Releases is too simple for a full Sphinx doc treatment (or
+for multiple release lines) and thus doesn't use itself. Here's a by-hand
+changelog:
+
+* 2013.09.16: **0.2.3**:
+
+    * Fix a handful of bugs in release assignment logic.
+
+* 2013.09.15: **0.2.2**:
+
+    * Python 3 compatibility.
+
+* 2013.09.15: **0.2.1**:
+
+    * Fixed a stupid bug causing invalid issue hyperlinks.
+    * Added this README.
+
+* 2013.09.15: **0.2.0**:
+
+  * Basic functionality.
+
+
+TODO
+====
+
+* Tests would be nice.
+* Migrate to a directive-driven (vs role-driven) format? Existing format
+  evolved from a purely role-oriented, prose-embedded setup; roles are no
+  longer really the right way to do this.
+
+    * Pro: possibly cleaner code + source formatting
+    * Con: possibly more verbose source formatting if I can't compact things
+      enough. Going from 1 line minimum to a, say, 3 or 4 line minimum per
+      entry is not (IMHO) acceptable.
+
+* Possibly add more keywords to allow control over additional edge cases.
+* Add shortcut format option for the release/issue URI settings - Github users
+  can just give their Github acct/repo and we will fill in the rest.
+* Maybe say pre-1.0 releases consider all bugs 'major' (so one can e.g. put out
+  an 0.4.0 which is all bugfixes). Iffy because what if you *wanted* regular
+  feature-vs-bugfix releases pre-1.0? (which is common.)
+* Allow skipping the actual issue number link somehow, sometimes you just don't
+  have an issue and it's stupid to make one.
---
+++ packages/releases/releases-0.2.3.tar.gz/releases-0.2.3/releases/__init__.py
@@ -0,0 +1,290 @@
+import re
+
+from docutils.parsers.rst import roles
+from docutils import nodes, utils
+
+
+# Issue type list (keys) + color values
+issue_types = {
+    'bug': 'A04040',
+    'feature': '40A056',
+    'support': '4070A0',
+}
+
+def issues_role(name, rawtext, text, lineno, inliner, options={}, content=[]):
+    """
+    Use: :issue|bug|feature|support:`ticket_number`
+
+    When invoked as :issue:, turns into just a "#NN" hyperlink to Github.
+
+    When invoked otherwise, turns into "[Type] <#NN hyperlink>: ".
+
+    May give a 'ticket number' of '<number> backported' to indicate a
+    backported feature or support ticket. This extra info will be stripped out
+    prior to parsing. May also give 'major' in the same vein, implying the bug
+    was a major bug released in a feature release.
+    """
+    #from ipdb import set_trace; set_trace()
+    # Old-style 'just the issue link' behavior
+    issue_no, _, ported = utils.unescape(text).partition(' ')
+    # Lol @ access back to Sphinx
+    config = inliner.document.settings.env.app.config
+    ref = config.releases_issue_uri % issue_no
+    link = nodes.reference(rawtext, '#' + issue_no, refuri=ref, **options)
+    # Additional 'new-style changelog' stuff
+    if name in issue_types:
+        which = '[<span style="color: #%s;">%s</span>]' % (
+            issue_types[name], name.capitalize()
+        )
+        nodelist = [
+            nodes.raw(text=which, format='html'),
+            nodes.inline(text=" "),
+            link,
+            nodes.inline(text=":")
+        ]
+        # Sanity check
+        if ported not in ('backported', 'major', ''):
+            raise ValueError("Gave unknown issue metadata '%s' for issue no. %s" % (ported, issue_no))
+        # Create temporary node w/ data & final nodes to publish
+        node = issue(
+            number=issue_no,
+            type_=name,
+            nodelist=nodelist,
+            backported=(ported == 'backported'),
+            major=(ported == 'major'),
+        )
+        return [node], []
+    # Return old style info for 'issue' for older changelog entries
+    else:
+        return [link], []
+
+
+def release_nodes(text, slug, date, config):
+    my_nodes = [
+        nodes.reference(
+            text=text,
+            refuri=config.releases_release_uri % slug,
+        ),
+    ]
+    if date:
+        my_nodes.extend([
+            nodes.inline(text=' '),
+            nodes.raw(text='<span style="font-size: 75%%;">%s</span>' % date, format='html'),
+        ])
+    return nodes.section('',
+        nodes.title('', '', *my_nodes),
+        ids=[text]
+    )
+
+
+year_arg_re = re.compile(r'^(.+?)\s*(?<!\x00)<(.*?)>$', re.DOTALL)
+
+def release_role(name, rawtext, text, lineno, inliner, options={}, content=[]):
+    """
+    Invoked as :release:`N.N.N <YYYY-MM-DD>`.
+
+    Turns into useful release header + link to GH tree for the tag.
+    """
+    # Make sure year has been specified
+    match = year_arg_re.match(text)
+    if not match:
+        msg = inliner.reporter.error("Must specify release date!")
+        return [inliner.problematic(rawtext, rawtext, msg)], [msg]
+    number, date = match.group(1), match.group(2)
+    # Lol @ access back to Sphinx
+    config = inliner.document.settings.env.app.config
+    nodelist = [release_nodes(number, number, date, config)]
+    # Return intermediate node
+    node = release(number=number, date=date, nodelist=nodelist)
+    return [node], []
+
+
+class issue(nodes.Element):
+    @property
+    def type(self):
+        return self['type_']
+
+    @property
+    def backported(self):
+        return self['backported']
+
+    @property
+    def major(self):
+        return self['major']
+
+    @property
+    def number(self):
+        return self.get('number', None)
+
+class release(nodes.Element):
+    @property
+    def number(self):
+        return self['number']
+
+
+def get_line(obj):
+    # 1.2.7 -> 1.2
+    return '.'.join(obj.number.split('.')[:-1])
+
+def construct_releases(entries, app):
+    # Walk from back to front, consuming entries & copying them into
+    # per-release buckets as releases are encountered. Store releases in order.
+    releases = []
+    lines = {'unreleased': []}
+    for obj in reversed(entries):
+        # The 'actual' intermediate object we want to focus on is wrapped first
+        # in a LI, then a P.
+        focus, rest = obj[0][0], obj[0][1:]
+        # Releases 'eat' the entries in their line's list and get added to the
+        # final data structure. They also inform new release-line 'buffers'.
+        # Release lines should have an empty 'rest' so it's ignored.
+        if isinstance(focus, release):
+            line = get_line(focus)
+            print "%s (%s)" % (focus.number, line)
+            # New release line/branch detected. Create it & dump unreleased into
+            # this new release. Skip non-major bugs.
+            if line not in lines:
+                print "> Is new line"
+                lines[line] = []
+                releases.append({
+                    'obj': focus,
+                    'entries': [
+                        x
+                        for x in lines['unreleased'] 
+                        if x.type in ('feature', 'support') or x.major
+                    ]
+                })
+                lines['unreleased'] = []
+                print "> After dump, lines: %r" % (lines,)
+                print "> And releases: %r" % (releases,)
+            # Existing line -> empty out its bucket into new release.
+            # Skip 'major' bugs as those "belong" to the next release.
+            else:
+                print "> Existing line"
+                releases.append({
+                    'obj': focus,
+                    'entries': [x for x in lines[line] if not x.major],
+                })
+                lines[line] = []
+                print "> After dump, lines: %r" % (lines,)
+                print "> And releases: %r" % (releases,)
+        # Entries get copied into release line buckets as follows:
+        # * Everything goes into 'unreleased' so it can be used in new lines.
+        # * Bugfixes (but not support or feature entries) go into all release
+        # lines, not just 'unreleased'.
+        # * However, support/feature entries marked as 'backported' go into all
+        # release lines as well, on the assumption that they were released to
+        # all active branches.
+        # * The 'rest' variable (which here is the bug description, vitally
+        # important!) is preserved by stuffing it into the focus (issue)
+        # object.
+        else:
+            # Handle rare-but-valid non-issue-attached line items, which are
+            # always bugs. (They are their own description.)
+            if not isinstance(focus, issue):
+                focus = issue(type_='bug', nodelist=[focus], backported=False, major=False, description=[focus])
+            else:
+                focus.attributes['description'] = rest
+            print "%s %s (backported=%s, major=%s)" % (focus.type, focus.number, focus.backported, focus.major)
+            if focus.type == 'bug':
+                # Regular bugs go into per-line buckets only.
+                if not focus.major:
+                    for line in [x for x in lines if x != 'unreleased']:
+                        print "> Appending to %r" % line
+                        lines[line].append(focus)
+                # Major bugs go into feature release bucket only.
+                else:
+                    print "> Appending to 'unreleased'"
+                    lines['unreleased'].append(focus)
+            else:
+                # Backported feature/support items go into all lines.
+                if focus.backported:
+                    for line in lines:
+                        print "> Appending to %r" % line
+                        lines[line].append(focus)
+                # Non-backported feature/support items go into feature releases
+                # only.
+                else:
+                    print "> Appending to 'unreleased'"
+                    lines['unreleased'].append(focus)
+
+    # Entries not yet released get special 'release' entries (that lack an
+    # actual release object).
+    nodelist = [release_nodes("Unreleased", "master", None, app.config)]
+    releases.append({
+        'obj': release(number='unreleased', date=None, nodelist=nodelist),
+        'entries': lines['unreleased']
+    })
+    return releases
+
+
+def construct_nodes(releases):
+    result = []
+    # Reverse the list again so the final display is newest on top
+    for d in reversed(releases):
+        if not d['entries']:
+            continue
+        obj = d['obj']
+        entries = []
+        for entry in d['entries']:
+            # Use nodes.Node.deepcopy to deepcopy the description
+            # nodes.  If this is not done, multiple references to the same
+            # object (e.g. a reference object in the description of #649, which
+            # is then copied into 2 different release lists) will end up in the
+            # doctree, which makes subsequent parse steps very angry (index()
+            # errors).
+            desc = list(map(lambda x: x.deepcopy(), entry['description']))
+            # Additionally, expand any other issue roles found in the
+            # description paragraph - sometimes we refer to related issues
+            # inline. (They can't be left as issue() objects at render time
+            # since that's undefined.)
+            for i, node in enumerate(desc[:]): # Copy to avoid self-mutation during loop
+                if isinstance(node, issue):
+                    desc[i:i+1] = node['nodelist']
+            # Tack on to end of this entry's own nodelist (which is the link +
+            # etc)
+            entries.append(
+                nodes.list_item('',
+                    nodes.paragraph('', '', *entry['nodelist'] + desc)
+                )
+            )
+        # Entry list
+        list_ = nodes.bullet_list('', *entries)
+        # Insert list into release nodelist (as it's a section)
+        obj['nodelist'][0].extend(list_)
+        # Release header
+        header = nodes.paragraph('', '', *obj['nodelist'])
+        result.extend(header)
+    return result
+
+
+def generate_changelog(app, doctree):
+    # This seems to be the cleanest way to tell what a not-fully-parsed
+    # document's 'name' is. Also lol @ not fully implementing dict protocol.
+    source = doctree.children[0]
+    if 'changelog' not in source.get('names', []):
+        return
+    # Second item inside main document is the 'modern' changelog bullet-list
+    # object, whose children are the nodes we care about.
+    changelog = source.children.pop(1)
+    # Walk + parse into release mapping
+    releases = construct_releases(changelog.children, app)
+    # Construct new set of nodes to replace the old, and we're done
+    source.children[1:1] = construct_nodes(releases)
+
+
+def setup(app):
+    # Issue base URI setting: releases_issue_uri
+    # E.g. 'https://github.com/fabric/fabric/issues/'
+    app.add_config_value(name='releases_issue_uri', default=None,
+        rebuild='html')
+    # Release-tag base URI setting: releases_release_uri
+    # E.g. 'https://github.com/fabric/fabric/tree/'
+    app.add_config_value(name='releases_release_uri', default=None,
+        rebuild='html')
+    # Register intermediate roles
+    for x in list(issue_types) + ['issue']:
+        app.add_role(x, issues_role)
+    app.add_role('release', release_role)
+    # Hook in our changelog transmutation at appropriate step
+    app.connect('doctree-read', generate_changelog)
