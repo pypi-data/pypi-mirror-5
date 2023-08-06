fb94eef3f2ff3793fb5d3acb302f677e780eb1ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:19 +0000

    Add package restapidoc/0.1.1/restapidoc-0.1.1.tar.gz

---
+++ packages/restapidoc/restapidoc-0.1.1.tar.gz/restapidoc-0.1.1/restapidoc/config.py
@@ -0,0 +1,11 @@
+modules = {}
+
+modules['calls'] = """
+Using the Twilio REST API, you can make outgoing calls to phones, SIP-enabled endpoints and Twilio Client connections.
+
+Note that calls initiated via the REST API are rate-limited to one per second. You can queue up as many calls as you like as fast as you like, but each call is popped off the queue at a rate of one per second.
+"""
+
+modules['tweets'] = """
+Tweets are the atomic building blocks of Twitter, 140-character status updates with additional associated metadata. People tweet for a variety of reasons about a multitude of topics.
+"""
