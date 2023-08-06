f18175f279fa650c3d4420b27434146f1638e389
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:04 +0000

    Add package django-bigfoot/0.2/django-bigfoot-0.2.tar.gz

---
+++ packages/django-bigfoot/django-bigfoot-0.2.tar.gz/django-bigfoot-0.2/setup.py
@@ -0,0 +1,19 @@
+import sys
+
+from setuptools import setup, find_packages
+
+install_requires = [
+    'django-tables2 == 0.13',
+]
+
+setup(
+    name = "django-bigfoot",
+    version = '0.2',
+    description = "A Django library for defining HTML building blocks (e.g.," +
+        " forms, form fields, tables) that can be easily rendered into HTML.",
+    url = "https://github.com/setaris/django-bigfoot",
+    author = "Setaris",
+    author_email = "support@setaris.com",
+    packages = find_packages(),
+    install_requires = install_requires,
+)
