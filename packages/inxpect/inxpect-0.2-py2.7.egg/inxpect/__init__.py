1caa45fcc5a04fd16682ac78d41c1eaa0a4f7b61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:19 +0000

    Add package inxpect/0.2/inxpect-0.2-py2.7.egg

---
+++ packages/inxpect/inxpect-0.2-py2.7.egg/inxpect/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf8 -*-
+__all__ = [
+    'expect_factory',
+    'getters', 'operator'
+    'And', 'Or',
+    'Expect', 'ExpectSame',
+    'List', 'ListItem',
+    'Dict', 'DictItem'
+]
+
+from .expect import *
+from .inspect import expect_factory
