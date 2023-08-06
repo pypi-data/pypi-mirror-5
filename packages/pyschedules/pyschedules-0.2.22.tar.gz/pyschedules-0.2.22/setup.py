4ab402ec2b3e0f0cb0f11cd3da8e6f95cce1c50f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:17 +0000

    Add package pyschedules/0.2.22/pyschedules-0.2.22.tar.gz

---
+++ packages/pyschedules/pyschedules-0.2.22.tar.gz/pyschedules-0.2.22/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+version = '0.2.22'
+
+setup(name='pyschedules',
+      version=version,
+      description="Interface to Schedules Direct.",
+      long_description="""\
+A complete library to pull channels, schedules, actors, lineups, and QAM-maps (channels.conf) data from Schedules Direct.""",
+      classifiers=['Development Status :: 5 - Production/Stable', 
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+                   'Programming Language :: Python',
+                   'Topic :: Home Automation',
+                   'Topic :: Multimedia :: Video :: Capture',
+                   'Topic :: Software Development :: Libraries :: Python Modules',
+                  ],
+      keywords='dvb television tv cable schedules direct channel channels qam',
+      author='Dustin Oprea',
+      author_email='myselfasunder@gmail.com',
+      url='https://github.com/dsoprea/PySchedules',
+      license='GPL2',
+      packages=['pyschedules/examples'] + find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          'parsedatetime',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      scripts=[
+          'scripts/qam'
+      ]
+      ),
+
