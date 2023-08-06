646be50381e0189c1ca082f4e59807cb03540c36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:11 +0000

    Add package keybase/0.0.0/keybase-0.0.0.tar.gz

---
+++ packages/keybase/keybase-0.0.0.tar.gz/keybase-0.0.0/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="keybase",
+      version="0.0.0",
+      description="CLI to keybase.io written in/for Python",
+      author="Maxwell Krohn",
+      author_email="themax@gmail.com",
+      url="https://github.com/keybase/python-client",
+      packages=['keybase'])
