f7c03492d589f095243739be20c664b87daa7d51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:23 +0000

    Add package PyBabel-hbs/0.1.3/PyBabel-hbs-0.1.3.tar.gz

---
+++ packages/PyBabel-hbs/PyBabel-hbs-0.1.3.tar.gz/PyBabel-hbs-0.1.3/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup, os
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='PyBabel-hbs',
+    version='0.1.3',
+    description='PyBabel handlebars gettext strings extractor',
+    author='Anton Bykov aka Tigra San',
+    author_email='tigrawap@gmail.com',
+    long_description=read('README.rst'),
+    packages=['pybabel_hbs'],
+    url="https://github.com/tigrawap/pybabel-hbs",
+    install_requires=[
+        'babel'
+    ],
+    include_package_data=True,
+    entry_points = """
+        [babel.extractors]
+        hbs = pybabel_hbs.extractor:extract_hbs
+        """,
+)
