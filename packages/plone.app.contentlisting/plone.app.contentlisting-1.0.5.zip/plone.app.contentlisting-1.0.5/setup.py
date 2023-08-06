627957d637b7d547b6dde751df94a53a1954aa85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:44 +0000

    Add package plone.app.contentlisting/1.0.5/plone.app.contentlisting-1.0.5.zip

---
+++ packages/plone.app.contentlisting/plone.app.contentlisting-1.0.5.zip/plone.app.contentlisting-1.0.5/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+version = '1.0.5'
+
+setup(name='plone.app.contentlisting',
+      version=version,
+      description="Listing of content for the Plone CMS",
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/plone.app.contentlisting',
+      license='GPL version 2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone', 'plone.app'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.uuid',
+      ],
+      extras_require={
+        'test': ['plone.app.testing'],
+      },
+      entry_points='''
+      [z3c.autoinclude.plugin]
+      target = plone
+      ''',
+      )
