0dc2aa167b9fd205f8e083fc55084d3fa7e44063
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:41 +0000

    Add package walkingcode/0.0.1/walkingcode-0.0.1.tar.gz

---
+++ packages/walkingcode/walkingcode-0.0.1.tar.gz/walkingcode-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name = 'walkingcode',
+    version = '0.0.1',
+    py_modules = ['walkingcode'],
+    author = 'wsy',
+    author_email = 'example@gmail.com',
+    url = 'http://www.walking-code.com',
+    description = 'An example',
+)
---
+++ packages/walkingcode/walkingcode-0.0.1.tar.gz/walkingcode-0.0.1/walkingcode.py
@@ -0,0 +1,8 @@
+class WalkingCode:
+    'A class for example'
+    def __init__(self):
+        pass
+
+    def walking(self):
+        print 'The code is walking'
+
