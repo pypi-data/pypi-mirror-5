1812b175267cdd6103e46ce96cf7b1a72d77e1a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:14 +0000

    Add package suite/0.1.2/suite-0.1.2.tar.gz

---
+++ packages/suite/suite-0.1.2.tar.gz/suite-0.1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='suite',
+      version='0.1.2',
+      description="A better dictionary.",
+      long_description="""\
+A set of things that belong together and methods to control those items.
+""",
+      classifiers=[],
+      keywords='',
+      author='@iopeak',
+      author_email='steve@stevepeak.net',
+      url='https://github.com/stevepeak/suite',
+      license='Apache v2',
+      packages=["suite"],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[],
+      entry_points="")
