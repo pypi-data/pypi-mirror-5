afd5425895ae058cc7b97e673a891f835d8c7681
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:33 +0000

    Add package django-ptree/0.1.1/django-ptree-0.1.1.zip

---
+++ packages/django-ptree/django-ptree-0.1.1.zip/django-ptree-0.1.1/setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-ptree',
+    version='0.1.1',
+    packages=['ptree'],
+    include_package_data=True,
+    license='MIT License',
+    description='pTree is a Django toolset that makes it easy to create and administer social science experiments to online participants.',
+    long_description=README,
+    url='http://www.ptree.org/',
+    author='Chris Wickens',
+    author_email='c.wickens+ptree@googlemail.com',
+    install_requires = ['boto', 'are-you-a-human', 'django-crispy-forms'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License', # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # replace these appropriately if you are using Python 3
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
