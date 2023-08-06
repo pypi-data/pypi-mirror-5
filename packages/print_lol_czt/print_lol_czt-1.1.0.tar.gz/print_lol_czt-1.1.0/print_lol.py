690681a3143e577b76b8e67141aee746315e9894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:26 +0000

    Add package print_lol_czt/1.1.0/print_lol_czt-1.1.0.tar.gz

---
+++ packages/print_lol_czt/print_lol_czt-1.1.0.tar.gz/print_lol_czt-1.1.0/print_lol.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""This is the "print_lol.py" module and it provides a function called print_lol()
+   which prints lists that may or may not include nested lists. """
+
+def print_lol(list_item, level = 0):
+    """This function takes one positionall argument called "list_item", which
+       is any Python list ( of - possibly - nested lists). Each data item in the
+       provided list is (recursively) printed to the screen on its own line"""
+    for each_item in list_item:
+        if isinstance(each_item, list):
+            print_lol(each_item, level + 1)
+        else:
+            for tab_stop in range(level):
+                print('\t', end='')
+            print(each_item)
+
+if __name__ == "__main__":
+    list_item = [1, 2, [3, 4], [5, [6, 7]]]
+    print_lol(list_item)
---
+++ packages/print_lol_czt/print_lol_czt-1.1.0.tar.gz/print_lol_czt-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = "print_lol_czt",
+        version = "1.1.0",
+        py_modules = ['print_lol'],
+        author = 'cztchoice',
+        author_email = 'cztchoice@gmail.com',
+        url = 'www.zhitao.me',
+        description = 'A simple printer of nested list',
+        )
