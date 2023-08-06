a75e9720277c6abf7df1346852916445cbc22e6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:58 +0000

    Add package django-macaddress-ng/1.0.0/django-macaddress-ng-1.0.0.tar.gz

---
+++ packages/django-macaddress-ng/django-macaddress-ng-1.0.0.tar.gz/django-macaddress-ng-1.0.0/macaddress/fields.py
@@ -0,0 +1,53 @@
+from django.core.exceptions import ValidationError
+from django.db import models
+
+from netaddr import EUI, AddrFormatError, mac_eui48
+
+from formfields import MACAddressField as MACAddressFormField
+
+
+class MACAddressField(models.Field):
+    description = "A MAC address validated by netaddr.EUI"
+    empty_strings_allowed = False
+    __metaclass__ = models.SubfieldBase
+
+    def get_prep_value(self, value):
+        if value is None:
+            return None
+        if not isinstance(value, EUI):
+            return int(EUI(value, dialect=mac_eui48))
+        return int(value)
+
+    def get_internal_type(self):
+        return "BigIntegerField"
+
+    def to_python(self, value):
+        if value is None:
+            return value
+        if isinstance(value, EUI):
+            value.dialect = mac_eui48
+            return value
+        try:
+            return EUI(value, dialect=mac_eui48)
+        except (TypeError, ValueError, AddrFormatError):
+            raise ValidationError(
+                "This value must be a valid MAC address.")
+
+    def formfield(self, **kwargs):
+        defaults = {'form_class': MACAddressFormField}
+        defaults.update(kwargs)
+        return super(MACAddressField, self).formfield(**defaults)
+
+    def get_prep_lookup(self, lookup_type, value):
+        # data is stored internally as integer so searching as string
+        # yeild 0 result. for example: useful for search in admin.
+        if lookup_type in ('exact', 'iexact', 'icontains', 'icontains'):
+            return self.get_prep_value(value)
+        else:
+            raise TypeError('Lookup type %r not supported.' % lookup_type)
+
+try:
+    from south.modelsinspector import add_introspection_rules
+    add_introspection_rules([], ["^macaddress\.fields\.MACAddressField"])
+except ImportError:
+    pass
