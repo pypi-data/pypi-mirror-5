8be650997c3cf5b3bda3dbf2e99948dffc8c384f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:31 +0000

    Add package yhat/0.1.8/yhat-0.1.8-py2.7.egg

---
+++ packages/yhat/yhat-0.1.8-py2.7.egg/yhat/document.py
@@ -0,0 +1,39 @@
+import warnings
+from math import log
+
+try:
+    import pandas as pd
+    import numpy as np
+except Exception, e:
+    warnings.warn("Warning: numpy required to auto-document models!")
+
+
+def isnumeric(x):
+    try:
+        x += 1
+        return True
+    except:
+        return False
+
+
+def document_column(df, col):
+    doc = {}
+    doc["name"] = col
+    if isnumeric(df[col]):
+        doc["dtype"] = "number"
+        doc["step"] = 0
+        step = df[col].quantile(.50) - df[col].quantile(.25)
+        doc["step"] = 10**np.round(np.log(step) - math.log(5.5, 10) + 0.5)
+        doc["palceholder"] = np.median(df[col])
+    elif (len(np.unique(df[col])) / float(len(df))) < 0.25:
+        doc["dtype"] = "factor"
+        doc["levels"] = sorted(np.unique(df[col]))
+    else:
+        doc["dtype"] = "text"
+    return doc
+
+def document_data(df):
+    docs = []
+    for col in df.columns:
+        docs.append(document_column(df, col))
+    return docs
