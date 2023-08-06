2692d8a0d700ebdf60325d64f96309e24ee6c1f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:36 +0000

    Add package infi.monotonic_time/0.1/infi.monotonic_time-0.1-py2.7.egg

---
+++ packages/infi.monotonic_time/infi.monotonic_time-0.1-py2.7.egg/infi/monotonic_time/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.1)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '362aa855ff3e8fd712571d8b55e6cf9701138a5c'
+__git_head_subject__ = 'Finished release v0.1.'
+__git_head_message__ = '* release/v0.1:\n  buildout.cfg: changed isolated python version to v2.7.5.4\n  TRIVIAL monotonic_time\n  added all project files'
+__git_dirty_diff__ = ''
