eda6553ea2b7e5e0ffb826a24c08e2cd8bcb839a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:57 +0000

    Add package django-rq-email-backend/0.1.2/django-rq-email-backend-0.1.2.tar.gz

---
+++ packages/django-rq-email-backend/django-rq-email-backend-0.1.2.tar.gz/django-rq-email-backend-0.1.2/tasks.py
@@ -0,0 +1,29 @@
+from invoke import run, task
+
+
+@task
+def package():
+    run('python setup.py sdist')
+    run('python setup.py bdist_wheel')
+
+
+@task
+def package_upload():
+    run('python setup.py sdist upload')
+    run('python setup.py bdist_wheel upload')
+
+
+@task
+def package_test():
+    run('python setup.py sdist -r test')
+    run('python setup.py bdist_wheel -r test')
+
+
+def package_test_upload():
+    run('python setup.py sdist upload -r test')
+    run('python setup.py bdist_wheel upload -r test')
+
+
+@task
+def test():
+    run('tox')
