6930964ab8c019aca7dd5862caba27aeff5ddd53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:29 +0000

    Add package fb/0.4.0/fb-0.4.0.tar.gz

---
+++ packages/fb/fb-0.4.0.tar.gz/fb-0.4.0/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+    name='fb',
+    version='0.4.0',
+    author='Samir Ahmed',
+    author_email='blaklites@gmail.com',
+    packages=['fb'],
+    #scripts=[],
+    url='https://github.com/blaklites/fb',
+    license='The MIT License (MIT)',
+    description='Python SDK for the Facebook Graph Api\nLatest Version Overview: Introduced the Helper Methods',
+    long_description=open('README.txt').read(),
+    install_requires=[],
+    keywords=['facebook', 'facebook-sdk', 'python-facebook', 'facebook-graph', 'facebook-api', 'facebook-python']
+)
