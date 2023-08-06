4f776a287a8637f5aa63a8ae819da775381787a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:36 +0000

    Add package django-bootstrap-crud-templates/1.0.12/django-bootstrap-crud-templates-1.0.12.tar.gz

---
+++ packages/django-bootstrap-crud-templates/django-bootstrap-crud-templates-1.0.12.tar.gz/django-bootstrap-crud-templates-1.0.12/demo/crud/models.py
@@ -0,0 +1,14 @@
+from django.core.urlresolvers import reverse
+from django.db import models
+from bsct.models import BSCTModelMixin
+
+class Widget( BSCTModelMixin, models.Model ):
+    name = models.CharField( max_length = 10 )
+    sku  = models.IntegerField()
+
+    def __unicode__( self ):
+        return '%s' % ( self.name )
+    
+    class Meta:
+        verbose_name = "Widget"
+        verbose_name_plural = "Widgets"
