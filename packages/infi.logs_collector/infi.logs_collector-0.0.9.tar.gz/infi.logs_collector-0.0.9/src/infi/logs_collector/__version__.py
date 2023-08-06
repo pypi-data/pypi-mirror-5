b73f013c6e24788abc03757455878d646373c0c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:07 +0000

    Add package infi.logs_collector/0.0.9/infi.logs_collector-0.0.9.tar.gz

---
+++ packages/infi.logs_collector/infi.logs_collector-0.0.9.tar.gz/infi.logs_collector-0.0.9/src/infi/logs_collector/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.9"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.0.9)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '0aa5663bd927cda11f5e8b2ad5d0efc2a1026982'
+__git_head_subject__ = 'Finished release v0.0.9.'
+__git_head_message__ = "* release/v0.0.9:\n  TRIVIAL better handling in filter_old_files\n  HOSTDEV-1199 add parameter to specify parent dir name in archive\n  prefer-final=false\n  TRIVIAL fixing was_this_file_modified_recently\n  TRIVIAL parse_timedelta now accepts strings like '10d\n  TRIVIAL adding support for optional_tempfile_dir for systems without a lot of free space on /tmp\n  fixing tests"
+__git_dirty_diff__ = ''
