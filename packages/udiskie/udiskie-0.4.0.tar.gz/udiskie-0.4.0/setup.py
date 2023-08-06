c95fd0ba2e57d9dfc8ce28e8cbfb803f5e414f65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:08 +0000

    Add package udiskie/0.4.0/udiskie-0.4.0.tar.gz

---
+++ packages/udiskie/udiskie-0.4.0.tar.gz/udiskie-0.4.0/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='udiskie',
+    version='0.4.0',
+    description='Removable disk automounter for udisks',
+    author='Byron Clark',
+    author_email='byron@theclarkfamily.name',
+    url='http://bitbucket.org/byronclark/udiskie',
+    license='MIT',
+    packages=[
+        'udiskie',
+    ],
+    scripts=[
+        'bin/udiskie',
+        'bin/udiskie-umount',
+    ],
+)
