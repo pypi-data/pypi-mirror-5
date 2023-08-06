1e5f3f388e9374c907315f8778e8f359a465e42c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:30 +0000

    Add package supervisor-serialrestart/0.1.1/supervisor-serialrestart-0.1.1.tar.gz

---
+++ packages/supervisor-serialrestart/supervisor-serialrestart-0.1.1.tar.gz/supervisor-serialrestart-0.1.1/HISTORY.rst
@@ -0,0 +1,14 @@
+.. :changelog:
+
+History
+-------
+
+0.1.1 (2013-10-29)
+++++++++++++++++++
+
+* just fixed documentation/project structure
+
+0.1.0 (2013-10-16)
+++++++++++++++++++
+
+* First release on PyPI.
---
+++ packages/supervisor-serialrestart/supervisor-serialrestart-0.1.1.tar.gz/supervisor-serialrestart-0.1.1/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+setup(
+    name='supervisor-serialrestart',
+    version='0.1.1',
+    description='Adds serialrestart command to Supervisor.',
+    long_description=readme + '\n\n' + history,
+    author='Sven Richter',
+    author_email='native2k@gmail.com',
+    url='https://github.com/native2k/supervisorserialrestart',
+    packages=[
+        'supervisorserialrestart',
+    ],
+    package_dir={'supervisorserialrestart': 'supervisorserialrestart'},
+    include_package_data=True,
+    install_requires=[
+    ],
+    license="BSD",
+    zip_safe=False,
+    keywords='supervisorserialrestart supervisor-serialrestart supervisor serialrestart',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+    ],
+    test_suite='tests.run_tests.run_all',
+)
