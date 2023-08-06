243f62409dd590d65756a4e543b1edc8a923c932
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:18 +0000

    Add package future/0.3.5/future-0.3.5.tar.gz

---
+++ packages/future/future-0.3.5.tar.gz/future-0.3.5/future/common_iterators.py
@@ -0,0 +1,49 @@
+"""
+This module is designed to be used as follows::
+
+    from __future__ import division, absolute_import, print_function
+    from future.common_iterators import *
+
+And then, for example::
+
+    for i in range(10**10):
+        pass
+    
+    for (a, b) in zip(range(10**10), range(-10**10, 0)):
+        pass
+
+Note that this is standard Python 3 code, plus some imports that do
+nothing on Python 3.
+
+The iterators this brings in are::
+
+- ``range``
+- ``filter``
+- ``map``
+- ``zip``
+
+``range`` is equivalent to ``xrange`` on Python 2 when imported from this
+module. As an alternative, there is a pure Python backport of Python 3's
+``range`` iterator available with slicing support. To use it, import it with::
+
+    from future.modified_builtins import range
+
+The other iterators (``filter``, ``map``, ``zip``) are from the ``itertools``
+module on Python 2. Note that these are also available in the
+``future_builtins`` module on Python 2 (but not Python 3).
+"""
+
+from __future__ import division, absolute_import, print_function
+
+from . import six
+
+if not six.PY3:
+    _oldrange, _oldmap, _oldzip, _oldfilter = range, map, zip, filter
+
+from .six.moves import xrange as range
+from .six.moves import map, zip, filter
+
+if six.PY3:
+    __all__ = []
+    
+
---
+++ packages/future/future-0.3.5.tar.gz/future-0.3.5/setup.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+import os
+import sys
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import future
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+NAME = "future"
+PACKAGES = ["future",
+            "future.modified_builtins",
+            "future.tests",
+            "future.backports",
+            "future.backports.html",
+            "future.backports.http",
+            "future.backports.test",
+            "libfuturize",
+            "libfuturize.fixes2",
+            "libfuturize.fixes3"]
+PACKAGE_DATA = {'': [
+                     'README.rst',
+                     'LICENSE',
+                     'NEWS.txt',
+                     'futurize.py',
+                     'discover_tests.py',
+                     'useful_links.txt',
+                     'check_rst.sh']}
+REQUIRES = []
+VERSION = future.__version__
+DESCRIPTION = "Clean single-source support for Python 3 and 2"
+LONG_DESC = future.__doc__
+AUTHOR = "Ed Schofield"
+AUTHOR_EMAIL = "ed@pythoncharmers.com"
+URL="http://github.com/edschofield/python-future"
+LICENSE = "MIT"
+KEYWORDS = "future python3 migration backport six 2to3 futurize modernize"
+CLASSIFIERS = [
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.3",
+    "License :: OSI Approved",
+    "License :: OSI Approved :: MIT License",
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+]
+
+setup_kwds = {}
+
+setup(name=NAME,
+      version=VERSION,
+      author=AUTHOR,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      description=DESCRIPTION,
+      long_description=LONG_DESC,
+      license=LICENSE,
+      keywords=KEYWORDS,
+      entry_points={
+          'console_scripts': [
+              'python-futurize = libfuturize.main:main'
+          ]
+      },
+      packages=PACKAGES,
+      package_data=PACKAGE_DATA,
+      include_package_data=True,
+      install_requires=REQUIRES,
+      classifiers=CLASSIFIERS,
+      test_suite = "discover_tests",
+      **setup_kwds
+     )
+
