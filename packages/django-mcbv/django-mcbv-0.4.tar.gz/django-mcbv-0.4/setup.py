af6ffaea1951c5a6bc8e72afa41235bee6f8f7a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:41 +0000

    Add package django-mcbv/0.4/django-mcbv-0.4.tar.gz

---
+++ packages/django-mcbv/django-mcbv-0.4.tar.gz/django-mcbv-0.4/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='django-mcbv',
+      version='0.4',
+      description='Modular Class Based Views (Django)',
+      author='Andrei Kulakov',
+      author_email='ak@lightbird.net',
+      url='https://github.com/akulakov/django/tree/master/dbe/mcbv/',
+      packages=['mcbv'],
+     )
