19a0a09ba8a8d57d0977ebcbf760a993d8ca6f45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:35 +0000

    Add package Django-Pizza/13.07.2/Django-Pizza-13.07.2.tar.gz

---
+++ packages/Django-Pizza/Django-Pizza-13.07.2.tar.gz/Django-Pizza-13.07.2/setup.py
@@ -0,0 +1,34 @@
+import sys
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "Django-Pizza",
+    version = '13.07.2',
+    description = "Yet another Django CMS.",
+    url = "https://github.com/pizzapanther/Django-Pizza",
+    author = "Paul Bailey",
+    author_email = "paul.m.bailey@gmail.com",
+    license = "BSD",
+    packages = [
+      'pizza',
+      'pizza.kitchen_sink',
+      'pizza.kitchen_sink.migrations',
+      'pizza.blog',
+      'pizza.blog.migrations',
+      'pizza.pagination',
+    ],
+    include_package_data = True,
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    install_requires = [
+      "sorl-thumbnail>=11.01"
+    ]
+)
