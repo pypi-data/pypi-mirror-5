7e2ddf98dc2d8c7c597034f3efc14b5d880f7d73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:37 +0000

    Add package bda.intellidatetime/1.2.1/bda.intellidatetime-1.2.1.tar.gz

---
+++ packages/bda.intellidatetime/bda.intellidatetime-1.2.1.tar.gz/bda.intellidatetime-1.2.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '1.2.1'
+shortdesc = 'bda.intellidatetime'
+longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+longdesc += open(os.path.join(os.path.dirname(__file__), 'LICENSE.rst')).read()
+
+setup(name='bda.intellidatetime',
+      version=version,
+      description=shortdesc,
+      long_description=longdesc,
+      classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python', 
+            'Topic :: Utilities',
+      ],
+      keywords='',
+      author='BlueDynamics Alliance',
+      author_email='dev@bluedynamics.com',
+      url=u'https://svn.plone.org/svn/collective/bda.intellidatetime',
+      license='GNU General Public Licence',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['bda'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'zope.interface',
+      ],
+      tests_require=['interlude'],
+      test_suite="bda.intellidatetime.tests.test_suite"
+      )
---
+++ packages/bda.intellidatetime/bda.intellidatetime-1.2.1.tar.gz/bda.intellidatetime-1.2.1/src/bda/intellidatetime/__init__.py
@@ -0,0 +1,11 @@
+from bda.intellidatetime.interfaces import (
+    ILocalePattern,
+    IIntelliDateTime,
+    DateTimeConversionError,
+)
+from bda.intellidatetime.converter import (
+    LocalePattern,
+    IntelliDateTime,
+    convert,
+)
+
