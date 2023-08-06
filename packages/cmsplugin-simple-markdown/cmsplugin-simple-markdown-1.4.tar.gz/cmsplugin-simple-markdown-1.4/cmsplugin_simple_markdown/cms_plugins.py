1fd9ce5a82f7eb2420839c3f21f9ae97c8bf00b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:03 +0000

    Add package cmsplugin-simple-markdown/1.4/cmsplugin-simple-markdown-1.4.tar.gz

---
+++ packages/cmsplugin-simple-markdown/cmsplugin-simple-markdown-1.4.tar.gz/cmsplugin-simple-markdown-1.4/cmsplugin_simple_markdown/cms_plugins.py
@@ -0,0 +1,46 @@
+import re
+
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+from cmsplugin_simple_markdown.models import SimpleMarkdownPlugin
+from cms.utils.page_resolver import get_page_from_path
+
+
+class SimpleMarkdownCMSPluginForm(forms.ModelForm):
+    class Meta:
+        model = SimpleMarkdownPlugin
+        widgets = {
+            'markdown_text': forms.Textarea(
+                attrs={'cols': 100, 'rows': 20,
+                       'style': 'font-family: Monaco, monospace;'}
+            )
+        }
+
+
+class SimpleMarkdownCMSPlugin(CMSPluginBase):
+    model = SimpleMarkdownPlugin
+    name = _('Text (Markdown)')
+    render_template = 'cmsplugin_simple_markdown/simple_markdown.html'
+    admin_preview = False
+    form = SimpleMarkdownCMSPluginForm
+
+    def replace_links(self, markdown_text):
+        def link_repl(match):
+            page = get_page_from_path(match.group(1))
+            if page:
+                return "(" + page.get_absolute_url() + ")"
+            else:
+                return "(#" + match.group(1) + ")"
+
+        return re.sub('\(page:([^\)]+)\)', link_repl, markdown_text)
+
+    def render(self, context, instance, placeholder):
+        context['text'] = self.replace_links(instance.markdown_text)
+        self.render_template = instance.template
+        return context
+
+
+plugin_pool.register_plugin(SimpleMarkdownCMSPlugin)
---
+++ packages/cmsplugin-simple-markdown/cmsplugin-simple-markdown-1.4.tar.gz/cmsplugin-simple-markdown-1.4/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+setup(
+    name='cmsplugin-simple-markdown',
+    version=".".join(map(str, __import__('cmsplugin_simple_markdown').__version__)),
+    packages=['cmsplugin_simple_markdown', 'cmsplugin_simple_markdown.migrations'],
+    package_dir={'cmsplugin_simple_markdown': 'cmsplugin_simple_markdown'},
+    package_data={'cmsplugin_simple_markdown': ['templates/*/*']},
+    install_requires=[
+        'django',
+        'markdown',
+        'django-markwhat',
+        'django-cms'
+    ],
+    url='https://www.github.com/Alir3z4/cmsplugin-simple-markdown',
+    license=open('LICENSE').read(),
+    author='Alireza Savand',
+    author_email='alireza.savand@gmail.com',
+    description='A plugin for django-cms that provides just a markdown plugin and nothing more.',
+    long_description=open('README.rst').read(),
+    keywords=[
+        'django',
+        'django-cms',
+        'web',
+        'cms',
+        'cmsplugin',
+        'plugin',
+    ],
+    platforms='OS Independent',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development'
+    ],
+)
