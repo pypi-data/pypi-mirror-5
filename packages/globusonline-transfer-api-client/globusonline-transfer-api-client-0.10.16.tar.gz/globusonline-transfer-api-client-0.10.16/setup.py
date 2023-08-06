6aad2cc7c9e5613d2cec13de4499124f125de0cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:11 +0000

    Add package globusonline-transfer-api-client/0.10.16/globusonline-transfer-api-client-0.10.16.tar.gz

---
+++ packages/globusonline-transfer-api-client/globusonline-transfer-api-client-0.10.16.tar.gz/globusonline-transfer-api-client-0.10.16/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+setup(name="globusonline-transfer-api-client",
+      version="0.10.16",
+      description="Globus Online Transfer API client library",
+      long_description=open("README.rst").read(),
+      author="Bryce Allen",
+      author_email="ballen@ci.uchicago.edu",
+      url="https://github.com/globusonline/transfer-api-client-python",
+      packages=["globusonline", "globusonline.transfer",
+                "globusonline.transfer.api_client",
+                "globusonline.transfer.api_client.x509_proxy"],
+      package_data={ "globusonline.transfer.api_client": ["ca/*.pem",
+                                                          "examples/*.py"] },
+      keywords=["globusonline"],
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: Apache Software License",
+          "Operating System :: MacOS :: MacOS X",
+          "Operating System :: Microsoft :: Windows",
+          "Operating System :: POSIX",
+          "Programming Language :: Python",
+          "Topic :: Communications :: File Sharing",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          ],
+      )
