d0f3079b2eae88c8141011fc3ce4ac2e01fa9c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:45 +0000

    Add package SQLAlchemy-i18n/0.6.0/SQLAlchemy-i18n-0.6.0.tar.gz

---
+++ packages/SQLAlchemy-i18n/SQLAlchemy-i18n-0.6.0.tar.gz/SQLAlchemy-i18n-0.6.0/sqlalchemy_i18n/__init__.py
@@ -0,0 +1,29 @@
+import sqlalchemy as sa
+from .manager import translation_manager, TranslationManager
+from .translatable import Translatable
+
+
+__all__ = (
+    Translatable,
+    TranslationManager,
+    translation_manager
+)
+
+
+__version__ = '0.6.0'
+
+
+def make_translatable(
+    mapper=sa.orm.mapper,
+    session=sa.orm.session.Session,
+    manager=translation_manager
+):
+    sa.event.listen(
+        mapper, 'instrument_class', manager.instrument_translatable_classes
+    )
+    sa.event.listen(
+        mapper, 'after_configured', manager.configure_translatable_classes
+    )
+    sa.event.listen(
+        session, 'before_flush', manager.auto_create_translations
+    )
