f0f4220eb794127b5744a66978cafba7411359fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:07 +0000

    Add package pyramid_health/0.3.18/pyramid_health-0.3.18.tar.gz

---
+++ packages/pyramid_health/pyramid_health-0.3.18.tar.gz/pyramid_health-0.3.18/pyramid_health/tests.py
@@ -0,0 +1,62 @@
+import unittest
+import mock
+
+import webtest
+from pyramid.config import Configurator
+
+
+class TestHealth(unittest.TestCase):
+
+    maxDiff = None
+
+    def setUp(self):
+        settings = {}
+        config = Configurator(settings=settings)
+        config.include('pyramid_health')
+        self.app = webtest.TestApp(config.make_wsgi_app())
+
+    def test_get(self):
+        response = self.app.get('/health')
+
+        self.assertEqual(response.status_int, 200)
+        self.assertEqual(response.body, b'OK')
+
+    def test_post(self):
+        response = self.app.post('/health')
+
+        self.assertEqual(response.status_int, 200)
+        self.assertEqual(response.body, b'OK')
+
+
+class TestHealthMaintenance(unittest.TestCase):
+
+    maxDiff = None
+
+    def setUp(self):
+        settings = {'healthcheck.disablefile': '/maintenance/file/test'}
+        config = Configurator(settings=settings)
+        config.include('pyramid_health')
+        self.app = webtest.TestApp(config.make_wsgi_app())
+
+    @mock.patch("os.path.exists")
+    def test_get_maintenance_on(self, m_exists):
+        m_exists.return_value = True
+
+        response = self.app.get('/health', expect_errors=True)
+
+        m_exists.assert_called_with('/maintenance/file/test')
+
+        self.assertEqual(response.status_int, 503)
+        self.assertIn(b'Healthcheck disabled by config',
+                      response.body)
+
+    @mock.patch("os.path.exists")
+    def test_get_maintenance_off(self, m_exists):
+        m_exists.return_value = False
+
+        response = self.app.get('/health')
+
+        m_exists.assert_called_with('/maintenance/file/test')
+
+        self.assertEqual(response.status_int, 200)
+        self.assertEqual(response.body, b'OK')
