aa840941a895bdb5844d561289bdf70a11c0db7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:00 +0000

    Add package pyen/1.1/pyen-1.1.tar.gz

---
+++ packages/pyen/pyen-1.1.tar.gz/pyen-1.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='pyen',
+    version='1.1', 
+    description='simple client for The Echo Nest',
+    author="@plamere",
+    author_email="paul@echonest.com",
+    url='http://github.com/plamere/pyen',
+    py_modules=['pyen'],)
