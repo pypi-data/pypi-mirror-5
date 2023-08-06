a1d2b935bc5d90885ed13d120c4019195bc963e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:09 +0000

    Add package pyicloud_dwoh/0.4/pyicloud_dwoh-0.4.tar.gz

---
+++ packages/pyicloud_dwoh/pyicloud_dwoh-0.4.tar.gz/pyicloud_dwoh-0.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+
+setup(
+    name='pyicloud_dwoh',
+    version='0.4',
+    url='https://github.com/latestrevision/pyicloud',
+    description=(
+        'PyiCloud is a module which allows pythonistas to '
+        'interact with iCloud webservices.  This is an unofficial '
+        'fork of the official package \'pyicloud\'.'
+    ),
+    author='Peter Evans and Adam Coddington',
+    author_email='me@adamcoddington.net',
+    packages=find_packages(),
+    install_requires=required
+)
