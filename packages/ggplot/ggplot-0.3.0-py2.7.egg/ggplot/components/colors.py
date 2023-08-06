b31d3bdae63d5a02d3ffb5b48ed423702c92f8d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:01 +0000

    Add package ggplot/0.3.0/ggplot-0.3.0-py2.7.egg

---
+++ packages/ggplot/ggplot-0.3.0-py2.7.egg/ggplot/components/colors.py
@@ -0,0 +1,15 @@
+COLORS = [
+    "#000000",
+    "#E69F00", 
+    "#56B4E9", 
+    "#009E73", 
+    "#F0E442", 
+    "#0072B2", 
+    "#D55E00",
+    "#CC79A7"
+]
+
+def color_gen():
+	while True:
+		for color in COLORS:
+			yield color
