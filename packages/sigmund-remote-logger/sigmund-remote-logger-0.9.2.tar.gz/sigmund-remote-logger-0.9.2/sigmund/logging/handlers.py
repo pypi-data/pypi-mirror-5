eb73b2fc006f8a9d6c8a36dad4bc33db4d4b7fd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:46 +0000

    Add package sigmund-remote-logger/0.9.2/sigmund-remote-logger-0.9.2.tar.gz

---
+++ packages/sigmund-remote-logger/sigmund-remote-logger-0.9.2.tar.gz/sigmund-remote-logger-0.9.2/sigmund/logging/handlers.py
@@ -0,0 +1,36 @@
+from django.conf import settings
+from logging import Handler
+import json, requests, urlparse
+
+class SigmundHandler(Handler):
+
+    def __init__(self):
+        super(SigmundHandler, self).__init__()
+
+    def emit(self, record):
+        if not hasattr(settings, 'SIGMUND_URL') or not hasattr(
+                       settings, 'SIGMUND_USERNAME') or not hasattr(
+                       settings, 'SIGMUND_API_KEY'):
+            return
+
+        url = urlparse.urljoin(settings.SIGMUND_URL, '/api/v1/sigmund/')
+
+        headers = {
+            'Authorization': 'ApiKey {username}:{api_key}'.format(
+                username=settings.SIGMUND_USERNAME,
+                api_key=settings.SIGMUND_API_KEY),
+            'Content-Type': 'application/json',
+        }
+
+        params = {
+            'message': self.format(record),
+            'level': record.levelname,
+            'filename': record.filename,
+            'line': record.lineno,
+            'timestamp': record.created,
+        }
+
+        requests.post(url, json.dumps(params), headers=headers)
+
+if __name__ == '__main__':
+    print SigmundHandler()
