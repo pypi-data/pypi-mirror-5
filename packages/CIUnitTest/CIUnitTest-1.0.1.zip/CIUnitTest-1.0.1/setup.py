9393e839b890c2f0840fed59ac52f6e0acfa3571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:13 +0000

    Add package CIUnitTest/1.0.1/CIUnitTest-1.0.1.zip

---
+++ packages/CIUnitTest/CIUnitTest-1.0.1.zip/CIUnitTest-1.0.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+from distutils.core import setup
+
+setup(name='CIUnitTest',
+      version='1.0.1',
+      description='Provides test results of unittest in JSON format, in order to be able to use the results programmatically.',
+      author='Arseni Mourzenko',
+      author_email='arseni.mourzenko@pelicandd.com',
+      url='http://source.pelicandd.com/Codebase/Continuous integration/Build platform/libraries/ciunittest/',
+      license='MS-PL',
+      keywords='unittest ci continuous-integration json',
+      py_modules=['ciunittest'])
