42ff1febb58604d071690eda3bbdfc7056b8cb80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:31 +0000

    Add package weechat_notifier/0.1.0/weechat_notifier-0.1.0.tar.gz

---
+++ packages/weechat_notifier/weechat_notifier-0.1.0.tar.gz/weechat_notifier-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name = 'weechat_notifier',
+    version = '0.1.0',
+    packages = ['weechat_notifier'],
+    description = 'Weechat Notification Plugin',
+    author='Fenton Travers',
+    author_email='fenton.travers@gmail.com',
+    url='www.google.com',
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description='A notification plugin for weechat',
+    classifiers=[
+        'Programming Language :: Python :: 3.3',
+    ]
+)
