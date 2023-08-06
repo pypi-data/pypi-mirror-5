bffc7cc61c7e35942ad791c224853855f5034d9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:30 +0000

    Add package you-get/0.3.19/you-get-0.3.19.tar.gz

---
+++ packages/you-get/you-get-0.3.19.tar.gz/you-get-0.3.19/src/you_get/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from .common import *
+from .version import *
+
+# Easy import
+#from .cli_wrapper.converter import *
+#from .cli_wrapper.player import *
+from .downloader import *
---
+++ packages/you-get/you-get-0.3.19.tar.gz/you-get-0.3.19/src/you_get/version.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+__all__ = ['__version__', '__date__']
+
+__version__ = '0.3.19'
+__date__ = '2013-08-16'
