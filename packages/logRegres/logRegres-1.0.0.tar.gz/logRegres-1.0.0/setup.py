d8799a37b417ad8ae45e176311b3299327a9f989
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:21 +0000

    Add package logRegres/1.0.0/logRegres-1.0.0.tar.gz

---
+++ packages/logRegres/logRegres-1.0.0.tar.gz/logRegres-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name = 'logRegres',
+	version = '1.0.0',
+	py_modules = ['logRegres'],
+	author = 'chenliyu',
+	author_email = '2572065090@qq.com',
+	url = 'http://pypi.python.org/ ',
+	description = 'implimention of logistic regression in python,and a test of it',
+)
+
