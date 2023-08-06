720c09bd22dabfcb813ff45a3891aec13796e195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:03 +0000

    Add package django-postgres-pebble/0.0.6/django-postgres-pebble-0.0.6.tar.gz

---
+++ packages/django-postgres-pebble/django-postgres-pebble-0.0.6.tar.gz/django-postgres-pebble-0.0.6/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-postgres-pebble',
+    version='0.0.6',
+    description="First-class Postgres feature support for the Django ORM. (Pebble Branch)",
+    author='Scott Walton',
+    author_email='scw@talktopebble.co.uk',
+    license='Public Domain',
+    packages=find_packages(),
+    install_requires=[
+        'bitstring',
+        'Django>=1.3',
+    ],
+)
