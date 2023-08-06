4e791c378eace0015bf70d04bf3ff99dd4adba17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:38 +0000

    Add package pyramid-fanstatic/0.5/pyramid_fanstatic-0.5.zip

---
+++ packages/pyramid-fanstatic/pyramid_fanstatic-0.5.zip/pyramid_fanstatic-0.5/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+version = '0.5'
+
+
+def description():
+    try:
+        return open('README.rst').read() + '\n' + open('CHANGES.rst').read()
+    except:
+        return ''
+
+setup(name='pyramid_fanstatic',
+      version=version,
+      description="pyramid tween for fanstatic",
+      long_description=description(),
+      classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Framework :: Pylons",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "License :: OSI Approved :: MIT License",
+      ],
+      keywords='pyramid fanstatic js css',
+      author='Gael Pasgrimaud',
+      author_email='gael@gawel.org',
+      url='https://github.com/FormAlchemy/pyramid_fanstatic',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+          'pyramid',
+          'fanstatic',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [pyramid.scaffold]
+      pyramid_fanstatic=pyramid_fanstatic.scaffolds:PyramidFanstaticTemplate
+      """,
+      )
