91871c633d1d53870d1aa2a857a7fb637672dc1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:18 +0000

    Add package omdb/0.0.1/omdb-0.0.1.tar.gz

---
+++ packages/omdb/omdb-0.0.1.tar.gz/omdb-0.0.1/omdb/__init__.py
@@ -0,0 +1,11 @@
+
+from .api import (
+    search,
+    get,
+    title,
+    Client
+)
+
+__version__ = '0.0.1'
+__author__ = 'Derrick Gilland <dgilland@gmail.com>'
+__all__ = ['search', 'get', 'title', 'request', 'Client']
