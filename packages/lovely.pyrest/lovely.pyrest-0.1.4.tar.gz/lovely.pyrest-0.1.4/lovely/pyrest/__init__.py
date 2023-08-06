38c91ea54c828c6be41b42d73f38211a1a96130b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:57 +0000

    Add package lovely.pyrest/0.1.4/lovely.pyrest-0.1.4.tar.gz

---
+++ packages/lovely.pyrest/lovely.pyrest-0.1.4.tar.gz/lovely.pyrest-0.1.4/lovely/pyrest/__init__.py
@@ -0,0 +1,63 @@
+from lovely.pyrest.predicates import ContentTypePredicate
+from lovely.pyrest.views import get_fallback_view, decorate_view
+from lovely.pyrest.settings import (
+    set_jsonp_param_name,
+    get_jsonp_param_name
+)
+from pyramid.events import NewRequest
+from errors import Errors
+from pyramid.renderers import JSONP
+import copy
+
+__version__ = "0.1.4"
+
+
+def add_service(config, service):
+    """ registers the views for a service """
+    # register the route
+    config.add_route(service.name, service.path)
+    # create the fallback view
+    config.add_view(view=get_fallback_view(service),
+                    route_name=service.name)
+    for method, view, args in service.definitions:
+        args = copy.deepcopy(args)  # make a copy of the dict to not modify it
+        args['request_method'] = method
+        decorated_view = decorate_view(view, dict(args))
+        # remove args which are unknown by pyramid
+        for item in ('validators', 'schema', 'jsonp', 'help'):
+            if item in args:
+                del args[item]
+        config.add_view(view=decorated_view, route_name=service.name, **args)
+    config.commit()
+
+
+def wrap_request(event):
+    """ Creates an empty errors list and adds it to the request """
+    request = event.request
+    if not hasattr(request, 'errors'):
+        request.errors = Errors()
+
+
+# The includeme function will be called after including
+# this module:
+# config.include('lovely.pyrest')
+def includeme(config):
+    config.add_directive('add_service', add_service)
+    config.add_subscriber(wrap_request, NewRequest)
+    config.add_view_predicate('content_type', ContentTypePredicate)
+    add_custom_config(config)
+
+
+def add_custom_config(config):
+    """ Custom configuration parameters in your .ini file
+    will be handled here.
+    """
+    settings = config.get_settings()
+
+    # Add JSONP support
+    # If param_name is set as qeury parameter in a request it will trigger
+    # the JSONP transformation.
+    # 'callback' will be set as default if `param_name` is not specified in
+    # the '.ini' file.
+    set_jsonp_param_name(settings)
+    config.add_renderer('jsonp', JSONP(param_name=get_jsonp_param_name()))
---
+++ packages/lovely.pyrest/lovely.pyrest-0.1.4.tar.gz/lovely.pyrest-0.1.4/lovely/pyrest/sphinx/service.py
@@ -0,0 +1,76 @@
+from docutils import nodes
+from docutils.parsers.rst import Directive
+from lovely.pyrest.service import get_services
+from importlib import import_module
+from lovely.pyrest.sphinx.helpers import create_node, trim
+from lovely.pyrest.sphinx.schema import SchemaDirective
+from lovely.pyrest.sphinx.validators import ValidatorsDirective
+
+
+def convert_to_list(value):
+    if value is None:
+        return []
+    return [v.strip() for v in value.split(',')]
+
+
+class ServiceDirective(Directive):
+    """ The Service directive renders all services defined in a module """
+
+    has_content = True
+    required_arguments = 1
+
+    option_spec = {'services': convert_to_list}
+
+    def run(self):
+        module = self.arguments[0]
+        import_module(module)
+        rendered = []
+        settings = self.state.document.settings
+        # fetch all services
+        services = get_services()
+        # if the services option is set render only the named services
+        names = self.options.get('services')
+        if names:
+            services = [s for s in services if s.name in names]
+        for service in services:
+            service_id = "service_%d" % settings.env.new_serialno('service')
+            rendered.append(ServiceDirective.render(service, service_id))
+        return rendered
+
+    @staticmethod
+    def render(service, service_id):
+        service_node = nodes.section(ids=[service_id])
+        title = "%s service - %s" % (service.name, service.path)
+        service_node += nodes.title(text=title)
+
+        if service.description is not None:
+            service_node += create_node(trim(service.description))
+
+        for method, view, args in service.definitions:
+            method_id = "%s_%s" % (service_id, method)
+            method_node = nodes.section(ids=[method_id])
+            method_node += nodes.title(text=method)
+
+            # Render Accept Header documentation
+            acceptables = service.acceptables(method)
+            if len(acceptables) > 0:
+                accept = 'Accept: ' + ', '.join(acceptables)
+                method_node += create_node(accept)
+
+            # Render Content-Type Header documentation
+            content_types = service.content_types(method)
+            if len(content_types) > 0:
+                ct = 'Content-Types: ' + ', '.join(content_types)
+                method_node += create_node(ct)
+
+            if 'schema' in args:
+                method_node += SchemaDirective.render_schema(service,
+                                                             method,
+                                                             service_id)
+
+            if 'validators' in args:
+                method_node += ValidatorsDirective.render(service,
+                                                          method,
+                                                          service_id)
+            service_node += method_node
+        return service_node
