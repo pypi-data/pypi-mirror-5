7dfc575462a7b6668335555771a6f8105dcb7831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:27 +0000

    Add package ttftree/0.2.4/ttftree-0.2.4.tar.gz

---
+++ packages/ttftree/ttftree-0.2.4.tar.gz/ttftree-0.2.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="ttftree",
+    version="0.2.4",
+    description="A 2-3 finger tree library for Python",
+    author="Alexander Boyd",
+    author_email="alex@opengroove.org",
+    py_modules=["ttftree"]
+)
