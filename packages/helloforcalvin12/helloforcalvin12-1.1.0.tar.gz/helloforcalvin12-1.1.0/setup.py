1440ac11aa70015dd8d268a87322c483ab55832f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:43 +0000

    Add package helloforcalvin12/1.1.0/helloforcalvin12-1.1.0.tar.gz

---
+++ packages/helloforcalvin12/helloforcalvin12-1.1.0.tar.gz/helloforcalvin12-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'helloforcalvin12',
+	version = '1.1.0',
+	py_modules = ['hello'],
+	author = 'calvinzhu',
+	author_email = 'chaoyuezhu@gmail.com',
+	url = 'http://www.calvinzhu123456.appspot.com',
+	description = 'Simple printer',
+	)
