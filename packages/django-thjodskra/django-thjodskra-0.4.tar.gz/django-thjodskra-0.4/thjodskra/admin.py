49502ef444a023b92dbfd336b7f1c50b6ea08e0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:09 +0000

    Add package django-thjodskra/0.4/django-thjodskra-0.4.tar.gz

---
+++ packages/django-thjodskra/django-thjodskra-0.4.tar.gz/django-thjodskra-0.4/thjodskra/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Person
+
+class PersonAdmin( admin.ModelAdmin ):
+	list_display = ( 'name', 'ssn', 'address', 'postcode', 'blocked' )
+	list_filter = ('sex', 'postcode')
+	search_fields = ('name', 'ssn')
+	
+admin.site.register( Person, PersonAdmin )
