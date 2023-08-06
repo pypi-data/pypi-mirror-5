3a796258cc0c8af7e5c4f675a5038d3ecf083ceb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:24 +0000

    Add package django-twitter-bootstrap/3.0.0-rc2/django-twitter-bootstrap-3.0.0-rc2.tar.gz

---
+++ packages/django-twitter-bootstrap/django-twitter-bootstrap-3.0.0-rc2.tar.gz/django-twitter-bootstrap-3.0.0-rc2/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="django-twitter-bootstrap",
+    version="3.0.0-rc2",
+    packages=find_packages(),
+    package_data={
+        'twitter_bootstrap': [
+            'static/js/*.js',
+            'static/less/*.less',
+        ],
+    },
+
+    # metadata for upload to PyPI
+    author="Steven Cummings",
+    author_email="cummingscs@gmail.com",
+    description="Provides a Django app whose static folder contains Twitter Bootstrap assets",
+    license="MIT",
+    keywords="django app staticfiles twitter bootstrap",
+    url="https://github.com/estebistec/django-twitter-bootstrap",
+    download_url="http://pypi.python.org/pypi/django-twitter-bootstrap",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries',
+    ]
+)
