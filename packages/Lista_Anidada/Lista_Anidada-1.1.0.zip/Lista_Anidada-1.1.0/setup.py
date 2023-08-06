04f8d37c6e6029eed0f80300244526bf732fdf9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:56 +0000

    Add package Lista_Anidada/1.1.0/Lista_Anidada-1.1.0.zip

---
+++ packages/Lista_Anidada/Lista_Anidada-1.1.0.zip/Lista_Anidada-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'Lista_Anidada',
+    version = '1.1.0',
+    py_modules = ['Lista_Anidada'],
+    author = 'dru1988',
+    author_email = 'germanmendez1988@gmail.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'Un simple print() de listas anidadas',
+    )
