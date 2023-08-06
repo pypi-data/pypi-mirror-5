d15b7a77e53a71b58337d9cabfb31d47aa41c0a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:15 +0000

    Add package dyndnsc/0.3.1/dyndnsc-0.3.1.tar.gz

---
+++ packages/dyndnsc/dyndnsc-0.3.1.tar.gz/dyndnsc-0.3.1/dyndnsc/cli.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+
+"""
+Example CLI program
+"""
+
+import sys
+import os
+import pkg_resources
+import logging
+
+from dyndnsc import getDynDnsClientForConfig
+
+
+def daemonize(stdout='/dev/null', stderr=None, stdin=os.devnull,
+              pidfile=None, startmsg='started with pid %s'):
+    """
+        This forks the current process into a daemon.
+        The stdin, stdout, and stderr arguments are file names that
+        will be opened and be used to replace the standard file descriptors
+        in sys.stdin, sys.stdout, and sys.stderr.
+        These arguments are optional and default to /dev/null.
+        Note that stderr is opened unbuffered, so
+        if it shares a file with stdout then interleaved output
+        may not appear in the order that you expect.
+    """
+    # Do first fork.
+    try:
+        pid = os.fork()
+        if pid > 0:
+            sys.exit(0)  # Exit first parent.
+    except OSError as e:
+        sys.stderr.write("fork #1 failed: (%d) %s%s" % (e.errno, e.strerror, os.linesep))
+        sys.exit(1)
+
+    # Decouple from parent environment.
+    os.chdir("/")
+    os.umask(0)
+    os.setsid()
+
+    # interestingly enough, we MUST open STDOUT explicitly before we
+    # fork the second time.
+    # Otherwise, the duping of sys.stdout won't work,
+    # and we will not be able to capture stdout
+    sys.stdout.write('')
+
+    # Do second fork.
+    try:
+        pid = os.fork()
+        if pid > 0:
+            sys.exit(0)  # Exit second parent.
+    except OSError as e:
+        sys.stderr.write("fork #2 failed: (%d) %s%s" % (e.errno, e.strerror, os.linesep))
+        sys.exit(1)
+
+    # Open file descriptors and print start message
+    if not stderr:
+        stderr = stdout
+    si = open(stdin, 'rb')
+    so = open(stdout, 'w+b')
+    se = open(stderr, 'w+b', 0)
+    pid = str(os.getpid())
+    sys.stderr.write("%s%s" % (startmsg, os.linesep) % pid)
+    sys.stderr.flush()
+    if pidfile:
+        open(pidfile, 'w+b').write("%s%s" % (pid, os.linesep))
+
+    # Redirect standard file descriptors.
+    os.dup2(si.fileno(), sys.stdin.fileno())
+    os.dup2(so.fileno(), sys.stdout.fileno())
+    os.dup2(se.fileno(), sys.stderr.fileno())
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--daemon", dest="daemon", help="go into daemon mode (implies --loop)", action="store_true", default=False)
+    parser.add_argument("--hostname", dest="hostname", help="hostname to update", default=None)
+    parser.add_argument("--key", dest="key", help="your authentication key", default=None)
+    parser.add_argument("--userid", dest="userid", help="your userid", default=None)
+    parser.add_argument("--password", dest="password", help="your password", default=None)
+    parser.add_argument("--protocol", dest="protocol", help="protocol/service to use for updating your IP (default dyndns)", default='dyndns')
+    parser.add_argument("--method", dest="method", help="method for detecting your IP (default webcheck)", default='webcheck')
+    parser.add_argument("--loop", dest="loop", help="loop forever (default is to update once)", action="store_true", default=False)
+    parser.add_argument("--sleeptime", dest="sleeptime", help="how long to sleep between checks in seconds", default=300)
+    parser.add_argument("--version", dest="version", help="show version and exit", action="store_true", default=False)
+    args = parser.parse_args()
+
+    if args.version:
+        print("dyndnsc %s" % pkg_resources.get_distribution("dyndnsc").version)  # pylint: disable=E1103
+        return 0
+
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
+    requests_log = logging.getLogger("requests")
+    requests_log.setLevel(logging.WARNING)
+
+    if args.hostname is None:
+        parser.error("Please specify a hostname using --hostname")
+
+    config = {}
+    config['hostname'] = args.hostname
+    config['key'] = args.key
+    config['userid'] = args.userid
+    config['password'] = args.password
+    config['protocol'] = args.protocol
+    config['method'] = args.method
+    config['sleeptime'] = int(args.sleeptime)
+
+    # done with command line options, bring on the dancing girls
+    dyndnsclient = getDynDnsClientForConfig(config)
+    if dyndnsclient is None:
+        return 1
+    # do an initial synchronization, before going into endless loop:
+    dyndnsclient.sync()
+
+    if args.daemon:
+        daemonize()  # fork into background
+        args.loop = True
+
+    if args.loop:
+        dyndnsclient.loop()
+    else:
+        dyndnsclient.check()
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ packages/dyndnsc/dyndnsc-0.3.1.tar.gz/dyndnsc-0.3.1/dyndnsc/detector/base.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from ..common.subject import Subject
+
+log = logging.getLogger(__name__)
+
+
+class IPDetector(Subject):
+    """
+    Base class for IP detectors. Really is just a state machine for
+    old/current value.
+    """
+    def __init__(self, *args, **kwargs):
+        super(IPDetector, self).__init__()
+
+    def canDetectOffline(self):
+        """
+        Must be overwritten. Return True when the IP detection can work
+        offline without causing network traffic.
+        """
+        raise NotImplementedError("Abstract method, must be overridden")
+
+    def getOldValue(self):
+        if not '_oldvalue' in vars(self):
+            self._oldvalue = self.getCurrentValue()
+        return self._oldvalue
+
+    def setOldValue(self, value):
+        self._oldvalue = value
+
+    def getCurrentValue(self, default=None):
+        if not hasattr(self, '_currentvalue'):
+            self._currentvalue = default
+        return self._currentvalue
+
+    def setCurrentValue(self, value):
+        if value != self.getCurrentValue(value):
+            self._oldvalue = self.getCurrentValue(value)
+            self._currentvalue = value
+            log.info("new IP detected: %s", value)
+        return value
+
+    def hasChanged(self):
+        """Detect a state change with old and current value"""
+        return self.getOldValue() != self.getCurrentValue()
