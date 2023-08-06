bb412b64a4b05208434874d936ce29891957a95a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:49 +0000

    Add package cs.bodysection/1.3/cs.bodysection-1.3.zip

---
+++ packages/cs.bodysection/cs.bodysection-1.3.zip/cs.bodysection-1.3/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.3'
+
+setup(name='cs.bodysection',
+      version=version,
+      description="Extra view to add section name for Plone. Specially useful when having INavigationRoot based sites (like ones with LinguaPlone",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Mikel Larreategi',
+      author_email='mlarreategi@codesyntax.com',
+      url='http://code.codesyntax.com/private/cs.bodysection',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['cs'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'Plone >= 4.0'
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
