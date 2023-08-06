7f524d041fd10cb93fe31d43e94f4c4528cbd29a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:22 +0000

    Add package Bashutils/0.0.4/Bashutils-0.0.4.tar.gz

---
+++ packages/Bashutils/Bashutils-0.0.4.tar.gz/Bashutils-0.0.4/setup.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup
+
+__version__ = "0.0.4"
+
+setup(
+    name="Bashutils",
+    version=__version__,
+    packages=["bashutils", "bashutils.test"],
+
+    author='Olivier BLIN',
+    author_email='olivier.oblin@gmail.com',
+    description="Bash colors management and log system",
+    long_description=open('README.rst').read(),
+    keywords="bash color log",
+    url='https://github.com/OlivierB/Bashutils.git',
+    license='GPLv3',
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+    ],
+)
