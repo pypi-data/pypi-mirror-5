110a499989199b9c952bb069f585eacb2f430746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:47 +0000

    Add package options/1.0.3/options-1.0.3.zip

---
+++ packages/options/options-1.0.3.zip/options-1.0.3/options/__init__.py
@@ -0,0 +1,8 @@
+from options.core import *
+from options.version import __version__
+
+__all__ = """
+    Unset Prohibited Transient attrs
+    Options OptionsChain OptionsContext
+    __version__
+""".split()
