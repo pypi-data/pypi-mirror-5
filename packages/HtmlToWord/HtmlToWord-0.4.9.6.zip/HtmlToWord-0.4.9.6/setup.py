2b1f99967796816ff884056a6237cf4680f084e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:57 +0000

    Add package HtmlToWord/0.4.9.6/HtmlToWord-0.4.9.6.zip

---
+++ packages/HtmlToWord/HtmlToWord-0.4.9.6.zip/HtmlToWord-0.4.9.6/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='HtmlToWord',
+    version='0.4.9.6',
+    packages=['HtmlToWord', 'HtmlToWord.elements'],
+    url='https://github.com/orf/HtmlToWord',
+    license='',
+    author='Tom',
+    author_email='tom@tomforb.es',
+    description='Render HTML to a specific portion of a word document',
+    install_requires=["BeautifulSoup>=3.2.1",
+                      "pywin32"],
+    include_package_data=True,
+    long_description="""\
+Render HTML to a word document using win32com.
+Check out the github repo for more information and code samples.
+"""
+)
