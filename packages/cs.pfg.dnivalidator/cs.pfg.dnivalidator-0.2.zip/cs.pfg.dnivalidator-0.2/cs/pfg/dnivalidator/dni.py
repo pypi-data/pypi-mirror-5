39cf2c880f81acac07e7f78b02f7de13267ddbdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:55 +0000

    Add package cs.pfg.dnivalidator/0.2/cs.pfg.dnivalidator-0.2.zip

---
+++ packages/cs.pfg.dnivalidator/cs.pfg.dnivalidator-0.2.zip/cs.pfg.dnivalidator-0.2/cs/pfg/dnivalidator/dni.py
@@ -0,0 +1,34 @@
+from Products.validation import validation, interfaces
+
+class Dni:
+    """ Validates a String field to contain a Spanish ID card number
+    and letter.
+
+    The verification algorithm is described here:
+      http://es.wikibooks.org/wiki/Algoritmo_para_obtener_la_letra_del_NIF
+
+    """
+
+    __implements__ = (interfaces.ivalidator,)
+
+    name = 'Dni'
+
+    def __init__(self, name, title='', description=''):
+        self.name = name
+        self.title = title or name
+        self.description = description
+
+    def __call__(self, value, *args, **kwargs):
+        if value[0] not in ['0','1','2','3','4','5','6','7','8','9']:
+            value=value[1:]
+        let = value[-1:].upper()
+	number = value[:-1]
+	number = int(number)
+	number = number % 23
+	letters = 'TRWAGMYFPDXBNJZSQVHLCKET'
+	letter = letters[number:number + 1]
+	if let == letter:
+            return 1
+        return ("Validation failed(%s): must be checked." % self.name)
+
+validation.register(Dni('dni')) 
