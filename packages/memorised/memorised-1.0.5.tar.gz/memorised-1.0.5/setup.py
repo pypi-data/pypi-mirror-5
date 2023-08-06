42e91926273516cf0a6329c10f0dc694fbcfb35f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:49 +0000

    Add package memorised/1.0.5/memorised-1.0.5.tar.gz

---
+++ packages/memorised/memorised-1.0.5.tar.gz/memorised-1.0.5/setup.py
@@ -0,0 +1,25 @@
+"""Installer for memorised
+"""
+
+import os
+cwd = os.path.dirname(__file__)
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+setup(
+        name='memorised',
+        version='1.0.5',
+        author='Wes Mason',
+        author_email='wes@1stvamp.org',
+        description='memcache memoization decorators and utils for python',
+        long_description=open(os.path.join(cwd, 'README.rst')).read(),
+        url='http://github.com/1stvamp/memorised/',
+        packages=find_packages(exclude=('ez_setup',)),
+        install_requires=('python-memcached',),
+        license='BSD'
+)
