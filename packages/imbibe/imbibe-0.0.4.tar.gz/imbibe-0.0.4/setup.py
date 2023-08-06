61a0cc88a7deab853ddc402caf7d8a73eb4e4107
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:52 +0000

    Add package imbibe/0.0.4/imbibe-0.0.4.tar.gz

---
+++ packages/imbibe/imbibe-0.0.4.tar.gz/imbibe-0.0.4/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='imbibe',
+    version='0.0.4',
+    description='Client to stream metrics from multiple Speakeasy servers',
+    author='Eric Wong',
+    py_modules=['imbibe'],
+    )
