41977074fedceb58545e8e7ce90675dc95a8ed4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:11 +0000

    Add package bart_api/0.1-3/bart_api-0.1-3.tar.gz

---
+++ packages/bart_api/bart_api-0.1-3.tar.gz/bart_api-0.1-3/bart_api/__init__.py
@@ -0,0 +1,31 @@
+import urllib.request
+from lxml import etree
+
+class BartApi():
+  def __init__(self, api_key="MW9S-E7SL-26DU-VV8V"):
+    self.api_key = api_key
+
+  def parse_response(self,raw_xml):
+    parsed_xml = etree.parse(raw_xml)
+    return parsed_xml
+
+  def number_of_trains(self):
+    url = "http://api.bart.gov/api/bsa.aspx?cmd=count&key=%s" % (self.api_key)
+    raw_response = urllib.request.urlopen(url)
+    xml = self.parse_response(raw_response)
+    train_count = xml.xpath('traincount')[0].text
+    return train_count
+
+  def elevator_status(self):
+    url = "http://api.bart.gov/api/bsa.aspx?cmd=elev&key=%s" % (self.api_key)
+    raw_response = urllib.request.urlopen(url)
+    xml = self.parse_response(raw_response)
+    train_count = xml.xpath('bsa')[0].xpath('description')[0].text
+    return train_count
+
+  def update_station_list(self):
+    url = "http://api.bart.gov/api/stn.aspx?cmd=stns&key=%s" % (self.api_key)
+    raw_response = urllib.request.urlopen(url)
+    xml = self.parse_response(raw_response)
+    with open('station_list.xml', 'w') as f:
+      f.write(etree.tostring(xml))
---
+++ packages/bart_api/bart_api-0.1-3.tar.gz/bart_api-0.1-3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='bart_api',
+    version='0.1-3',
+    description='a python client that interacts with the bay area rapid transit api',
+    url='http://github.com/projectdelphai/bart_api',
+    author='Reuben Castelino',
+    author_email='projetdelphai@gmail.com',
+    license='MIT',
+    packages=['bart_api', 'bart_api.test'],)
