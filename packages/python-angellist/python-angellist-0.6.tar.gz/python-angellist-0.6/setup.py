3971012decf33e999abed3f8b37958fcbe168eca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:00 +0000

    Add package python-angellist/0.6/python-angellist-0.6.tar.gz

---
+++ packages/python-angellist/python-angellist-0.6.tar.gz/python-angellist-0.6/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python-angellist',
+    version="0.6",
+    license="MIT",
+
+    install_requires = [
+        "urllib","urllib2","json"
+    ],
+
+    description='Light wrapper around AngelList API',
+    long_description=open('readme.txt').read(),
+
+    author='Sandeep Bhaskar',
+    author_email='sandeep.bhaskar19@gmail.com',
+
+    url='https://github.com/sandeepbhaskar/python-angellist',
+
+    include_package_data=True,
+
+    packages=['angellist'],
+
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ]
+)
