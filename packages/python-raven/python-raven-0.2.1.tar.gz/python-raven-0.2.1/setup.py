8adf6110c6204d66a47ea65d3d5a78246ef72a7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:38 +0000

    Add package python-raven/0.2.1/python-raven-0.2.1.tar.gz

---
+++ packages/python-raven/python-raven-0.2.1.tar.gz/python-raven-0.2.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name = "python-raven",
+    version = "0.2.1",
+    packages = ["raven"],
+    py_modules = ["ucam_webauth"],
+    package_data = {"raven": ["keys/pubkey*.crt"]},
+    install_requires = ["M2Crypto", "setuptools"],
+    extras_require = {"tests": ["nosetests"],
+                      "simple_demo": ["Flask>=0.10"]},
+
+    author = "Daniel Richman",
+    author_email = "main@danielrichman.co.uk",
+    description = "Ucam-webauth and Raven application agent in Python",
+    license="GNU Lesser General Public License Version 3",
+    keywords = "Raven Cambridge ucam-webauth",
+    url = "http://github.com/danielrichman/python-raven",
+)
