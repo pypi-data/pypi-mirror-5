9be6c536a9e1198be24ee2636f006d52097f8573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:00 +0000

    Add package corsa/0.1.2/corsa-0.1.2.tar.gz

---
+++ packages/corsa/corsa-0.1.2.tar.gz/corsa-0.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='corsa',
+    version="0.1.2",
+    description='CORS proxy and web server for static apps',
+    author='Oliver Tonnhofer',
+    author_email='olt@bogosoft.com',
+    url='https://github.com/olt/corsa',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points = {
+        'console_scripts': [
+            'corsa = corsa.app:main',
+        ],
+    },
+    install_requires=[
+        'tornado',
+    ],
+)
