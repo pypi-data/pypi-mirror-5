6a01f3bbe76bc47c8bf6a62175cf8594c6d6d8b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:11 +0000

    Add package Flask-Triangle/0.5.1/Flask-Triangle-0.5.1.tar.gz

---
+++ packages/Flask-Triangle/Flask-Triangle-0.5.1.tar.gz/Flask-Triangle-0.5.1/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+
+    name='Flask-Triangle',
+    version='0.5.1',
+    author='Morgan Delahaye-Prat',
+    author_email='mdp@m-del.fr',
+    description=('Integration of AngularJS and Flask.'),
+    long_description=open('README.rst').read(),
+    packages=find_packages(),
+    zip_safe=False,
+    platforms='any',
+    install_requires=['flask', 'jsonschema'],
+    tests_require=['nose'],
+    url='https://github.com/morgan-del/flask-triangle',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
