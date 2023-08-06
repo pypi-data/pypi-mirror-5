48d3b4a81b6924a001ccb23ab5a1d96ef6bacc0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:38 +0000

    Add package dj-stripe/0.2.3/dj-stripe-0.2.3.tar.gz

---
+++ packages/dj-stripe/dj-stripe-0.2.3.tar.gz/dj-stripe-0.2.3/djstripe/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+
+__title__ = "dj-stripe"
+__summary__ = "Django + Stripe for humans "
+__uri__ = "https://github.com/pydanny/dj-stripe/"
+
+__version__ = "0.2.3"
+
+__author__ = "Daniel Greenfeld"
+__email__ = "pydanny@gmail.com"
+
+__license__ = "BSD"
+__copyright__ = "Copyright 2013 Daniel Greenfeld"
