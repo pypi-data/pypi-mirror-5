73472caea826883eb7267bd75fb55a84658b39b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:52 +0000

    Add package haigha/0.6.1/haigha-0.6.1.tar.gz

---
+++ packages/haigha/haigha-0.6.1.tar.gz/haigha-0.6.1/haigha/__init__.py
@@ -0,0 +1,7 @@
+'''
+Copyright (c) 2011-2013, Agora Games, LLC All rights reserved.
+
+https://github.com/agoragames/haigha/blob/master/LICENSE.txt
+'''
+
+__version__ = "0.6.1"
---
+++ packages/haigha/haigha-0.6.1.tar.gz/haigha-0.6.1/haigha/frames/__init__.py
@@ -0,0 +1,11 @@
+'''
+Copyright (c) 2011-2013, Agora Games, LLC All rights reserved.
+
+https://github.com/agoragames/haigha/blob/master/LICENSE.txt
+'''
+
+from frame import Frame
+from method_frame import MethodFrame
+from header_frame import HeaderFrame
+from content_frame import ContentFrame
+from heartbeat_frame import HeartbeatFrame
