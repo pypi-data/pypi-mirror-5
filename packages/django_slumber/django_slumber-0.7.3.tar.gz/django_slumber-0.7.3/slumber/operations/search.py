b581a34054dd89a139a2698c13c5baf53c271e94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:48 +0000

    Add package django_slumber/0.7.3/django_slumber-0.7.3.tar.gz

---
+++ packages/django_slumber/django_slumber-0.7.3.tar.gz/django_slumber-0.7.3/slumber/operations/search.py
@@ -0,0 +1,24 @@
+"""
+    Allows the data URL to be found for a given object.
+"""
+from django.http import HttpResponseNotFound
+
+from slumber.operations import ModelOperation
+from slumber.operations.instancedata import instance_data
+
+
+class DereferenceInstance(ModelOperation):
+    """Given a primary key (or other unique set of attributes) redirects
+    to the instance item.
+    """
+    def get(self, request, response, _appname, _modelname):
+        """Work out the correct data URL for an instance we're going to
+        search for.
+        """
+        try:
+            instance = self.model.model.objects.get(
+                **dict([(k, request.GET[k])
+                    for k in request.GET.keys()]))
+            return instance_data(response, self.model, instance)
+        except self.model.model.DoesNotExist:
+            return HttpResponseNotFound()
