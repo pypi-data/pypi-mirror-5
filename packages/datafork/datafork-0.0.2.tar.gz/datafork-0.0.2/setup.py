c17a26ece9f903d705086ea7e05fe5c83be847f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:12 +0000

    Add package datafork/0.0.2/datafork-0.0.2.tar.gz

---
+++ packages/datafork/datafork-0.0.2.tar.gz/datafork-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name="datafork",
+    version="0.0.2",
+    description="Forkable global state",
+    packages=['datafork'],
+    author="Martin Atkins",
+    author_email="mart@degeneration.co.uk",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+    ]
+)
