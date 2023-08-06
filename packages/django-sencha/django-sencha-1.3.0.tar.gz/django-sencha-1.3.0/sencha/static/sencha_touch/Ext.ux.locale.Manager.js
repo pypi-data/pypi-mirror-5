5aa9bf14240698ceec6eb14adae7e47bf4ed4ec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:10 +0000

    Add package django-sencha/1.3.0/django-sencha-1.3.0.tar.gz

---
+++ packages/django-sencha/django-sencha-1.3.0.tar.gz/django-sencha-1.3.0/sencha/static/sencha_touch/Ext.ux.locale.Manager.js
@@ -0,0 +1,36 @@
+Ext.Loader.setConfig({
+    enabled : true,
+    paths   : {
+        Ux : '/mastertek-api/sencha-touch-gpl/2.2.1/Ux'
+    }
+});
+
+Ext.require([
+    'Ext.Container',
+    'Ext.TitleBar',
+    'Ext.field.Select',
+    'Ext.field.DatePicker',
+    'Ext.tab.Panel',
+    'Ext.navigation.View',
+
+    'Ux.locale.Manager',
+    'Ux.locale.override.st.Component',
+    'Ux.locale.override.st.Button',
+    'Ux.locale.override.st.Container',
+    'Ux.locale.override.st.TitleBar',
+    'Ux.locale.override.st.field.Field',
+    'Ux.locale.override.st.field.DatePicker',
+    'Ux.locale.override.st.picker.Picker',
+    'Ux.locale.override.st.picker.Date',
+    'Ux.locale.override.st.navigation.View',
+    'Ux.locale.override.st.navigation.Bar'
+], function() {
+    Ux.locale.Manager.setConfig({
+        ajaxConfig : {
+            method : 'GET'
+        },
+        language   : 'it',
+        tpl        : 'app/locales/{locale}.json',
+        type       : 'ajax'
+    });
+});
