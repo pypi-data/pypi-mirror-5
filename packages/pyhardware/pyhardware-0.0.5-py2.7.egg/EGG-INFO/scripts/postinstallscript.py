1a2fbbaa38f8f36b1a78a7441fdfe859a7402b0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:27 +0000

    Add package pyhardware/0.0.5/pyhardware-0.0.5-py2.7.egg

---
+++ packages/pyhardware/pyhardware-0.0.5-py2.7.egg/EGG-INFO/scripts/postinstallscript.py
@@ -0,0 +1,18 @@
+#!C:\Python27\python.exe
+# -*- coding: utf-8 -*-
+
+import subprocess
+import shutil
+
+build_folder = os.path.join(os.environ['TEMP'],
+            'pip_build_' + os.environ['USERNAME'] + '_pyinstruments')
+if os.path.exists(build_folder):
+    shutil.rmtree(build_folder)
+subprocess.call(['pip', 
+                 'install',
+                 'pyivi', 
+                 '-I', 
+                 '-U', 
+                 '-b', 
+                 build_folder])
+
