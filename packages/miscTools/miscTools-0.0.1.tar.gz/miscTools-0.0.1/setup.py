55b32e8efba40965fbaa3f1268e9f85a563f1343
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:55 +0000

    Add package miscTools/0.0.1/miscTools-0.0.1.tar.gz

---
+++ packages/miscTools/miscTools-0.0.1.tar.gz/miscTools-0.0.1/setup.py
@@ -0,0 +1,15 @@
+import os
+from distutils.core import setup
+
+setup(
+    name='miscTools',
+    description='Tools for most projects',
+    version='0.0.1',
+    author="Borrey Kim",
+    author_email="borrey@gmail.com",
+    url="https://bitbucket.org/borreykim/misctools/",
+    keywords=['misc'],
+    long_description = """\
+    Currently tools are to parse arguments
+    """
+)
