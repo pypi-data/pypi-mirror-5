8938411202b755079982737216a6d3128b8b54ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:09 +0000

    Add package tld/0.3/tld-0.3.tar.gz

---
+++ packages/tld/tld-0.3.tar.gz/tld-0.3/src/tld/__init__.py
@@ -0,0 +1,7 @@
+__title__ = 'tld'
+__version__ = '0.2'
+__build__ = 0x000002
+__author__ = 'Artur Barseghyan'
+__all__ = ('get_tld',)
+
+from tld.utils import get_tld
---
+++ packages/tld/tld-0.3.tar.gz/tld-0.3/src/tld/update.py
@@ -0,0 +1,12 @@
+__title__ = 'tld.update'
+__version__ = '0.2'
+__build__ = 0x000002
+__author__ = 'Artur Barseghyan'
+
+from tld.utils import update_tld_names
+
+_ = lambda x: x
+
+if __name__ == '__main__':
+    update_tld_names()
+    print _("Local TLD names file has been successfully updated!")
