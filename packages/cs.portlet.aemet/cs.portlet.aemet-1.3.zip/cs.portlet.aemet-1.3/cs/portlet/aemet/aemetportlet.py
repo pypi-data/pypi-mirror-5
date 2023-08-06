c2c7e79e87283556a2b6fd2dd85d8947fe799460
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:03 +0000

    Add package cs.portlet.aemet/1.3/cs.portlet.aemet-1.3.zip

---
+++ packages/cs.portlet.aemet/cs.portlet.aemet-1.3.zip/cs.portlet.aemet-1.3/cs/portlet/aemet/aemetportlet.py
@@ -0,0 +1,101 @@
+from aemetparser import parseXML
+from cs.portlet.aemet import AEMETPortletMessageFactory as _
+from plone.app.portlets.portlets import base
+from plone.memoize.ram import cache
+from plone.portlets.interfaces import IPortletDataProvider
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope import schema
+from zope.formlib import form
+from zope.interface import implements
+
+
+class IAEMETPortlet(IPortletDataProvider):
+    """A portlet
+
+    It inherits from IPortletDataProvider because for this portlet, the
+    data that is being rendered and the portlet assignment itself are the
+    same.
+    """
+
+    portlet_title = schema.TextLine(
+        title=_(u"Title"),
+        description=_(u"Enter the title of the portlet"),
+        required=True,
+    )
+
+    url = schema.TextLine(
+        title=_(u"URL"),
+        description=_(u"Enter the URL of the XML file with the weather data"),
+        required=True,
+    )
+
+    daynumber = schema.Int(
+        title=_('Day number to show'),
+        description=_('The number of days to show in the portlet'),
+        required=True,
+    )
+
+
+class Assignment(base.Assignment):
+    """Portlet assignment.
+
+    This is what is actually managed through the portlets UI and associated
+    with columns.
+    """
+
+    implements(IAEMETPortlet)
+
+    def __init__(self, portlet_title=u"", url=u'', daynumber=0):
+        self.portlet_title = portlet_title
+        self.url = url
+        self.daynumber = daynumber
+
+    def title(self):
+        return self.portlet_title
+
+
+class Renderer(base.Renderer):
+    """Portlet renderer.
+
+    This is registered in configure.zcml. The referenced page template is
+    rendered, and the implicit variable 'view' will refer to an instance
+    of this class. Other methods can be added and referenced in the template.
+    """
+
+    render = ViewPageTemplateFile('aemetportlet.pt')
+
+    def title(self):
+        return self.data.portlet_title
+
+    def _render_cache_key(func, item):
+        return item.data.url
+
+    @cache(_render_cache_key)
+    def get_weather(self):
+        try:
+            data = parseXML(self.data.url)
+            return data[:self.data.daynumber]
+        except:
+            return []
+
+
+class AddForm(base.AddForm):
+    """Portlet add form.
+
+    This is registered in configure.zcml. The form_fields variable tells
+    zope.formlib which fields to display. The create() method actually
+    constructs the assignment that is being added.
+    """
+    form_fields = form.Fields(IAEMETPortlet)
+
+    def create(self, data):
+        return Assignment(**data)
+
+
+class EditForm(base.EditForm):
+    """Portlet edit form.
+
+    This is registered with configure.zcml. The form_fields variable tells
+    zope.formlib which fields to display.
+    """
+    form_fields = form.Fields(IAEMETPortlet)
---
+++ packages/cs.portlet.aemet/cs.portlet.aemet-1.3.zip/cs.portlet.aemet-1.3/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.3'
+
+setup(name='cs.portlet.aemet',
+      version=version,
+      description="A portlet to show the weather via AEMET",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.0",
+        "Framework :: Plone :: 4.1",
+        "Framework :: Plone :: 4.2",
+        "Framework :: Plone :: 4.3",
+        "Programming Language :: Python",
+        ],
+      keywords='weather portlet plone aemet',
+      author='Mikel Larreategi',
+      author_email='mlarreategi@codesyntax.com',
+      url='https://github.com/codesyntax/cs.portlet.aemet',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['cs', 'cs.portlet'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'lxml',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
