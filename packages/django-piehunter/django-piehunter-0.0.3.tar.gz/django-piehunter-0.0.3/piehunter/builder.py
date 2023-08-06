79d2e0b55d1768246f6633e7e967412ad14a8ccf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:19 +0000

    Add package django-piehunter/0.0.3/django-piehunter-0.0.3.tar.gz

---
+++ packages/django-piehunter/django-piehunter-0.0.3.tar.gz/django-piehunter-0.0.3/piehunter/builder.py
@@ -0,0 +1,13 @@
+from tastypie.api import Api
+
+from .discovery import autodiscover
+
+
+class PieHunter(object):
+    urlpatterns = None
+    _registry = {}
+
+    def __init__(self, api_name='v1', dotted_path='api.resources', enabled_attribute='EnabledResources'):
+        cage = Api(api_name)
+        autodiscover(dotted_path, enabled_attribute, cage)
+        self.urlpatterns = cage.urls
