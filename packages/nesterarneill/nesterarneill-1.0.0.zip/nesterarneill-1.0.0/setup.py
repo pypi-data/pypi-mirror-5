5ca53c53bbf0e8109090a9bb84bd2f2c51f4cee9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:56 +0000

    Add package nesterarneill/1.0.0/nesterarneill-1.0.0.zip

---
+++ packages/nesterarneill/nesterarneill-1.0.0.zip/nesterarneill-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(
+    name='nesterarneill',
+    version='1.0.0',
+    py_module=['nesterarneill'],
+    author='BigAl',
+    author_email='alanarneill@googlemail.com',
+    )
