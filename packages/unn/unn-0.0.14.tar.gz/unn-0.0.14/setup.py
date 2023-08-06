a5582dfbeafd73d808674cc6cf69ce2344b1eb87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:09 +0000

    Add package unn/0.0.14/unn-0.0.14.tar.gz

---
+++ packages/unn/unn-0.0.14.tar.gz/unn-0.0.14/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+requires = open('requirements.txt').read().split('\n')
+
+setup(
+    name = 'unn',
+    version = '0.0.14',
+    packages = ['unn',],
+    scripts = ['unn/unn'],
+    license = 'MIT',
+    install_requires = requires,
+    description = "The most minimal blog engine you've never heard of",
+    long_description = open('README.md').read(),
+)
