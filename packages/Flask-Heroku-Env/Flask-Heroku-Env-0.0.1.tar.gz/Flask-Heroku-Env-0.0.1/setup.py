e06b2347031ec0e1a0c1f9f20fb9840e86237bce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:54 +0000

    Add package Flask-Heroku-Env/0.0.1/Flask-Heroku-Env-0.0.1.tar.gz

---
+++ packages/Flask-Heroku-Env/Flask-Heroku-Env-0.0.1.tar.gz/Flask-Heroku-Env-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+setup(
+    name='Flask-Heroku-Env',
+    version='0.0.1',
+    url='https://github.com/cburmeister/flask-heroku-env',
+    license='MIT',
+    author='Corey Burmeister',
+    author_email='cburmeister@discogs.com',
+    description='Easily fetch Heroku environment variables.',
+    long_description=__doc__,
+    py_modules=['flask_heroku'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=['Flask'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
