fd11cf2f2169381edcd1ac9bc54371bf2b5c51a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:56 +0000

    Add package mad2/0.0.20/mad2-0.0.20.tar.gz

---
+++ packages/mad2/mad2-0.0.20.tar.gz/mad2-0.0.20/mad2/plugin/checksum.py
@@ -0,0 +1,63 @@
+from __future__ import print_function
+
+import logging
+import sys
+
+import os
+import leip
+import hashlib
+from mad2.util import get_all_mad_files
+
+lg = logging.getLogger(__name__)
+
+
+def hashit(hasher, filename):
+    """
+    Provde a quick & dirty hash
+
+    this is by no means secure, but quick for very large files, and as long
+    as one does not try to create duplicate hashes, the chance is still very
+    slim that a duplicate will arise
+    """
+    h = hasher()
+    with open(filename) as F:
+        h.update(F.read())
+    return h.hexdigest()
+
+
+@leip.arg('-f', '--force', action='store_true', help='apply force')
+@leip.arg('file', nargs='*')
+@leip.command
+def md5(app, args):
+    """
+    Calculate a checksum
+    """
+    for madfile in get_all_mad_files(app, args):
+        if not args.force and 'md5' in madfile.mad:
+            #exists - and not forcing
+            lg.warning("Skipping md5 checksum - exists")
+            continue
+        lg.info("Processing %s for md5sum" % madfile.filename)
+        cs = hashit(hashlib.md5, madfile.filename)
+        madfile.mad.hash.md5 = cs
+        madfile.save()
+        print(madfile.filename)
+
+@leip.arg('-f', '--force', action='store_true', help='apply force')
+@leip.arg('file', nargs='*')
+@leip.command
+def sha1(app, args):
+    """
+    Calculate a sha1 checksum
+    """
+    for madfile in get_all_mad_files(app, args):
+        if not args.force and 'sha1' in madfile.mad:
+            #exists - and not forcing
+            lg.warning("Skipping sha1 checksum - exists")
+            continue
+        cs = hashit(hashlib.sha1, madfile.filename)
+        madfile.mad.hash.sha1 = cs
+        madfile.save()
+        print(madfile.filename)
+
+
