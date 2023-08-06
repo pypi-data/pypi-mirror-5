bb5c914538b28fc86b0c51b05c55b91119fa0ce2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:54 +0000

    Add package django-knockout-modeler/0.4.8/django-knockout-modeler-0.4.8.tar.gz

---
+++ packages/django-knockout-modeler/django-knockout-modeler-0.4.8.tar.gz/django-knockout-modeler-0.4.8/knockout_modeler/templatetags/knockout.py
@@ -0,0 +1,58 @@
+dfrom django import template
+import simplejson as json
+import datetime
+from knockout_modeler.ko import ko, koData, koModel, get_fields
+
+register = template.Library()
+
+def knockout(values):
+    """
+    Knockoutify a QuerySet!
+    """
+
+    modelClass = values[0].__class__
+    if hasattr(modelClass, "knockout_fields"):
+        field_names = values[0].knockout_fields()
+    else:
+        try:
+            field_names = values[0].to_dict().keys()
+        except Exception, e:
+            field_names = values[0]._meta.fields
+
+    return ko(values, field_names)
+
+def knockout_data(values):
+    """
+
+    """
+
+    modelClass = values[0].__class__
+    if hasattr(modelClass, "knockout_fields"):
+        field_names = values[0].knockout_fields()
+    else:
+        try:
+            field_names = values[0].to_dict().keys()
+        except Exception, e:
+            field_names = values[0]._meta.fields
+
+    return koData(values, field_names)
+
+def knockout_model(values):
+    """
+
+    """
+
+    modelClass = values[0].__class__
+    if hasattr(modelClass, "knockout_fields"):
+        field_names = values[0].knockout_fields()
+    else:
+        try:
+            field_names = values[0].to_dict().keys()
+        except Exception, e:
+            field_names = values[0]._meta.fields
+
+    return koModel(modelClass, field_names)
+
+register.filter(knockout)
+register.filter(knockout_data)
+register.filter(knockout_model)
