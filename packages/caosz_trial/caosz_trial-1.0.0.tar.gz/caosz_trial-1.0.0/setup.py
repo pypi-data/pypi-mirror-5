9249824ad7740368fd710081b0ff429a21d96324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:13 +0000

    Add package caosz_trial/1.0.0/caosz_trial-1.0.0.tar.gz

---
+++ packages/caosz_trial/caosz_trial-1.0.0.tar.gz/caosz_trial-1.0.0/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+
+setup(
+    
+name = 'caosz_trial',
+    
+version = '1.0.0',
+    py_modules = ['caosz_trial'],
+    
+author = 'caosz',
+    author_email = 'caosz11@mails.tsinghua.edu.cn',
+    
+url = 'www.caosz.com',
+    
+description = ' Just for trial '
+    
+)
