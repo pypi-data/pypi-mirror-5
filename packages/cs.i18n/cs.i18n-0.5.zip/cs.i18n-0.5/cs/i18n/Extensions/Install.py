bbfdd99c75edc343d5467d85f283185d24572d49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:51 +0000

    Add package cs.i18n/0.5/cs.i18n-0.5.zip

---
+++ packages/cs.i18n/cs.i18n-0.5.zip/cs.i18n-0.5/cs/i18n/Extensions/Install.py
@@ -0,0 +1,27 @@
+import transaction
+from Products.CMFCore.utils import getToolByName
+
+PRODUCT_DEPENDENCIES = ('plone.browserlayer',
+                        )
+EXTENSION_PROFILES = ('cs.i18n:default',
+                      )
+
+
+def install(self, reinstall=False):
+    qi = getToolByName(self, 'portal_quickinstaller')
+    ps = getToolByName(self, 'portal_setup')
+    
+    for product in PRODUCT_DEPENDENCIES:
+        if reinstall and q.isProductInstalled(product):
+            qi.reinstallProducts([product])
+            transaction.savepoint()
+        elif not qi.isProductInstalled(product):
+            qi.installProducts([product])
+            transaction.savepoint()
+
+    for extension_id in EXTENSION_PROFILES:
+        ps.runAllImportStepsFromProfile('profile-%s' % extension_id,
+                                        purge_old=False)
+        product_name = extension_id.split(':')[0]
+        qi.notifyInstalled(product_name)
+        transaction.savepoint()
