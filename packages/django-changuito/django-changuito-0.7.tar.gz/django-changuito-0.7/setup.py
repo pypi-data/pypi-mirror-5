c508c7f238dcce5507e25b095ecd7c77e8c00c87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:06 +0000

    Add package django-changuito/0.7/django-changuito-0.7.tar.gz

---
+++ packages/django-changuito/django-changuito-0.7.tar.gz/django-changuito-0.7/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='django-changuito',
+    version='0.7',
+    description='A fork of django-cart with the same simplicity but up-to-date',
+    maintainer='Angel Velasquez',
+    maintainer_email='angvp@archlinux.org',
+    license="LGPL v3",
+    url='https://github.com/angvp/django-changuito',
+    packages=['changuito'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+
+    test_suite="runtests.runtests",
+)
