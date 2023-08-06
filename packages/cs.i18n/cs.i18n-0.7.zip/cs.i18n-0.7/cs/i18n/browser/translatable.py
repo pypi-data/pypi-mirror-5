5b7338a11a7ea52e33f33d42870d0e34afabb2bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:52 +0000

    Add package cs.i18n/0.7/cs.i18n-0.7.zip

---
+++ packages/cs.i18n/cs.i18n-0.7.zip/cs.i18n-0.7/cs/i18n/browser/translatable.py
@@ -0,0 +1,19 @@
+try:
+    from Products.LinguaPlone.browser.selector import TranslatableLanguageSelector as LSBase
+except ImportError:
+    from plone.app.i18n.locales.browser.selector import LanguageSelector as LSBase
+
+from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
+
+from zope.i18n.interfaces import IUserPreferredLanguages
+
+class TranslatableLanguageSelector(LSBase):
+    render = ZopeTwoPageTemplateFile('templates/translatablelanguageselector.pt')
+
+    def languages(self):
+        """ Return languages sorted according to their native name """
+        results = LSBase.languages(self)
+        import pdb;pdb.set_trace()
+        
+        return sorted(results, lambda x,y:cmp(x['native'],y['native']))
+
