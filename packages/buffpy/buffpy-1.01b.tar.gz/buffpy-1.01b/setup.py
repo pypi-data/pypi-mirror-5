7616f3c545135cfce1cb89862d79a7e5d9c2bcc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:08 +0000

    Add package buffpy/1.01b/buffpy-1.01b.tar.gz

---
+++ packages/buffpy/buffpy-1.01b.tar.gz/buffpy-1.01b/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as file:
+  long_description = file.read()
+
+requires_list = [
+  'argparse>=1.2.1',
+  'colorama>=0.2.5',
+  'coverage>=3.6',
+  'mock>=1.0.1',
+  'nose>=1.3.0',
+  'rauth>=0.5.5',
+  'requests>=1.2.3',
+  'wsgiref>=0.1.2']
+
+setup(name='buffpy',
+      version='1.01b',
+      platforms='any',
+      description='Python library for Buffer App',
+      author='Vlad Temian',
+      author_email='vladtemian@gmail.com',
+      url='https://github.com/vtemian/buffpy',
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=requires_list,
+      long_description = long_description
+     )
