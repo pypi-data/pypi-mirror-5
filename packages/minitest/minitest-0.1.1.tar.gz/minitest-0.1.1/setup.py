364ada4957aff692448e9f084cec48970fc5b7f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:36 +0000

    Add package minitest/0.1.1/minitest-0.1.1.tar.gz

---
+++ packages/minitest/minitest-0.1.1.tar.gz/minitest-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='minitest',
+    version='0.1.1',
+    author='Colin Ji',
+    author_email='jichen3000@gmail.com',
+    packages=['minitest'],
+    description='Simple test inspring by Ruby minispec.',
+    long_description=open('README.txt').read(),
+)
