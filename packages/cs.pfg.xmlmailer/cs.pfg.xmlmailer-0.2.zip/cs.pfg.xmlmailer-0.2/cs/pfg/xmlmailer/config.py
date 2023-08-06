33fdf2f06bc02c91faa01c4a82b1ab0933139650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:00 +0000

    Add package cs.pfg.xmlmailer/0.2/cs.pfg.xmlmailer-0.2.zip

---
+++ packages/cs.pfg.xmlmailer/cs.pfg.xmlmailer-0.2.zip/cs.pfg.xmlmailer-0.2/cs/pfg/xmlmailer/config.py
@@ -0,0 +1,7 @@
+PROJECTNAME = 'cs.pfg.xmlmailer'
+
+ADD_PERMISSIONS = {
+    # -*- extra stuff goes here -*-
+    'XMLMailerAdapter': 'cs.pfg.xmlmailer: Add XML Mailer Adapter',
+    #'XMLMailerAdapter': 'PloneFormGen: Add Content',
+}
---
+++ packages/cs.pfg.xmlmailer/cs.pfg.xmlmailer-0.2.zip/cs.pfg.xmlmailer-0.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.2'
+
+setup(name='cs.pfg.xmlmailer',
+      version=version,
+      description="Send by e-mail an XML file representing the form data together with the e-mail",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Lur Ibargutxi',
+      author_email='libargutxi@codesyntax.com',
+      url='http://svn.plone.org/svn/plone/plone.app.example',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['cs', 'cs.pfg'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
