54dab221adc6a58c331be22b7748ff61f03082e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:18 +0000

    Add package sphinxcontrib-bibtex/0.2.9/sphinxcontrib-bibtex-0.2.9.zip

---
+++ packages/sphinxcontrib-bibtex/sphinxcontrib-bibtex-0.2.9.zip/sphinxcontrib-bibtex-0.2.9/setup.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import codecs
+import sys
+
+
+def readfile(filename):
+    with codecs.open(filename, encoding="utf-8") as stream:
+        return stream.read().split("\n")
+
+doclines = readfile("README.rst")
+requires = readfile("requirements.txt")
+version = readfile("VERSION")[0].strip()
+
+if sys.version_info < (2, 7):
+    requires.append('ordereddict>=1.1')
+
+setup(
+    name='sphinxcontrib-bibtex',
+    version=version,
+    url='https://github.com/mcmtroffaes/sphinxcontrib-bibtex',
+    download_url='http://pypi.python.org/pypi/sphinxcontrib-bibtex',
+    license='BSD',
+    author='Matthias C. M. Troffaes',
+    author_email='matthias.troffaes@gmail.com',
+    description=doclines[0],
+    long_description="\n".join(doclines[2:]),
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Documentation',
+        'Topic :: Utilities',
+    ],
+    platforms='any',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requires,
+    namespace_packages=['sphinxcontrib'],
+    use_2to3=True,
+)
---
+++ packages/sphinxcontrib-bibtex/sphinxcontrib-bibtex-0.2.9.zip/sphinxcontrib-bibtex-0.2.9/test/test_issue2.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+    test_issue2
+    ~~~~~~~~~~~
+
+    Test mixing of ``:cite:`` and ``[]_``.
+"""
+
+import nose.tools
+
+from util import path, with_app
+
+srcdir = path(__file__).parent.joinpath('issue2').abspath()
+
+
+def teardown_module():
+    (srcdir / '_build').rmtree(True)
+
+
+@with_app(srcdir=srcdir, warningiserror=True)
+def test_mixing_citation_styles(app):
+    app.builder.build_all()
+    nose.tools.assert_true(app.env.bibtex_cache.is_cited(u"Test"))
+    nose.tools.assert_equal(
+        app.env.bibtex_cache.get_label_from_key(u"Test"), u"1")
