f7da63bd49d7bd7443e5c6345c586336e386012c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:34 +0000

    Add package tiddlywebplugins.ibuilder/0.1.4/tiddlywebplugins.ibuilder-0.1.4.tar.gz

---
+++ packages/tiddlywebplugins.ibuilder/tiddlywebplugins.ibuilder-0.1.4.tar.gz/tiddlywebplugins.ibuilder-0.1.4/test/test_cache.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+
+from tiddlywebplugins.ibuilder import cache_tiddlers
+
+
+def setup_module(module):
+    try:
+        shutil.rmtree('testpackage/resources')
+    except:
+        pass
+
+
+def test_cache():
+    cache_tiddlers('testpackage')
+
+    assert os.path.exists('testpackage/resources/store')
+    assert os.path.exists('testpackage/resources/store/bags/bagone/tiddlers/')
+    assert os.path.exists('testpackage/resources/store/bags/bagone/tiddlers/test')
+    assert os.path.exists('testpackage/resources/store/bags/bagone/tiddlers/file.css')
