5042b14017c99b7593e414cc545b9d8351c351ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:01 +0000

    Add package scenariobuilder/0.1.5/scenariobuilder-0.1.5.tar.gz

---
+++ packages/scenariobuilder/scenariobuilder-0.1.5.tar.gz/scenariobuilder-0.1.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='scenariobuilder',
+    version='0.1.5',
+    packages=['scenariobuilder'],
+    license='Apache',
+    url='https://github.com/michaeltchapman/scenariobuilder',
+    install_requires=['python-novaclient', 'python-neutronclient', 'PyYaml', 'Jinja2'],
+    scripts=['bin/sb'],
+    include_package_data=True
+)
+
+
