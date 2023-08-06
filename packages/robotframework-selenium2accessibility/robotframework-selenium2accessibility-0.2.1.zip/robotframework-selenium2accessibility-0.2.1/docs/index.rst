ba0f0f7991e90d5af7f187363a4f3a9fa3880175
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:52 +0000

    Add package robotframework-selenium2accessibility/0.2.1/robotframework-selenium2accessibility-0.2.1.zip

---
+++ packages/robotframework-selenium2accessibility/robotframework-selenium2accessibility-0.2.1.zip/robotframework-selenium2accessibility-0.2.1/docs/index.rst
@@ -0,0 +1,23 @@
+Robot Framework resources for automating accessibility tools
+============================================================
+
+Libraries
+---------
+
+.. toctree::
+   :maxdepth: 2
+
+   wavetoolbar
+   contrastchecker
+
+Common keywords
+---------------
+
+.. robot_keywords::
+   :source: Accessibility:accessibility.robot
+
+Source
+------
+
+.. robot_source::
+   :source: Accessibility:accessibility.robot
---
+++ packages/robotframework-selenium2accessibility/robotframework-selenium2accessibility-0.2.1.zip/robotframework-selenium2accessibility-0.2.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="robotframework-selenium2accessibility",
+    version='0.2.1',
+    description="Robot Framework resources for automating accessibility tools",
+    long_description=(open("README.rst").read() + "\n" +
+                      open("CHANGES.txt").read()),
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Programming Language :: Python",
+    ],
+    keywords="",
+    author="Asko Soukka",
+    author_email="asko.soukka@iki.fi",
+    url="https://github.com/datakurre/robotframework-selenium2accessibility/",
+    license="GPL",
+    packages=find_packages("src", exclude=["ez_setup"]),
+    package_dir={"": "src"},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "setuptools",
+        "robotframework",
+        "robotframework-selenium2library",
+    ],
+    extras_require={"docs": [
+        "sphinxcontrib-robotdoc",
+        "sphinx"
+    ]},
+)
