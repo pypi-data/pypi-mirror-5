907fa6a0b649338ad75e1739da9890934c2e5e8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:48 +0000

    Add package eea.facetednavigation/5.9/eea.facetednavigation-5.9.zip

---
+++ packages/eea.facetednavigation/eea.facetednavigation-5.9.zip/eea.facetednavigation-5.9/eea/facetednavigation/syndication/feed.py
@@ -0,0 +1,16 @@
+""" Feed
+"""
+from zope import component
+from Products.CMFPlone.browser.syndication.adapters import FolderFeed
+
+
+class FacetedFeed(FolderFeed):
+    """ Feed
+    """
+    def _brains(self):
+        request = getattr(self.context, 'REQUEST',
+                          getattr(self.context, 'request', None))
+        handler = component.getMultiAdapter((self.context, request),
+                                            name=u'faceted_query')
+
+        return handler.query(ajax=False)
