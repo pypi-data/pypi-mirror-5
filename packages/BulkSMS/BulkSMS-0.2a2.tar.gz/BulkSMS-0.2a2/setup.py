ea60987be62289dc30bb4e88ef0bb68b36cde0a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:43 +0000

    Add package BulkSMS/0.2a2/BulkSMS-0.2a2.tar.gz

---
+++ packages/BulkSMS/BulkSMS-0.2a2.tar.gz/BulkSMS-0.2a2/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="BulkSMS",
+	version="0.2a2",
+	description="BulkSMS.co.uk Python implementation.",
+	author="David Wilson",
+	license='LGPL',
+	url="http://code.google.com/p/py-sms/",
+	packages=['BulkSMS'],
+	scripts=['sms']
+)
