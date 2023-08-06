0c2b604e56beabc6e358faac98f5fe768d7b1ebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:23 +0000

    Add package tornadohttpclient/0.1/tornadohttpclient-0.1.tar.gz

---
+++ packages/tornadohttpclient/tornadohttpclient-0.1.tar.gz/tornadohttpclient-0.1/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import sys
+from distutils.core import setup
+
+if sys.version_info < (2,7) or sys.version_info >= (3, 0):
+    raise NotImplementedError("Sorry, you need at least Python 2.7 to use tornadohttpclient.")
+
+
+requires = ['tornado']
+
+setup(name='tornadohttpclient',
+      version= '0.1',
+      description='Asynchronous http client.',
+      long_description="TornadoHTTPClient 是一个基于Tornado的高效的异步HTTP客户端库, 支持Cookie和代理",
+      author='cold',
+      author_email='wh_linux@126.com',
+      url='http://www.linuxzen.com',
+      py_modules=['tornadohttpclient'],
+      scripts=['tornadohttpclient.py'],
+      license='Apache 2.0',
+      platforms = 'any',
+      classifiers=['Development Status :: 3 - Alpha',
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Internet :: WWW/HTTP',
+        'Programming Language :: Python :: 2.7',
+        ],
+     )
