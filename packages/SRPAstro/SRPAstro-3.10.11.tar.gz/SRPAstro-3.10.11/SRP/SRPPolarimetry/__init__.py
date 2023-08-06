45777990d7c21f4434475838c056f2937fac67f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:23 +0000

    Add package SRPAstro/3.10.11/SRPAstro-3.10.11.tar.gz

---
+++ packages/SRPAstro/SRPAstro-3.10.11.tar.gz/SRPAstro-3.10.11/SRP/SRPPolarimetry/__init__.py
@@ -0,0 +1,23 @@
+""" Init file for SRPPolarimetry
+
+Context : SRP
+Module  : Polarimetry
+Version : 1.1.0
+Author  : Stefano Covino
+Date    : 29/02/2012
+E-mail  : stefano.covino@brera.inaf.it
+URL     : http://www.me.oa-brera.inaf.it/utenti/covino
+
+Usage   : to be imported
+
+History : (21/02/2012) First named version.
+        : (29/02/2012) Wave plate matrix added.
+"""
+
+
+__all__ = ['AluminiumRefractiveIndex', 'MuellerMetallicMirrorMatrix', 'MuellerRotationMatrix',
+            'MuellerTransmissionMatrix', 'MuellerHalfWavePlateMatrix', 'MuellerQuarterWavePlateMatrix',
+           'MuellerWavePlateMatrix', 'Pol2Stokes', 'Stokes2Pol']
+
+
+
