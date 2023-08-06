0b7d1df034f62cb1b3c2eb726bda34f7f95953e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:27 +0000

    Add package expiringdict/1.0/expiringdict-1.0.tar.gz

---
+++ packages/expiringdict/expiringdict-1.0.tar.gz/expiringdict-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='expiringdict',
+      description='Expiring Dictionary',
+      author='Mailgun (Rackspace)',
+      author_email='admin@mailgunhq.com',
+      version='1.0',
+      py_modules=['expiringdict'],
+      zip_safe=True,
+      )
