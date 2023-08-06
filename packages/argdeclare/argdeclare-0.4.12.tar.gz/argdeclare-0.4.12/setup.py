f84403e3e814c73b4fac58564ff6acfdec26eb8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:57 +0000

    Add package argdeclare/0.4.12/argdeclare-0.4.12.tar.gz

---
+++ packages/argdeclare/argdeclare-0.4.12.tar.gz/argdeclare-0.4.12/setup.py
@@ -0,0 +1,24 @@
+import sys
+from distutils.core import setup
+
+version = '0.4.12'
+
+with open("LICENSE", 'r') as f:
+    LICENSE = f.read()
+
+setup(name="argdeclare",
+      version=version,
+      description="Argdeclare is a declarative argument configurator for python's argparse.",
+      classifiers=["Intended Audience :: Developers",
+                   "License :: OSI Approved :: Python Software Foundation License",
+                   "Programming Language :: Python",
+                   "Programming Language :: Python :: 3",
+                   "Topic :: Software Development :: Libraries :: Python Modules",
+                   ],
+      author="Parnell Springmeyer",
+      author_email="parnell@ixmat.us",
+      url="https://github.com/ixmatus/argdeclare",
+      license=LICENSE,
+      zip_safe=False,
+      include_package_data=True
+      )
