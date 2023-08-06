6ce4cde78e1e6ced1e3e610bc11f55054e3fc1c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:50 +0000

    Add package sonnet/0.1.1/sonnet-0.1.1.tar.gz

---
+++ packages/sonnet/sonnet-0.1.1.tar.gz/sonnet-0.1.1/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='sonnet',
+    version='0.1.1',
+    author='David Michael Brown',
+    author_email='davidmichaelbrown1@gmail.com',
+    packages=['sonnet', ],
+    url='myurl',
+    license='MIT',
+    long_description=open('README.txt').read(),
+    install_requires=['networkx==1.8.1'],
+    keywords='networkx json graph javascript D3',
+    classifiers=[]
+)
