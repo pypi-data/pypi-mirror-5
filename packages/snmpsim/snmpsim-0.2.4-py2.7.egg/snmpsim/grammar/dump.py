cd66915406a6dcb1de87a98e3ae7c2c719c0cdac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:55 +0000

    Add package snmpsim/0.2.4/snmpsim-0.2.4-py2.7.egg

---
+++ packages/snmpsim/snmpsim-0.2.4-py2.7.egg/snmpsim/grammar/dump.py
@@ -0,0 +1,43 @@
+from pysnmp.proto import rfc1902
+from pyasn1.type import univ
+from pyasn1.compat.octets import octs2str
+from snmpsim.grammar import abstract
+from snmpsim import error
+
+class DumpGrammar(abstract.AbstractGrammar):
+    tagMap = {
+        '0': rfc1902.Counter32,
+        '1': rfc1902.Gauge32,
+        '2': rfc1902.Integer32,
+        '3': rfc1902.IpAddress,
+        '4': univ.Null,
+        '5': univ.ObjectIdentifier,
+        '6': rfc1902.OctetString,
+        '7': rfc1902.TimeTicks,
+        '8': rfc1902.Counter32,  # an alias
+        '9': rfc1902.Counter64,
+    }
+
+    def __nullFilter(value):
+        return '' # simply drop whatever value is there when it's a Null
+    
+    def __unhexFilter(value):
+        if value[:5].lower() == 'hex: ':
+            value = [ int(x, 16) for x in value[5:].split('.') ]
+        elif value[0] == '"' and value[-1] == '"':
+            value = value[1:-1]
+        return value
+
+    filterMap = {
+        '4': __nullFilter,
+        '6': __unhexFilter
+    }
+
+    def parse(self, line):
+        try:
+            oid, tag, value = octs2str(line).split('|', 2)
+        except:
+            raise error.SnmpsimError('broken record <%s>' % line)
+        else:
+            return oid, tag, self.filterMap.get(tag, lambda x: x)(value.strip())
+
