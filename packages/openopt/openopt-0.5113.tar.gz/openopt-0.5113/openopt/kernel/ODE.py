48c244649a64f6d70ee96e26469c81cf30c3c7b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:22 +0000

    Add package openopt/0.5113/openopt-0.5113.tar.gz

---
+++ packages/openopt/openopt-0.5113.tar.gz/openopt-0.5113/openopt/kernel/ODE.py
@@ -0,0 +1,27 @@
+from baseProblem import NonLinProblem
+
+
+class ODE(NonLinProblem):
+    probType = 'ODE'
+    goal = 'solution'
+    allowedGoals = ['solution']
+    showGoal = False
+    _optionalData = []
+    FuncDesignerSign = 'equations'
+    expectedArgs = ['equations', 'startPoint', 'times']
+    ftol = None
+    reltol = 1e-3
+    def __init__(self, *args, **kwargs):
+        NonLinProblem.__init__(self, *args, **kwargs)
+        domain= args[1]
+        self.x0 = domain
+#        if any(diff(times) < 0): self.err('''
+#        currently required ODE times should be sorted 
+#        in ascending order, other cases are unimplemented yet
+#        ''')
+        
+        #self.constraints = [timeVariable > times[0], timeVariable < times[-1]]
+        
+
+    def objFunc(self, x):
+        return 0
