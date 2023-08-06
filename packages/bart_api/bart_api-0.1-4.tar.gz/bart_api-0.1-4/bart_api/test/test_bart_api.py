a34ebfa72eab46496fd941fd6f9598f0af2cd869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:12 +0000

    Add package bart_api/0.1-4/bart_api-0.1-4.tar.gz

---
+++ packages/bart_api/bart_api-0.1-4.tar.gz/bart_api-0.1-4/bart_api/test/test_bart_api.py
@@ -0,0 +1,37 @@
+import unittest
+from unittest.mock import patch, Mock
+from bart_api import BartApi
+import io
+
+class TestBartApi(unittest.TestCase):
+
+  def setUp(self):
+    self.bart = BartApi()
+  
+  def test_api_key(self):
+    expected = "MW9S-E7SL-26DU-VV8V"
+    actual = self.bart.api_key
+    self.assertEqual(expected, actual)
+
+  @patch('bart_api.urllib.request.urlopen')
+  def test_number_of_trains(self,mock_urlopen):
+    a = Mock()
+    with open('bart_api/test/mocks/train_count.xml') as f:
+      raw_xml = f.read()
+    mock_urlopen.return_value = raw_xml.encode('utf-8')
+    expected=str(51)
+    actual = self.bart.number_of_trains()
+    self.assertEqual(expected, actual)
+
+  @patch('bart_api.urllib.request.urlopen')
+  def test_elevator_status(self,mock_urlopen):
+    a = Mock()
+    with open('bart_api/test/mocks/elev.xml') as f:
+      raw_xml = f.read().encode('utf-8')
+    mock_urlopen.return_value = raw_xml
+    expected=" Attention passengers: All elevators are in service. Thank You. "
+    actual = self.bart.elevator_status()
+    self.assertEqual(expected, actual)
+  
+if __name__ == '__main__':
+  unittest.main()
