873032f675b46eeda947fc78b2eb5f27893d84cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:24 +0000

    Add package pyramid-breve/0.6.4dev/pyramid_breve-0.6.4dev.tar.bz2

---
+++ packages/pyramid-breve/pyramid_breve-0.6.4dev.tar.bz2/pyramid_breve-0.6.4dev/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+version = '0.6.4dev'
+
+setup(name='pyramid_breve',
+      version=version,
+      description="Breve template engine renderer for Pyramid",
+      long_description="""Just call config.include('pyramid_breve') and use
+      Breve templates as Pyramid view renderers""",
+      keywords='pyramid breve',
+      author='Alexander Kulakov',
+      author_email='homo.programmerus@gmail.com',
+      url='http://github.com/momyc/pyramid-breve',
+      license='MIT',
+      packages=['pyramid_breve'],
+      test_suite='pyramid_breve.tests',
+      install_requires=['breve', 'pyramid>=1.3', 'zope.interface'],
+      )
