d1600f1fa45d7eeafeca757618048d483fecb2c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:18 +0000

    Add package django-query-exchange/0.5/django-query-exchange-0.5.tar.gz

---
+++ packages/django-query-exchange/django-query-exchange-0.5.tar.gz/django-query-exchange-0.5/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="django-query-exchange",
+    version="0.5",
+
+    license="New BSD License",
+
+    author='Alex Koshelev',
+    author_email="daevaorn@gmail.com",
+
+    url="http://github.com/daevaorn/django-query-exchange/",
+
+    packages=[
+        'query_exchange',
+        'query_exchange.templatetags',
+    ],
+
+    description="Django application for handling GET query params for url creation",
+
+    classifiers=[
+        "Framework :: Django",
+        "License :: OSI Approved :: BSD License",
+    ]
+)
