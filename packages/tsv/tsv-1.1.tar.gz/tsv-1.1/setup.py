1d3f9b3edcc1db30bdafbc55c03eafde5870ae8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:05 +0000

    Add package tsv/1.1/tsv-1.1.tar.gz

---
+++ packages/tsv/tsv-1.1.tar.gz/tsv-1.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# Setup script to install the PyBC generic blockchain library. Copied from the
+# example distutils script.
+
+from setuptools import setup
+
+setup(name='tsv',
+      version='1.1',
+      description='Tab-Separated-Value IO Library',
+      author='Adam Novak',
+      author_email='anovak@soe.ucsc.edu',
+      py_modules=['tsv'],
+     )
