f0baa40a04ccc1e472f97a923930fae474ed8430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:33 +0000

    Add package marionette_client/0.5.36/marionette_client-0.5.36.tar.gz

---
+++ packages/marionette_client/marionette_client-0.5.36.tar.gz/marionette_client-0.5.36/marionette/test_report.py
@@ -0,0 +1,32 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import unittest
+
+from marionette_test import MarionetteTestCase
+
+
+class TestReport(MarionetteTestCase):
+
+    def test_pass(self):
+        assert True
+
+    def test_fail(self):
+        assert False
+
+    @unittest.skip('Skip Message')
+    def test_skip(self):
+        assert False
+
+    @unittest.expectedFailure
+    def test_expected_fail(self):
+        assert False
+
+    @unittest.expectedFailure
+    def test_unexpected_pass(self):
+        assert True
+
+    def test_error(self):
+        raise Exception()
+
