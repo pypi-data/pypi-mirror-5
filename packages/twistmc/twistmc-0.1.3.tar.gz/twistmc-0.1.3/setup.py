5d4287fc479f0330df2d5f07686931d5e7d6bb50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:28 +0000

    Add package twistmc/0.1.3/twistmc-0.1.3.tar.gz

---
+++ packages/twistmc/twistmc-0.1.3.tar.gz/twistmc-0.1.3/setup.py
@@ -0,0 +1,23 @@
+"""
+Setup configuration.
+"""
+
+import setuptools
+
+
+setuptools.setup(
+    name="twistmc",
+    version="0.1.3",
+    author="Pierre Jaury",
+    author_email="pierre@jaury.eu",
+    description="Twist My Components!",
+    long_description=open("README.txt").read(),
+    license="GPLv3",
+    url="https://github.com/kaiyou/twistmc",
+    py_modules=["twistmc"],
+    install_requires=[
+        "python-breadcrumbs >= 0.1.1",
+        "zope.interface >= 3.6",
+        "Twisted >= 12.0"
+    ]
+)
