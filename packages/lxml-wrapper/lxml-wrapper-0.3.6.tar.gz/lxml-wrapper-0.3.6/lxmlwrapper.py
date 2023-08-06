cf19e50864d527797517abb61842817428d6c422
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:28 +0000

    Add package lxml-wrapper/0.3.6/lxml-wrapper-0.3.6.tar.gz

---
+++ packages/lxml-wrapper/lxml-wrapper-0.3.6.tar.gz/lxml-wrapper-0.3.6/lxmlwrapper.py
@@ -0,0 +1,157 @@
+# -*- coding: utf-8 -*-
+
+"""
+Now you can simplify your xml generation code.
+
+Example XML:
+
+<root atr="100">
+    text1
+    <child atr="atr">
+        <superchild atr="">sctext1</superchild>
+    tail1
+    tail2
+    </child>
+    tail
+    <child atr="">text</child>
+</root>
+
+Old way:
+
+root = Element('root', atr=str(100))
+root.text = 'text1'
+child = SubElement(root, 'child', atr="atr")
+superchild = SubElement(root, 'superchild', atr="" if value is None else value)
+superchild.text = 'sctext1'
+superchild.tail = 'tail1'
+superchild.tail += 'tail2'
+child.tail = 'tail'
+child = SubElement(root, 'child', atr="")
+child.text = 'text'
+
+New way:
+
+root = E('root', atr=100).add(
+           'text1',
+           E('child', atr="atr").add(
+               E('superchild', atr=None).add('sctext1'),
+               'tail1',
+               'tail2'
+           ),
+           'tail',
+           E('child', atr="").add(
+               'text'
+           )
+       )
+"""
+
+__VERSION__ = '0.3.5'
+
+from lxml import etree
+
+
+def E(_tag, attrib=None, nsmap=None, **_extra):
+    return _make_elem(_tag, attrib, nsmap, **_extra)
+
+
+def SE(elem, _tag, attrib=None, nsmap=None, **_extra):
+    e = _make_elem(_tag, attrib, nsmap, **_extra)
+    elem.append(e)
+    return e
+
+
+def _make_elem(_tag, attrib=None, nsmap=None, **_extra):
+    for k, v in _extra.items():
+        if isinstance(v, (int, long, float)):
+            v = str(v)
+        elif v is None:
+            v = ''
+        _extra[k] = v
+    _E.TAG = _tag
+    return _E(**_extra)
+
+
+#
+# Main magick class
+#
+class _E(etree.ElementBase):
+
+    def add_if(self, condition, * elements):
+        """Conditional add.
+
+        Add given elements only if condition is true.
+        """
+        if condition:
+            return self.add(* elements)
+        else:
+            return self
+
+    def add_for(self, iterable, fun=lambda item: item):
+        self.add(*[fun(item) for item in iterable])
+        return self
+
+    def add(self, * elements):
+        """You can add ElementTrees or strings or even integers now."""
+
+        if not elements:  # .add()
+            return self
+
+        prev_elem = None
+        text_elems = []
+
+        for elem in elements:
+            if elem is None:  # we don't like nones
+                continue
+
+            if isinstance(elem, (basestring)) and elem == '':
+                continue
+
+            if not isinstance(elem, (basestring, int, long, float)):
+                # we have elemettree
+                if prev_elem is not None:
+                    # because we have previous elem
+                    # we could append it now
+                    if text_elems:
+                        prev_elem.tail = "".join(text_elems)
+                    self.append(prev_elem)
+                    text_elems = []
+                else:
+                    # we haven't elementtree yet
+                    # so we shoud join together text_elems
+                    # assign them to self.text
+                    # and clear
+                    if text_elems:
+                        self.text = "".join(text_elems)
+                    text_elems = []
+                prev_elem = elem
+            else:
+                # we have some text
+                # so we can store it for later
+                if isinstance(elem, (int, long, float)):
+                    elem = str(elem)
+                text_elems.append(elem)
+        else:
+            if elem is None:  # we don't like nones
+                return self
+
+            if isinstance(elem, (basestring)) and elem == '':
+                return self
+
+            if not isinstance(elem, (basestring, int, long, float)):
+                # last element isn't string
+                # so we shoud append it
+                self.append(elem)
+            else:
+                # last element isn't element tree
+                # so we should do something
+                if prev_elem is not None:
+                    if text_elems:
+                        prev_elem.tail = "".join(text_elems)
+                    self.append(prev_elem)
+                    text_elems = []
+                else:
+                    if text_elems:
+                        self.text = "".join(text_elems)
+                    text_elems = []
+
+        return self
---
+++ packages/lxml-wrapper/lxml-wrapper-0.3.6.tar.gz/lxml-wrapper-0.3.6/setup.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+# http://pypi.python.org/pypi?%3Aaction=list_classifiers
+setup(name='lxml-wrapper',
+      description='lxml wrapper that simplifies xml generation code.',
+      keywords='lxml wrapper dsl',
+      version='0.3.6',
+      license='BSD',
+      url='http://github.com/matee911/lxml-wrapper', # home page for the package
+      download_url='https://github.com/matee911/lxml-wrapper/archive/0.3.6.tar.gz',
+      author='Mateusz `matee` Pawlik',
+      author_email='matee@matee.net',
+      long_description='''
+      This wrapper simplifies your Python xml generation code.
+
+      Example XML:
+
+      <root attrib="10">
+        text
+        <child attrib="">
+          childtext
+        </child>
+        tail
+      </root>
+
+      New way:
+
+      E('root', attrib=10).add(
+          'text',
+          E('child', attrib=None).add(
+              'childtext'),
+          'tail')
+
+      Old way:
+
+      root = Element('root', attrib=str(10)) # cast to str
+      root.text = 'text'
+      child = SubElement(root, 'child', attrib=value or "") # change None to empty string
+      child.text = 'childtext'
+      child.tail = 'tail'
+
+
+      Now with .add_if and .add_for methods:
+
+      E('root').add_if(1==1, E('child')) -> <root><child /></root>
+
+      E('root').add_if(1==0, E('child')) -> <root/>
+
+      E('root').add_for([1,2], lambda item: E('item', attr=item)) -> <root><item attr="1"/><item attr="2"/></root>
+
+      ''',
+      requires=['lxml'],
+      #package_dir={'':'src'},
+      py_modules=['lxmlwrapper'],
+      platforms=['Any'],
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: PyPy',
+      ],)
+
