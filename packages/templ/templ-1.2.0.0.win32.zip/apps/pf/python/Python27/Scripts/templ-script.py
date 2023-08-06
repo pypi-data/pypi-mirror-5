dfb74f035ceb5e675e4b0c048498c267ae40650e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:13 +0000

    Add package templ/1.2.0.0/templ-1.2.0.0.win32.zip

---
+++ packages/templ/templ-1.2.0.0.win32.zip/apps/pf/python/Python27/Scripts/templ-script.py
@@ -0,0 +1,9 @@
+#!D:\apps\pf\python\Python27\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'templ==1.2.0.0','console_scripts','templ'
+__requires__ = 'templ==1.2.0.0'
+import sys
+from pkg_resources import load_entry_point
+
+sys.exit(
+   load_entry_point('templ==1.2.0.0', 'console_scripts', 'templ')()
+)
