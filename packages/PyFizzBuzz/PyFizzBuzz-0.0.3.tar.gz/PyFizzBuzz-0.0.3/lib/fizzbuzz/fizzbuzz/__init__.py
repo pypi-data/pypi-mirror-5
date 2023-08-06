a35ddf0108fc95731c7f66873be8f1d9077d0050
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:25 +0000

    Add package PyFizzBuzz/0.0.3/PyFizzBuzz-0.0.3.tar.gz

---
+++ packages/PyFizzBuzz/PyFizzBuzz-0.0.3.tar.gz/PyFizzBuzz-0.0.3/lib/fizzbuzz/fizzbuzz/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+
+__info__ = dict(
+  name        = "PyFizzBuzz",
+  version     = "0.0.2",
+  description = "FizzBuzz cli tool",
+  author      = "Keiji Matsuzaki",
+  author_email = "futoase@gmail.com",
+  license     = "MIT License",
+  url         = "https://github.com/futoase/fizzbuzz",
+  classifiers = [
+    "Programming Language :: Python :: 3.2",
+    "Programming Language :: Python :: 3.3",
+    "License :: OSI Approved :: MIT License"
+  ]
+)
+
+from .fizzbuzz import FizzBuzz
+from .bizzbuzz import BizzBuzz
