e7358ffe77568b416109f79e5e8e58ccf01e0997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:57 +0000

    Add package slikio-python/0.0.2/slikio-python-0.0.2.tar.gz

---
+++ packages/slikio-python/slikio-python-0.0.2.tar.gz/slikio-python-0.0.2/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name="slikio-python",
+      version="0.0.2",
+      description="Python library for SlikIO - Charts for developers",
+      long_description= "The source is on GitHub: http://github.com/SlikIO/slikio-python. Feel free to send me questions or feature requests to <daniel@slik.io>.",
+      license="MIT",
+      install_requires=["requests"],
+      author="Daniel Brodsky, SlikIO",
+      author_email="daniel@slik.io",
+      url="http://slik.io",
+      packages = find_packages(),
+      keywords= "slikio,SlikIO,slik.io, charts, graphs, django chart templates")
