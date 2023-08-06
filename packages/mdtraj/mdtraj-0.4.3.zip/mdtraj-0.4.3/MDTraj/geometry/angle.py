d5ca38fd93c8319a19a3e0558e61f0090eb4f9ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:16 +0000

    Add package mdtraj/0.4.3/mdtraj-0.4.3.zip

---
+++ packages/mdtraj/mdtraj-0.4.3.zip/mdtraj-0.4.3/MDTraj/geometry/angle.py
@@ -0,0 +1,86 @@
+# This file is part of MDTraj.
+#
+# Copyright 2013 Stanford University
+#
+# MDTraj is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+##############################################################################
+# Imports
+##############################################################################
+
+from __future__ import print_function, division
+import numpy as np
+from mdtraj.utils import ensure_type
+try:
+    import _geometry
+    _HAVE_OPT = True
+except ImportError:
+    _HAVE_OPT = False
+
+__all__ = ['compute_angles']
+
+##############################################################################
+# Functions
+##############################################################################
+
+
+def compute_angles(traj, angle_indices, opt=True):
+    """Compute the bond angles between the supplied triplets of indices in each frame of a trajectory.
+
+    Parameters
+    ----------
+    traj : Trajectory
+        An mtraj trajectory.
+    angle_indices : np.ndarray, shape=(num_pairs, 2), dtype=int
+       Each row gives the indices of three atoms which together make an angle.
+    opt : bool, default=True
+        Use an optimized native library to calculate distances. Using this
+        library requires the python package "cffi" (c foreign function
+        interface) which is installable via "easy_install cffi" or "pip
+        install cffi". See https://pypi.python.org/pypi/cffi for more details.
+        Our optimized angle calculation implementation is 10-20x faster than
+        the (itself optimized) numpy implementation, so installing cffi is
+        generally worth it.
+
+    Returns
+    -------
+    angles : np.ndarray, shape=[n_frames, n_angles], dtype=float
+        The angles are in radians
+    """
+    xyz = ensure_type(traj.xyz, dtype=np.float32, ndim=3, name='traj.xyz', shape=(None, None, 3), warn_on_cast=False)
+    triplets = ensure_type(np.asarray(angle_indices), dtype=np.int32, ndim=2, name='angle_indices', shape=(None, 3), warn_on_cast=False)
+    out = np.zeros((xyz.shape[0], triplets.shape[0]), dtype=np.float32)
+    if _HAVE_OPT and opt:
+        print("CALLING OPT")
+        _geometry._angle(xyz, triplets, out)
+        print("CALLED OPT")
+    else:
+        _angle(xyz, triplets, out)
+    return out
+
+
+def _angle(xyz, angle_indices, out):
+    #for j, (m, o, n) in enumerate(angle_indices):
+    u_prime = xyz[:, angle_indices[:, 0], :] - xyz[:, angle_indices[:, 1], :]
+    v_prime = xyz[:, angle_indices[:, 2], :] - xyz[:, angle_indices[:, 1], :]
+    u_norm = np.sqrt((u_prime**2).sum(-1))
+    v_norm = np.sqrt((v_prime**2).sum(-1))
+
+    # adding a new axis makes sure that broasting rules kick in on the third
+    # dimension
+    u = u_prime / (u_norm[..., np.newaxis])
+    v = v_prime / (v_norm[..., np.newaxis])
+
+    out = np.arccos((u * v).sum(-1), out=out)
