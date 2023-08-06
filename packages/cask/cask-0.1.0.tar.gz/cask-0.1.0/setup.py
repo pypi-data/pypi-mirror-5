ba031de243a7bc28b88626ddb0af5b1e68ec94dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:01 +0000

    Add package cask/0.1.0/cask-0.1.0.tar.gz

---
+++ packages/cask/cask-0.1.0.tar.gz/cask-0.1.0/setup.py
@@ -0,0 +1,47 @@
+from __future__ import absolute_import, division, print_function
+
+from setuptools import setup
+
+from os.path import abspath, dirname, join
+
+PROJECT_ROOT = abspath(dirname(__file__))
+
+with open(join(PROJECT_ROOT, 'README.rst')) as f:
+    readme = f.read()
+
+with open(join(PROJECT_ROOT, 'cask.py')) as f:
+    version_line = [line for line in f.readlines() if line.startswith('__version__')][0]
+    version = version_line.split('=')[1].strip().strip("'")
+
+
+setup(
+    name='cask',
+    version=version,
+    description='Injector-enabled Python application microframework',
+    long_description=readme,
+    author='Jakub Stasiak',
+    url='https://github.com/jstasiak/cask',
+    author_email='jakub@stasiak.at',
+    py_modules=['cask'],
+    license='MIT',
+    install_requires=[
+        'injector >= 0.7.4',
+    ],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+)
