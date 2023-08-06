b24cf23a09e2e0daf564cb69139513dfd31a005a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:44 +0000

    Add package pysqes/0.1.4/pysqes-0.1.4.tar.gz

---
+++ packages/pysqes/pysqes-0.1.4.tar.gz/pysqes-0.1.4/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='pysqes',
+    version='0.1.4',
+    license='Apache',
+    description='A simple queue service using Amazon SQS and boto',
+    long_description=open('README.md').read(),
+    author='Essau Ramirez',
+    author_email='saumotions+pypi@gmail.com',
+    url='https://github.com/sauramirez/pysqes',
+    download_url='https://github.com/sauramirez/pysqes',
+    keywords=('queue', 'amazon', 'distributed'),
+    packages=find_packages(exclude=['tests', ]),
+    include_package_data = True,
+    install_requires=[
+        item for item in
+        open("requirements.txt").read().split("\n")
+    ]
+)
