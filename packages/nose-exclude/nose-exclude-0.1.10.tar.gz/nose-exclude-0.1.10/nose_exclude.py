a6b1c251f45498fe34eb6390231543da97efeff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:55 +0000

    Add package nose-exclude/0.1.10/nose-exclude-0.1.10.tar.gz

---
+++ packages/nose-exclude/nose-exclude-0.1.10.tar.gz/nose-exclude-0.1.10/nose_exclude.py
@@ -0,0 +1,95 @@
+import os
+import logging
+from nose.plugins import Plugin
+
+log = logging.getLogger('nose.plugins.nose_exclude')
+
+class NoseExclude(Plugin):
+
+    def options(self, parser, env=os.environ):
+        """Define the command line options for the plugin."""
+        super(NoseExclude, self).options(parser, env)
+        env_dirs = []
+        if 'NOSE_EXCLUDE_DIRS' in env:
+            exclude_dirs = env.get('NOSE_EXCLUDE_DIRS','')
+            env_dirs.extend(exclude_dirs.split(';'))
+        parser.add_option(
+            "--exclude-dir", action="append",
+            dest="exclude_dirs",
+            default=env_dirs,
+            help="Directory to exclude from test discovery. \
+                Path can be relative to current working directory \
+                or an absolute path. May be specified multiple \
+                times. [NOSE_EXCLUDE_DIRS]")
+
+        parser.add_option(
+            "--exclude-dir-file", type="string",
+            dest="exclude_dir_file",
+            default=env.get('NOSE_EXCLUDE_DIRS_FILE', False),
+            help="A file containing a list of directories to exclude \
+                from test discovery. Paths can be relative to current \
+                working directory or an absolute path. \
+                [NOSE_EXCLUDE_DIRS_FILE]")
+
+    def _force_to_abspath(self, pathname):
+        if os.path.isabs(pathname):
+            abspath = pathname
+        else:
+            abspath = os.path.abspath(pathname)
+
+        if os.path.exists(abspath):
+            return abspath
+        else:
+            log.warn('The following path was not found: %s' % pathname)
+
+    def _load_from_file(self, filename):
+        infile = open(filename)
+        new_list = [l.strip() for l in infile.readlines() if l.strip()
+                    and not l.startswith('#')]
+
+        return new_list
+
+    def configure(self, options, conf):
+        """Configure plugin based on command line options"""
+        super(NoseExclude, self).configure(options, conf)
+
+        self.exclude_dirs = {}
+
+        # preload directories from file
+        if options.exclude_dir_file:
+            if not options.exclude_dirs:
+                options.exclude_dirs = []
+
+            new_dirs = self._load_from_file(options.exclude_dir_file)
+            options.exclude_dirs.extend(new_dirs)
+
+        if not options.exclude_dirs:
+            self.enabled = False
+            return
+
+        self.enabled = True
+        root = os.getcwd()
+        log.debug('cwd: %s' % root)
+
+        # Normalize excluded directory names for lookup
+        for exclude_param in options.exclude_dirs:
+            # when using setup.cfg, you can specify only one 'exclude-dir'
+            # separated by some character (new line is good enough)
+            for d in exclude_param.split('\n'):
+                d = d.strip()
+                abs_d = self._force_to_abspath(d)
+                if abs_d:
+                    self.exclude_dirs[abs_d] = True
+
+        exclude_str = "excluding dirs: %s" % ",".join(self.exclude_dirs.keys())
+        log.debug(exclude_str)
+
+    def wantDirectory(self, dirname):
+        """Check if directory is eligible for test discovery"""
+        if dirname in self.exclude_dirs:
+            log.debug("excluded: %s" % dirname)
+            return False
+        else:
+            return None
+
+
---
+++ packages/nose-exclude/nose-exclude-0.1.10.tar.gz/nose-exclude-0.1.10/tests.py
@@ -0,0 +1,101 @@
+import os
+import unittest
+from nose.plugins import PluginTester
+from nose_exclude import NoseExclude
+
+class TestNoseExcludeDirs_Relative_Args(PluginTester, unittest.TestCase):
+    """Test nose-exclude directories using relative paths passed
+    on the commandline via --exclude-dir
+    """
+
+    activate = "--exclude-dir=test_dirs/build"
+    args = ['--exclude-dir=test_dirs/test_not_me']
+    plugins = [NoseExclude()]
+    suitepath = os.path.join(os.getcwd(), 'test_dirs')
+
+    def test_proper_dirs_omitted(self):
+        assert "FAILED" not in self.output
+
+class TestNoseExcludeDirs_Absolute_Args(PluginTester, unittest.TestCase):
+    """Test nose-exclude directories using absolute paths passed
+    on the commandline via --exclude-dir
+    """
+
+    plugins = [NoseExclude()]
+    suitepath = os.path.join(os.getcwd(), 'test_dirs')
+
+    def __init__(self, *args, **kwargs):
+        self.activate = "--exclude-dir=%s" % \
+                os.path.join(self.suitepath, 'build') 
+        arg_path = os.path.join(self.suitepath, 'test_not_me')
+        self.args = ['--exclude-dir=%s' % arg_path]
+        super(TestNoseExcludeDirs_Absolute_Args, self).__init__(*args, **kwargs)
+
+    def test_proper_dirs_omitted(self):
+        assert "FAILED" not in self.output
+
+class TestNoseExcludeDirs_Relative_Args_File(PluginTester, unittest.TestCase):
+    """Test nose-exclude directories using relative paths passed
+    by file using --exclude-dir-file
+    """
+
+    activate = "--exclude-dir-file=test_dirs/exclude_dirs.txt"
+    plugins = [NoseExclude()]
+    suitepath = os.path.join(os.getcwd(), 'test_dirs')
+
+    def test_proper_dirs_omitted(self):
+        assert "FAILED" not in self.output
+
+class TestNoseExcludeDirs_Relative_Args_Mixed(PluginTester, unittest.TestCase):
+    """Test nose-exclude directories using paths passed
+    by file and commandline
+    """
+
+    activate = "--exclude-dir-file=test_dirs/exclude_dirs2.txt"
+    args = ["--exclude-dir=test_dirs/test_not_me"]
+    plugins = [NoseExclude()]
+    suitepath = os.path.join(os.getcwd(), 'test_dirs')
+
+    def test_proper_dirs_omitted(self):
+        assert "FAILED" not in self.output
+
+class TestNoseExcludeEnvVariables(PluginTester, unittest.TestCase):
+    """Test nose-exclude's use of environment variables"""
+    
+    #args = ['--exclude-dir=test_dirs/test_not_me']
+    activate = "-v"
+    plugins = [NoseExclude()]
+    suitepath = os.path.join(os.getcwd(), 'test_dirs')
+    
+    env = {'NOSE_EXCLUDE_DIRS':'test_dirs/build;test_dirs/test_not_me'}
+
+    def test_proper_dirs_omitted(self):
+        assert "FAILED" not in self.output
+
+class TestNoseExcludeDirsEnvFile(PluginTester, unittest.TestCase):
+    """Test nose-exclude directories using relative paths passed
+    by file specified by environment variable
+    """
+
+    activate = "-v"
+    plugins = [NoseExclude()]
+    env = {'NOSE_EXCLUDE_DIRS_FILE':'test_dirs/exclude_dirs.txt'}
+    suitepath = os.path.join(os.getcwd(), 'test_dirs')
+
+    def test_proper_dirs_omitted(self):
+        assert "FAILED" not in self.output
+
+class TestNoseExcludeDirs_Arg_Does_Not_Exist(PluginTester, unittest.TestCase):
+    """Test nose-exclude directories for a directory that doesn't exist.
+    """
+
+    activate = "--exclude-dir=test_dirs/build"
+    args = ["--exclude-dir=test_dirs/test_not_me \n --exclude-dir=test_dirs/test_i_dont_exist"]
+    plugins = [NoseExclude()]
+    suitepath = os.path.join(os.getcwd(), 'test_dirs')
+
+    def test_proper_dirs_omitted(self):
+        assert "FAILED" not in self.output
+
+if __name__ == '__main__':
+    unittest.main()
