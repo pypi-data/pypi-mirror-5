c3049aa596a462ad53a084f763adcaa48abe90f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:20 +0000

    Add package ImageResolver/0.1.3/ImageResolver-0.1.3.tar.gz

---
+++ packages/ImageResolver/ImageResolver-0.1.3.tar.gz/ImageResolver-0.1.3/bin/resolveimg.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import sys
+import imageresolver
+
+i = imageresolver.ImageResolver()
+i.register(imageresolver.FileExtensionResolver())
+i.register(imageresolver.ImgurPageResolver())
+i.register(imageresolver.WebpageResolver(load_images=True, parser='lxml'))
+f = sys.argv[1]
+
+print i.resolve(f)
