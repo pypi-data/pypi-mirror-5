40e00c7a42fc7fc07e1580dad0494b999237abf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:44 +0000

    Add package ach/0.1/ach-0.1.1.tar.gz

---
+++ packages/ach/ach-0.1.1.tar.gz/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup 
+
+setup(
+    name='ach',
+    author='Travis Hathaway',
+    author_email='travis.j.hathaway@gmail.com',
+    version='0.1',
+    packages=[
+        'ach',
+    ],
+    url='https://github.com/travishathaway/python-ach',
+    license='MIT License',
+    long_description=open('README.md').read(),
+)
