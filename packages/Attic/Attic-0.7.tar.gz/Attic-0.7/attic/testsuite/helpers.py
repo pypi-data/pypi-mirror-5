89a044f3d1af4a620626543ea7c858eb60564fd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:25 +0000

    Add package Attic/0.7/Attic-0.7.tar.gz

---
+++ packages/Attic/Attic-0.7.tar.gz/Attic-0.7/attic/testsuite/helpers.py
@@ -0,0 +1,68 @@
+from datetime import datetime
+from attic.helpers import Location, format_timedelta, IncludePattern, ExcludePattern, make_path_safe
+from attic.testsuite import AtticTestCase
+
+
+class LocationTestCase(AtticTestCase):
+
+    def test(self):
+        self.assert_equal(
+            repr(Location('ssh://user@host:1234/some/path::archive')),
+            "Location(proto='ssh', user='user', host='host', port=1234, path='/some/path', archive='archive')"
+        )
+        self.assert_equal(
+            repr(Location('file:///some/path::archive')),
+            "Location(proto='file', user=None, host=None, port=None, path='/some/path', archive='archive')"
+        )
+        self.assert_equal(
+            repr(Location('user@host:/some/path::archive')),
+            "Location(proto='ssh', user='user', host='host', port=22, path='/some/path', archive='archive')"
+        )
+        self.assert_equal(
+            repr(Location('mybackup.attic::archive')),
+            "Location(proto='file', user=None, host=None, port=None, path='mybackup.attic', archive='archive')"
+        )
+        self.assert_equal(
+            repr(Location('/some/absolute/path::archive')),
+            "Location(proto='file', user=None, host=None, port=None, path='/some/absolute/path', archive='archive')"
+        )
+        self.assert_equal(
+            repr(Location('some/relative/path::archive')),
+            "Location(proto='file', user=None, host=None, port=None, path='some/relative/path', archive='archive')"
+        )
+
+
+class FormatTimedeltaTestCase(AtticTestCase):
+
+    def test(self):
+        t0 = datetime(2001, 1, 1, 10, 20, 3, 0)
+        t1 = datetime(2001, 1, 1, 12, 20, 4, 100000)
+        self.assert_equal(
+            format_timedelta(t1 - t0),
+            '2 hours 1.10 seconds'
+        )
+
+
+class PatternTestCase(AtticTestCase):
+
+    def test(self):
+        self.assert_equal(IncludePattern('/usr').match('/usr'), True)
+        self.assert_equal(IncludePattern('/usr').match('/usr/bin'), True)
+        self.assert_equal(IncludePattern('/usr').match('/usrbin'), False)
+        self.assert_equal(ExcludePattern('*.py').match('foo.py'), True)
+        self.assert_equal(ExcludePattern('*.py').match('foo.pl'), False)
+        self.assert_equal(ExcludePattern('/tmp').match('/tmp'), True)
+        self.assert_equal(ExcludePattern('/tmp').match('/tmp/foo'), True)
+        self.assert_equal(ExcludePattern('/tmp').match('/tmofoo'), False)
+
+
+class MakePathSafeTestCase(AtticTestCase):
+
+    def test(self):
+        self.assert_equal(make_path_safe('/foo/bar'), 'foo/bar')
+        self.assert_equal(make_path_safe('/foo/bar'), 'foo/bar')
+        self.assert_equal(make_path_safe('../foo/bar'), 'foo/bar')
+        self.assert_equal(make_path_safe('../../foo/bar'), 'foo/bar')
+        self.assert_equal(make_path_safe('/'), '.')
+        self.assert_equal(make_path_safe('/'), '.')
+
