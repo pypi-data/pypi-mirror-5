fe41f87ea28677dc985b600d5da1b21e1eb1473c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:58 +0000

    Add package pycse/1.1/pycse-1.1.zip

---
+++ packages/pycse/pycse-1.1.zip/pycse-1.1/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+import os
+
+# for installing magic IPython stuff
+import IPython
+IPydir = os.path.join(IPython.utils.path.get_ipython_dir(),
+                      'profile_default',
+                      'startup')
+                      
+print 'Installing ipython magic to : ',IPydir
+
+   
+setup(name = 'pycse',
+      version='1.1',
+      description='python computations in science and engineering',
+      url='http://github.com/jkitchin/pycse',
+      maintainer='John Kitchin',
+      maintainer_email='jkitchin@andrew.cmu.edu',
+      license='GPL',
+      platforms=['linux'],
+      packages=['pycse'],
+      scripts=['pycse/publish.py'],
+      data_files=[(IPydir, ['pycse/00-pycse-magic.py'])],
+      long_description='''python computations in science and engineering''')
