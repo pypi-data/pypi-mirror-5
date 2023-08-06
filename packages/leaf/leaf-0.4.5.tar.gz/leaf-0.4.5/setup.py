251d6fc862075d1bb3247bc1b62b4a75164b7c60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:08 +0000

    Add package leaf/0.4.5/leaf-0.4.5.tar.gz

---
+++ packages/leaf/leaf-0.4.5.tar.gz/leaf-0.4.5/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# encoding: utf-8
+from setuptools import setup, find_packages
+import os.path
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='leaf',
+    version='0.4.5',
+    description='Simple Python library for HTML parsing',
+    author='Roman Koblov',
+    author_email='pingu.g@gmail.com',
+    url='https://github.com/penpen/Leaf',
+    license='MIT',
+    keywords=['html', 'parsing', 'web scrapping'],
+    packages=['leaf'],
+    data_files=[
+            ('', ['LICENSE.txt', 'README.rst'])
+    ],
+    package_data={
+        '': ['*.txt', '*.rst', '*.md']
+    },
+    classifiers=[
+         'Development Status :: 3 - Alpha',
+         'Intended Audience :: Developers',
+         'License :: OSI Approved :: MIT License',
+         'Operating System :: OS Independent',
+         'Programming Language :: Python',
+         'Topic :: Scientific/Engineering :: Information Analysis'],
+    install_requires = ['setuptools', 'lxml'],
+    long_description=read('README.rst')+read('CHANGES.rst'),
+)
