5ed89096c4bc3add1a697c41406dd5e6a68cbe2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:28 +0000

    Add package pyramid_cachebust/0.1/pyramid_cachebust-0.1.tar.gz

---
+++ packages/pyramid_cachebust/pyramid_cachebust-0.1.tar.gz/pyramid_cachebust-0.1/setup.py
@@ -0,0 +1,28 @@
+import os
+
+from setuptools import setup, find_packages
+
+
+requires = [
+    'pyramid>=1.0.2', # wsgiref server entry point
+]
+
+setup(name='pyramid_cachebust',
+      version='0.1',
+      description='Nascent cache busting for the Pyramid web framework',
+      classifiers=[
+        "Framework :: Pyramid",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        ],
+      keywords='web wsgi pylons pyramid cachebust',
+      author="maisano",
+      author_email="rickmaisano@gmail.com",
+      maintainer="maisano",
+      maintainer_email="rickmaisano@gmail.com",
+      url="https://github.com/maisano/pyramid_cachebust",
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires
+      )
