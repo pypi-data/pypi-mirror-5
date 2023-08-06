a7e40f4fe05ad429d2039e53081f2f44f9b7b6ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:40 +0000

    Add package sloth-ci/0.2.9/sloth-ci-0.2.9.tar.gz

---
+++ packages/sloth-ci/sloth-ci-0.2.9.tar.gz/sloth-ci-0.2.9/sloth_ci/__init__.py
@@ -0,0 +1,11 @@
+"""
+*******
+Sloth
+*******
+
+**Sloth** is a lightweight CI script to perform push-based actions.
+
+The repo is at `bitbucket.org/moigagoo/configs <https://bitbucket.org/moigagoo/configs>`_.
+"""
+
+from .sloth import Sloth
