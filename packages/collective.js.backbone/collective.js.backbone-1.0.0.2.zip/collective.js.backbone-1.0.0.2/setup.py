b3b820d3ba05d92273613cdd2a68fc52d9029bf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:27 +0000

    Add package collective.js.backbone/1.0.0.2/collective.js.backbone-1.0.0.2.zip

---
+++ packages/collective.js.backbone/collective.js.backbone-1.0.0.2.zip/collective.js.backbone-1.0.0.2/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0.0.2'
+
+setup(name='collective.js.backbone',
+      version=version,
+      description="backbone.js for plone",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        ],
+      keywords='javascript plone backbone',
+      author='Patrick Gerken',
+      author_email='gerken@patrick-gerken.de',
+      url='https://github.com/collective/collective.js.backbone',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.js'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'collective.js.underscore',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
