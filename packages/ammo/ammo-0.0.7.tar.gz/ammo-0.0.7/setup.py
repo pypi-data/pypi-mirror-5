b899cb68e734cf9f93dfedbbd0af5fc294405ff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:21 +0000

    Add package ammo/0.0.7/ammo-0.0.7.tar.gz

---
+++ packages/ammo/ammo-0.0.7.tar.gz/ammo-0.0.7/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+from ammo import __version__
+
+setup(
+    name='ammo',
+    version=__version__,
+    author='Gregory Komissarov',
+    author_email='gregory.komissarov@gmail.com',
+    description='Input data generator for different load tools',
+    license='BSD',
+    url='https://github.com/greggyNapalm/ammo',
+    keywords=['phantom', 'yandex-tank', 'ammo'],
+    packages=[
+        'ammo',
+        'ammo.lunapark',
+        'ammo.phantom'
+    ],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
+)
