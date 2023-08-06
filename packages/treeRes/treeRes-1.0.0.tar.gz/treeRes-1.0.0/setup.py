0f5451ea4aae7fb91ce3cb15307a7bb8ae4bc467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:10 +0000

    Add package treeRes/1.0.0/treeRes-1.0.0.tar.gz

---
+++ packages/treeRes/treeRes-1.0.0.tar.gz/treeRes-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name = 'treeRes',
+	version = '1.0.0',
+	py_modules = ['treeRes'],
+	author = 'chenliyu',
+	author_email = '2572065090@qq.com',
+	url = 'http://pypi.python.org/ ',
+	description = 'implimention of tree regression in python,and you can set the spilt methods in linear model to split or in constant model by changing the argument and a use for both conditons',
+)
+
