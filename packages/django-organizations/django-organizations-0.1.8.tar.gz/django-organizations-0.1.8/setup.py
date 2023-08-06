5772485cd42cf3de3ed7fd3279d4799db4697acd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:21 +0000

    Add package django-organizations/0.1.8/django-organizations-0.1.8.tar.gz

---
+++ packages/django-organizations/django-organizations-0.1.8.tar.gz/django-organizations-0.1.8/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+
+setup(
+    author="Ben Lopatin",
+    author_email="ben.lopatin@wellfireinteractive.com",
+    name='django-organizations',
+    version='0.1.8',
+    description='Group accounts for Django',
+    long_description=open(os.path.join(os.path.dirname(__file__),
+        'README.rst')).read(),
+    url='https://github.com/wellfire/django-organizations/',
+    license='BSD License',
+    platforms=['OS Independent'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    install_requires=[
+        'Django>=1.4',
+        'django-extensions>=0.9',
+    ],
+    #test_suite='tests.runtests.runtests',
+    include_package_data=True,
+    packages=find_packages(exclude=["tests.tests", "tests.test_app", "tests"]),
+    zip_safe=False
+)
