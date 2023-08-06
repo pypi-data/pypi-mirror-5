317d8159c3db5df35970c67953c4ca8b48f277b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:05 +0000

    Add package mixer/0.11.3/mixer-0.11.3.tar.gz

---
+++ packages/mixer/mixer-0.11.3.tar.gz/mixer-0.11.3/mixer/__init__.py
@@ -0,0 +1,20 @@
+"""
+    Mixer is a python module that adds object generation
+    tool for testing your application.
+
+    It's supported Django_ ORM, SQLAlchemy_ ORM, Mongoengine_ ODM and etc.
+
+    Mixer is very useful for testing and fixtures replacement.
+
+    :copyright: 2013 by Kirill Klenov.
+    :license: BSD, see LICENSE for more details.
+"""
+from __future__ import unicode_literals
+
+# Module information
+# ==================
+
+__version__ = '0.11.3'
+__project__ = 'mixer'
+__author__ = "horneds <horneds@gmail.com>"
+__license__ = "BSD"
