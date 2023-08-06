a258ec3e62982c6a66a2a0271893418f29a46588
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:55 +0000

    Add package django-swingers/1.0.2/django-swingers-1.0.2.tar.gz

---
+++ packages/django-swingers/django-swingers-1.0.2.tar.gz/django-swingers-1.0.2/swingers/management/commands/deploy.py
@@ -0,0 +1,11 @@
+from django.core.management.base import NoArgsCommand
+from django.core.management import call_command
+
+
+class Command(NoArgsCommand):
+    help = "Runs `syncdbmigrate` and `quickinstall`."
+    option_list = NoArgsCommand.option_list + tuple()
+
+    def handle_noargs(self, *args, **options):
+        call_command("syncdbmigrate", **options)
+        call_command("quickinstall", **options)
