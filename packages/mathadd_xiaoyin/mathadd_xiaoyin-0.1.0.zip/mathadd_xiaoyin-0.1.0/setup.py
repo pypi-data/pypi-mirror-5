0c98b35aa13253c9bfa7ca3ea7c9226fa2500d9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:14 +0000

    Add package mathadd_xiaoyin/0.1.0/mathadd_xiaoyin-0.1.0.zip

---
+++ packages/mathadd_xiaoyin/mathadd_xiaoyin-0.1.0.zip/mathadd_xiaoyin-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'mathadd_xiaoyin',
+    version = '0.1.0',
+    py_modules = ['mathadd'],
+    author = 'xiaoyin',
+    author_email = 'li.xiaoyin@163.com',
+    url = 'http://weibo.com/oxiaoyin',
+    description = "this is an add method.",
+    )
