57079325a818cd0db50473c963d4ecda8986a2cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:13 +0000

    Add package thinkify/0.2.0/thinkify-0.2.0.tar.gz

---
+++ packages/thinkify/thinkify-0.2.0.tar.gz/thinkify-0.2.0/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='thinkify',
+    description='A Python wrapper for the Thinkify Reader Protocol.',
+    version='0.2.0',
+    author='Sean Coonce',
+    author_email='cooncesean@gmail.com',
+    packages=['thinkify',],
+    url='https://github.com/cooncesean/thinkify',
+    license='LICENSE.txt',
+    long_description='More info can be found at: http://github.com/cooncesean/thinkify',
+    install_requires=[
+    	'pyserial>=2.6',
+	],
+)
