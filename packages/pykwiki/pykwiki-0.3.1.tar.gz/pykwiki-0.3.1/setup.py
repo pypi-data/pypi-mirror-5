d8533d8c008fe6491c47048b82a39548b2441330
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:21 +0000

    Add package pykwiki/0.3.1/pykwiki-0.3.1.tar.gz

---
+++ packages/pykwiki/pykwiki-0.3.1.tar.gz/pykwiki-0.3.1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import os
+import sys
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, os.path.dirname(__file__))
+from pykwiki import __version__
+
+setup(name='pykwiki',
+    version = __version__,
+    description = 'A simple pure-python wiki engine.',
+    long_description = '''
+A pure-python, light-weight, static wiki, blog, website engine designed for speed. 
+    ''',
+    author = 'Aaron Meier',
+    author_email = 'webgovernor@gmail.com',
+    packages = ['pykwiki','pykwiki.ext'],
+    package_dir={'pykwiki':'pykwiki'},
+    scripts=['pykwiki/scripts/pykwiki'],
+    package_data={'pykwiki':['data/*.zip']},
+    url = 'http://pykwiki.nullism.com',
+    license = 'MIT',
+    install_requires = ['markdown>=2.3', 'jinja2>=2.6', 'pyyaml>=3.0'],
+    provides = ['pykwiki']
+)
+
