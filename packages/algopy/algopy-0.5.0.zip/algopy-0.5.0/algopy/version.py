a5ad9cc1b187ffeb25c92bcc6062aa947e548ed6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:56 +0000

    Add package algopy/0.5.0/algopy-0.5.0.zip

---
+++ packages/algopy/algopy-0.5.0.zip/algopy-0.5.0/algopy/version.py
@@ -0,0 +1,8 @@
+
+# THIS FILE IS GENERATED FROM ALGOPY SETUP.PY
+short_version='0.5.0'
+version='0.5.0'
+release=True
+
+if not release:
+    version += '.devbee8ae314012f9f76840b93c628e149834f80f45'
