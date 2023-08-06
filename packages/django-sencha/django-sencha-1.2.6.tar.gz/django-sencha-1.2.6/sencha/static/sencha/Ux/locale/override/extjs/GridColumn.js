52f129beb2950d1016d0f20c12fd6b54e07081bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:40 +0000

    Add package django-sencha/1.2.6/django-sencha-1.2.6.tar.gz

---
+++ packages/django-sencha/django-sencha-1.2.6.tar.gz/django-sencha-1.2.6/sencha/static/sencha/Ux/locale/override/extjs/GridColumn.js
@@ -0,0 +1,32 @@
+Ext.define('Ux.locale.override.extjs.GridColumn', {
+    override : 'Ext.grid.Column',
+
+    requires : [
+        'Ux.locale.override.extjs.Component'
+    ],
+
+    setLocale : function(locale) {
+
+        var me          = this,
+            locales     = me.locales,
+            text        = locales.text,
+            manager     = me.locale,
+            defaultText = '';            
+
+        if (text) {
+            if (Ext.isObject(text)) {
+                defaultText = text.defaultText;
+                text        = text.key;
+            }
+
+            text = manager.get(text, defaultText);
+
+            if (Ext.isString(text)) {
+                me.setText(text);
+            }
+        }
+
+        me.callOverridden(arguments);
+    }
+
+});
