8273ed6a9ed493357f86509f69584ee4913ae3b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:57 +0000

    Add package pykb/1.4/pykb-1.4.tar.gz

---
+++ packages/pykb/pykb-1.4.tar.gz/pykb-1.4/setup.py
@@ -0,0 +1,11 @@
+ # -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='pykb',
+      version='1.4',
+      description='Python API to access a KB-API conformant knowledge base',
+      author='Séverin Lemaignan',
+      author_email='severin.lemaignan@epfl.ch',
+      py_modules=['kb'],
+      )
