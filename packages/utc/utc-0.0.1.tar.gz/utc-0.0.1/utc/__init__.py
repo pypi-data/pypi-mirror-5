1a884ae2bc6bf05a83fc82d9ad193af14a02a46f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:39 +0000

    Add package utc/0.0.1/utc-0.0.1.tar.gz

---
+++ packages/utc/utc-0.0.1.tar.gz/utc-0.0.1/utc/__init__.py
@@ -0,0 +1,30 @@
+import datetime as dt
+
+
+ZERO = dt.timedelta(0)
+
+
+class UTC(dt.tzinfo):
+    def tzname(self, dt):
+        return "UTC"
+    def utcoffset(self, dt):
+        return ZERO
+    def dst(self, dt):
+        return ZERO
+    def __repr__(self):
+        return "<UTC>"    
+
+
+
+def now():
+    return dt.datetime.utcnow().replace(tzinfo=UTC())
+
+
+def datetime(*args, **kwargs):
+    kwargs['tzinfo'] = UTC()
+    return dt.datetime(*args, **kwargs)
+
+
+def time(*args, **kwargs):
+    kwargs['tzinfo'] = UTC()
+    return dt.time(*args, **kwargs)
