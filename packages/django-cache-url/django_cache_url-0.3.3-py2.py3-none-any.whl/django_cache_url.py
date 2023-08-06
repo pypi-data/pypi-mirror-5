996676606d11202e3dd3269d1754aba7f4f2ed4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:33 +0000

    Add package django-cache-url/0.3.3/django_cache_url-0.3.3-py2.py3-none-any.whl

---
+++ packages/django-cache-url/django_cache_url-0.3.3-py2.py3-none-any.whl/django_cache_url.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+try:
+    import urlparse
+except ImportError:
+    from urllib.parse import urlparse
+
+
+# Register cache schemes in URLs.
+urlparse.uses_netloc.append('db')
+urlparse.uses_netloc.append('dummy')
+urlparse.uses_netloc.append('file')
+urlparse.uses_netloc.append('locmem')
+urlparse.uses_netloc.append('memcached')
+urlparse.uses_netloc.append('djangopylibmc')
+urlparse.uses_netloc.append('pymemcached')
+
+DEFAULT_ENV = 'CACHE_URL'
+
+CACHE_TYPES = {
+    'db': 'django.core.cache.backends.db.DatabaseCache',
+    'dummy': 'django.core.cache.backends.dummy.DummyCache',
+    'file': 'django.core.cache.backends.filebased.FileBasedCache',
+    'locmem': 'django.core.cache.backends.locmem.LocMemCache',
+    'memcached': 'django.core.cache.backends.memcached.PyLibMCCache',
+    'djangopylibmc': 'django_pylibmc.memcached.PyLibMCCache',
+    'pymemcached': 'django.core.cache.backends.memcached.MemcachedCache'
+}
+
+def config(env=DEFAULT_ENV, default='locmem://'):
+    """Returns configured CACHES dictionary from CACHE_URL"""
+    config = {}
+
+    s = os.environ.get(env, default)
+
+    if s:
+        config = parse(s)
+
+    return config
+
+def parse(url):
+    """Parses a cache URL."""
+    config = {}
+
+    url = urlparse.urlparse(url)
+
+    # Update with environment configuration.
+    config['BACKEND'] = CACHE_TYPES[url.scheme]
+    if url.scheme == 'file':
+        config['LOCATION'] = url.path
+        return config
+
+    config['LOCATION'] = url.netloc
+    config['KEY_PREFIX'] = url.path[1:]
+
+    return config
+
