051f3a7c484c962bc82592eb578bd38c56a9ca8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:27 +0000

    Add package pql/0.3.2/pql-0.3.2.tar.gz

---
+++ packages/pql/pql-0.3.2.tar.gz/pql-0.3.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='pql',
+      version='0.3.2',
+      description='A python expression to MongoDB query translator',
+      author='Alon Horev',
+      author_email='alon@horev.net',
+      url='https://github.com/alonho/pql',
+      classifiers = ["Development Status :: 3 - Alpha",
+                     "Intended Audience :: Developers",
+                     "Programming Language :: Python :: 2.6",
+                     "Programming Language :: Python :: 2.7",
+                     "Programming Language :: Python :: 3.3",
+                     "Operating System :: POSIX :: Linux",
+                     "Operating System :: MacOS :: MacOS X"],
+      license='BSD',
+      # I know it's bad practice to not specify a pymongo version, but we only
+      # require the bson.ObjectId type, It's safe to assume it won't change (famous last words)
+      install_requires=['pymongo',
+                        'python-dateutil'], 
+      packages=['pql'])
