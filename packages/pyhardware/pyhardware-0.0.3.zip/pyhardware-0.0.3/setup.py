4988b88e35f3d141cce3a79bd8886caf5bb7c41b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:23 +0000

    Add package pyhardware/0.0.3/pyhardware-0.0.3.zip

---
+++ packages/pyhardware/pyhardware-0.0.3.zip/pyhardware-0.0.3/setup.py
@@ -0,0 +1,51 @@
+import os
+from setuptools import setup
+from setuptools.command.install import install
+import subprocess
+
+
+
+class installWithPost(install):
+    def run(self):
+        install.run(self)         
+        subprocess.call(['pip', 'install', '-I', 'pyivi'])
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "pyhardware",
+    cmdclass={"install": installWithPost},
+    scripts={'postinstallscript.py'},
+    version = "0.0.3",
+    author = "Samuel Deleglise",
+    author_email = "samuel.deleglise@gmail.com",
+    description = ("""Control of data acquisition with remote instruments using 
+    IVI-COM, or IVI-C, Visa, and serial protocols.
+    python dotnet and/or comtypes should be installed"""),
+    license = "BSD",
+    keywords = "instruments data-acquisition IVI interface",
+    url = "https://github.com/SamuelDeleglise/pyinstruments",
+    packages=['pyhardware',
+              'pyhardware/config',
+              'pyhardware/config/gui',
+              'pyhardware/drivers',
+              'pyhardware/drivers/ivi',
+              'pyhardware/drivers/serial',
+              'pyhardware/drivers/visa',
+              'pyhardware/utils',
+              'pyhardware/utils/conf_xml',
+              'pyhardware/utils/curve',
+              'pyhardware/utils/guiwrappersutils'],
+    long_description=read('README.rst'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Scientific/Engineering :: Human Machine Interfaces",
+        "License :: OSI Approved :: BSD License",
+    ],
+    install_requires=[]
+)
