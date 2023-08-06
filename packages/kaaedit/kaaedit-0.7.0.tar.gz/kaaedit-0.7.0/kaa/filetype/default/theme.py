8d86645eeee53f832dd1e541646431f05172ce2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:34 +0000

    Add package kaaedit/0.7.0/kaaedit-0.7.0.tar.gz

---
+++ packages/kaaedit/kaaedit-0.7.0.tar.gz/kaaedit-0.7.0/kaa/filetype/default/theme.py
@@ -0,0 +1,16 @@
+from kaa.theme import Theme, Style
+
+DefaultThemes = {
+    'default':
+        Theme([
+            Style('default', 'default', 'default'),
+            Style('lineno', 'White', 'Blue'),
+            Style('parenthesis_cur', 'White', 'Blue'),
+            Style('parenthesis_match', 'Red', 'Yellow'),
+
+            Style('keyword', 'Red', 'default'),
+            Style('comment', 'Cyan', 'default'),
+            Style('string', 'Blue', 'default'),
+            Style('number', 'Green', 'default'),
+        ])
+}
