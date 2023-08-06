4b31b73db481405029b7e78588c1d7425014bab6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:12 +0000

    Add package humanapi/0.1.1/humanapi-0.1.1.tar.gz

---
+++ packages/humanapi/humanapi-0.1.1.tar.gz/humanapi-0.1.1/setup.py
@@ -0,0 +1,25 @@
+# from setuptools import setup
+# import os.path
+
+import os
+import sys
+
+from distutils.core import setup
+
+setup(
+    name = 'humanapi',
+    version = '0.1.1',
+    author = 'Ola Wiberg',
+    author_email = 'ola@humanapi.co',
+    description = 'A CLI client and Python API library for the HumanAPI health data platform.',
+    long_description = open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+    license = 'MIT',
+    keywords = 'humanapi sensor data',
+    url = 'https://github.com/humanapi/humanapi-python-client/',
+    install_requires = ['requests >= 0.13.2', 'docopt == 0.4.0'],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP'
+    ]
+)
