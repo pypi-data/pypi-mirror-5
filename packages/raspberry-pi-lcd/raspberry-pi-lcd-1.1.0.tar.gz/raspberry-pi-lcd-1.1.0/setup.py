d4da303ff887de13765ae2584778cdcb6b493a84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:03 +0000

    Add package raspberry-pi-lcd/1.1.0/raspberry-pi-lcd-1.1.0.tar.gz

---
+++ packages/raspberry-pi-lcd/raspberry-pi-lcd-1.1.0.tar.gz/raspberry-pi-lcd-1.1.0/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='raspberry-pi-lcd',
+    version='1.1.0',
+    author='Bruce Collie',
+    author_email='bruce.collie@blueyonder.co.uk',
+    packages=['lcd'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/raspberry-pi-lcd/',
+    license='LICENSE.txt',
+    description='Easy to use LCD API for the adafruit raspberry pi lcd module..',
+    long_description=open('README.txt').read(),
+    install_requires=[],
+)
+
