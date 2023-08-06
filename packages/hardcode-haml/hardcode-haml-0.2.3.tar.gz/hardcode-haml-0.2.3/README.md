f1ad4643c0306eb33d0a639994687e68440af6f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:45 +0000

    Add package hardcode-haml/0.2.3/hardcode-haml-0.2.3.tar.gz

---
+++ packages/hardcode-haml/hardcode-haml-0.2.3.tar.gz/hardcode-haml-0.2.3/README.md
@@ -0,0 +1,151 @@
+# Hardcode Haml - Haml for Hardcore Coders (and Embedded devices)
+
+Compiling Haml markup/templates to program code.
+
+*Warning:* There are still some language features missing. The TODO section is
+an imcomplete documentation of the the parts I am aware of.
+
+## Concept
+
+Most template engines for compiled and statically typed languages (like C and
+C++) are only able to do simple search and replace jobs for given values. Having
+used template engines in script languages like Ruby, Python or Java Script makes
+returning to those template engines hard. This is where Hardcode Haml comes into
+play.
+
+Haml was first implemented in ruby and got ported to multiple other languages.
+It compines the ability to replace parts of your markup with values calculated
+at runtime (even allowing evaluation of code directly from the markup) and
+additionally replaces XML with more writeable and readable markup.
+
+This project turns Haml markup into program code. Therefore the markup gets
+parsed and processed before runtime and maybe even optimized by your compiler.
+Therefore execution time, memory footprint and file size on the target device
+should be superior to other approaches. The drawback is, that your templates are
+compiled in your binary and you can't edit them on the target system.
+
+The language modules will generate a function which, when executed, will output
+the generated XML markup into a stream. The parameters of the function can be
+referenced directly from within the template. Haml contains syntax elements
+which let you print out evaluated code and even pass code directly to the
+generated code. Loops and other control flow elements can be used to repeat
+blocks or conditionally display them.
+
+To fullfill the needs of the target languages, Haml syntax has to be extended.
+We need a way to specify the name and type of the parameters given to the
+template. Additionally most languages need a way to execute code before the
+function declaration (e.g. to include headers).
+
+## State
+
+Hardcode Haml is used in at least one production environment (in which I am
+involved). The development is driven by this project as we find bugs and
+important missing features. I am confident that Hardcode Haml in its current
+state is stable and comprehensive enough to be useful in other projects.
+
+If you are missing a useful feature or find a bug please file an issue on
+[Github](https://github.com/thammi/Hardcode-Haml/issues). Also feel free to
+contact me there if you have any questions.
+
+## Supported Languages
+
+Output can currently be generated in several languages
+
+* C++ _(using ostream)_
+* C _(using fputs())_
+* python _(using Pythons file objects)_
+
+Other languages can be implemented with ease. All language implementations are
+currently smaller than 100 lines (including whitespaces).
+
+Haml passes code directly to the underlying language, so the language modules
+won't be interchangable without replacing some code in your templates.
+
+## Hardcode Haml Dialect
+
+Some syntax changes were neccessary to adopt Haml to the target languages.
+
+### Declaration
+
+As languages like C and C++ do not have tightly built-in dictionary types using
+those does not come natural in these languages. I therefore decided to use a
+more native approach to pass the data into the template: Function parameters. To
+declare those the Haml syntax has to be extended:
+
+    ? int a, std::string b
+
+The `?` declares the parameters passed to the templates and starts the actual
+template. An implicit `?` at the start of the document is assumed if there is
+none explicit declaration.
+
+Introducing this operator has another advantage: It is possible to introduce
+code which will be placed outside the actual function processing the template.
+Simply write this code before the `?` declaration. The way this code behaves
+depends on the language you are using. In C/C++ this code will be added to the
+header which enables you to include files and declare types used in your
+template.
+
+### Boolean attributes
+
+Boolean attributes are implicit in ruby and detected at runtime. Hardcode Haml
+tries to do as much as possible in the parser and avoids complexity in the
+language modules. Another problem is that not all target languages are able to
+determine whether an expression evaluates to a boolean.  Hardcode Haml therefore
+extends the syntax with the '?=' and '?=&gt;' operators for attributes.
+
+Boolean attributes are useful for radiobuttons, checkboxes and select elements.
+Simply write:
+
+    %input(type="radio" checked?=foo)
+
+... or ...
+
+    %input(type="radio"){"checked" ?=> foo}
+
+This code evaluates to the following (X)HTML when `foo` is true:
+
+    <input type="radio" checked="checked">
+
+... and when `foo` is false to:
+
+    <input type="radio">
+
+## Example
+
+There is an example in the [Wiki](https://github.com/thammi/Hardcode-Haml/wiki/Example-Workflow-%28C++%29).
+
+## Todos
+
+### Tasks
+
+* correct scoping according to indent
+* implement most common syntax/language elements (see below)
+* ...
+* document implemented functionality and introduced syntax extensions of Haml
+* clean up the code (especially in the parser)
+* target language site helper
+* ...
+
+### Syntax/Language Elements
+
+#### Must have
+
+* entity escape (static, dynamic?)
+* filters (infrastructure, some filters)
+
+### May have
+
+* whitespace removal (&lt; and &gt;)
+* conditional comments /\[] (only needed to support IE afaik)
+* whitespace preservation
+* escaping/unescaping html in evaluations
+
+### Won't implement (in the near future)
+
+* object reference \[] (too close to the target language)
+
+## Links
+
+* [Official Haml Homepage](http://haml-lang.com/)
+* [Haml "Reference"](http://haml-lang.com/docs/yardoc/file.HAML_REFERENCE.html)
+
---
+++ packages/hardcode-haml/hardcode-haml-0.2.3.tar.gz/hardcode-haml-0.2.3/src/hardcode_haml/lang/cpp.py
@@ -0,0 +1,205 @@
+###############################################################################
+##
+##  hardcode_haml - Haml for hardcore coders
+##  Copyright (C) 2010  Thammi
+## 
+##  This program is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU Affero General Public License as published by
+##  the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
+## 
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU Affero General Public License for more details.
+## 
+##  You should have received a copy of the GNU Affero General Public License
+##  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+###############################################################################
+
+from os.path import join, exists
+import re
+
+from hardcode_haml import primitives
+
+class AbstractCppWriter:
+
+    def __init__(self, name, directory):
+        self.write_buf = []
+        self.name = name
+        self.directory = directory
+
+    def to_file(self, out_str):
+        if self.header:
+            self.header.write(out_str)
+        else:
+            self.out.write(out_str)
+
+
+    def start(self):
+        directory = self.directory
+        name = self.name
+
+        self.out = open(join(directory, name + ".cpp"), 'w')
+        self.header = open(join(directory, name + ".h"), 'w')
+
+        inc_f = '#include <iostream>\n#include "{name}.h"\n\n'
+        self.out.write(inc_f.format(name=name))
+
+        gate = self.name.upper() + "_H"
+
+        self.header.write("#ifndef {gate}\n".format(gate=gate))
+        self.header.write("#define {gate}\n\n".format(gate=gate))
+        self.header.write("#include <iostream>\n\n")
+
+        self.indent = 0
+
+    def evaluate(self, cmd):
+        prim = primitives.find_primitive(cmd)
+
+        if prim:
+            self.write_buf.append(prim)
+        else:
+            self.execute("out << ({cmd})".format(cmd=cmd))
+
+    def execute(self, cmd):
+        self.flush()
+
+        cmd = cmd.strip()
+
+        if cmd.startswith('#'):
+            out_str = cmd + "\n"
+        else:
+            out_str = "\t" * self.indent + cmd + ";\n"
+
+        self.to_file(out_str)
+
+    def write(self, data):
+        # escape!
+        replacements = [
+                ('\\', '\\\\'),
+                ('\n', '\\n'),
+                ('"', '\\"'),
+                ]
+        
+        for old, new in replacements:
+            data = data.replace(old, new)
+
+        self.write_buf.append(data)
+
+    def flush(self):
+        if self.write_buf:
+            data = ''.join(self.write_buf)
+            self.write_buf = []
+            self.execute('out << "{data}"'.format(data=data))
+
+    def conditional_block(self, expression):
+        self.block_exec("if({expr})".format(expr=expression))
+
+    def block_exec(self, cmd):
+        self.flush()
+        self.to_file("\t" * self.indent + cmd + " {\n")
+        self.indent += 1
+
+    def close_block(self):
+        self.flush()
+        self.indent -= 1
+        self.to_file("\t" * self.indent + "}\n")
+
+    def comment(self, data):
+        self.flush()
+        self.to_file("\t" * self.indent + "// " + data + "\n")
+
+class FunCppWriter(AbstractCppWriter):
+
+    IDS = ['funcpp']
+    NAME = "C++ using functions"
+
+    def declare(self, paras):
+        para_str = ', '.join(['std::ostream &out'] + paras)
+        decl = "void {name}({para})".format(name=self.name, para=para_str)
+
+        self.out.write("\n{decl} {{\n".format(decl=decl))
+
+        self.header.write("\n{decl};\n\n#endif\n".format(decl))
+
+        self.header.close()
+        self.header = None
+
+        self.indent += 1
+
+    def finish(self):
+        self.flush()
+        self.out.write("}}\n")
+
+template_header = '''\
+#ifndef HAMLTEMPLATE_H
+#define HAMLTEMPLATE_H
+
+#include <ostream>
+
+class HamlTemplate {
+public:
+    virtual ~HamlTemplate() {}
+    virtual void run(std::ostream& out) = 0;
+    friend std::ostream& operator<<(std::ostream& out, HamlTemplate& templ) { templ.run(out); return out; }
+    friend std::ostream& operator<<(std::ostream& out, HamlTemplate* templ) { templ->run(out); return out; }
+};
+
+#endif /* HAMLTEMPLATE_H */
+'''
+
+class ClassCppWriter(AbstractCppWriter):
+
+    IDS = ['cpp', 'classcpp']
+    NAME = "C++ using classes"
+
+    def declare(self, paras):
+        th_file = join(self.directory, "hamltemplate.h")
+        if not exists(th_file):
+            th_out = open(th_file, 'w')
+            th_out.write(template_header)
+            th_out.close()
+
+        h_write = self.header.write
+        o_write = self.out.write
+
+        # getting a sane class name
+        re_up = lambda m: m.group(1).upper()
+        class_name = re.sub("(?:^|_)([a-z])", re_up, self.name)
+
+        para_str = ', '.join(paras)
+
+        # writing into the .cpp
+        o_write("void %s::run(std::ostream &out) {\n" % class_name)
+
+        # writing into the header
+        h_write('#include "hamltemplate.h"\n\n')
+        h_write("class {name} : public HamlTemplate {{\npublic:\n".format(name=class_name))
+        h_write("\t{name}({para}) ".format(name=class_name, para=para_str))
+
+        if paras:
+            cp_str = ', '.join("{0}({0})".format(para.split()[-1])
+                    for para in paras)
+
+            h_write(": " + cp_str)
+
+        h_write("{}\n")
+        h_write("\tvirtual void run(std::ostream &out);\n")
+        h_write("\nprivate:\n")
+
+        for para in paras:
+            h_write("\t{para};\n".format(para=para))
+
+        h_write("};\n\n#endif\n")
+
+        self.header.close()
+        self.header = None
+
+        self.indent += 1
+
+    def finish(self):
+        self.flush()
+        self.out.write("}\n")
+
