08c2bfd22ea5373e20b314c33b7206a7550f5c91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:30 +0000

    Add package tags2sdists/1.1/tags2sdists-1.1.zip

---
+++ packages/tags2sdists/tags2sdists-1.1.zip/tags2sdists-1.1/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+
+version = '1.1'
+
+long_description = '\n\n'.join([
+    open('README.rst').read(),
+    open('CREDITS.rst').read(),
+    open('CHANGES.rst').read(),
+    ])
+
+install_requires = [
+    'pkginfo',
+    'setuptools',
+    'zest.releaser',
+    ],
+
+tests_require = [
+    ]
+
+setup(name='tags2sdists',
+      version=version,
+      description="Create python sdists from tags",
+      long_description=long_description,
+      # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[],
+      keywords=[],
+      author='Reinout van Rees',
+      author_email='reinout@vanrees.org',
+      url='https://github.com/lizardsystem/tags2sdists/',
+      license='GPL',
+      packages=['tags2sdists'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      tests_require=tests_require,
+      extras_require = {'test': tests_require},
+      entry_points={
+          'console_scripts': [
+              'tags2sdists = tags2sdists.script:main',
+          ]},
+      )
