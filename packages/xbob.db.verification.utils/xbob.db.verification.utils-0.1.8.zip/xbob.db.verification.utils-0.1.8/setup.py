1fec88a19f32916af9aec0d60153644d5aee9725
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:46 +0000

    Add package xbob.db.verification.utils/0.1.8/xbob.db.verification.utils-0.1.8.zip

---
+++ packages/xbob.db.verification.utils/xbob.db.verification.utils-0.1.8.zip/xbob.db.verification.utils-0.1.8/setup.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+# @author: Manuel Guenther <Manuel.Guenther@idiap.ch>
+# @date:   Thu Dec  6 12:28:25 CET 2012
+#
+# This file contains the python (distutils/setuptools) instructions so your
+# package can be installed on **any** host system. It defines some basic
+# information like the package name for instance, or its homepage.
+#
+# It also defines which other packages this python package depends on and that
+# are required for this package's operation. The python subsystem will make
+# sure all dependent packages are installed or will install them for you upon
+# the installation of this package.
+#
+# The 'buildout' system we use here will go further and wrap this package in
+# such a way to create an isolated python working environment. Buildout will
+# make sure that dependencies which are not yet installed do get installed, but
+# **without** requiring adminstrative privileges on the host system. This
+# allows you to test your package with new python dependencies w/o requiring
+# administrative interventions.
+
+from setuptools import setup, find_packages
+
+# The only thing we do in this file is to call the setup() function with all
+# parameters that define our package.
+setup(
+
+    # This is the basic information about your project. Modify all this
+    # information before releasing code publicly.
+    name='xbob.db.verification.utils',
+    version='0.1.8',
+    description='Utilities for xbob.db verification databases',
+
+    url='http://github.com/bioidiap/xbob.db.verification.utils',
+    license='GPLv3',
+    author='Manuel Guenther',
+    author_email='manuel.guenther@idiap.ch',
+
+    # If you have a better, long description of your package, place it on the
+    # 'doc' directory and then hook it here
+    long_description=open('README.rst').read(),
+
+    # This line is required for any distutils based packaging.
+    packages=find_packages(),
+    include_package_data=True,
+
+    # This line defines which packages should be installed when you "install"
+    # this package. All packages that are mentioned here, but are not installed
+    # on the current system will be installed locally and only visible to the
+    # scripts of this package. Don't worry - You won't need adminstrative
+    # privileges when using buildout.
+    install_requires=[
+      'setuptools',
+      'bob', # base signal proc./machine learning library
+    ],
+
+    # Our database packages are good examples of namespace implementations
+    # using several layers. You can check them out here:
+    # https://github.com/idiap/bob/wiki/Satellite-Packages
+    namespace_packages = [
+      'xbob',
+      'xbob.db',
+      'xbob.db.verification'
+    ],
+
+    entry_points={
+
+      # tests that are _exported_ (that can be executed by other packages) can
+      # be signalized like this:
+#      'bob.test': [
+#         'example = xbob.example.test:MyTests',
+#         ],
+      },
+
+    # Classifiers are important if you plan to distribute this package through
+    # PyPI. You can find the complete list of classifiers that are valid and
+    # useful here (http://pypi.python.org/pypi?%3Aaction=list_classifiers).
+    classifiers = [
+      'Development Status :: 4 - Beta',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+      'Natural Language :: English',
+      'Programming Language :: Python',
+      'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+)
