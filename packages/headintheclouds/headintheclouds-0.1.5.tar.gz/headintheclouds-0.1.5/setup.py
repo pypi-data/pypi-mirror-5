36243994bd6d1dece8e8d34ec5d28bf5c31200d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:30 +0000

    Add package headintheclouds/0.1.5/headintheclouds-0.1.5.tar.gz

---
+++ packages/headintheclouds/headintheclouds-0.1.5.tar.gz/headintheclouds-0.1.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name='headintheclouds',
+      version='0.1.5',
+      description='Provider-agnostic cloud computing on the cheap',
+      author='Andreas Jansson',
+      author_email='andreas@jansson.me.uk',
+      license='GNU GPLv3',
+      packages=['headintheclouds'],
+      url='https://github.com/andreasjansson/head-in-the-clouds/commits?author=andreasjansson',
+      depends = ['configobj'],
+      install_requires=[
+          'Fabric==1.6.1',
+          'PyYAML==3.10',
+          'boto==2.9.8',
+          'dop==0.1.4',
+          'pyfscache==0.9.11',
+          'python-dateutil==2.1',
+      ]
+  )
