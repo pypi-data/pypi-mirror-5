da47e67b6b5eaaa7f4b7255b00dd2522c465d575
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:52 +0000

    Add package pearpass/1.0/pearpass-1.0.tar.gz

---
+++ packages/pearpass/pearpass-1.0.tar.gz/pearpass-1.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='pearpass',
+    version='1.0',
+    description="Scripts for managing secrets with Octopus and GnuPG.",
+    author="Miles Shang",
+    author_email='mail@mshang.ca',
+    url='https://github.com/mshang/pearpass',
+    py_modules=['pearpass'],
+)
