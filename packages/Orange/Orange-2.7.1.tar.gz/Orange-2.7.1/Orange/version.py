dc32be2d964ee8f30949d579e828ee3039b81cde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:23 +0000

    Add package Orange/2.7.1/Orange-2.7.1.tar.gz

---
+++ packages/Orange/Orange-2.7.1.tar.gz/Orange-2.7.1/Orange/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM ORANGE SETUP.PY
+short_version = '2.7.1'
+version = '2.7.1'
+full_version = '2.7.1'
+hg_revision = 'b3d7eae5e2c4'
+release = True
+
+if not release:
+    version = full_version
