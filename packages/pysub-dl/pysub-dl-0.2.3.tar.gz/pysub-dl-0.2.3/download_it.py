0deb9cc77d70961d3e3fc134f96ae149ecd6d5e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:26 +0000

    Add package pysub-dl/0.2.3/pysub-dl-0.2.3.tar.gz

---
+++ packages/pysub-dl/pysub-dl-0.2.3.tar.gz/pysub-dl-0.2.3/download_it.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import requests
+import shutil
+from prompt_user import prompt
+
+def download_file(url):
+    ''' Downloads the File.
+    '''
+    response = requests.get(url, stream = True)
+    with open('subtitle.zip', 'wb') as out_file:
+        shutil.copyfileobj(response.raw, out_file)
+    del response
+    return prompt()
