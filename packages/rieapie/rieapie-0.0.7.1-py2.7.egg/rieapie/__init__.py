1e3b789dadd67ef0d6df39f2d15194c726ddca19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:35 +0000

    Add package rieapie/0.0.7.1/rieapie-0.0.7.1-py2.7.egg

---
+++ packages/rieapie/rieapie-0.0.7.1-py2.7.egg/rieapie/__init__.py
@@ -0,0 +1,8 @@
+"""
+"""
+version = "0.0.7.1"
+try:
+    from rieapie.trickery import Api, pre_request,post_request
+    import rieapie.wrappers
+except ImportError as e:
+    print("error initializing rieapie")
