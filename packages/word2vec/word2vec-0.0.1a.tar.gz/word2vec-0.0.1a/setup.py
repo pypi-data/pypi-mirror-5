1d2739254d5804a80986522de4aea36892381f23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:55 +0000

    Add package word2vec/0.0.1a/word2vec-0.0.1a.tar.gz

---
+++ packages/word2vec/word2vec-0.0.1a.tar.gz/word2vec-0.0.1a/setup.py
@@ -0,0 +1,24 @@
+# encoding: utf-8
+import subprocess
+from distutils.core import setup
+
+DESCRIPTION = 'Google word2vec python wrapper'
+
+subprocess.call(['make', '-C', 'word2vec-src'])
+
+setup(
+    name='word2vec',
+    version='0.0.1a',
+    maintainer='Daniel Rodriguez',
+    maintainer_email='df.rodriguez143@gmail.com',
+    url='https://github.com/danielfrg/word2vec',
+    packages=['word2vec', 'word2vec.tests'],
+    description=DESCRIPTION,
+    license='Apache License Version 2.0, January 2004',
+    data_files=[('bin', ['bin/word2vec', 'bin/word2phrase', 'bin/w2v-distance',
+                         'bin/w2v-word-analogy', 'bin/w2v-compute-accuracy'])],
+    install_requires=[
+        'numpy==1.7.1',
+        'scipy==0.12.0'
+    ],
+)
