06929c3c5d012a01eccd03e74d34a91794360a25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:42 +0000

    Add package django-easy-select2/1.0.1/django-easy-select2-1.0.1.tar.gz

---
+++ packages/django-easy-select2/django-easy-select2-1.0.1.tar.gz/django-easy-select2-1.0.1/setup.py
@@ -0,0 +1,40 @@
+import os
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+
+data_files = []
+root_dir = os.path.dirname(__file__)
+if root_dir:
+    os.chdir(root_dir)
+
+
+# this for cycle took from django-registration setup.py script
+for dirpath, dirnames, filenames in os.walk('easy_select2'):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+    if filenames:
+        prefix = dirpath[13:] # Strip "easy_select2/" or "easy_select2\"
+        for f in filenames:
+            data_files.append(os.path.join(prefix, f))
+
+
+setup(
+    name = "django-easy-select2",
+    version = "1.0.1",
+    packages = find_packages(),
+    author = "asyncee",
+    description = "Django select2 theme for select input widgets.",
+    long_description=long_description,
+    license = "MIT",
+    keywords = "django select2",
+    url = 'https://github.com/asyncee/django-easy-select2',
+
+    package_dir={'easy_select2': 'easy_select2'},
+    package_data={'easy_select2': data_files},
+)
