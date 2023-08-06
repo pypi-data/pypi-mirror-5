7e737a60e90f7043d1178b1849d24da477a21beb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:01 +0000

    Add package configurati/0.1.3/configurati-0.1.3.tar.gz

---
+++ packages/configurati/configurati-0.1.3.tar.gz/configurati-0.1.3/configurati/attrs.py
@@ -0,0 +1,24 @@
+"""
+Attribute dictionary
+"""
+
+from .utils import recursive_apply
+
+class attrs(dict):
+  """A dict you can access with dot notation"""
+
+  def __getattr__(self, key):
+    return self[key]
+
+  def __setattr__(self, key, value):
+    self[key] = value
+    return self[key]
+
+  def to_dict(self):
+    f = lambda x: dict(x) if isinstance(x, attrs) else x
+    return recursive_apply(self, value_func=f)
+
+  @staticmethod
+  def from_dict(d):
+    f = lambda x: attrs(x) if isinstance(x, dict) else x
+    return recursive_apply(d, value_func=f)
