a47d034aad787789ac9a408e270753a9c4dd0dcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:26 +0000

    Add package gdrivefs/0.10.4/gdrivefs-0.10.4.tar.gz

---
+++ packages/gdrivefs/gdrivefs-0.10.4.tar.gz/gdrivefs-0.10.4/gdrivefs/tools/gdfs.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import logging
+
+from sys import argv
+from os.path import basename
+from argparse import ArgumentParser
+
+from gdrivefs.gdfs.gdfuse import mount, load_mount_parser_args
+
+def main():
+    parser = ArgumentParser()
+    load_mount_parser_args(parser)
+
+    args = parser.parse_args()
+
+    option_string = args.opt[0] if args.opt else None
+
+    try:
+        mount(auth_storage_filepath=args.auth_storage_file, 
+              mountpoint=args.mountpoint, debug=args.debug, 
+              nothreads=args.debug, option_string=option_string)
+    except (Exception) as e:
+        message = ("Could not do mount [%s]: %s" % (e.__class__.__name__, str(e)))
+        logging.exception(message)
+        print(message)
+
+if __name__ == '__main__':
+    main()
+
