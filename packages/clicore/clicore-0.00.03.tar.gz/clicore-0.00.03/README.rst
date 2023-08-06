effe3adf995a7a2257e2d2ce146b83d072c2f80a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:29 +0000

    Add package clicore/0.00.03/clicore-0.00.03.tar.gz

---
+++ packages/clicore/clicore-0.00.03.tar.gz/clicore-0.00.03/README.rst
@@ -0,0 +1,8 @@
+cli for python
+==============
+
+    Small cli to use in python programs *Version: 0.00.01*
+
+--------------
+
+Comming soon...
---
+++ packages/clicore/clicore-0.00.03.tar.gz/clicore-0.00.03/clicore/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from clicore.core import Cli
+from clicore.cliitem import CliItem, CliSysPathItem
+from clicore.clihelper import CliHelper
