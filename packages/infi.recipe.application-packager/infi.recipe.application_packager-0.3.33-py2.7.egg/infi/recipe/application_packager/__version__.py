980d62129a091e88de231ac3dcffc3ff89c80801
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:55 +0000

    Add package infi.recipe.application-packager/0.3.33/infi.recipe.application_packager-0.3.33-py2.7.egg

---
+++ packages/infi.recipe.application-packager/infi.recipe.application_packager-0.3.33-py2.7.egg/infi/recipe/application_packager/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.3.33"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.3.33)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'f6884294b7d2078be113a5af19ef65cc1ac71dc3'
+__git_head_subject__ = 'fix tests'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
