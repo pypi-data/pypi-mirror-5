cd6446f19d89feef743a54c0f971edb07b5026a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:53 +0000

    Add package python-raven/0.3/python-raven-0.3.tar.gz

---
+++ packages/python-raven/python-raven-0.3.tar.gz/python-raven-0.3/description.rst
@@ -0,0 +1,27 @@
+Python dependencies
+===================
+
+* M2Crypto (`apt-get install python-m2crypto`; far easier than via pip)
+* nose (for unit testing only)
+* flask (for `simple_demo/`)
+
+License
+=======
+
+GNU LGPLv3, see COPYING.lesser
+
+`Documentation <https://pythonhosted.org/python-raven/>`_
+=========================================================
+
+`Quickstart <https://pythonhosted.org/python-raven/quickstart.html>`_
+=====================================================================
+
+Other links
+===========
+
+* `source on github <https://github.com/danielrichman/python-raven>`_
+* `documentation`_
+* `pypi page <https://pypi.python.org/pypi/python-raven>`_
+* `Raven documentation <https://raven.cam.ac.uk/project/>`_
+* `WAA2WLS protocol <https://raven.cam.ac.uk/project/waa2wls-protocol.txt>`_
+
---
+++ packages/python-raven/python-raven-0.3.tar.gz/python-raven-0.3/setup.py
@@ -0,0 +1,36 @@
+import os.path
+from setuptools import setup
+
+filename = os.path.join(os.path.dirname(__file__), 'description.rst')
+with open(filename) as f:
+    long_description = f.read()
+
+setup(
+    name = "python-raven",
+    version = "0.3",
+    packages = ["raven", "ucam_webauth"],
+    package_data = {"raven": ["keys/pubkey*.crt"]},
+    install_requires = ["M2Crypto", "setuptools"],
+    extras_require = {"flask_glue": ["Flask"]},
+    tests_require = ["nose", "Flask"],
+    test_suite = 'nose.collector',
+
+    author = "Daniel Richman",
+    author_email = "main@danielrichman.co.uk",
+    description = "Ucam-webauth and Raven application agent in Python",
+    long_description = long_description,
+    license="GNU Lesser General Public License Version 3",
+    keywords = "Raven Cambridge ucam-webauth",
+    url = "http://github.com/danielrichman/python-raven",
+
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2"
+    ]
+)
+
+# python setup.py test
+# python setup.py build_sphinx sdist upload upload_sphinx
