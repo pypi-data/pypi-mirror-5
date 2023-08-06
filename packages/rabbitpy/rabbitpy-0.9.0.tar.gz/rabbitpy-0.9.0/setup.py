d35f0b77d4ca3da175799b09ad2aba3b71c62e02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:27 +0000

    Add package rabbitpy/0.9.0/rabbitpy-0.9.0.tar.gz

---
+++ packages/rabbitpy/rabbitpy-0.9.0.tar.gz/rabbitpy-0.9.0/setup.py
@@ -0,0 +1,42 @@
+import distutils.core
+import sys
+
+from rabbitpy import __version__
+
+tests_require = ['nose', 'mock']
+if sys.version_info < (2, 7, 0):
+    tests_require.append('unittest2')
+
+desc = ('A pure python, thread-safe, minimalistic and pythonic RabbitMQ '
+        'client library')
+
+distutils.core.setup(name='rabbitpy',
+                     version=__version__,
+                     description=desc,
+                     long_description=open('README.rst').read(),
+                     author='Gavin M. Roy',
+                     author_email='gavinmroy@gmail.com',
+                     url='http://rabbitpy.readthedocs.org',
+                     packages=['rabbitpy'],
+                     package_data={'': ['LICENSE', 'README.md']},
+                     include_package_data=True,
+                     install_requires=['pamqp>=1.2.0'],
+                     tests_require=tests_require,
+                     test_suite='nose.collector',
+                     license=open('LICENSE').read(),
+                     classifiers=['Development Status :: 4 - Beta',
+                                  'Intended Audience :: Developers',
+                                  'License :: OSI Approved :: BSD License',
+                                  'Operating System :: OS Independent',
+                                  'Programming Language :: Python :: 2',
+                                  'Programming Language :: Python :: 2.6',
+                                  'Programming Language :: Python :: 2.7',
+                                  'Programming Language :: Python :: 3',
+                                  'Programming Language :: Python :: 3.2',
+                                  'Programming Language :: Python :: 3.3',
+                                  'Programming Language :: Python :: Implementation :: CPython',
+                                  'Programming Language :: Python :: Implementation :: PyPy',
+                                  'Topic :: Communications',
+                                  'Topic :: Internet',
+                                  'Topic :: Software Development :: Libraries'],
+                     zip_safe=True)
