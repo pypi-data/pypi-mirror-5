88805c237f44c3b2fa935bf274769c6d78e31053
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:06 +0000

    Add package replmon/0.1.2/replmon-0.1.2.tar.gz

---
+++ packages/replmon/replmon-0.1.2.tar.gz/replmon-0.1.2/replmon/__init__.py
@@ -0,0 +1,59 @@
+#coding:utf-8
+import time
+import os
+import logging
+
+import pymysql
+
+
+logger = logging.getLogger(__name__)
+
+
+
+
+class IsMaster(Exception):
+    """
+    Raised if Replmon is started against a Master MySQL host.
+    """
+
+
+class Replmon(object):
+    def __init__(self, mysql_args, status_file, interval):
+        self.mysql_args = mysql_args
+        self.status_file = status_file
+        self.interval = interval
+
+    def touch_status_file(self):
+        if not os.path.exists(self.status_file):
+            open(self.status_file, "w")
+        os.utime(self.status_file, None)
+
+    def get_connection(self):
+        logger.debug("Connecting to MySQL")
+        conn = pymysql.Connect(**self.mysql_args)
+        conn.cursorclass = pymysql.cursors.DictCursor
+        return conn
+
+    def check_replication(self):
+        with self.get_connection() as cursor:
+            ret = cursor.execute("SHOW SLAVE STATUS")
+            status = cursor.fetchone()
+            logger.debug("Query status: %s", ret)
+
+        if status is None:
+            raise IsMaster()
+
+        return (status["Slave_IO_Running"], status["Slave_SQL_Running"]) == ("Yes", "Yes")
+
+    def run(self):
+        logger.info("Starting monitoring loop")
+        try:
+            while 1:
+                if self.check_replication():
+                    logger.debug("Status: OK")
+                    self.touch_status_file()
+                else:
+                    logger.warning("Status: KO")
+                    time.sleep(self.interval)
+        except IsMaster:
+            logger.warning("Running on master server. Exiting.")
---
+++ packages/replmon/replmon-0.1.2.tar.gz/replmon-0.1.2/setup.py
@@ -0,0 +1,35 @@
+#coding:utf-8
+from setuptools import setup
+
+execfile('replmon/version.py') # Load version
+
+setup(
+    name="replmon",
+    version=__version__,
+    packages=["replmon"],
+    url="https://github.com/scalr-tutorials/replmon",
+    license="Apache 2.0",
+    author="Thomas Orozco",
+    author_email="thomas@scalr.com",
+    install_requires=["python-daemon", "pymysql", "lockfile", "argparse", "procname"],
+    entry_points={
+        'console_scripts': [
+            'replmon = replmon.cli:main',
+            ],
+        },
+    setup_requires=["nose"],
+    tests_require=["tox", "nose", "unittest2"],
+    description="A simple MySQL replication monitor",
+    long_description=open("README.rst").read(),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: System :: Systems Administration",
+    ]
+)
