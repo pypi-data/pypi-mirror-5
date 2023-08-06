24c3faaabe93c072af059f46c57ef7e6da10b7d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:47 +0000

    Add package pyen/2.3/pyen-2.3.tar.gz

---
+++ packages/pyen/pyen-2.3.tar.gz/pyen-2.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='pyen',
+    version='2.3', 
+    description='simple client for The Echo Nest',
+    author="@plamere",
+    author_email="paul@echonest.com",
+    url='http://github.com/plamere/pyen',
+    install_requires=['requests>=1.0',],
+    py_modules=['pyen'],)
