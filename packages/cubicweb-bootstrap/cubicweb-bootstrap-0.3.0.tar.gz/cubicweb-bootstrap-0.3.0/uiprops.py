a4d42bca324dce63ffe5571f30d3ea945bc61564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:30 +0000

    Add package cubicweb-bootstrap/0.3.0/cubicweb-bootstrap-0.3.0.tar.gz

---
+++ packages/cubicweb-bootstrap/cubicweb-bootstrap-0.3.0.tar.gz/cubicweb-bootstrap-0.3.0/uiprops.py
@@ -0,0 +1,11 @@
+# copyright 2012 CreaLibre (Monterrey, MEXICO), all rights reserved.
+# contact http://www.crealibre.com/ -- mailto:info@crealibre.com
+
+"""
+"""
+STYLESHEETS = [data('css/bootstrap.min.css'),
+#               data('css/bootstrap-responsive.min.css'),
+               data('css/custom.css'),
+               data('cubes.bootstrap.css'),
+               ]
+JAVASCRIPTS.append(data('js/bootstrap.min.js'))
---
+++ packages/cubicweb-bootstrap/cubicweb-bootstrap-0.3.0.tar.gz/cubicweb-bootstrap-0.3.0/views/tableview.py
@@ -0,0 +1,3 @@
+from cubicweb.web.views import tableview
+
+tableview.TableLayout.cssclass = 'table table-striped table-bordered table-condensed'
