442a2698db93ecdd85a577cb387146b565ded0e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:11 +0000

    Add package django-choice-object/0.5/django-choice-object-0.5.zip

---
+++ packages/django-choice-object/django-choice-object-0.5.zip/django-choice-object-0.5/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='django-choice-object',
+    version='0.5',
+    packages=['django_choice_object'],
+    url='https://github.com/orf/django-choice-object',
+    license='',
+    author='Tom',
+    author_email='tom@tomforb.es',
+    description='A choice object for Django forms and models'
+)
