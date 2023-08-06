0a66717419f5aaf5098fdbfec3f522ec71714005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:42 +0000

    Add package torweb/0.0.7/torweb-0.0.7.tar.gz

---
+++ packages/torweb/torweb-0.0.7.tar.gz/torweb-0.0.7/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.txt') as file:
+    #long_description = file.read()
+    readlines = file.readlines()
+    long_description = "".join(readlines)
+
+print long_description
+setup(
+    name = "torweb",
+    version = "0.0.7",
+    #packages = find_packages(exclude=["test.*", "test"]),
+    packages = ["torweb"],
+    include_package_data = True,
+    author = "zhangpeng",
+    author_email = "zhangpeng1@infohold.com.cn",
+    url = "",
+    description = "torweb",
+    long_description=long_description,
+    install_requires=[
+        "tornado", "PyYAML", "jinja2", "mysql-python", "storm", "sqlalchemy", "pymemcache"
+    ],
+)
