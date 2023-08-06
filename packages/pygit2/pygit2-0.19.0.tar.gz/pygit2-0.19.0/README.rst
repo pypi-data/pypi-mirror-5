161e5b35c18043e6f2192529e456d96dead75ae9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:51 +0000

    Add package pygit2/0.19.0/pygit2-0.19.0.tar.gz

---
+++ packages/pygit2/pygit2-0.19.0.tar.gz/pygit2-0.19.0/README.rst
@@ -0,0 +1,128 @@
+
+######################################################################
+pygit2 - libgit2 bindings in Python
+######################################################################
+
+.. image:: https://secure.travis-ci.org/libgit2/pygit2.png
+   :target: http://travis-ci.org/libgit2/pygit2
+
+Pygit2 is a set of Python bindings to the libgit2 shared library, libgit2
+implements the core of Git.  Pygit2 works with Python 2.6, 2.7, 3.1, 3.2 and
+3.3
+
+Pygit2 links:
+
+- http://github.com/libgit2/pygit2 -- Source code and issue tracker
+- http://www.pygit2.org/ -- Documentation
+- http://pypi.python.org/pypi/pygit2 -- Download
+
+
+Quick install guide
+===================
+
+1. Checkout the libgit2 stable branch::
+
+   $ git clone git://github.com/libgit2/libgit2.git -b master
+
+2. Build and install libgit2
+   https://github.com/libgit2/libgit2/#building-libgit2---using-cmake
+
+3. Install pygit2 with *pip*::
+
+   $ pip install pygit2
+
+For detailed instructions check the documentation,
+http://www.pygit2.org/install.html
+
+
+Contributing
+============
+
+Fork libgit2/pygit2 on GitHub, make it awesomer (preferably in a branch named
+for the topic), send a pull request.
+
+
+Authors
+==============
+
+This is the list of authors of pygit2, sorted by number of commits (as shown by
+``git shortlog -sn``):
+
+- J David Ibáñez
+- Nico von Geyso
+- W Trevor King
+- Dave Borowitz
+- Carlos Martín Nieto
+- Daniel Rodríguez Troitiño
+- Richo Healey
+- Christian Boos
+- Julien Miotte
+- Martin Lenders
+- Xavier Delannoy
+- Yonggang Luo
+- Valentin Haenel
+- Bernardo Heynemann
+- John Szakmeister
+- David Versmisse
+- Petr Hosek
+- Rémi Duraffort
+- Sebastian Thiel
+- Han-Wen Nienhuys
+- Petr Viktorin
+- Alex Chamberlain
+- Amit Bakshi
+- Andrey Devyatkin
+- Ben Davis
+- Hervé Cauwelier
+- Jared Flatow
+- Jiunn Haur Lim
+- Sarath Lakshman
+- Vicent Marti
+- Zoran Zaric
+- András Veres-Szentkirályi
+- Benjamin Kircher
+- Benjamin Pollack
+- Bryan O'Sullivan
+- David Fischer
+- David Sanders
+- Eric Davis
+- Eric Schrijver
+- Erik van Zijst
+- Ferengee
+- Fraser Tweedale
+- Hugh Cole-Baker
+- Josh Bleecher Snyder
+- Jun Omae
+- Ridge Kennedy
+- Rui Abreu Ferreira
+- Xu Tao
+- pistacchio
+
+
+License
+==============
+
+**GPLv2 with linking exception.**
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License,
+version 2, as published by the Free Software Foundation.
+
+In addition to the permissions in the GNU General Public License,
+the authors give you unlimited permission to link the compiled
+version of this file into combinations with other programs,
+and to distribute those combinations without any restriction
+coming from the use of this file.  (The General Public License
+restrictions do apply in other respects; for example, they cover
+modification of the file, and distribution when not linked into
+a combined executable.)
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; see the file COPYING.  If not, write to
+the Free Software Foundation, 51 Franklin Street, Fifth Floor,
+Boston, MA 02110-1301, USA.
---
+++ packages/pygit2/pygit2-0.19.0.tar.gz/pygit2-0.19.0/test/test_remote.py
@@ -0,0 +1,128 @@
+# -*- coding: UTF-8 -*-
+#
+# Copyright 2010-2013 The pygit2 contributors
+#
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2,
+# as published by the Free Software Foundation.
+#
+# In addition to the permissions in the GNU General Public License,
+# the authors give you unlimited permission to link the compiled
+# version of this file into combinations with other programs,
+# and to distribute those combinations without any restriction
+# coming from the use of this file.  (The General Public License
+# restrictions do apply in other respects; for example, they cover
+# modification of the file, and distribution when not linked into
+# a combined executable.)
+#
+# This file is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; see the file COPYING.  If not, write to
+# the Free Software Foundation, 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301, USA.
+
+"""Tests for Remote objects."""
+
+
+import unittest
+import pygit2
+from . import utils
+
+REMOTE_NAME = 'origin'
+REMOTE_URL = 'git://github.com/libgit2/pygit2.git'
+REMOTE_FETCHSPEC_SRC = 'refs/heads/*'
+REMOTE_FETCHSPEC_DST = 'refs/remotes/origin/*'
+REMOTE_REPO_OBJECTS = 30
+REMOTE_REPO_BYTES = 2758
+
+
+class RepositoryTest(utils.RepoTestCase):
+    def test_remote_create(self):
+        name = 'upstream'
+        url = 'git://github.com/libgit2/pygit2.git'
+
+        remote = self.repo.create_remote(name, url)
+
+        self.assertEqual(type(remote), pygit2.Remote)
+        self.assertEqual(name, remote.name)
+        self.assertEqual(url, remote.url)
+
+        self.assertRaises(ValueError, self.repo.create_remote, *(name, url))
+
+
+    def test_remote_rename(self):
+        remote = self.repo.remotes[0]
+
+        self.assertEqual(REMOTE_NAME, remote.name)
+        remote.name = 'new'
+        self.assertEqual('new', remote.name)
+
+        self.assertRaisesAssign(ValueError, remote, 'name', '')
+
+
+    def test_remote_set_url(self):
+        remote = self.repo.remotes[0]
+
+        self.assertEqual(REMOTE_URL, remote.url)
+        new_url = 'git://github.com/cholin/pygit2.git'
+        remote.url = new_url
+        self.assertEqual(new_url, remote.url)
+
+        self.assertRaisesAssign(ValueError, remote, 'url', '')
+
+
+    def test_refspec(self):
+        remote = self.repo.remotes[0]
+
+        self.assertEqual(remote.refspec_count, 1)
+        refspec = remote.get_refspec(0)
+        self.assertEqual(refspec[0], REMOTE_FETCHSPEC_SRC)
+        self.assertEqual(refspec[1], REMOTE_FETCHSPEC_DST)
+
+#       new_fetchspec = ('refs/foo/*', 'refs/remotes/foo/*')
+#       remote.fetchspec = new_fetchspec
+#       refspec = remote.get_refspec(0)
+#       self.assertEqual(new_fetchspec[0], refspec[0])
+#       self.assertEqual(new_fetchspec[1], refspec[1])
+
+
+    def test_remote_list(self):
+        self.assertEqual(1, len(self.repo.remotes))
+        remote = self.repo.remotes[0]
+        self.assertEqual(REMOTE_NAME, remote.name)
+        self.assertEqual(REMOTE_URL, remote.url)
+
+        name = 'upstream'
+        url = 'git://github.com/libgit2/pygit2.git'
+        remote = self.repo.create_remote(name, url)
+        self.assertTrue(remote.name in [x.name for x in self.repo.remotes])
+
+
+    def test_remote_save(self):
+        remote = self.repo.remotes[0]
+
+        remote.name = 'new-name'
+        remote.url = 'http://example.com/test.git'
+
+        remote.save()
+
+        self.assertEqual('new-name', self.repo.remotes[0].name)
+        self.assertEqual('http://example.com/test.git',
+                         self.repo.remotes[0].url)
+
+
+class EmptyRepositoryTest(utils.EmptyRepoTestCase):
+    def test_fetch(self):
+        remote = self.repo.remotes[0]
+        stats = remote.fetch()
+        self.assertEqual(stats['received_bytes'], REMOTE_REPO_BYTES)
+        self.assertEqual(stats['indexed_objects'], REMOTE_REPO_OBJECTS)
+        self.assertEqual(stats['received_objects'], REMOTE_REPO_OBJECTS)
+
+
+if __name__ == '__main__':
+    unittest.main()
