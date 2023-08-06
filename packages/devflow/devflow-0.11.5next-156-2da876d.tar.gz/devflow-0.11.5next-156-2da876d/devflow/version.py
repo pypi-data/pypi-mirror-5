02fc4202784eb8d7135981bf3c29827b42cabb07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:55 +0000

    Add package devflow/0.11.5next-156-2da876d/devflow-0.11.5next-156-2da876d.tar.gz

---
+++ packages/devflow/devflow-0.11.5next-156-2da876d.tar.gz/devflow-0.11.5next-156-2da876d/devflow/version.py
@@ -0,0 +1,7 @@
+__version__ = "0.11.5next_156_2da876d"
+__version_vcs_info__ = {
+    'branch': 'develop',
+    'revid': '2da876d',
+    'revno': 156}
+__version_user_email__ = "cstavr@grnet.gr"
+__version_user_name__ = "Christos Stavrakakis"
