2d072bfe246dae9520e3999dc340b3589506c32d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:52 +0000

    Add package ak_temp_test/1.2.0/ak_temp_test-1.2.0.tar.gz

---
+++ packages/ak_temp_test/ak_temp_test-1.2.0.tar.gz/ak_temp_test-1.2.0/ak_temp_test.py
@@ -0,0 +1,5 @@
+"""this is temporary module"""
+def ak_temp_print():
+    print( "Hello, everyone!" )
+    print( "I am success!Oh, yeah!" )
+    print( "It is Linux Edition!" )
---
+++ packages/ak_temp_test/ak_temp_test-1.2.0.tar.gz/ak_temp_test-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name         = 'ak_temp_test',
+    version      = '1.2.0',
+    py_modules   = [ 'ak_temp_test' ],
+    author       = 'AresKris',
+    author_email = 'areskris@163.com',
+    url          = 'areskris.blog.163.com',
+    description  = 'This is temporary module',
+    )
