24f7854693930a8510b7b2624a35206b6dbd5130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:11 +0000

    Add package ftw.meeting/1.4.5/ftw.meeting-1.4.5.zip

---
+++ packages/ftw.meeting/ftw.meeting-1.4.5.zip/ftw.meeting-1.4.5/ftw/meeting/tests/builders.py
@@ -0,0 +1,16 @@
+from ftw.builder import builder_registry
+from ftw.builder.archetypes import ArchetypesBuilder
+
+
+class MeetingBuilder(ArchetypesBuilder):
+
+    portal_type = 'Meeting'
+
+builder_registry.register('meeting', MeetingBuilder)
+
+
+class MeetingItemBuilder(ArchetypesBuilder):
+
+    portal_type = 'Meeting Item'
+
+builder_registry.register('meeting item', MeetingItemBuilder)
