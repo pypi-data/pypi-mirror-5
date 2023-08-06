a2e09e3d74dabbe807707c2937673887e779d606
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:54 +0000

    Add package everysport/1.2.1/everysport-1.2.1.tar.gz

---
+++ packages/everysport/everysport-1.2.1.tar.gz/everysport-1.2.1/everysport/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''A Python wrapper for the Everysport API
+'''
+
+from everysport import Everysport
+from everysport import EverysportException
+
+
+
+'''
+    Leagues
+'''
+#Football
+ALLSVENSKAN = 57973
+SUPERETTAN = 57974
+
+#Hockey
+SWISS_LEAGUE = 58882
+SHL = 60243
+NHL = 58878
+
+
+
+
