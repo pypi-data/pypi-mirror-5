dbc3ca23df080c8905a47d40ebe47e9f9a0058e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:55 +0000

    Add package infi.logs_collector/0.0.12/infi.logs_collector-0.0.12.tar.gz

---
+++ packages/infi.logs_collector/infi.logs_collector-0.0.12.tar.gz/infi.logs_collector-0.0.12/src/infi/logs_collector/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.12"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.0.12)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '273bc943ced7e65aaa96c3ab457fce48aaf8a4b7'
+__git_head_subject__ = 'Finished release v0.0.12.'
+__git_head_message__ = '* release/v0.0.12:\n  support overloading of Directory class\n  yet another timestamp format'
+__git_dirty_diff__ = ''
