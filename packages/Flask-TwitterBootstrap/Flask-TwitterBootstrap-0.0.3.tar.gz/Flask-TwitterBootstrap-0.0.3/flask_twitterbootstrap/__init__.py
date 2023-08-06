5ddf570bf76fa12f2fae306c171e3a63ceb908d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:39 +0000

    Add package Flask-TwitterBootstrap/0.0.3/Flask-TwitterBootstrap-0.0.3.tar.gz

---
+++ packages/Flask-TwitterBootstrap/Flask-TwitterBootstrap-0.0.3.tar.gz/Flask-TwitterBootstrap-0.0.3/flask_twitterbootstrap/__init__.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, flash
+
+
+class Bootstrap(Blueprint):
+    def __init__(self, app):
+        super(Bootstrap, self).__init__(
+            'bootstrap', __name__, template_folder='templates'
+        )
+        app.register_blueprint(self)
+
+
+def success(msg):
+    flash(msg, 'success')
