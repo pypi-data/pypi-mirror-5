678def4e310838bba33077b14026b45988e7325d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:27 +0000

    Add package dubizzle/0.0.1-1/dubizzle-0.0.1-1.tar.gz

---
+++ packages/dubizzle/dubizzle-0.0.1-1.tar.gz/dubizzle-0.0.1-1/dubizzle/regionmapper.py
@@ -0,0 +1,11 @@
+# Maps sites to their respective classes
+
+from . import uae
+
+mapper = {
+    'uae': {
+        'search': uae.Search,
+        'results': uae.Results,
+        'listing': uae.Listing,
+    }
+}
