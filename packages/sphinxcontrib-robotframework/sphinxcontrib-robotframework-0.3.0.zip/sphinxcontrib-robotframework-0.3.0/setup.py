4bb9095eff217011b7179f43933336172708912f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:47 +0000

    Add package sphinxcontrib-robotframework/0.3.0/sphinxcontrib-robotframework-0.3.0.zip

---
+++ packages/sphinxcontrib-robotframework/sphinxcontrib-robotframework-0.3.0.zip/sphinxcontrib-robotframework-0.3.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sphinxcontrib-robotframework',
+    version='0.3.0',
+    description='Robot Framework plugin for Sphinx',
+    url='http://github.com/vivekkodu/sphinxcontrib_robotframework',
+    author='Vivek',
+    author_email='1990vivekkumarverma@gmail.com',
+    license='GPL',
+    py_modules=[
+        'sphinxcontrib_robotframework'
+    ],
+    packages=find_packages('src', exclude=['ez_setup']),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'Sphinx',
+        'robotframework',
+        'Pygments',
+    ],
+    extras_require={'docs': [
+        'robotframework',
+        'robotframework-selenium2library',
+        'robotframework-selenium2screenshots[Pillow]',
+    ]}
+)
---
+++ packages/sphinxcontrib-robotframework/sphinxcontrib-robotframework-0.3.0.zip/sphinxcontrib-robotframework-0.3.0/src/sphinxcontrib_robotframework.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+
+import os
+import docutils
+from docutils.parsers.rst import Directive
+import robot
+import tempfile
+
+from sphinx.directives import CodeBlock
+
+
+class RobotAwareCodeBlock(CodeBlock):
+
+    option_spec = dict(
+        docutils.parsers.rst.directives.body.CodeBlock.option_spec.items()
+        + CodeBlock.option_spec.items()
+    )
+
+    def run(self):
+        if u"robotframework" in self.arguments:
+            document = self.state_machine.document
+            robot_source = u"\n".join(self.content)
+            if not hasattr(document, '_robot_source'):
+                document._robot_source = robot_source
+            else:
+                document._robot_source += u"\n" + robot_source
+            if 'hidden' in (self.options.get('class', []) or []):
+                return []  # suppress nodes with :class: hidden
+        return super(RobotAwareCodeBlock, self).run()
+
+
+def creates_option(argument):
+    """Splits list of filenames into a list (and defaults to an empty list).
+    """
+    return filter(bool, argument.split() if argument else [])
+
+
+class RobotSettingsDirective(Directive):
+    """Per-document directive for controlling Robot Framework tests
+    """
+    has_content = False
+    option_spec = {
+        'creates': creates_option,
+    }
+
+    def run(self):
+        document = self.state_machine.document
+        # Stores list of test created artifacts to control test execution:
+        creates = self.options.get('creates', [])
+        if not hasattr(document, '_robot_creates'):
+            document._robot_creates = creates[:]
+        else:
+            document._robot_creates.extend(creates)
+        # Return an empty list of nodes to not affect the resulting docs:
+        return []
+
+
+def run_robot(app, doctree, docname):
+
+    # Tests can be switched off with a global setting:
+    if not app.config.sphinxcontrib_robotframework_enabled:
+        return
+
+    # Set up a variable for "the current working directory":
+    robot_dir = os.path.dirname(os.path.join(app.srcdir, docname))
+
+    # Tests can be made conditional by listing artifacts created by the
+    # tests. When artifacts are listed, tests are only run as long as those
+    # artifacts don't exist:
+    creates_paths = [os.path.join(robot_dir, x)
+                     for x in getattr(doctree, '_robot_creates', [])]
+    missing_paths = [path for path in creates_paths
+                     if not os.path.isfile(path)]
+    if creates_paths and not missing_paths:
+        return
+
+    # Tests are only run when they are found:
+    if not hasattr(doctree, '_robot_source'):
+        return
+
+    # Build a test suite:
+    robot_file = tempfile.NamedTemporaryFile(dir=robot_dir, suffix='.robot')
+    robot_file.write(doctree._robot_source.encode('utf-8'))
+    robot_file.flush()  # flush buffer into file
+
+    # Run the test suite:
+    options = {
+        'outputdir': robot_dir,
+        'output': 'NONE',
+        'log': 'NONE',
+        'report': 'NONE',
+        'variable': [
+            '%s:%s' % key_value for key_value in
+            app.config.sphinxcontrib_robotframework_variables.items()
+        ]
+    }
+    robot.run(robot_file.name, **options)
+
+    # Close the test suite (and delete it, because it's a tempfile):
+    robot_file.close()
+
+    # Re-process images to include robot generated images:
+    if not os.path.sep in docname:
+        app.env.process_images(docname, doctree)
+    else:
+        # XXX: Not sure why, but this seems to be necessary to properly
+        # locate images in a large Sphinx-documentation
+        app.env.process_images(os.path.dirname(docname), doctree)
+
+
+def setup(app):
+    app.add_config_value('sphinxcontrib_robotframework_enabled', True, True)
+    app.add_config_value('sphinxcontrib_robotframework_variables', {}, True)
+    app.add_directive('code', RobotAwareCodeBlock)
+    app.add_directive('robotframework', RobotSettingsDirective)
+    app.connect('doctree-resolved', run_robot)
