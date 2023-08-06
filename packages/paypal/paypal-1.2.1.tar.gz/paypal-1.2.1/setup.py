a515dd0bc38660703c108920ba9dc20a589d764f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:13 +0000

    Add package paypal/1.2.1/paypal-1.2.1.tar.gz

---
+++ packages/paypal/paypal-1.2.1.tar.gz/paypal-1.2.1/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+import re
+from setuptools import setup
+
+VERSION_PATTERN = re.compile(r'VERSION\s*=\s*(.*)$', re.I)
+VERSION = VERSION_PATTERN.search(open('paypal/__init__.py').read()) \
+                         .groups()[0].strip().strip('\'"')
+
+LONG_DESCRIPTION = """An implementation of PayPal's API in Python.
+Currently features Direct Payment (Guest), and PayPal Express checkouts.
+"""
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: Apache Software License',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3',
+    'Topic :: Software Development :: Libraries :: Python Modules'
+]
+
+required = [
+    'requests',
+]
+
+packages = [
+    'paypal',
+]
+
+setup(
+    name='paypal',
+    version=VERSION,
+    description='PayPal API implementation in Python.',
+    long_description=LONG_DESCRIPTION,
+    author='Pat Collins',
+    author_email='pat@burned.com',
+    maintainer='Gregory Taylor',
+    maintainer_email='gtaylor@gc-taylor.com',
+    install_requires=required,
+    url='http://github.com/duointeractive/paypal-python/',
+    download_url='http://pypi.python.org/pypi/paypal/',
+    packages=packages,
+    platforms=['Platform Independent'],
+    license='Apache Software License',
+    classifiers=CLASSIFIERS,
+    keywords='paypal nvp',
+)
---
+++ packages/paypal/paypal-1.2.1.tar.gz/paypal-1.2.1/tests/test_express_checkout.py
@@ -0,0 +1,72 @@
+# coding=utf-8
+
+import unittest
+from . import interface_factory
+from . import api_details
+
+interface = interface_factory.get_interface_obj()
+
+
+class TestExpressCheckout(unittest.TestCase):
+
+    def setUp(self):
+        self.returnurl = 'http://www.paypal.com'
+        self.cancelurl = 'http://www.ebay.com'
+
+    def test_sale(self):
+        """
+        Tests the first part of a sale. At this point, this is a partial unit
+        test. The user has to login to PayPal and approve the transaction,
+        which is not something we have tackled in the unit test yet. So we'll
+        just test the set/get_express_checkout methods.
+
+        A call to `SetExpressCheckoutDetails`.
+        A call to `DoExpressCheckoutPayment`.
+        A call to `GetExpressCheckoutDetails`.
+        """
+        setexp_response = interface.set_express_checkout(
+            amt='10.00',
+            returnurl=self.returnurl, cancelurl=self.cancelurl,
+            paymentaction='Order',
+            email=api_details.EMAIL_PERSONAL
+        )
+
+        self.assertTrue(setexp_response)
+        token = setexp_response.token
+
+        getexp_response = interface.get_express_checkout_details(token=token)
+
+        # Redirect your client to this URL for approval.
+        redir_url = interface.generate_express_checkout_redirect_url(token)
+        # Once they have approved your transaction at PayPal, they'll get
+        # directed to the returnurl value you defined in set_express_checkout()
+        # above. This view should then call do_express_checkout_payment() with
+        # paymentaction = 'Sale'. This will finalize and bill.
+
+    def test_authorize_and_delayed_capture(self):
+        """
+        Tests a four-step checkout process involving the following flow::
+
+            One or more calls to `SetExpressCheckout`.
+            --- User goes to PayPal, logs in, and confirms shipping, taxes,
+                and total amount. ---
+            A call to `GetExpressCheckoutDetails`.
+            A call to `DoExpressCheckoutPayment`.
+            A call to `DoAuthorization`.
+            A call to `DoCapture`.
+        """
+        pass
+
+    def test_authorize_and_void(self):
+        """
+        Tests a four-step checkout process involving the following flow::
+
+            One or more calls to `SetExpressCheckout`.
+            --- User goes to PayPal, logs in, and confirms shipping, taxes,
+                and total amount. ---
+            A call to `GetExpressCheckoutDetails`.
+            A call to `DoExpressCheckoutPayment`.
+            A call to `DoAuthorization`.
+            A call to `DoVoid`.
+        """
+        pass
