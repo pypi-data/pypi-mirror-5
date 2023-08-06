7610be345f3d3e105275f23c660524c27e60e7e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:00 +0000

    Add package django-auth-pack/0.1.1/django-auth-pack-0.1.1.tar.gz

---
+++ packages/django-auth-pack/django-auth-pack-0.1.1.tar.gz/django-auth-pack-0.1.1/auth_pack/forms.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth import authenticate
+from django import forms
+from django.contrib.sites.models import Site
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+class AdminUserCreationForm(forms.ModelForm):
+    """
+    A form for creating new users. Includes all the required fields, plus a
+    repeated password.
+    """
+    error_messages = {
+        'duplicate_email': _("A user with that email already exists."),
+        'password_mismatch': _("The two password fields didn't match."),
+    }
+
+    password1 = forms.CharField(label=_("Password"),
+        widget=forms.PasswordInput)
+    password2 = forms.CharField(label=_("Password confirmation"),
+        widget=forms.PasswordInput,
+        help_text=_("Enter the same password as above, for verification."))
+
+    class Meta:
+        model = User
+        fields = ('email',)
+
+    def clean_email(self):
+        # Since EmailUser.email is unique, this check is redundant,
+        # but it sets a nicer error message than the ORM. See #13147.
+        email = self.cleaned_data["email"]
+        try:
+            User.objects.get(email=email)
+        except User.DoesNotExist:
+            return email
+        raise forms.ValidationError(self.error_messages['duplicate_email'])
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(
+                self.error_messages['password_mismatch'])
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(AdminUserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class AdminUserChangeForm(forms.ModelForm):
+    """
+    A form for updating users. Includes all the fields on the user, but
+    replaces the password field with admin's password hash display field.
+    """
+
+    password = ReadOnlyPasswordHashField(label=_("Password"),
+        help_text=_("Raw passwords are not stored, so there is no way to see "
+                    "this user's password, but you can change the password "
+                    "using <a href=\"password/\">this form</a>."))
+
+    class Meta:
+        model = User
+
+    def __init__(self, *args, **kwargs):
+        super(AdminUserChangeForm, self).__init__(*args, **kwargs)
+        f = self.fields.get('user_permissions', None)
+        if f is not None:
+            f.queryset = f.queryset.select_related('content_type')
+
+    def clean_password(self):
+        # Regardless of what the user provides, return the initial value.
+        # This is done here, rather than on the field, because the
+        # field does not have access to the initial value
+        return self.initial["password"]
+
+
+class UserRegistrationForm(AdminUserCreationForm):
+    def save(self, commit=True):
+        cd = self.cleaned_data
+        site = Site.objects.get_current()
+
+        return User.objects.create_inactive_user(
+            email=cd['email'], password=cd['password1'],
+            site=site
+        )
+
+
+class UserAuthenticationForm(forms.Form):
+    email = forms.EmailField(label=_("Email"), max_length=254)
+    password = forms.CharField(label=_("Password"), widget=forms.PasswordInput)
+
+    error_messages = {
+        'invalid_login': _("Please enter a correct e-mail and password. "
+                           "Note that both fields may be case-sensitive."),
+        'inactive': _("This account is inactive."),
+    }
+
+    def __init__(self, request=None, *args, **kwargs):
+        self.request = request
+        self.user_cache = None
+        super(UserAuthenticationForm, self).__init__(*args, **kwargs)
+
+    def clean(self):
+        email = self.cleaned_data.get('email')
+        password = self.cleaned_data.get('password')
+
+        if email and password:
+            self.user_cache = authenticate(email=email, password=password)
+
+            if self.user_cache is None:
+                raise forms.ValidationError(
+                    self.error_messages['invalid_login'],
+                    code='invalid_login',
+                    params={'email': email},
+                )
+            elif not self.user_cache.is_active:
+                raise forms.ValidationError(
+                    self.error_messages['inactive'],
+                    code='inactive',
+                )
+        return self.cleaned_data
+
+    def get_user_id(self):
+        if self.user_cache:
+            return self.user_cache.id
+        return None
+
+    def get_user(self):
+        return self.user_cache
