3c7b364a8d0ec1964cb01bcaf1cdc5af8668d400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:22 +0000

    Add package begins/0.7.1/begins-0.7.1.tar.gz

---
+++ packages/begins/begins-0.7.1.tar.gz/begins-0.7.1/docs/examples/flask_quickstart.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import begin
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@begin.start(auto_convert=True, env_prefix='WEB_')
+def main(host='127.0.0.1', port=8080, debug=False):
+    app.run(host=host, port=port, debug=debug)
