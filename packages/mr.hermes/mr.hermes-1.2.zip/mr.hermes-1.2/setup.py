a0d3bafacd2cc3daf1bcfc9320836630c0ab0836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:09 +0000

    Add package mr.hermes/1.2/mr.hermes-1.2.zip

---
+++ packages/mr.hermes/mr.hermes-1.2.zip/mr.hermes-1.2/setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup
+
+version = "1.2"
+
+setup(
+    name='mr.hermes',
+    version=version,
+    description="A simple debug smtp server for development.",
+    long_description=open("README.rst").read() + "\n\n" +
+                     open(os.path.join("HISTORY.txt")).read(),
+    author='Florian Schulze',
+    author_email='florian.schulze@gmx.net',
+    url='http://github.com/fschulze/mr.hermes',
+    packages=['mr', 'mr.hermes'],
+    namespace_packages=['mr'],
+    zip_safe=True,
+    install_requires=[
+        'setuptools'])
