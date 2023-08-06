7dd7c9c0e50931798536bdb420169f79b1a43e1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:16 +0000

    Add package pynsive/0.1.5/pynsive-0.1.5.tar.gz

---
+++ packages/pynsive/pynsive-0.1.5.tar.gz/pynsive-0.1.5/pynsive/reflection.py
@@ -0,0 +1,113 @@
+import sys
+import os.path
+import inspect
+
+from .common import *
+
+
+def _list_classes(module, cls_filter):
+    found = list()
+    for name, module_obj in inspect.getmembers(module):
+        if inspect.isclass(module_obj):
+            append = not cls_filter or cls_filter(module_obj)
+            if append:
+                found.append(module_obj)
+    return found
+
+
+def _scan_paths_for(mname, paths):
+    mpath_part = mname.replace(MODULE_PATH_SEP, os.sep)
+    for mpath in paths:
+        scan_target = os.path.join(mpath, mpath_part)
+        is_dir = os.path.isdir(scan_target)
+        is_file = os.path.isfile('{}.py'.format(scan_target))
+        if is_dir or is_file:
+            return mpath
+
+
+def _scan_dir(directory):
+    found = list()
+    module_py = os.path.join(directory, MODULE_INIT_FILE)
+    if os.path.isfile(module_py):
+        # Scan only if there's an __init__.py file
+        for dir_entry in os.listdir(directory):
+            if dir_entry != MODULE_INIT_FILE and dir_entry.endswith('.py'):
+                found.append(dir_entry.rstrip('.py'))
+    return found
+
+
+def _should_use_module_path(module):
+    """
+    Checks to make sure that the passed module has the correct hidden
+    variables set.
+    """
+    return hasattr(module, PATH_ATTRUBITE) and len(module.__path__) > 0
+
+
+def list_modules(mname):
+    """
+    Attempts to list all of the submodules under a module. This function
+    works for modules located in the default path as well as extended paths
+    via the sys.meta_path hooks.
+
+    This function carries the expectation that the hidden module variable
+    '__path__' has been set correctly.
+    """
+    found = list()
+    module = import_module(mname)
+
+    if module and _should_use_module_path(module):
+        mpath = module.__path__[0]
+    else:
+        mpaths = sys.path
+        mpath = _scan_paths_for(mname, mpaths)
+
+    if mpath:
+        for pmname in _scan_dir(mpath):
+            found_mod = MODULE_PATH_SEP.join((mname, pmname))
+            found.append(found_mod)
+    return found
+
+
+def list_classes(mname, cls_filter=None):
+    """
+    Attempts to list all of the classes within a specified module. This
+    function works for modules located in the default path as well as
+    extended paths via the sys.meta_path hooks.
+
+    If a class filter is set, it will be called with each class as its
+    parameter. This filter's return value must be interpretable as a
+    boolean. Results that evaluate as True will include the type in the
+    list of returned classes. Results that evaluate as False will exclude
+    the type in the list of returned classes.
+
+    Keyword arguments:
+    mname -- name of the module to inspect
+    cls_filter -- a function to call to determine what classes should be
+                  included (default None)
+
+    """
+    found = list()
+    module = import_module(mname)
+    if inspect.ismodule(module):
+        [found.append(mod) for mod in _list_classes(module, cls_filter)]
+    return found
+
+
+def rlist_classes(module, cls_filter=None):
+    """
+    Attempts to list all of the classes within a given module namespace.
+    This method, unlike list_classes, will recurse into discovered
+    submodules.
+
+    If a type filter is set, it will be called with each class as its
+    parameter. This filter's return value must be interpretable as a
+    boolean. Results that evaluate as True will include the type in the
+    list of returned classes. Results that evaluate as False will exclude
+    the type in the list of returned classes.
+    """
+    found = list()
+    mnames = list_modules(module)
+    for mname in mnames:
+        [found.append(c) for c in list_classes(mname, cls_filter)]
+    return found
---
+++ packages/pynsive/pynsive-0.1.5.tar.gz/pynsive-0.1.5/tests/plugin_test.py
@@ -0,0 +1,101 @@
+import os
+import shutil
+import tempfile
+import unittest
+import pynsive
+
+
+INIT_PY = """
+from .test_classes import *
+"""
+
+TEST_CLASSES_PY = """
+SUCCESS = True
+
+class PynsiveTestingClass(object):
+    pass
+
+
+class OtherPynsiveTestingClass(PynsiveTestingClass):
+    pass
+"""
+
+
+class WhenLoading(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        This crazy setUp method for the following unit tests creates a
+        temporary plugin directory and then drops a Python module and related
+        testing code into it. The method then spins up a plugin context via
+        a pynsive.PluginManager instance.
+        """
+
+        cls.directory = tempfile.mkdtemp()
+        cls.module_path = os.path.join(cls.directory, 'pynsive_test')
+        os.mkdir(cls.module_path)
+        with open(os.path.join(cls.module_path, '__init__.py'), 'w') as f:
+            f.write(INIT_PY)
+        with open(os.path.join(cls.module_path, 'test_classes.py'), 'w') as f:
+            f.write(TEST_CLASSES_PY)
+        cls.plugin_manager = pynsive.PluginManager()
+        cls.plugin_manager.plug_into(cls.directory)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.plugin_manager.destroy()
+        if cls.directory:
+            shutil.rmtree(cls.directory)
+
+    def test_plugging_into_directory(self):
+        """
+        When plugging into a directory using a PluginManager, the manager
+        will make the new directory available for search when importing
+        modules. These modules must be available for import via the
+        import_module function provided by pynsive.
+        """
+
+        test_module = pynsive.import_module('pynsive_test.test_classes')
+        self.assertTrue(test_module.SUCCESS)
+
+    def test_listing_classes(self):
+        classes = pynsive.list_classes('pynsive_test')
+        self.assertEqual(len(classes), 2)
+
+    def test_listing_classes_with_filter(self):
+        test_module = pynsive.import_module('pynsive_test.test_classes')
+
+        def subclasses_only(test_type):
+            same = test_type is not test_module.PynsiveTestingClass
+            is_subclass = issubclass(
+                test_type, test_module.PynsiveTestingClass)
+            return not same and is_subclass
+
+        classes = pynsive.list_classes('pynsive_test', subclasses_only)
+        self.assertEqual(len(classes), 1)
+
+    def test_crawling_plugged_in_modules(self):
+        found_modules = pynsive.list_modules('pynsive_test')
+        self.assertTrue('pynsive_test.test_classes' in found_modules)
+
+    def test_crawling_modules(self):
+        found_modules = pynsive.list_modules('pynsive.plugin')
+        self.assertTrue('pynsive.plugin.manager' in found_modules)
+        self.assertTrue('pynsive.plugin.loader' in found_modules)
+
+    def test_discovering_classes(self):
+        classes = pynsive.list_classes('pynsive_test')
+        self.assertEqual(2, len(classes))
+
+    def test_recursively_listing_classes_with_filter(self):
+        test_module = pynsive.import_module('pynsive_test.test_classes')
+
+        def subclasses_only(test_type):
+            same = test_type is not test_module.PynsiveTestingClass
+            is_subclass = issubclass(
+                test_type, test_module.PynsiveTestingClass)
+            return not same and is_subclass
+
+        classes = pynsive.rlist_classes('pynsive_test', subclasses_only)
+        self.assertEqual(len(classes), 1)
