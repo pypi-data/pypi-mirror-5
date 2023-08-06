74577f6be82a9e901a78b3ca85261a54ffab6d04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:43 +0000

    Add package mc2/0.1.1/mc2-0.1.1.tar.gz

---
+++ packages/mc2/mc2-0.1.1.tar.gz/mc2-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='mc2',
+    version='0.1.1',
+    author='Graham Arden',
+    author_email='graham@trigfa.org.uk',
+    packages=['mc2'],
+    url='http://pypi.python.org/pypi/mc2/',
+    license='LICENSE.txt',
+    description='Useful plotting tank stuff.',
+    long_description=open('README.md').read(),
+)
