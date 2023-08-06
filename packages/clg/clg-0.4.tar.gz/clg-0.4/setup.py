824fcfe24bb98defbded73df6594c71b05e20909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:25 +0000

    Add package clg/0.4/clg-0.4.tar.gz

---
+++ packages/clg/clg-0.4.tar.gz/clg-0.4/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='clg',
+    version='0.4',
+    author='François Ménabé',
+    author_email='francois.menabe@gmail.com',
+    url='https://clg.readthedocs.org/en/latest/',
+    download_url='https://github.com/fmenabe/python-clg',
+    license='MIT License',
+    description='Command-line generator from a dictionary.',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities'
+    ],
+    py_modules=['clg'],
+    install_requires=['argparse',]
+)
