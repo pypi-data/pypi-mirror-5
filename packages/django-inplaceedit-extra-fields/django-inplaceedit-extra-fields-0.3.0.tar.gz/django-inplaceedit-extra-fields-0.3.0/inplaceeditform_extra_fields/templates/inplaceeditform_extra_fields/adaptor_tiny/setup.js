a8cea0e7f1ff9ff3e2cb623261e13790a9d98239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:58 +0000

    Add package django-inplaceedit-extra-fields/0.3.0/django-inplaceedit-extra-fields-0.3.0.tar.gz

---
+++ packages/django-inplaceedit-extra-fields/django-inplaceedit-extra-fields-0.3.0.tar.gz/django-inplaceedit-extra-fields-0.3.0/inplaceeditform_extra_fields/templates/inplaceeditform_extra_fields/adaptor_tiny/setup.js
@@ -0,0 +1,43 @@
+function (editor) {
+    var self = $.inplaceeditform;
+    setTimeout(function () {
+       $('#' + editor.id).parents(self.formSelector).find('.apply').data('editor', editor);
+       $('#' + editor.id).parents(self.formSelector).find('.cancel').data('editor', editor);
+    }, 300);
+    var value = $('#' + editor.id).html();
+    var form = $('#' + editor.id).parents(self.formSelector);
+    {% ifequal autosavetiny '1' %}
+        editor.on('blur', function () {
+            if (editor.isDirty()) {
+                self.methods.bind(self.methods.autoSaveCallBack, {'oldValue': value,
+                                                              'tag': $('#' + editor.id)})();
+            } else {
+                form.find('.cancel').click();
+            }
+        });
+    {% else %}
+        editor.on('blur', function () {
+            return false;
+        });
+    {% endifequal %}
+    editor.addMenuItem('apply', {
+        text: 'Apply',
+        context: 'edit',
+        onclick: function() {
+            if (form.data('ajaxTime')) {
+                return
+            }
+            form.find('.apply').click();
+        }
+    });
+    editor.addMenuItem('cancel', {
+        text: 'Cancel',
+        context: 'edit',
+        onclick: function() {
+            if (form.data('ajaxTime')) {
+                return
+            }
+            form.find('.cancel').click();
+        }
+    });
+}
