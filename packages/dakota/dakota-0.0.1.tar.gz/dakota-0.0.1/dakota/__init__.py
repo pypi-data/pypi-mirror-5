d1ee3edfb911d871af0ab51e5a5b0c5c852a7d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:15 +0000

    Add package dakota/0.0.1/dakota-0.0.1.tar.gz

---
+++ packages/dakota/dakota-0.0.1.tar.gz/dakota-0.0.1/dakota/__init__.py
@@ -0,0 +1,4 @@
+"""
+This is my python package namespace.
+dakota@tensen.net
+"""
---
+++ packages/dakota/dakota-0.0.1.tar.gz/dakota-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="dakota",
+    version="0.0.1",
+    packages=["dakota"],
+    license="Creative Commons Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0).",
+    long_description=open('README', 'r').read(),
+)
