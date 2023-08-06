fdadda4aea959df3fdc84301a74bc61df84d982c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:36 +0000

    Add package printNestedList/1.2.0/printNestedList-1.2.0.zip

---
+++ packages/printNestedList/printNestedList-1.2.0.zip/printNestedList-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'printNestedList',
+	version = '1.2.0',
+	py_modules = ['printNestedList'],
+	author = 'vinay',
+	author_email = 'vinay.mca08@yahoo.com.au',
+	url = 'http://www.google.com',
+	description = 'Prints nested list items',	
+)
