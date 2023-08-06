836b121ca4d318ad1d4d9f89ee3bedf22eb8c0b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:03 +0000

    Add package display/1.0.0/display-1.0.0.tar.gz

---
+++ packages/display/display-1.0.0.tar.gz/display-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+       name         = 'display',
+       version      = '1.0.0',
+       py_modules   = ['displayfunction'],
+       author       ='xiaozhen',
+       author_email ='1046512973@qq.com',
+       url          ='none',
+       description  ='simple printer of nested list',
+       )
