f3f579f536a9dff538562481cc8cc6f3a80f4135
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:59 +0000

    Add package django-haystack/2.1.0/django-haystack-2.1.0.tar.gz

---
+++ packages/django-haystack/django-haystack-2.1.0.tar.gz/django-haystack-2.1.0/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+
+setup(
+    name='django-haystack',
+    version='2.1.0',
+    description='Pluggable search for Django.',
+    author='Daniel Lindsley',
+    author_email='daniel@toastdriven.com',
+    long_description=open('README.rst', 'r').read(),
+    url='http://haystacksearch.org/',
+    packages=[
+        'haystack',
+        'haystack.backends',
+        'haystack.management',
+        'haystack.management.commands',
+        'haystack.templatetags',
+        'haystack.utils',
+    ],
+    package_data={
+        'haystack': [
+            'templates/panels/*',
+            'templates/search_configuration/*',
+        ]
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities',
+    ],
+    zip_safe=False,
+)
