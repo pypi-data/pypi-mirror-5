e6da14efb027aff389d2e7954852b00ab499014f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:54 +0000

    Add package snf_image_creator/0.5.2next/snf_image_creator-0.5.2next.tar.gz

---
+++ packages/snf_image_creator/snf_image_creator-0.5.2next.tar.gz/snf_image_creator-0.5.2next/image_creator/version.py
@@ -0,0 +1,8 @@
+
+__version__ = "0.5.2next"
+__version_vcs_info__ = {
+    'branch': 'develop',
+    'revid': '694ca1e',
+    'revno': 396}
+__version_user_email__ = "skalkoto@grnet.gr"
+__version_user_name__ = "Nikos Skalkotos"
