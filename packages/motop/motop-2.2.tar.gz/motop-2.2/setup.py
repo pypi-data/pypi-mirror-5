8bae8599507f86e3342f65eaa4a87bf44db38308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:29 +0000

    Add package motop/2.2/motop-2.2.tar.gz

---
+++ packages/motop/motop-2.2.tar.gz/motop-2.2/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from libmotop.Motop import Motop
+
+def readme ():
+    with open ('README') as readmeFile:
+        return readmeFile.read ()
+
+setup (name = 'motop',
+        version = str (Motop.version),
+        packages = ['libmotop'],
+        scripts = ['motop'],
+        install_requires = ['pymongo', 'argparse'],
+        author = 'Emre Hasegeli',
+        author_email = 'hasegeli@tart.com.tr',
+        license = 'ICS',
+        url = 'https://github.com/tart/motop',
+        platforms = 'POSIX',
+        description = '"Top" clone for MongoDB.',
+        keywords = 'mongo realtime monitoring examine explain kill operations',
+        long_description = readme ())
+
