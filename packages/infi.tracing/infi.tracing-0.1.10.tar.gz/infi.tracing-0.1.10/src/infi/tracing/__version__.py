1e321e206aa827515eb3899f6c2ba1d52cecf84b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:01 +0000

    Add package infi.tracing/0.1.10/infi.tracing-0.1.10.tar.gz

---
+++ packages/infi.tracing/infi.tracing-0.1.10.tar.gz/infi.tracing-0.1.10/src/infi/tracing/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.10"
+__git_commiter_name__ = "Tal Yalon"
+__git_commiter_email__ = "tal.yalon@gmail.com"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '99d7f6b7db5d9dfa0477fd07fc7592942b0ad316'
+__git_head_subject__ = 'TRIVIAL trying to remove the pure virtual call when destructing TraceDump'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
