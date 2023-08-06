35b0c97ecaff5f2d7f786c7c1cf9e863bf10f1c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:37 +0000

    Add package staticserve/0.1.0/staticserve-0.1.0.tar.gz

---
+++ packages/staticserve/staticserve-0.1.0.tar.gz/staticserve-0.1.0/HISTORY.rst
@@ -0,0 +1,11 @@
+.. :changelog:
+
+History
+-------
+
+0.1.1 (2013-07-27)
+++++++++++++++++++
+
+* Frustation
+* Inception
+* First release on PyPI
---
+++ packages/staticserve/staticserve-0.1.0.tar.gz/staticserve-0.1.0/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+import static
+
+version = static.__version__
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist upload")
+    print("You probably want to also tag the version now:")
+    print("  git tag -a %s -m 'version %s'" % (version, version))
+    print("  git push --tags")
+    sys.exit()
+
+readme = open('README.rst', 'rt').read()
+history = open('HISTORY.rst', 'rt').read()
+
+setup(
+    name='staticserve',
+    version=version,
+    description='A simple WSGI way to serve static (or mixed) content.',
+    long_description=readme + '\n\n' + history,
+    author='Daniel Greenfeld',
+    author_email='pydanny@gmail.com',
+    url='https://github.com/pydanny/staticserve',
+    license="LGPL",
+    py_modules=['static'],
+    include_package_data=True,
+    zip_safe=False,
+    keywords='wsgi web http static content webapps',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities'
+    ],
+    #test_suite='tests',
+)
