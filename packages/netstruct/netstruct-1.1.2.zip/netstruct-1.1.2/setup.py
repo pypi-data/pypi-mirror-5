887b516b09debedbe11ca83fd267f31df81a8547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:10 +0000

    Add package netstruct/1.1.2/netstruct-1.1.2.zip

---
+++ packages/netstruct/netstruct-1.1.2.zip/netstruct-1.1.2/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+try:
+    with open("README.rst") as file:
+        long_description = file.read()
+except IOError:
+    long_description = "Packed binary data for networking."
+
+setup(
+    name="netstruct",
+    version="1.1.2",
+    description="Packed binary data for networking.",
+    long_description=long_description,
+    author="Stendec",
+    author_email="me@stendec.me",
+    url="https://github.com/stendec/netstruct",
+    py_modules = [ "netstruct" ],
+    
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Internet",
+        "Topic :: Software Development",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy"
+        ],
+    )
