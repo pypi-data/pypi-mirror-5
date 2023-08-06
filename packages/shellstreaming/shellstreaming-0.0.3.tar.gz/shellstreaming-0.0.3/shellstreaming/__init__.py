ae6ac65b829a8592329742fefca485958893e634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:29 +0000

    Add package shellstreaming/0.0.3/shellstreaming-0.0.3.tar.gz

---
+++ packages/shellstreaming/shellstreaming-0.0.3.tar.gz/shellstreaming-0.0.3/shellstreaming/__init__.py
@@ -0,0 +1,10 @@
+'''shellstreaming - explain
+
+foobarbuz
+'''
+
+__versioninfo__ = (0, 0, 3)
+__version__     = '.'.join(map(str, __versioninfo__))
+__author__      = 'Sho Nakatani'
+__email__       = 'lay.sakura@gmail.com'
+__copyright__   = 'Copyright 2013, Sho Nakatani'
