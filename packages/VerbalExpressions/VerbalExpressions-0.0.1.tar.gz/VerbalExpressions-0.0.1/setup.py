b2a7d050727fcf58060e68883fd4d587001d7054
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:54 +0000

    Add package VerbalExpressions/0.0.1/VerbalExpressions-0.0.1.tar.gz

---
+++ packages/VerbalExpressions/VerbalExpressions-0.0.1.tar.gz/VerbalExpressions-0.0.1/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup(
+          name = 'VerbalExpressions',
+          version = '0.0.1',
+          description = 'Make difficult regular expressions easy! Python port of the awesome VerbalExpressions repo - https://github.com/jehna/VerbalExpressions',
+          long_description = '''Please see https://github.com/VerbalExpressions/PythonVerbalExpressions/blob/master/README.md for more information!''',
+          author = "Yan Wenjun, diogobeda, Mihai Ionut Vilcu, Peder Soholt",
+          author_email = "",
+          license = 'MIT',
+          url = 'https://github.com/VerbalExpressions/PythonVerbalExpressions',
+          test_suite='tests',
+          scripts = [],
+          packages = ['verbalexpressions'],
+          classifiers = [
+                          'License :: OSI Approved :: MIT License',
+                          'Programming Language :: Python',
+                          'Topic :: Software Development :: Libraries',
+                          'Topic :: Text Processing'
+                        ],
+          zip_safe=True
+    )
+
