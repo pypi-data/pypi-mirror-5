47bdaacc6b64aeed2fd26d0a7b9505dc9e33e4f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:52 +0000

    Add package lambdaJSON/0.2.11/lambdaJSON-0.2.11.zip

---
+++ packages/lambdaJSON/lambdaJSON-0.2.11.zip/lambdaJSON-0.2.11/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name='lambdaJSON',
+    version='0.2.11',
+    author='Pooya Eghbali',
+    author_email='persian.writer@gmail.com',
+    packages=['lambdaJSON'],
+    url='https://github.com/pooya-eghbali/lambdaJSON',
+    license='LGPLv3',
+    description="""Serialize python standard types (function, tuple, set, frozenset, complex, range, bytes,
+                   bytearray, dict with number keys, byte keys or tuple keys, and etc) with json. 
+                   No dependencies, just needs json lib (or other json libraries). --Changes: 
+                   important bug fix, added version and json identifier""",
+    long_description=open('README.txt').read(),
+    classifiers= ['Intended Audience :: Developers',
+                  'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+                  'Operating System :: OS Independent',
+                  'Programming Language :: Python :: 2',
+                  'Programming Language :: Python :: 3'],
+    keywords = 'json, serialization, serialize, pickle, marshal',
+    platforms= ['Any']
+)
