6efa402e1745b29437569a37646f65a44d1c273f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:56 +0000

    Add package microurl/0.0.2/microurl-0.0.2.tar.gz

---
+++ packages/microurl/microurl-0.0.2.tar.gz/microurl-0.0.2/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+      name='microurl',
+      version='0.0.2',
+      #install_requires=['lxml', 'PIL'],
+      description='microurl is a python module to create minfied urls',
+      author='MicroPyramid',
+      author_email='MicroPyramid@googlegroups.com',
+      maintainer='Archana, Ashwin',
+      maintainer_email='MicroPyramid@googlegroups.com',
+      url='http://github.com/micropyramid/microurl',
+      py_modules=['microurl'],
+      
+      license=open('LICENSE').read(),
+      long_description=open('README.rst').read(),
+      
+      keywords = 'Url minifier python library using google, bitly, supr, dottk',
+      packages=['microurl'],
+      include_package_data=True,
+      zip_safe=True,
+      )
+      
+      
+      
+      
