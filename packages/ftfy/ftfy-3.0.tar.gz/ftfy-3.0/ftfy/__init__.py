869f810c472689ef83aece6b837278f2a905d1e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:24 +0000

    Add package ftfy/3.0/ftfy-3.0.tar.gz

---
+++ packages/ftfy/ftfy-3.0.tar.gz/ftfy-3.0/ftfy/__init__.py
@@ -0,0 +1,208 @@
+# -*- coding: utf-8 -*-
+"""
+ftfy: fixes text for you
+
+This is a module for making text less broken. See the `fix_text` function
+for more information.
+"""
+
+from __future__ import unicode_literals
+from ftfy import fixes
+from ftfy.fixes import fix_text_encoding
+import unicodedata
+import warnings
+
+
+def fix_text(text,
+             fix_entities=True,
+             remove_terminal_escapes=True,
+             fix_encoding=True,
+             normalization='NFKC',
+             uncurl_quotes=True,
+             fix_line_breaks=True,
+             remove_control_chars=True,
+             remove_bom=True,
+             max_decode_length=2**16):
+    """
+    Given Unicode text as input, make its representation consistent and
+    possibly less broken, by applying these steps in order:
+
+    - If `remove_terminal_escapes` is True, remove sequences of bytes that are
+      instructions for Unix terminals, such as the codes that make text appear
+      in different colors.
+    - If `fix_entities` is True, consider replacing HTML entities with their
+      equivalent characters. However, this never applies to text with a pair
+      of angle brackets in it already; you're probably not supposed to decode
+      entities there, and you'd make things ambiguous if you did.
+    - If `fix_encoding` is True, look for common mistakes that come from
+      encoding or decoding Unicode text incorrectly, and fix them if they are
+      reasonably fixable. See `fix_text_encoding` for details.
+    - If `normalization` is not None, apply the specified form of Unicode
+      normalization, which can be one of 'NFC', 'NFKC', 'NFD', and 'NFKD'.
+      The default, 'NFKC', applies the following relevant transformations:
+      - C: Combine characters and diacritics that are written using separate
+        code points, such as converting "e" plus an acute accent modifier
+        into "é", or converting "ka" (か) plus a dakuten into the
+        single character "ga" (が).
+      - K: Replace characters that are functionally equivalent with the most
+        common form. For example, half-width katakana will be replaced with
+        full-width versions, full-width Roman characters will be replaced with
+        ASCII characters, ellipsis characters will be replaced with three
+        periods, and the ligature 'ﬂ' will be replaced with 'fl'.
+    - If `uncurl_quotes` is True, replace various curly quotation marks with
+      plain-ASCII straight quotes.
+    - If `fix_line_breaks` is true, convert all line breaks to Unix style
+      (CRLF and CR line breaks become LF line breaks).
+    - If `fix_control_characters` is true, remove all C0 control characters
+      except the common useful ones: TAB, CR, LF, and FF. (CR characters
+      may have already been removed by the `fix_line_breaks` step.)
+    - If `remove_bom` is True, remove the Byte-Order Mark if it exists.
+    - If anything was changed, repeat all the steps, so that the function is
+      idempotent. "&amp;amp;" will become "&", for example, not "&amp;".
+
+        >>> print(fix_text('uÌˆnicode'))
+        ünicode
+
+        >>> print(fix_text('Broken text&hellip; it&#x2019;s ﬂubberiﬁc!'))
+        Broken text... it's flubberific!
+
+        >>> print(fix_text('HTML entities &lt;3'))
+        HTML entities <3
+
+        >>> print(fix_text('<em>HTML entities &lt;3</em>'))
+        <em>HTML entities &lt;3</em>
+
+        >>> print(fix_text('\001\033[36;44mI&#x92;m blue, da ba dee da ba '
+        ...               'doo&#133;\033[0m'))
+        I'm blue, da ba dee da ba doo...
+
+        >>> print(fix_text('\\ufeffParty like\\nit&rsquo;s 1999!'))
+        Party like
+        it's 1999!
+
+        >>> len(fix_text('ﬁ' * 100000))
+        200000
+
+        >>> len(fix_text(''))
+        0
+
+    `fix_text` will work one line at a time, with the possibility that some
+    lines are in different encodings.
+
+    If you are certain your entire text is in the same encoding (though that
+    encoding is possibly flawed), and do not mind performing operations on
+    the whole text at once, use `fix_text_segment`.
+    """
+    if isinstance(text, bytes):
+        raise UnicodeError(fixes.BYTES_ERROR_TEXT)
+
+    out = []
+    pos = 0
+    while pos < len(text):
+        textbreak = text.find('\n', pos) + 1
+        fix_encoding_this_time = fix_encoding
+        if textbreak == 0:
+            textbreak = len(text)
+        if (textbreak - pos) > max_decode_length:
+            fix_encoding_this_time = False
+
+        substring = text[pos:textbreak]
+
+        if '<' in substring and '>' in substring:
+            # we see angle brackets together; this could be HTML
+            fix_entities = False
+
+        out.append(
+            fix_text_segment(
+                substring,
+                fix_entities=fix_entities,
+                remove_terminal_escapes=remove_terminal_escapes,
+                fix_encoding=fix_encoding_this_time,
+                normalization=normalization,
+                uncurl_quotes=uncurl_quotes,
+                fix_line_breaks=fix_line_breaks,
+                remove_control_chars=remove_control_chars,
+                remove_bom=remove_bom
+            )
+        )
+        pos = textbreak
+
+    return ''.join(out)
+
+ftfy = fix_text
+
+
+def fix_file(file, normalization='NFKC'):
+    """
+    Fix text that is found in a file.
+
+    If the file is being read as Unicode text, use that. If it's being read as
+    bytes, then we'll try to decode each line as Latin-1, and then fix it if it
+    was really UTF-8 or something.
+
+    That's kind of awful, but it will have to suffice until we have a working
+    encoding detector.
+
+    The output is a stream of fixed lines of text.
+    """
+    entities = True
+    for line in file:
+        if isinstance(line, bytes):
+            line = line.decode('latin-1')
+        if '<' in line and '>' in line:
+            entities = False
+        yield fix_text_segment(line, normalization, entities)
+
+
+def fix_text_segment(text,
+                     fix_entities=True,
+                     remove_terminal_escapes=True,
+                     fix_encoding=True,
+                     normalization='NFKC',
+                     uncurl_quotes=True,
+                     fix_line_breaks=True,
+                     remove_control_chars=True,
+                     remove_bom=True):
+    """
+    Apply fixes to text in a single chunk. This could be a line of text
+    within a larger run of `fix_text`, or it could be a larger amount
+    of text that you are certain is all in the same encoding.
+
+    See `fix_text` for a description of the parameters.
+    """
+    if isinstance(text, bytes):
+        raise UnicodeError(fixes.BYTES_ERROR_TEXT)
+
+    unsafe_entities = ('<' in text and '>' in text)
+    while True:
+        origtext = text
+        if fix_entities and not unsafe_entities:
+            text = fixes.unescape_html(text)
+        if remove_terminal_escapes:
+            text = fixes.remove_terminal_escapes(text)
+        if fix_encoding:
+            text = fixes.fix_text_encoding(text)
+        if normalization is not None:
+            text = unicodedata.normalize(normalization, text)
+        if uncurl_quotes:
+            text = fixes.uncurl_quotes(text)
+        if fix_line_breaks:
+            text = fixes.fix_line_breaks(text)
+        if remove_control_chars:
+            text = fixes.remove_control_chars(text)
+        if remove_bom:
+            text = fixes.remove_bom(text)
+        if text == origtext:
+            return text
+
+
+def fix_bad_encoding(text):
+    """
+    Kept for compatibility with previous versions of ftfy.
+    """
+    warnings.warn(
+        'fix_bad_encoding is now known as fix_text_encoding',
+        DeprecationWarning
+    )
+    return fix_text_encoding(text)
+
---
+++ packages/ftfy/ftfy-3.0.tar.gz/ftfy-3.0/ftfy/badness.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+"""
+Heuristics to determine whether re-encoding text is actually making it
+more reasonable.
+"""
+
+from __future__ import unicode_literals
+from ftfy.chardata import chars_to_classes
+import re
+import unicodedata
+
+# The following regex uses the mapping of character classes to ASCII
+# characters defined in chardata.py and build_data.py:
+#
+# L = Latin capital letter
+# l = Latin lowercase letter
+# A = Non-latin capital or title-case letter
+# a = Non-latin lowercase letter
+# C = Non-cased letter (Lo)
+# X = Control character (Cc)
+# m = Letter modifier (Lm)
+# M = Mark (Mc, Me, Mn)
+# N = Miscellaneous numbers (No)
+# 0 = Math symbol (Sm)
+# 1 = Currency symbol (Sc)
+# 2 = Symbol modifier (Sk)
+# 3 = Other symbol (So)
+# S = UTF-16 surrogate
+# _ = Unassigned character
+#   = Whitespace
+# o = Other
+
+def _make_weirdness_regex():
+    """
+    Creates a list of regexes that match 'weird' character sequences.
+    The more matches there are, the weirder the text is.
+    """
+    groups = []
+
+    # Match lowercase letters that are followed by non-ASCII uppercase letters
+    groups.append('lA')
+
+    # Match diacritical marks, except when they modify a non-cased letter or
+    # another mark.
+    #
+    # You wouldn't put a diacritical mark on a digit or a space, for example.
+    # You might put it on a Latin letter, but in that case there will almost
+    # always be a pre-composed version, and we normalize to pre-composed
+    # versions first. The cases that can't be pre-composed tend to be in
+    # large scripts without case, which are in class C.
+    groups.append('[^CM]M')
+
+    # Match non-Latin characters adjacent to Latin characters.
+    #
+    # This is a simplification from ftfy version 2, which compared all
+    # adjacent scripts. However, the ambiguities we need to resolve come from
+    # encodings designed to represent Latin characters.
+    groups.append('[Ll][AaC]')
+    groups.append('[AaC][Ll]')
+
+    # Match C1 control characters, which are almost always the result of
+    # decoding Latin-1 that was meant to be Windows-1252.
+    groups.append('X')
+
+    # Match adjacent characters from any different pair of these categories:
+    # - Modifier marks (M)
+    # - Letter modifiers (m)
+    # - Miscellaneous numbers (N)
+    # - Symbols (0123)
+
+    exclusive_categories = 'MmN0123'
+    for cat1 in exclusive_categories:
+        others_range = ''.join(c for c in exclusive_categories if c != cat1)
+        groups.append('{cat1}[{others_range}]'.format(
+            cat1=cat1, others_range=others_range
+        ))
+    regex = '|'.join('({0})'.format(group) for group in groups)
+    return re.compile(regex)
+
+WEIRDNESS_RE = _make_weirdness_regex()
+
+
+def sequence_weirdness(text):
+    """
+    Determine how often a text has unexpected characters or sequences of
+    characters. This metric is used to disambiguate when text should be
+    re-decoded or left as is.
+
+    We start by normalizing text in NFC form, so that penalties for
+    diacritical marks don't apply to characters that know what to do with
+    them.
+    """
+    text2 = unicodedata.normalize('NFC', text)
+    return len(WEIRDNESS_RE.findall(chars_to_classes(text2)))
+
+
+def better_text(newtext, oldtext):
+    """
+    Is newtext better than oldtext?
+    """
+    return text_cost(newtext) < text_cost(oldtext)
+
+
+def text_cost(text):
+    """
+    An overall cost function for text. All else being equal, shorter strings
+    are better.
+    """
+    return sequence_weirdness(text) * 2 + len(text)
+
