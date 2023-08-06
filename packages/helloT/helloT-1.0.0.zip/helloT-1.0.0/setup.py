5ecdc820dd52d27a08749697ebad089d4798c4c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:48 +0000

    Add package helloT/1.0.0/helloT-1.0.0.zip

---
+++ packages/helloT/helloT-1.0.0.zip/helloT-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'helloT',
+	version		= '1.0.0',
+	py_modules	= ['helloT'],
+	author		= 'houlu',
+	author_email= 'houlu8674@sina.com',
+	url			= 'www.sina.com.cn',
+	description = 'A simple program',
+	)
