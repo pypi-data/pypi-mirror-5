68f3dd8418d86cabba0cff83ef47de7455406b9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:27 +0000

    Add package stfc_stratum_uploader/0.1.4/stfc_stratum_uploader-0.1.4.linux-x86_64.tar.gz

---
+++ packages/stfc_stratum_uploader/stfc_stratum_uploader-0.1.4.linux-x86_64.tar.gz/usr/local/lib/python2.7/dist-packages/archer/urls.py
@@ -0,0 +1,24 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+from archer.projects import views
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+                       # Examples:
+                       # url(r'^$', 'archer.views.home', name='home'),
+
+                       # Uncomment the admin/doc line below to enable admin documentation:
+                       # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+                       # url(r'^grappelli/', include('grappelli.urls')),
+                       url(r'^$', include('archer.projects.urls'), name='index'),
+                       url(r'^setup/', include('archer.appsetup.urls', namespace='appsetup')),
+                       url(r'^projects/', include('archer.projects.urls', namespace='projects')),
+                       url(r'^packages/', include('archer.packages.urls', namespace='packages')),
+                       # Uncomment the next line to enable the admin:
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^unauthenticated$', views.unauthenticated, name='unauthenticated')
+)
