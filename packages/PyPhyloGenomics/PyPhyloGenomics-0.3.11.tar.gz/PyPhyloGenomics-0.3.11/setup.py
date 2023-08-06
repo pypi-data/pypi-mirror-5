ede75b6142d9fd985d6637c7b697153b6c879ab6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:55 +0000

    Add package PyPhyloGenomics/0.3.11/PyPhyloGenomics-0.3.11.tar.gz

---
+++ packages/PyPhyloGenomics/PyPhyloGenomics-0.3.11.tar.gz/PyPhyloGenomics-0.3.11/setup.py
@@ -0,0 +1,33 @@
+#from distutils.core import setup
+from distribute_setup import use_setuptools
+use_setuptools()
+from setuptools import setup, find_packages
+
+
+setup(
+		name='PyPhyloGenomics', 
+		version='0.3.11',
+		packages = ['pyphylogenomics'],
+		author="Carlos Pena, Victor Solis, Pavel Matos, Chris Wheat",
+		author_email="mycalesis@gmail.com",
+		maintainer="Carlos Pena, Victor Solis, Pavel Matos, Chris Wheat",
+		maintainer_email="mycalesis@gmail.com",
+		contact="Carlos Pena",
+		contact_email="mycalesis@gmail.com",
+		license="GPL v3",
+		description="Tools to work in phylogenomics, from NSG group http://nymphalidae.utu.fi",
+		long_description=open('README.txt').read(),
+		keywords="DNA, genomics, genomes, phylogenetics, genes",
+		url="http://carlosp420.github.com/PyPhyloGenomics/",
+		download_url = "http://carlosp420.github.com/PyPhyloGenomics/",
+		classifiers=[
+					"Programming Language :: Python",
+					("Topic :: Scientific/Engineering :: Bio-Informatics"),
+					("Intended Audience :: Science/Research"),
+					("License :: OSI Approved :: GNU General Public License v2 (GPLv2)"),
+					("Operating System :: OS Independent"),
+					("Environment :: Console"),
+					],
+
+		install_requires=['biopython','pp','requests','beautifulsoup4']
+		);
