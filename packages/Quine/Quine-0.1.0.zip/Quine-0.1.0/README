00a72d2a829892e87d27f3a30231132630ad73d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:42 +0000

    Add package Quine/0.1.0/Quine-0.1.0.zip

---
+++ packages/Quine/Quine-0.1.0.zip/Quine-0.1.0/README
@@ -0,0 +1,5 @@
+=====
+Quine
+=====
+
+Small modules for generating programs that are able to print themselves.
---
+++ packages/Quine/Quine-0.1.0.zip/Quine-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='Quine',
+    version='0.1.0',
+    author='Lucas Boppre Niehues',
+    author_email='lucasboppre@gmail.com',
+    packages=['quine'],
+    url='http://pypi.python.org/pypi/Quine/',
+    license='LICENSE.txt',
+    description='Quine generator.',
+    long_description=open('README').read(),
+)
