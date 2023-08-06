26242f117210b1476b036464732770b9f75f9475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:33 +0000

    Add package property-caching/1.0.0/property-caching-1.0.0.tar.gz

---
+++ packages/property-caching/property-caching-1.0.0.tar.gz/property-caching-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='property-caching',
+    version='1.0.0',
+    description='Property caching',
+    author='Yola',
+    author_email='engineers@yola.com',
+    license='MIT (Expat)',
+    url='https://github.com/yola/property-caching',
+    packages=['property_caching'],
+)
