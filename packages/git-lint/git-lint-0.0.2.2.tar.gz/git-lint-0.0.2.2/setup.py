2eb82e04492aaedb9f46b5a23592ae3d73d19bd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:48 +0000

    Add package git-lint/0.0.2.2/git-lint-0.0.2.2.tar.gz

---
+++ packages/git-lint/git-lint-0.0.2.2.tar.gz/git-lint-0.0.2.2/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# Copyright 2013 Sebastian Kreft
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from setuptools import setup, find_packages
+
+setup(name='git-lint',
+      version='0.0.2.2',
+      description='Git Lint',
+      author='Sebastian Kreft',
+      url='http://github.com/sk-/git-lint',
+      packages=find_packages(),
+      package_dir={'gitlint': 'gitlint'},
+      package_data={'gitlint': ['*.yaml']},
+      scripts=['scripts/git-lint'],
+      install_requires=['nose', 'mock', 'pyyaml', 'termcolor', 'docopt'],
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Version Control',
+    ],
+     )
