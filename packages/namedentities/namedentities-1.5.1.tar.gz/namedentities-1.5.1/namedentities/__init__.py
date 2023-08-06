cb76fb3c147d939eac184da93a7784c5f2bd33e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:56 +0000

    Add package namedentities/1.5.1/namedentities-1.5.1.tar.gz

---
+++ packages/namedentities/namedentities-1.5.1.tar.gz/namedentities-1.5.1/namedentities/__init__.py
@@ -0,0 +1,3 @@
+from namedentities.core import named_entities, numeric_entities, hex_entities, \
+                               unicode_entities, unescape, \
+                               encode_ampersands
