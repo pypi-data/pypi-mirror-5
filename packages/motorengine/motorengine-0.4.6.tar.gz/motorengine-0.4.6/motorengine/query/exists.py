a98f4bff8325aad11e57f10b3f8fa6010f9ec51a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:26 +0000

    Add package motorengine/0.4.6/motorengine-0.4.6.tar.gz

---
+++ packages/motorengine/motorengine-0.4.6.tar.gz/motorengine-0.4.6/motorengine/query/exists.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from motorengine.query.base import QueryOperator
+
+
+class ExistsQueryOperator(QueryOperator):
+    def to_query(self, field_name, value):
+        return {
+            field_name: {
+                "$exists": value
+            }
+        }
+
+    def get_value(self, field, value):
+        return value
