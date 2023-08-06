29397d1be7a44055dec90f092401acfcc7b98629
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:17 +0000

    Add package mdtime/1.0.1a2/mdtime-1.0.1a2.tar.gz

---
+++ packages/mdtime/mdtime-1.0.1a2.tar.gz/mdtime-1.0.1a2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(name='mdtime',
+      version='1.0.1a2',
+      author='Zhuyi Xue',
+      author_email='alfred532008@gmail.com',
+      url='http://www.zhuyixue.com',
+      keywords = 'molecular dynamics gromacs time',
+      license='GPLv3',
+      description='mdtime checks the time of a tpr file or cpt file from Gromacs, or compare the two to see if time in cpt is less than that in tpr',
+      long_description='supposed to be a long description',
+      platforms=['unix-like'],
+
+      packages=find_packages(),
+      scripts=['mdtime.py']
+      )
