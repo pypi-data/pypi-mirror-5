febdbc56e53228c7ee04dcb25137ce21b13e6840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:09 +0000

    Add package ExEmGel/0.1/ExEmGel-0.1.tar.gz

---
+++ packages/ExEmGel/ExEmGel-0.1.tar.gz/ExEmGel-0.1/exemgel/test_doc.py
@@ -0,0 +1,11 @@
+import doc
+import unittest
+import data_for_testing
+
+class DocTestCases(unittest.TestCase):
+    def testsimple(self):
+        d = doc.Doc(data_for_testing.simple_xml_file_obj())
+        self.assertEquals(d.guidata.skin.scoreFontName, "Tahoma Bold")
+
+
+
