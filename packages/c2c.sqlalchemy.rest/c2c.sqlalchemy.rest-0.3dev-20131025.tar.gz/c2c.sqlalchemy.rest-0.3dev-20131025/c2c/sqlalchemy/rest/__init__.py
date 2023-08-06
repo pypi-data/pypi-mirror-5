2267a45685f7ea18e2eb6ab1d949a02a90742f8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:07 +0000

    Add package c2c.sqlalchemy.rest/0.3dev-20131025/c2c.sqlalchemy.rest-0.3dev-20131025.tar.gz

---
+++ packages/c2c.sqlalchemy.rest/c2c.sqlalchemy.rest-0.3dev-20131025.tar.gz/c2c.sqlalchemy.rest-0.3dev-20131025/c2c/sqlalchemy/rest/__init__.py
@@ -0,0 +1,166 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+from json import loads
+from sqlalchemy import or_
+from sqlalchemy.orm.util import class_mapper
+from sqlalchemy.ext.associationproxy import _AssociationList
+from sqlalchemy.orm.properties import ColumnProperty, RelationshipProperty
+from sqlalchemy.orm.exc import NoResultFound
+from pyramid.response import Response
+from pyramid.security import has_permission
+from pyramid.httpexceptions import HTTPNotFound, HTTPForbidden, HTTPBadRequest
+from geoalchemy.postgis import PGPersistentSpatialElement
+from shapely.wkb import loads as loadsWKB
+
+def add_rest_routes(config, route_name_prefix, base_url, create=True):
+    route_name = route_name_prefix + '_read_many'
+    config.add_route(route_name, base_url, request_method='GET')
+    route_name = route_name_prefix + '_count'
+    config.add_route(route_name, base_url + '/count', request_method='GET')
+    route_name = route_name_prefix + '_read_one'
+    config.add_route(route_name, base_url + '/{id:\\d+}', request_method='GET')
+    if create:
+        route_name = route_name_prefix + '_create'
+        config.add_route(route_name, base_url, request_method='POST')
+    route_name = route_name_prefix + '_update'
+    config.add_route(route_name, base_url + '/{id:\\d+}', request_method='PUT')
+    route_name = route_name_prefix + '_delete'
+    config.add_route(route_name, base_url + '/{id:\\d+}', request_method='DELETE')
+
+
+class REST(object):
+    def __init__(self, session, mapped_class, children=None):
+        self.session = session
+        self.mapped_class = mapped_class
+
+        self.columns = []
+        self.relationships = {}
+        self.id = None
+
+        for p in class_mapper(mapped_class).iterate_properties:
+            if isinstance(p, ColumnProperty):
+                if len(p.columns) != 1:
+                    raise NotImplementedError
+                col = p.columns[0]
+                if col.primary_key:
+                    self.id = p.key
+                elif not col.foreign_keys:
+                    self.columns.append(p.key)
+            elif children is not None and \
+                    isinstance(p, RelationshipProperty) and \
+                    p.key in children.keys():
+                rel = children[p.key]
+                if 'rest' not in rel or not isinstance(rel['rest'], REST):
+                    raise HTTPBadRequest("Missing REST object for relationship %s"
+                        % p.key)
+                self.relationships[p.key] = rel
+
+    def _properties(self, obj):
+        properties = {}
+        for col in self.columns:
+            attr = getattr(obj, col)
+            if isinstance(attr, (datetime.date, datetime.datetime)):
+                attr = attr.isoformat()
+            elif isinstance(attr, _AssociationList):
+                attr = list(attr)
+            elif isinstance(attr, PGPersistentSpatialElement):
+                attr = loadsWKB(str(attr.geom_wkb))
+            properties[col] = attr
+        properties[self.id] = getattr(obj, self.id)
+        for key in self.relationships:
+            rel = self.relationships[key]
+            data = [rel['rest']._properties(o) for o in getattr(obj, key)]
+            propname = rel['propname'] if 'propname' in rel else key
+            properties[propname] = data
+            
+        return properties
+
+    def read_many(self, request):
+        if has_permission('view', self.mapped_class, request) \
+                or not hasattr(self.mapped_class, '__acl__'):
+            query = self.session().query(self.mapped_class)
+            return {
+                "objects": [self._properties(o) for o in query.all()]
+            }
+        raise HTTPForbidden()
+
+    def _read_one(self, request, session=None):
+        if not session:
+            session = self.session()
+        id = request.matchdict.get('id', None)
+        query = session.query(self.mapped_class)
+        query = query.filter(getattr(self.mapped_class, self.id) == int(id))
+        try:
+            o = query.one()
+            return o
+        except NoResultFound:
+            raise HTTPNotFound("No %s found with id: %s." % (
+                self.mapped_class.__name__,
+                id
+            ))
+
+    def read_one(self, request):
+        if has_permission('view', self.mapped_class, request) \
+                or not hasattr(self.mapped_class, '__acl__'):
+            o = self._read_one(request)
+            return self._properties(o)
+        raise HTTPForbidden()
+
+    def count(self, request):
+        if has_permission('view', self.mapped_class, request) \
+                or not hasattr(self.mapped_class, '__acl__'):
+            return self.session().query(self.mapped_class).count()
+        raise HTTPForbidden()
+
+    def _fill_properties(self, request, session, obj):
+        body = loads(request.body)
+        for col in self.columns:
+            if col in body:
+                setattr(obj, col, body[col])
+        for key in self.relationships:
+            rel = self.relationships[key]
+            propname = rel['propname'] if 'propname' in rel else key
+            if propname in body:
+                ids = body[propname]
+                if not isinstance(ids, list):
+                    ids = [ids]
+                if len(ids) > 0:
+                    rest = rel['rest']
+                    clause = or_(*[getattr(rest.mapped_class, rest.id)==int(id) for id in ids])
+                    children = session.query(rest.mapped_class).filter(clause).all()
+                else:
+                    children = []
+                setattr(obj, key, children)
+
+    def create(self, request):
+        if has_permission('new', self.mapped_class, request) \
+                or not hasattr(self.mapped_class, '__acl__'):
+            session = self.session()
+            obj = self.mapped_class()
+            self._fill_properties(request, session, obj)
+            session.add(obj)
+            session.flush()
+            request.response.status_int = 201
+            return getattr(obj, self.id)
+        raise HTTPForbidden()
+
+    def update(self, request):
+        if has_permission('edit', self.mapped_class, request) \
+                or not hasattr(self.mapped_class, '__acl__'):
+            session = self.session()
+            obj = self._read_one(request, session)
+            self._fill_properties(request, session, obj)
+            session.flush()
+            return Response(status_int=202)
+        raise HTTPForbidden()
+
+    def delete(self, request):
+        if has_permission('delete', self.mapped_class, request) \
+                or not hasattr(self.mapped_class, '__acl__'):
+            session = self.session()
+            obj = self._read_one(request, session)
+            session.delete(obj)
+            session.flush()
+            return Response(status_int=204)
+        raise HTTPForbidden()
