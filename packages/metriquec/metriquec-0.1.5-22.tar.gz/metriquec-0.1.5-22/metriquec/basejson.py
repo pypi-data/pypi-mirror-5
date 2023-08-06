4b631dc56ffaed4c72504ea9fe725ac4a6d5a7ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:39 +0000

    Add package metriquec/0.1.5-22/metriquec-0.1.5-22.tar.gz

---
+++ packages/metriquec/metriquec-0.1.5-22.tar.gz/metriquec-0.1.5-22/metriquec/basejson.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+# Author: "Chris Ward <cward@redhat.com>
+
+'''
+Base cube for extracting data from JSON documents
+'''
+
+import simplejson as json
+
+from metriquec.basecube import BaseCube
+
+
+class BaseJSON(BaseCube):
+    """
+    Object used for extracting data in JSON format
+    """
+
+    def loads(self, json_str, strict=False):
+        '''
+        Given a string of valid JSON, load it into memory.
+
+        No strict loading (DEFAULT); ignore control characters
+        '''
+        # 'strict=False: control characters will be allowed in strings'
+        return json.loads(json_str, strict=strict)
+
+    def loadi(self, json_iter):
+        '''
+        Given an iterator object, convert it to simple
+        joined string, then try to load the json
+        as as a string.
+        '''
+        return self.loads(''.join(json_iter))
