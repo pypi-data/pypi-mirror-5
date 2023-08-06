1be8354f7afc5472aa3bf58556df6855a2ad4d96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:06 +0000

    Add package twine/1.1.0/twine-1.1.0.tar.bz2

---
+++ packages/twine/twine-1.1.0.tar.bz2/twine-1.1.0/setup.py
@@ -0,0 +1,78 @@
+# Copyright 2013 Donald Stufft
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from setuptools import setup, find_packages
+
+import sys
+
+
+about = {}
+with open("twine/__about__.py") as fp:
+    exec(fp.read(), about)
+
+
+install_requires = [
+    "six",
+    "requests",
+    "pkginfo",
+]
+
+if sys.version_info[:2] < (2, 7):
+    install_requires += [
+        "argparse",
+    ]
+
+
+setup(
+    name=about["__title__"],
+    version=about["__version__"],
+
+    description=about["__summary__"],
+    long_description=open("README.rst").read(),
+    license=about["__license__"],
+    url=about["__uri__"],
+
+    author=about["__author__"],
+    author_email=about["__email__"],
+
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: BSD",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+
+    packages=find_packages(exclude=["tests", "tests.*"]),
+
+    entry_points={
+        "console_scripts": [
+            "twine = twine.__main__:main",
+        ],
+    },
+
+    install_requires=install_requires,
+)
