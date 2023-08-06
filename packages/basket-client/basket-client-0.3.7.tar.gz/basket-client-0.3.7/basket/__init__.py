7021e60325670725c9290690d9933e4fc57f3f4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:11 +0000

    Add package basket-client/0.3.7/basket-client-0.3.7.tar.gz

---
+++ packages/basket-client/basket-client-0.3.7.tar.gz/basket-client-0.3.7/basket/__init__.py
@@ -0,0 +1,7 @@
+"""A Python client for Mozilla's basket service."""
+
+# flake8: noqa
+
+from base import (BasketException, BasketNetworkException, confirm, debug_user,
+                  get_newsletters, lookup_user, request, send_recovery_message,
+                  send_sms, subscribe, unsubscribe, update_user, user)
