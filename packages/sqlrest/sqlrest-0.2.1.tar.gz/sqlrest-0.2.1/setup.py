72911e78274d854c10dd19a90f2a9e0607a2830f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:22 +0000

    Add package sqlrest/0.2.1/sqlrest-0.2.1.tar.gz

---
+++ packages/sqlrest/sqlrest-0.2.1.tar.gz/sqlrest-0.2.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'sqlrest',
+    version = '0.2.1',
+    author = 'Daniel Duckworth',
+    author_email = 'duckworthd@gmail.com',
+    description = "Instant REST APIs for SQL tables",
+    license = 'BSD',
+    keywords = 'database sql rest api',
+    url = 'http://github.com/duckworthd/sqlrest',
+    packages = find_packages(),
+    classifiers = [
+      'Development Status :: 4 - Beta',
+      'License :: OSI Approved :: BSD License',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python',
+    ],
+    install_requires = [     # dependencies
+      'configurati>=0.1.3',
+      'bottle>=0.11.6',
+      'SQLAlchemy>=0.8.2',
+      'mysql-connector-python>=1.0.12',
+      'psycopg2>=2.5.1',
+      'psycogreen>=1.0',
+      'gevent>=0.13.8',
+    ],
+)
