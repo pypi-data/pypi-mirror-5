b0cf90fc1b253424a5736fa9f33c732a7cb58600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:23 +0000

    Add package cs10606/1.0.0/cs10606-1.0.0.zip

---
+++ packages/cs10606/cs10606-1.0.0.zip/cs10606-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'cs10606',
+    version = '1.0.0',
+    py_moduels = ['cs10606'],
+    author = 'hfpython',
+    author_email = 'huanglong@able-elec.com',
+    url = 'http://www.zhihuishu.com',
+    descrition = 'AAAAAA',
+    )
