cad3c0f966736cfaaea2f1b369830fcfe9122a43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:07 +0000

    Add package signupto/0.0.3/signupto-0.0.3.tar.gz

---
+++ packages/signupto/signupto-0.0.3.tar.gz/signupto-0.0.3/HISTORY.rst
@@ -0,0 +1,20 @@
+.. :changelog:
+
+History
+-------
+
+0.0.3 (2013-11-12)
+++++++++++++++++++
+
+* Fixed put methods
+
+
+0.0.2 (2013-11-12)
+++++++++++++++++++
+
+* Fixed delete methods
+
+0.0.1 (2013-11-12)
+++++++++++++++++++
+
+* First release on PyPI.
---
+++ packages/signupto/signupto-0.0.3.tar.gz/signupto-0.0.3/signupto/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+__author__ = 'Luke Plant'
+__email__ = 'L.Plant.98@cantab.net'
+__version__ = '0.0.3'
+
+from .client import Client, HashAuthorization, TokenAuthorization, ClientError, ObjectNotFound
