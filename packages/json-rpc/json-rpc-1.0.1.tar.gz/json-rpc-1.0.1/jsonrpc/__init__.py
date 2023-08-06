e95dc8b44acd71ed553f72b186d1882b0c2e3b14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:48 +0000

    Add package json-rpc/1.0.1/json-rpc-1.0.1.tar.gz

---
+++ packages/json-rpc/json-rpc-1.0.1.tar.gz/json-rpc-1.0.1/jsonrpc/__init__.py
@@ -0,0 +1,9 @@
+﻿__version = (1, 0, 1)
+
+__version__ = version = '.'.join(map(str, __version))
+__project__ = PROJECT = __name__
+
+#from .jsonrpc import JSONRPCProtocol, JSONRPCRequest, JSONRPCResponse
+#from .exceptions import *
+
+# lint_ignore=W0611,W0401
