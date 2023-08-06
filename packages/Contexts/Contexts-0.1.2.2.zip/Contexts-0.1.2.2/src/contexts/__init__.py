60908e99806023e1b4a024d59f3d0e20a097e2d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:52 +0000

    Add package Contexts/0.1.2.2/Contexts-0.1.2.2.zip

---
+++ packages/Contexts/Contexts-0.1.2.2.zip/Contexts-0.1.2.2/src/contexts/__init__.py
@@ -0,0 +1,38 @@
+import sys
+from . import builders
+from . import reporting
+from . import core
+
+
+__all__ = ['run', 'main', 'catch']
+
+
+def main():
+    passed = run()
+    if not passed:
+        sys.exit(1)
+    sys.exit(0)
+
+
+def run(spec=None, result=None):
+    if result is None:
+        result = reporting.CapturingDefaultResult()
+    if spec is None:
+        spec = sys.modules['__main__']
+
+    _run_impl(spec, result)
+
+    return not result.failed
+
+
+def _run_impl(spec, result):
+    result_runner = core.ResultRunner(result)
+    suite = builders.build_suite(spec)
+    suite.run(result_runner)
+
+
+def catch(func, *args, **kwargs):
+    try:
+        func(*args, **kwargs)
+    except Exception as e:
+        return e
---
+++ packages/Contexts/Contexts-0.1.2.2.zip/Contexts-0.1.2.2/src/contexts/importers.py
@@ -0,0 +1,34 @@
+import contextlib
+import importlib
+import os
+import sys
+from . import finders
+
+
+def import_from_directory(dir_path):
+    module_specs = finders.find_modules(dir_path)
+    return (load_module(*spec) for spec in module_specs)
+
+
+def load_module(dir_path, module_name):
+    prune_sys_dot_modules(dir_path, module_name)
+    with prepend_folder_to_sys_dot_path(dir_path):
+        return importlib.import_module(module_name)
+
+
+def prune_sys_dot_modules(dir_path, module_name):
+    requested_file = os.path.join(dir_path, module_name + '.py')
+    if module_name in sys.modules:
+        same_file = os.path.realpath(sys.modules[module_name].__file__) == os.path.realpath(requested_file)
+        if not same_file:
+            del sys.modules[module_name]
+        importlib.invalidate_caches()
+
+
+@contextlib.contextmanager
+def prepend_folder_to_sys_dot_path(folder_path):
+    sys.path.insert(0, folder_path)
+    try:
+        yield
+    finally:
+        sys.path.pop(0)
