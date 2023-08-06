ad7624f8ce519a28a94f5ceaf0e1c2c7e483cc6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:02 +0000

    Add package leap.bitmask/0.3.1/leap.bitmask-0.3.1.tar.gz

---
+++ packages/leap.bitmask/leap.bitmask-0.3.1.tar.gz/leap.bitmask-0.3.1/setup.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+import sys
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from pkg import distribute_setup
+    distribute_setup.use_setuptools()
+    from setuptools import setup, find_packages
+import os
+
+from pkg import utils
+
+import versioneer
+versioneer.versionfile_source = 'src/leap/bitmask/_version.py'
+versioneer.versionfile_build = 'leap/bitmask/_version.py'
+versioneer.tag_prefix = ''  # tags are like 1.2.0
+versioneer.parentdir_prefix = 'leap.bitmask-'
+
+#from setuptools import Command
+
+# The following import avoids the premature unloading of the `util` submodule
+# when running tests, which would cause an error when nose finishes tests and
+# calls the exit function of the multiprocessing module.
+from multiprocessing import util
+assert(util)
+
+setup_root = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(setup_root, "src"))
+
+trove_classifiers = [
+    "Development Status :: 3 - Alpha",
+    "Environment :: X11 Applications :: Qt",
+    "Intended Audience :: End Users/Desktop",
+    ("License :: OSI Approved :: GNU General "
+     "Public License v3 or later (GPLv3+)"),
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2.6",
+    "Programming Language :: Python :: 2.7",
+    "Topic :: Communications",
+    "Topic :: Security",
+    "Topic :: System :: Networking",
+    "Topic :: Utilities"
+]
+
+
+parsed_reqs = utils.parse_requirements()
+
+cmdclass = versioneer.get_cmdclass()
+leap_launcher = 'bitmask=leap.bitmask.app:main'
+
+from setuptools.command.develop import develop as _develop
+
+
+def copy_reqs(path, withsrc=False):
+    # add a copy of the processed requirements to the package
+    _reqpath = ('leap', 'bitmask', 'util', 'reqs.txt')
+    if withsrc:
+        reqsfile = os.path.join(path, 'src', *_reqpath)
+    else:
+        reqsfile = os.path.join(path, *_reqpath)
+    print("UPDATING %s" % reqsfile)
+    if os.path.isfile(reqsfile):
+        os.unlink(reqsfile)
+    with open(reqsfile, "w") as f:
+        f.write('\n'.join(parsed_reqs))
+
+
+class cmd_develop(_develop):
+    def run(self):
+        # versioneer:
+        versions = versioneer.get_versions(verbose=True)
+        self._versioneer_generated_versions = versions
+        # unless we update this, the command will keep using the old version
+        self.distribution.metadata.version = versions["version"]
+
+        _develop.run(self)
+        copy_reqs(self.egg_path)
+
+cmdclass["develop"] = cmd_develop
+
+# next two classes need to augment the versioneer modified ones
+
+versioneer_build = cmdclass['build']
+versioneer_sdist = cmdclass['sdist']
+
+
+class cmd_build(versioneer_build):
+    def run(self):
+        versioneer_build.run(self)
+        copy_reqs(self.build_lib)
+
+
+class cmd_sdist(versioneer_sdist):
+    def run(self):
+        return versioneer_sdist.run(self)
+
+    def make_release_tree(self, base_dir, files):
+        versioneer_sdist.make_release_tree(self, base_dir, files)
+        copy_reqs(base_dir, withsrc=True)
+
+
+cmdclass["build"] = cmd_build
+cmdclass["sdist"] = cmd_sdist
+
+
+setup(
+    name="leap.bitmask",
+    package_dir={"": "src"},
+    version=versioneer.get_version(),
+    cmdclass=cmdclass,
+    description="The Internet Encryption Toolkit",
+    long_description=(
+        "Desktop Client for the LEAP Platform."
+        "\n"
+        "LEAP (LEAP Encryption Access Project) develops "
+        "a multi-year plan to secure everyday communication, breaking down"
+        "into discrete services, to be rolled out one at a time.\n"
+        "The client for the current phase gives support to the EIP Service."
+        "EIP (the Encrypted Internet Proxy) provides circumvention, location "
+        "anonymization, and traffic "
+        "encryption in a hassle-free, automatically self-configuring fashion, "
+        "and has an enhanced level of security."
+    ),
+    classifiers=trove_classifiers,
+    install_requires=parsed_reqs,
+    test_suite='nose.collector',
+    tests_require=utils.parse_requirements(
+        reqfiles=['pkg/requirements-testing.pip']),
+    keywords=('Bitmask, LEAP, client, qt, encryption, '
+              'proxy, openvpn, imap, smtp'),
+    author='The LEAP Encryption Access Project',
+    author_email='info@leap.se',
+    url='https://leap.se',
+    license='GPL-3+',
+    packages=find_packages(
+        'src',
+        exclude=['ez_setup', 'setup', 'examples', 'tests']),
+    namespace_packages=["leap"],
+    package_data={'': ['util/*.txt']},
+    include_package_data=True,
+    # not being used? -- setuptools does not like it.
+    # looks like debhelper is honoring it...
+    data_files=[
+        # ("share/man/man1",
+        #     ["docs/man/bitmask.1"]),
+        ("share/polkit-1/actions",
+         ["pkg/linux/polkit/net.openvpn.gui.leap.policy"]),
+    ],
+    zip_safe=False,
+    platforms="all",
+    entry_points={
+        'console_scripts': [leap_launcher]
+    },
+)
---
+++ packages/leap.bitmask/leap.bitmask-0.3.1.tar.gz/leap.bitmask-0.3.1/src/leap/bitmask/gui/ui_login.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'src/leap/bitmask/gui/ui/login.ui'
+#
+# Created: Mon Aug 26 14:24:11 2013
+#      by: pyside-uic 0.2.14 running on PySide 1.1.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PySide import QtCore, QtGui
+
+class Ui_LoginWidget(object):
+    def setupUi(self, LoginWidget):
+        LoginWidget.setObjectName("LoginWidget")
+        LoginWidget.resize(356, 223)
+        self.gridLayout = QtGui.QGridLayout(LoginWidget)
+        self.gridLayout.setObjectName("gridLayout")
+        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.gridLayout.addItem(spacerItem, 5, 2, 1, 1)
+        self.cmbProviders = QtGui.QComboBox(LoginWidget)
+        self.cmbProviders.setObjectName("cmbProviders")
+        self.gridLayout.addWidget(self.cmbProviders, 1, 1, 1, 2)
+        spacerItem1 = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.gridLayout.addItem(spacerItem1, 5, 0, 1, 1)
+        self.btnCreateAccount = QtGui.QPushButton(LoginWidget)
+        self.btnCreateAccount.setObjectName("btnCreateAccount")
+        self.gridLayout.addWidget(self.btnCreateAccount, 6, 1, 1, 1)
+        self.label_4 = QtGui.QLabel(LoginWidget)
+        self.label_4.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+        self.label_4.setObjectName("label_4")
+        self.gridLayout.addWidget(self.label_4, 1, 0, 1, 1)
+        self.lnPassword = QtGui.QLineEdit(LoginWidget)
+        self.lnPassword.setInputMask("")
+        self.lnPassword.setObjectName("lnPassword")
+        self.gridLayout.addWidget(self.lnPassword, 3, 1, 1, 2)
+        self.lnUser = QtGui.QLineEdit(LoginWidget)
+        self.lnUser.setObjectName("lnUser")
+        self.gridLayout.addWidget(self.lnUser, 2, 1, 1, 2)
+        self.chkRemember = QtGui.QCheckBox(LoginWidget)
+        self.chkRemember.setObjectName("chkRemember")
+        self.gridLayout.addWidget(self.chkRemember, 4, 1, 1, 2)
+        self.label_2 = QtGui.QLabel(LoginWidget)
+        self.label_2.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+        self.label_2.setObjectName("label_2")
+        self.gridLayout.addWidget(self.label_2, 2, 0, 1, 1)
+        self.label_3 = QtGui.QLabel(LoginWidget)
+        self.label_3.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+        self.label_3.setObjectName("label_3")
+        self.gridLayout.addWidget(self.label_3, 3, 0, 1, 1)
+        self.btnLogin = QtGui.QPushButton(LoginWidget)
+        self.btnLogin.setObjectName("btnLogin")
+        self.gridLayout.addWidget(self.btnLogin, 5, 1, 1, 1)
+        self.lblStatus = QtGui.QLabel(LoginWidget)
+        self.lblStatus.setText("")
+        self.lblStatus.setAlignment(QtCore.Qt.AlignCenter)
+        self.lblStatus.setWordWrap(True)
+        self.lblStatus.setObjectName("lblStatus")
+        self.gridLayout.addWidget(self.lblStatus, 0, 0, 1, 3)
+
+        self.retranslateUi(LoginWidget)
+        QtCore.QMetaObject.connectSlotsByName(LoginWidget)
+        LoginWidget.setTabOrder(self.cmbProviders, self.lnUser)
+        LoginWidget.setTabOrder(self.lnUser, self.lnPassword)
+        LoginWidget.setTabOrder(self.lnPassword, self.chkRemember)
+        LoginWidget.setTabOrder(self.chkRemember, self.btnLogin)
+        LoginWidget.setTabOrder(self.btnLogin, self.btnCreateAccount)
+
+    def retranslateUi(self, LoginWidget):
+        LoginWidget.setWindowTitle(QtGui.QApplication.translate("LoginWidget", "Form", None, QtGui.QApplication.UnicodeUTF8))
+        self.btnCreateAccount.setText(QtGui.QApplication.translate("LoginWidget", "Create a new account", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_4.setText(QtGui.QApplication.translate("LoginWidget", "<b>Provider:</b>", None, QtGui.QApplication.UnicodeUTF8))
+        self.chkRemember.setText(QtGui.QApplication.translate("LoginWidget", "Remember username and password", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_2.setText(QtGui.QApplication.translate("LoginWidget", "<b>Username:</b>", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_3.setText(QtGui.QApplication.translate("LoginWidget", "<b>Password:</b>", None, QtGui.QApplication.UnicodeUTF8))
+        self.btnLogin.setText(QtGui.QApplication.translate("LoginWidget", "Log In", None, QtGui.QApplication.UnicodeUTF8))
+
