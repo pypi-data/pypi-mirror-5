1dfbc5ea351b11cbb8fa3f4c54ba362096834308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:45 +0000

    Add package swqhtest/1.0.0/swqhtest-1.0.0.zip

---
+++ packages/swqhtest/swqhtest-1.0.0.zip/swqhtest-1.0.0/setup.py
@@ -0,0 +1,14 @@
+
+from distutils.core import setup
+
+setup(
+        name         = 'swqhtest',
+        version      = '1.0.0',
+        ## TODO: print hellow pypi
+        py_modules   = ['swqhtest'],
+        author       = 'swqhgl',
+        author_email = 'swqhgl@163.com',
+        url          = 'http://www.headfirstlabs.com',
+        description  = 'A simple printer of hellow pypi',
+     )
+
