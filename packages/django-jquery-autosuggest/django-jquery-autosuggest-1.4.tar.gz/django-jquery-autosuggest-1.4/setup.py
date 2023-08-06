f1a7577093020ed67abefb9c261b088bc62594bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:51 +0000

    Add package django-jquery-autosuggest/1.4/django-jquery-autosuggest-1.4.tar.gz

---
+++ packages/django-jquery-autosuggest/django-jquery-autosuggest-1.4.tar.gz/django-jquery-autosuggest-1.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='django-jquery-autosuggest',
+    version='1.4',
+    url='https://github.com/benbacardi/django-jquery-autosuggest',
+    description='jQuery autosuggest (http://code.drewwilson.com/entry/autosuggest-jquery-plugin) packaged in a django app to speed up new applications and deployment.',
+    author='Ben Cardy',
+    author_email='benbacardi@gmail.com',
+    license='MIT',
+    keywords='django jquery autosugegst staticfiles'.split(),
+    platforms='any',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ],
+    packages=['jquery_autosuggest'],
+    package_data={'jquery_autosuggest': ['static/js/*.js', 'static/css/*.css',]},
+    install_requires=['django-jquery >= 1.6',],
+)
