48576a57ca25830270e4ba7e97b8fd34e9fcf72c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:56 +0000

    Add package fjd/0.1.30/fjd-0.1.30.tar.gz

---
+++ packages/fjd/fjd-0.1.30.tar.gz/fjd-0.1.30/fjd/dispatcher.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import time
+
+from fjd import CoreProcess
+from fjd import Recruiter
+from fjd.utils import ensure_wdir
+
+
+class Dispatcher(CoreProcess):
+    '''
+    Directory and File-based work dispatcher.
+    Expects jobs to be put in directory `jobqueue` and workers to
+    annouce themselves in directory `workerqueue`. Will then assign jobs 
+    to workers by moving jobs to the `jobpod` directory. 
+    '''
+
+    def __init__(self, interval=.1, project=None, end_on_empty_queue=True):
+        if not project:
+            project = 'default'
+        self.wdir = ensure_wdir(project)
+        self.start_up()
+
+        print('[fjd-dispatcher] Started on project "{}"'.format(project))
+
+        do_work = True
+        while do_work:
+            time.sleep(interval)
+            jq = os.listdir('{}/jobqueue'.format(self.wdir))
+            jp = os.listdir('{}/jobpod'.format(self.wdir))
+            wq = os.listdir('{}/workerqueue'.format(self.wdir))
+            if len(jq) > 0:
+                sys.stdout.write("\r[fjd-dispatcher] Found {} job(s) and {} free worker(s)...   "\
+                       .format(len(jq), len(wq)))
+                sys.stdout.flush()
+                for _ in range(min(len(jq), len(wq))):
+                    worker = wq.pop()
+                    job = jq.pop()
+                    os.rename('{wdir}/jobqueue/{j}'.format(wdir=self.wdir, j=job),
+                              '{wdir}/jobpod/{w}'.format(wdir=self.wdir, w=worker))
+                    os.remove('{wdir}/workerqueue/{w}'.format(wdir=self.wdir, w=worker))
+            elif len(jp) == 0:
+                sys.stdout.write("\r[fjd-dispatcher] No (more) jobs.                               ")
+                sys.stdout.flush()
+                if end_on_empty_queue:
+                    Recruiter(project=project).fire()
+                    do_work = False
+        self.wrap_up()
+
