b211b96eae15fb06aedcba6923c10411f4daaf07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:27 +0000

    Add package txt2bootstrap/1.1/txt2bootstrap-1.1.tar.gz

---
+++ packages/txt2bootstrap/txt2bootstrap-1.1.tar.gz/txt2bootstrap-1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name='txt2bootstrap',
+    version='1.1',
+    packages=[],
+    license='MIT',
+    long_description=open('README.rst').read(),
+    scripts=['bin/txt2bootstrap'],
+    install_requires=[],
+    author='Gil Goncalves',
+    author_email='lursty@gmail.com',
+    url='https://github.com/lurst/txt2bootstrap',
+)
