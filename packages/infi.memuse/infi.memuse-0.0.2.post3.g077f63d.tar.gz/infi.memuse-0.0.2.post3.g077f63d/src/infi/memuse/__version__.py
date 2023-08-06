db775b9158cb7fc3f564e75047f5f943d1cb179a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:54 +0000

    Add package infi.memuse/0.0.2.post3.g077f63d/infi.memuse-0.0.2.post3.g077f63d.tar.gz

---
+++ packages/infi.memuse/infi.memuse-0.0.2.post3.g077f63d.tar.gz/infi.memuse-0.0.2.post3.g077f63d/src/infi/memuse/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.2.post3.g077f63d"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = 'develop'
+__git_remote_tracking_branch__ = 'origin/develop'
+__git_remote_url__ = 'git@infinigit.infinidat.com:/host/memuse.git'
+__git_head_hash__ = '077f63d956d5fbb12c7bff0ef36e3ecd482facab'
+__git_head_subject__ = 'freeze psutil version (new one is broken on Ubuntu 11.04)'
+__git_head_message__ = 'see https://code.google.com/p/psutil/issues/detail?id=423'
+__git_dirty_diff__ = ''
