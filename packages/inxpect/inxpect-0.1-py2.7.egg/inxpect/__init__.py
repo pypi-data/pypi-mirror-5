b1704763c1a4f5349025ff2d4df9366192ec9d82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:11 +0000

    Add package inxpect/0.1/inxpect-0.1-py2.7.egg

---
+++ packages/inxpect/inxpect-0.1-py2.7.egg/inxpect/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf8 -*-
+__all__ = ['getters', 'DefaultProperty', 'DefaultMethod', 'SameMethod', 'ListMethod', 'ListItemMethod', 'DictMethod', 'DictItemMethod']
+
+from .expect import *
