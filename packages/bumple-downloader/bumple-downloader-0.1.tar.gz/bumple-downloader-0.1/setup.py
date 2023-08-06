e10108326bf817993039a0505a3598493ff37459
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:33 +0000

    Add package bumple-downloader/0.1/bumple-downloader-0.1.tar.gz

---
+++ packages/bumple-downloader/bumple-downloader-0.1.tar.gz/bumple-downloader-0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+
+setup(
+    name='bumple-downloader',
+    version='0.1',
+    packages=['bumple-downloader', ],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    requires=["Django", ],
+)
