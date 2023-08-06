ce343f3ded0c167555b1ccc1d13d8f61d58618f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:39 +0000

    Add package pushi/0.1.0/pushi-0.1.0.zip

---
+++ packages/pushi/pushi-0.1.0.zip/pushi-0.1.0/pushi/base.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Hive Pushi System
+# Copyright (C) 2008-2012 Hive Solutions Lda.
+#
+# This file is part of Hive Pushi System.
+#
+# Hive Pushi System is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Pushi System is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Pushi System. If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "João Magalhães joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision$"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate$"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008-2012 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "GNU General Public License (GPL), Version 3"
+""" The license for the module """
+
+import os
+import hmac
+import hashlib
+
+def authenticate(channel, socket_id, app_key = None, app_secret = None):
+    key = app_key or os.environ["PUSHI_KEY"]
+    secret = app_secret or os.environ["PUSHI_SECRET"]
+    string = "%s:%s" % (socket_id, channel)
+
+    structure = hmac.new(secret, string, hashlib.sha256)
+    digest = structure.hexdigest()
+    return "%s:%s" % (key, digest)
