3003d10ed4a82e4e5125ecc2a068c10aa1272a45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:52 +0000

    Add package django-ckeditor-fc/1.1/django-ckeditor-fc-1.1.tar.gz

---
+++ packages/django-ckeditor-fc/django-ckeditor-fc-1.1.tar.gz/django-ckeditor-fc-1.1/AUTHORS.rst
@@ -0,0 +1,14 @@
+AUTHORS
+=======
+
+Created By
+----------
+#. `shaunsephton <http://github.com/shaunsephton>`_
+
+Contributors
+------------
+#. `buchuki <http://github.com/buchuki>`_
+#. `chr15m <http://github.com/chr15m>`_
+#. `jeffh <https://github.com/jeffh>`_
+#. `loop0 <http://github.com/loop0>`_
+
---
+++ packages/django-ckeditor-fc/django-ckeditor-fc-1.1.tar.gz/django-ckeditor-fc-1.1/ckeditor/fields.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django import forms
+
+from ckeditor.widgets import CKEditorWidget
+
+class RichTextField(models.TextField):
+    def __init__(self, config_name='default', *args, **kwargs):
+        self.config_name = config_name
+        super(RichTextField, self).__init__(*args, **kwargs)
+    
+    def formfield(self, **kwargs):
+        defaults = {
+            'form_class': RichTextFormField,
+            'config_name': self.config_name,
+        }
+        defaults.update(kwargs)
+        return super(RichTextField, self).formfield(**defaults)
+
+try:
+    from south.modelsinspector import add_introspection_rules
+    add_introspection_rules([], ["^ckeditor\.fields\.RichTextField"])
+except:
+    pass
+        
+class RichTextFormField(forms.fields.Field):
+    def __init__(self, config_name='default', *args, **kwargs):
+        kwargs.update({'widget': CKEditorWidget(config_name=config_name)})
+        super(RichTextFormField, self).__init__(*args, **kwargs)
