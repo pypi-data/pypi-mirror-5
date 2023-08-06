0e4389b0e268ecb0662b382da9be19bd5580de95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:48 +0000

    Add package git-repo-hooks/1.01.001/git-repo-hooks-1.01.001.tar.gz

---
+++ packages/git-repo-hooks/git-repo-hooks-1.01.001.tar.gz/git-repo-hooks-1.01.001/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+'''
+Created on Feb 18, 2013
+
+@author: legion
+'''
+from distutils.core import setup
+
+
+
+setup(name='git-repo-hooks',
+    version='1.01.001',
+    description='A tool to manage project, user, and global Git hooks for multiple git repositories',
+    author='K Jonathan',
+    author_email='phenixdoc@gmail.com',
+    url='https://github.com/legion0/git-repo-hooks',
+    packages=[],
+    scripts=['git-hooks'],
+    )
