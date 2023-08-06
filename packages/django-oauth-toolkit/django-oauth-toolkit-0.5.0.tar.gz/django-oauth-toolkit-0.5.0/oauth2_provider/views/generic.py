ac92749b53e2f2ec4cfcec85bda04fd4a0ca0135
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:13 +0000

    Add package django-oauth-toolkit/0.5.0/django-oauth-toolkit-0.5.0.tar.gz

---
+++ packages/django-oauth-toolkit/django-oauth-toolkit-0.5.0.tar.gz/django-oauth-toolkit-0.5.0/oauth2_provider/views/generic.py
@@ -0,0 +1,29 @@
+from django.views.generic import View
+
+from oauthlib.oauth2 import Server
+
+from ..oauth2_validators import OAuth2Validator
+from .mixins import ProtectedResourceMixin, ScopedResourceMixin, ReadWriteScopedResourceMixin
+
+
+class ProtectedResourceView(ProtectedResourceMixin, View):
+    """
+    Generic view protecting resources by providing OAuth2 authentication out of the box
+    """
+    server_class = Server
+    validator_class = OAuth2Validator
+
+
+class ScopedProtectedResourceView(ScopedResourceMixin, ProtectedResourceView):
+    """
+    Generic view protecting resources by providing OAuth2 authentication and Scopes handling out of the box
+    """
+    pass
+
+
+class ReadWriteScopedResourceView(ReadWriteScopedResourceMixin, ProtectedResourceView):
+    """
+    Generic view protecting resources with OAuth2 authentication and read/write scopes.
+    GET, HEAD, OPTIONS http methods require "read" scope. Otherwise "write" scope is required.
+    """
+    pass
