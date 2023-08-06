c33628bd387649dc000286516f66bf9527ae51e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:01 +0000

    Add package pypdfocr/0.2.2/pypdfocr-0.2.2.tar.gz

---
+++ packages/pypdfocr/pypdfocr-0.2.2.tar.gz/pypdfocr-0.2.2/setup.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+from setuptools import setup, find_packages
+
+import pypdfocr
+
+with open('README.rst') as file:
+        long_description = file.read()
+
+packages = find_packages(exclude="tests")
+setup (
+    name = "pypdfocr",
+    version = pypdfocr.__version__,
+    description="Converts a scanned PDF into an OCR'ed pdf using Tesseract-OCR and Ghostscript",
+    license = "LICENSE",
+    long_description = long_description,
+    author="Virantha N. Ekanayake",
+    author_email="virantha@gmail.com", # Removed.
+    package_data = {'': ['*.xml']},
+    zip_safe = True,
+    packages = packages,
+    install_requires = [ 'pil>=1.1.7', 'reportlab>=2.7', "watchdog>=0.6.0" ],
+    entry_points = {
+            'console_scripts': [
+                    'pypdfocr = pypdfocr.pypdfocr:main'
+                ],
+        },
+    options = {
+	    "pyinstaller": {"packages": packages}
+	    }
+
+)
