ebd056e39390c31b8b73ef2d6f0e4fa3770c3bde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:04 +0000

    Add package mixcoatl/0.2.8/mixcoatl-0.2.8.tar.gz

---
+++ packages/mixcoatl/mixcoatl-0.2.8.tar.gz/mixcoatl-0.2.8/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import mixcoatl
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+packages = [
+    'mixcoatl',
+    'mixcoatl.admin',
+    'mixcoatl.analytics',
+    'mixcoatl.automation',
+    'mixcoatl.config',
+    'mixcoatl.decorators',
+    'mixcoatl.exceptions',
+    'mixcoatl.geography',
+    'mixcoatl.infrastructure',
+    'mixcoatl.network',
+    'mixcoatl.settings'
+]
+
+requires = ['requests==1.0.4']
+setup(
+    name='mixcoatl',
+    version=mixcoatl.__version__,
+    description='enStratus API Python wrapper',
+    long_description=open('README.rst').read(),
+    author='John E. Vincent',
+    author_email='lusis.org+github.com@gmail.com',
+    url='https://github.com/enStratus/mixcoatl',
+    packages=packages,
+    package_data={'': ['LICENSE', 'README.rst', 'requirements.txt']},
+    package_dir={'mixcoatl': 'mixcoatl'},
+    include_package_data=True,
+    install_requires=requires,
+    scripts=['bin/es-dump.py'],
+    license='Apache 2.0',
+    zip_safe=False,
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+    ),
+)
