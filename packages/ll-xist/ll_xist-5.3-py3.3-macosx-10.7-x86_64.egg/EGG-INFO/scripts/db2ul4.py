edd037ae5f4fecf49620ec3cfd3bbf1342f177bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:51 +0000

    Add package ll-xist/5.3/ll_xist-5.3-py3.3-macosx-10.7-x86_64.egg

---
+++ packages/ll-xist/ll_xist-5.3-py3.3-macosx-10.7-x86_64.egg/EGG-INFO/scripts/db2ul4.py
@@ -0,0 +1,10 @@
+#!/Users/walter/pyvenvs/default/bin/python
+# -*- coding: utf-8 -*-
+# cython: language_level=3
+
+import sys
+
+
+if __name__ == "__main__":
+	from ll.scripts import db2ul4
+	sys.exit(db2ul4.main())
