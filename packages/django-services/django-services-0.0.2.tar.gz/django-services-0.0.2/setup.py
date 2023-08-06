2ffa248cfe5b67571906aec66267c1e3a25fb3b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:09 +0000

    Add package django-services/0.0.2/django-services-0.0.2.tar.gz

---
+++ packages/django-services/django-services-0.0.2.tar.gz/django-services-0.0.2/setup.py
@@ -0,0 +1,52 @@
+import os
+import sys
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+STATUS_PROD = 'Development Status :: 5 - Production/Stable'
+STATUS_BETA = 'Development Status :: 4 - Beta'
+STATUS_ALPHA = 'Development Status :: 3 - Alpha'
+
+version = '0.0.2'
+README = os.path.join(os.path.dirname(__file__), 'README.rst')
+long_description = open(README).read()
+
+
+class Tox(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import tox
+        errno = tox.cmdline(self.test_args)
+        sys.exit(errno)
+
+
+setup(
+    name='django-services',
+    version=version,
+    description="Django Services Pattern Support",
+    long_description=long_description,
+    classifiers=[
+        STATUS_PROD,
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities'],
+    keywords='django model admin rest api service',
+    author='Silvano Buback',
+    author_email='snbuback@gmail.com',
+    url='https://github.com/snbuback/django-services',
+    license='APACHE2',
+    packages=find_packages('.', exclude=('testproject*',)),
+    include_package_data=True,
+    require=['Django', 'mock', 'djangorestframework'],
+    tests_require=['tox'],
+    cmdclass = {'test': Tox},
+)
