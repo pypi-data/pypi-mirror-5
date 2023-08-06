db6e19e33b8b47e85d825457640f3841b211614e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:22 +0000

    Add package django-ssl-auth/0.8.2.2/django-ssl-auth-0.8.2.2.tar.gz

---
+++ packages/django-ssl-auth/django-ssl-auth-0.8.2.2.tar.gz/django-ssl-auth-0.8.2.2/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django-ssl-auth',
+      version='0.8.2.2',
+      description='Django SSL Client Authentication',
+      author='Kimmo Parviainen-Jalanko',
+      author_email='kimvais@ssh.com',
+      url='https://github.com/kimvais/django-ssl-client-auth/',
+      packages=['django_ssl_auth'],
+      requires=['Django (>=1.4)']
+)
