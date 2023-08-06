18725e8186b9e23c01b52c12e162dcc365a2e706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:32 +0000

    Add package pyinstruments/0.3.11/pyinstruments-0.3.11-py2.7.egg

---
+++ packages/pyinstruments/pyinstruments-0.3.11-py2.7.egg/pyinstruments/pyhardwaredb/__init__.py
@@ -0,0 +1,13 @@
+"""
+This minimal package simply combines pyinstruments and datastore to provide
+instruments that are allowed to store their curves in the datastore.
+"""
+
+
+#_APP = __qapplication()
+
+import dbinstruments
+from pyhardware import instrument, gui
+
+#_APP.exec_()
+
