645e75dfc21aeee7fff88471ddf05fa7d9beb04b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:45 +0000

    Add package RuSocSci/0.6.5/RuSocSci-0.6.5.zip

---
+++ packages/RuSocSci/RuSocSci-0.6.5.zip/RuSocSci-0.6.5/rusocsci/__init__.py
@@ -0,0 +1,14 @@
+# Part of the PsychoPy library
+# Copyright (C) 2013 Wilbert van Ham, Radboud University Nijmegen
+# Distributed under the terms of the GNU General Public License (GPL).
+
+#version info for RuSocSci
+__version__='0.6.5'
+__license__='GNU GPLv3 (or more recent equivalent)'
+__author__='Wilbert van Ham'
+__author_email__='W.vanHam@socsci.ru.nl'
+__maintainer_email__='W.vanHam@socsci.ru.nl'
+__url__='http://www.socsci.ru.nl/~wilberth/python/hardware.html'
+
+
+
