df6458546b4c25360f8c8bc0555e653832761a51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:43 +0000

    Add package tendenci/5.1.165/tendenci-5.1.165.tar.gz

---
+++ packages/tendenci/tendenci-5.1.165.tar.gz/tendenci-5.1.165/tendenci/core/base/management/commands/update_tendenci.py
@@ -0,0 +1,23 @@
+import os
+
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+from tendenci.core.base.models import UpdateTracker
+
+class Command(BaseCommand):
+    """
+    Update tendenci via pip and restarts the server
+    """
+    def handle(self, *args, **kwargs):
+        print "Updating tendenci package"
+        os.system('pip install tendenci --upgrade')
+
+        print "Updating tendenci site"
+        os.system('python deploy.py')
+
+        print "Restarting Server"
+        # This depends on the server used, please update accordingly
+        os.system('reload tendencisite')
+
+        UpdateTracker.end()
+        call_command('clear_cache')
