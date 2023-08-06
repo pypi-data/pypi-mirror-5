b0dc0af50b4c5f57ae6d255ee38314fa0482aa5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:37 +0000

    Add package pyspecs/2.0/pyspecs-2.0.tar.gz

---
+++ packages/pyspecs/pyspecs-2.0.tar.gz/pyspecs-2.0/pyspecs/__init__.py
@@ -0,0 +1,39 @@
+import sys
+from pyspecs._runner import Step, _counter, _step_runner
+from pyspecs._should import _Should
+
+
+__version__ = '2.0'
+
+
+given = Step('given', _counter)
+provided = Step('provided', _counter)
+at = Step('at', _counter)
+when = Step('when', _counter)
+and_ = Step('and', _counter)
+then = Step('then', _counter)
+so = Step('so', _counter)
+therefore = Step('therefore', _counter)
+however = Step('however', _counter)
+as_well_as = Step('as well as', _counter)
+
+the = _Should
+it = _Should
+this = _Should
+that = _Should
+
+
+# noinspection PyBroadException
+def catch(callable_):
+    """
+    Utility method for saving any exceptions raised from a callable.
+    """
+    sys.exc_clear()
+    try:
+        callable_()
+    except:
+        return sys.exc_info()
+    else:
+        return None
+    finally:
+        sys.exc_clear()
---
+++ packages/pyspecs/pyspecs-2.0.tar.gz/pyspecs-2.0/setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+import pyspecs
+
+
+setup(
+    name='pyspecs',
+    version=pyspecs.__version__,
+    packages=['pyspecs'],
+    scripts=['scripts/run_pyspecs.py'],
+    url='https://github.com/mdwhatcott/pyspecs',
+    license='MIT',
+    author='Michael Whatcott',
+    author_email='mdwhatcott+pyspecs@gmail.com',
+    description='Concise BDD in python',
+    long_description='pyspecs is a testing framework that strives to achieve '
+                     'more readable specifications (tests) by leveraging '
+                     'some fancy syntactic sugar and auto-discovery of '
+                     'tests/specs.',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Environment :: Console',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Documentation',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Utilities',
+    ]
+)
