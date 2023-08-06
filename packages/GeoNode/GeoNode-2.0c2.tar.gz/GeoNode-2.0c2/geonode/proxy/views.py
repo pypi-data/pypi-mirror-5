25e476e7b4fac7f7da7a9cabab9071a6a1292c0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:40 +0000

    Add package GeoNode/2.0c2/GeoNode-2.0c2.tar.gz

---
+++ packages/GeoNode/GeoNode-2.0c2.tar.gz/GeoNode-2.0c2/geonode/proxy/views.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+#########################################################################
+#
+# Copyright (C) 2012 OpenPlans
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+#########################################################################
+
+from django.http import HttpResponse
+from httplib import HTTPConnection
+from urlparse import urlsplit
+import httplib2
+from django.conf import settings
+from geonode.utils import ogc_server_settings
+
+def proxy(request):
+    if 'url' not in request.GET:
+        return HttpResponse(
+                "The proxy service requires a URL-encoded URL as a parameter.",
+                status=400,
+                content_type="text/plain"
+                )
+
+    url = urlsplit(request.GET['url'])
+    locator = url.path
+    if url.query != "":
+        locator += '?' + url.query
+    if url.fragment != "":
+        locator += '#' + url.fragment
+
+    headers = {}
+    if settings.SESSION_COOKIE_NAME in request.COOKIES:
+        headers["Cookie"] = request.META["HTTP_COOKIE"]
+
+    if request.method in ("POST", "PUT") and "CONTENT_TYPE" in request.META:
+        headers["Content-Type"] = request.META["CONTENT_TYPE"]
+
+    if request.META.get('HTTP_AUTHORIZATION'):
+        headers['AUTHORIZATION'] = request.META.get('HTTP_AUTHORIZATION')
+
+    conn = HTTPConnection(url.hostname, url.port)
+    conn.request(request.method, locator, request.raw_post_data, headers)
+    result = conn.getresponse()
+    response = HttpResponse(
+            result.read(),
+            status=result.status,
+            content_type=result.getheader("Content-Type", "text/plain")
+            )
+
+    if result.getheader('www-authenticate'):
+        response['www-authenticate'] = result.getheader('www-authenticate')
+
+    return response
+
+def geoserver_rest_proxy(request, proxy_path, downstream_path):
+    if not request.user.is_authenticated():
+        return HttpResponse(
+            "You must be logged in to access GeoServer",
+            mimetype="text/plain",
+            status=401)
+
+    def strip_prefix(path, prefix):
+        assert path.startswith(prefix)
+        return path[len(prefix):]
+
+    path = strip_prefix(request.get_full_path(), proxy_path)
+    url = "".join([ogc_server_settings.LOCATION, downstream_path, path])
+
+    http = httplib2.Http()
+    http.add_credentials(*(ogc_server_settings.credentials))
+    headers = dict()
+
+    if request.method in ("POST", "PUT") and "CONTENT_TYPE" in request.META:
+        headers["Content-Type"] = request.META["CONTENT_TYPE"]
+
+    response, content = http.request(
+        url, request.method,
+        body=request.raw_post_data or None,
+        headers=headers)
+        
+    # we need to sync django here
+    # we should remove this geonode dependency calling layers.views straight
+    # from GXP, bypassing the proxy
+    if downstream_path == 'rest/styles' and len(request.raw_post_data)>0:
+        # for some reason sometime gxp sends a put with empty request
+        # need to figure out with Bart
+        from geonode.layers import utils
+        utils.style_update(request, url)
+
+    return HttpResponse(
+        content=content,
+        status=response.status,
+        mimetype=response.get("content-type", "text/plain"))
