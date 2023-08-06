0c1fb20c548b479de0d72345626ad59aa24e6992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:57 +0000

    Add package django-online/0.2/django-online-0.2.tar.gz

---
+++ packages/django-online/django-online-0.2.tar.gz/django-online-0.2/online/tests/settings.py
@@ -0,0 +1,39 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'test_db',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+SITE_ID = 1
+DEBUG = True
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'online',
+    'online.tests',
+]
+
+ROOT_URLCONF = 'online.tests.urls'
+
+TEMPLATE_DIRS = (
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'online.middleware.OnlineMiddleware',
+)
+
+SECRET_KEY = 'blabla'
