84e09a1c9a419845f1090fef3cb881d0d3457fa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:59 +0000

    Add package django-shop-sofortpayment/0.1.3/django-shop-sofortpayment-0.1.3.tar.gz

---
+++ packages/django-shop-sofortpayment/django-shop-sofortpayment-0.1.3.tar.gz/django-shop-sofortpayment-0.1.3/setup.py
@@ -0,0 +1,27 @@
+#-*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import os
+
+CLASSIFIERS = []
+
+setup(
+    author="Benjamin Wohlwend",
+    author_email="bw@piquadrat.ch",
+    name='django-shop-sofortpayment',
+    version='0.1.3',
+    description='A Sofort Bank payment backend for django SHOP',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    url='https://github.com/piquadrat/django-shop-sofortpayment',
+    license='BSD License',
+    platforms=['OS Independent'],
+    classifiers=CLASSIFIERS,
+    install_requires=[
+        'Django>=1.4',
+        'django-appconf',
+        'requests',
+        'xmltodict',
+    ],
+    packages=find_packages(exclude=["example", "example.*"]),
+    zip_safe = False,
+    include_package_data=True,
+)
