bdd40116df86fd624bb9cdc9874c919ff7dc5d38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:29 +0000

    Add package TrelloCardUpdate/0.0.4/TrelloCardUpdate-0.0.4.tar.gz

---
+++ packages/TrelloCardUpdate/TrelloCardUpdate-0.0.4.tar.gz/TrelloCardUpdate-0.0.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='TrelloCardUpdate',
+    version='0.0.4',
+    author='Thomas Ballinger',
+    author_email='tom@hackerschool.com',
+    packages=['trellocardupdate'],
+    scripts=['bin/tu.py', 'bin/tu'],
+    url='https://github.com/thomasballinger/trellocardupdate',
+    install_requires=[
+        'unidecode',
+        'python_Levenshtein',
+        'clint',
+        'httplib2',
+        'trolly',
+        ],
+    license='LICENSE.txt',
+    description='basic command line trello client',
+    long_description=open('README.txt').read(),
+)
+
+
