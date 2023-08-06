13bf3e887da0b7af5327918716055ea8fc5fcece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:09 +0000

    Add package TDTPy/0.7/TDTPy-0.7.zip

---
+++ packages/TDTPy/TDTPy-0.7.zip/TDTPy-0.7/setup.py
@@ -0,0 +1,39 @@
+from distutils.core import setup
+from os import path
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Science/Research',
+    #'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Topic :: Scientific/Engineering',
+    'Topic :: System :: Hardware',
+    'License :: OSI Approved :: GNU General Public License (GPL)'
+]
+
+here = path.dirname(path.abspath(__file__))
+with open(path.join(here, 'readme.rst')) as f:
+    long_description = f.read().strip()
+
+long_description += '''
+
+Source code: http://bitbucket.org/bburan/tdtpy
+
+Documentation: http://tdtpy.readthedocs.org
+
+'''
+
+setup(
+    name='TDTPy',
+    version='0.7',
+    author='Brad Buran',
+    author_email='bburan@alum.mit.edu',
+    packages=['tdt', 'tdt.actxobjects', 'tdt.device'],
+    url='http://bradburan.com/programs/tdtpy',
+    license='GPLv3',
+    description='Module for communicating with TDT\'s System 3 hardware',
+    long_description=long_description,
+    requires=['win32com'],
+    package_data={'tdt': ['components/*.rcx']},
+    classifiers=CLASSIFIERS,
+)
