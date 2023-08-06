9e10444962c5f65df5e0fdade33fb37b848f5286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:47 +0000

    Add package snsapi/0.6.1/snsapi-0.6.1.tar.gz

---
+++ packages/snsapi/snsapi-0.6.1.tar.gz/snsapi-0.6.1/snsapi/plugin/__init__.py
@@ -0,0 +1,10 @@
+#-*- encoding: utf-8 -*-
+
+from tencent import TencentWeiboStatus
+from sina import SinaWeiboStatus
+from renren import RenrenStatus
+from renren import RenrenShare
+from renren import RenrenBlog
+from rss import RSS
+from rss import RSS2RW
+from rss import RSSSummary
