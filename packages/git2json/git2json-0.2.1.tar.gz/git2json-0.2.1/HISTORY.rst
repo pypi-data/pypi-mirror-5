4c6f37d63c0e0069bc62bb6e82208e35ee047519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:34 +0000

    Add package git2json/0.2.1/git2json-0.2.1.tar.gz

---
+++ packages/git2json/git2json-0.2.1.tar.gz/git2json-0.2.1/HISTORY.rst
@@ -0,0 +1,25 @@
+.. :changelog:
+
+History
+-------
+
+0.1.0 (2013-09-23)
+++++++++++++++++++
+
+* First release on PyPI.
+
+0.1.1 (2013-09-25)
+++++++++++++++++++
+
+* Fixed broken PyPI package.
+
+0.2.0 (2013-09-28)
+++++++++++++++++++
+
+* Parents now an array instead of a single value.
+
+0.2.1 (2013-10-07)
+++++++++++++++++++
+
+* Re-implemented parser with regular expressions.
+* Character encoding issues fixed.
---
+++ packages/git2json/git2json-0.2.1.tar.gz/git2json-0.2.1/git2json/__init__.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Generate a json log of a git repository.
+"""
+
+__author__ = 'Tavish Armstrong'
+__email__ = 'tavisharmstrong@gmail.com'
+__version__ = '0.2.1'
+
+
+import json
+import sys
+from parser import parse_commits
+
+#-------------------------------------------------------------------
+# Main
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--git-dir',
+        default=None,
+        help='Path to the .git/ directory of the repository you are targeting'
+    )
+    args = parser.parse_args()
+    if sys.version_info < (3, 0):
+        print (git2json(run_git_log(args.git_dir)))
+    else:
+        print (git2jsons(run_git_log(args.git_dir)))
+
+#-------------------------------------------------------------------
+# Main API functions
+
+
+def git2jsons(s):
+    return json.dumps(list(parse_commits(s)), ensure_ascii=False)
+
+
+def git2json(fil):
+    return json.dumps(list(parse_commits(fil.read())), ensure_ascii=False)
+
+
+#-------------------------------------------------------------------
+# Functions for interfacing with git
+
+
+def run_git_log(git_dir=None):
+    '''run_git_log([git_dir]) -> File
+
+    Run `git log --numstat --pretty=raw` on the specified
+    git repository and return its stdout as a pseudo-File.'''
+    import subprocess
+    if git_dir:
+        command = [
+            'git',
+            '--git-dir=' + git_dir,
+            'log',
+            '--numstat',
+            '--pretty=raw'
+        ]
+    else:
+        command = ['git', 'log', '--numstat', '--pretty=raw']
+    raw_git_log = subprocess.Popen(
+        command,
+        stdout=subprocess.PIPE
+    )
+    if sys.version_info < (3, 0):
+        return raw_git_log.stdout
+    else:
+        return raw_git_log.stdout.read().decode('utf-8', 'ignore')
