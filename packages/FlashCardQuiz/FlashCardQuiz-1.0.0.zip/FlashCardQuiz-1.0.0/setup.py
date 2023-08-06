18bc08670dcced1d8a8563595f992739650e3b67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:25 +0000

    Add package FlashCardQuiz/1.0.0/FlashCardQuiz-1.0.0.zip

---
+++ packages/FlashCardQuiz/FlashCardQuiz-1.0.0.zip/FlashCardQuiz-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup 
+
+setup(
+        name                 ='FlashCardQuiz',
+        version              ='1.0.0',
+        py_modules           =['FlashCardQuiz'],
+        author               ='akshay',
+        author_email         ='akshayhpai25@gmail.com',
+        description          ='a flash card quiz game',
+        platform             ='python3',
+      )
