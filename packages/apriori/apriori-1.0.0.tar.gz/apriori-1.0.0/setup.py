6ae777f49f0d70ddf99c84fc0dc0265fc002f683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:05 +0000

    Add package apriori/1.0.0/apriori-1.0.0.tar.gz

---
+++ packages/apriori/apriori-1.0.0.tar.gz/apriori-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name = 'apriori',
+	version = '1.0.0',
+	py_modules = ['apriori'],
+	author = 'chenliyu',
+	author_email = '2572065090@qq.com',
+	url = 'http://pypi.python.org/ ',
+	description = 'implimention of apriori in python',
+)
+
