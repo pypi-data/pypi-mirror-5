33fc3d9aa5a3eafc3df781be77af60681901100f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:29 +0000

    Add package feedfinder2/0.0.1/feedfinder2-0.0.1.tar.gz

---
+++ packages/feedfinder2/feedfinder2-0.0.1.tar.gz/feedfinder2-0.0.1/setup.py
@@ -0,0 +1,32 @@
+import os
+import sys
+from setuptools import setup
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist upload")
+    sys.exit()
+
+setup(
+    name="feedfinder2",
+    version="0.0.1",
+    url="https://github.com/dfm/feedfinder2",
+    license="MIT",
+    author="Dan Foreman-Mackey",
+    author_email="hi@dfm.io",
+    install_requires=[
+        "requests",
+        "beautifulsoup4",
+    ],
+    description="Find the feed URLs for a website.",
+    long_description=open("README.rst").read(),
+    py_modules=["feedfinder2"],
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Natural Language :: English",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
