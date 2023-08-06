8b5006af65888008e7fee77bf245d8d0cee92924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:44 +0000

    Add package weave-minimal/1.0/weave-minimal-1.0.tar.gz

---
+++ packages/weave-minimal/weave-minimal-1.0.tar.gz/weave-minimal-1.0/weave/minimal/misc.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+from werkzeug import Response
+
+def captcha_html(app, environ, request, version):
+    return Response('<img src="http://posativ.org/scratch.png" \
+    width="298px" height="130" style="overflow-x: scroll; overflow-y: hidden;" />', content_type="text/html")
---
+++ packages/weave-minimal/weave-minimal-1.0.tar.gz/weave-minimal-1.0/weave/minimal/utils.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+from werkzeug import Response
+
+import re
+import json
+import base64
+import struct
+
+from os.path import isfile
+from hashlib import sha1
+
+from weave.minimal.compat import iterkeys
+
+
+def encode(uid):
+    if re.search('[^A-Z0-9._-]', uid, re.I):
+        return base64.b32encode(sha1(uid).digest()).lower()
+    return uid
+
+
+class login:
+    """login decorator using HTTP Basic Authentication. Pattern based on
+    http://flask.pocoo.org/docs/patterns/viewdecorators/"""
+
+    def __init__(self, methods=['GET', 'POST', 'DELETE', 'PUT']):
+        self.methods = methods
+
+    def __call__(self, f):
+
+        def dec(app, env, req, *args, **kwargs):
+            """This decorater function will send an authenticate header, if none
+            is present and denies access, if HTTP Basic Auth fails."""
+            if req.method not in self.methods:
+                return f(app, env, req, *args, **kwargs)
+            if not req.authorization:
+                response = Response('Unauthorized', 401)
+                response.www_authenticate.set_basic('Weave')
+                return response
+            else:
+                user = req.authorization.username
+                passwd = req.authorization.password
+                if not isfile(app.dbpath(user, passwd)):
+                    return Response('Unauthorized', 401)  # kinda stupid
+                return f(app, env, req, *args, **kwargs)
+        return dec
+
+
+def wbo2dict(query):
+    """converts sqlite table to WBO (dict [json-parsable])"""
+
+    res = {'id': query[0], 'modified': round(query[1], 2),
+           'sortindex': query[2], 'payload': query[3],
+           'parentid': query[4], 'predecessorid': query[5], 'ttl': query[6]}
+
+    for key in list(iterkeys(res)):
+        if res[key] is None:
+            res.pop(key)
+
+    return res
+
+
+def convert(value, mime):
+    """post processor producing lists in application/newlines format."""
+
+    if mime and mime.endswith(('/newlines', '/whoisi')):
+        try:
+            value = value["items"]
+        except (KeyError, TypeError):
+            pass
+
+        if mime.endswith('/whoisi'):
+            res = []
+            for record in value:
+                js = json.dumps(record)
+                res.append(struct.pack('!I', len(js)) + js.encode('utf-8'))
+            rv = b''.join(res)
+        else:
+            rv = '\n'.join(json.dumps(item).replace('\n', '\000a') for item in value)
+    else:
+
+        rv, mime = json.dumps(value), 'application/json'
+
+    return rv, mime, len(value)
