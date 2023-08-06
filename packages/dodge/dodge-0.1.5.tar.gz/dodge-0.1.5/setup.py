388762a644c8db091167e1c6686edbc8bb8dd711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:03 +0000

    Add package dodge/0.1.5/dodge-0.1.5.tar.gz

---
+++ packages/dodge/dodge-0.1.5.tar.gz/dodge-0.1.5/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+import sys
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+if sys.version_info[:2] < (2, 7):
+    install_requires = ["ordereddict>=1.1,<2.0"]
+else:
+    install_requires = []
+    
+setup(
+    name='dodge',
+    version='0.1.5',
+    description='Create data objects that can be easily converted to and from dicts suitable for use as JSON',
+    long_description=read("README"),
+    author='Michael Williamson',
+    author_email='mike@zwobble.org',
+    url='http://github.com/mwilliamson/dodge.py',
+    keywords="data object serialise serialisation json",
+    packages=['dodge'],
+    install_requires=install_requires,
+)
