272644d1d12c68d33673569a0baa0b3528976894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:57 +0000

    Add package django-uturn/0.3.0/django-uturn-0.3.0.tar.gz

---
+++ packages/django-uturn/django-uturn-0.3.0.tar.gz/django-uturn-0.3.0/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='django-uturn',
+    version='0.3.0',
+    author='Kevin Wetzels',
+    author_email='kevin@roam.be',
+    url='https://github.com/roam/django-uturn',
+    install_requires=['Django>=1.4'],
+    packages=['uturn', 'uturn.templatetags'],
+    license='BSD licence, see LICENCE',
+    description='Overriding redirects in Django, to return where you came '\
+                'from',
+    long_description=open('README.rst').read(),
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+   ],
+)
