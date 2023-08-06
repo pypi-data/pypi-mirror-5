ad0fa7721ae0125df06930273c84907427a46cdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:59 +0000

    Add package tweetup-v1.3/1.2/tweetup-v1.3-1.2.tar.gz

---
+++ packages/tweetup-v1.3/tweetup-v1.3-1.2.tar.gz/tweetup-v1.3-1.2/readme.rst
@@ -0,0 +1,15 @@
+tweetup
+-----------
+A script will tweet along with a media file.
+
+`tweetup`_ code link.
+
+.. _tweetup: https://github.com/anurag619/mywork/tree/master/tweet/tweetup
+ 
+
+
+
+
+$python sharevalue.py FB
+
+
---
+++ packages/tweetup-v1.3/tweetup-v1.3-1.2.tar.gz/tweetup-v1.3-1.2/setup.py
@@ -0,0 +1,17 @@
+
+#!/usr/bin/env python
+"""tweetup-v1.2 script"""
+from setuptools import find_packages, setup
+
+setup(name = 'tweetup-v1.3',
+    version = '1.2',
+    description = "Tweetup  module.",
+    long_description = "A script created to tweet with a media",
+    platforms = ["Linux"],
+    author="Anurag Kumar",
+    author_email="anurag3rdsep@gmail.com",
+    url="https://github.com/anurag619/mywork/tree/master/tweet",
+    install_requires=["Twython", "simplejson", "oauth2", "httlib2"],
+    license = "MIT",
+    packages=find_packages()
+    )
