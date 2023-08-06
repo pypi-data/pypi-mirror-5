26a315e0767126ec82311d18c0954bd184216353
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:48 +0000

    Add package ftree/1.0.1/ftree-1.0.1.tar.gz

---
+++ packages/ftree/ftree-1.0.1.tar.gz/ftree-1.0.1/README.rst
@@ -0,0 +1,3 @@
+FTree is used to create file trees from directory listing (console-based).
+
+    *Should be compatible on any system with Python 2/3*
---
+++ packages/ftree/ftree-1.0.1.tar.gz/ftree-1.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ftree',
+    version='1.0.1',
+    author='Victor Kindhart',
+    author_email='digitloft@gmail.com',
+    url='https://pypi.python.org/pypi/ftree',
+    packages=find_packages(),
+    install_requires=[],
+    license='GPL',
+    description='Create a File Tree from directory listing.',
+    long_description=open('README.rst').read(),
+    classifiers=["Environment :: Console", "Programming Language :: Python"],
+    )
