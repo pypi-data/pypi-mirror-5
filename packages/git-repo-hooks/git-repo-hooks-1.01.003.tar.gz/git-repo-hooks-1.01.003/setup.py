d10fc493fd606e79e3dd4c8da62556f52f9e9dd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:49 +0000

    Add package git-repo-hooks/1.01.003/git-repo-hooks-1.01.003.tar.gz

---
+++ packages/git-repo-hooks/git-repo-hooks-1.01.003.tar.gz/git-repo-hooks-1.01.003/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+'''
+Created on Feb 18, 2013
+
+@author: legion
+'''
+from setuptools import setup
+setup(
+	name = "git-repo-hooks",
+	version = "1.01.003",
+	scripts = ['git-hooks'],
+
+	# Project uses reStructuredText, so ensure that the docutils get
+	# installed or upgraded on the target machine
+	install_requires = ['PyYAML'],
+
+	# metadata for upload to PyPI
+    author='K Jonathan',
+    author_email='phenixdoc@gmail.com',
+    description='A tool to manage project, user, and global Git hooks for multiple git repositories',
+    url='https://github.com/legion0/git-repo-hooks',
+)
+
