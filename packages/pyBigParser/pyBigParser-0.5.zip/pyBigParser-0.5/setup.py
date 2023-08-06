3b60e7874df26f60c330057313c256502a4c53da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:45 +0000

    Add package pyBigParser/0.5/pyBigParser-0.5.zip

---
+++ packages/pyBigParser/pyBigParser-0.5.zip/pyBigParser-0.5/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='pyBigParser',
+    version='0.5',
+    py_modules=['evaluator'],
+    author='Nelson Carrasquel',
+    author_email='carrasquel@outlook.com',
+    url='',
+    packages=['pybigparser',],
+    license='BSD License',
+    long_description=open('README.txt').read(),
+)
