dacdededbc7a1f38df96eec31033494d552265c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:31 +0000

    Add package postgres/1.1.0/postgres-1.1.0.tar.gz

---
+++ packages/postgres/postgres-1.1.0.tar.gz/postgres-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup( name='postgres'
+     , author='Gittip, LLC'
+     , description="postgres is a high-value abstraction over psycopg2."
+     , url='https://postgres-py.readthedocs.org'
+     , version='1.1.0'
+     , py_modules=['postgres']
+     , install_requires=['psycopg2 >= 2.0.0']
+      )
