e52d049f1a626e8826298eb600f1ca8249df54a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:02 +0000

    Add package Temper/0.1/Temper-0.1.tar.gz

---
+++ packages/Temper/Temper-0.1.tar.gz/Temper-0.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+
+setup(
+    name='Temper',
+    version='0.1',
+    description='Pure Python HTML DSL',
+    author='Ian Robinson',
+    author_email='ianronrobinson@gmail.com',
+    url='https://github.com/drtchops/temper',
+    download_url='https://github.com/drtchops/temper/tarball/0.1',
+    packages=['temper', 'temper.ext'],
+    keywords=['template', 'templating', 'html'],
+    classifiers=[],
+)
