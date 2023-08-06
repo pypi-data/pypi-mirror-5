87a4d25bbcde887b90b6b774835ee60af611f44d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:13 +0000

    Add package ping_sweep/1.0.1/ping_sweep-1.0.1.zip

---
+++ packages/ping_sweep/ping_sweep-1.0.1.zip/ping_sweep-1.0.1/setup.py
@@ -0,0 +1,24 @@
+
+
+from setuptools import setup, find_packages
+
+entry_points = {'console_scripts': ['ping_sweep = ping_sweep:main']}
+
+# Other stuff.
+version = '1.0.1'
+description = 'Ping return time analysis tool'
+
+# Do it.
+setup(name='ping_sweep',
+      packages=find_packages(),
+
+      entry_points=entry_points,
+
+      # Metadata
+      version=version,
+      license='FreeBSD',
+      author='Pierre V. Villeneuve',
+      author_email='pierre.villeneuve@gmail.com',
+      url='https://github.com/Who8MyLunch/Ping_Sweep',
+      description=description,
+)
