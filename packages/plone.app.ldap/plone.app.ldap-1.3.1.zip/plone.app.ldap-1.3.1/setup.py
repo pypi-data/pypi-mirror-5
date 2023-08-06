d99bb70c184865836c30021f4d7fc73a9946cd51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:24 +0000

    Add package plone.app.ldap/1.3.1/plone.app.ldap-1.3.1.zip

---
+++ packages/plone.app.ldap/plone.app.ldap-1.3.1.zip/plone.app.ldap-1.3.1/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+version = '1.3.1'
+
+setup(name='plone.app.ldap',
+      version=version,
+      description="LDAP control panel for Plone 4.1 and higher",
+      long_description=(open("README.txt").read() + "\n" +
+                        open("CHANGES.rst").read()),
+      classifiers=[
+          "Framework :: Plone",
+          "Framework :: Plone :: 4.1",
+          "Framework :: Plone :: 4.2",
+          "Framework :: Plone :: 4.3",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "Topic :: System :: Systems Administration :: Authentication/Directory :: LDAP",
+          ],
+      keywords='plone ldap',
+      author='Wichert Akkerman - Simplon',
+      author_email='wichert@simplon.biz',
+      maintainer='Kevin Teague',
+      maintainer_email='kevin@bud.ca',
+      url='http://pypi.python.org/pypi/plone.app.ldap',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone', 'plone.app'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # "python-ldap",
+          "Products.CMFCore",
+          "Products.PloneLDAP",
+          "setuptools"
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
