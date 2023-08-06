949b5db1cdcc1ef0726e9840588db6edbd8b183b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:06 +0000

    Add package pynsive/0.1.2/pynsive-0.1.2.tar.gz

---
+++ packages/pynsive/pynsive-0.1.2.tar.gz/pynsive-0.1.2/pynsive/reflection.py
@@ -0,0 +1,104 @@
+import sys
+import os.path
+import inspect
+
+from .common import *
+
+
+def _list_classes(module, type_filter):
+    found = list()
+
+    for name, module_obj in inspect.getmembers(module):
+        if inspect.isclass(module_obj):
+            append = not type_filter or type_filter(module_obj)
+            if append:
+                found.append(module_obj)
+    return found
+
+
+def _should_use_module(module):
+    return hasattr(module, PATH_ATTRUBITE) and len(module.__path__) > 0
+
+
+def _scan_paths_for(mname, paths):
+    mpath_part = mname.replace(MODULE_PATH_SEP, os.sep)
+    for path in paths:
+        scan_target = os.path.join(path, mpath_part)
+        is_dir = os.path.isdir(scan_target)
+        is_file = os.path.isfile('{}.py'.format(scan_target))
+        if is_dir or is_file:
+            return mpath
+
+
+def _scan_dir(directory):
+    found = list()
+    module_py = os.path.join(directory, MODULE_INIT_FILE)
+    if os.path.isfile(module_py):
+        # Scan only if there's an __init__.py file
+        for dir_entry in os.listdir(directory):
+            if dir_entry != MODULE_INIT_FILE and dir_entry.endswith('.py'):
+                found.append(dir_entry.rstrip('.py'))
+    return found
+
+
+def list_classes(mname, type_filter=None):
+    """
+    Attempts to list all of the clases within a specified module. This
+    function works for modules located in the default path as well as
+    extended paths via the sys.meta_path hooks.
+
+    If a type filter is set, it will be called with each class as its
+    parameter. This filter's return value must be interpretable as a
+    boolean. Results that evaluate as True will include the type in the
+    list of returned classes. Results that evaluate as False will excluse
+    the type in the list of returned classes.
+    """
+    found = list()
+    module = import_module(mname)
+    if inspect.ismodule(module):
+        [found.append(mod) for mod in _list_classes(module, type_filter)]
+    return found
+
+
+def discover_modules(mname):
+    """
+    Attempts to list all of the submodules under a module. This function
+    works for modules located in the default path as well as extended paths
+    via the sys.meta_path hooks.
+
+    This funciton carries the expectation that the hidden module variable
+    '__path__' has been set correctly.
+    """
+    found = list()
+    module = import_module(mname)
+
+    if module and _should_use_module(module):
+        mpath = module.__path__[0]
+    else:
+        mpaths = sys.path
+        mpath = _scan_paths_for(mname, mpaths)
+
+    if mpath:
+        for pmname in _scan_dir(mpath):
+            found_mod = '{}{}{}'.format(mname, MODULE_PATH_SEP, pmname)
+            found.append(found_mod)
+    return found
+
+
+def discover_classes(module, type_filter=None):
+    """
+    Attempts to list all of the classes within a given module namespace.
+    This method, unlike list_classes, will recurse into discovered
+    submodules.
+
+    If a type filter is set, it will be called with each class as its
+    parameter. This filter's return value must be interpretable as a
+    boolean. Results that evaluate as True will include the type in the
+    list of returned classes. Results that evaluate as False will excluse
+    the type in the list of returned classes.
+    """
+    classes = list()
+    mnames = discover_modules(module)
+    for mname in mnames:
+        [classes.append(c) for c in list_classes(mname, type_filter)]
+    return classes
---
+++ packages/pynsive/pynsive-0.1.2.tar.gz/pynsive-0.1.2/setup.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup, find_packages
+    from setuptools.command import easy_install
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+    from setuptools.command import easy_install
+
+import sys
+from os import path
+from distutils.core import setup
+from distutils.extension import Extension
+
+
+def read(relative):
+    contents = open(relative, 'r').read()
+    return [l for l in contents.split('\n') if l != '']
+
+
+def ez_install(package):
+    easy_install.main(["-U", package])
+
+
+setup(
+    name='pynsive',
+    version='0.1.2',
+    description=("""A Python plugin and introspection library prounounced,
+"pensive." This library allows a developer to introduce plugins via PEP 302
+and allows for class searching via type and inheritance introspections.
+"""),
+    author='John Hopper',
+    author_email='john.hopper@jpserver.net',
+    url='https://github.com/zinic/pynsive',
+    license='Apache 2.0',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Environment :: Plugins',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3'
+    ],
+    tests_require=read('./tools/test-requires'),
+    install_requires=read('./tools/install-requires'),
+    test_suite='nose.collector',
+    zip_safe=False,
+    include_package_data=True,
+    packages=find_packages(exclude=['ez_setup']))
