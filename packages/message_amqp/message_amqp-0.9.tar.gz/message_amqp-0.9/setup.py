630fb453ab8e4de91a6f16691700a63f03cf5dfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:40 +0000

    Add package message_amqp/0.9/message_amqp-0.9.tar.gz

---
+++ packages/message_amqp/message_amqp-0.9.tar.gz/message_amqp-0.9/setup.py
@@ -0,0 +1,17 @@
+import os
+from distutils.core import setup
+
+setup(
+    name='message_amqp',
+    packages = [""],
+    description='amqp setup for python nodejs and php',
+    version='0.9',
+    author="Borrey Kim",
+    author_email="borrey@gmail.com",
+    url="git@github.com:borrey/message-amqp.git",
+    download_url="https://bitbucket.org/borreykim/message-amqp/downloads/message_amqp-0.9.tar.gz",
+    keywords=['amqp'],
+    long_description = """\
+    Python module to handle messaging with other message_amqp services.
+    """
+)
