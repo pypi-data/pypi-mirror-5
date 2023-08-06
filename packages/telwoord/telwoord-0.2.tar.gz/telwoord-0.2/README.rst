c7e89a9c50e5af1eb87feee2ca938b82b0db7fe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:18 +0000

    Add package telwoord/0.2/telwoord-0.2.tar.gz

---
+++ packages/telwoord/telwoord-0.2.tar.gz/telwoord-0.2/README.rst
@@ -0,0 +1,122 @@
+Telwoord
+========
+
+A Python library to convert numbers into their Dutch (nl) written form.
+
+Links:
+
+* Code: https://github.com/wbolster/telwoord
+* PyPI: https://pypi.python.org/pypi/telwoord
+
+
+Installation
+============
+
+
+To install from PyPI::
+
+    $ pip install telwoord
+
+There are no dependencies; a Python 2 or Python 3 interpreter is all you need.
+
+
+Usage
+=====
+
+Use ``telwoord.cardinal()`` to convert an integer to its string representation.
+
+By default, a friendly representation is used, based on some common style
+recommendations about when to spell out numbers, and when to leave them as is::
+
+    >>> from telwoord import cardinal
+
+    >>> numbers = (0, 1, 2, 3, 19, 20, 30, 31, 100, 200, 215, 300, 14999, 15000, 1000000, 12345678)
+
+    >>> for n in numbers: print("{0: 10d}   {1}".format(n, cardinal(n)))
+             0   nul
+             1   een
+             2   twee
+             3   drie
+            19   negentien
+            20   twintig
+            30   dertig
+            31   31
+           100   honderd
+           200   tweehonderd
+           215   215
+           300   driehonderd
+         14999   14999
+         15000   15 duizend
+       1000000   1 miljoen
+      12345678   12345678
+
+To forcibly spell out all the numbers, pass ``friendly=False``::
+
+    >>> for n in numbers: print("{0: 10d}   {1}".format(n, cardinal(n, friendly=False)))
+             0   nul
+             1   een
+             2   twee
+             3   drie
+            19   negentien
+            20   twintig
+            30   dertig
+            31   eenendertig
+           100   honderd
+           200   tweehonderd
+           215   tweehonderdvijftien
+           300   driehonderd
+         14999   veertienduizend negenhonderdnegenennegentig
+         15000   vijftienduizend
+       1000000   een miljoen
+      12345678   twaalf miljoen driehonderdvijfenveertigduizend zeshonderdachtenzeventig
+
+
+Spelling rules and style recommendations
+========================================
+
+* Taalunie, `Aaneenschrijven van telwoorden
+  <http://woordenlijst.org/leidraad/6/9/>`_
+
+* Genootschap Onze Taal, `Getallen in letters of cijfers
+  <https://onzetaal.nl/taaladvies/advies/getallen-in-letters-of-cijfers>`_
+
+* Genootschap Onze Taal, `Getallen uitschrijven
+  <https://onzetaal.nl/taaladvies/advies/getallen-uitschrijven>`_
+
+
+Version history
+===============
+
+* 0.2
+
+  * Improved support for friendly representation of negative numbers
+
+* 0.1
+
+  * Initial release
+  * Support for spelling out numbers, optionally in a friendly way
+
+
+Development
+===========
+
+To install from a source tree::
+
+    $ pip install -e .
+
+To run the tests you will need a few extra packages::
+
+    $ pip install -r test-requirements.txt
+
+To run the test::
+
+    $ py.test
+
+To test against multiple Python 2 and 3 versions::
+
+    $ tox
+
+Possible future work:
+
+* Ordinal numbers
+* Fractions
---
+++ packages/telwoord/telwoord-0.2.tar.gz/telwoord-0.2/telwoord/telwoord.py
@@ -0,0 +1,124 @@
+# coding: UTF-8
+
+from __future__ import unicode_literals
+
+try:
+    unicode
+except NameError:
+    # Python 3
+    unicode = str
+
+UNITS = [
+    "nul", "een", "twee", "drie", "vier", "vijf", "zes", "zeven",
+    "acht", "negen", "tien", "elf", "twaalf", "dertien", "veertien",
+    "vijftien", "zestien", "zeventien", "achttien", "negentien"]
+
+TENS = [
+    None, "tien", "twintig", "dertig", "veertig", "vijftig", "zestig",
+    "zeventig", "tachtig", "negentig"]
+
+MILLION = 10 ** 6
+BILLION = 10 ** 9
+TRILLION = 10 ** 12
+QUADRILLION = 10 ** 15
+
+
+def cardinal(n, friendly=True):
+    """Convert a number into its Dutch written form.
+
+    If `friendly` is `True` (the default), this function uses various
+    heuristics for "human friendly" results.
+
+    If `friendly` is `False`, the number is always spelled out in full,
+    which will result in not so readable results for many larger
+    numbers.
+    """
+    if friendly:
+        n_abs = abs(n)
+
+        if n_abs < 20:
+            return cardinal(n, friendly=False)
+
+        if n_abs < 100 and n_abs % 10 == 0:
+            return cardinal(n, friendly=False)
+
+        if n_abs < 1000 and n_abs % 100 == 0:
+            return cardinal(n, friendly=False)
+
+        if n_abs < 12000 and n_abs % 1000 == 0:
+            return cardinal(n, friendly=False)
+
+        if n_abs < MILLION:
+            q, r = divmod(n, 1000)
+            if r == 0:
+                return "%d duizend" % q
+
+        if n_abs < BILLION:
+            q, r = divmod(n, MILLION)
+            if r == 0:
+                return "%d miljoen" % q
+
+        # No friendly variant, just return the numerical representation.
+        return unicode(n)
+
+    # Code below completely spells out each number.
+
+    if n < 0:
+        return "min " + cardinal(abs(n))
+
+    if n < 20:
+        return UNITS[n]
+
+    if n < 100:
+        q, r = divmod(n, 10)
+        a = TENS[q]
+        if r == 0:
+            return a
+        b = cardinal(r)
+        joiner = "en" if not b.endswith("e") else "ën"
+        return b + joiner + a
+
+    if n < 1000:
+        q, r = divmod(n, 100)
+        a = cardinal(q, friendly=False) if q > 1 else ""
+        b = cardinal(r, friendly=False) if r > 0 else ""
+        return a + "honderd" + b
+
+    if 1000 < n < 10000 and n % 1000:
+        # Special case for numbers that are exactly divisble by 100, but
+        # not by 1000, e.g. "tweeëntwintighonderd"
+        q, r = divmod(n, 100)
+        if r == 0:
+            a = cardinal(q, friendly=False) if q > 1 else ""
+            return a + "honderd"
+
+    if n < MILLION:
+        q, r = divmod(n, 1000)
+        a = cardinal(q, friendly=False) if q > 1 else ""
+        b = " " + cardinal(r, friendly=False) if r > 0 else ""
+        return a + "duizend" + b
+
+    if n < BILLION:
+        q, r = divmod(n, MILLION)
+        a = cardinal(q, friendly=False)
+        b = " " + cardinal(r, friendly=False) if r > 0 else ""
+        return a + " miljoen" + b
+
+    if n < TRILLION:
+        q, r = divmod(n, BILLION)
+        a = cardinal(q, friendly=False)
+        b = " " + cardinal(r, friendly=False) if r > 0 else ""
+        return a + " miljard" + b
+
+    if n < QUADRILLION:
+        q, r = divmod(n, TRILLION)
+        a = cardinal(q, friendly=False)
+        b = " " + cardinal(r, friendly=False) if r > 0 else ""
+        return a + " biljoen" + b
+
+    # Fallback to numerical representation
+    return unicode(n)
+
+
+def ordinal(n):
+    raise NotImplementedError("Ordinal numbers not yet implemented")
