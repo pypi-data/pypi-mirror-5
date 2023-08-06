1b4f87538e74faf8ef6023b42629489e0a87451a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:25 +0000

    Add package django_function_cache/1.0/django_function_cache-1.0.tar.gz

---
+++ packages/django_function_cache/django_function_cache-1.0.tar.gz/django_function_cache-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='django_function_cache',
+      description="Cache (memoize) a function's output using Django's caching API",
+      version='1.0',
+      url="https://github.com/Wilfred/django-function-cache",
+      maintainer="Wilfred Hughes",
+      maintainer_email="me@wilfred.me.uk",
+      py_modules=['cached'],
+      )
