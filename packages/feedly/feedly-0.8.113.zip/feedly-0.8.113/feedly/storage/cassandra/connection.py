98906e25d75bd773803c4324261fe50dc617cf0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:29 +0000

    Add package feedly/0.8.113/feedly-0.8.113.zip

---
+++ packages/feedly/feedly-0.8.113.zip/feedly-0.8.113/feedly/storage/cassandra/connection.py
@@ -0,0 +1,10 @@
+from cqlengine import connection
+from feedly import settings
+
+
+def setup_connection():
+    connection.setup(
+        settings.FEEDLY_CASSANDRA_HOSTS,
+        consistency=settings.FEEDLY_CASSANDRA_CONSITENCY_LEVEL,
+        default_keyspace=settings.FEEDLY_DEFAULT_KEYSPACE
+    )
