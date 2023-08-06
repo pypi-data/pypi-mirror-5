95f4d22057087110893e3a2e62098b5f474e09b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:12 +0000

    Add package pysigset/0.2.1/pysigset-0.2.1.tar.gz

---
+++ packages/pysigset/pysigset-0.2.1.tar.gz/pysigset-0.2.1/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+with open('README.txt') as file:
+    long_description = file.read()
+
+setup(
+    name='pysigset',
+    # Trying to use a PEP386 and distutils.version.StrictVersion compatible
+    # versioning scheme here: 0.2a sorts before 0.2 and will mean
+    # not-exactly-0.2-yet.
+    version='0.2.1',
+    py_modules=['pysigset'],
+    description='Signal blocking under Linux & OS X',
+    long_description=long_description,
+    author='Walter Doekes',
+    author_email='wjdoekes@osso.nl',
+    url='https://github.com/ossobv/pysigset',
+    license='GPLv3+',
+    platforms=('linux', 'darwin'),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        ('License :: OSI Approved :: GNU General Public License v3 or later '
+         '(GPLv3+)'),
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Libraries',
+    ],
+)
+
+# vim: set ts=8 sw=4 sts=4 et ai tw=79:
