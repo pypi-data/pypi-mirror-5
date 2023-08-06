1820984627d81f7a562c12f49f54d94bfa5e6662
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:30 +0000

    Add package foxInstaller/0.0.78/foxInstaller-0.0.78.zip

---
+++ packages/foxInstaller/foxInstaller-0.0.78.zip/foxInstaller-0.0.78/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "foxInstaller",
+    version = "0.0.78",
+    author = "Jyrno Ader",
+    author_email = "jyrno42@gmail.com",
+    description = ("A simple installation script for my own django projects."),
+    license = "BSD",
+    keywords = "foxInstaller django jyrno42",
+    url = "http://th3f0x.com",
+    packages=['foxInstaller'],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
