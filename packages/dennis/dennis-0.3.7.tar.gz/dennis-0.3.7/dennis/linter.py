bc3ea26b53003bedabd8941ac112b83917bc1ce6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:40 +0000

    Add package dennis/0.3.7/dennis-0.3.7.tar.gz

---
+++ packages/dennis/dennis-0.3.7.tar.gz/dennis-0.3.7/dennis/linter.py
@@ -0,0 +1,206 @@
+from collections import namedtuple
+
+import polib
+
+from dennis.tools import VariableTokenizer
+
+
+TranslatedString = namedtuple(
+    'TranslatedString',
+    ('msgid_field', 'msgid_string', 'msgstr_field', 'msgstr_string'))
+
+
+class LintedEntry(object):
+    def __init__(self, poentry):
+        self.poentry = poentry
+        self.msgid = poentry.msgid
+
+        strs = []
+
+        if not poentry.msgid_plural:
+            strs.append(
+                TranslatedString(
+                    'msgid', poentry.msgid, 'msgstr', poentry.msgstr))
+
+        else:
+            for key in sorted(poentry.msgstr_plural.keys()):
+                if key == '0':
+                    # This is the 1 case
+                    msgid_field = 'msgid'
+                    text = poentry.msgid
+                else:
+                    msgid_field = 'msgid_plural'
+                    text = poentry.msgid_plural
+                strs.append(
+                    TranslatedString(
+                        msgid_field,
+                        text,
+                        'msgstr[{0}]'.format(key),
+                        poentry.msgstr_plural[key]))
+
+        # List of (msgid field, msgid string, msgstr field, msgstr
+        # string) tuples
+        self.strs = strs
+
+        self.warnings = []
+        self.errors = []
+
+    def add_warning(self, code, trstr, msg):
+        self.warnings.append((code, trstr, msg))
+
+    def add_error(self, code, trstr, msg):
+        self.errors.append((code, trstr, msg))
+
+    def has_problems(self, errorsonly=False):
+        if errorsonly:
+            return bool(self.errors)
+        return bool(self.warnings or self.errors)
+
+
+class LintRule(object):
+    name = ''
+    desc = ''
+
+    def lint(self, vartok, linted_entry):
+        """Takes a linted entry and adds errors and warnings
+
+        :arg vartok: the variable tokenizer to use for tokenizing
+            on variable tokens
+        :arg linted_entry: the LintedEntry to work on
+
+        """
+        raise NotImplemented
+
+
+class MalformedVarsLintRule(LintRule):
+    name = 'malformed'
+    desc = 'Checks for malformed variables that cause errors'
+
+    def lint(self, vartok, linted_entry):
+        malformed_re = vartok.malformed_vars_re
+
+        for trstr in linted_entry.strs:
+            if not trstr.msgstr_string:
+                continue
+
+            # For each Var class, we ask it to find malformed
+            for var in vartok.vars_:
+                malformed = malformed_re.findall(trstr.msgstr_string)
+                if not malformed:
+                    continue
+
+                malformed = [item.strip() for item in malformed]
+                linted_entry.add_error(
+                    self.name,
+                    trstr,
+                    u'malformed variables: {0}'.format(u', '.join(malformed)))
+
+
+class MismatchedVarsLintRule(LintRule):
+    name = 'mismatched'
+    desc = 'Checks for variables in one string not in the other'
+
+    @classmethod
+    def compare_lists(cls, list_a, list_b):
+        """Compares contents of two lists
+
+        This returns two lists:
+
+        * list of tokens in list_a missing from list_b
+
+        * list of tokens in list_b missing from list_a
+
+        :returns: tuple of (list of tokens in list_a not in list_b, list
+            of tokens in list_b not in list_a)
+
+        """
+        return (
+            [token for token in list_a if token not in list_b],
+            [token for token in list_b if token not in list_a]
+        )
+
+    def lint(self, vartok, linted_entry):
+        for trstr in linted_entry.strs:
+            if not trstr.msgstr_string:
+                continue
+
+            missing, invalid = self.compare_lists(
+                vartok.extract_tokens(trstr.msgid_string),
+                vartok.extract_tokens(trstr.msgstr_string))
+
+            if missing:
+                linted_entry.add_warning(
+                    self.name,
+                    trstr,
+                    u'missing variables: {0}'.format(u', '.join(missing)))
+
+            if invalid:
+                linted_entry.add_error(
+                    self.name,
+                    trstr,
+                    u'invalid variables: {0}'.format(u', '.join(invalid)))
+
+
+def get_available_lint_rules():
+    lint_rules = {}
+
+    for name, thing in globals().items():
+        if (name.endswith('LintRule')
+            and issubclass(thing, LintRule)
+            and thing.name):
+            lint_rules[thing.name] = thing
+
+    return lint_rules
+
+
+class InvalidRulesSpec(Exception):
+    pass
+
+
+def convert_rules(rules_spec):
+    lint_rules = get_available_lint_rules()
+
+    # This removes empty strings from the rules_spec.
+    rules_spec = [rule for rule in rules_spec if rule]
+
+    if not rules_spec:
+        return [rule() for name, rule in lint_rules.items()]
+
+    try:
+        rules = [lint_rules[rule]() for rule in rules_spec]
+    except KeyError:
+        raise InvalidRulesSpec(rules_spec)
+
+    return rules
+
+
+class Linter(object):
+    def __init__(self, vars_, rules_spec):
+        self.vartok = VariableTokenizer(vars_)
+        self.rules_spec = rules_spec
+        self.rules = convert_rules(self.rules_spec)
+
+    def lint_poentry(self, poentry):
+        linted_entry = LintedEntry(poentry)
+
+        for lint_rule in self.rules:
+            lint_rule.lint(self.vartok, linted_entry)
+
+        return linted_entry
+
+    def verify_file(self, filename_or_string):
+        """Verifies strings in file.
+
+        :arg filename_or_string: filename to verify or the contents of
+            a pofile as a string
+
+        :returns: list of LintedEntry objects each with errors and
+            warnings
+
+        :raises IOError: if the file is not a valid .po file or
+            doesn't exist
+        """
+        po = polib.pofile(filename_or_string)
+        return [
+            self.lint_poentry(entry) for entry in po.translated_entries()
+        ]
---
+++ packages/dennis/dennis-0.3.7.tar.gz/dennis-0.3.7/dennis/tools.py
@@ -0,0 +1,160 @@
+try:
+    from blessings import Terminal
+except ImportError:
+    class MockBlessedThing(object):
+        def __call__(self, s):
+            return s
+
+    class Terminal(object):
+        def __getattr__(self, attr, default=None):
+            return MockBlessedThing()
+
+import optparse
+import re
+
+
+class Var(object):
+    """Variable base class"""
+    name = ''
+    desc = ''
+    regexp = ''
+    malformed_regexp = ''
+
+
+class PythonVar(Var):
+    name = 'python'
+    desc = 'Python %(foo)s and {foo} syntax'
+    regexp = (
+        # %s and %(foo)s
+        r'(?:%(?:[(]\S+?[)])?[#0+-]?[\.\d\*]*[hlL]?[diouxXeEfFgGcrs%])'
+        r'|'
+        # {foo}
+        r'(?:\{\S+?\})'
+    )
+    malformed_regexp = (
+        # %(count) with no type at end
+        r'(?:%[\(][^\)\s]+[\)](?:\s|$))'
+        r'|'
+        # %{foo with missing }
+        r'(?:\{[^\}]+(?:\{|$))'
+    )
+
+
+def get_available_vars():
+    return dict(
+        (thing.name, thing)
+        for name, thing in globals().items()
+        if (name.endswith('Var')
+            and issubclass(thing, Var)
+            and thing.name)
+    )
+
+
+class UnknownVar(Exception):
+    pass
+
+
+class VariableTokenizer(object):
+    def __init__(self, vars_=None):
+        """
+        :arg vars_: List of variable formats
+
+            If None, creates a VariableTokenizer that tokenizes on all
+            types of variables. Otherwise just recognizes the listed
+            types.
+
+        """
+        all_vars = get_available_vars()
+
+        if vars_ is None:
+            vars_ = all_vars.keys()
+
+        # Convert names to classes
+        self.vars_ = []
+
+        for v in vars_:
+            try:
+                self.vars_.append(all_vars[v])
+            except KeyError:
+                raise UnknownVar(
+                    '{0} is not a known variable type'.format(v))
+
+        # Generate variable regexp
+        self.vars_re = re.compile(
+            r'(' +
+            '|'.join([vt.regexp for vt in self.vars_]) +
+            r')'
+        )
+
+        # Generate malformed variable regexp
+        self.malformed_vars_re = re.compile(
+            r'(' +
+            '|'.join([vt.malformed_regexp for vt in self.vars_]) +
+            r')'
+        )
+
+
+    def tokenize(self, text):
+        """Breaks s into strings and Python formatting tokens
+
+        This preserves whitespace.
+
+        :arg text: the string to tokenize
+
+        :returns: list of tokens---every even one is a Python formatting
+            token
+
+        """
+        return self.vars_re.split(text)
+
+    def extract_tokens(self, text):
+        """Returns sorted tuple of tokens in the text"""
+        try:
+            tokens = [token for token in self.vars_re.findall(text)]
+            return tuple(sorted(tokens))
+        except TypeError:
+            print 'TYPEERROR', repr(text)
+
+    def is_token(self, text):
+        return self.vars_re.match(text) is not None
+
+
+class BetterArgumentParser(optparse.OptionParser):
+    """OptionParser that allows for additional help sections
+
+    OptionParser can take a description and an epilog, but it
+    textwraps them which destroys all formatting. This allows us to
+    have additional sections after the epilog which can maintain
+    formatting.
+
+    When creating the parser, pass in a ``sections`` kw argument with
+    a list of tuples of the form ``(text, boolean)``. The text is the
+    section text. The boolean indicates whether or not to textwrap the
+    text.
+
+    Example::
+
+        BetterArgumentParser(usage='usage: %prog blah blah', version='1.0',
+            sections=[
+                ('List\nof\nthings', False),  # Maintains format
+                ('List\nof\nthings', True),   # Textwrapped
+            ])
+
+    """
+    def __init__(self, *args, **kw):
+        if 'sections' in kw:
+            self.sections = kw.pop('sections')
+        else:
+            self.sections = []
+        optparse.OptionParser.__init__(self, *args, **kw)
+
+    def format_help(self, formatter=None):
+        help_text = optparse.OptionParser.format_help(self, formatter)
+        for (section, raw) in self.sections:
+            if raw:
+                help_text += section
+            else:
+                help_text += self._format_text(section)
+            help_text += '\n'
+
+        return help_text
