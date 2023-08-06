1ba296ef970d6838187295561ed852b7db2ae16f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:17 +0000

    Add package complexity/0.2.0/complexity-0.2.0.tar.gz

---
+++ packages/complexity/complexity-0.2.0.tar.gz/complexity-0.2.0/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+import complexity
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+readme = open('README.rst', 'rt').read()
+history = open('HISTORY.rst', 'rt').read()
+
+setup(
+    name='complexity',
+    version=complexity.__version__,
+    description='A refreshingly simple static site generator, for those who like to work in HTML.',
+    long_description=readme + '\n\n' + history,
+    author='Audrey Roy',
+    author_email='audreyr@gmail.com',
+    url='https://github.com/audreyr/complexity',
+    packages=[
+        'complexity',
+    ],
+    entry_points={
+        'console_scripts': [
+            'complexity = complexity.complexity:command_line_runner',
+        ]
+    },
+    include_package_data=True,
+    install_requires=[
+        'jinja2',
+    ],
+    license="BSD",
+    zip_safe=False,
+    keywords='complexity,static site generator,HTML,Jinja2,templates,S3',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        "Environment :: Console",
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+    ],
+    test_suite='tests',
+)
