0238ffa478bf5bfc096cc367aefc4a9127502ac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:24 +0000

    Add package django-mbtiles/1.1/django-mbtiles-1.1.zip

---
+++ packages/django-mbtiles/django-mbtiles-1.1.zip/django-mbtiles-1.1/setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+setup(
+    name='django-mbtiles',
+    version='1.1',
+    author='Mathieu Leplatre',
+    author_email='mathieu.leplatre@makina-corpus.com',
+    url='https://github.com/makinacorpus/django-mbtiles',
+    download_url = "http://pypi.python.org/pypi/django-mbtiles/",
+    description="Serve maps from MBTiles files using Django",
+    long_description=open(os.path.join(here, 'README.rst')).read(),
+    install_requires = open(os.path.join(here, 'requirements.txt')).readlines(),
+    packages=find_packages(),
+    classifiers  = ['Topic :: Utilities', 
+                    'Natural Language :: English',
+                    'Operating System :: OS Independent',
+                    'Intended Audience :: Developers',
+                    'Environment :: Web Environment',
+                    'Framework :: Django',
+                    'Development Status :: 5 - Production/Stable',
+                    'Programming Language :: Python :: 2.5'],
+)
