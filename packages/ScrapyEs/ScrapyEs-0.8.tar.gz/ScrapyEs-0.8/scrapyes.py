bb0f6946d843760131b06259dbe9e319894eef62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:12 +0000

    Add package ScrapyEs/0.8/ScrapyEs-0.8.tar.gz

---
+++ packages/ScrapyEs/ScrapyEs-0.8.tar.gz/ScrapyEs-0.8/scrapyes.py
@@ -0,0 +1,35 @@
+from scrapy import signals
+from scrapy.exceptions import NotConfigured
+from pyes import ES
+
+class Sender(object):
+
+    def __init__(self, crawler):
+        uri = "%s:%d" % (crawler.settings['ELASTICSEARCH_SERVER'], crawler.settings['ELASTICSEARCH_PORT'])
+        self.es = ES([uri], bulk_size=crawler.settings['ELASTICSEARCH_BULK_SIZE'])
+        self.index = crawler.settings['ELASTICSEARCH_INDEX']
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        # instantiate the extension object
+        ext = cls(crawler)
+        # connect the extension object to signals
+        crawler.signals.connect(ext.item_scraped, signal=signals.item_scraped)
+        crawler.signals.connect(ext.spider_closed, signal=signals.spider_closed)
+        # return the extension object	
+        return ext
+
+    def item_scraped(self, item, spider):
+        self.store(item, spider)
+
+    def spider_closed(self, spider):
+        self.es.force_bulk()
+
+    def store(self, item, spider):
+        index = spider.index or self.index
+        id_field = crawler.settings.get('ELASTICSEARCH_ITEM_ID_FIELD')
+        if id_field is None:
+            self.es.index(dict(item), index, item.__class__.__name__, bulk=True)
+        else:
+            self.es.index(dict(item),
+                    index, item.__class__.__name__, id=item[id_field], bulk=True)
