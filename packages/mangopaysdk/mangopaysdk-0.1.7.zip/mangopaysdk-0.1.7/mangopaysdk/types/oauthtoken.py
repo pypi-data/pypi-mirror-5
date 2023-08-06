538ae65d09505f836d34f5f9a9ec0a6e1dbc7128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:07 +0000

    Add package mangopaysdk/0.1.7/mangopaysdk-0.1.7.zip

---
+++ packages/mangopaysdk/mangopaysdk-0.1.7.zip/mangopaysdk-0.1.7/mangopaysdk/types/oauthtoken.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+import time
+
+
+class OAuthToken:
+    """Token data for OAuth Authentication."""
+    
+    def __init__(self):
+        # timestamp
+        self.create_time = int(time.time() - 5)
+        self.expires_in = None
+        self.access_token = None
+        self.token_type = None
+
+    def IsExpire(self):
+        """Check that current tokens are expire and return true if yes.
+		return bool
+		"""
+        return time.time() >= (self.create_time + self.expires_in)
