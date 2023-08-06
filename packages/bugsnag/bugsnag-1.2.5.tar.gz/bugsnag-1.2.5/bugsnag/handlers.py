f4186b0b3e79b8c32249c9a7d9c3284732d8e0f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:01 +0000

    Add package bugsnag/1.2.5/bugsnag-1.2.5.tar.gz

---
+++ packages/bugsnag/bugsnag-1.2.5.tar.gz/bugsnag-1.2.5/bugsnag/handlers.py
@@ -0,0 +1,9 @@
+import logging
+import bugsnag
+
+
+class BugsnagHandler(logging.Handler):
+    def emit(self, record):
+        print record.__dict__.keys()
+        if record.exc_info:
+            bugsnag.notify(record.exc_info)
