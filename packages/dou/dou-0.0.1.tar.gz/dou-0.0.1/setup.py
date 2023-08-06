f06e9a039fc37c530bbb7c9feb2a131110007757
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:38 +0000

    Add package dou/0.0.1/dou-0.0.1.tar.gz

---
+++ packages/dou/dou-0.0.1.tar.gz/dou-0.0.1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='dou',
+      version='0.0.1',
+      keywords='dou',
+      description='dou egg',
+      license='MIT License',
+      url='http://liluo.org',
+      author='liluo',
+      author_email='i@liluo.org',
+
+      py_modules=['dou'],
+      include_package_data=True,
+      platforms='any',
+      install_requires=[])
