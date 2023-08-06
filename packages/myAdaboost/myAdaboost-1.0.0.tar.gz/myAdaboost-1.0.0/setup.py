575fbb324f5bc69cf7e557c601d295bdad1369ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:24 +0000

    Add package myAdaboost/1.0.0/myAdaboost-1.0.0.tar.gz

---
+++ packages/myAdaboost/myAdaboost-1.0.0.tar.gz/myAdaboost-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name = 'myAdaboost',
+	version = '1.0.0',
+	py_modules = ['myAdaboost'],
+	author = 'chenliyu',
+	author_email = '2572065090@qq.com',
+	url = 'http://pypi.python.org/ ',
+	description = 'implimention of myAdaboost and ROC in python,and a test of it',
+)
+
