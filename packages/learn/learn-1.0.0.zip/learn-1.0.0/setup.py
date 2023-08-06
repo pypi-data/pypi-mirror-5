00e798caa6c317b84470f8d7ea40e9508317b889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:15 +0000

    Add package learn/1.0.0/learn-1.0.0.zip

---
+++ packages/learn/learn-1.0.0.zip/learn-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name = 'learn',
+		version = '1.0.0',
+		py_modules = ['learn'],
+		author = 'edwn112',
+		author_email = 'mrthapa322@gmail.com',
+		url = 'http://www.edwn112.hpage.com',
+		description = 'A simple printer of nested lines',
+		)
