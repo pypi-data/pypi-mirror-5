ebbf907a1aea9584e37b9daaec69ab8749a56834
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:38 +0000

    Add package globexc/0.1.1/globexc-0.1.1.tar.gz

---
+++ packages/globexc/globexc-0.1.1.tar.gz/globexc-0.1.1/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+"""Dist configuration file for globexc.
+"""
+
+from distutils.core import setup
+
+from globexc import __version__
+
+with open('README.rst') as f:
+	readme = f.read()
+
+readme = readme.replace(
+	'`<extra/trace.dump>`_',
+	'https://github.com/mjem/globexc/blob/master/extra/trace.dump').replace(
+	'  .. code::\n',
+	'')
+
+with open('ChangeLog.rst') as f:
+	history = f.read()
+
+setup(name='globexc',
+	  version=__version__,
+	  description='Write a detailed stack trace if a program crashes',
+	  long_description='{readme}\n{history}'.format(readme=readme, history=history),
+	  author='Mike Elson',
+	  author_email='mike.elson@gmail.com',
+	  url='http://github.com/mjem/globexc',
+	  packages=['globexc'],
+	  license='Apache-2',
+	  classifiers=[
+		'Environment :: Console',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: Apache Software License',
+		'Operating System :: POSIX :: Linux',
+		],
+	  )
