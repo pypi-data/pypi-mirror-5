d23b36cdca7a014e5dc2aae455a70789dbc568ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:58 +0000

    Add package PyHCUP/0.1.0dev/PyHCUP-0.1.0dev.zip

---
+++ packages/PyHCUP/PyHCUP-0.1.0dev.zip/PyHCUP-0.1.0dev/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='PyHCUP',
+    version='0.1.0dev',
+    packages=['pyhcup'],
+    license='',
+    long_description=open('README.txt').read(),
+)
