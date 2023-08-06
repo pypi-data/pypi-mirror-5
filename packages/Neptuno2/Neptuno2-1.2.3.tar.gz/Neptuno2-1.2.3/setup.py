7cc091788baef7b74311e7267bb097cbf94004a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:54 +0000

    Add package Neptuno2/1.2.3/Neptuno2-1.2.3.tar.gz

---
+++ packages/Neptuno2/Neptuno2-1.2.3.tar.gz/Neptuno2-1.2.3/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='Neptuno2',
+      version='1.2.3',
+      author=u'León Domingo',
+      author_email='leon.domingo@ender.es',
+      description=('A little set of utilities that Ender has been using in their web projects'),
+      #license=???,
+      keywords='neptuno ender web',
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Topic :: Utilities',
+      ],
+      url='http://www.ender.es',      
+      packages=['neptuno',
+                'neptuno.postgres',
+                'neptuno.templates',
+      ],
+      package_data={
+            'neptuno': ['default/*.*', 
+                        'default/update/*.*',
+                        'default/update/TODO',
+                        'default/update/new-issue',
+                        'default/update/ejemplo/*.*',                        
+                        ],
+            'neptuno.templates': ['*.xml'],
+      },
+      install_requires=[
+        'psycopg2',
+        'sqlalchemy',
+        'lxml',
+        'xlrd',
+        'xlwt',
+        'simplejson',
+        'jinja2',
+      ],
+     )
