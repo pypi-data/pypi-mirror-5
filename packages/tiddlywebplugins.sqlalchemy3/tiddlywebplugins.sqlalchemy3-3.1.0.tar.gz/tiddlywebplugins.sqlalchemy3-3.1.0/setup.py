6f7e9c2bb018b2fd0d9a23f703373eb35397736d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:52 +0000

    Add package tiddlywebplugins.sqlalchemy3/3.1.0/tiddlywebplugins.sqlalchemy3-3.1.0.tar.gz

---
+++ packages/tiddlywebplugins.sqlalchemy3/tiddlywebplugins.sqlalchemy3-3.1.0.tar.gz/tiddlywebplugins.sqlalchemy3-3.1.0/setup.py
@@ -0,0 +1,32 @@
+AUTHOR = 'Chris Dent'
+AUTHOR_EMAIL = 'cdent@peermore.com'
+NAME = 'tiddlywebplugins.sqlalchemy3'
+DESCRIPTION = 'sqlalchemy store for tiddlyweb'
+VERSION = '3.1.0' # make sure you update in __init__ too
+
+
+import os
+
+from setuptools import setup, find_packages
+
+
+# You should carefully review the below (install_requires especially).
+setup(
+    namespace_packages = ['tiddlywebplugins'],
+    name = NAME,
+    version = VERSION,
+    description = DESCRIPTION,
+    long_description = open(os.path.join(os.path.dirname(__file__), 'README')).read(),
+    author = AUTHOR,
+    author_email = AUTHOR_EMAIL,
+    url = 'http://pypi.python.org/pypi/%s' % NAME,
+    packages = find_packages(exclude=['test']),
+    platforms = 'Posix; MacOS X; Windows',
+    install_requires = ['setuptools',
+        'tiddlyweb>=1.2.0',
+        'sqlalchemy',
+        'pyparsing<2.0.0',
+        ],
+    zip_safe = False,
+    license = 'BSD'
+    )
