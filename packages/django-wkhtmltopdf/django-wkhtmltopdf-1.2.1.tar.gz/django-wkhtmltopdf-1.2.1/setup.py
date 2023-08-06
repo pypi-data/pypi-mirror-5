64578139546b83cbaa657eaafd847df356d39d21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:44 +0000

    Add package django-wkhtmltopdf/1.2.1/django-wkhtmltopdf-1.2.1.tar.gz

---
+++ packages/django-wkhtmltopdf/django-wkhtmltopdf-1.2.1.tar.gz/django-wkhtmltopdf-1.2.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+import wkhtmltopdf
+
+
+setup(
+    name='django-wkhtmltopdf',
+    packages=find_packages(),
+    include_package_data=True,
+    version=wkhtmltopdf.__version__,
+    description='Converts html to PDF using http://code.google.com/p/wkhtmltopdf/.',
+    long_description=open('README.rst').read(),
+    author=wkhtmltopdf.__author__,
+    author_email='admin@incuna.com',
+    url='https://github.com/incuna/django-wkhtmltopdf',
+    install_requires=['Django>=1.3'],
+    zip_safe=False,
+)
+
