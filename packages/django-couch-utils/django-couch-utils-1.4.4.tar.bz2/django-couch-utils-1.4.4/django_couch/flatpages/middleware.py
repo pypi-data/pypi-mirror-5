1f28505f3d04b47f3814a325048f14a1344c6381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:27 +0000

    Add package django-couch-utils/1.4.4/django-couch-utils-1.4.4.tar.bz2

---
+++ packages/django-couch-utils/django-couch-utils-1.4.4.tar.bz2/django-couch-utils-1.4.4/django_couch/flatpages/middleware.py
@@ -0,0 +1,32 @@
+from annoying.decorators import render_to
+from django.http import Http404
+from django.conf import settings
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+class FlatpageFallbackMiddleware(object):
+    def process_response(self, request, response):
+        if response.status_code != 404:
+            return response # No need to check for a flatpage for non-404 responses.
+        try:
+            return flatpage(request)
+        # Return the original response if any errors happened. Because this
+        # is a middleware, we can't assume the errors will be caught elsewhere.
+        except Http404:
+            return response
+        except:
+            if settings.DEBUG:
+                raise
+            return response
+
+def flatpage(request):
+    
+    rows = request.db.view(settings.COUCHDB_FLATPAGES, key=request.path, include_docs=True, limit=1).rows
+    if rows:
+        row = rows[0]
+        
+        
+        return render_to_response(row.doc.get('template', 'static.html'), {'doc': row.doc}, RequestContext(request), mimetype=row.doc.get('content_type', 'text/html'))
+    else:
+        raise Http404('Page not found')
+    
