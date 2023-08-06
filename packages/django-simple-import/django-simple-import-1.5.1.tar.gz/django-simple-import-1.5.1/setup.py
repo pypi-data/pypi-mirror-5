2628cef46a7fafedd3311afd342d836449b96976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:23 +0000

    Add package django-simple-import/1.5.1/django-simple-import-1.5.1.tar.gz

---
+++ packages/django-simple-import/django-simple-import-1.5.1.tar.gz/django-simple-import-1.5.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "django-simple-import",
+    version = "1.5.1",
+    author = "David Burke",
+    author_email = "david@burkesoftware.com",
+    description = ("A Django import tool easy enough your users could use it"),
+    license = "BSD",
+    keywords = "django import",
+    url = "https://github.com/burke-software/django-simple-import",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        "License :: OSI Approved :: BSD License",
+    ],
+    install_requires=['openpyxl', 'odfpy', 'xlrd']
+)
