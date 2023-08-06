bd92eae05854a45ac710eb890fb401e89e787386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:20 +0000

    Add package collective.pdfexport/1.0a6/collective.pdfexport-1.0a6.zip

---
+++ packages/collective.pdfexport/collective.pdfexport-1.0a6.zip/collective.pdfexport-1.0a6/collective/pdfexport/interfaces.py
@@ -0,0 +1,16 @@
+from zope.interface import Interface
+
+class IProductSpecific(Interface):
+    pass
+
+class IPDFExportCapable(Interface):
+    pass    
+
+class IPDFConverter(Interface):
+    pass
+
+class IPDFHTMLProvider(Interface):
+
+    def pdf_html(view):
+        """ get pdf html of current content """
+
