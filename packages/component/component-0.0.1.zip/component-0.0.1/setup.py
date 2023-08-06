3dce97239e264728195cef6112cae576a47a1c32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:49 +0000

    Add package component/0.0.1/component-0.0.1.zip

---
+++ packages/component/component-0.0.1.zip/component-0.0.1/setup.py
@@ -0,0 +1,24 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='component',
+    version='0.0.1',
+    license='MIT',
+    author='Daniel Chatfield',
+    author_email='chatfielddaniel@gmail.com',
+    url='http://github.com/import/component',
+    py_modules=['component'],
+    description='A python library that makes is easy to consume bower '
+                'components with python.',
+    zip_safe=False,
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+)
