274cd07fd958f21c722168844e87a27227fd2a98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:18 +0000

    Add package rebecca.repository/0.1.1/rebecca.repository-0.1.1.zip

---
+++ packages/rebecca.repository/rebecca.repository-0.1.1.zip/rebecca.repository-0.1.1/setup.py
@@ -0,0 +1,60 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1.1'
+
+requires = [
+    "setuptools",
+    "pyramid",
+    "sqlalchemy",
+    "six",
+]
+
+tests_require = [
+    "pytest",
+    "pytest-cov",
+    "coverage",
+    "testfixtures",
+]
+
+
+long_description = (
+    open('README.rst').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+setup(name='rebecca.repository',
+      version=version,
+      description="helper utility for repository pattern of PofEAA",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Atsushi Odagiri',
+      author_email='aodagx@gmail.com',
+      url='https://github.com/rebeccaframework/rebecca.repository',
+      license='MIT',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['rebecca'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=tests_require,
+      extras_require={
+          "testing": tests_require,
+          "dev": ["docutils"],
+      },
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
