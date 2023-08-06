fad085e398caf5b532f8e34e2976a78e644a1ef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:31 +0000

    Add package Ibe/0.1.3/Ibe-0.1.3.tar.gz

---
+++ packages/Ibe/Ibe-0.1.3.tar.gz/Ibe-0.1.3/README.txt
@@ -0,0 +1,4 @@
+IBE
+===
+
+Simple IBE library - This might be the start of something really neat!
---
+++ packages/Ibe/Ibe-0.1.3.tar.gz/Ibe-0.1.3/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='Ibe',
+    version=open('CHANGES.txt').read().split()[0],
+    author='Lucas Pandolfo Perin / Lucas Boppre Niehues',
+    author_email='lucasperin@gmail.com / lucasboppre@gmail.com',
+    packages=['ibe'],
+    url='http://pypi.python.org/pypi/Ibe/',
+    license='LICENSE.txt',
+    description='Simple IBE library - This might be the start of something really neat!',
+    long_description=open('README.txt').read(),
+
+    scripts=['bin/pkg_server.py','bin/install_pypbc.sh'],
+    install_requires=[
+        "crypto",
+    ],
+
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Topic :: Security :: Cryptography",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
