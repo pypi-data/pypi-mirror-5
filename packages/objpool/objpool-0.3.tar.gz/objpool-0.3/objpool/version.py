aa3f2a5b30a24ce09c8e9ccfcab9bf21eeb8312a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:01 +0000

    Add package objpool/0.3/objpool-0.3.tar.gz

---
+++ packages/objpool/objpool-0.3.tar.gz/objpool-0.3/objpool/version.py
@@ -0,0 +1,8 @@
+
+__version__ = "0.3"
+__version_vcs_info__ = {
+    'branch': 'master',
+    'revid': 'a9dd3d0',
+    'revno': 46}
+__version_user_email__ = "cstavr@grnet.gr"
+__version_user_name__ = "Christos Stavrakakis"
