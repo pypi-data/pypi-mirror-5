19aa0c69aebb998eeca2590cafe9f14bf614cd64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:06 +0000

    Add package VTIXyProxy/0.1.0/VTIXyProxy-0.1.0.tar.gz

---
+++ packages/VTIXyProxy/VTIXyProxy-0.1.0.tar.gz/VTIXyProxy-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='VTIXyProxy',
+    version='0.1.0',
+    packages=['vtixy_proxy',],
+    license='LICENSE.txt',
+    description='Web services for applications of VTIX.RU clients',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "django-rest-framework-proxy >= 1.1.0",
+    ],
+)
