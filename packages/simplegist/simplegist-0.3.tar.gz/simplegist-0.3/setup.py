1fb2670038e7339df6f8b865dcb929a2e5ebbd02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:14 +0000

    Add package simplegist/0.3/simplegist-0.3.tar.gz

---
+++ packages/simplegist/simplegist-0.3.tar.gz/simplegist-0.3/setup.py
@@ -0,0 +1,30 @@
+
+import sys
+from setuptools import setup
+
+required = ['requests']
+
+
+if sys.version_info[:2] < (2,6):
+    required.append('simplejson')
+
+setup(
+    name = 'simplegist',
+    packages = ['simplegist'], 
+    version = '0.3',
+    install_requires=required,
+    description = 'Python wrapper for Gist ',
+    long_description=open('README.rst').read(),
+    author = 'Varun Malhotra',
+    author_email = 'varun2902@gmail.com',
+    url = 'https://github.com/softvar/gist', 
+    download_url = 'https://github.com/softvar/GistApi-Wrapper-python/tarball/0.3',
+    keywords = ['gist', 'github', 'API'],
+    license = 'MIT', 
+    classifiers = (
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        ),
+)
