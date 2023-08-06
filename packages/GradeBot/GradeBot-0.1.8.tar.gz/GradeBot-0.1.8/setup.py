e3e8c5bd62513adca43b35291cfeae274b186a41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:27 +0000

    Add package GradeBot/0.1.8/GradeBot-0.1.8.tar.gz

---
+++ packages/GradeBot/GradeBot-0.1.8.tar.gz/GradeBot-0.1.8/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    description='College GPA Calculator',
+    author='Alex Kang',
+    url='https://pypi.python.org/pypi?:action=display&name=GradeBot',
+    download_url='https://pypi.python.org/pypi?:action=display&name=GradeBot',
+    license='GPL',
+    author_email='akang95@gmail.com',
+    version='0.1.8',
+    install_requires=['nose'],
+    packages=['gradebot', 'gradebot.test'],
+    scripts=['bin/gradebot'],
+    name='GradeBot'
+)
