785014367dd2bed678784ab988f8b6f3919c0769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:26 +0000

    Add package imread/0.3.0/imread-0.3.0.tar.gz

---
+++ packages/imread/imread-0.3.0.tar.gz/imread-0.3.0/imread/lib/_png.cpp
@@ -0,0 +1,135 @@
+// Copyright 2012 Luis Pedro Coelho <luis@luispedro.org>
+// License: MIT (see COPYING.MIT file)
+
+#include "base.h"
+#include "_png.h"
+#include "tools.h"
+
+#include <png.h>
+
+#include <cstring>
+#include <vector>
+#include <sstream>
+
+namespace {
+
+void throw_error(png_structp png_ptr, png_const_charp error_msg) {
+    throw CannotReadError(error_msg);
+}
+
+class png_holder {
+    public:
+        png_holder(int m)
+            :png_ptr((m == write_mode ? png_create_write_struct : png_create_read_struct)(PNG_LIBPNG_VER_STRING, 0, throw_error, 0))
+            ,png_info(0)
+            ,mode(holder_mode(m))
+            { }
+        ~png_holder() {
+            png_infopp pp = (png_info ? &png_info : 0);
+            if (mode == read_mode) png_destroy_read_struct(&png_ptr, pp, 0);
+            else png_destroy_write_struct(&png_ptr, pp);
+        }
+        void create_info() {
+            png_info = png_create_info_struct(png_ptr);
+            if (!png_info) throw "error";
+        }
+
+        png_structp png_ptr;
+        png_infop png_info;
+        enum holder_mode { read_mode, write_mode } mode;
+};
+
+void read_from_source(png_structp png_ptr, png_byte* buffer, size_t n) {
+    byte_source* s = static_cast<byte_source*>(png_get_io_ptr(png_ptr));
+    const size_t actual = s->read(reinterpret_cast<byte*>(buffer), n);
+    if (actual != n) {
+        throw CannotReadError();
+    }
+}
+
+void write_to_source(png_structp png_ptr, png_byte* buffer, size_t n) {
+    byte_sink* s = static_cast<byte_sink*>(png_get_io_ptr(png_ptr));
+    const size_t actual = s->write(reinterpret_cast<byte*>(buffer), n);
+    if (actual != n) {
+        throw CannotReadError();
+    }
+}
+void flush_source(png_structp png_ptr) {
+    byte_sink* s = static_cast<byte_sink*>(png_get_io_ptr(png_ptr));
+    s->flush();
+}
+
+int color_type_of(Image* im) {
+    if (im->nbits() != 8) throw CannotWriteError("Image must be 8 bits for PNG saving");
+    if (im->ndims() == 2) return PNG_COLOR_TYPE_GRAY;
+    if (im->ndims() != 3) throw CannotWriteError("Image must be either 2 or 3 dimensional");
+    if (im->dim(2) == 3) return PNG_COLOR_TYPE_RGB;
+    if (im->dim(2) == 4) return PNG_COLOR_TYPE_RGBA;
+    throw CannotWriteError();
+}
+}
+std::auto_ptr<Image> PNGFormat::read(byte_source* src, ImageFactory* factory) {
+    png_holder p(png_holder::read_mode);
+    png_set_read_fn(p.png_ptr, src, read_from_source);
+    p.create_info();
+    png_read_info(p.png_ptr, p.png_info);
+    
+    const int w = png_get_image_width (p.png_ptr, p.png_info);
+    const int h = png_get_image_height(p.png_ptr, p.png_info);
+    int bit_depth = png_get_bit_depth(p.png_ptr, p.png_info);
+    if (bit_depth != 1 && bit_depth != 8 && bit_depth != 16) {
+        std::ostringstream out;
+        out << "imread.png: Cannot read this bit depth ("
+                << bit_depth
+                << "). Only bit depths ∈ {1,8,16} are supported.";
+        throw CannotReadError(out.str());
+    }
+    int d = -1;
+    switch (png_get_color_type(p.png_ptr, p.png_info)) {
+        case PNG_COLOR_TYPE_PALETTE:
+            png_set_palette_to_rgb(p.png_ptr);
+        case PNG_COLOR_TYPE_RGB:
+            d = 3;
+            break;
+        case PNG_COLOR_TYPE_RGB_ALPHA:
+            d = 4;
+            break;
+        case PNG_COLOR_TYPE_GRAY:
+            d = -1;
+            break;
+        default: {
+            std::ostringstream out;
+            out << "imread.png: Color type ("
+                << int(png_get_color_type(p.png_ptr, p.png_info))
+                << ") cannot be handled";
+            throw CannotReadError(out.str());
+        }
+    }
+
+    std::auto_ptr<Image> output(factory->create(bit_depth, h, w, d));
+    std::vector<png_bytep> rowps = allrows<png_byte>(*output);
+    png_read_image(p.png_ptr, &rowps[0]);
+
+    return output;
+}
+
+void PNGFormat::write(Image* input, byte_sink* output) {
+    png_holder p(png_holder::write_mode);
+    p.create_info();
+    png_set_write_fn(p.png_ptr, output, write_to_source, flush_source);
+    const int height = input->dim(0);
+    const int width = input->dim(1);
+    const int bit_depth = 8;
+    const int color_type = color_type_of(input);
+
+    png_set_IHDR(p.png_ptr, p.png_info, width, height,
+                     bit_depth, color_type, PNG_INTERLACE_NONE,
+                     PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
+    png_write_info(p.png_ptr, p.png_info);
+
+    std::vector<png_bytep> rowps = allrows<png_byte>(*input);
+    png_write_image(p.png_ptr, &rowps[0]);
+    png_write_end(p.png_ptr, p.png_info);
+}
+
+
---
+++ packages/imread/imread-0.3.0.tar.gz/imread-0.3.0/imread/tests/test_png.py
@@ -0,0 +1,68 @@
+from nose.tools import with_setup, raises
+import numpy as np
+from imread import imread, imsave
+import numpy as np
+
+_filename = 'imread_testing_file.png'
+
+def _remove_file():
+    from os import unlink
+    try:
+        unlink(_filename)
+    except:
+        pass
+
+@with_setup(teardown=_remove_file)
+def test_png_raw():
+    simple = np.arange(16*16).reshape((16,16))
+    simple = simple.astype(np.uint8)
+    imsave(_filename, simple, 'png')
+    back = imread(_filename)
+    assert np.all(simple == back)
+
+@with_setup(teardown=_remove_file)
+def test_asym():
+    simple = np.arange(16*16).reshape((32,8))
+    simple = simple.astype(np.uint8)
+    imsave(_filename, simple, 'png')
+    back = imread(_filename)
+    assert np.all(simple == back)
+
+@with_setup(teardown=_remove_file)
+def test_random():
+    np.random.seed(23)
+    for i in range(8):
+        simple = np.random.random_sample((64,64,3))
+        simple *= 255
+        simple = simple.astype(np.uint8)
+        if i < 3:
+            simple[:,:,i] = 0
+        imsave(_filename, simple, 'png')
+        back = imread(_filename)
+        assert np.all(simple == back)
+
+
+@with_setup(teardown=_remove_file)
+def test_non_carray():
+    np.random.seed(87)
+    simple = np.random.random_sample((128,128,3))
+    simple *= 255
+    simple = simple.astype(np.uint8)
+    simple = simple[32:64,32::2]
+    imsave(_filename, simple, 'png')
+    back = imread(_filename)
+    assert np.all(simple == back)
+
+
+def test_binary():
+    f = imread('imread/tests/data/bit1.png')
+    assert f.dtype == np.bool_
+
+@raises(RuntimeError)
+def test_error():
+    imread('imread/tests/data/error.png')
+
+def test_regression():
+    im = imread('imread/tests/data/palette_zero.png')
+    assert im.sum() == 0
+    assert im.shape == (128, 64, 3)
