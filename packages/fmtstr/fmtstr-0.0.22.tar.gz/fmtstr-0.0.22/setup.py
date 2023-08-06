9bc1d7660d1d3d3e8b9969ca500372610b2e15de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:15 +0000

    Add package fmtstr/0.0.22/fmtstr-0.0.22.tar.gz

---
+++ packages/fmtstr/fmtstr-0.0.22.tar.gz/fmtstr-0.0.22/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='fmtstr',
+      version='0.0.22',
+      description='string-like objects marked up with terminal formatting',
+      url='https://github.com/thomasballinger/fmtstr',
+      author='Thomas Ballinger',
+      author_email='thomasballinger@gmail.com',
+      license='MIT',
+      packages=['fmtstr'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          ],
+      zip_safe=False)
