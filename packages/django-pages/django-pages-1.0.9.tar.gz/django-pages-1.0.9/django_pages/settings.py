8f8c8ff90925460e1d105f97eff60b71a7fd77f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:41 +0000

    Add package django-pages/1.0.9/django-pages-1.0.9.tar.gz

---
+++ packages/django-pages/django-pages-1.0.9.tar.gz/django-pages-1.0.9/django_pages/settings.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+
+from django.conf import settings
+
+
+try:
+    ADMIN_MEDIA_PREFIX = settings.ADMIN_MEDIA_PREFIX
+
+except AttributeError:
+    ADMIN_MEDIA_PREFIX = '/static/admin/'
+
+# where to upload flag images /media/FLAG_UPLOAD_DIR
+try:
+    FLAG_UPLOAD_DIR = settings.FLAG_UPLOAD_DIR
+
+except AttributeError:
+    FLAG_UPLOAD_DIR = settings.MEDIA_ROOT
+
+try:
+    POSTS_ON_PAGE = settings.POSTS_ON_PAGE
+
+except AttributeError:
+    POSTS_ON_PAGE = 10
