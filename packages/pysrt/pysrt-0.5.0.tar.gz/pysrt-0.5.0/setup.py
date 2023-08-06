09941e507393de4d71b1df01ce7f0d8b345cc19d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:11 +0000

    Add package pysrt/0.5.0/pysrt-0.5.0.tar.gz

---
+++ packages/pysrt/pysrt-0.5.0.tar.gz/pysrt-0.5.0/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import sys
+from setuptools import setup, find_packages
+
+
+README = ''
+try:
+    f = open('README.rst')
+    README = f.read()
+    f.close()
+except:
+    pass
+
+REQUIRES = []
+if sys.version_info < (2, 6):
+    REQUIRES.append('chardet')
+else:
+    REQUIRES.append('charade')
+
+if sys.version_info < (2, 7):
+    REQUIRES.append('argparse')
+
+setup(name='pysrt',
+      version='0.5.0',
+      author='Jean Boussier',
+      author_email='jean.boussier@gmail.com',
+      packages=find_packages(),
+      description = "SubRip (.srt) subtitle parser and writer",
+      long_description=README,
+      install_requires=REQUIRES,
+      entry_points={'console_scripts': ['srt = pysrt.commands:main']},
+      license="GPLv3",
+      platforms=["Independent"],
+      keywords="SubRip srt subtitle",
+      url="https://github.com/byroot/pysrt",
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Topic :: Multimedia :: Video",
+          "Topic :: Software Development :: Libraries",
+          "Topic :: Text Processing :: Markup",
+      ]
+)
