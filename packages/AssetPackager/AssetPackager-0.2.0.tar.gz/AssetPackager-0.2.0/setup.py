f67095c9b4dafe628361e2720f5c82720088b034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:39 +0000

    Add package AssetPackager/0.2.0/AssetPackager-0.2.0.tar.gz

---
+++ packages/AssetPackager/AssetPackager-0.2.0.tar.gz/AssetPackager-0.2.0/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+    name = 'AssetPackager',
+    packages = ['AssetPackager'], # this must be the same as the name above
+    version = '0.2.0',
+    description = 'Utility for determining and making CSS/JS packages from list of paths',
+    author = 'Jason Bonta',
+    author_email = 'jbonta@gmail.com',
+    url = 'https://github.com/jbonta/AssetPackager',   # use the URL to the github repo
+    download_url = 'https://github.com/jbonta/AssetPackager/tarball/0.3', # I'll explain this in a second
+    keywords = ['CSS', 'JS', 'static', 'analysis', 'dependency', 'resources'], # arbitrary keywords
+    classifiers = [],
+    install_requires=[
+      'yuicompressor >= 2.4.7',
+    ],
+)
