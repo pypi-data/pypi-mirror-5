020ef0110c10e61f5b5f6fa98595fccff1d38386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:48 +0000

    Add package windUtils/1.0.0/windUtils-1.0.0.zip

---
+++ packages/windUtils/windUtils-1.0.0.zip/windUtils-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name='windUtils',
+	version='1.0.0',
+	py_modules=['windUtils'],
+	author='Mstr838Wind',
+	author_email='Mstr838Wind@gmail.com',
+	url='https://pypi.python.org/id/Mstr838Wind',
+	description='a simple module for test.'
+)
---
+++ packages/windUtils/windUtils-1.0.0.zip/windUtils-1.0.0/windUtils.py
@@ -0,0 +1,11 @@
+"""
+It's wind utils.
+just for test.
+"""
+
+def printArgs(args):
+    print('------print args----')
+    print(args)
+
+
+#printArgs('wind utils,gogogo!')
