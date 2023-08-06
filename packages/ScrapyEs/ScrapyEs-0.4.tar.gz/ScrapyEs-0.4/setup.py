66bdd2731418fedc3aa0d7eeab60ebafc23a3466
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:15 +0000

    Add package ScrapyEs/0.4/ScrapyEs-0.4.tar.gz

---
+++ packages/ScrapyEs/ScrapyEs-0.4.tar.gz/ScrapyEs-0.4/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(name='ScrapyEs',
+      version='0.4',
+      license='Apache License, Version 2.0',
+      description='An extension module to send data to elasticsearch in bulk format.',
+      author='Ernesto Miguez',
+      author_email='ernesto.miguez@asquera.de',
+      url='https://github.com/Asquera/scrapy-elasticsearch-extension',
+      keywords="scrapy elastic search",
+      py_modules=['scrapyes'],
+      platforms = ['Any'],
+      install_requires = ['scrapy', 'pyes'],
+      classifiers = [ 'Development Status :: 4 - Beta',
+                      'Environment :: No Input/Output (Daemon)',
+                      'License :: OSI Approved :: BSD License',
+                      'Operating System :: OS Independent',
+                      'Programming Language :: Python']
+)
