001e75389be8b26317c7891d852af64603266a0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:40 +0000

    Add package pwr/2.2/pwr-2.2.tar.gz

---
+++ packages/pwr/pwr-2.2.tar.gz/pwr-2.2/pwr/__init__.py
@@ -0,0 +1,11 @@
+__author__ = "Filipp Frizzy"
+__copyright__ = ""
+__credits__ = ["Filipp Frizzy"]
+__license__ = "BSD3"
+__version__ = "2.2"
+__maintainer__ = "Filipp Frizzy"
+__email__ = "filipp.s.frizzy@gmail.com"
+__status__ = "Development"
+__description__ = "This program helps you write html pages like documents in word processors"
+__keywords__ = "python wysiwyg html documents qt webkit"
+__url__ = "https://github.com/Friz-zy/WYSIWYG"
