5d0fedddc39995b2b8bedcbdfa8930028b2f47b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:10 +0000

    Add package brewer2mpl/1.3.2/brewer2mpl-1.3.2.zip

---
+++ packages/brewer2mpl/brewer2mpl-1.3.2.zip/brewer2mpl-1.3.2/brewer2mpl/__init__.py
@@ -0,0 +1,9 @@
+"""
+brewer2mpl is a pure Python package for accessing
+colorbrewer2.org color maps from Python. With brewer2mpl you can get the
+raw RGB colors of all 165 colorbrewer2.org color maps.
+
+"""
+from .brewer2mpl import *
+
+__version__ = '1.4.dev'
