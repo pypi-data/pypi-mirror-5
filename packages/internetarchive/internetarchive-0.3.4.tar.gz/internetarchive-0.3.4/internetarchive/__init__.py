3bcdfdc3c18820563e8b7580b8d38b344a6e6a56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:03 +0000

    Add package internetarchive/0.3.4/internetarchive-0.3.4.tar.gz

---
+++ packages/internetarchive/internetarchive-0.3.4.tar.gz/internetarchive-0.3.4/internetarchive/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Internetarchive Library
+~~~~~~~~~~~~~~~~~~~~~~~
+
+Internetarchive is a python interface to archive.org.
+usage:
+
+    >>> import internetarchive
+    >>> item = internetarchive.Item('govlawgacode20071')
+    >>> item.exists
+    True
+
+:copyright: (c) 2013 by Jacob M. Johnson.
+:license: GPL, see LICENSE for more details.
+
+"""
+
+__title__ = 'internetarchive'
+__version__ = '0.3.4'
+__author__ = 'Jacob M. Johnson'
+__license__ = 'GPL'
+__copyright__ = 'Copyright 2013 Jacob M. Johnson'
+
+from .internetarchive import Item, File, Catalog, Search, Mine
