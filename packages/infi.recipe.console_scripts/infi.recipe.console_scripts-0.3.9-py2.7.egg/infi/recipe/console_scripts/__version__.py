c3e33212b835386b2cf22d0c2152eb32d2138823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:34 +0000

    Add package infi.recipe.console_scripts/0.3.9/infi.recipe.console_scripts-0.3.9-py2.7.egg

---
+++ packages/infi.recipe.console_scripts/infi.recipe.console_scripts-0.3.9-py2.7.egg/infi/recipe/console_scripts/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.3.9"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '6919d9a3d767c2e374b0ee1fc16334a0b3b05c74'
+__git_head_subject__ = 'fix minimal-packages'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
