97c972792209c56fefc14590d806e3410d2f3b25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:52 +0000

    Add package flask-bundle-system/0.1/flask-bundle-system-0.1.tar.gz

---
+++ packages/flask-bundle-system/flask-bundle-system-0.1.tar.gz/flask-bundle-system-0.1/flask_bundlesystem/bundle_system.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+"""
+    flaskext.bundle_system
+    ~~~~~~~~~~~~~~~~~~~~~~
+
+    Blueprints function in class, for programming with
+    Blueprints as Bundles.
+
+    :copyright: (c) 2013 by yograterol.
+    :license: BSD, see LICENSE for more details.
+"""
+from os.path import join as joinpath
+from os.path import (isdir, isfile, splitext)
+from os import listdir
+from imp import (find_module, load_module)
+
+
+class IsNotPathBundle(Exception):
+
+    def __repr__(self):
+        print "The namespace_bundle value is not a valid path."
+
+
+class BundleSystem(object):
+
+    __slots__ = ['app', 'namespace_bundle', 'bundle_var_name']
+
+    def __init__(self, app, namespace_bundle=None, bundle_var_name='bundle'):
+        self.app = app
+        self.namespace_bundle = namespace_bundle
+        self.bundle_var_name = bundle_var_name
+        if isdir(namespace_bundle):
+            self.namespace_bundle = namespace_bundle
+        else:
+            raise IsNotPathBundle()
+        self.load_module()
+
+    def register_bundle(self, bundles_dict):
+        """
+        Get the variables bundle and register in the Flask App.
+        :params:
+            bundles_dict - Dictionary with all bundles (Blueprints)
+        """
+        for bundle_name, bundle in bundles_dict.iteritems():
+            try:
+                # Try get bundle variable and bundle_config variable
+                blueprint = getattr(bundle, self.bundle_var_name, None)
+                kwargs = getattr(bundle, self.bundle_var_name + '_config',
+                                 dict())
+                if blueprint:
+                    # Register the blueprint
+                    self.app.register_blueprint(blueprint, **kwargs)
+            except:
+                continue
+
+    def load_module(self, folder=None):
+        """
+        Method recursive
+        Find all files in the current project path, and try import the bundle
+        name.
+        :params:
+            folder - The absolute path of the folder to find.
+        """
+        if not folder:
+            folder = self.namespace_bundle
+
+        bundles_list_dir = listdir(folder)
+        bundles = dict()
+
+        for bundle_name in bundles_list_dir:
+            # Join the path folder and bundle name file for check if is folder
+            # or file.
+            abs_path = joinpath(folder, bundle_name)
+            if isdir(abs_path):
+                self.load_module(abs_path)
+
+            if isfile(abs_path):
+                try:
+                    # Split the bundle name, for only check python files.
+                    name, extension = splitext(bundle_name)
+                    if extension == '.py':
+                        f, filename, descr = find_module(name, [folder, ])
+                        bundles[bundle_name] = load_module(name, f,
+                                                           filename, descr)
+                except ImportError:
+                    continue
+        # After of check all bundles name in the current "folder", passing
+        # bundles dict as parameter to method register_bundle.
+        self.register_bundle(bundles)
---
+++ packages/flask-bundle-system/flask-bundle-system-0.1.tar.gz/flask-bundle-system-0.1/setup.py
@@ -0,0 +1,43 @@
+"""
+flask-bundle-system
+-------------------
+
+
+
+Links
+`````
+
+* `documentation <http://packages.python.org/flask-bundle-system>`_
+* `development version
+  <http://github.com/yograterol/flask-bundle-system/zipball/master>`_
+
+"""
+from setuptools import setup
+
+
+setup(
+    name='flask-bundle-system',
+    version='0.1',
+    url='https://github.com/yograterol/flask-bundle-system',
+    license='BSD',
+    author='yograterol',
+    author_email='yograterol@fedoraproject.org',
+    description='Flask extension for work with blueprints as bundles',
+    long_description=__doc__,
+    packages=['flask_bundlesystem'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'Flask'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
