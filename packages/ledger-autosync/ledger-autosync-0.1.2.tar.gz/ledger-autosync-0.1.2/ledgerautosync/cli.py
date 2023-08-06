edba868137800fd8477b324aefe7bb4fc592f97c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:37 +0000

    Add package ledger-autosync/0.1.2/ledger-autosync-0.1.2.tar.gz

---
+++ packages/ledger-autosync/ledger-autosync-0.1.2.tar.gz/ledger-autosync-0.1.2/ledgerautosync/cli.py
@@ -0,0 +1,56 @@
+import datetime
+import time
+from ofxclient.config import OfxConfig
+from ofxparse import OfxParser
+import argparse
+from ofxclient.client import Client
+from formatter import Formatter
+from ledgerautosync.sync import Synchronizer
+from ledgerautosync.ledger import mk_ledger
+import logging
+import sys
+
+def sync(ledger, config, max_days=90, resync=False):
+    sync = Synchronizer(ledger)
+    for acct in config.accounts():
+        (ofx, txns) = sync.get_new_txns(acct, resync=resync, max_days=max_days)
+        formatter = Formatter(account=ofx.account, name=acct.description, ledger=ledger)
+        for txn in txns:
+            print formatter.format_txn(txn)
+
+def import_ofx(ledger, path, accountname=None):
+    sync = Synchronizer(ledger)
+    (ofx, txns) = sync.parse_file(path)
+    if accountname is None:
+        accountname = "%s:%s"%(ofx.account.institution.organization, ofx.account.account_id)
+    formatter = Formatter(account=ofx.account, name=accountname, ledger=ledger)
+    for txn in txns:
+        print formatter.format_txn(txn)
+        
+def run(args=None):
+    if args is None: args = sys.argv[1:]
+
+    parser = argparse.ArgumentParser(description='Synchronize ledger.')
+    parser.add_argument('-m', '--max', type=int, default=90,
+                        help='maximum number of days to process')
+    parser.add_argument('-r', '--resync', action='store_true', default=False,
+                        help='do not stop until max days reached')
+    parser.add_argument('PATH', nargs='?', help='do not sync; import from OFX file')
+    parser.add_argument('-a', '--account', type=str, default=None,
+                        help='set account name for import')
+    parser.add_argument('-l', '--ledger', type=str, default=None,
+                        help='specify ledger file to READ for syncing')
+    parser.add_argument('-d', '--debug', action='store_true', default=False,
+                        help='enable debug logging')
+    args = parser.parse_args(args)
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+    ledger = mk_ledger(args.ledger)
+    if args.PATH is None:
+        config = OfxConfig()
+        sync(ledger, config, max_days=args.max, resync=args.resync)
+    else:
+        import_ofx(ledger, args.PATH, args.account)
+
+if __name__ == '__main__':
+    run()
---
+++ packages/ledger-autosync/ledger-autosync-0.1.2.tar.gz/ledger-autosync-0.1.2/ledgerautosync/formatter.py
@@ -0,0 +1,76 @@
+from decimal import Decimal
+from ofxparse.ofxparse import Transaction, InvestmentTransaction
+
+def clean_ofx_id(ofxid):
+    ofxid = ofxid.replace('/', '_')
+    ofxid = ofxid.replace('$', '_')
+    ofxid = ofxid.replace(' ', '_')
+    return ofxid
+
+class Formatter(object):
+    def __init__(self, account, name, ledger=None):
+        self.acctid=account.account_id
+        self.currency=account.statement.currency
+        self.fid=account.institution.fid
+        self.name = name
+        self.ledger = ledger
+
+    def mk_dynamic_account(self, txn, exclude):
+        if self.ledger is None:
+            return "Expenses:Misc"
+        else:
+            payee = self.format_payee(txn)
+            account = self.ledger.get_account_by_payee(payee, exclude)
+            if account is None:
+                return "Expenses:Misc"
+            else:
+                return account
+
+    def format_amount(self, amount, reverse=False, unlimited=False):
+        currency = self.currency.upper()
+        if currency == "USD": currency = "$"
+        if unlimited:
+            amt = str(abs(amount))
+        else:
+            amt = "%0.2f"%(abs(amount))
+        if amount.is_signed() != reverse:
+            return "-%s%s"%(currency, amt)
+        else:
+            return "%s%s"%(currency, amt)
+
+    def format_payee(self, txn):
+        if (txn.payee is None) or txn.memo.startswith(txn.payee):
+            return txn.memo
+        elif (txn.memo is None) or txn.payee.startswith(txn.memo):
+            return txn.payee
+        else:
+            return "%s %s"%(txn.payee, txn.memo)
+
+    def format_txn_line(self, acct, amt, extra=None):
+        space_count = 50 - len(acct) - len(amt)
+        if extra:
+            extra_str = " %s"%(extra)
+        else:
+            extra_str = ""
+        return "  %s%s%s%s\n"%(acct, (" " * space_count), amt, extra_str)
+
+    def format_txn(self, txn):
+        retval = ""
+        ofxid = clean_ofx_id("%s.%s.%s"%(self.fid, self.acctid, txn.id))
+        if isinstance(txn, Transaction):
+            date = "%s"%(txn.date.strftime("%Y/%m/%d"))
+            retval += "%s %s\n"%(date, self.format_payee(txn))
+            retval += "  ; ofxid: %s\n"%(ofxid)
+            retval += self.format_txn_line(self.name, self.format_amount(txn.amount))
+            retval += self.format_txn_line(self.mk_dynamic_account(txn, exclude=self.name), self.format_amount(txn.amount, reverse=True))
+        elif isinstance(txn, InvestmentTransaction):
+            trade_date = "%s"%(txn.tradeDate.strftime("%Y/%m/%d"))
+            if txn.settleDate is not None:
+                retval = "%s=%s %s\n"%(txn.tradeDate.strftime("%Y/%m/%d"), txn.settleDate.strftime("%Y/%m/%d"), txn.memo)
+            else:
+                retval = "%s %s\n"%(txn.tradeDate.strftime("%Y/%m/%d"), txn.memo)
+            retval += "  ; ofxid: %s\n"%(ofxid)
+            retval += self.format_txn_line(self.name, str(txn.units), 
+                                           "%s @ %s"%(txn.security, self.format_amount(txn.unit_price, unlimited=True)))
+            retval += self.format_txn_line(self.name, self.format_amount(txn.units * txn.unit_price, reverse=True))
+        return retval
