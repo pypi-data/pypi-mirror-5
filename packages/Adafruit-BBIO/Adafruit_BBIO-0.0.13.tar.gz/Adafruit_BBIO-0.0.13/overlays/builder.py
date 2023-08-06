9d98a2b7e609a5bc717b1ca775d6a7728b5e4e48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:15 +0000

    Add package Adafruit-BBIO/0.0.13/Adafruit_BBIO-0.0.13.tar.gz

---
+++ packages/Adafruit-BBIO/Adafruit_BBIO-0.0.13.tar.gz/Adafruit_BBIO-0.0.13/overlays/builder.py
@@ -0,0 +1,14 @@
+from subprocess import call
+import os
+import shutil
+import glob
+
+def compile():
+    call(["dtc", "-O", "dtb", "-o", "overlays/ADAFRUIT-SPI0-00A0.dtbo", "-b", "o", "-@", "overlays/ADAFRUIT-SPI0-00A0.dts"])
+    call(["dtc", "-O", "dtb", "-o", "overlays/ADAFRUIT-SPI1-00A0.dtbo", "-b", "o", "-@", "overlays/ADAFRUIT-SPI1-00A0.dts"])
+
+def copy():
+    for fl in glob.glob("/lib/firmware/ADAFRUIT-SPI*-00A0.dtbo"):
+        os.remove(fl)
+    shutil.move("overlays/ADAFRUIT-SPI0-00A0.dtbo", "/lib/firmware/ADAFRUIT-SPI0-00A0.dtbo")
+    shutil.move("overlays/ADAFRUIT-SPI1-00A0.dtbo", "/lib/firmware/ADAFRUIT-SPI1-00A0.dtbo")
