c0fd843713ecac11529af3bec8cf054426d0e89e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:23 +0000

    Add package PyMarkovChain/1.7.5/PyMarkovChain-1.7.5.tar.gz

---
+++ packages/PyMarkovChain/PyMarkovChain-1.7.5.tar.gz/PyMarkovChain-1.7.5/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='PyMarkovChain',
+    version='1.7.5',
+    author='Max Wagner',
+    author_email='max@trollbu.de',
+    packages=['pymarkovchain',],
+    url='http://pypi.python.org/pypi/PyMarkovChain/',
+    license='LICENSE.txt',
+    description='Simple markov chain implementation',
+    long_description=open('README.md').read(),
+)
