59368b8c8879a90de30a1263d3bd0b458329bb6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:19 +0000

    Add package snf_image_creator/0.5.2/snf_image_creator-0.5.2.tar.gz

---
+++ packages/snf_image_creator/snf_image_creator-0.5.2.tar.gz/snf_image_creator-0.5.2/image_creator/version.py
@@ -0,0 +1,8 @@
+
+__version__ = "0.5.2"
+__version_vcs_info__ = {
+    'branch': 'hotfix-0.5.2',
+    'revid': '97a59b7',
+    'revno': 397}
+__version_user_email__ = "skalkoto@grnet.gr"
+__version_user_name__ = "Nikos Skalkotos"
