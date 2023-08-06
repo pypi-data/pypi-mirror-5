0ebcf30090113f8f9b75d9a11b6deaabb018ece1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:36 +0000

    Add package TemplateAlchemy/0.1.14/TemplateAlchemy-0.1.14.tar.gz

---
+++ packages/TemplateAlchemy/TemplateAlchemy-0.1.14.tar.gz/TemplateAlchemy-0.1.14/templatealchemy_driver/string.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#------------------------------------------------------------------------------
+# file: $Id$
+# lib:  templatealchemy_driver.string
+# auth: Philip J Grabner <grabner@cadit.com>
+# date: 2013/07/03
+# copy: (C) Copyright 2013 Cadit Health Inc., All Rights Reserved.
+#------------------------------------------------------------------------------
+
+from StringIO import StringIO
+from templatealchemy import api, util
+
+#------------------------------------------------------------------------------
+def loadSource(spec=None):
+  return StringSource(spec)
+
+#------------------------------------------------------------------------------
+class StringSource(api.Source):
+
+  #----------------------------------------------------------------------------
+  def __init__(self, spec, *args, **kw):
+    super(StringSource, self).__init__(self.ns('string', spec), *args, **kw)
+    self.data = spec
+
+  #----------------------------------------------------------------------------
+  def getSource(self, name):
+    return self
+
+  #----------------------------------------------------------------------------
+  def getFormats(self):
+    return ['data']
+
+  #----------------------------------------------------------------------------
+  def get(self, format):
+    return StringIO(self.data)
+
+  #----------------------------------------------------------------------------
+  def getRelated(self, name):
+    return None
+
+#------------------------------------------------------------------------------
+# end of $Id$
+#------------------------------------------------------------------------------
