4f0b7d26de253be52dcc086b52172c340f7bae68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:41 +0000

    Add package GitResultsManager/0.1/GitResultsManager-0.1.tar.gz

---
+++ packages/GitResultsManager/GitResultsManager-0.1.tar.gz/GitResultsManager-0.1/setup.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='GitResultsManager',
+      description='The GitResultsManager Python module and scripts (resman) for keeping track of research results using Git.',
+      version='0.1',
+      url='https://github.com/yosinski/GitResultsManager',
+      author='Jason Yosinski',
+      author_email='git_results_manager.jyo@0sg.net',
+      py_modules=['GitResultsManager'],
+      )
+
+
+print '''\nNote: you may still need to install other scripts.'''
+print '''Copy gitresman to your path.'''
