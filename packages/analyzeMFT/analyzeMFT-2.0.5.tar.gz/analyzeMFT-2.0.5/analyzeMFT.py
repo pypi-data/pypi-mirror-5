2f2bb0f900b358621056d142a4c27ffbdaae8e30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:59 +0000

    Add package analyzeMFT/2.0.5/analyzeMFT-2.0.5.tar.gz

---
+++ packages/analyzeMFT/analyzeMFT-2.0.5.tar.gz/analyzeMFT-2.0.5/analyzeMFT.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+try:
+     from analyzemft import mftsession
+except:
+     from .analyzemft import mftsession
+     
+if __name__=="__main__":
+
+     session = mftsession.MftSession()
+     session.mft_options()
+     session.open_files()
+     session.process_mft_file()
