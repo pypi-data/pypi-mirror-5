ab4bb15985e561624d4f1b0b5503456128f6a18d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:30 +0000

    Add package shttps/0.1/shttps-0.1.tar.gz

---
+++ packages/shttps/shttps-0.1.tar.gz/shttps-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name='shttps',
+        version='0.1',
+        packages=['',],
+        url='https://github.com/ggarber/shttps',
+        license='',
+        install_requires=[],
+        long_description='Simple HTTPS Server to run from command line and serve local files over https',
+        scripts=['scripts/shttps']
+)
